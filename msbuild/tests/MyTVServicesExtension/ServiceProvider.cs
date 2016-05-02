9d8eb97c6196f49e2085c2c0ab052503bc847bb4
Vincent Dondain <vincent.dondain@xamarin.com>
2016-05-02 16:47:48 -0400

    [msbuild/tests] Add tvOS extension test
    We now have MyTVServicesExtension project which
    comes from a simple tvOS extension template.
    It is attached to MyTVApp.
    
    The TVApp test has been updated to also build the extension.

---
+++ msbuild/tests/MyTVServicesExtension/ServiceProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Foundation;
+using TVServices;
+
+namespace MyTVServicesExtension
+{
+	public class ServiceProvider : NSObject, ITVTopShelfProvider
+	{
+		protected ServiceProvider (IntPtr handle) : base (handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public TVContentItem [] TopShelfItems {
+			[Export ("topShelfItems")]
+			get;
+		}
+
+		public TVTopShelfContentStyle TopShelfStyle {
+			[Export ("topShelfStyle")]
+			get;
+		}
+	}
+}
+
