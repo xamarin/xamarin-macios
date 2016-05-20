52aea54bc34abbbb5bc20f0526aefc8ca313fe6a
Sebastien Pouliot <sebastien@xamarin.com>
2016-05-20 13:28:30 -0400

    [tests] Move linker tests from maccore

---
+++ tests/linker-ios/link all/CalendarTest.cs
@@ -0,0 +1,42 @@
+// Copyright 2013 Xamarin Inc. All rights reserved.
+
+using System;
+using System.Globalization;
+#if XAMCORE_2_0
+using Foundation;
+#else
+using MonoTouch.Foundation;
+#endif
+using NUnit.Framework;
+
+namespace LinkAll.Calendars {
+
+	[TestFixture]
+	// we want the tests to be available because we use the linker
+	[Preserve (AllMembers = true)]
+	public class CalendarTest {
+
+		// application *MUST* be build with I18N.MidEast and I18N.Other (Thai)
+
+		[Test]
+		public void UmAlQura ()
+		{
+			var ci = CultureInfo.GetCultureInfo ("ar");
+			Assert.That (ci.Calendar.ToString (), Is.EqualTo ("System.Globalization.UmAlQuraCalendar"), "Calendar");
+		}
+
+		[Test]
+		public void Hijri ()
+		{
+			var ci = CultureInfo.GetCultureInfo ("ps");
+			Assert.That (ci.Calendar.ToString (), Is.EqualTo ("System.Globalization.HijriCalendar"), "Calendar");
+		}
+
+		[Test]
+		public void ThaiBuddhist ()
+		{
+			var ci = CultureInfo.GetCultureInfo ("th");
+			Assert.That (ci.Calendar.ToString (), Is.EqualTo ("System.Globalization.ThaiBuddhistCalendar"), "Calendar");
+		}
+	}
+}
---
+++ tests/linker-ios/link sdk/ReflectionTest.cs
@@ -0,0 +1,43 @@
+﻿// Copyright 2016 Xamarin Inc. All rights reserved.
+
+using System;
+using System.Reflection;
+#if XAMCORE_2_0
+using Foundation;
+#else
+using MonoTouch.Foundation;
+#endif
+using NUnit.Framework;
+
+namespace Linker.Shared.Reflection {
+
+	[TestFixture]
+	// we want the tests to be available because we use the linker
+	[Preserve (AllMembers = true)]
+	public class ReflectionTest {
+
+		public void MethodWithParameters (string firstParameter, int secondParameter)
+		{
+		}
+
+		[Test]
+		public void ParameterInfoName ()
+		{
+			// linker will disable the metadata removal optimization if that property is used by user code
+			// however it's used inside mscorlib.dll (and SDK) so it cannot be checked while testing
+			//Assert.Null (typeof (ParameterInfo).GetProperty ("Name"), "Name");
+
+			var mi = this.GetType ().GetMethod ("MethodWithParameters");
+			var p = mi.GetParameters ();
+#if DEBUG || !LINKALL
+			// this optimization is only applied for release builds (not debug ones)
+			// link sdk won't touch this assembly (user code) so the parameters will be available
+			Assert.That (p [0].ToString (), Is.EqualTo ("System.String firstParameter"), "1");
+			Assert.That (p [1].ToString (), Is.EqualTo ("Int32 secondParameter"), "2");
+#else
+			Assert.That (p [0].ToString (), Is.EqualTo ("System.String "), "1");
+			Assert.That (p [1].ToString (), Is.EqualTo ("Int32 "), "2");
+#endif
+		}
+	}
+}
