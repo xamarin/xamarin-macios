43da4642f16b1240b8925663972a7cf2cae4916e
Rolf Bjarne Kvinge <rolf@xamarin.com>
2016-05-31 19:15:45 +0200

    [monotouch-test] Implement a non-GUI version of TestRuntime.RunAsync that works on watchOS.

--- tests/monotouch-test/Main.cs
+++ tests/monotouch-test/Main.cs
@@ -270,7 +270,14 @@ class TestRuntime
 	public static bool RunAsync (DateTime timeout, Action action, Func<bool> check_completed)
 	{
 #if __WATCHOS__
-		throw new NotImplementedException ("TestRuntime.RunAsync");
+		NSTimer.CreateScheduledTimer (0.01, (v) => action ());
+		do {
+			if (timeout < DateTime.Now)
+				return false;
+			NSRunLoop.Main.RunUntil (NSDate.Now.AddSeconds (0.1));
+		} while (!check_completed ());
+
+		return true;
 #else
 		return MonoTouchFixtures.AppDelegate.RunAsync (timeout, action, check_completed);
 #endif
