d135612f591afff9a9bcfd464ee6eaaf0ac1d45f
Rolf Bjarne Kvinge <rolf@xamarin.com>
2016-06-06 12:48:53 +0200

    [tests] Implement support in xharness for running the simulator tests grouped by simulator, and write out an html report.

---
+++ tests/xharness/TestProject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace xharness
+{
+	public class TestProject
+	{
+		public string Path;
+		public bool IsExecutableProject;
+
+		public TestProject ()
+		{
+		}
+
+		public TestProject (string path, bool isExecutableProject = true)
+		{
+			Path = path;
+			IsExecutableProject = isExecutableProject;
+		}
+	}
+
+
+}
+
