2ab071d436cf3bb40c86343b5ea58d68b9080382
Alex Soto <alex@alexsoto.me>
2016-06-14 04:47:14 -0500

    [CallKit] Add CallKit framework

---
+++ src/callkit.cs
@@ -0,0 +1,509 @@
+﻿//
+// CallKit bindings
+//
+// Authors:
+//	Alex Soto  <alex.soto@xamarin.com>
+//
+// Copyright 2016 Xamarin Inc. All rights reserved.
+//
+
+using System;
+using XamCore.AVFoundation;
+using XamCore.CoreFoundation;
+using XamCore.Foundation;
+using XamCore.ObjCRuntime;
+
+// FIXME: This Api is supposed to be available on macOS 10.12
+// per apple headers but it uses types that are only available
+// in iOS filled radar://26786260 with apple
+// https://trello.com/c/afWXDZ3A
+#if !MONOMAC || XAMCORE_2_0
+namespace XamCore.CallKit {
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[Native]
+	public enum CXAuthorizationStatus : nint {
+		NotDetermined = 0,
+		Restricted = 1,
+		Denied = 2,
+		Authorized = 3
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[Native]
+	public enum CXCallDirectoryEnabledStatus : nint {
+		Unknown = 0,
+		Disabled = 1,
+		Enabled = 2
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[Native]
+	public enum CXErrorCode : nint {
+		UnknownError = 0
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[Native]
+	public enum CXErrorCodeIncomingCallError : nint {
+		Unknown = 0,
+		NotAuthorized = 1,
+		CallUuidAlreadyExists = 2,
+		FilteredByDoNotDisturb = 3,
+		FilteredByBlockList = 4
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[Native]
+	public enum CXErrorCodeRequestTransactionError : nint {
+		Unknown = 0,
+		NotAuthorized = 1,
+		UnknownCallProvider = 2,
+		EmptyTransaction = 3,
+		UnknownCallUuid = 4,
+		InvalidAction = 5,
+		MaximumCallGroupsReached = 6
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[Native]
+	public enum CXErrorCodeCallDirectoryManagerError : nint {
+		Unknown = 0,
+		NoExtensionFound = 1,
+		NoAttachmentFound = 2,
+		LoadingInterrupted = 3,
+		ParsingFailed = 4,
+		MaximumEntriesExceeded = 5,
+		ExtensionDisabled = 6
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[Native]
+	public enum CXPlayDtmfCallActionType : nint {
+		SingleTone = 1,
+		SoftPause = 2,
+		HardPause = 3
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[Native]
+	public enum CXCallEndedReason : nint {
+		Failed = 1,
+		RemoteEnded = 2,
+		Unanswered = 3
+	}
+
+	// FIXME: Should we put these constants somewhere else?
+	[Static]
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	interface CXErrorDomain {
+		[Field ("CXErrorDomain")]
+		NSString ErrorDomain { get; }
+
+		[Field ("CXErrorDomainIncomingCall")]
+		NSString IncomingCall { get; }
+
+		[Field ("CXErrorDomainRequestTransaction")]
+		NSString RequestTransaction { get; }
+
+		[Field ("CXErrorDomainCallDirectoryManager")]
+		NSString CallDirectoryManager { get; }
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[BaseType (typeof (NSObject))]
+	interface CXAction : NSCopying, NSSecureCoding {
+
+		[Export ("UUID", ArgumentSemantic.Copy)]
+		NSUuid Uuid { get; }
+
+		[Export ("complete", ArgumentSemantic.Assign)]
+		bool Complete { [Bind ("isComplete")] get; }
+
+		[Export ("timeoutDate", ArgumentSemantic.Strong)]
+		NSDate TimeoutDate { get; }
+
+		[Export ("fulfill")]
+		void Fulfill ();
+
+		[Export ("fail")]
+		void Fail ();
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[BaseType (typeof (CXCallAction))]
+	interface CXAnswerCallAction {
+
+		[Export ("fulfillWithDateConnected:")]
+		void Fulfill (NSDate dateConnected);
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[BaseType (typeof (NSObject))]
+	[DisableDefaultCtor]
+	interface CXCall {
+
+		[Export ("UUID", ArgumentSemantic.Copy)]
+		NSUuid Uuid { get; }
+
+		[Export ("outgoing")]
+		bool Outgoing { [Bind ("isOutgoing")] get; }
+
+		[Export ("onHold")]
+		bool OnHold { [Bind ("isOnHold")] get; }
+
+		[Export ("hasConnected")]
+		bool HasConnected { get; }
+
+		[Export ("hasEnded")]
+		bool HasEnded { get; }
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[BaseType (typeof (CXAction))]
+	[DisableDefaultCtor]
+	interface CXCallAction {
+
+		[Export ("callUUID", ArgumentSemantic.Copy)]
+		NSUuid CallUuid { get; }
+
+		[Export ("initWithCallUUID:")]
+		[DesignatedInitializer]
+		IntPtr Constructor (NSUuid callUUID);
+
+		// FIXME: Deprecated in xcode8 beta1: "Use -[CXAction fulfill] instead"
+		// Check if we should expose it later or if it gets removed from the header
+		//[Export ("fulfillWithResponse:")]
+		//void FulfillWithResponse ([NullAllowed] CXCallActionResponse response);
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[BaseType (typeof (NSObject))]
+	interface CXCallController {
+
+		[Export ("initWithQueue:")]
+		[DesignatedInitializer]
+		IntPtr Constructor (DispatchQueue queue);
+
+		[Export ("callObserver", ArgumentSemantic.Strong)]
+		CXCallObserver CallObserver { get; }
+
+		[Async]
+		[Export ("requestTransaction:completion:")]
+		void RequestTransaction (CXTransaction transaction, Action<NSError> completion);
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[BaseType (typeof (NSExtensionContext))]
+	interface CXCallDirectoryExtensionContext {
+
+		[Export ("addBlockingEntryWithNextSequentialPhoneNumber:")]
+		void AddBlockingEntry (string nextSequentialPhoneNumber);
+
+		[Export ("addIdentificationEntryWithNextSequentialPhoneNumber:label:")]
+		void AddIdentificationEntry (string nextSequentialPhoneNumber, string label);
+
+		[Async]
+		[Export ("completeRequestWithCompletionHandler:")]
+		void CompleteRequest ([NullAllowed] Action<bool> completion);
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[BaseType (typeof (NSObject))]
+	interface CXCallDirectoryManager {
+
+		[Export ("sharedInstance")]
+		CXCallDirectoryManager SharedInstance { get; }
+
+		[Async]
+		[Export ("reloadExtensionWithIdentifier:completionHandler:")]
+		void ReloadExtension (string identifier, [NullAllowed] Action<NSError> completion);
+
+		[Async]
+		[Export ("getEnabledStatusForExtensionWithIdentifier:completionHandler:")]
+		void GetEnabledStatusForExtension (string identifier, Action<CXCallDirectoryEnabledStatus, NSError> completion);
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[BaseType (typeof (NSObject))]
+	interface CXCallDirectoryProvider {
+
+		[Export ("beginRequestWithExtensionContext:")]
+		void BeginRequestWithExtensionContext (CXCallDirectoryExtensionContext context);
+	}
+
+	interface ICXCallObserverDelegate { }
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[Protocol, Model]
+	[BaseType (typeof (NSObject))]
+	interface CXCallObserverDelegate {
+
+		[Abstract]
+		[Export ("callObserver:callChanged:")]
+		void CallChanged (CXCallObserver callObserver, CXCall call);
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[BaseType (typeof (NSObject))]
+	interface CXCallObserver {
+
+		[Export ("calls", ArgumentSemantic.Copy)]
+		CXCall [] Calls { get; }
+
+		[Export ("setDelegate:queue:")]
+		void SetDelegate ([NullAllowed] ICXCallObserverDelegate aDelegate, [NullAllowed] DispatchQueue queue);
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[BaseType (typeof (NSObject))]
+	interface CXCallUpdate : NSCopying {
+
+		[NullAllowed, Export ("callerIdentifier")]
+		string CallerIdentifier { get; set; }
+
+		[NullAllowed, Export ("localizedCallerName")]
+		string LocalizedCallerName { get; set; }
+
+		[Export ("supportsHolding", ArgumentSemantic.Assign)]
+		bool SupportsHolding { get; set; }
+
+		[Export ("supportsGrouping", ArgumentSemantic.Assign)]
+		bool SupportsGrouping { get; set; }
+
+		[Export ("supportsUngrouping", ArgumentSemantic.Assign)]
+		bool SupportsUngrouping { get; set; }
+
+		[Export ("supportsDTMF", ArgumentSemantic.Assign)]
+		bool SupportsDtmf { get; set; }
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[BaseType (typeof (CXCallAction))]
+	interface CXEndCallAction {
+
+		[Export ("fulfillWithDateEnded:")]
+		void Fulfill (NSDate dateEnded);
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[BaseType (typeof (CXCallAction), Name = "CXPlayDTMFCallAction")]
+	interface CXPlayDtmfCallAction {
+
+		[Export ("digits")]
+		string Digits { get; set; }
+
+		[Export ("type", ArgumentSemantic.Assign)]
+		CXPlayDtmfCallActionType Type { get; set; }
+	}
+
+	interface ICXProviderDelegate { }
+
+	[Protocol, Model]
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[BaseType (typeof (NSObject))]
+	interface CXProviderDelegate {
+
+		[Export ("providerDidReset:")]
+		void ProviderDidReset (CXProvider provider);
+
+		[Export ("providerDidBegin:")]
+		void ProviderDidBegin (CXProvider provider);
+
+		[Export ("provider:executeTransaction:")]
+		bool ExecuteTransaction (CXProvider provider, CXTransaction transaction);
+
+		[Export ("provider:performStartCallAction:")]
+		void StartCallAction (CXProvider provider, CXStartCallAction action);
+
+		[Export ("provider:performAnswerCallAction:")]
+		void AnswerCallAction (CXProvider provider, CXAnswerCallAction action);
+
+		[Export ("provider:performEndCallAction:")]
+		void EndCallAction (CXProvider provider, CXEndCallAction action);
+
+		[Export ("provider:performSetHeldCallAction:")]
+		void SetHeldCallAction (CXProvider provider, CXSetHeldCallAction action);
+
+		[Export ("provider:performSetMutedCallAction:")]
+		void SetMutedCallAction (CXProvider provider, CXSetMutedCallAction action);
+
+		[Export ("provider:performSetGroupCallAction:")]
+		void SetGroupCallAction (CXProvider provider, CXSetGroupCallAction action);
+
+		[Export ("provider:performPlayDTMFCallAction:")]
+		void PlayDtmfCallAction (CXProvider provider, CXPlayDtmfCallAction action);
+
+		[Export ("provider:timedOutPerformingAction:")]
+		void TimedOutPerformingAction (CXProvider provider, CXAction action);
+
+		// FIXME: Header says this is available on macOS 10.12 but AVAudioSession is iOS only radar reported
+#if !MONOMAC
+		[Export ("provider:didActivateAudioSession:")]
+		void DidActivateAudioSession (CXProvider provider, AVAudioSession audioSession);
+
+		[Export ("provider:didDeactivateAudioSession:")]
+		void DidDeactivateAudioSession (CXProvider provider, AVAudioSession audioSession);
+#endif
+
+		[Export ("provider:didChangeAuthorizationStatus:")]
+		void DidChangeAuthorizationStatus (CXProvider provider, CXAuthorizationStatus authorizationStatus);
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[BaseType (typeof (NSObject))]
+	[DisableDefaultCtor]
+	interface CXProvider {
+
+		[Export ("authorizationStatus")]
+		CXAuthorizationStatus AuthorizationStatus { get; }
+
+		[Export ("initWithConfiguration:")]
+		[DesignatedInitializer]
+		IntPtr Constructor (CXProviderConfiguration configuration);
+
+		[Export ("setDelegate:queue:")]
+		void SetDelegate ([NullAllowed] ICXProviderDelegate aDdelegate, [NullAllowed] DispatchQueue queue);
+
+		[Export ("requestAuthorization")]
+		void RequestAuthorization ();
+
+		[Async]
+		[Export ("reportNewIncomingCallWithUUID:update:completion:")]
+		void ReportNewIncomingCall (NSUuid uuid, CXCallUpdate update, Action<NSError> completion);
+
+		[Export ("reportCallWithUUID:updated:")]
+		void ReportCall (NSUuid uuid, CXCallUpdate update);
+
+		[Export ("reportCallWithUUID:endedAtDate:reason:")]
+		void ReportCall (NSUuid uuid, [NullAllowed] NSDate dateEnded, CXCallEndedReason endedReason);
+
+		[Export ("reportOutgoingCallWithUUID:startedConnectingAtDate:")]
+		void ReportConnectingOutgoingCall (NSUuid uuid, [NullAllowed] NSDate dateStartedConnecting);
+
+		[Export ("reportOutgoingCallWithUUID:connectedAtDate:")]
+		void ReportConnectedOutgoingCall (NSUuid uuid, [NullAllowed] NSDate dateConnected);
+
+		[Export ("configuration", ArgumentSemantic.Copy)]
+		CXProviderConfiguration Configuration { get; set; }
+
+		[Export ("invalidate")]
+		void Invalidate ();
+
+		[Export ("pendingTransactions", ArgumentSemantic.Copy)]
+		CXTransaction [] PendingTransactions { get; }
+
+		[Export ("pendingCallActionsOfClass:withCallUUID:")]
+		CXCallAction [] GetPendingCallActions (Class callActionClass, NSUuid callUuid);
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[BaseType (typeof (NSObject))]
+	[DisableDefaultCtor]
+	interface CXProviderConfiguration : NSCopying {
+
+		[Export ("localizedName")]
+		string LocalizedName { get; }
+
+		[NullAllowed, Export ("ringtoneSound", ArgumentSemantic.Strong)]
+		string RingtoneSound { get; set; }
+
+		[Export ("maximumCallGroups")]
+		nuint MaximumCallGroups { get; set; }
+
+		[Export ("maximumCallsPerCallGroup")]
+		nuint MaximumCallsPerCallGroup { get; set; }
+
+		[Export ("supportsVideo")]
+		bool SupportsVideo { get; set; }
+
+		[Export ("initWithLocalizedName:")]
+		IntPtr Constructor (string localizedName);
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[BaseType (typeof (CXCallAction))]
+	interface CXSetGroupCallAction {
+
+		[NullAllowed, Export ("callUUIDToGroupWith", ArgumentSemantic.Assign)]
+		NSUuid CallUuidToGroupWith { get; set; }
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[BaseType (typeof (CXCallAction))]
+	interface CXSetHeldCallAction {
+
+		[Export ("onHold")]
+		bool OnHold { [Bind ("isOnHold")] get; set; }
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[BaseType (typeof (CXCallAction))]
+	interface CXSetMutedCallAction {
+
+		[Export ("muted")]
+		bool Muted { [Bind ("isMuted")] get; set; }
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[BaseType (typeof (CXCallAction))]
+	interface CXStartCallAction {
+		// FIXME: Apple header annotates this as TODO improve name? we sould check later
+		[Export ("destination")]
+		string Destination { get; set; }
+
+		[NullAllowed, Export ("contactIdentifier")]
+		string ContactIdentifier { get; set; }
+
+		[Export ("fulfillWithDateStarted:")]
+		void Fulfill (NSDate dateStarted);
+	}
+
+	[Introduced (PlatformName.iOS, 10, 0)]
+	[Introduced (PlatformName.MacOSX, 10, 12)]
+	[BaseType (typeof (NSObject))]
+	interface CXTransaction : NSCopying, NSSecureCoding {
+
+		[Export ("UUID", ArgumentSemantic.Copy)]
+		NSUuid Uuid { get; }
+
+		[Export ("complete", ArgumentSemantic.Assign)]
+		bool Complete { [Bind ("isComplete")] get; }
+
+		[Export ("actions", ArgumentSemantic.Copy)]
+		CXAction [] Actions { get; }
+
+		[Export ("addAction:")]
+		void AddAction (CXAction action);
+	}
+}
+#endif // !MONOMAC || XAMCORE_2_0
