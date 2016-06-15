29b47a8c7d1b5f4bd4d58ea21c299b624c2405f5
Alex Soto <alex@alexsoto.me>
2016-06-15 14:32:48 -0500

    [Speech] Add Speech Framework from Xcode 8 Beta 1
    * Added Speech Framework bindings
    * Ensured introspection test pass
    * Ensured we link against Speech framework
    * FIXME: SFSpeechRecordingRecognitionRequest is not in the public api
      filled radar://26799291 https://trello.com/c/s6s6YKua

--- tools/common/Frameworks.cs
+++ tools/common/Frameworks.cs
@@ -219,6 +219,7 @@ public class Frameworks : Dictionary <string, Framework>
 					{ "HealthKitUI", "HealthKitUI", 9,3 },
 
 					{ "CallKit", "CallKit", 10 },
+					{ "Speech", "Speech", 10 },
 				};
 			}
 			return ios_frameworks;
