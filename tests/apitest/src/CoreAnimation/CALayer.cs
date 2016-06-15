9a5d4a17afaaf95cff4aea4dd145949912607eff
Rolf Bjarne Kvinge <rolf@xamarin.com>
2016-06-15 08:53:13 -0700

    [apitest] Modify a couple of test to not take a screen image just to get an image.
    Taking a screen image does not work unless there's a window server
    running, which may not always be the case on bots.
    
    So instead just create an image manually, since these tests do
    not seem to be requiring any particular type of image.

--- tests/apitest/src/CoreAnimation/CALayer.cs
+++ tests/apitest/src/CoreAnimation/CALayer.cs
@@ -24,20 +24,23 @@ namespace Xamarin.Mac.Tests
 		public void CALayer_ValuesTests ()
 		{
 			CALayer layer = new CALayer ();
-			CGRect frame = new CGRect (10, 10, 0, 0);
-			CGImage image = CGImage.ScreenImage (0, frame);
-			NSImage NSImage = new NSImage ();
+			CGRect frame = new CGRect (10, 10, 10, 10);
+			using (var provider = new CGDataProvider (new byte [(int) frame.Width * (int) frame.Height * 4 ])) {
+				using (var image = new CGImage ((int) frame.Width, (int) frame.Height, 8, 32, (int) frame.Width * 4, CGColorSpace.CreateDeviceRGB (), CGBitmapFlags.None, provider, null, false, CGColorRenderingIntent.Default)) {
+					NSImage NSImage = new NSImage ();
 
-			layer.Contents = image;
-			CGImage arrayImage = layer.Contents;
-			Assert.AreEqual (image.Handle, arrayImage.Handle);
-			
-			layer.SetContents (NSImage);
-			NSImage arrayNSImage = layer.GetContentsAs<NSImage> ();
-			Assert.AreEqual (NSImage.Handle, arrayNSImage.Handle);
+					layer.Contents = image;
+					CGImage arrayImage = layer.Contents;
+					Assert.AreEqual (image.Handle, arrayImage.Handle);
+					
+					layer.SetContents (NSImage);
+					NSImage arrayNSImage = layer.GetContentsAs<NSImage> ();
+					Assert.AreEqual (NSImage.Handle, arrayNSImage.Handle);
 
-			layer.SetContents (null); // Should not throw
-			layer.Contents = null; // Should not throw
+					layer.SetContents (null); // Should not throw
+					layer.Contents = null; // Should not throw
+				}
+			}
 		}
 	}
 }
