feff7770fff24d36ece75d1a4602da121791834d
Chris Hamons <chris.hamons@xamarin.com>
2016-06-21 16:43:49 -0500

    Fix stub

--- tests/dontlink-mac/Stub.cs
+++ tests/dontlink-mac/Stub.cs
@@ -5,3 +5,5 @@ namespace MonoMacFixtures {
 	public class StubTest {
 		// dontlink-mac is currently obsolete due to movement of all tests to introspection tests
 		// But in the future, we'll add tests like iOS that actually test linking here 
+	}
+}
