97cab48a4138566e38afb82350c645047ca832e6
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2016-07-06 22:14:49 -0400

    [tests] Fix issues with monotouch-tests with beta2
    * TLSMaximumSupportedProtocol returns Unknown, which also means default,
      instead of SslProtocol.Tls_1_2
    
    * Setting NSZone.Name crash the app
    
    * The MDLMesh tests are broken in a different way than beta1

--- tests/monotouch-test/ModelIO/MDLMesh.cs
+++ tests/monotouch-test/ModelIO/MDLMesh.cs
@@ -105,13 +105,10 @@ namespace MonoTouchFixtures.ModelIO {
 
 			using (var obj = MDLMesh.CreatePlane (V2, V2i, MDLGeometryType.Triangles, null)) {
 				Assert.IsNotNull (obj, "obj");
-				if (TestRuntime.CheckXcodeVersion (8, 0)) {
-					Asserts.AreEqual (new MDLAxisAlignedBoundingBox { MaxBounds = new Vector3 (1.5f, 0, 0), MinBounds = new Vector3 (-1.5f, 0, 0) }, obj.BoundingBox, "BoundingBox");
-				} else {
-					Asserts.AreEqual (new MDLAxisAlignedBoundingBox { MaxBounds = new Vector3 (1.5f, 0, 1.5f), MinBounds = new Vector3 (-1.5f, 0, -1.5f) }, obj.BoundingBox, "BoundingBox");
-				}
+				Asserts.AreEqual (new MDLAxisAlignedBoundingBox { MaxBounds = new Vector3 (1.5f, 0, 1.5f), MinBounds = new Vector3 (-1.5f, 0, -1.5f) }, obj.BoundingBox, "BoundingBox");
 				Assert.AreEqual (1, obj.Submeshes.Count, "Submeshes Count");
-				Assert.AreEqual (1, obj.VertexBuffers.Length, "VertexBuffers Count");
+				var vb = TestRuntime.CheckXcodeVersion (8,0) ? 3 : 1;
+				Assert.AreEqual (vb, obj.VertexBuffers.Length, "VertexBuffers Count");
 				Assert.AreEqual (16, obj.VertexCount, "VertexCount");
 				Assert.AreEqual (31, obj.VertexDescriptor.Attributes.Count, "VertexDescriptor Attributes Count");
 				Assert.AreEqual (31, obj.VertexDescriptor.Layouts.Count, "VertexDescriptor Layouts Count");
@@ -142,12 +139,13 @@ namespace MonoTouchFixtures.ModelIO {
 			using (var obj = MDLMesh.CreateCylindroid (1, V2, 3, 1, MDLGeometryType.Triangles, true, null)) {
 				Assert.IsNotNull (obj, "obj");
 				if (TestRuntime.CheckXcodeVersion (8, 0)) {
-					Asserts.AreEqual (new MDLAxisAlignedBoundingBox { MaxBounds = new Vector3 (0.866025448f, 0.5f, 1f), MinBounds = new Vector3 (-0.866025388f, -0.5f, -0.5f) }, obj.BoundingBox, "BoundingBox");
+					Asserts.AreEqual (new MDLAxisAlignedBoundingBox { MaxBounds = new Vector3 (0.866025448f, 1.0f, 1f), MinBounds = new Vector3 (-0.866025388f, -1.0f, -0.5f) }, obj.BoundingBox, "BoundingBox");
+					Assert.AreEqual (3, obj.VertexBuffers.Length, "VertexBuffers Count");
 				} else {
 					Asserts.AreEqual (new MDLAxisAlignedBoundingBox { MaxBounds = new Vector3 (1f, 0.5f, 1f), MinBounds = new Vector3 (-0.866025388f, -0.5f, -0.866025388f) }, obj.BoundingBox, "BoundingBox");
+					Assert.AreEqual (1, obj.VertexBuffers.Length, "VertexBuffers Count");
 				}
 				Assert.AreEqual (1, obj.Submeshes.Count, "Submeshes Count");
-				Assert.AreEqual (1, obj.VertexBuffers.Length, "VertexBuffers Count");
 				Assert.AreEqual (18, obj.VertexCount, "VertexCount");
 				Assert.AreEqual (31, obj.VertexDescriptor.Attributes.Count, "VertexDescriptor Attributes Count");
 				Assert.AreEqual (31, obj.VertexDescriptor.Layouts.Count, "VertexDescriptor Layouts Count");
@@ -162,12 +160,13 @@ namespace MonoTouchFixtures.ModelIO {
 			using (var obj = MDLMesh.CreateEllipticalCone (5, V2, 3, 1, MDLGeometryType.Triangles, true, null)) {
 				Assert.IsNotNull (obj, "obj");
 				if (TestRuntime.CheckXcodeVersion (8, 0)) {
-					Asserts.AreEqual (new MDLAxisAlignedBoundingBox { MaxBounds = new Vector3 (0.866025448f, 0f, 1f), MinBounds = new Vector3 (-0.866025388f, -25f, -0.50000006f) }, obj.BoundingBox, "BoundingBox");
+					Asserts.AreEqual (new MDLAxisAlignedBoundingBox { MaxBounds = new Vector3 (0.433012783f, 4.5f, 0.5f), MinBounds = new Vector3 (-0.433012783f, -0.5f, -0.25f) }, obj.BoundingBox, "BoundingBox");
+					Assert.AreEqual (3, obj.VertexBuffers.Length, "VertexBuffers Count");
 				} else {
 					Asserts.AreEqual (new MDLAxisAlignedBoundingBox { MaxBounds = new Vector3 (0.866025448f, 0f, 1f), MinBounds = new Vector3 (-0.866025388f, -5f, -0.50000006f) }, obj.BoundingBox, "BoundingBox");
+					Assert.AreEqual (1, obj.VertexBuffers.Length, "VertexBuffers Count");
 				}
 				Assert.AreEqual (1, obj.Submeshes.Count, "Submeshes Count");
-				Assert.AreEqual (1, obj.VertexBuffers.Length, "VertexBuffers Count");
 				Assert.AreEqual (13, obj.VertexCount, "VertexCount");
 				Assert.AreEqual (31, obj.VertexDescriptor.Attributes.Count, "VertexDescriptor Attributes Count");
 				Assert.AreEqual (31, obj.VertexDescriptor.Layouts.Count, "VertexDescriptor Layouts Count");
