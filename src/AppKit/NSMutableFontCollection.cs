1086ab4241b67bb53324c3044e82efc098310837
Chris Hamons <chris.hamons@xamarin.com>
2016-07-07 14:29:00 -0500

    Fix missing ctor API break

---
+++ src/AppKit/NSMutableFontCollection.cs
@@ -0,0 +1,23 @@
+//
+// NSMutableFontCollection.cs
+//
+// Author:
+//  Chris Hamons <chris.hamons@xamarin.com>
+//
+// Copyright 2016 Xamarin Inc. (http://xamarin.com)
+
+
+using System;
+
+
+namespace XamCore.AppKit
+{
+#if !XAMCORE_4_0
+	public partial class NSMutableFontCollection
+	{
+		public NSMutableFontCollection ()
+		{
+		}
+	}
+#endif
+}
---
+++ src/AppKit/NSTextTableBlock.cs
@@ -0,0 +1,23 @@
+//
+// NSTextTableBlock.cs
+//
+// Author:
+//  Chris Hamons <chris.hamons@xamarin.com>
+//
+// Copyright 2016 Xamarin Inc. (http://xamarin.com)
+
+
+using System;
+
+
+namespace XamCore.AppKit
+{
+#if !XAMCORE_4_0
+	public partial class NSTextTableBlock
+	{
+		public NSTextTableBlock ()
+		{
+		}
+	}
+#endif
+}
