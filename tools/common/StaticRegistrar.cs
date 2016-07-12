06ae5381368a0d5cf42b1146bd86bd609318fbb1
Rolf Bjarne Kvinge <rolf@xamarin.com>
2016-07-13 00:30:19 +0200

    [registrar] Forward-declare ObjC classes. Fixes #42454.
    There can be circular dependencies between Objective-C classes,
    so make sure we don't fail compilation when that occurs by
    forward declaring any Objective-C classes/protocols.
    
    The test case in question does not contain a circular dependency,
    but the same issue occurs due to types not being generated in the
    correct order (a correct order could be constructed for the test
    case, but there's no general solution since circular dependencies
    can exist).
    
    https://bugzilla.xamarin.com/show_bug.cgi?id=42454

--- tools/common/StaticRegistrar.cs
+++ tools/common/StaticRegistrar.cs
@@ -2272,11 +2272,16 @@ namespace XamCore.Registrar {
 				var class_name = EncodeNonAsciiCharacters (@class.ExportedName);
 				var is_protocol = @class.IsProtocol;
 				if (@class.IsCategory) {
-					sb.Write ("@interface {0} ({1})", EncodeNonAsciiCharacters (@class.BaseType.ExportedName), @class.CategoryName);
+					var exportedName = EncodeNonAsciiCharacters (@class.BaseType.ExportedName);
+					sb.Write ("@interface {0} ({1})", exportedName, @class.CategoryName);
+					declarations.AppendFormat ("@class {0};\n", exportedName);
 				} else if (is_protocol) {
-					sb.Write ("@protocol ").Write (EncodeNonAsciiCharacters (@class.ProtocolName));
+					var exportedName = EncodeNonAsciiCharacters (@class.ProtocolName);
+					sb.Write ("@protocol ").Write (exportedName);
+					declarations.AppendFormat ("@protocol {0};\n", exportedName);
 				} else {
 					sb.Write ("@interface {0} : {1}", class_name, EncodeNonAsciiCharacters (@class.SuperType.ExportedName));
+					declarations.AppendFormat ("@class {0};\n", class_name);
 				}
 				bool any_protocols = false;
 				ObjCType tp = @class;
