65ffc88aa5a72a88be0e8a8c1060760d0129d9db
Manuel de la Pena <v-mandel@microsoft.com>
2016-08-11 14:22:59 +0200

    [AVFoundation] Update bindings for XCode8. Focus on enums and structs. (#591)

--- src/AVFoundation/AVAudioConverterPrimeInfo.cs
+++ src/AVFoundation/AVAudioConverterPrimeInfo.cs
@@ -34,5 +34,44 @@ namespace XamCore.AVFoundation {
 	public struct AVAudioConverterPrimeInfo {
 		public uint LeadingFrames;
 		public uint TrailingFrames;
+
+		public AVAudioConverterPrimeInfo (uint leadingFrames, uint trailingFrames)
+		{
+			LeadingFrames = leadingFrames;
+			TrailingFrames = trailingFrames;
+		}
+
+		public override string ToString ()
+		{
+			return $"({LeadingFrames}:{TrailingFrames})";
+		}
+
+		public static bool operator == (AVAudioConverterPrimeInfo left, AVAudioConverterPrimeInfo right)
+		{
+			return left.Equals (right);
+		}
+
+		public static bool operator != (AVAudioConverterPrimeInfo left, AVAudioConverterPrimeInfo right)
+		{
+			return !left.Equals (right);
+		}
+
+		public override bool Equals (object obj)
+		{
+			if (!(obj is AVAudioConverterPrimeInfo))
+				return false;
+
+			return this.Equals ((AVAudioConverterPrimeInfo)obj);
+		}
+
+		public bool Equals (AVAudioConverterPrimeInfo other)
+		{
+			return LeadingFrames == other.LeadingFrames && TrailingFrames == other.TrailingFrames;
+		}
+
+		public override int GetHashCode ()
+		{
+			return LeadingFrames.GetHashCode () ^ TrailingFrames.GetHashCode ();
+		}
 	}
 }
