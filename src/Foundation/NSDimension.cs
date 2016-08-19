c7abd28f2c07407602080527b6b9486743af995b
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2016-08-19 09:27:23 -0400

    [foundation] Fix NSDimension.BaseUnit on subclasses. Fixes #43444 (#623)
    This is a static selector that needs to be overridden, which is not the
    usual pattern in C#. We re-define it using [New] on subclasses.
    
    Unit tests added for all NSDimension subclasses.
    
    reference:
    https://bugzilla.xamarin.com/show_bug.cgi?id=43444

---
+++ src/Foundation/NSDimension.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace XamCore.Foundation {
+	
+	public partial class NSDimension {
+
+		// this is something that need to be overridden by subclasses, which is not something we _usually_ do in C#
+		// it is exposed here so we can throw a managed exception if some subclasses (e.g. user code) fails to override
+		// (re-declare with `new`) the static property
+		public static NSDimension BaseUnit {
+
+			get {
+				throw new NotImplementedException ("All subclasses of NSDimension must re-implement this property");
+			}
+		}
+	}
+}
