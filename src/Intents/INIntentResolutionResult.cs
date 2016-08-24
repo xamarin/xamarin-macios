9354e2044e2c1731a8d756e78e29a5b232ed411d
Alex Soto <alex@alexsoto.me>
2016-08-24 10:14:27 -0500

    [Intents] Fixes bug 43205 - SiriKit API Inconsistencies https://bugzilla.xamarin.com/show_bug.cgi?id=43205
    Per documentation[1] INIntentResolutionResult is an abstract class
    and now we honor that, also we now provide the right return type
    on each of the properties (NeedsValue, NotRequired and Unsupported)
    in all subclasses of INIntentResolutionResult.
    
    Unit tests added for all INIntentResolutionResult subclasses.
    
    [1]: https://developer.apple.com/reference/intents/inintentresolutionresult?language=objc

--- src/Intents/INIntentResolutionResult.cs
+++ src/Intents/INIntentResolutionResult.cs
@@ -23,5 +23,26 @@ namespace XamCore.Intents {
 		{
 		}
 	}
+
+	public partial class INIntentResolutionResult {
+
+		public static INIntentResolutionResult NeedsValue {
+			get {
+				throw new NotImplementedException ("All subclasses of INIntentResolutionResult must re-implement this property");
+			}
+		}
+
+		public static INIntentResolutionResult NotRequired {
+			get {
+				throw new NotImplementedException ("All subclasses of INIntentResolutionResult must re-implement this property");
+			}
+		}
+
+		public static INIntentResolutionResult Unsupported {
+			get {
+				throw new NotImplementedException ("All subclasses of INIntentResolutionResult must re-implement this property");
+			}
+		}
+	}
 }
 #endif // XAMCORE_2_0
