c2e679441156a171e7b5c647456e25864c475c55
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2016-08-28 11:29:35 -0400

    [tvos][scenekit] Fix binary compatibility with earlier versions (even if Apple removed some API)

--- src/SceneKit/SCNCompat.cs
+++ src/SceneKit/SCNCompat.cs
@@ -1,11 +1,12 @@
 // Copyright 2016 Xamarin Inc. All rights reserved.
 
-#if !XAMCORE_3_0
-
 using System;
+using XamCore.Foundation;
+using XamCore.ObjCRuntime;
 
 namespace XamCore.SceneKit {
 
+#if !XAMCORE_3_0
 	partial class SCNAction {
 
 		[Obsolete ("Use TimingFunction property")]
@@ -14,6 +15,21 @@ namespace XamCore.SceneKit {
 			TimingFunction = timingFunction;
 		}
 	}
-}
+#elif TVOS && !XAMCORE_4_0
+	partial class SCNMaterialProperty {
+	[Introduced (PlatformName.iOS, 8, 0)]
+		[Deprecated (PlatformName.iOS, 10, 0, message: "Deprecated")]
+		[Deprecated (PlatformName.TvOS, 10, 0, message: "Deprecated in iOS 10 but removed from tvOS 10")]
+		public virtual NSObject BorderColor { get; set; }
+	}
 
+	partial class SCNRenderer {
+		[Introduced (PlatformName.iOS, 8, 0)]
+		[Deprecated (PlatformName.iOS, 9, 0, message: "Deprecated")]
+		[Deprecated (PlatformName.TvOS, 10, 0, message: "Deprecated in iOS 9 but removed from tvOS 10")]
+		public virtual void Render ()
+		{
+		}
+	}
 #endif
+}
