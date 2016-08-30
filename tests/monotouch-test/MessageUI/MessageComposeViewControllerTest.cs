e63fcafe4c655e9257b2f7296829a5c0b325de3d
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2016-08-30 13:28:59 -0400

    [messageui] Review (and add test) the enum breaking change from Apple. Fixes #42682
    Xcode8 SDK changed untyped enums (always 32 bits) into NSInteger. In this
    case we can continue with the existing API (and ignore them in xtro)
    
    reference:
    https://bugzilla.xamarin.com/show_bug.cgi?id=42682

---
+++ tests/monotouch-test/MessageUI/MessageComposeViewControllerTest.cs
@@ -0,0 +1,48 @@
+﻿//
+// Unit tests for MFMessageComposeViewController
+//
+// Authors:
+//	Sebastien Pouliot <sebastien@xamarin.com>
+//
+// Copyright 2016 Xamarin Inc. All rights reserved.
+//
+
+#if !__TVOS__ && !__WATCHOS__
+
+using System;
+using System.Drawing;
+#if XAMCORE_2_0
+using Foundation;
+using UIKit;
+
+using MessageUI;
+#else
+using MonoTouch.Foundation;
+using MonoTouch.MessageUI;
+using MonoTouch.UIKit;
+#endif
+using NUnit.Framework;
+
+namespace MonoTouchFixtures.MessageUI {
+
+	[TestFixture]
+	[Preserve (AllMembers = true)]
+	public class MessageComposeViewControllerTest {
+
+		[Test]
+		public void MessageComposeDelegate ()
+		{
+			if (!MFMessageComposeViewController.CanSendText)
+				Assert.Inconclusive ("Not configured to send text");
+
+			using (var mail = new MFMessageComposeViewController ()) {
+				Assert.Null (mail.MessageComposeDelegate, "MessageComposeDelegate");
+				mail.Finished += (sender, e) => { };
+				Assert.NotNull (mail.MessageComposeDelegate, "MessageComposeDelegate");
+			}
+		}
+	}
+}
+
+#endif // !__TVOS__ && !__WATCHOS__
+
