f048f5bc6483b11e96aaad60c2292c299c5348b6
Manuel de la Pena <mandel@themacaque.com>
2016-09-05 18:53:15 +0200

    [AVFoundation] Add missing AVCapturePhotoOutput settings methods on ios 10.

--- src/avfoundation.cs
+++ src/avfoundation.cs
@@ -8105,6 +8105,14 @@ namespace XamCore.AVFoundation {
 		[Export ("DNGPhotoDataRepresentationForRawSampleBuffer:previewPhotoSampleBuffer:")]
 		[return: NullAllowed]
 		NSData GetDngPhotoDataRepresentation (CMSampleBuffer rawSampleBuffer, [NullAllowed] CMSampleBuffer previewPhotoSampleBuffer);
+
+		[iOS (10,0), NoTV, NoWatch, NoMac]
+		[Export ("preparedPhotoSettingsArray")]
+		AVCapturePhotoSettings[] PreparedPhotoSettings { get; }
+
+		[iOS (10,0), NoTV, NoWatch, NoMac]
+		[Export ("setPreparedPhotoSettingsArray:completionHandler:")]
+		void SetPreparedPhotoSettings (AVCapturePhotoSettings[] preparedPhotoSettingsArray, [NullAllowed] Action<bool, NSError> completionHandler);
 	}
 #endif
 	
