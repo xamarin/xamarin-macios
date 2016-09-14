ac28bd5480a74ad6fb40d68682d0f90b07de3050
Manuel de la Pena <mandel@themacaque.com>
2016-09-14 09:35:44 +0200

    [CoreMedia] Added missing tests.

--- src/CoreMedia/CMSync.cs
+++ src/CoreMedia/CMSync.cs
@@ -474,10 +474,8 @@ namespace XamCore.CoreMedia {
 		{
 		}
 
-		public CMClockOrTimebase (IntPtr handle)
+		public CMClockOrTimebase (IntPtr handle) : this (handle, false)
 		{
-			this.handle = handle;
-			CFObject.CFRetain (this.handle);
 		}
 
 		internal CMClockOrTimebase (IntPtr handle, bool owns)
