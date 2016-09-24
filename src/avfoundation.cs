7845caa577769e36f9a28a28637d6e48ff45e5c8
Alex Soto <alex@alexsoto.me>
2016-09-23 19:17:57 -0500

    [AVFoundaation] Fix bug 44322 - AVAssetDownloadUrlSession.CreateSession throws a System.InvalidCastException: Specified cast is not valid. https://bugzilla.xamarin.com/show_bug.cgi?id=44322
    AVAssetDownloadUrlSession.CreateSession according to headers it
    should return an AVAssetDownloadUrlSession but it is returning
    an apple internal type NSURLBackgroundSession so with our
    current bindings it throws an InvalidCastException, adding
    ForcedTypeAttribute will create the managed type wihout
    the actual typecheck. Added test verifing that the API
    no longer throws.

--- src/avfoundation.cs
+++ src/avfoundation.cs
@@ -10453,6 +10453,7 @@ namespace XamCore.AVFoundation {
 	[BaseType (typeof (NSUrlSession), Name = "AVAssetDownloadURLSession")]
 	interface AVAssetDownloadUrlSession {
 		[Static]
+		[ForcedType]
 		[Export ("sessionWithConfiguration:assetDownloadDelegate:delegateQueue:")]
 		AVAssetDownloadUrlSession CreateSession (NSUrlSessionConfiguration configuration, [NullAllowed] IAVAssetDownloadDelegate @delegate, [NullAllowed] NSOperationQueue delegateQueue);
 
