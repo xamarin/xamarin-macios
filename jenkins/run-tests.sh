63f0894bf44890f5644578c2988564e836ff8852
Manuel de la Pena <mandel@themacaque.com>
2016-10-25 12:49:02 +0200

    Added xslt to be used to keep the old Test Reports until we move to only Jenkins reports.

--- jenkins/run-tests.sh
+++ jenkins/run-tests.sh
@@ -2,7 +2,6 @@
 
 export BUILD_REVISION=jenkins
 cd $WORKSPACE
-export BUILD_REVISION=jenkins
 # Unlock
 security default-keychain -s builder.keychain
 security list-keychains -s builder.keychain
@@ -14,5 +13,12 @@ security set-keychain-settings -lut 7200
 # Run tests
 make -C tests jenkins
 
+# go through the diff xml results and do an xslt transformation for the old Test Report
+for i in tests/logs/*/*.xml; do
+	logname=$(basename "${i%.*}");
+	dirname=$(dirname "$i");
+	full_path="$dirname/$logname.log";
+	xsltproc jenkins/nunit-summary.xslt $i > $full_path;
+done
 # Lock
 security lock-keychain
