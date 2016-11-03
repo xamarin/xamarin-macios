bc4d5972a2fc9b774cc6d37b1edf02b1efcc148d
Rolf Bjarne Kvinge <rolf@xamarin.com>
2016-11-03 11:34:54 +0100

    [jenkins] Produce a comment file with descriptive information.
    Produce a comment file that will be posted to GitHub with more descriptive
    information than "Build succeeded/failed".

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash -e
+
+COMMENT_FILE=$WORKSPACE/jenkins-results/comments
+
 cd $WORKSPACE
 export BUILD_REVISION=jenkins
 ./configure --disable-ios-device
-time make world
+
+mkdir -p $(dirname $COMMENT_FILE)
+
+if time make world; then
+	RV=$?
+	echo "Build succeeded" > $COMMENT_FILE
+else
+	RV=$?
+	echo "Build failed" > $COMMENT_FILE
+fi
+
+exit $RV
