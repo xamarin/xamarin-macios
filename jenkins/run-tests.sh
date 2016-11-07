68701672686e1be5647a16790a26c3aea921b13f
Manuel de la Pena <mandel@themacaque.com>
2016-11-07 10:11:15 +0100

    Point to the correcto dir in jenkins.

--- jenkins/run-tests.sh
+++ jenkins/run-tests.sh
@@ -14,12 +14,12 @@ security set-keychain-settings -lut 7200
 make -C tests jenkins
 
 # go through the diff xml results and do an xslt transformation for the old Test Report
-for i in tests/logs/*/*.xml; do
+for i in jenkins-results/tests/*/*.xml; do
 	logname=$(basename "${i%.*}");
 	dirname=$(dirname "$i");
 	full_path="$dirname/$logname.log";
 	echo "Cleaning xml log on $full_path";
-	xsltproc jenkins/nunit-summary.xslt $i > $full_path;
+	xsltproc jenkins/nunit-summary.xslt $i > "$full_path";
 done
 # Lock
 security lock-keychain
