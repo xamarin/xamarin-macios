19ae097f79dbf4e152c611e4264e8bbbb60fe805
Jeffrey Stedfast <jestedfa@microsoft.com>
2016-11-16 16:20:51 -0500

    [msbuild] Log warnings and errors for the ibtool --link stage as well (#1188)
    * [msbuild] Log warnings and errors for the ibtool --link stage as well
    
    Fixes https://bugzilla.xamarin.com/show_bug.cgi?id=44674
    
    * [msbuild] Only need to log warnings and errors when ibtool returns error in --link phase
    
    * [msbuild] Don't log empty errors from ibtool
    
    * [msbuild] Moved LogWarningsAndErrors() logic into XcodeCompilerToolTask.Compile()
    
    * [msbuild] Tweak minor glitches
    
    - rc is not needed anymore since we don't *yet* do anything with the error code.
    - copy&paste issue on the LogError.

--- msbuild/Xamarin.MacDev.Tasks.Core/Tasks/ACToolTaskBase.cs
+++ msbuild/Xamarin.MacDev.Tasks.Core/Tasks/ACToolTaskBase.cs
@@ -250,7 +250,6 @@ namespace Xamarin.MacDev.Tasks
 			string bundleIdentifier = null;
 			var knownSpecs = new HashSet<string> ();
 			var specs = new PArray ();
-			int rc;
 
 			Log.LogTaskName ("ACTool");
 			Log.LogTaskProperty ("AppManifest", AppManifest);
@@ -379,21 +378,8 @@ namespace Xamarin.MacDev.Tasks
 			Directory.CreateDirectory (intermediateBundleDir);
 
 			// Note: Compile() will set the PartialAppManifest property if it is used...
-			if ((rc = Compile (catalogs.ToArray (), output, manifest)) != 0) {
-				if (File.Exists (manifest.ItemSpec)) {
-					try {
-						var log = PDictionary.FromFile (manifest.ItemSpec);
-
-						LogWarningsAndErrors (log, catalogs[0]);
-					} catch (FormatException) {
-						Log.LogError ("actool exited with code {0}", rc);
-					}
-
-					File.Delete (manifest.ItemSpec);
-				}
-
+			if ((Compile (catalogs.ToArray (), output, manifest)) != 0)
 				return false;
-			}
 
 			if (PartialAppManifest != null && !File.Exists (PartialAppManifest.GetMetadata ("FullPath")))
 				Log.LogError ("Partial Info.plist file was not generated: {0}", PartialAppManifest.GetMetadata ("FullPath"));
