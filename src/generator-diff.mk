30cf039f63b839c20ea8b00b1d5e9b3cc65ac404
Rolf Bjarne Kvinge <rolf@xamarin.com>
2016-11-23 12:29:44 +0100

    [src] Add makefile targets to calculate a diff of generated source code. (#1230)

---
+++ src/generator-diff.mk
@@ -0,0 +1,21 @@
+.generated_copy = rsync -qavz --exclude=*.dll --exclude=*.exe --exclude=*.mdb build generator-reference
+
+generator-reference:
+	@rm -rf $@
+	@rm -rf src/build
+	$(MAKE) all -j
+	@$(.generated_copy)
+	@cd $@ && git init . && git add --all && git commit -m init
+
+generator-diff:
+	@if [ ! -d generator-reference/.git ]; then \
+		echo "No generator reference built. Run 'make generator-reference' first."; \
+		exit 1; \
+	fi
+	@rm -rf generator-reference/*
+	@git clean -fxdq -e generator-reference
+	$(MAKE) all -j
+	@$(.generated_copy)
+	@cd generator-reference && git diff
+
+.PHONY: generator-reference generator-diff
