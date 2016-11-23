7f8c081789f334e88bca8f7413fc4a0f7503a6d6
Christian Helle <christian.helle@outlook.com>
2016-11-23 23:44:01 +0100

    Use DrawViewHierarchy to capture a snapshot of the current screen

--- src/UIKit/UIScreen.cs
+++ src/UIKit/UIScreen.cs
@@ -37,31 +37,43 @@ namespace XamCore.UIKit {
 
 		public UIImage Capture ()
 		{
-			// This is from: https://developer.apple.com/library/ios/#qa/qa2010/qa1703.html
-			var selScreen = new Selector ("screen");
-			var size = Bounds.Size;
-
-			UIGraphics.BeginImageContextWithOptions (size, false, 0);
-
-			try {
-				var context = UIGraphics.GetCurrentContext ();
-
-				foreach (var window in UIApplication.SharedApplication.Windows) {
-					if (window.RespondsToSelector (selScreen) && window.Screen != this)
-						continue;
-
-					context.SaveState ();
-					context.TranslateCTM (window.Center.X, window.Center.Y);
-					context.ConcatCTM (window.Transform);
-					context.TranslateCTM (-window.Bounds.Size.Width * window.Layer.AnchorPoint.X, -window.Bounds.Size.Height * window.Layer.AnchorPoint.Y);
-
-					window.Layer.RenderInContext (context);
-					context.RestoreState ();
+			if (UIDevice.CurrentDevice.CheckSystemVersion(7, 0)) {
+				// This is from https://developer.apple.com/library/content/qa/qa1817/_index.html
+				try {
+					var view = UIApplication.SharedApplication.KeyWindow;
+					UIGraphics.BeginImageContextWithOptions(view.Bounds.Size, view.Opaque, 0);
+					view.DrawViewHierarchy(view.Bounds, true);
+					return UIGraphics.GetImageFromCurrentImageContext();
+				} finally {
+					UIGraphics.EndImageContext();
 				}
+			} else {
+				// This is from: https://developer.apple.com/library/ios/#qa/qa2010/qa1703.html
+				var selScreen = new Selector("screen");
+				var size = Bounds.Size;
 
-				return UIGraphics.GetImageFromCurrentImageContext ();
-			} finally {
-				UIGraphics.EndImageContext ();
+				UIGraphics.BeginImageContextWithOptions(size, false, 0);
+
+				try {
+					var context = UIGraphics.GetCurrentContext();
+
+					foreach (var window in UIApplication.SharedApplication.Windows) {
+						if (window.RespondsToSelector(selScreen) && window.Screen != this)
+							continue;
+
+						context.SaveState();
+						context.TranslateCTM(window.Center.X, window.Center.Y);
+						context.ConcatCTM(window.Transform);
+						context.TranslateCTM(-window.Bounds.Size.Width * window.Layer.AnchorPoint.X, -window.Bounds.Size.Height * window.Layer.AnchorPoint.Y);
+
+						window.Layer.RenderInContext(context);
+						context.RestoreState();
+					}
+
+					return UIGraphics.GetImageFromCurrentImageContext();
+				} finally {
+					UIGraphics.EndImageContext ();
+				}
 			}
 		}
 	}
