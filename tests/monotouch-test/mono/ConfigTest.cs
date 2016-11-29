88e16560f48b4978230c3e60954642c69bd1d8fa
Rolf Bjarne Kvinge <rolf@xamarin.com>
2016-11-29 19:44:49 +0100

    [monotouch-test] Add test to ensure config files are properly bundled in apps.

---
+++ tests/monotouch-test/mono/ConfigTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+#if XAMCORE_2_0
+using Foundation;
+#else
+using MonoTouch;
+using MonoTouch.Foundation;
+#endif
+
+using NUnit.Framework;
+
+namespace MonoTouchFixtures
+{
+	[TestFixture]
+	[Preserve (AllMembers = true)]
+	public partial class ConfigTest
+	{
+		[Test]
+		public void Existence ()
+		{
+			var config_file = Assembly.GetExecutingAssembly ().Location + ".config";
+			Assert.True (File.Exists (config_file), "existence");
+			Assert.That (File.ReadAllText (config_file), Contains.Substring ("<secretMessage>Xamarin rocks</secretMessage>"), "content");
+		}
+	}
+}
+
