33e778bd660508586c2bff61649e66999c19def3
Rolf Bjarne Kvinge <rolf@xamarin.com>
2016-12-03 01:53:36 +0100

    Bump maccore to get new mlaunch. (#1296)
    * Bump maccore to get new mlaunch.
    
    A new mlaunch that:
    
    * Should have fewer random failures when launching watchOS apps.
    * Supports launching extensions on device.
    * Supports uninstalling apps from devices.

--- tests/xharness/Harness.cs
+++ tests/xharness/Harness.cs
@@ -91,7 +91,7 @@ namespace xharness
 		string DownloadMlaunch ()
 		{
 			// NOTE: the filename part in the url must be unique so that the caching logic works properly.
-			var mlaunch_url = "http://bosstoragemirror.blob.core.windows.net/public-builder/mlaunch/mlaunch-320d91b71c71c4768184c2d2b1ce553c1364970f.zip";
+			var mlaunch_url = "http://bosstoragemirror.blob.core.windows.net/public-builder/mlaunch/mlaunch-63121a575eff6fa291c28d0f70bacff97b0ecc72.zip";
 			var extraction_dir = Path.Combine (Path.GetTempPath (), Path.GetFileNameWithoutExtension (mlaunch_url));
 			var mlaunch_path = Path.Combine (extraction_dir, "bin", "mlaunch");
 
