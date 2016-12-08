2a5c4aa6d0d26cfe617041511b57a71e7b2c17c3
Rolf Bjarne Kvinge <rolf@xamarin.com>
2016-12-08 17:59:08 +0100

    [avfoundation] Fix incorrect selector for AVPlayerItemVideoOutput .ctor. Fixes #48382 (#1276) (#1321)
    Commit ba37aa44 workaround around a signature clash incorrectly and
    turned the selector to static ones (and incorrectly set the handle)
    
    Also fix a typo in the [Advice] attribute of the old API
    
    https://bugzilla.xamarin.com/show_bug.cgi?id=48382

---
+++ tests/monotouch-test/AVFoundation/PlayerItemVideoOutputTest.cs
@@ -0,0 +1,44 @@
+﻿//
+// Unit tests for AVPlayerItemVideoOutput
+//
+// Authors:
+//	Sebastien Pouliot <sebastien@xamarin.com>
+//
+// Copyright 2016 Xamarin Inc. All rights reserved.
+//
+
+#if !__WATCHOS__
+
+using System;
+#if XAMCORE_2_0
+using AVFoundation;
+using CoreVideo;
+using Foundation;
+using UIKit;
+#else
+using MonoTouch.AVFoundation;
+using MonoTouch.CoreVideo;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+#endif
+using NUnit.Framework;
+
+namespace MonoTouchFixtures.AVFoundation {
+
+	[TestFixture]
+	[Preserve (AllMembers = true)]
+	public class PlayerItemVideoOutputTest {
+
+		[Test]
+		public void Ctor_CVPixelBufferAttributes ()
+		{
+			var attributes = new CVPixelBufferAttributes () {
+				PixelFormatType = CVPixelFormatType.CV32BGRA
+			};
+			using (var output = new AVPlayerItemVideoOutput (attributes))
+				Assert.That (output.Handle, Is.Not.EqualTo (IntPtr.Zero), "valid");
+		}
+	}
+}
+
+#endif // !__WATCHOS__
