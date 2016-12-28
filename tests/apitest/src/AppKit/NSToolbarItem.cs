643becb9374ce97cad7519160735075324f1e956
Chris Hamons <chris.hamons@xamarin.com>
2016-12-28 11:51:17 -0600

    Fix test

--- tests/apitest/src/AppKit/NSToolbarItem.cs
+++ tests/apitest/src/AppKit/NSToolbarItem.cs
@@ -18,10 +18,17 @@ namespace Xamarin.Mac.Tests
 		[Test]
 		public void InitTests ()
 		{
-			NSSToolbarItem item = new NSToolbarItem ();
-			Assert.IsNotNull (item);
+			const string TestLabel = "NSToolbarItemTests.Label";
+			NSToolbarItem item = new NSToolbarItem ();
+			Assert.IsNotNull (item.Handle, "NSToolbarItem has handle");
+			item.Label = TestLabel;
+			Assert.AreEqual (item.Label, TestLabel, "NSToolbarItem has non null Label");
+
 			NSToolbarItemGroup group = new NSToolbarItemGroup ();
-			Assert.IsNotNull (group);
+			Assert.IsNotNull (group.Handle, "NSToolbarItemGroup has handle");
+			Assert.AreEqual (group.Subitems.Length, 0, "NSToolbarItemGroup has zero items");
+			group.Label = TestLabel;
+			Assert.AreEqual (group.Label, TestLabel, "NSToolbarItemGroup has non null Label");
 		}
 	}
 }
