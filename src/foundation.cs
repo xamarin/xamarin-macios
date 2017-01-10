b4266d66a8ff14820b25d17a4cec8d5c151767ea
Timothy Risi <timothy.risi@xamarin.com>
2017-01-10 12:41:41 -0900

    Fix NSFormatter.IsPartialStringValid. Fixes #50407 (#1456)
    https://bugzilla.xamarin.com/show_bug.cgi?id=50407

--- src/foundation.cs
+++ src/foundation.cs
@@ -2131,7 +2131,7 @@ namespace XamCore.Foundation
 		bool IsPartialStringValid (string partialString, out string newString, out NSString error);
 
 		[Export ("isPartialStringValid:proposedSelectedRange:originalString:originalSelectedRange:errorDescription:")]
-		unsafe bool IsPartialStringValid (out string partialString, out NSRange proposedSelRange, string origString, NSRange origSelRange, out NSString error);
+		bool IsPartialStringValid (ref string partialString, out NSRange proposedSelRange, string origString, NSRange origSelRange, out string error);
 	}
 
 	[BaseType (typeof (NSObject))]
