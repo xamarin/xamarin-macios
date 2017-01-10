06c79eb5b032b5d25c9e0519761a4e76faf8b308
Timothy Risi <timothy.risi@gmail.com>
2017-01-10 09:30:44 -0900

    Fix obsolete message

--- src/Foundation/NSFormatter.cs
+++ src/Foundation/NSFormatter.cs
@@ -3,7 +3,7 @@
 namespace XamCore.Foundation {
 	public partial class NSFormatter {
 #if !XAMCORE_4_0
-		[Obsolete ("Use IsPartialStringValid (ref NSString partialString, out NSRange proposedSelRange, string origString, NSRange origSelRange, out NSString error); instead")]
+		[Obsolete ("Use IsPartialStringValid (ref string partialString, out NSRange proposedSelRange, string origString, NSRange origSelRange, out string error) instead")]
 		public virtual bool IsPartialStringValid (out string partialString, out NSRange proposedSelRange, string origString, NSRange origSelRange, out NSString error)
 		{
 			partialString = origString;
