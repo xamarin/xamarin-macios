01039c8679780abdda1191810bcf972b74dfc297
Vincent Dondain <VincentDondain@users.noreply.github.com>
2017-01-27 09:39:47 -0500

    [foundation] Update to iOS 10.3 beta 1 (#1562)

--- src/Foundation/Enum.cs
+++ src/Foundation/Enum.cs
@@ -425,6 +425,7 @@ namespace XamCore.Foundation  {
 		FileIsDirectory = 		-1101,
 		NoPermissionsToReadFile = 	-1102,
 		DataLengthExceedsMaximum =	-1103,
+		FileOutsideSafeArea = 	-1104,
 
 		SecureConnectionFailed = 		-1200,
 		ServerCertificateHasBadDate = 	-1201,
