6f4f54a79024647d5aceb09918507f55dd3ebd1e
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-02-01 15:43:45 +0100

    [mtouch] Extensions don't need a NOTICE file, since it's already in the container app.

--- tools/mtouch/Application.cs
+++ tools/mtouch/Application.cs
@@ -1365,7 +1365,7 @@ namespace Xamarin.Bundler {
 
 		void WriteNotice ()
 		{
-			if (!IsDeviceBuild)
+			if (!IsDeviceBuild || IsExtension)
 				return;
 
 			if (Directory.Exists (Path.Combine (AppDirectory, "NOTICE")))
