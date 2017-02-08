b710d5ff1e01b10a280255c05e31354c223ce74d
Timothy Risi <timothy.risi@xamarin.com>
2017-02-08 09:47:18 -0900

    Remove MediaPlayer from XM Classic and 32-bit Unified (#1579)
    Make new MediaPlayer APIs 64 bit only and remove from classic

--- src/MediaPlayer/MPRemoteCommandCenter.cs
+++ src/MediaPlayer/MPRemoteCommandCenter.cs
@@ -1,11 +1,12 @@
 using System;
 
 namespace XamCore.MediaPlayer {
+#if !MONOMAC
 	public partial class MPRemoteCommandCenter {
-
 		[Obsolete ("Use MPRemoteCommandCenter.Shared")]
 		public MPRemoteCommandCenter ()
 		{
 		}
 	}
+#endif
 }
