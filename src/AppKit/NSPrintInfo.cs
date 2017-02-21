42d1f0752f0d43a6df33f325d7d3ac18a8712732
Timothy Risi <timothy.risi@gmail.com>
2017-02-21 13:52:57 -0900

    Add helper properties for the PrintCore methods that return the correct types instead ofead of IntPtrs

---
+++ src/AppKit/NSPrintInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using XamCore.PrintCore;
+
+namespace XamCore.AppKit {
+	public partial class NSPrintInfo {
+		public PMPrintSession PMPrintSession {
+			get {
+				var ptr = GetPMPrintSession ();
+				return new PMPrintSession (ptr, false);
+			}
+		}
+
+		public PMPageFormat PMPageFormat {
+			get {
+				var ptr = GetPMPageFormat ();
+				return new PMPageFormat (ptr, false);
+			}
+		}
+
+		public PMPrintSettings PMPrintSettings {
+			get {
+				var ptr = GetPMPrintSettings ();
+				return new PMPrintSettings (ptr, false);
+			}
+		}
+	}
+}
