a477eb66299a8a62abdf52700de3af3eb466caf0
Timothy Risi <timothy.risi@gmail.com>
2017-02-23 10:14:29 -0900

    NSPrintInfo make bindings internal and change manual properties to getter methods

--- src/AppKit/NSPrintInfo.cs
+++ src/AppKit/NSPrintInfo.cs
@@ -3,25 +3,22 @@ using XamCore.PrintCore;
 
 namespace XamCore.AppKit {
 	public partial class NSPrintInfo {
-		public PMPrintSession PMPrintSession {
-			get {
-				var ptr = GetPMPrintSession ();
-				return new PMPrintSession (ptr, false);
-			}
+		public PMPrintSession GetPrintSession ()
+		{
+			var ptr = GetPMPrintSession ();
+			return new PMPrintSession (ptr, false);
 		}
 
-		public PMPageFormat PMPageFormat {
-			get {
-				var ptr = GetPMPageFormat ();
-				return new PMPageFormat (ptr, false);
-			}
+		public PMPageFormat GetPageFormat ()
+		{
+			var ptr = GetPMPageFormat ();
+			return new PMPageFormat (ptr, false);
 		}
 
-		public PMPrintSettings PMPrintSettings {
-			get {
-				var ptr = GetPMPrintSettings ();
-				return new PMPrintSettings (ptr, false);
-			}
+		public PMPrintSettings GetPrintSettings ()
+		{
+			var ptr = GetPMPrintSettings ();
+			return new PMPrintSettings (ptr, false);
 		}
 	}
 }
