9988a9df35020a0ebccb1cdbe33906ee63432853
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-03-01 11:54:38 +0100

    [Foundation] Set 'sentRequest' when sending a request in NSUrlSessionHandler.
    Fixes this compiler warning:
    
    > [..]/external/mono/mcs/class/System.Net.Http/HttpClientEx.cs(50,8): warning CS0649: Field `Foundation.NSUrlSessionHandler.sentRequest' is never assigned to, and will always have its default value `false'
    
    However it changes the runtime behavior, and we'll now throw an exception in
    cases that we accepted before:
    
    * `sentRequest` is only read in `EnsureModifiability ()`, which throws an
      exception if `sentRequest` is true.
    * Previously `sentRequest` was never set (thus the compiler warning), which
      meant `EnsureModifiability` would never throw an exception.
    * Looking at the similar `CFNetworkHandler` (which has the identical field and
      methods), it seems that the intended behavior is to set `sentRequest` in
      `SendAsync`, and then `EnsureModifiability` is called whenever a property is
      set to ensure the property isn't set too late (and any change would be
      ignored because the request was already sent).
    * This means that previously setting any property after the request was sent
      would not throw any exceptions (even though the change would be ignored),
      while with this change we'd start throwing exceptions.

--- src/Foundation/NSUrlSessionHandler.cs
+++ src/Foundation/NSUrlSessionHandler.cs
@@ -169,6 +169,8 @@ namespace Foundation {
 #endif
 		protected override async Task<HttpResponseMessage> SendAsync (HttpRequestMessage request, CancellationToken cancellationToken)
 		{
+			sentRequest = true;
+
 			var nsrequest = await CreateRequest (request).ConfigureAwait(false);
 			var dataTask = session.CreateDataTask (nsrequest);
 
