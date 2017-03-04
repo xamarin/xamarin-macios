8c032ec5edf9c1780b79a29f41d10b40f9f62811
Sebastien Pouliot <sebastien@xamarin.com>
2017-03-04 13:12:19 -0500

    [tests][intro] Also check for generics in delegates, ref: #42699

--- tests/introspection/iOS/iOSApiPInvokeTest.cs
+++ tests/introspection/iOS/iOSApiPInvokeTest.cs
@@ -110,23 +110,32 @@ namespace Introspection {
 		[Test]
 		public void MonoPInvokeCallback ()
 		{
-			var pinvokeCallbacks = from type in Assembly.GetTypes () where !Skip (type)
-				from mi in type.GetMethods (BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static)
-				let attr = mi.GetCustomAttribute<MonoPInvokeCallbackAttribute> ()
-				where attr != null && !Skip (mi)
-				select mi;
-
 			var failed_api = new List<string> ();
 			Errors = 0;
 			int c = 0, n = 0;
-			foreach (var mi in pinvokeCallbacks) {
-				if (LogProgress)
-					Console.WriteLine ("{0}. {1}", c++, mi);
+			foreach (var type in Assembly.GetTypes ()) {
+				if (Skip (type))
+					continue;
+				foreach (var mi in type.GetMethods (BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static)) {
+					if (mi.DeclaringType != type)
+						continue;
+					if (Skip (mi))
+						continue;
+					var attr = mi.GetCustomAttribute<MonoPInvokeCallbackAttribute> ();
+					if (attr == null)
+						continue;
+					
+					if (LogProgress)
+						Console.WriteLine ("{0}. {1}", c++, mi);
 
-				var attr = mi.GetCustomAttribute<MonoPInvokeCallbackAttribute> ();
-				foreach (var m in attr.DelegateType.GetMethods ())
-					CheckSignature (m);
-				n++;
+					var at = attr.DelegateType;
+					foreach (var m in at.GetMethods ()) {
+						if (m.DeclaringType != at)
+							continue;
+						CheckSignature (m);
+					}
+					n++;
+				}
 			}
 			Assert.AreEqual (0, Errors, "{0} errors found in {1} native delegate validated: {2}", Errors, n, string.Join (", ", failed_api));
 		}
