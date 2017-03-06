0a844d7d50f130c4902bc5a94ff12cb47354cd31
Jeffrey Stedfast <jestedfa@microsoft.com>
2017-03-06 14:56:20 -0500

    [msbuild] Added logging for all of the overridden MSBuild default tasks

--- msbuild/Xamarin.MacDev.Tasks/MsBuildTasks/Move.cs
+++ msbuild/Xamarin.MacDev.Tasks/MsBuildTasks/Move.cs
@@ -1,6 +1,17 @@
-﻿namespace Microsoft.Build.Tasks
+﻿using Xamarin.MacDev.Tasks;
+
+namespace Microsoft.Build.Tasks
 {
 	public class Move : MoveTaskBase
 	{
+		public override bool Execute ()
+		{
+			Log.LogTaskName ("Move");
+			Log.LogTaskProperty ("DestinationFiles", DestinationFiles);
+			Log.LogTaskProperty ("DestinationFolder", DestinationFolder);
+			Log.LogTaskProperty ("SourceFiles", SourceFiles);
+
+			return base.Execute ();
+		}
 	}
 }
--- msbuild/Xamarin.MacDev.Tasks/MsBuildTasks/Touch.cs
+++ msbuild/Xamarin.MacDev.Tasks/MsBuildTasks/Touch.cs
@@ -1,6 +1,18 @@
-﻿namespace Microsoft.Build.Tasks
+﻿using Xamarin.MacDev.Tasks;
+
+namespace Microsoft.Build.Tasks
 {
 	public class Touch : TouchBase
 	{
+		public override bool Execute ()
+		{
+			Log.LogTaskName ("Touch");
+			Log.LogTaskProperty ("AlwaysCreate", AlwaysCreate);
+			Log.LogTaskProperty ("Files", Files);
+			Log.LogTaskProperty ("ForceTouch", ForceTouch);
+			Log.LogTaskProperty ("Time", Time);
+
+			return base.Execute ();
+		}
 	}
 }
