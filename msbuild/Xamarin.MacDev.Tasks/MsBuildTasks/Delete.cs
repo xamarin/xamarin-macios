694bd212b3b232607a86b9479d4955d9f80247a7
Jeffrey Stedfast <jestedfa@microsoft.com>
2017-03-07 17:40:26 -0500

    [msbuild] Added logging for all of the overridden MSBuild default tasks (#1829)

--- msbuild/Xamarin.MacDev.Tasks/MsBuildTasks/Delete.cs
+++ msbuild/Xamarin.MacDev.Tasks/MsBuildTasks/Delete.cs
@@ -1,6 +1,16 @@
-﻿namespace Microsoft.Build.Tasks
+﻿using Xamarin.MacDev.Tasks;
+
+namespace Microsoft.Build.Tasks
 {
 	public class Delete : DeleteBase
 	{
+		public override bool Execute ()
+		{
+			Log.LogTaskName ("Delete");
+			Log.LogTaskProperty ("Files", Files);
+			Log.LogTaskProperty ("TreatErrorsAsWarnings", TreatErrorsAsWarnings);
+
+			return base.Execute ();
+		}
 	}
 }
--- msbuild/Xamarin.MacDev.Tasks/MsBuildTasks/RemoveDir.cs
+++ msbuild/Xamarin.MacDev.Tasks/MsBuildTasks/RemoveDir.cs
@@ -1,6 +1,15 @@
+using Xamarin.MacDev.Tasks;
+
 namespace Microsoft.Build.Tasks
 {
 	public class RemoveDir : RemoveDirBase
 	{
+		public override bool Execute ()
+		{
+			Log.LogTaskName ("RemoveDir");
+			Log.LogTaskProperty ("Directories", Directories);
+
+			return base.Execute ();
+		}
 	}
 }
