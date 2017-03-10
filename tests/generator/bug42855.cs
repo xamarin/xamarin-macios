e6bd9a6bebb0a92b7270914bb251bd02f9b81dc6
Alex Soto <alex@alexsoto.me>
2017-03-09 19:22:48 -0600

    [generator] Fixes bug 42855 generator should report an error for [Protocol] without a [Model] that specify a [BaseType]
    https://bugzilla.xamarin.com/show_bug.cgi?id=42855
    
    We had some logic to catch this but we did not do anything with it.
    We now error out with BI1060 whenever we find [Model] but no [BaseType]
    added test case.
    
    I am using BI1060 because Rolf reserved today the missing ones in PR
    https://github.com/xamarin/xamarin-macios/pull/1849

---
+++ tests/generator/bug42855.cs
@@ -0,0 +1,16 @@
+using System;
+using Foundation;
+
+namespace Bug42855Tests {
+
+	[Protocol][Model]
+	interface MyFooClass {
+
+		[Abstract]
+		[Export ("stringMethod:")]
+		NSString StringMethod (int arg1);
+
+		[Export ("stringMethod2:")]
+		NSString StringMethod2 (int arg1);
+	}
+}
