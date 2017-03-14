b8d412b838f3b67bcfa1d73735f3fa86424c7ce1
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-03-14 14:43:34 +0100

    [Darwin] Fix kqueue/kevent bindings to actually work.
    Fix kqueue/kevent bindings to actually work. The P/Invoke signatures were
    badly broken (missing an argument), so there's no way this could ever have
    worked.
    
    Additionally obsoletes kevent signatures that return bool (they're ignoring
    vital information: how many events were actually returned from kevent), and
    add overloads that do the right thing.

---
+++ tests/apitest/src/Darwin/KernelNotificationTest.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Diagnostics;
+
+#if XAMCORE_2_0
+using Darwin;
+#else
+using MonoMac.Darwin;
+#endif
+
+using NUnit.Framework;
+
+namespace apitest
+{
+	[TestFixture]
+	public class KernelNotificationTest
+	{
+		KernelEvent [] CreateEvents (Process process)
+		{
+			return new KernelEvent [] {
+				new KernelEvent {
+					Ident = (IntPtr) process.Id,
+					Filter = EventFilter.Proc,
+					Flags = EventFlags.Add,
+					FilterFlags = (uint) (FilterFlags.ProcExit),
+				}
+			};
+		}
+
+		[Test]
+		public void KEvent ()
+		{
+			// (KernelEvent[], KernelEvent[])
+			using (var sleep = Process.Start ("/bin/sleep", "0.5")) {
+				using (var kqueue = new KernelQueue ()) {
+					var events = CreateEvents (sleep);
+					Assert.IsTrue (kqueue.KEvent (events, events), "kevent");
+				}
+			}
+
+			// (KernelEvent[], KernelEvent[], TimeSpan?)
+			using (var sleep = Process.Start ("/bin/sleep", "0.5")) {
+				using (var kqueue = new KernelQueue ()) {
+					var events = CreateEvents (sleep);
+					Assert.AreEqual (1, kqueue.KEvent (events, events, TimeSpan.FromSeconds (5)), "kevent");
+				}
+			}
+
+			// (KernelEvent[], KernelEvent[], TimeSpan?)
+			using (var sleep = Process.Start ("/bin/sleep", "0.5")) {
+				using (var kqueue = new KernelQueue ()) {
+					var events = CreateEvents (sleep);
+					Assert.AreEqual (1, kqueue.KEvent (events, events, null), "kevent");
+				}
+			}
+
+			// (KernelEvent[], int, KernelEvent[], int, TimeSpec?)
+			using (var sleep = Process.Start ("/bin/sleep", "0.5")) {
+				using (var kqueue = new KernelQueue ()) {
+					var events = CreateEvents (sleep);
+					TimeSpec ts = new TimeSpec
+					{
+						Seconds = 5,
+					};
+					Assert.AreEqual (1, kqueue.KEvent (events, events.Length, events, events.Length, ts), "kevent");
+				}
+			}
+
+			// (KernelEvent[], int, KernelEvent[], int, TimeSpec?)
+			using (var sleep = Process.Start ("/bin/sleep", "0.5")) {
+				using (var kqueue = new KernelQueue ()) {
+					var events = CreateEvents (sleep);
+					Assert.AreEqual (1, kqueue.KEvent (events, events.Length, events, events.Length, null), "kevent");
+				}
+			}
+
+			// (KernelEvent[], int, KernelEvent[], int, ref TimeSpec)
+			using (var sleep = Process.Start ("/bin/sleep", "0.5")) {
+				using (var kqueue = new KernelQueue ()) {
+					var events = CreateEvents (sleep);
+					TimeSpec ts = new TimeSpec
+					{
+						Seconds = 5,
+					};
+					Assert.IsTrue (kqueue.KEvent (events, events.Length, events, events.Length, ref ts), "kevent");
+				}
+			}
+
+			// (KernelEvent[], KernelEvent[], ref TimeSpec)
+			using (var sleep = Process.Start ("/bin/sleep", "0.5")) {
+				using (var kqueue = new KernelQueue ()) {
+					var events = CreateEvents (sleep);
+					TimeSpec ts = new TimeSpec
+					{
+						Seconds = 5,
+					};
+					Assert.IsTrue (kqueue.KEvent (events, events, ref ts), "kevent");
+				}
+			}
+
+			// (KernelEvent[], int, KernelEvent[], int)
+			using (var sleep = Process.Start ("/bin/sleep", "0.5")) {
+				using (var kqueue = new KernelQueue ()) {
+					var events = CreateEvents (sleep);
+					Assert.IsTrue (kqueue.KEvent (events, events.Length, events, events.Length), "kevent");
+				}
+			}
+		}
+	}
+}
