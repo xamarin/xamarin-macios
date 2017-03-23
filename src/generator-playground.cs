2f19df4ab039cbd5a69fc7c833f13fbd0f1e7b92
Alex Soto <alex@alexsoto.me>
2017-03-23 11:08:14 -0600

    [Proposal] Add playground to IKVM generator template
    When adding new features to the generator we find ourselves
    creating new test cases in test/generator or with binding projects
    and not actually modifying the actual SDK api definition source files
    because you get a lot of noise on the generated code.
    
    By doing the above it would not allow us to leverage the new debugging
    capabilities (thanks Rolf!!) that the new IKVM based generator grant us.
    
    So my proposal is to have this Playground (generator-playground.cs)
    where you can add arbitrary interface definitions to test and debug the
    generator that sould end up as your test case in tests/generator.

---
+++ src/generator-playground.cs
@@ -0,0 +1,16 @@
+// 
+// generator-playground.cs: Used to debug generator, this is an ApiDefinition.cs file
+//							that you can use to create your own interfaces definitions
+//
+// Authors:
+//    Alex Soto (alexsoto@microsoft.com
+// 
+// Copyright 2017 Xamarin Inc.
+//
+
+using System;
+using XamCore.Foundation;
+
+namespace Playground {
+
+}
