c29466b5dd1aa20b73038feebe9b6edb2e93e747
Chris Hamons <chris.hamons@xamarin.com>
2017-04-06 14:18:50 -0500

    Add XM SDK + LinkSkip test

---
+++ tests/mmptest/src/LinkerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Reflection;
+
+namespace Xamarin.MMP.Tests
+{
+	public partial class MMPTests
+	{
+		[Test]
+		public void UnifiedLinkingSDK_WithAllNonProductSkipped_Builds ()
+		{
+			RunMMPTest (tmpDir => {
+				TI.UnifiedTestConfig test = new TI.UnifiedTestConfig (tmpDir) { CSProjConfig = "<LinkMode>SdkOnly</LinkMode><MonoBundlingExtraArgs>--linkskip=mscorlib.dll --linkskip=System.Core.dll --linkskip=System.dll</MonoBundlingExtraArgs>" };
+				TI.TestUnifiedExecutable (test);
+			});
+		}
+	}
+}
