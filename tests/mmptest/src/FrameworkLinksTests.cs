xamarin/xamarin-macios

commit d20ccf5bc6bd5ed06571d09b424f3d5cd3121e5d

[macos] Rework framework/weak_framework handling in mmp (#1953)
- Before this mmp was not adding -framework, -weak_framework consistently on non-static registrar use cases
- GatherFrameworks was previously not ported from mtouch, and did not work as DeploymentTarget was unset in mmp
- Added verbose prints so users can determine why various framework linkages are added
- Fixed an issue where duplicate were being added due to HandleFramework shoving args by hand
- Tested with auto test and https://github.com/chamons/xm-version-regression-test manual test

tools/common/Assembly.cs
@@ -384,6 +384,20 @@ namespace Xamarin.Bundler {
 						if (Frameworks.Add ("OpenAL"))
 							Driver.Log (3, "Linking with the framework OpenAL because {0} is referenced by a module reference in {1}", file, FileName);
 						break;
+#if MONOMAC
+					case "PrintCore":
+						if (Frameworks.Add ("ApplicationServices"))
+							Driver.Log (3, "Linking with the framework ApplicationServices because {0} is referenced by a module reference in {1}", file, FileName);
+						break;
+					case "SearchKit":
+						if (Frameworks.Add ("CoreServices"))
+							Driver.Log (3, "Linking with the framework CoreServices because {0} is referenced by a module reference in {1}", file, FileName);
+						break;
+					case "CFNetwork":
+						if (Frameworks.Add ("CoreServices"))
+							Driver.Log (3, "Linking with the framework CoreServices because {0} is referenced by a module reference in {1}", file, FileName);
+						break;
+#endif
 					default:
 						// detect frameworks
 						int f = name.IndexOf (".framework/", StringComparison.Ordinal);

tests/mmptest/src/FrameworkLinksTests.cs
@@ -1,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Reflection;
+
+namespace Xamarin.MMP.Tests
+{
+	public partial class MMPTests
+	{
+		const string LinkerEnabledConfig = "<LinkMode>Full</LinkMode>";
+		const string StaticRegistrarConfig = "<MonoBundlingExtraArgs>--registrar=static</MonoBundlingExtraArgs>";
+
+		enum LinkStatus { Strong, Weak }
+
+		Dictionary <string, LinkStatus> CalculateFrameworkLinkStatus (string[] clangParts)
+		{
+			var status = new Dictionary<string, LinkStatus> ();
+			for (int i = 0; i < clangParts.Length; ++i) {
+				string currentPart = clangParts[i];
+				if (currentPart == "-weak_framework" || currentPart == "-framework") {
+					string name = clangParts[i + 1];
+					status[name] = currentPart == "-framework" ? LinkStatus.Strong : LinkStatus.Weak;
+				}
+			}
+			return status;
+		}
+
+		void AssertAppKitLinkage (Dictionary<string, LinkStatus> status)
+		{
+			Assert.IsTrue (status.ContainsKey ("AppKit"), "AppKit must have framework reference in clang invocation");
+			Assert.AreEqual (LinkStatus.Strong, status["AppKit"], "AppKit must be strong linked");
+		}
+
+		void AssertFrameworkMinOSRespected (Dictionary <string, LinkStatus> status)
+		{
+			// Walk rest of frameworks and verify they are weak_framework if newer than 10.7, which is defined in tests/common/mac/Info-Unified.plist
+			foreach (var entry in status) {
+				LinkStatus linkStatus;
+				Framework currentFramework = Frameworks.MacFrameworks.Find (entry.Key);
+				if (currentFramework == null) {
+					// There are a few entries not in Framesworks.cs that we know about
+					switch (entry.Key) {
+						case "Carbon":
+						case "CoreGraphics":
+						case "CoreFoundation":
+						case "ApplicationServices":
+							linkStatus = LinkStatus.Strong;
+							break;
+						default:
+							Assert.Fail ("Unknown entry in AssertFrameworkMinOSRespected - " + entry.Key);
+							return;
+					}
+				}
+				else {
+					linkStatus = currentFramework.Version > new Version (10, 7) ? LinkStatus.Weak : LinkStatus.Strong;
+				}
+				Assert.AreEqual (linkStatus, entry.Value, $"Framework link status of {entry.Key} was {entry.Value} but expected to be {linkStatus}");
+			}
+		}
+
+		[Test]
+		public void UnifiedWithoutLinking_ShouldHaveManyFrameworkClangLines ()
+		{
+			RunMMPTest (tmpDir => {
+				// By default we -framework in all frameworks we bind.
+				string [] clangParts = GetUnifiedProjectClangInvocation (tmpDir);
+				AssertUnlinkedFrameworkStatus (clangParts);
+
+				// Even with static registrar
+				clangParts = GetUnifiedProjectClangInvocation (tmpDir, StaticRegistrarConfig);
+				AssertUnlinkedFrameworkStatus (clangParts);
+			});
+		}
+
+		void AssertUnlinkedFrameworkStatus (string[] clangParts)
+		{
+			Dictionary<string, LinkStatus> status = CalculateFrameworkLinkStatus (clangParts);
+
+			AssertAppKitLinkage (status);	
+
+			// We expect a large number of entires, which will grow as we add more bindings
+			Assert.Greater (status.Count, 20, "Did not found as many framework entries in clang invocation as expected - {0}\n{1}", status.Count, string.Join (" ", clangParts));
+
+			AssertFrameworkMinOSRespected (status);
+		}
+
+		[Test]
+		public void UnifiedWithLinking_ShouldHaveFewFrameworkClangLines ()
+		{
+			RunMMPTest (tmpDir => {
+				// When we link, we should throw away pretty much everything that isn't AppKit. 
+				string[] clangParts = GetUnifiedProjectClangInvocation (tmpDir, LinkerEnabledConfig);
+				AssertLinkedFrameworkStatus (clangParts);
+
+				// Even with static registrar
+				clangParts = GetUnifiedProjectClangInvocation (tmpDir, LinkerEnabledConfig + StaticRegistrarConfig);
+				AssertLinkedFrameworkStatus (clangParts);
+			});
+		}
+
+		void AssertLinkedFrameworkStatus (string[] clangParts)
+		{
+			Dictionary<string, LinkStatus> status = CalculateFrameworkLinkStatus (clangParts);
+
+			AssertAppKitLinkage (status);
+
+			// We expect a few number of entires, which should not grow much over time
+			// Today - Foundation, AppKit, Security, QuartzCore, CoreFoundation, CFNetwork, Carbon, CoreServices
+			Assert.Less (status.Count, 10, "Found more framework entries in clang invocation then expected - {0}\n{1}", status.Count, string.Join (" ", clangParts));
+
+			AssertFrameworkMinOSRespected (status);
+		}
+	}
+}

src/Foundation/NSObject.mac.cs
@@ -96,6 +96,7 @@ namespace XamCore.Foundation {
 		static IntPtr nc = Dlfcn.dlopen (Constants.NotificationCenterLibrary, 1);
 		static IntPtr pl = Dlfcn.dlopen (Constants.PhotosLibrary, 1);
 		static IntPtr mp = Dlfcn.dlopen (Constants.MediaPlayerLibrary, 1);
+		static IntPtr pc = Dlfcn.dlopen (Constants.PrintCoreLibrary, 1);
 #endif
 		// ** IF YOU ADD ITEMS HERE PLEASE UPDATE linker/ObjCExtensions.cs and mmp/linker/MonoMac.Tuner/MonoMacNamespaces.cs
 

tests/mmptest/mmptest.csproj
@@ -95,6 +95,7 @@
     <Compile Include="..\..\tools\mmp\tests\aot.cs">
       <Link>unit\aot.cs</Link>
     </Compile>
+    <Compile Include="src\FrameworkLinksTests.cs" />
   </ItemGroup>
   <ItemGroup>
     <None Include="Info.plist" />

tests/mmptest/src/MMPTest.cs
@@ -28,6 +28,16 @@ namespace Xamarin.MMP.Tests
 			}
 		}
 
+		// TODO - We have multiple tests using this. It doesn't take that long, but is it worth caching?
+		string [] GetUnifiedProjectClangInvocation (string tmpDir, string projectConfig = "")
+		{
+			TI.UnifiedTestConfig test = new TI.UnifiedTestConfig (tmpDir) { CSProjConfig = projectConfig };
+			string buildOutput = TI.TestUnifiedExecutable (test).BuildOutput;
+			string [] splitBuildOutput = TI.TestUnifiedExecutable (test).BuildOutput.Split (new string[] { Environment.NewLine }, StringSplitOptions.None);
+			string clangInvocation = splitBuildOutput.Single (x => x.Contains ("clang"));
+			return clangInvocation.Split (new string[] { " " }, StringSplitOptions.None);
+		}
+
 		[Test]
 		public void CollisionsBetweenLibraryNameAndEXE_ShouldFailBuild ()
 		{
@@ -630,10 +640,8 @@ namespace Xamarin.MMP.Tests
 		public void UnifiedDebug_ShouldOnlyHaveOne_ObjCArgument ()
 		{
 			RunMMPTest (tmpDir => {
-				TI.UnifiedTestConfig test = new TI.UnifiedTestConfig (tmpDir);
-				var buildOutput = TI.TestUnifiedExecutable (test).BuildOutput.Split (new string[] { Environment.NewLine }, StringSplitOptions.None);
-				string clangInvocation = buildOutput.Single (x => x.Contains ("clang"));
-				int objcCount = clangInvocation.Split (new string[] { " " }, StringSplitOptions.None).Count (x => x.Contains ("-ObjC"));
+				string [] clangParts = GetUnifiedProjectClangInvocation (tmpDir);
+				int objcCount = clangParts.Count (x => x.Contains ("-ObjC"));
 				Assert.AreEqual (1, objcCount, "Found more than one -OjbC");
 			});
 		}

tools/common/Target.cs
@@ -101,5 +101,85 @@ namespace Xamarin.Bundler {
 			ErrorHelper.Warning (54, "Unable to canonicalize the path '{0}': {1} ({2}).", path, strerror (errno), errno);
 			return path;
 		}
+
+		public void ComputeLinkerFlags ()
+		{
+			foreach (var a in Assemblies)
+				a.ComputeLinkerFlags ();
+#if MTOUCH
+			if (App.Platform != ApplePlatform.WatchOS && App.Platform != ApplePlatform.TVOS)
+				Frameworks.Add ("CFNetwork"); // required by xamarin_start_wwan
+#endif
+		}
+
+		public void GatherFrameworks ()
+		{
+			AssemblyDefinition productAssembly = null;
+
+			foreach (var assembly in Assemblies) {
+				if (assembly.AssemblyDefinition.Name.Name == Driver.GetProductAssembly (App)) {
+					productAssembly = assembly.AssemblyDefinition;
+					break;
+				}
+			}
+
+			// *** make sure any change in the above lists (or new list) are also reflected in 
+			// *** Makefile so simlauncher-sgen does not miss any framework
+
+			HashSet<string> processed = new HashSet<string> ();
+#if !MONOMAC
+			Version v80 = new Version (8, 0);
+#endif
+
+			foreach (ModuleDefinition md in productAssembly.Modules) {
+				foreach (TypeDefinition td in md.Types) {
+					// process only once each namespace (as we keep adding logic below)
+					string nspace = td.Namespace;
+					if (processed.Contains (nspace))
+						continue;
+					processed.Add (nspace);
+
+					Framework framework;
+					if (Driver.GetFrameworks (App).TryGetValue (nspace, out framework)) {
+						// framework specific processing
+						switch (framework.Name) {
+#if !MONOMAC
+						case "CoreAudioKit":
+							// CoreAudioKit seems to be functional in the iOS 9 simulator.
+							if (App.IsSimulatorBuild && App.SdkVersion.Major < 9)
+								continue;
+							break;
+						case "Metal":
+						case "MetalKit":
+						case "MetalPerformanceShaders":
+							// some frameworks do not exists on simulators and will result in linker errors if we include them
+							if (App.IsSimulatorBuild)
+								continue;
+							break;
+						case "PushKit":
+							// in Xcode 6 beta 7 this became an (ld) error - it was a warning earlier :(
+							// ld: embedded dylibs/frameworks are only supported on iOS 8.0 and later (@rpath/PushKit.framework/PushKit) for architecture armv7
+							// this was fixed in Xcode 6.2 (6.1 was still buggy) see #29786
+							if ((App.DeploymentTarget < v80) && (Driver.XcodeVersion < new Version (6, 2))) {
+								ErrorHelper.Warning (49, "{0}.framework is supported only if deployment target is 8.0 or later. {0} features might not work correctly.", framework.Name);
+								continue;
+							}
+							break;
+#endif
+						}
+
+						if (App.SdkVersion >= framework.Version) {
+							var add_to = App.DeploymentTarget >= framework.Version ? Frameworks : WeakFrameworks;
+							add_to.Add (framework.Name);
+							continue;
+						}
+					}
+				}
+			}
+
+			// Make sure there are no duplicates between frameworks and weak frameworks.
+			// Keep the weak ones.
+			Frameworks.ExceptWith (WeakFrameworks);
+		}
 	}
 }

tools/linker/ObjCExtensions.cs
@@ -70,6 +70,7 @@ namespace Xamarin.Linker {
 			ScriptingBridge = profile.GetNamespace ("ScriptingBridge");
 			WebKit = profile.GetNamespace ("WebKit");
 			MediaPlayer = profile.GetNamespace ("MediaPlayer");
+			PrintCore = profile.GetNamespace ("PrintCore");
 #else
 			Registrar = profile.GetNamespace ("Registrar");
 			UIKit = profile.GetNamespace ("UIKit");
@@ -151,6 +152,7 @@ namespace Xamarin.Linker {
 
 		public static string WebKit { get; private set; }
 		public static string MediaPlayer { get; private set; }
+		public static string PrintCore { get; private set; }
 #else
 		public static string Registrar { get; private set; }
 

tools/mmp/Application.cs
@@ -1,7 +1,15 @@
+using System;
+
 namespace Xamarin.Bundler {
 	public partial class Application
 	{
 		public bool Is32Build => !Driver.Is64Bit; 
-		public bool Is64Build => Driver.Is64Bit; 
+		public bool Is64Build => Driver.Is64Bit;
+
+		internal void Initialize ()
+		{
+			if (DeploymentTarget == null) 
+				DeploymentTarget = new Version (10, 7);
+		}
 	}
 }

commit fe55039b7b65d9cd54ba65724d0fba932cd7c3e9

[macos] Fix XM projects with p/invokes into non-system frameworks
- Broken by https://github.com/xamarin/xamarin-macios/commit/d20ccf5bc6bd5ed06571d09b424f3d5cd3121e5d

tools/common/Assembly.cs
@@ -335,6 +335,12 @@ namespace Xamarin.Bundler {
 				foreach (var mr in m.ModuleReferences) {
 					string name = mr.Name;
 					string file = Path.GetFileNameWithoutExtension (name);
+#if MONOMAC
+					string path = Path.GetDirectoryName (name);
+					if (!path.StartsWith ("/System/Library/Frameworks"))
+						continue;
+#endif
+
 					switch (file) {
 					// special case
 					case "__Internal":

tests/mmptest/src/FrameworkLinksTests.cs
@@ -114,5 +114,21 @@ namespace Xamarin.MMP.Tests
 
 			AssertFrameworkMinOSRespected (status);
 		}
+
+
+		[Test]
+		public void ProjectWithLinkToPrivateFramework_ShouldBuild ()
+		{
+			RunMMPTest (tmpDir => {
+
+				TI.UnifiedTestConfig test = new TI.UnifiedTestConfig (tmpDir) { TestDecl = @"
+	const string MobileDeviceLibrary = ""/System/Library/PrivateFrameworks/MobileDevice.framework/MobileDevice"";
+	[System.Runtime.InteropServices.DllImport (MobileDeviceLibrary)]
+	private static extern void TestMethod ();
+				" };
+
+				TI.TestUnifiedExecutable (test);
+			});
+		}
 	}
 }