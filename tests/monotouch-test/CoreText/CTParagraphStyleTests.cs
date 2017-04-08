02ffdfa2f59bf5f156f04ac3f253695f118fae16
Alex Soto <alex@alexsoto.me>
2017-04-08 01:13:53 -0500

    [CoreText] Fix bug 54148 - CoreText.CTParagraphStyle does not pick up settings from CTParagraphStyleSettings
    https://bugzilla.xamarin.com/show_bug.cgi?id=54148
    
    CTParagraphStyle float properties have the incorrect float return type,
    the headers state this API's returns CGFloats (aka nfloat) instead of floats
    this used to work ok fetching them due to there is no difference in size
    for 32 bits devices but once 64 bit devices appeared the API began to fail
    
    The actual method that fetches the values `CTParagraphStyleGetValueForSpecifier`
    asks for the size of the returned data and we used to give the size of a float
    which is incorrect in 64 bits devices and the API call just correctly returned
    false because it could not write back the value to us.
    
    Added tests for the full properties available on CTParagraphStyle

---
+++ tests/monotouch-test/CoreText/CTParagraphStyleTests.cs
@@ -0,0 +1,88 @@
+﻿//
+// Unit tests for CTParagraphStyle
+//
+// Authors:
+//	Alex Soto <alexsoto@microsoft.com>
+//
+// Copyright 2017 Xamarin Inc. All rights reserved.
+//
+
+#if !__WATCHOS__
+
+using System;
+using NUnit.Framework;
+using System.Linq;
+
+#if XAMCORE_2_0
+using Foundation;
+using CoreText;
+#else
+using MonoTouch.CoreText;
+using MonoTouch.Foundation;
+#endif
+
+
+#if XAMCORE_2_0
+using RectangleF = CoreGraphics.CGRect;
+using SizeF = CoreGraphics.CGSize;
+using PointF = CoreGraphics.CGPoint;
+#else
+using nfloat = global::System.Single;
+using nint = global::System.Int32;
+using nuint = global::System.UInt32;
+#endif
+
+namespace MonoTouchFixtures.CoreText {
+
+	[TestFixture]
+	[Preserve (AllMembers = true)]
+	public class CTParagraphStyleTests {
+
+		[Test]
+		public void StylePropertiesTest ()
+		{
+			var settings = new CTParagraphStyleSettings () {
+				TailIndent = 5,
+				ParagraphSpacingBefore = 5,
+				ParagraphSpacing = 5,
+				LineSpacing = 5,
+				MinimumLineHeight = 5,
+				MaximumLineHeight = 5,
+				LineHeightMultiple = 5,
+				DefaultTabInterval = 5,
+				HeadIndent = 5,
+				FirstLineHeadIndent = 5,
+				LineBreakMode = CTLineBreakMode.TruncatingHead,
+				BaseWritingDirection = CTWritingDirection.Natural,
+				Alignment = CTTextAlignment.Justified,
+				TabStops = new [] {
+					new CTTextTab (CTTextAlignment.Justified, 2),
+					new CTTextTab (CTTextAlignment.Natural, 1)
+				}
+			};
+
+			var style = new CTParagraphStyle (settings);
+			Assert.DoesNotThrow (() => {
+				Assert.AreEqual (settings.TailIndent, style.TailIndent, "TailIndent");
+				Assert.AreEqual (settings.ParagraphSpacingBefore, style.ParagraphSpacingBefore, "ParagraphSpacingBefore");
+				Assert.AreEqual (settings.ParagraphSpacing, style.ParagraphSpacing, "ParagraphSpacing");
+				Assert.AreEqual (settings.LineSpacing, style.LineSpacing, "LineSpacing");
+				Assert.AreEqual (settings.MinimumLineHeight, style.MinimumLineHeight, "MinimumLineHeight");
+				Assert.AreEqual (settings.MaximumLineHeight, style.MaximumLineHeight, "MaximumLineHeight");
+				Assert.AreEqual (settings.LineHeightMultiple, style.LineHeightMultiple, "LineHeightMultiple");
+				Assert.AreEqual (settings.DefaultTabInterval, style.DefaultTabInterval, "DefaultTabInterval");
+				Assert.AreEqual (settings.HeadIndent, style.HeadIndent, "HeadIndent");
+				Assert.AreEqual (settings.FirstLineHeadIndent, style.FirstLineHeadIndent, "FirstLineHeadIndent");
+				Assert.AreEqual (settings.LineBreakMode, style.LineBreakMode, "LineBreakMode");
+				Assert.AreEqual (settings.BaseWritingDirection, style.BaseWritingDirection, "LineBreakMode");
+				Assert.AreEqual (settings.Alignment, style.Alignment, "Alignment");
+
+				var styleTabStops = style.GetTabStops ();
+				Assert.AreEqual (settings.TabStops.Count (), styleTabStops.Length, "TabStops");
+				Assert.True (styleTabStops.Any (t => t.Location == 2 && t.TextAlignment == CTTextAlignment.Justified));
+				Assert.True (styleTabStops.Any (t => t.Location == 1 && t.TextAlignment == CTTextAlignment.Natural));
+			});
+		}
+	}
+}
+#endif
