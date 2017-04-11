54e16ab9c5a7d073c9828dd40b6d42fe3b9030c9
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2017-04-11 12:01:06 -0400

    [mmp] Add new Profile subclasses for Xamarin Mac (full / system) (#1989)
    The fallback to MonoMacProfile is not correct if we ask for the
    platform assembly - which blocks enabling the linker for that
    assembly (only)

---
+++ tools/mmp/linker/MonoMac.Tuner/XamarinMacProfile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MonoMac.Tuner {
+
+	class XamarinMacProfile : MacBaseProfile {
+
+		readonly int bits;
+
+		public XamarinMacProfile (int arch)
+		{
+			bits = arch;
+		}
+
+		public bool Is32Bits {
+			get { return bits == 32; }
+		}
+
+		public bool Is64Bits {
+			get { return bits == 64; }
+		}
+
+		public override string ProductAssembly {
+			get { return "Xamarin.Mac"; }
+		}
+	}
+}
