24d17da112d958902d83b30fe03614868fc16dca
Chris Hamons <chris.hamons@xamarin.com>
2017-05-16 14:05:51 -0500

    Fix build

--- tests/common/mac/ProjectTestHelpers.cs
+++ tests/common/mac/ProjectTestHelpers.cs
@@ -84,7 +84,7 @@ namespace Xamarin.MMP.Tests
 			return new Version (versionRegex.Match (output).Value.Split (' ')[2]);
 		}
 
-		public static string RunAndAssert (string exe, StringBuilder args, string stepName, bool shouldFail = false, Func<string> getAdditionalFailInfo = null)
+		public static string RunAndAssert (string exe, string args, string stepName, bool shouldFail = false, Func<string> getAdditionalFailInfo = null)
 		{
 			StringBuilder output = new StringBuilder ();
 			Environment.SetEnvironmentVariable ("MONO_PATH", null);
