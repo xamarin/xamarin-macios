0818741f3b3913a25226aa569b10d13163b8b411
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-05-19 08:52:58 +0200

    [mtouch] Fix linking with the same third-party framework from both extension and container app. Fixes #56635.
    If both an extension and the container app (or multiple extensions) reference
    the same binding assembly for a framework, then we'd error out with an
    internal error when trying to copy the framework from both locations to the
    container app.
    
    So instead detect when we're trying to copy multiple identical (by comparing
    the on-disk contents) frameworks, and only copy one of them.
    
    We'll still show an error if the frameworks are different, but now a nice
    MT1035 error with a proper error description instead of an internal error.
    
    https://bugzilla.xamarin.com/show_bug.cgi?id=56635

--- tools/mtouch/Application.cs
+++ tools/mtouch/Application.cs
@@ -1580,12 +1580,32 @@ namespace Xamarin.Bundler {
 				if (!HasFrameworksDirectory && (isFramework || info.DylibToFramework))
 					continue; // Don't copy frameworks to app extensions (except watch extensions), they go into the container app.
 
+				if (!files.All ((v) => Directory.Exists (v)))
+					throw ErrorHelper.CreateError (99, $"Internal error: 'can't process a mix of dylibs and frameworks: {string.Join (", ", files)}'. Please file a bug report with a test case (https://bugzilla.xamarin.com).");
+
 				if (isFramework) {
 					// This is a framework
-					if (files.Count != 1)
-						throw ErrorHelper.CreateError (99, "Internal error: 'can't lipo directories'. Please file a bug report with a test case (http://bugzilla.xamarin.com).");
+					if (files.Count > 1) {
+						// If we have multiple frameworks, check if they're identical, and remove any duplicates
+						var firstFile = files.First ();
+						foreach (var otherFile in files.Where ((v) => v != firstFile).ToArray ()) {
+							if (Cache.CompareDirectories (firstFile, otherFile, ignore_cache: true)) {
+								Driver.Log (6, $"Framework '{name}' included from both '{firstFile}' and '{otherFile}', but they are identical, so the latter will be ignored.");
+								files.Remove (otherFile);
+								continue;
+							}
+						}
+					}
+					if (files.Count != 1) {
+						var exceptions = new List<Exception> ();
+						var fname = Path.GetFileName (name);
+						exceptions.Add (ErrorHelper.CreateError (1035, $"Cannot include different versions of the framework '{fname}'"));
+						foreach (var file in files)
+							exceptions.Add (ErrorHelper.CreateError (1036, $"Framework '{fname}' included from: {file} (Related to previous error)"));
+						throw new AggregateException (exceptions);
+					}
 					if (info.DylibToFramework)
-						throw ErrorHelper.CreateError (99, "Internal error: 'can't convert frameworks to frameworks'. Please file a bug report with a test case (http://bugzilla.xamarin.com).");
+						throw ErrorHelper.CreateError (99, $"Internal error: 'can't convert frameworks to frameworks: {files.First ()}'. Please file a bug report with a test case (https://bugzilla.xamarin.com).");
 					var framework_src = files.First ();
 					var framework_filename = Path.Combine (framework_src, Path.GetFileNameWithoutExtension (framework_src));
 					if (!MachO.IsDynamicFramework (framework_filename)) {
