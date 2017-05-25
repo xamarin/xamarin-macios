43e220df7fbea8e438b8a0ce422a0c283120b62e
Alex Soto <dalexsoto@gmail.com>
2017-05-25 14:42:25 -0500

    [generator] Keep [NotImplemented] info so it is usable in 3rd party bindings. Fixes bug 52664 (#2131)
    * [generator] Keep [NotImplemented] info so it is usable in 3rd party bindings. Fixes bug 52664
    
    https://bugzilla.xamarin.com/show_bug.cgi?id=52664
    
    Currently we do not keep the [NotImplemented](0) information and the
    generator gets a little confused because it will not find an Export
    inside the getters/setters of properties and there is no way to tell
    if this was intentional or not. We now keep the [NotImplemented]
    and also add some null checks in the generator where needed.
    
    Reenabled tests disabled in https://github.com/xamarin/xamarin-macios/pull/1745/commits/9f036b218a7246822000488695f0edd0e43c4778
    
    [0]: https://developer.xamarin.com/guides/cross-platform/macios/binding/objective-c-libraries/#Objective-C_Mutable_Pattern_and_Properties
    
    * [generator] Implement feedback
    
    * Added quote method
    * Teach linker about NotImplementedAttribute
    
    * [generator] Improve Quote method
    
    * [generator] Fix quote now for realsssss

---
+++ src/Foundation/NotImplementedAttribute.cs
@@ -0,0 +1,26 @@
+//
+// NotImplementedAttribute.cs
+//
+// Authors:
+//   Alex Soto (alexsoto@microsoft.com)
+//
+// Copyright 2017 Xamarin Inc.
+//
+
+using System;
+
+namespace XamCore.Foundation {
+	//
+	// This is designed to be applied to setter methods in
+	// a base class `Foo' when a `MutableFoo' exists.
+	//
+	// This allows the Foo.set_XXX to exists but throw an exception
+	// but derived classes would then override the property
+	//
+	[AttributeUsage (AttributeTargets.Method, AllowMultiple = false)]
+	public class NotImplementedAttribute : Attribute {
+		public NotImplementedAttribute () { }
+		public NotImplementedAttribute (string message) { Message = message; }
+		public string Message { get; set; }
+	}
+}
