31f300b1ebc0df0b8098ff46e466812fd1b7a10d
Timothy Risi <timothy.risi@gmail.com>
2017-06-12 11:21:42 -0800

    Fix driver also

--- tools/mmp/driver.cs
+++ tools/mmp/driver.cs
@@ -1340,7 +1340,7 @@ namespace Xamarin.Bundler {
 				args.Append ("-liconv -x objective-c++ ");
 				args.Append ("-I").Append (StringUtils.Quote (Path.Combine (GetXamMacPrefix (), "include"))).Append (' ');
 				if (registrarPath != null)
-					args.Append (Quote (registrarPath)).Append (' ');
+					args.Append (StringUtils.Quote (registrarPath)).Append (' ');
 				args.Append ("-fno-caret-diagnostics -fno-diagnostics-fixit-info ");
 				if (link_flags != null)
 					args.Append (link_flags + " ");
