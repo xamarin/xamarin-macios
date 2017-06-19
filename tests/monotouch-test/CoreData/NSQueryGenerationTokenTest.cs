a2ae0d67e540721cc2e7beeb106c29eabcfe71de
Manuel de la Pena <mandel@microsoft.com>
2017-06-19 12:13:42 +0200

    Fix test as per review.

--- tests/monotouch-test/CoreData/NSQueryGenerationTokenTest.cs
+++ tests/monotouch-test/CoreData/NSQueryGenerationTokenTest.cs
@@ -19,7 +19,7 @@ namespace MonoTouchFixtures.CoreData
 		public void EncodeWithCoderTest ()
 		{
 			// Added test to ensure we do support NSCoding even when introspection fails.
-			if (IntPtr == 8  && if (TestRuntime.CheckExactXcodeVersion (11, 0, beta: 1)) {
+			if (IntPtr == 8  && TestRuntime.CheckExactXcodeVersion (11, 0, beta: 1)) {
 				using (var data = new NSMutableData ())
 				using (var archiver = new NSKeyedArchiver (data))
 				using (var coder = new NSCoder ()) {
