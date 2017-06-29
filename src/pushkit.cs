cee6e4e779ba908046bf1e3d55cd1590ca66fdec
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2017-06-28 22:56:26 -0400

    [pushkit] Update for beta 1 (no change in beta 2) (#2264)

--- src/pushkit.cs
+++ src/pushkit.cs
@@ -59,6 +59,10 @@ namespace XamCore.PushKit
 		[iOS (9,0)]
 		[Field ("PKPushTypeComplication")]
 		NSString Complication { get; }
+
+		[iOS (11,0)]
+		[Field ("PKPushTypeFileProvider")]
+		NSString FileProvider { get; }
 	}
 
 	[iOS (8,0)]
@@ -70,9 +74,14 @@ namespace XamCore.PushKit
 		[Export ("pushRegistry:didUpdatePushCredentials:forType:"), EventArgs ("PKPushRegistryUpdated"), EventName ("CredentialsUpdated")]
 		void DidUpdatePushCredentials (PKPushRegistry registry, PKPushCredentials credentials, string type);
 
-		[Abstract]
+		[Abstract] // now optional in iOS 11
+		[Deprecated (PlatformName.iOS, 11,0, message: "Use the 'DidReceiveIncomingPushWithPayload' overload accepting an 'Action' argument instead.")]
 		[Export ("pushRegistry:didReceiveIncomingPushWithPayload:forType:"), EventArgs ("PKPushRegistryRecieved"), EventName ("IncomingPushReceived")]
 		void DidReceiveIncomingPush (PKPushRegistry registry, PKPushPayload payload, string type);
+
+		[iOS (11,0)]
+		[Export ("pushRegistry:didReceiveIncomingPushWithPayload:forType:withCompletionHandler:")]
+		void DidReceiveIncomingPush (PKPushRegistry registry, PKPushPayload payload, string type, Action completion);
 
 		[Export ("pushRegistry:didInvalidatePushTokenForType:"), EventArgs ("PKPushRegistryRecieved"), EventName ("PushTokenInvalidated")]
 		void DidInvalidatePushToken (PKPushRegistry registry, string type);
