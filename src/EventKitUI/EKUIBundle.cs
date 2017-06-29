05edc5e2be344eb8c6728d02bdea58f8c4f9f833
Alex Soto <alex@alexsoto.me>
2017-06-28 22:59:58 -0500

    [EventKitUI] Update to Xcode 9 Beta 1

---
+++ src/EventKitUI/EKUIBundle.cs
@@ -0,0 +1,27 @@
+﻿//
+// EventKitUIBundle C# bindings
+//
+// Authors:
+//	Alex Soto  <alexsoto@microsoft.com>
+//
+// Copyright 2017 Xamarin Inc. All rights reserved.
+//
+
+#if XAMCORE_2_0
+using System;
+using System.Runtime.InteropServices;
+using XamCore.Foundation;
+using XamCore.ObjCRuntime;
+
+namespace XamCore.EventKitUI {
+	public static class EKUIBundle {
+
+		[iOS (11,0)]
+		[DllImport (Constants.EventKitUILibrary)]
+		static extern IntPtr EventKitUIBundle ();
+
+		[iOS (11,0)]
+		public static NSBundle UIBundle { get; } = Runtime.GetNSObject<NSBundle> (EventKitUIBundle ());
+	}
+}
+#endif
