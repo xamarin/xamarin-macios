b36bb3ef5a02b503772e886bf3e7d403f29945ca
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-07-05 10:42:28 +0200

    [generator] Don't generate a 'GetConstant (T?)' and 'GetNullableValue' for smart enums.
    Instead inline the corresponding code in the generator, so that we don't add a
    lot of metadata for little benefit.
    
    Generator diff (compared with the commit before starting to generate
    GetConstant/GetNullableValue (32fb0b65ddb52c2bbc6962bdf40dab6ef41bc5c2)):
    https://gist.github.com/rolfbjarne/440f3a78089098881145dfb231d86d66 (empty).

--- src/generator-enums.cs
+++ src/generator-enums.cs
@@ -160,18 +160,6 @@ public partial class Generator {
 				print ("}");
 				print ("");
 			}
-			
-			print ("public static NSString GetConstant (this {0}? self)", type.Name);
-			print ("{");
-			indent++;
-			print ("if (self == null)");
-			indent++;
-			print ("return null;");
-			indent--;
-			print ("return GetConstant (self.Value);");
-			indent--;
-			print ("}");
-			print ("");
 
 			print ("public static NSString GetConstant (this {0} self)", type.Name);
 			print ("{");
@@ -195,18 +183,6 @@ public partial class Generator {
 			indent--;
 			print ("}");
 			
-			print ("");
-			
-			print ("public static {0}? GetNullableValue (NSString constant)", type.Name);
-			print ("{");
-			indent++;
-			print ("if (constant == null)");
-			indent++;
-			print ("return null;");
-			indent--;
-			print ("return GetValue (constant);");
-			indent--;
-			print ("}");
 			print ("");
 
 			print ("public static {0} GetValue (NSString constant)", type.Name);
