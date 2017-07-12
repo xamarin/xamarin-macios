f49c7c1aa566d42596f3525d158ff546601c6db3
Chris Hamons <chris.hamons@xamarin.com>
2017-07-12 09:32:09 -0500

    Remove Xcode9 radar test exclusions that have been closed by Apple (#2313)

--- src/corebluetooth.cs
+++ src/corebluetooth.cs
@@ -85,10 +85,12 @@ namespace XamCore.CoreBluetooth {
 
 		[NoTV]
 		[Availability (Obsoleted = Platform.iOS_9_0)]
+		[Mac (10, 7, onlyOn64: true)] // Was removed from 32-bit in 10.13 unannounced
 		[Export ("retrievePeripherals:"), Internal]
 		void RetrievePeripherals (NSArray peripheralUUIDs);
 
 		[NoTV]
+		[Mac (10, 7, onlyOn64: true)] // Was removed from 32-bit in 10.13 unannounced
 		[Export ("retrieveConnectedPeripherals")]
 		[Availability (Introduced = Platform.iOS_5_0, Deprecated = Platform.iOS_7_0, Obsoleted = Platform.iOS_9_0, Message = "Use 'RetrievePeripheralsWithIdentifiers' instead.")]
 		void RetrieveConnectedPeripherals ();
@@ -363,7 +365,7 @@ namespace XamCore.CoreBluetooth {
 		[NullAllowed]
 		[Export ("UUID", ArgumentSemantic.Retain)]
 		[Override]
-		CBUUID UUID { get; set; }
+		CBUUID UUID { get; [Availability (Obsoleted = Platform.Mac_10_13)] set; }
 
 		[Export ("properties", ArgumentSemantic.Assign)]
 		[Override]
@@ -440,6 +442,7 @@ namespace XamCore.CoreBluetooth {
 		[NoTV]
 		[Availability (Deprecated = Platform.iOS_7_0, Obsoleted = Platform.iOS_9_0)]
 		[Export ("isConnected")]
+		[Mac (10, 7, onlyOn64: true)] // Was removed from 32-bit in 10.13 unannounced
 		bool IsConnected { get;  }
 
 		[Export ("services", ArgumentSemantic.Retain)]
@@ -618,7 +621,7 @@ namespace XamCore.CoreBluetooth {
 #if !MONOMAC
 		[Override]
 #endif
-		CBUUID UUID { get; set; }
+		CBUUID UUID { get; [Availability (Obsoleted = Platform.Mac_10_13)] set; }
 
 		[NoTV]
 		[Export ("isPrimary")]
