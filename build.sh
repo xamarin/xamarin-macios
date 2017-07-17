31a64bf947b9c95226142b26b0e79b5618c05f37
Joseph Stroman <joewstroman@gmail.com>
2017-07-16 23:57:05 -0400

    Tribulations of a hack

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 echo "PATH ======= $PATH"
 if [ "$1" = "world" ]; then 
     cd .. && mv s xamarin-macios && cd xamarin-macios
+    make world
+    cd .. && mv xamarin-macios s && cd s
+else
+	make $1
 fi
-make $1
+
