89071bc19dafc2e7c0d714709866de8d90f35441
Alex Soto <dalexsoto@gmail.com>
2017-07-18 11:12:10 -0500

    [Vision] Add bindings for Xcode 9 Beta 1, 2 & 3 (#2301)
    * [Vision] Add bindings for Xcode 9 Beta 1 & 2
    
    This commit also adds two convenience overloads to GetAttachments
    that allows you to get a more strongly typed version of the
    returned dictionary, this is needed because there is no easy way
    to downcast from NSDictionary to NSDictionary<TKey, TValue>
    and it is used in the sample that excecises the Vision API found here
    
    https://github.com/dalexsoto/FaceDetector
    
    * [Vision] Implement feedback
    
    * Add commas to enums to avoid diff spam
    * Throw a managed exception when we fail to dinamically call Vision framework
    
    * [Vision] Update to xcode 9 Beta 3
    
    * [vision] fix VNImageCropAndScaleOption enum
    
    Also removed public modifier from enums in order to have interfaces
    and enums in sync
    
    * [Vision] Add missing comma to enum
    
    * [Vision] More API enhancements
    
    * Removed default .ctor from types that are not user created
    * Added Strong Dictionary to VNImageRequestHandler ctors
    * Turned VNImageOption into a StrongDictionary
    * Added bug report[1] for BarcodeDescriptor so we do not forget
    * Removed duped metadata from manual bindings
    * VNDetectedObjectObservation subclasses now have the factory
      method FromBoundingBox inherited from its parent class
      (VNDetectedObjectObservation).
    
    [1]: https://bugzilla.xamarin.com/show_bug.cgi?id=58197
    
    * [Vision] Merge extern API with wrappers, fixed intro!
    
    * [Vision] Add one missing public accessor to VNUtils

--- runtime/bindings.m
+++ runtime/bindings.m
@@ -11,6 +11,7 @@
  */
 
 #include "bindings.h"
+#include <dlfcn.h>
 
 /*
  * Hand-written bindings to support ObjectiveC exceptions.
@@ -88,3 +89,67 @@ void * monotouch_IntPtr_objc_msgSendSuper_IntPtr (struct objc_super *super, SEL 
 #endif
 #endif
 
+/*
+ * Vector c bindings
+ *
+ * The following code uses dlsym to not have a linking dependency on Xcode 9,
+ * the code can be simplified once we require to remove dlsym or xcode 9 becomes the minimum supported version
+ */
+
+typedef CGPoint (*vision_func) (vector_float2 faceLandmarkPoint, CGRect faceBoundingBox, size_t imageWidth, size_t imageHeight);
+
+CGPoint
+xamarin_CGPoint__VNNormalizedFaceBoundingBoxPointForLandmarkPoint_Vector2_CGRect_nuint_nuint_string (struct Vector2f faceLandmarkPoint, CGRect faceBoundingBox, xm_nuint_t imageWidth, xm_nuint_t imageHeight, const char **error_msg) {
+
+	static vision_func func = NULL;
+	*error_msg = NULL;
+
+	if (func == NULL) {
+		void *vision_handle = dlopen ("/System/Library/Frameworks/Vision.framework/Vision", RTLD_LAZY);
+		if (vision_handle == NULL) {
+			*error_msg = "Could not open Vision Framework";
+			return CGPointMake (0, 0);
+		}
+
+		func = (vision_func) dlsym (vision_handle, "VNNormalizedFaceBoundingBoxPointForLandmarkPoint");
+
+		if (func == NULL) {
+			*error_msg = "Could not obtain the address for VNNormalizedFaceBoundingBoxPointForLandmarkPoint";
+			return CGPointMake (0, 0);
+		}
+	}
+
+	vector_float2 flp;
+	flp [0] = faceLandmarkPoint.a;
+	flp [1] = faceLandmarkPoint.b;
+
+	return func (flp, faceBoundingBox, imageWidth, imageHeight);
+}
+
+CGPoint
+xamarin_CGPoint__VNImagePointForFaceLandmarkPoint_Vector2_CGRect_nuint_nuint_string (struct Vector2f faceLandmarkPoint, CGRect faceBoundingBox, xm_nuint_t imageWidth, xm_nuint_t imageHeight, const char **error_msg) {
+
+	static vision_func func = NULL;
+	*error_msg = NULL;
+
+	if (func == NULL) {
+		void *vision_handle = dlopen ("/System/Library/Frameworks/Vision.framework/Vision", RTLD_LAZY);
+		if (vision_handle == NULL) {
+			*error_msg = "Could not open Vision Framework";
+			return CGPointMake (0, 0);
+		}
+
+		func = (vision_func) dlsym (vision_handle, "VNImagePointForFaceLandmarkPoint");
+
+		if (func == NULL) {
+			*error_msg = "Could not obtain the address for VNImagePointForFaceLandmarkPoint";
+			return CGPointMake (0, 0);
+		}
+	}
+
+	vector_float2 flp;
+	flp [0] = faceLandmarkPoint.a;
+	flp [1] = faceLandmarkPoint.b;
+
+	return func (flp, faceBoundingBox, imageWidth, imageHeight);
+}
