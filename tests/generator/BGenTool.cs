60e52e1823d205cd47be3a10a8fc168e0077dd27
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-07-18 14:29:24 +0200

    [tests][generator] Add support for inspecting/asserting the generated content.

--- tests/generator/BGenTool.cs
+++ tests/generator/BGenTool.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 using NUnit.Framework;
+
+using Mono.Cecil;
 
 using Xamarin.Utils;
 
@@ -22,6 +25,8 @@ namespace Xamarin.Tests
 	
 	class BGenTool : Tool
 	{
+		AssemblyDefinition assembly;
+
 		public Profile Profile;
 		public bool ProcessEnums;
 
@@ -112,6 +117,66 @@ namespace Xamarin.Tests
 		public void AssertExecuteError (string message)
 		{
 			Assert.AreNotEqual (0, Execute (BuildArguments ()), message);
+		}
+
+		public void AssertPublicTypeCount (int count, string message = null)
+		{
+			LoadAssembly ();
+
+			var actual = assembly.MainModule.Types.Where ((v) => v.IsPublic || v.IsNestedPublic);
+			if (actual.Count () != count)
+				Assert.Fail ($"Expected {count} public type(s), found {actual} public type(s). {message}\n\t{string.Join ("\n\t", actual.Select ((v) => v.FullName).ToArray ())}");
+		}
+
+		public void AssertPublicMethodCount (string typename, int count, string message = null)
+		{
+			LoadAssembly ();
+
+			var t = assembly.MainModule.Types.FirstOrDefault ((v) => v.FullName == typename);
+			var actual = t.Methods.Count ((v) => {
+				if (v.IsPrivate || v.IsFamily || v.IsFamilyAndAssembly)
+					return false;
+				return true;
+			});
+			if (actual != count)
+				Assert.Fail ($"Expected {count} publicly accessible method(s) in {typename}, found {actual} publicly accessible method(s). {message}");
+		}
+
+		public void AssertType (string fullname, TypeAttributes? attributes = null, string message = null)
+		{
+			LoadAssembly ();
+
+			var allTypes = assembly.MainModule.GetTypes ().ToArray ();
+			var t = allTypes.FirstOrDefault ((v) => v.FullName == fullname);
+			if (t == null)
+				Assert.Fail ($"No type named '{fullname}' in the generated assembly. {message}\nList of types:\n\t{string.Join ("\n\t", allTypes.Select ((v) => v.FullName))}");
+			if (attributes != null)
+				Assert.AreEqual (attributes.Value, t.Attributes, $"Incorrect attributes for type {fullname}.");
+		}
+
+		public void AssertMethod (string typename, string method, string returnType = null, params string [] parameterTypes)
+		{
+			LoadAssembly ();
+
+			var t = assembly.MainModule.Types.First ((v) => v.FullName == typename);
+			var m = t.Methods.FirstOrDefault ((v) => {
+				if (v.Name != method)
+					return false;
+				if (v.Parameters.Count != parameterTypes.Length)
+					return false;
+				for (int i = 0; i < v.Parameters.Count; i++)
+					if (v.Parameters [i].ParameterType.FullName != parameterTypes [i])
+						return false;
+				return true;
+			});
+			if (m == null)
+				Assert.Fail ($"No method '{method}' with signature '{string.Join ("', '", parameterTypes)}' was found.");
+		}
+
+		void LoadAssembly ()
+		{
+			if (assembly == null)
+				assembly = AssemblyDefinition.ReadAssembly (Path.Combine (TmpDirectory, "api.dll"));
 		}
 
 		void EnsureTempDir ()
