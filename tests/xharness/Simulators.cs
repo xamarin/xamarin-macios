689b5f1744840e8683f35ed014bd244835cb397c
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-07-19 15:28:38 +0200

    [xharness] Make sure to mark device listing as completed even if it fails.

--- tests/xharness/Simulators.cs
+++ tests/xharness/Simulators.cs
@@ -533,10 +533,9 @@ namespace xharness
 								continue;
 							connected_devices.Add (d);
 						}
-
-						connected_devices.SetCompleted ();
 					}
 				} finally {
+					connected_devices.SetCompleted ();
 					File.Delete (tmpfile);
 				}
 			});
