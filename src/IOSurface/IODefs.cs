8275514be98c3789fe91ae156a2cab5352aca397
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-08-01 16:33:04 +0200

    Add trailing comma in last enum value

--- src/IOSurface/IODefs.cs
+++ src/IOSurface/IODefs.cs
@@ -14,14 +14,14 @@ namespace XamCore.IOSurface {
 
 	public enum IOSurfaceLockOptions : uint {
 		ReadOnly = 1,
-		AvoidSync = 2
+		AvoidSync = 2,
 	}
 
 	public enum IOSurfacePurgeabilityState : uint {
 		NonVolatile = 0,
 		Volatile = 1,
 		Empty = 2,
-		KeepCurrent = 3
+		KeepCurrent = 3,
 	}
 
 	// To be used with kIOSurfaceCacheMode or IOSurfacePropertyKeyCacheMode
@@ -31,7 +31,7 @@ namespace XamCore.IOSurface {
 		WriteThruCache = 2 << 8,
 		CopybackCache = 3 << 8,
 		WriteCombineCache = 4 << 8,
-		CopybackInnerCache = 5 << 8
+		CopybackInnerCache = 5 << 8,
 	};
 
 	
