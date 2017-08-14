e5d012c5b810cf40ffb2622f009eb72fdc1cb514
Chris Hamons <chris.hamons@xamarin.com>
2017-08-14 13:17:10 -0500

    [macos] System mono should resolve non-XM libraries from system (#2480)
    - https://bugzilla.xamarin.com/show_bug.cgi?id=58703
    - Was broken on msbuild but undetected due to https://bugzilla.xamarin.com/show_bug.cgi?id=53164
    - Unified45Build_CompileToNativeOutput was broken in a recent commit bf53e6204d0950acd5f8efcce8732bd8d8
    - This was not caught as the mmp tests are not run by default
    - The test was bad/wrong, and checking msbuild not mmp ouput anyway, so fixing.

--- tests/mmptest/src/MMPTest.cs
+++ tests/mmptest/src/MMPTest.cs
@@ -641,8 +641,7 @@ namespace Xamarin.MMP.Tests
 			RunMMPTest (tmpDir => {
 				TI.UnifiedTestConfig test = new TI.UnifiedTestConfig (tmpDir) { XM45 = true };
 				var output = TI.TestUnifiedExecutable (test);
-				Assert.That (output.BuildOutput, Contains.Substring ("TargetFrameworkIdentifier: .NETFramework"));
-				Assert.That (output.BuildOutput, Contains.Substring ("TargetFrameworkVersion: v4.5"));
+				Assert.That (output.BuildOutput, Contains.Substring ("Selected target framework: .NETFramework,Version=v4.5; API: Unified"));
 			});
 		}
 
