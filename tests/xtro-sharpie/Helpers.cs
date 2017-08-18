a3314bc2edff1ae54069facaf96214371a859bac
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-08-18 17:25:14 +0200

    [xtro-sharpie] Add a Simd check class to verify signatures with Simd types.

--- tests/xtro-sharpie/Helpers.cs
+++ tests/xtro-sharpie/Helpers.cs
@@ -247,5 +247,47 @@ namespace Extrospection {
 		{
 			return self.Selector.ToString () ?? (self.IsPropertyAccessor ? self.Name : null);
 		}
+
+		public static bool IsObsolete (this ICustomAttributeProvider provider)
+		{
+			if (provider.HasCustomAttributes) {
+				foreach (var attrib in provider.CustomAttributes) {
+					var attribType = attrib.Constructor.DeclaringType;
+					if (attribType.Namespace == "System" && attribType.Name == "ObsoleteAttribute")
+						return true;
+				}
+			}
+
+			// If we're a property accessor, check the property as well.
+			var prop = FindProperty (provider as MethodReference);
+			if (prop != null)
+				return IsObsolete (prop);
+
+			return false;
+		}
+
+		public static PropertyDefinition FindProperty (this MethodReference method)
+		{
+			var def = method?.Resolve ();
+			if (def == null)
+				return null;
+
+			if (!def.IsSpecialName)
+				return null;
+
+			if (!def.DeclaringType.HasProperties)
+				return null;
+
+			if (!method.Name.StartsWith ("get_", StringComparison.Ordinal) && !method.Name.StartsWith ("set_", StringComparison.Ordinal))
+				return null;
+
+			var propName = method.Name.Substring (4);
+			foreach (var prop in def.DeclaringType.Properties) {
+				if (prop.Name == propName)
+					return prop;
+			}
+
+			return null;
+		}
 	}
 }
