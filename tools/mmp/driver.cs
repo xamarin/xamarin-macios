af44978cce80326e6645e9d75946a056c8014a61
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-08-22 09:40:26 +0200

    [mtouch] Print verbosity using an invariant culture. Fixes #58849. (#2507)
    * [mtouch/mmp] Set CultureInfo.CurrentCulture according to LANG.
    
    This makes it easier to run mtouch/mmp under different locales when testing.
    
    Unfortunately mono checks the system locale before checking LANG, which means
    that there's no built-in way in macOS/mono to specify the current locale
    without changing the system locale.
    
    * [mtouch] Print verbosity using an invariant culture. Fixes #58849.
    
    https://bugzilla.xamarin.com/show_bug.cgi?id=58849
    
    * [mtouch/mmp] Fix error code.
    
    * [mtouch/mmp] Log/warn when we set the current language.

--- tools/mmp/driver.cs
+++ tools/mmp/driver.cs
@@ -221,6 +221,7 @@ namespace Xamarin.Bundler {
 		{
 			try {
 				Console.OutputEncoding = new UTF8Encoding (false, false);
+				SetCurrentLanguage ();
 				Main2 (args);
 			}
 			catch (Exception e) {
