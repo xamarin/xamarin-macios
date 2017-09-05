5bdcc78905e8f6992a4b106781a018c975adb753
Jeffrey Stedfast <jestedfa@microsoft.com>
2017-09-05 13:19:30 -0400

    Added unit tests for CoreML build

---
+++ msbuild/tests/Xamarin.iOS.Tasks.Tests/ProjectsTests/CoreMLCompiler.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Xamarin.iOS.Tasks
+{
+	[TestFixture ("iPhone")]
+	[TestFixture ("iPhoneSimulator")]
+	public class CoreMLCompiler : ProjectTest
+	{
+		public CoreMLCompiler (string platform) : base (platform)
+		{
+		}
+
+		[Test]
+		public void BuildTest ()
+		{
+			BuildProject ("MarsHabitatPricePredictor", Platform, "Debug");
+		}
+	}
+}
