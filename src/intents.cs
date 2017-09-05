483561c38f26fe9e82b49d85830c2e4ca811a853
Alex Soto <dalexsoto@gmail.com>
2017-09-05 07:37:34 -0500

    [Intents] Fix breaking changes in Intents added in Xcode 9 Bindings (#2590)
    Apple added more protocol conformances to INRidesharingDomainHandling and
    INPaymentsDomainHandling protocols in Xcode 9, I introduced these breaking
    changes back when I did intents binding so fixing them :)

--- src/intents.cs
+++ src/intents.cs
@@ -2171,7 +2171,11 @@ namespace XamCore.Intents {
 	[Introduced (PlatformName.WatchOS, 3, 2)]
 	[Unavailable (PlatformName.MacOSX)]
 	[Protocol] 
-	interface INPaymentsDomainHandling : INSendPaymentIntentHandling, INRequestPaymentIntentHandling, INPayBillIntentHandling, INSearchForBillsIntentHandling, INSearchForAccountsIntentHandling, INTransferMoneyIntentHandling {
+	interface INPaymentsDomainHandling : INSendPaymentIntentHandling, INRequestPaymentIntentHandling, INPayBillIntentHandling, INSearchForBillsIntentHandling
+#if XAMCORE_4_0 // Added in iOS 11 -> #if __IPHONE_OS_VERSION_MIN_REQUIRED >= 110000
+	, INSearchForAccountsIntentHandling, INTransferMoneyIntentHandling
+#endif
+	{
 	}
 
 	[Introduced (PlatformName.iOS, 10, 0)]
@@ -2186,7 +2190,7 @@ namespace XamCore.Intents {
 	[Unavailable (PlatformName.MacOSX)]
 	[Protocol]
 	interface INRidesharingDomainHandling : INListRideOptionsIntentHandling, INRequestRideIntentHandling, INGetRideStatusIntentHandling 
-#if !WATCH
+#if XAMCORE_4_0 // Added in iOS 11 -> #if __IPHONE_OS_VERSION_MIN_REQUIRED >= 110000
 	, INCancelRideIntentHandling, INSendRideFeedbackIntentHandling
 #endif
 	{
