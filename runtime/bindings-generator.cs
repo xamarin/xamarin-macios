1e08602bda0fa61f5ce17b108ca8e49ec5bccb4e
Alex Soto <alex@alexsoto.me>
2017-09-08 18:02:19 -0500

    [SIMD] Implement Simd double vector types (VectorDouble2/3/4/MatrixDouble4x4).
    The following types will be used by ModelIO bindings

--- runtime/bindings-generator.cs
+++ runtime/bindings-generator.cs
@@ -2311,6 +2311,7 @@ namespace Xamarin.BindingMethods.Generator
 			case "Vector2i":
 			case "Vector2":
 			case "VectorFloat2":
+			case "VectorDouble2":
 				writer.WriteLine ("\t{0}{2}a = {1} [0];", managedVariable, nativeVariable, accessor);
 				writer.WriteLine ("\t{0}{2}b = {1} [1];", managedVariable, nativeVariable, accessor);
 				break;
@@ -2318,6 +2319,7 @@ namespace Xamarin.BindingMethods.Generator
 			case "Vector3i":
 			case "Vector3":
 			case "VectorFloat3":
+			case "VectorDouble3":
 				writer.WriteLine ("\t{0}{2}a = {1} [0];", managedVariable, nativeVariable, accessor);
 				writer.WriteLine ("\t{0}{2}b = {1} [1];", managedVariable, nativeVariable, accessor);
 				writer.WriteLine ("\t{0}{2}c = {1} [2];", managedVariable, nativeVariable, accessor);
@@ -2326,6 +2328,7 @@ namespace Xamarin.BindingMethods.Generator
 			case "Vector4i":
 			case "Vector4":
 			case "VectorFloat4":
+			case "VectorDouble4":
 				if (type.NativeType == "vector_float3") {
 					writer.WriteLine ("\t{0}{2}a = {1} [0];", managedVariable, nativeVariable, accessor);
 					writer.WriteLine ("\t{0}{2}b = {1} [1];", managedVariable, nativeVariable, accessor);
@@ -2357,6 +2360,7 @@ namespace Xamarin.BindingMethods.Generator
 				break;
 			case "Matrix4":
 			case "MatrixFloat4x4":
+			case "MatrixDouble4x4":
 				writer.WriteLine ("\tfor (int i = 0; i < 4; i++) {");
 				writer.WriteLine ("\t\t{0}{2}columns [i].a = {1}.columns [i] [0];", managedVariable, nativeVariable, accessor);
 				writer.WriteLine ("\t\t{0}{2}columns [i].b = {1}.columns [i] [1];", managedVariable, nativeVariable, accessor);
@@ -2434,6 +2438,7 @@ namespace Xamarin.BindingMethods.Generator
 			case "Vector2i":
 			case "Vector2":
 			case "VectorFloat2":
+			case "VectorDouble2":
 				writer.WriteLine ("\t{0} [0] = {1}{2}a;", nativeVariable, managedVariable, accessor);
 				writer.WriteLine ("\t{0} [1] = {1}{2}b;", nativeVariable, managedVariable, accessor);
 				break;
@@ -2441,6 +2446,7 @@ namespace Xamarin.BindingMethods.Generator
 			case "Vector3i":
 			case "Vector3":
 			case "VectorFloat3":
+			case "VectorDouble3":
 				writer.WriteLine ("\t{0} [0] = {1}{2}a;", nativeVariable, managedVariable, accessor);
 				writer.WriteLine ("\t{0} [1] = {1}{2}b;", nativeVariable, managedVariable, accessor);
 				writer.WriteLine ("\t{0} [2] = {1}{2}c;", nativeVariable, managedVariable, accessor);
@@ -2449,6 +2455,7 @@ namespace Xamarin.BindingMethods.Generator
 			case "Vector4i":
 			case "Vector4":
 			case "VectorFloat4":
+			case "VectorDouble4":
 				if (type.NativeType == "vector_float3") {
 					writer.WriteLine ("\t{0} [0] = {1}{2}a;", nativeVariable, managedVariable, accessor);
 					writer.WriteLine ("\t{0} [1] = {1}{2}b;", nativeVariable, managedVariable, accessor);
@@ -2479,6 +2486,7 @@ namespace Xamarin.BindingMethods.Generator
 				break;
 			case "Matrix4":
 			case "MatrixFloat4x4":
+			case "MatrixDouble4x4":
 				writer.WriteLine ("\tfor (int i = 0; i < 4; i++) {");
 				writer.WriteLine ("\t\t{0}.columns [i][0] = {1}{2}columns [i].a;", nativeVariable, managedVariable, accessor);
 				writer.WriteLine ("\t\t{0}.columns [i][1] = {1}{2}columns [i].b;", nativeVariable, managedVariable, accessor);
@@ -2997,14 +3005,32 @@ namespace Xamarin.BindingMethods.Generator
 				NativeWrapperType = "struct Vector2d",
 				RequireMarshal = true
 			};
+			public static TypeData VectorDouble2 = new TypeData {
+				ManagedType = "VectorDouble2",
+				NativeType = "vector_double2",
+				NativeWrapperType = "struct Vector2d",
+				RequireMarshal = true,
+			};
 			public static TypeData Vector3d = new TypeData {
 				ManagedType = "Vector3d",
 				NativeType = "vector_double3",
 				NativeWrapperType = "struct Vector3d",
 				RequireMarshal = true,
 			};
+			public static TypeData VectorDouble3 = new TypeData {
+				ManagedType = "VectorDouble3",
+				NativeType = "vector_double3",
+				NativeWrapperType = "struct Vector3d",
+				RequireMarshal = true,
+			};
 			public static TypeData Vector4d = new TypeData {
 				ManagedType = "Vector4d",
+				NativeType = "vector_double4",
+				NativeWrapperType = "struct Vector4d",
+				RequireMarshal = true,
+			};
+			public static TypeData VectorDouble4 = new TypeData {
+				ManagedType = "VectorDouble4",
 				NativeType = "vector_double4",
 				NativeWrapperType = "struct Vector4d",
 				RequireMarshal = true,
@@ -3058,6 +3084,15 @@ namespace Xamarin.BindingMethods.Generator
 				ManagedType = "MatrixFloat4x4",
 				NativeType = "matrix_float4x4",
 				NativeWrapperType = "struct MatrixFloat4x4",
+				RequireMarshal = true,
+				IsARMStret = true,
+				IsX86Stret = true,
+				IsX64Stret = true,
+			};
+			public static TypeData MatrixDouble4x4 = new TypeData {
+				ManagedType = "MatrixDouble4x4",
+				NativeType = "matrix_double4x4",
+				NativeWrapperType = "struct MatrixDouble4x4",
 				RequireMarshal = true,
 				IsARMStret = true,
 				IsX86Stret = true,
---
+++ tests/monotouch-test/Simd/VectorDouble2Test.cs
@@ -0,0 +1,144 @@
+﻿
+using System;
+using System.Diagnostics;
+
+using Foundation;
+using ObjCRuntime;
+
+using OpenTK;
+using Simd;
+
+using NUnit.Framework;
+
+namespace MonoTouchFixtures.Simd {
+	[TestFixture]
+	[Preserve (AllMembers = true)]
+	public class VectorDouble2Test {
+
+		[Test]
+		public void ElementConstructor ()
+		{
+			var expected = GetTestVector ();
+			var actual = new VectorDouble2 (expected.X, expected.Y);
+			Asserts.AreEqual (expected, actual, "ctor 1");
+		}
+
+		[Test]
+		public void Equality_Operator ()
+		{
+			var inputL = GetTestVector ();
+			var inputR = GetTestVector ();
+			var inputSimdL = (VectorDouble2) inputL;
+			var inputSimdR = (VectorDouble2) inputR;
+
+			// matrices are different
+			Assert.AreEqual (inputL == inputR, inputSimdL == inputSimdR, "inequality");
+			Assert.IsFalse (inputL == inputR, "inequality 2 expected");
+			Assert.IsFalse (inputSimdL == inputSimdR, "inequality 2 actual");
+
+			inputL = inputR;
+			inputSimdL = inputSimdR;
+			// matrices are identical
+			Assert.AreEqual (inputL == inputR, inputSimdL == inputSimdR, "equality");
+			Assert.IsTrue (inputL == inputR, "equality 2 expected");
+			Assert.IsTrue (inputSimdL == inputSimdR, "equality 2 actual");
+		}
+
+		[Test]
+		public void Inequality_Operator ()
+		{
+			var inputL = GetTestVector ();
+			var inputR = GetTestVector ();
+			var inputSimdL = (VectorDouble2) inputL;
+			var inputSimdR = (VectorDouble2) inputR;
+
+			// matrices are different
+			Assert.AreEqual (inputL != inputR, inputSimdL != inputSimdR, "inequality");
+			Assert.IsTrue (inputL != inputR, "inequality 2 expected");
+			Assert.IsTrue (inputSimdL != inputSimdR, "inequality 2 actual");
+
+			inputL = inputR;
+			inputSimdL = inputSimdR;
+			// matrices are identical
+			Assert.AreEqual (inputL != inputR, inputSimdL != inputSimdR, "equality");
+			Assert.IsFalse (inputL != inputR, "equality 2 expected");
+			Assert.IsFalse (inputSimdL != inputSimdR, "equality 2 actual");
+		}
+
+		[Test]
+		public void Explicit_Operator_ToVector2 ()
+		{
+			var expected = GetTestVector ();
+			var actual = (Vector2d) expected;
+
+			Asserts.AreEqual (expected, actual, "ToVector2d");
+		}
+
+		[Test]
+		public void Explicit_Operator_FromVector2 ()
+		{
+			var expected = new Vector2d (2, 3);
+			var actual = (VectorDouble2) expected;
+
+			Asserts.AreEqual (expected, actual, "FromVector2d");
+		}
+
+		[Test]
+		public void ToStringTest ()
+		{
+			var vector = new VectorDouble2 (1, 2);
+
+			Assert.AreEqual ("(1, 2)", vector.ToString (), "tostring");
+		}
+
+		// GetHashCode doesn't have to be identical, so no need to test
+
+		[Test]
+		public void Equals_Object ()
+		{
+			var expectedA = GetTestVector ();
+			var expectedB = GetTestVector ();
+			var actualA = (VectorDouble2) expectedA;
+			var actualB = (VectorDouble2) expectedB;
+
+			Assert.IsTrue (actualA.Equals ((object) actualA), "self");
+			Assert.IsFalse (actualA.Equals ((object) actualB), "other");
+			Assert.IsFalse (actualA.Equals (null), "null");
+			Assert.IsTrue (actualA.Equals (expectedA), "same type");
+		}
+
+		[Test]
+		public void Equals_Vector ()
+		{
+			var expectedA = GetTestVector ();
+			var expectedB = GetTestVector ();
+			var actualA = (VectorDouble2) expectedA;
+			var actualB = (VectorDouble2) expectedB;
+
+			Assert.IsTrue (actualA.Equals (actualA), "self");
+			Assert.IsFalse (actualA.Equals (actualB), "other");
+		}
+
+		static VectorDouble2 [] test_vectors = new [] {
+			new VectorDouble2 (0.1532144d, 0.5451511d),
+			new VectorDouble2 (0.7717745d, 0.559364d),
+			new VectorDouble2 (0.2023053d, 0.4701468d),
+			new VectorDouble2 (0.4904693d, 0.841727d),
+			new VectorDouble2 (0.1252193d, 0.08986127d),
+			new VectorDouble2 (0.006755914d, 0.07464754d),
+			new VectorDouble2 (9.799572E+08d, 1.64794E+09d),
+			new VectorDouble2 (1.102396E+09d, 3.082477E+08d),
+			new VectorDouble2 (2.263112E+08d, 8.79644E+08d),
+			new VectorDouble2 (8.176959E+08d, 1.386156E+09d),
+		};
+
+		static int counter;
+		internal static VectorDouble2 GetTestVector ()
+		{
+			counter++;
+			if (counter == test_vectors.Length)
+				counter = 0;
+			return test_vectors [counter];
+		}
+	}
+}
