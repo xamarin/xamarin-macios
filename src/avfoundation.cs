450e54b48b5a5146560e54859fe6b004fb976bd2
Manuel de la Pena <v-mandel@microsoft.com>
2017-09-11 14:16:02 +0200

    [AVFoundation] Add missing interface AVRouteDetector. (#2637)

--- src/avfoundation.cs
+++ src/avfoundation.cs
@@ -12167,4 +12167,19 @@ namespace XamCore.AVFoundation {
 		AVContentKeyResponse Create (NSData keyData, [NullAllowed] NSData initializationVector);
 	}
 
+	[TV (11,0), NoWatch, Mac (10,13), iOS (11,0)]
+	[DisableDefaultCtor]
+	[BaseType (typeof(NSObject))]
+	interface AVRouteDetector {
+		[Notification]
+		[TV (11, 0), NoWatch, Mac (10, 13), iOS (11, 0)]
+		[Field ("AVRouteDetectorMultipleRoutesDetectedDidChangeNotification")]
+		NSString MultipleRoutesDetectedDidChange { get; }
+
+		[Export ("routeDetectionEnabled")]
+		bool RouteDetectionEnabled { [Bind ("isRouteDetectionEnabled")] get; set; }
+
+		[Export ("multipleRoutesDetected")]
+		bool MultipleRoutesDetected { get; }
+	}
 }
