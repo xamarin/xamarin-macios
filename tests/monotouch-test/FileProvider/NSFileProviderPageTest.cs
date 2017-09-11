60d6c0027f6d7b0551b6def466417fb956872cce
Alex Soto <alex@alexsoto.me>
2017-09-11 14:04:38 -0500

    Add tests

---
+++ tests/monotouch-test/FileProvider/NSFileProviderPageTest.cs
@@ -0,0 +1,36 @@
+﻿//
+// Unit tests for NSFileProviderPage
+//
+// Authors:
+//	Alex Soto <alexsoto@microsoft.com>
+//
+//
+// Copyright 2017 Xamarin Inc. All rights reserved.
+//
+
+#if XAMCORE_2_0 && __IOS__
+
+using System;
+using FileProvider;
+using Foundation;
+using NUnit.Framework;
+
+namespace MonoTouchFixtures.FileProvider {
+
+	[TestFixture]
+	[Preserve (AllMembers = true)]
+	public class NSFileProviderPageTests {
+
+		[Test]
+		public void CompressionSessionCreateTest ()
+		{
+			// The FileProvider's NSData constants are only available running on device.
+			TestRuntime.AssertDevice ();
+			TestRuntime.AssertXcodeVersion (9,0);
+
+			Assert.IsNotNull (NSFileProviderPage.InitialPageSortedByDate, "InitialPageSortedByDate should not be null");
+			Assert.IsNotNull (NSFileProviderPage.InitialPageSortedByName, "InitialPageSortedByName should not be null");
+		}
+	}
+}
+#endif
