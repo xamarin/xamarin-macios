a584411645234146aa12f1f0f6408e3a00ba8d14
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2017-09-13 23:12:53 -0400

    [avfoundation] Remove AVAssetDownloadDelegate.DidLoadTimeRange since it was removed in one of the betas (#2687)

--- src/avfoundation.cs
+++ src/avfoundation.cs
@@ -11548,10 +11548,6 @@ namespace XamCore.AVFoundation {
 		[NoWatch, NoMac, NoTV, iOS (11,0)]
 		[Export ("URLSession:aggregateAssetDownloadTask:didCompleteForMediaSelection:")]
 		void DidCompleteForMediaSelection (NSUrlSession session, AVAggregateAssetDownloadTask aggregateAssetDownloadTask, AVMediaSelection mediaSelection);
-
-		[NoWatch, NoMac, NoTV, iOS (11,0)]
-		[Export ("URLSession:aggregateDownloadTask:didLoadTimeRange:totalTimeRangesLoaded:timeRangeExpectedToLoad:forMediaSelection:")]
-		void DidLoadTimeRange (NSUrlSession session, AVAggregateAssetDownloadTask aggregateAssetDownloadTask, CMTimeRange timeRange, NSValue[] loadedTimeRanges, CMTimeRange timeRangeExpectedToLoad, AVMediaSelection mediaSelection);
 	}
 
 #endif
