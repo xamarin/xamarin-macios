d2415cae1fbb7ccc33f7277d20f57544b9d49288
Alex Soto <dalexsoto@gmail.com>
2017-09-25 09:12:53 -0500

    [CoreAnimation] CATextLayer.Alignment* strings should be CATextLayerAlignment enum / type. Fixes bug 59537 (#2723)
    https://bugzilla.xamarin.com/show_bug.cgi?id=59537
    
    Removes constants from `CATextLayer` (only for XAMCORE_4_0) and creates
    two smart enums `CATextLayerTruncationMode` and `CATextLayerAlignmentMode`.
    Also this introduces two new strong properties into CATextLayer class,
    `TextTruncationMode` and `TextAlignmentMode` that takes the new enums
    respectively, these new properties are meant to replace their
    string counterparts `TruncationMode` and `AlignmentMode`.

---
+++ tests/monotouch-test/CoreAnimation/CATextLayerTests.cs
@@ -0,0 +1,64 @@
+﻿//
+// Unit tests for CATextLayerTests
+//
+// Authors:
+//	Alex Soto <alexsoto@microsoft.com>
+//
+//
+// Copyright 2017 Xamarin Inc. All rights reserved.
+//
+
+#if !__WATCHOS__
+
+using System;
+using NUnit.Framework;
+
+#if XAMCORE_2_0
+using Foundation;
+using CoreAnimation;
+#else
+using MonoTouch.CoreAnimation;
+using MonoTouch.Foundation;
+#endif
+
+namespace MonoTouchFixtures.CoreAnimation {
+	[TestFixture]
+	[Preserve (AllMembers = true)]
+	public class CATextLayerTests {
+
+		[Test]
+		public void CATextLayerTruncationModeTest ()
+		{
+			var textLayer = new CATextLayer {
+				String = "Hello",
+				TextTruncationMode = CATextLayerTruncationMode.Middle
+			};
+
+			Assert.AreEqual (CATextLayerTruncationMode.Middle, textLayer.TextTruncationMode, "TextTruncationMode");
+			Assert.AreEqual (textLayer.TruncationMode, (string) textLayer.TextTruncationMode.GetConstant (), "TruncationMode");
+
+			textLayer.TruncationMode = CATextLayer.TruncantionEnd;
+			Assert.AreEqual (CATextLayerTruncationMode.End, textLayer.TextTruncationMode, "TextTruncationMode 2");
+
+			Assert.Throws<ArgumentNullException> (() => textLayer.TruncationMode = null);
+		}
+
+		[Test]
+		public void CATextLayerAlignmentModeTest ()
+		{
+			var textLayer = new CATextLayer {
+				String = "Hello",
+				TextAlignmentMode = CATextLayerAlignmentMode.Justified
+			};
+
+			Assert.AreEqual (CATextLayerAlignmentMode.Justified, textLayer.TextAlignmentMode, "TextAlignmentMode");
+			Assert.AreEqual (textLayer.AlignmentMode, (string) textLayer.TextAlignmentMode.GetConstant (), "AlignmentMode");
+
+			textLayer.AlignmentMode = CATextLayer.AlignmentNatural;
+			Assert.AreEqual (CATextLayerAlignmentMode.Natural, textLayer.TextAlignmentMode, "TextAlignmentMode 2");
+
+			Assert.Throws<ArgumentNullException> (() => textLayer.AlignmentMode = null);
+		}
+	}
+}
+#endif // !__WATCHOS__
