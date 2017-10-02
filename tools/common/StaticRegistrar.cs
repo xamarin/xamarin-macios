aca0e2ed2e019ded169eadae6949307a7ab663b7
Chris Hamons <chris.hamons@xamarin.com>
2017-10-02 16:55:23 -0500

    Fix a number of introspection test failures on 10.13 (#2815)

--- tools/common/StaticRegistrar.cs
+++ tools/common/StaticRegistrar.cs
@@ -2382,6 +2382,7 @@ namespace XamCore.Registrar {
 		static bool IsQTKitType (ObjCType type) => IsTypeCore (type, "QTKit");
 		static bool IsMapKitType (ObjCType type) => IsTypeCore (type, "MapKit");
 		static bool IsIntentsType (ObjCType type) => IsTypeCore (type, "Intents");
+		static bool IsExternalAccessoryType (ObjCType type) => IsTypeCore (type, "ExternalAccessory");
 
 		static bool IsMetalType (ObjCType type)
 		{
@@ -2440,7 +2441,7 @@ namespace XamCore.Registrar {
 
 				// These are 64-bit frameworks that extend NSExtensionContext / NSUserActivity, which you can't do
 				// if the header doesn't declare them. So hack it away, since they are useless in 64-bit anyway
-				if (!Is64Bits && (IsMapKitType (@class) || IsIntentsType (@class)))
+				if (!Is64Bits && (IsMapKitType (@class) || IsIntentsType (@class) || IsExternalAccessoryType (@class)))
 					continue;
 #endif
 
