94e06274e965c4758c4dcb614260f91a3b48f167
Alex Soto <alex@alexsoto.me>
2017-10-06 10:28:45 -0500

    [VideoToolbox] Adds bindings from Xcode 9 Beta 1 to Stable

--- src/VideoToolbox/VTDefs.cs
+++ src/VideoToolbox/VTDefs.cs
@@ -128,7 +128,11 @@ namespace XamCore.VideoToolbox {
 		MP4VAdvancedSimpleL4,
 		H263Profile0Level10,
 		H263Profile0Level45,
-		H263Profile3Level45
+		H263Profile3Level45,
+		[Mac (10,13), iOS (11,0), TV (11,0)]
+		HevcMainAutoLevel,
+		[Mac (10,13), iOS (11,0), TV (11,0)]
+		HevcMain10AutoLevel,
 	}
 
 	// Strongly Typed VTH264EntropyModeKeys
@@ -245,5 +249,29 @@ namespace XamCore.VideoToolbox {
 		Unset,
 		Decimate,
 		Average
+	}
+
+	[Mac (10,13), iOS (11,0), TV (11,0)]
+	public struct VTInt32Point {
+		public int X { get; set; }
+		public int Y { get; set; }
+
+		public VTInt32Point (int x, int y)
+		{
+			X = x;
+			Y = y;
+		}
+	}
+
+	[Mac (10,13), iOS (11,0), TV (11,0)]
+	public struct VTInt32Size {
+		public int Width { get; set; }
+		public int Height { get; set; }
+
+		public VTInt32Size (int width, int height)
+		{
+			Width = width;
+			Height = height;
+		}
 	}
 }
