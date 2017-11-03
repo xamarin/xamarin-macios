d95ef412c7072789892c4bbcd4778aba3f16eb03
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-11-03 18:46:58 +0100

    [tests][generator] Port some Xamarin.Mac tests to NUnit.
    Ported:
    
    * bmac_smoke
    * bmac-with-hyphen-in-name
    * property-redefination-mac
    * NSApplicationPublicEnsureMethods
    * protocol-duplicate-abstract

---
+++ tests/generator/BGenTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+using Xamarin.Tests;
+
+namespace GeneratorTests
+{
+	[TestFixture ()]
+	[Parallelizable (ParallelScope.All)]
+	public class BGenTests
+	{
+		[Test]
+		[TestCase (Profile.macClassic)]
+		[TestCase (Profile.macFull)]
+		[TestCase (Profile.macModern)]
+		public void BMac_Smoke (Profile profile)
+		{
+			BuildFile (profile, "bmac_smoke.cs");
+		}
+
+		[Test]
+		[TestCase (Profile.macClassic)]
+		[TestCase (Profile.macFull)]
+		[TestCase (Profile.macModern)]
+		public void BMac_With_Hyphen_In_Name (Profile profile)
+		{
+			BuildFile (profile, "bmac-with-hyphen-in-name.cs");
+		}
+
+		[Test]
+		[TestCase (Profile.macClassic)]
+		[TestCase (Profile.macFull)]
+		[TestCase (Profile.macModern)]
+		public void PropertyRedefinitionMac (Profile profile)
+		{
+			BuildFile (profile, "property-redefination-mac.cs");
+		}
+
+		[Test]
+		[TestCase (Profile.macClassic)]
+		[TestCase (Profile.macFull)]
+		[TestCase (Profile.macModern)]
+		public void NSApplicationPublicEnsureMethods (Profile profile)
+		{
+			BuildFile (profile, "NSApplicationPublicEnsureMethods.cs");
+		}
+
+		[Test]
+		[TestCase (Profile.macClassic)]
+		[TestCase (Profile.macFull)]
+		[TestCase (Profile.macModern)]
+		public void ProtocolDuplicateAbstract (Profile profile)
+		{
+			BuildFile (profile, "protocol-duplicate-abstract.cs");
+		}
+
+		BGenTool BuildFile (Profile profile, string filename)
+		{
+			var bgen = new BGenTool ();
+			bgen.Profile = profile;
+			bgen.Defines = BGenTool.GetDefaultDefines (bgen.Profile);
+			bgen.CreateTemporaryBinding (File.ReadAllText (Path.Combine (Configuration.SourceRoot, "tests", "generator", filename)));
+			bgen.AssertExecute ("build");
+			bgen.AssertNoWarnings ();
+			return bgen;
+		}
+	}
+}
