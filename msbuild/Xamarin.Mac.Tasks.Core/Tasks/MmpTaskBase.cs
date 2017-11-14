0461dcb6a75162fe08ac7253122f809db0844482
Jeffrey Stedfast <jestedfa@microsoft.com>
2017-11-14 15:24:58 -0500

    Renamed ProcessArgumentBuilder to CommandLineArgumentBuilder
    This is needed to prevent symbol conflicts with Xamarin.MacDev's
    ProcessArgumentBuilder (which is functionally different from
    Xamarin.MacDev.Tasks.Core's class of the same name).

--- msbuild/Xamarin.Mac.Tasks.Core/Tasks/MmpTaskBase.cs
+++ msbuild/Xamarin.Mac.Tasks.Core/Tasks/MmpTaskBase.cs
@@ -1,4 +1,4 @@
-//
+﻿//
 // MmpTask.cs
 //
 // Author:
@@ -98,7 +98,7 @@ namespace Xamarin.Mac.Tasks
 
 		protected override string GenerateCommandLineCommands ()
 		{
-			var args = new ProcessArgumentBuilder ();
+			var args = new CommandLineArgumentBuilder ();
 			bool msym;
 
 			args.Add ("/verbose");
@@ -226,7 +226,7 @@ namespace Xamarin.Mac.Tasks
 		string GetMonoBundleDirName ()
 		{
 			if (!string.IsNullOrEmpty (ExtraArguments)) {
-				var args = ProcessArgumentBuilder.Parse (ExtraArguments);
+				var args = CommandLineArgumentBuilder.Parse (ExtraArguments);
 
 				for (int i = 0; i < args.Length; i++) {
 					string arg;
