50e43fd11ae51483ca217024f911626093e9d169
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-11-28 14:47:15 +0100

    [generator] CoreImage filters are never direct bindings.
    CoreImage filter types are our own creation, and as such will never be direct bindings.
    
    This means we must set the IsDirectBinding value to false manually, and we can
    also remove the IsDirectBinding condition in the constructor, since it's
    redundant.
    
    Additionally we can remove a condition in the introspection tests that
    excluded the CoreImage filters from tests.
    
    Generated diff: https://gist.github.com/rolfbjarne/88968a034b0cbfefb78651e831da6b5c

--- src/generator-filters.cs
+++ src/generator-filters.cs
@@ -78,16 +78,9 @@ public partial class Generator {
 		print ("public {0} (NSCoder coder) : base (NSObjectFlag.Empty)", type_name);
 		print ("{");
 		indent++;
+		print ("IsDirectBinding = false;");
 		print ("IntPtr h;");
-		print ("if (IsDirectBinding) {");
-		indent++;
-		print ("h = global::{0}.Messaging.IntPtr_objc_msgSend_IntPtr (this.Handle, Selector.GetHandle (\"initWithCoder:\"), coder.Handle);", ns.CoreObjCRuntime);
-		indent--;
-		print ("} else {");
-		indent++;
 		print ("h = global::{0}.Messaging.IntPtr_objc_msgSendSuper_IntPtr (this.SuperHandle, Selector.GetHandle (\"initWithCoder:\"), coder.Handle);", ns.CoreObjCRuntime);
-		indent--;
-		print ("}");
 		print ("InitializeHandle (h, \"initWithCoder:\");");
 		indent--;
 		print ("}");
@@ -247,6 +240,7 @@ public partial class Generator {
 	void PrintEmptyBody ()
 	{
 		print ("{");
+		print ("\tIsDirectBinding = false;");
 		print ("}");
 		print ("");
 	}
