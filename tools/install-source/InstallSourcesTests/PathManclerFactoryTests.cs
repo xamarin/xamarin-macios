c76380a1c139f24976c4fa79e86852677d83102c
Manuel de la Pena <mandel@microsoft.com>
2017-12-01 12:13:46 +0100

    [Tests] Set the install-source tests to run.

---
+++ tools/install-source/InstallSourcesTests/PathManclerFactoryTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace InstallSourcesTests
+{
+	[TestFixture()]
+	public class PathManclerFactoryTests
+	{
+		[Test]
+		public void SetUp ()
+		{
+		}
+		
+		[Test]
+		public void TearDown ()
+		{
+		}
+		
+		[Test()]
+		public void TestIsMonoPath ()
+		{
+		}
+		
+		[Test]
+		public void TestIsOpenTKPath ()
+		{
+		}
+
+		[Test]
+		public void TestPathIsIgnored ()
+		{
+		}
+	}
+}
