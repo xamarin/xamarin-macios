85b8b1457c213cf4d1b91bc0d41fc4897d33e63d
Chris Hamons <chris.hamons@xamarin.com>
2017-12-01 09:32:56 -0600

    Fix missing .

--- src/appkit.cs
+++ src/appkit.cs
@@ -7457,7 +7457,7 @@ namespace XamCore.AppKit {
 		Selector Action { get; set; }
 
 		[Export ("state")]
-		NSGestureRecognizerState State { get; [Advice ("For Subclass Use")] set; }
+		NSGestureRecognizerState State { get; [Advice ("For Subclass Use.")] set; }
 
 		[Export ("delegate", ArgumentSemantic.Weak)]
 		[NullAllowed]
