a35b87dc654c7de1880e44f0d6dce9fcb9ad24e4
Chris Hamons <chris.hamons@xamarin.com>
2017-12-01 10:29:43 -0600

    Rework advice wording

--- src/appkit.cs
+++ src/appkit.cs
@@ -7457,7 +7457,7 @@ namespace XamCore.AppKit {
 		Selector Action { get; set; }
 
 		[Export ("state")]
-		NSGestureRecognizerState State { get; [Advice ("For Subclass Use.")] set; }
+		NSGestureRecognizerState State { get; [Advice ("Only subclasses of 'NSGestureRecognizer' can set this property.")] set; }
 
 		[Export ("delegate", ArgumentSemantic.Weak)]
 		[NullAllowed]
