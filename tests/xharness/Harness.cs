1dc462641cc5afeb9eca5e06dc479da36384fe54
Manuel de la Pena <mandel@microsoft.com>
2017-12-01 19:36:24 +0100

    [Tests] Add System.ServiceModel tests in the bcl.

--- tests/xharness/Harness.cs
+++ tests/xharness/Harness.cs
@@ -289,6 +289,7 @@ namespace xharness
 				"System.IO.Compression.FileSystem",
 				"Mono.CSharp",
 				"System.Security",
+				"System.ServiceModel",
 			};
 			foreach (var p in bcl_suites) {
 				foreach (var flavor in new MacFlavors [] { MacFlavors.Full, MacFlavors.Modern }) {
@@ -332,6 +333,7 @@ namespace xharness
 				"System.IO.Compression.FileSystem",
 				"Mono.CSharp",
 				"System.Security",
+				"System.ServiceModel",
 			};
 			var bcl_skip_watchos = new string [] {
 				"Mono.Security",
