dd95d17b5f19faa2b6efd0a5fa15442c6c96ecb9
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-12-01 15:18:20 +0100

    [generator] Properly set the IsDirectBinding value. (#3063)
    * [generator] Properly set the IsDirectBinding value.
    
    Properly set the IsDirectBinding value to false for models and synthetic types.
    
    This also means we can now stop excluding models when testing if the
    IsDirectBinding value is correct.
    
    Also set IsDirectBinding value to true for sealed wrapper types, since those
    will always be direct bindings since they can't be subclassed.
    
    https://gist.github.com/rolfbjarne/24028bf944db848fed4083c460d0ec71
    
    * [tests] Add introspection exclusion for XM.
    
    * [introspection] Add back exclusions for Classic, since we can't modify/fix Classic assemblies anymore.
    
    * [generator] Print the correct protocol name with the protocol attribute.
    
    Fixes this test failure:
    
        [FAIL] Foundation.NSUrlDownloadDelegate : ConformsToProtocol(null) failed
    
    because our binding code claimed that our `NSUrlDownloadDelegate` class
    implemented the `NSUrlDownloadDelegate` protocol, but since the
    `NSUrlDownloadDelegate` protocol doesn't exist (it's `NSURLDownloadDelegate` -
    different case), we'd verify against a null protocol (and return true from
    `ConformsToProtocol(null)`, which would fail the test).
    
    * [xtro] Only treat interfaces as protocols.
    
    Unfortunately we add [Protocol] to [Model]s as well as on interfaces, but we
    must not process those in xtro, since they don't correspond with the actual protocol.

---
+++ tests/generator/tests/is-direct-binding.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Foundation;
+
+namespace NS {
+	[BaseType (typeof (NSObject))]
+	interface C {
+	}
+
+	[Sealed]
+	[BaseType (typeof (NSObject))]
+	interface S {
+	}
+
+	[Model][Protocol]
+	[BaseType (typeof (NSObject))]
+	interface P {
+	}
+}
