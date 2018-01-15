600b0e0844cf7cfc622822e9564860894bf63610
Manuel de la Pena <mandel@microsoft.com>
2018-01-15 21:14:18 +0100

    Address comments from review.

--- src/CoreGraphics/CGPath.cs
+++ src/CoreGraphics/CGPath.cs
@@ -560,7 +560,7 @@ namespace XamCore.CoreGraphics {
 		static CGPath MakeMutable (IntPtr source)
 		{
 			var mutable = CGPathCreateMutableCopy (source);
-			return new CGPath (mutable, true);
+			return new CGPath (mutable, owns: true);
 		}
 
 		[DllImport (Constants.CoreGraphicsLibrary)]
