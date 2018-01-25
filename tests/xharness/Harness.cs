94352bfef5dc5768268d3bdd430ef6dc665427b2
Vincent Dondain <vidondai@microsoft.com>
2018-01-25 17:50:32 -0500

    [harness] Update mlaunch_url link (w/Xcode 9.3 fix)

--- tests/xharness/Harness.cs
+++ tests/xharness/Harness.cs
@@ -165,7 +165,7 @@ namespace xharness
 		string DownloadMlaunch ()
 		{
 			// NOTE: the filename part in the url must be unique so that the caching logic works properly.
-			var mlaunch_url = "https://dl.xamarin.com/uploads/3euiqmcoizk/mlaunch-18deb964b64886af65fb1760b19adeee58dd8bea.zip";
+			var mlaunch_url = "https://dl.xamarin.com/ios/mlaunch-84c7ab8f92934c8c817c40c4701c1174.zip";
 			var extraction_dir = Path.Combine (Path.GetTempPath (), Path.GetFileNameWithoutExtension (mlaunch_url));
 			var mlaunch_path = Path.Combine (extraction_dir, "bin", "mlaunch");
 
