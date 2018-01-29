5cc720ff3742e373d6910d1afaeed2f71021c305
Vincent Dondain <vidondai@microsoft.com>
2018-01-29 11:07:52 -0500

    [backport][arkit] Fix 'Vertices', 'TextureCoordinates' and 'TriangleIndices' in 'ARFaceGeometry' (#3090) (#3342)
    - Fixes bug #61056: [ARKit] TriangleIndices, Vertices and TextureCoordinates should be respectively short [], Vector3 [] and Vector2 []
    (https://bugzilla.xamarin.com/show_bug.cgi?id=61056)
    - Obsolete `short TriangleIndices`.
    - Obsolete `Vector3 Vertices`.
    - Obsolete `Vector2 TextureCoordinates`.
    - Introduced new `short [] GetTriangleIndices ()`.
    - Introduced new `Vector3 [] GetVertices ()`.
    - Introduced new `Vector2 [] GetTextureCoordinates ()`.

---
+++ src/ARKit/ARCompat.cs
@@ -0,0 +1,29 @@
+//
+// ARCompat.cs: Compatibility functions
+//
+// Authors:
+//	Vincent Dondain <vidondai@microsoft.com>
+//
+// Copyright 2017 Microsoft Inc. All rights reserved.
+//
+
+using System;
+using Vector2 = global::OpenTK.Vector2;
+using Vector3 = global::OpenTK.NVector3;
+
+namespace XamCore.ARKit {
+
+#if !XAMCORE_4_0 && IOS
+	public partial class ARFaceGeometry {
+
+		[Obsolete ("Use the 'GetVertices' method instead.")]
+		public virtual Vector3 Vertices { get; }
+
+		[Obsolete ("Use the 'GetTextureCoordinates' method instead.")]
+		public virtual Vector2 TextureCoordinates { get; }
+
+		[Obsolete ("Use the 'GetTriangleIndices' method instead.")]
+		public virtual short TriangleIndices { get; }
+	}
+#endif
+}
