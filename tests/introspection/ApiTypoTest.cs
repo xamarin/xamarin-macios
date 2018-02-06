46382d86b84d06e2b9f81ff2c2ea0482e1de5221
Chris Hamons <chris.hamons@xamarin.com>
2018-02-06 11:51:06 -0600

    [ImageIO] Update to Xcode 9.3 Beta 1 (#3339)

--- tests/introspection/ApiTypoTest.cs
+++ tests/introspection/ApiTypoTest.cs
@@ -79,6 +79,7 @@ namespace Introspection
 			"Actionname",
 			"Activitiy",
 			"Addin",
+			"Addl",
 			"Addr",
 			"Adjustmentfor",
 			"Aifc",
@@ -173,6 +174,7 @@ namespace Introspection
 			"Dns",
 			"Dont",
 			"Dop",
+			"Dopesheet",
 			"Downsample",
 			"Downmix", // Sound terminology that means making a stereo mix from a 5.1 surround mix.
 			"Dpa",
@@ -192,9 +194,13 @@ namespace Introspection
 			"Elu",
 			"Emagic",
 			"Emaili",
+			"Embd",
+			"Enc",
 			"Eppc",
 			"Exhange",
 			"Exp",
+			"Expr",
+			"Exr",
 			"Felica", // Japanese contactless RFID smart card system
 			"Femtowatts",
 			"Flipside",
@@ -215,6 +221,7 @@ namespace Introspection
 			"Gpu",	// acronym for Graphics Processing Unit
 			"Grbg", // acronym for Green-Red-Blue-Green
 			"Greeking",
+			"Gtin",
 			"Hardlink",
 			"Hdmi",
 			"Hdr",
@@ -238,6 +245,7 @@ namespace Introspection
 			"Imap",
 			"Imaps",
 			"Img",
+			"Inv",
 			"Indoorrun",
 			"Indoorcycle",
 			"Inklist",
@@ -335,6 +343,7 @@ namespace Introspection
 			"Oid",
 			"Olus",
 			"Oneup", // TVElementKeyOneupTemplate
+			"Organisation", // kCGImagePropertyIPTCExtRegistryOrganisationID in Xcode9.3-b1
 			"Orthographyrange",
 			"Orth",
 			"Ostprandial",
@@ -428,6 +437,7 @@ namespace Introspection
 			"Subentities",
 			"Subheadline",
 			"Sublocality",
+			"Sublocation",
 			"Submesh",
 			"Submeshes",
 			"Subpixel",
