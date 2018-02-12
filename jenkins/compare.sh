45d647207476b18373bb4a81d73c2bde17a0eba3
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-02-12 13:09:49 +0100

    [jenkins] Fix copying api comparison files.

--- jenkins/compare.sh
+++ jenkins/compare.sh
@@ -3,5 +3,8 @@
 cd $WORKSPACE
 ./tools/compare-commits.sh --base=origin/pr/$ghprbPullId/merge^1
 
-cp -R tools/comparison/apidiff jenkins-results/apicomparison
+mkdir -p jenkins-results/apicomparison
+
+cp -R tools/comparison/apidiff/diff jenkins-results/apicomparison/
+cp    tools/comparison/apidiff/*.html jenkins-results/apicomparison/
 cp -R tools/comparison/generator-diff jenkins-results/generator-diff
