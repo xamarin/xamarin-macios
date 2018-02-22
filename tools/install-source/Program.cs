22f124666f6c8921bedfb75743da42b0ed682a7b
Chris Hamons <chris.hamons@xamarin.com>
2018-02-22 11:07:33 -0600

    Build with CSC instead of MCS (#3393)
    - Improve error messages from install-source when crashing

--- tools/install-source/Program.cs
+++ tools/install-source/Program.cs
@@ -156,13 +156,18 @@ public class ListSourceFiles {
 				continue;
 			}
 
-			var assemblySrcs = GetFilePathsFromPdb(pdbFile);
-			if (verbose) {
-				Console.WriteLine("Pdb file sources are:");
-				foreach (var p in srcs)
-					Console.WriteLine($"\t{p}");
+			try {
+				var assemblySrcs = GetFilePathsFromPdb(pdbFile);
+				if (verbose) {
+					Console.WriteLine("Pdb file sources are:");
+					foreach (var p in srcs)
+						Console.WriteLine($"\t{p}");
+				}
+				srcs.UnionWith(assemblySrcs);
+			} catch (Exception) {
+				Console.WriteLine ("Error processing: {0}", pdbFile);
+				throw;
 			}
-			srcs.UnionWith(assemblySrcs);
 		}
 
 		// add the paths from the mdb files
@@ -171,13 +176,18 @@ public class ListSourceFiles {
 				Console.WriteLine ("File does not exist: {0}", mdbFile);
 				continue;
 			}
-			var assemblySrcs = GetFilePathsFromMdb (mdbFile);
-			if (verbose) {
-				Console.WriteLine("Mdb file sources are:");
-				foreach (var p in srcs)
-					Console.WriteLine($"\t{p}");
+			try {
+				var assemblySrcs = GetFilePathsFromMdb (mdbFile);
+				if (verbose) {
+					Console.WriteLine("Mdb file sources are:");
+					foreach (var p in srcs)
+						Console.WriteLine($"\t{p}");
+				}
+				srcs.UnionWith (assemblySrcs);
+			} catch (Exception) {
+				Console.WriteLine ("Error processing: {0}", mdbFile);
+				throw;
 			}
-			srcs.UnionWith (assemblySrcs);
 		}
 
 		if (verbose) {
