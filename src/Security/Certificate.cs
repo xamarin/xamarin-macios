1681ab3e0354e28ade1d8fda935b248111a46c31
Vincent Dondain <vidondai@microsoft.com>
2018-02-27 14:50:25 -0500

    Clarify 'ArgumentException' for invalid 'SecKeyType'

--- src/Security/Certificate.cs
+++ src/Security/Certificate.cs
@@ -801,7 +801,7 @@ namespace Security {
 			if (parameters == null)
 				throw new ArgumentNullException (nameof (parameters));
 			if (parameters.KeyType == SecKeyType.Invalid)
-				throw new ArgumentException (nameof (parameters.KeyType));
+				throw new ArgumentException ("invalid 'SecKeyType'", nameof (parameters.KeyType));
 
 			using (var dictionary = parameters.GetDictionary ()) {
 				return CreateRandomKey (dictionary, out error);
