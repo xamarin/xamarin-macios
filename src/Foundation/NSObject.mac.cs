cfc3cbf6efd1b2b4f5e33fdb695a4d6f1697beb0
Manuel de la Pena <mandel@microsoft.com>
2018-02-27 12:53:13 +0100

    [BusinessChat] Introduction of the new framework on iOS and Mac. (#3569)

--- src/Foundation/NSObject.mac.cs
+++ src/Foundation/NSObject.mac.cs
@@ -103,6 +103,7 @@ namespace XamCore.Foundation {
 		static IntPtr ios = Dlfcn.dlopen (Constants.IOSurfaceLibrary, 1);
 		static IntPtr ex = Dlfcn.dlopen (Constants.ExternalAccessoryLibrary, 1);
 		static IntPtr ms = Dlfcn.dlopen (Constants.MetalPerformanceShadersLibrary, 1);
+		static IntPtr bc = Dlfcn.dlopen (Constants.BusinessChatLibrary, 1);
 #endif
 		// ** IF YOU ADD ITEMS HERE PLEASE UPDATE linker/ObjCExtensions.cs and mmp/linker/MonoMac.Tuner/MonoMacNamespaces.cs
 
