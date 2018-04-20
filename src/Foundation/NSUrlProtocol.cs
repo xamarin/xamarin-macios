f71139310a1b819ddb304f04364c580ba2a15bb7
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-04-20 18:17:03 +0200

    [Foundation] Implement NSUrlProtocolClient properly on macOS. Fixes #3968.
    This fixes a 3 year old "temporary" workaround: https://github.com/xamarin/maccore/commit/89e981147d47833e6e8eeaeda8a9635044f10e2c
    
    Fixes https://github.com/xamarin/xamarin-macios/issues/3968.

--- src/Foundation/NSUrlProtocol.cs
+++ src/Foundation/NSUrlProtocol.cs
@@ -26,6 +26,8 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
+using System;
+using ObjCRuntime;
 
 namespace Foundation {
 	public abstract partial class NSUrlProtocol : NSObject {
@@ -39,6 +41,36 @@ namespace Foundation {
 					MarkDirty ();
 				}
 				return client;
+			}
+		}
+#endif
+#if XAMCORE_2_0 && MONOMAC && !XAMCORE_4_0
+		[BindingImpl (BindingImplOptions.Optimizable)]
+		[Obsolete ("Use the overload that takes an 'INSUrlProtocolClient' instead.")]
+		public NSUrlProtocol (NSUrlRequest request, NSCachedUrlResponse cachedResponse, NSUrlProtocolClient client)
+			: base (NSObjectFlag.Empty)
+		{
+			if (request == null)
+				throw new ArgumentNullException ("request");
+			if (client == null)
+				throw new ArgumentNullException ("client");
+			if (IsDirectBinding) {
+				InitializeHandle (global::ObjCRuntime.Messaging.IntPtr_objc_msgSend_IntPtr_IntPtr_IntPtr (this.Handle, selInitWithRequest_CachedResponse_Client_Handle, request.Handle, cachedResponse == null ? IntPtr.Zero : cachedResponse.Handle, client.Handle), "initWithRequest:cachedResponse:client:");
+			} else {
+				InitializeHandle (global::ObjCRuntime.Messaging.IntPtr_objc_msgSendSuper_IntPtr_IntPtr_IntPtr (this.SuperHandle, selInitWithRequest_CachedResponse_Client_Handle, request.Handle, cachedResponse == null ? IntPtr.Zero : cachedResponse.Handle, client.Handle), "initWithRequest:cachedResponse:client:");
+			}
+		}
+
+		[Obsolete ("Use 'Client' instead.")]
+		public virtual NSObject WeakClient {
+			get {
+				var client = Client;
+				if (client == null)
+					return null;
+				var nsclient = client as NSObject;
+				if (nsclient != null)
+					return nsclient;
+				return Runtime.GetNSObject (client.Handle);
 			}
 		}
 #endif
