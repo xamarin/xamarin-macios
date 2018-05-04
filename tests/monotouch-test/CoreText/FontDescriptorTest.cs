90ff07728f0a73d8b3dbcd528ea8c1f8f9efabad
Sebastien Pouliot <sebastien@xamarin.com>
2018-05-04 09:39:41 -0400

    [tests] Remove iOS6 checks (it's the minimum we support now) and ignore RareLigaturesOn on watchOS (like tvOS there's no font with that information we can test upon)

--- tests/monotouch-test/CoreText/FontDescriptorTest.cs
+++ tests/monotouch-test/CoreText/FontDescriptorTest.cs
@@ -60,7 +60,7 @@ namespace MonoTouchFixtures.CoreText {
 			}
 		}
 
-#if __TVOS__
+#if __TVOS__ || __WATCHOS__
 		[Ignore ("No font with ligatures are available on the platform")] // more details in https://bugzilla.xamarin.com/show_bug.cgi?id=58929
 #endif
 		[Test]
