478076b7951f10b2aa3b889ea0fbd20a36435f2f
Sebastien Pouliot <sebastien@xamarin.com>
2018-05-03 22:16:11 -0400

    [coretext] Enable CTFont tests on watchOS and macOS. Fixes #3925
    CTFontCreateWithGraphicsFont is actually available on macOS and watchOS,
    so it's enabled, in CGFont, along with its unit test.
    
    https://github.com/xamarin/xamarin-macios/issues/3925

--- tests/monotouch-test/CoreText/FontDescriptorTest.cs
+++ tests/monotouch-test/CoreText/FontDescriptorTest.cs
@@ -7,8 +7,6 @@
 // Copyright 2012 Xamarin Inc. All rights reserved.
 //
 
-#if !__WATCHOS__
-
 using System;
 #if XAMCORE_2_0
 using Foundation;
@@ -90,5 +88,3 @@ namespace MonoTouchFixtures.CoreText {
 		}
 	}
 }
-
-#endif // !__WATCHOS__
