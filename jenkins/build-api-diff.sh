35ea4630fc82bbcd7baa062ff8a8b0e97cda3a14
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-05-24 09:04:14 -0400

    [jenkins] Update the urls for html reports. (#4119)
    * [jenkins] Update the urls for html reports.
    
    The urls for html reports changed as a consequence of a Jenkins update [1], so
    update how we construct the links accordingly.
    
    See also:
    
    * https://xamarinhq.slack.com/archives/C03CCJNR7/p1526646040000120
    * https://xamarinhq.slack.com/archives/C03CCJNR7/p1526653512000249
    
    [1] https://wiki.jenkins.io/display/JENKINS/HTML+Publisher+Plugin (update to v1.16)
    
    * Remove extra closing parenthesis.

--- jenkins/build-api-diff.sh
+++ jenkins/build-api-diff.sh
@@ -10,4 +10,4 @@ cd $WORKSPACE
 export BUILD_REVISION=jenkins
 make -j8 -C tools/apidiff jenkins-api-diff
 
-printf "✅ [API Diff (from stable)]($BUILD_URL/API_diff_(from_stable))\\n" >> $WORKSPACE/jenkins/pr-comments.md
+printf "✅ [API Diff (from stable)]($BUILD_URL/API_20diff_20_28from_20stable_29)\\n" >> $WORKSPACE/jenkins/pr-comments.md
