71d1a3945e651436d5de344447c0988dd0e824fc
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-05-29 14:27:17 +0200

    [jenkins] Fix scripts to be shellcheck-happy.
    A few general categories of fixes:
    
    * Sprinkle lots of quotes everywhere.
    * Don't use environment variables in the format string to printf, instead pass them as arguments.
    * Don't use backticks to execute commands (it's deprecated), use the new "$(...)" syntax instead.

--- jenkins/compare.sh
+++ jenkins/compare.sh
@@ -1,28 +1,32 @@
 #!/bin/bash -e
 
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+WORKSPACE=$(pwd)
+
 report_error ()
 {
-	printf "🔥 [Failed to compare API and create generator diff]($BUILD_URL/console) 🔥\\n" >> $WORKSPACE/jenkins/pr-comments.md
-	touch $WORKSPACE/jenkins/failure-stamp
+	printf "🔥 [Failed to compare API and create generator diff](%s/console) 🔥\\n" "$BUILD_URL" >> "$WORKSPACE/jenkins/pr-comments.md"
+	touch "$WORKSPACE/jenkins/failure-stamp"
 	exit 0
 }
 trap report_error ERR
 
-cd $WORKSPACE
 
 if ./jenkins/fetch-pr-labels.sh --check=skip-api-comparison; then
-	printf "❎ Skipped API comparison because the PR has the label 'skip-api-comparison'\\n" >> $WORKSPACE/jenkins/pr-comments.md
+	printf "❎ Skipped API comparison because the PR has the label 'skip-api-comparison'\\n" >> "$WORKSPACE/jenkins/pr-comments.md"
 	exit 0
 fi
 
-BASE=origin/pr/$ghprbPullId/merge
-if ! git rev-parse $BASE >/dev/null 2>&1; then
+# SC2154: ghprbPullId is referenced but not assigned.
+# shellcheck disable=SC2154
+BASE="origin/pr/$ghprbPullId/merge"
+if ! git rev-parse "$BASE" >/dev/null 2>&1; then
 	echo "Can't compare API and create generator diff because the pull request has conflicts that must be resolved first (the branch '$BASE' doesn't exist)."
-	printf "🔥 [Failed to compare API and create generator diff because the pull request has conflicts that must be resolved first]($BUILD_URL/console) 🔥\\n" >> $WORKSPACE/jenkins/pr-comments.md
+	printf "🔥 [Failed to compare API and create generator diff because the pull request has conflicts that must be resolved first](%s/console) 🔥\\n" "$BUILD_URL" >> "$WORKSPACE/jenkins/pr-comments.md"
 	exit 0
 fi
 
-./tools/compare-commits.sh --base=$BASE^1
+./tools/compare-commits.sh --base="$BASE^1"
 
 mkdir -p jenkins-results/apicomparison
 
@@ -30,5 +34,5 @@ cp -R tools/comparison/apidiff/diff jenkins-results/apicomparison/
 cp    tools/comparison/apidiff/*.html jenkins-results/apicomparison/
 cp -R tools/comparison/generator-diff jenkins-results/generator-diff
 
-printf "✅ [API Diff (from PR only)]($BUILD_URL/API_20diff_20_28PR_20only_29)\\n" >> $WORKSPACE/jenkins/pr-comments.md
-printf "✅ [Generator Diff]($BUILD_URL/Generator_20Diff)\\n" >> $WORKSPACE/jenkins/pr-comments.md
+printf "✅ [API Diff (from PR only)](%s/API_20diff_20_28PR_20only_29)\\n" "$BUILD_URL" >> "$WORKSPACE/jenkins/pr-comments.md"
+printf "✅ [Generator Diff](%s/Generator_20Diff)\\n" "$BUILD_URL" >> "$WORKSPACE/jenkins/pr-comments.md"
