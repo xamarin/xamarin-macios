82e563226d80514b5e27baf8823b462356fdd69e
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-05-29 20:55:43 +0200

    [jenkins] Revert unintended change that prevents the keychain from unlocking properly.

--- jenkins/run-tests.sh
+++ jenkins/run-tests.sh
@@ -22,7 +22,7 @@ export BUILD_REVISION=jenkins
 security default-keychain -s builder.keychain
 security list-keychains -s builder.keychain
 echo "Unlock keychain"
-security unlock-keychain -p "$(cat ~/.config/"$KEYCHAIN"-keychain)"
+security unlock-keychain -p "$(cat ~/.config/keychain)"
 echo "Increase keychain unlock timeout"
 security set-keychain-settings -lut 7200
 
