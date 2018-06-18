099caab0d5293a0f66382a9a205cbf31b338e016
Manuel de la Pena <mandel@microsoft.com>
2018-06-18 08:20:26 +0200

    Remove argument semantic as mentioned in reviews.

--- src/usernotificationsui.cs
+++ src/usernotificationsui.cs
@@ -94,11 +94,11 @@ namespace UserNotificationsUI {
 
 		// property, but we have to add the two methods since it is a category.
 		[iOS (12, 0)]
-		[Export ("notificationActions", ArgumentSemantic.Copy)]
+		[Export ("notificationActions")]
 		UNNotificationAction[] GetNotificationActions ();
 
 		[iOS (12, 0)]
-		[Export ("setNotificationActions:", ArgumentSemantic.Copy)]
+		[Export ("setNotificationActions:")]
 		void SetNotificationActions (UNNotificationAction[] actions);
 	}
 }
