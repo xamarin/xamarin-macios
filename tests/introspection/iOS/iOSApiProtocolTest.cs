8940743de6c805e818fbb216ae9f544d291e7c9d
Alex Soto <alex@alexsoto.me>
2018-06-20 13:06:44 -0500

    [ReplayKit] Update bindings to Xcode 10 beta 2

--- tests/introspection/iOS/iOSApiProtocolTest.cs
+++ tests/introspection/iOS/iOSApiProtocolTest.cs
@@ -63,7 +63,7 @@ namespace Introspection {
 				break;
 			case "MTLFence":
 			case "MTLHeap":
-			case "RPBroadcastPickerView": // Symbol not available in simulator
+			case "RPSystemBroadcastPickerView": // Symbol not available in simulator
 				if (Runtime.Arch != Arch.DEVICE)
 					return true;
 
