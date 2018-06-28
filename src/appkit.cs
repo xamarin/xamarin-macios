b610560b1d7936ebfd2e69c0b0a60527fd59de77
Chris Hamons <chris.hamons@xamarin.com>
2018-06-28 15:37:36 -0500

    Add onlyOn64 to all Mac (10, 14)'s

--- src/appkit.cs
+++ src/appkit.cs
@@ -7900,12 +7900,12 @@ namespace AppKit {
 
 #if XAMCORE_4_0
 		[Export ("itemArray", ArgumentSemantic.Copy)]
-		NSMenuItem[] ItemArray { get; [Mac (10, 14)] set; }
+		NSMenuItem[] ItemArray { get; [Mac (10, 14, onlyOn64: true))] set; }
 #else
 		[Export ("itemArray")]
 		NSMenuItem [] ItemArray ();
 
-		[Mac (10, 14)]
+		[Mac (10, 14, onlyOn64: true))]
 		[Export ("setItemArray:")]
 		void SetItemArray (NSMenuItem [] items);
 #endif
@@ -17823,12 +17823,12 @@ namespace AppKit {
 
 #if XAMCORE_4_0
 		[Export ("tabViewItems")]
-		NSTabViewItem [] Items { get; [Mac (10, 14)] set; }
+		NSTabViewItem [] Items { get; [Mac (10, 14, onlyOn64: true))] set; }
 #else
 		[Export ("tabViewItems")]
 		NSTabViewItem [] Items { get; }
 
-		[Mac (10, 14)]
+		[Mac (10, 14, onlyOn64: true))]
 		[Export ("setTabViewItems:")]
 		void SetItems (NSTabViewItem [] items);
 #endif
@@ -18238,12 +18238,12 @@ namespace AppKit {
 
 #if XAMCORE_4_0
 		[Export ("cellBaselineOffset")]
-		CGPoint CellBaselineOffset { get; [Mac (10, 14)] set; }
+		CGPoint CellBaselineOffset { get; [Mac (10, 14, onlyOn64: true))] set; }
 #else
 		[Export ("cellBaselineOffset")]
 		CGPoint CellBaselineOffset { get; }
 
-		[Mac (10, 14)]
+		[Mac (10, 14, onlyOn64: true)]
 		[Export ("setCellBaselineOffset:")]
 		void SetCellBaselineOffset (CGPoint point);
 #endif
