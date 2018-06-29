d74694c20d40f6f0b839383cf5a8f9ac7c7ecd4e
Chris Hamons <chris.hamons@xamarin.com>
2018-06-29 17:25:27 -0500

    Add basic test for NSKeyUnarchiever wrappers

---
+++ tests/monotouch-test/Foundation/NSKeyedUnarchiverTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Foundation;
+using NUnit.Framework;
+using ObjCRuntime;
+
+namespace MonoTouchFixtures.Foundation
+{
+	[TestFixture]
+	[Preserve (AllMembers = true)]
+	public class NSKeyedUnarchiverTest
+	{
+		[Test]
+		public void GetUnarchivedObject_TypeWrappers ()
+		{
+			if (!TestRuntime.CheckXcodeVersion (10, 0))
+				Assert.Ignore ("Ignoring GetUnarchivedObject_TypeWrappers as requires new APIs");
+
+			NSDictionary<NSString, NSString> testValues = new NSDictionary<NSString, NSString> ((NSString)"1", (NSString)"a");
+			NSData data = NSKeyedArchiver.ArchivedDataWithRootObject (testValues, true, out NSError error);
+			Assert.IsNull (error);
+
+			Type dictionaryType = typeof (NSDictionary<NSString, NSString>);
+			Class dictionaryClass = new Class (dictionaryType);
+			NSObject o = NSKeyedUnarchiver.GetUnarchivedObject (dictionaryClass, data, out error);
+			Assert.IsNotNull (o);
+			Assert.IsNull (error);
+
+			o = NSKeyedUnarchiver.GetUnarchivedObject (new NSSet<Class> (new Class [] { dictionaryClass }), data, out error);
+			Assert.IsNotNull (o);
+			Assert.IsNull (error);
+
+			o = NSKeyedUnarchiver.GetUnarchivedObject (dictionaryType, data, out error);
+			Assert.IsNotNull (o);
+			Assert.IsNull (error);
+
+			o = NSKeyedUnarchiver.GetUnarchivedObject (new Type [] { dictionaryType }, data, out error);
+			Assert.IsNotNull (o);
+			Assert.IsNull (error);
+		}
+	}
+}
