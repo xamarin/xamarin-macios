970ae9cabaf9f76cec9e0f2c0f0aec7e13c21285
Manuel de la Pena <mandel@microsoft.com>
2018-07-19 17:49:37 +0200

    Update struct definition.

--- src/CoreMidi/MidiCIDeviceIdentification.cs
+++ src/CoreMidi/MidiCIDeviceIdentification.cs
@@ -10,10 +10,15 @@ namespace CoreMidi {
 	[NoWatch, NoTV, Mac (10,14, onlyOn64: true), iOS (12,0)]
 	[StructLayout (LayoutKind.Sequential)]
 	public struct MidiCIDeviceIdentification {
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
 		public byte[] Manufacturer;
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
 		public byte[] Family;
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
 		public byte[] ModelNumber;
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
 		public byte[] RevisionLevel;
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)]
 		public byte[] Reserved;
 	}
 }
