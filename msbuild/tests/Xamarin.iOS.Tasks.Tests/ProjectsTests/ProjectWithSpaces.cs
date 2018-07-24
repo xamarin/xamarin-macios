b2183e38a8bf639c31be4b45532bc75fe44bfab9
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-07-24 11:00:27 +0200

    [msbuild] Don't put gcc/linker flags in the response file, since Mono.Options doesn't support escaping quotes. Fixes vsts#649776.
    Mono.Options doesn't (yet) support escaped quotes when parsing response files,
    which becomes a problem because escaped quotes are necessary when passing
    paths with spaces as gcc/linker flags.
    
    So don't write gcc/linker flags in the response file, and instead pass them as
    normal command line arguments, and to be on the safe side, do the same thing
    for all extra arguments passed to mmp/mtouch.
    
    Also add tests.
    
    Fixes https://devdiv.visualstudio.com/DevDiv/_workitems/edit/649776.

---
+++ msbuild/tests/Xamarin.iOS.Tasks.Tests/ProjectsTests/ProjectWithSpaces.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Xamarin.iOS.Tasks
+{
+	[TestFixture ("iPhone")]
+	[TestFixture ("iPhoneSimulator")]
+	public class ProjectWithSpacesTests : ProjectTest
+	{
+		public ProjectWithSpacesTests (string platform) : base (platform)
+		{
+		}
+
+		[Test]
+		public void BasicTest ()
+		{
+			this.BuildProject ("My Spaced App", Platform, "Debug", clean: false);
+		}
+	}
+}
+
