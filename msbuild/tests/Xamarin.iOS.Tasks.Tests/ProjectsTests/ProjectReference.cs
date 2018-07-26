c40cbf295d03dd6ddd6cb84294ec1db96e0e0eaf
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-07-26 19:04:32 +0200

    [msbuild] Set 'CopyNuGetImplementations' to true for app extensions. Fixes #4235 and #4237.
    In Xamarin.iOS.Common.targets, just before the _CompileToNative target, we
    modify the mtouch references to ensure that we get the lib assemblies for
    nugets, and not the ref references:
    
    https://github.com/xamarin/xamarin-macios/blob/9e31d07ecc08a64372dd562e843c3d8950d24985/msbuild/Xamarin.iOS.Tasks.Core/Xamarin.iOS.Common.targets#L784-L791
    
    This logic removes nuget references, and then re-adds any copy-local dll
    references.
    
    This works fine in executable projects, but not in library projects (aka
    extensions), because nugets aren't copied for library projects:
    
    https://github.com/NuGet/NuGet.BuildTasks/blob/cf4b0a12cf1f75e0654f28c2a9020251c41d126a/src/Microsoft.NuGet.Build.Tasks/Microsoft.NuGet.targets#L86
    
    So we need to set the CopyNuGetImplementations variable to 'true' for our
    library projects.
    
    Fixes https://github.com/xamarin/xamarin-macios/issues/4235.
    Fixes https://github.com/xamarin/xamarin-macios/issues/4237.

---
+++ msbuild/tests/Xamarin.iOS.Tasks.Tests/ProjectsTests/ProjectReference.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+using Xamarin.Tests;
+
+namespace Xamarin.iOS.Tasks
+{
+	[TestFixture ("iPhone")]
+	[TestFixture ("iPhoneSimulator")]
+	public class ProjectReferenceTests : ProjectTest
+	{
+
+		public ProjectReferenceTests (string platform) : base (platform)
+		{
+		}
+
+		[Test]
+		public void BasicTest ()
+		{
+			NugetRestore ("../MyAppWithPackageReference/MyAppWithPackageReference.csproj");
+			NugetRestore ("../MyExtensionWithPackageReference/MyExtensionWithPackageReference.csproj");
+
+			// Can't use the in-process MSBuild engine, because it complains that the project file is invalid (the attribute 'Version' in the element '<PackageReference>' is unrecognized)
+			var rv = ExecutionHelper.Execute ("msbuild", $"../MyAppWithPackageReference/MyAppWithPackageReference.csproj /p:Platform={Platform} /p:Configuration=Debug", out var output);
+			if (rv != 0) {
+				Console.WriteLine ("Build failed:");
+				Console.WriteLine (output);
+				Assert.Fail ("Build failed");
+			}
+		}
+	}
+}
+
