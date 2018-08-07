5d3f60c06393cfe3d0c1f3e022e39ccdb6f43c64
Chris Hamons <chris.hamons@xamarin.com>
2018-08-07 15:04:04 -0500

    Add Xamarin.Mac test showing that fix is not needed (?!?)

---
+++ tests/mmptest/src/PackageReferenceTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Xamarin.MMP.Tests
+{
+	[TestFixture]
+	public class PackageReferenceTests {
+
+		[TestCase (true)]
+		[TestCase (false)]
+		public void PackageReferencs_ResolveLibAndRefCorrectly (bool full)
+		{
+			MMPTests.RunMMPTest (tmpDir => {
+				var config = new TI.UnifiedTestConfig (tmpDir) {
+					ItemGroup = @"<ItemGroup><PackageReference Include = ""Newtonsoft.Json"" Version = ""10.0.3"" /></ItemGroup>",
+					TestCode = @"var output = Newtonsoft.Json.JsonConvert.SerializeObject (new int[] { 1, 2, 3 });
+			if (output == ""[1,2,3]"")
+				",
+					XM45 = full
+				};
+				TI.AddGUIDTestCode (config);
+
+				string project = TI.GenerateUnifiedExecutableProject (config);
+				TI.NugetRestore (project);
+				TI.BuildProject (project, true, useMSBuild: true);
+				TI.RunGeneratedUnifiedExecutable (config);
+			});
+		}
+	}
+}
