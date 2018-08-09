4904d53afb1cb3920d7e97e72f9f6e0e1a28fe68
Vincent Dondain <vidondai@microsoft.com>
2018-08-09 17:29:15 -0400

    Add MetalTask and MetalLibTask tests

---
+++ tests/mtouch/ToolTasksBinPathTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+using Xamarin.Tests;
+
+namespace AppleSdkSettings {
+
+	public static class XcodeVersion {
+		public static int Major = Version.Parse (Configuration.XcodeVersion).Major;
+	}
+}
+
+namespace Xamarin.MacDev.Tasks {
+
+	public abstract class MetalTaskBase {
+
+		protected abstract string OperatingSystem {
+			get;
+		}
+
+		protected abstract string DevicePlatformBinDir {
+			get;
+		}
+
+		protected string SdkDevPath {
+			get { return ""; }
+		}
+	}
+
+	public abstract class MetalLibTaskBase {
+
+		protected abstract string DevicePlatformBinDir {
+			get;
+		}
+
+		protected string SdkDevPath {
+			get { return ""; }
+		}
+	}
+}
+
+namespace Xamarin.Mac.Tasks {
+
+	public class MetalPoker : Metal {
+
+		public new string DevicePlatformBinDir {
+			get { return base.DevicePlatformBinDir; }
+		}
+	}
+
+	public class MetalLibPoker : MetalLib {
+
+		public new string DevicePlatformBinDir {
+			get { return base.DevicePlatformBinDir; }
+		}
+	}
+
+	[TestFixture]
+	public class ToolTasksBinPathTest {
+
+		[SetUp]
+		public void Setup ()
+		{
+			Environment.SetEnvironmentVariable ("DEVELOPER_DIR", Configuration.xcode_root);
+		}
+
+		[Test]
+		public void MetalBinPathTest ()
+		{
+			var metalTask = new MetalPoker ();
+
+			var psi = new ProcessStartInfo ("xcrun") {
+				Arguments = "-f metal",
+				UseShellExecute = false,
+				CreateNoWindow = true,
+				RedirectStandardOutput = true,
+			};
+			var proc = Process.Start (psi);
+
+			string output = proc.StandardOutput.ReadToEnd ();
+			Console.WriteLine (output); // Print the output in case something wrong happens when executing the command
+
+			Assert.True (output.Contains (metalTask.DevicePlatformBinDir));
+		}
+
+		[Test]
+		public void MetalLibBinPathTest ()
+		{
+			var metalLibTask = new MetalLibPoker ();
+
+			var psi = new ProcessStartInfo ("xcrun") {
+				Arguments = "-f metallib",
+				UseShellExecute = false,
+				CreateNoWindow = true,
+				RedirectStandardOutput = true,
+			};
+			var proc = Process.Start (psi);
+
+			string output = proc.StandardOutput.ReadToEnd ();
+			Console.WriteLine (output); // Print the output in case something wrong happens when executing the command
+
+			Assert.True (output.Contains (metalLibTask.DevicePlatformBinDir));
+		}
+	}
+}
