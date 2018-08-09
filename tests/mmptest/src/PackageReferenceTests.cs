8b73f51b074679cbc3759d50b1384a6dbb808502
Chris Hamons <chris.hamons@xamarin.com>
2018-08-09 08:45:38 -0500

    Disable XM tests due to msbuild redirect sadness

--- tests/mmptest/src/PackageReferenceTests.cs
+++ tests/mmptest/src/PackageReferenceTests.cs
@@ -11,8 +11,8 @@ namespace Xamarin.MMP.Tests
 		const string PackageReference = @"<ItemGroup><PackageReference Include = ""Newtonsoft.Json"" Version = ""10.0.3"" /></ItemGroup>";
 		const string TestCode = @"var output = Newtonsoft.Json.JsonConvert.SerializeObject (new int[] { 1, 2, 3 });";
 
-		[TestCase (true)]
-		[TestCase (false)]
+		// [TestCase (true)] https://github.com/xamarin/xamarin-macios/issues/4110
+		// [TestCase (false)] https://github.com/xamarin/xamarin-macios/issues/4110
 		public void AppsWithPackageReferencs_BuildAndRun (bool full)
 		{
 			MMPTests.RunMMPTest (tmpDir => {
@@ -31,7 +31,7 @@ namespace Xamarin.MMP.Tests
 			});
 		}
 
-		[Test]
+		// [Test] https://github.com/xamarin/xamarin-macios/issues/4110
 		public void ExtensionProjectPackageReferencs_Build ()
 		{
 			MMPTests.RunMMPTest (tmpDir => {
