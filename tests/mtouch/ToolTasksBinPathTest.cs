7d8bf273dddb838b7db1122fcf5ed053095e5ac1
Vincent Dondain <vidondai@microsoft.com>
2018-08-16 16:14:30 -0400

    Fixes based on feedback

--- tests/mtouch/ToolTasksBinPathTest.cs
+++ tests/mtouch/ToolTasksBinPathTest.cs
@@ -58,50 +58,36 @@ namespace Xamarin.Mac.Tasks {
 	[TestFixture]
 	public class ToolTasksBinPathTest {
 
-		[SetUp]
-		public void Setup ()
-		{
-			Environment.SetEnvironmentVariable ("DEVELOPER_DIR", Configuration.xcode_root);
-		}
-
 		[Test]
 		public void MetalBinPathTest ()
 		{
 			var metalTask = new MetalPoker ();
-
-			var psi = new ProcessStartInfo ("xcrun") {
-				Arguments = "-f metal",
-				UseShellExecute = false,
-				CreateNoWindow = true,
-				RedirectStandardOutput = true,
-				RedirectStandardError = true,
-			};
-			var proc = Process.Start (psi);
-
-			string output = proc.StandardOutput.ReadToEnd ();
-			string err = proc.StandardError.ReadToEnd ();
-
-			Assert.True (output.Contains (metalTask.DevicePlatformBinDir), err);
+			CheckToolBinDir ("metal", metalTask.DevicePlatformBinDir);
 		}
 
 		[Test]
 		public void MetalLibBinPathTest ()
 		{
 			var metalLibTask = new MetalLibPoker ();
+			CheckToolBinDir ("metallib", metalLibTask.DevicePlatformBinDir);
+		}
 
+		public void CheckToolBinDir (string taskName, string binDirToCheck)
+		{
 			var psi = new ProcessStartInfo ("xcrun") {
-				Arguments = "-f metallib",
+				Arguments = $"-f {taskName}",
 				UseShellExecute = false,
 				CreateNoWindow = true,
 				RedirectStandardOutput = true,
 				RedirectStandardError = true,
 			};
+			psi.EnvironmentVariables.Add ("DEVELOPER_DIR", Configuration.xcode_root);
 			var proc = Process.Start (psi);
 
 			string output = proc.StandardOutput.ReadToEnd ();
 			string err = proc.StandardError.ReadToEnd ();
 
-			Assert.True (output.Contains (metalLibTask.DevicePlatformBinDir), err);
+			Assert.True (output.Contains (binDirToCheck), err);
 		}
 	}
 }
