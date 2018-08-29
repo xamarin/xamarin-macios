b1ebb892225347eb99dca054b445ec9742accffb
Manuel de la Pena <mandel@microsoft.com>
2018-08-29 23:33:09 +0200

    [Security] Fix some of the new availability attrs. (#4728)

--- src/Security/SecTrust2.cs
+++ src/Security/SecTrust2.cs
@@ -20,12 +20,11 @@ using CoreFoundation;
 
 namespace Security {
 
-	[TV (12,0), Mac (10,14), iOS (12,0)]
+	[TV (12,0), Mac (10,14, onlyOn64: true), iOS (12,0)]
 	public class SecTrust2 : NativeObject {
 		internal SecTrust2 (IntPtr handle) : base (handle, false) {}
 		public SecTrust2 (IntPtr handle, bool owns) : base (handle, owns) {}
 
-		[TV (12,0), Mac (10,14), iOS (12,0)]
 		[DllImport (Constants.SecurityLibrary)]
 		extern static IntPtr sec_trust_create (IntPtr sectrustHandle);
 
@@ -37,11 +36,9 @@ namespace Security {
 			Handle = sec_trust_create (trust.Handle);
 		}
 
-		[TV (12,0), Mac (10,14), iOS (12,0)]
 		[DllImport (Constants.SecurityLibrary)]
 		extern static IntPtr sec_trust_copy_ref (IntPtr handle);
 
-		[TV (12,0), Mac (10,14), iOS (12,0)]
 		public SecTrust Trust => new SecTrust (sec_trust_copy_ref (GetCheckedHandle ()), owns: true);
 	}
 }
