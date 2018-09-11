778e7ac6a7c0f94b5d172090dc629653b96f5fd0
Sebastien Pouliot <sebastien.pouliot@microsoft.com>
2018-09-10 21:36:02 -0400

    [tests] Add Xcode checks to new tests

--- tests/monotouch-test/Security/IdentityTest.cs
+++ tests/monotouch-test/Security/IdentityTest.cs
@@ -46,6 +46,7 @@ namespace MonoTouchFixtures.Security {
 		[Test]
 		public void I2 ()
 		{
+			TestRuntime.AssertXcodeVersion (10,0);
 			using (var i1 = GetIdentity ()) 
 			using (var i2 = new SecIdentity2 (i1)) {
 				Assert.That (i1.Certificate.GetCommonName (), Is.EqualTo (i2.Identity.Certificate.GetCommonName ()), "GetCommonName");
