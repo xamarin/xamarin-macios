895422505d774d70fc5c714f3bd217d4cdd25024
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-09-12 12:27:44 +0200

    [HealthKit] Simplify code a bit more after enum changes.

--- src/HealthKit/HKObjectType.cs
+++ src/HealthKit/HKObjectType.cs
@@ -16,79 +16,28 @@ namespace HealthKit
 	public partial class HKQuantityType {
 		public static HKQuantityType Create (HKQuantityTypeIdentifier kind)
 		{
-			return HKObjectType.GetQuantityType (HKQuantityTypeIdentifierExtensions.GetConstant (kind));
+			return HKObjectType.GetQuantityType (kind.GetConstant ());
 		}
 	}
 
 	public partial class HKCategoryType {
-		static internal NSString ToKey (HKCategoryTypeIdentifier kind)
-		{
-			switch (kind){
-			case HKCategoryTypeIdentifier.SleepAnalysis:
-				return HKCategoryTypeIdentifierKey.SleepAnalysis;
-			case HKCategoryTypeIdentifier.AppleStandHour:
-				return HKCategoryTypeIdentifierKey.AppleStandHour;
-			case HKCategoryTypeIdentifier.CervicalMucusQuality:
-				return HKCategoryTypeIdentifierKey.CervicalMucusQuality;
-			case HKCategoryTypeIdentifier.OvulationTestResult:
-				return HKCategoryTypeIdentifierKey.OvulationTestResult;
-			case HKCategoryTypeIdentifier.MenstrualFlow:
-				return HKCategoryTypeIdentifierKey.MenstrualFlow;
-			case HKCategoryTypeIdentifier.IntermenstrualBleeding:
-				return HKCategoryTypeIdentifierKey.IntermenstrualBleeding;
-			case HKCategoryTypeIdentifier.SexualActivity:
-				return HKCategoryTypeIdentifierKey.SexualActivity;
-			case HKCategoryTypeIdentifier.MindfulSession:
-				return HKCategoryTypeIdentifierKey.MindfulSession;
-			}
-			return null;
-		}
-
 		public static HKCategoryType Create (HKCategoryTypeIdentifier kind)
 		{
-			return HKObjectType.GetCategoryType (ToKey (kind));
+			return HKObjectType.GetCategoryType (kind.GetConstant ());
 		}
 	}
 
 	public partial class HKCharacteristicType {
-		static internal NSString ToKey (HKCharacteristicTypeIdentifier kind)
-		{
-			switch (kind){
-			case HKCharacteristicTypeIdentifier.BiologicalSex:
-				return HKCharacteristicTypeIdentifierKey.BiologicalSex;
-			case HKCharacteristicTypeIdentifier.BloodType:
-				return HKCharacteristicTypeIdentifierKey.BloodType;
-			case HKCharacteristicTypeIdentifier.DateOfBirth:
-				return HKCharacteristicTypeIdentifierKey.DateOfBirth;
-			case HKCharacteristicTypeIdentifier.FitzpatrickSkinType:
-				return HKCharacteristicTypeIdentifierKey.FitzpatrickSkinType;
-			case HKCharacteristicTypeIdentifier.WheelchairUse:
-				return HKCharacteristicTypeIdentifierKey.WheelchairUse;
-			}
-			return null;
-		}
-		
 		public static HKCharacteristicType Create (HKCharacteristicTypeIdentifier kind)
 		{
-			return HKObjectType.GetCharacteristicType (ToKey (kind));
+			return HKObjectType.GetCharacteristicType (kind.GetConstant ());
 		}
 	}
 
 	public partial class HKCorrelationType {
-		static internal NSString ToKey (HKCorrelationTypeIdentifier kind)
-		{
-			switch (kind) {
-			case HKCorrelationTypeIdentifier.BloodPressure:
-				return HKCorrelationTypeKey.IdentifierBloodPressure;
-			case HKCorrelationTypeIdentifier.Food:
-				return HKCorrelationTypeKey.IdentifierFood;
-			}
-			return null;
-		}
-
 		public static HKCorrelationType Create (HKCorrelationTypeIdentifier kind)
 		{
-			return HKObjectType.GetCorrelationType (ToKey (kind));
+			return HKObjectType.GetCorrelationType (kind.GetConstant ());
 		}
 	}
 
