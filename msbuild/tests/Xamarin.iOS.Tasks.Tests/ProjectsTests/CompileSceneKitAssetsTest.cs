0d65ed93578d45183b704ccef98f3f939721b9fc
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-09-24 18:42:04 +0200

    [msbuild] Fix SceneKit asset compilation. Fixes #3766. (#4851)
    * [msbuild] Fix SceneKit asset compilation. Fixes #3766.
    
    It seems SceneKit assets must be compiled into a directory whose parent
    directory is named like the app.
    
    In addition the `--resource-folder-path` argument is required.
    
    Fixes https://github.com/xamarin/xamarin-macios/issues/3766.
    
    * [msbuild] Use AppBundlePath to get correct .app[ex] variant.
    
    * [msbuild] Fix XM builds.
    
    * [msbuild] Use AppBundleDir instead of AppBundlePath.

---
+++ msbuild/tests/Xamarin.iOS.Tasks.Tests/ProjectsTests/CompileSceneKitAssetsTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Xamarin.Tests;
+
+namespace Xamarin.iOS.Tasks
+{
+	// [TestFixture ("iPhone")] // Skip this to speed things up a bit.
+	[TestFixture ("iPhoneSimulator")]
+	public class CompileSceneKitAssetsTest : ProjectTest
+	{
+		public CompileSceneKitAssetsTest (string platform) : base (platform)
+		{
+		}
+
+		[Test]
+		public void Compilation ()
+		{
+			var csproj = BuildProject ("MySceneKitApp", Platform, "Debug", clean: true);
+			var appPath = Path.Combine (Path.GetDirectoryName (csproj), "bin", Platform, "Debug", "MySceneKitApp.app");
+			var scenePath = Path.GetFullPath (Path.Combine (appPath, "art.scnassets", "scene.scn"));
+
+			var xml = Configuration.ReadPListAsXml (scenePath);
+			Assert.That (xml, Is.StringContaining ("<string>art.scnassets/texture.png</string>"), "asset with path");
+		}
+	}
+}
