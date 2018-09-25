ea63e4559c2632a8981a2ceecdce0985c55f1fd9
Manuel de la Pena <mandel@microsoft.com>
2018-09-25 16:24:41 +0200

    [CoreMedia] Expose the CMSampleAttachmentKey interface. Fixes #4688 (#4841)
    * [CoreMedia] Expose the CMSampleAttachmentKey interface. Fixes #4688
    
    We have an issue when the user wants to use the CMAttachmentSample,
    ideally, the user will want to do:
    
    var cameraIntrinsicData = CMAttachmentBearer.GetAttachment(CMSampleAttachmentKey.CameraIntrinsicMatrixKey, sampleBuffer );
    
    Instead of the current workaround:
    
    var att = new CMSampleBufferAttachmentSettings (sampleBuffer.GetAttachments (CMAttachmentMode.ShouldNotPropagate));
    var matrix = att.CameraIntrinsicMatrix;
    
    With this push, we can allow the user use the first example provided in
    this commit.
    
    Fixes https://github.com/xamarin/xamarin-macios/issues/4688

---
+++ src/CoreMedia/Enums.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Runtime.InteropServices;
+using Foundation;
+using ObjCRuntime;
+
+namespace CoreMedia {
+	// keys names got changed at some point, but they all refer to a CMSampleBuffer (there is not CMSample obj)
+	enum CMSampleBufferAttachmentKey { 
+		[Field ("kCMSampleAttachmentKey_NotSync")]
+		NotSync,
+		[Field ("kCMSampleAttachmentKey_PartialSync")]
+		PartialSync,
+		[Field ("kCMSampleAttachmentKey_HasRedundantCoding")]
+		HasRedundantCoding,
+		[Field ("kCMSampleAttachmentKey_IsDependedOnByOthers")]
+		IsDependedOnByOthers,
+		[Field ("kCMSampleAttachmentKey_DependsOnOthers")]
+		DependsOnOthers,
+		[Field ("kCMSampleAttachmentKey_EarlierDisplayTimesAllowed")]
+		EarlierDisplayTimesAllowed,
+		[Field ("kCMSampleAttachmentKey_DisplayImmediately")]
+		DisplayImmediately,
+		[Field ("kCMSampleAttachmentKey_DoNotDisplay")]
+		DoNotDisplay,
+		[iOS (11,0), Mac (10,13), TV (11,0)]
+		[Field ("kCMSampleAttachmentKey_HEVCTemporalLevelInfo")]
+		HevcTemporalLevelInfo,
+		[iOS (11,0), Mac (10,13), TV (11,0)]
+		[Field ("kCMSampleAttachmentKey_HEVCTemporalSubLayerAccess")]
+		HevcTemporalSubLayerAccess,
+		[iOS (11,0), Mac (10,13), TV (11,0)]
+		[Field ("kCMSampleAttachmentKey_HEVCStepwiseTemporalSubLayerAccess")]
+		HevcStepwiseTemporalSubLayerAccess,
+		[iOS (11,0), Mac (10,13), TV (11,0)]
+		[Field ("kCMSampleAttachmentKey_HEVCSyncSampleNALUnitType")]
+		HevcSyncSampleNalUnitType,
+		[Field ("kCMSampleBufferAttachmentKey_ResetDecoderBeforeDecoding")]
+		ResetDecoderBeforeDecoding,
+		[Field ("kCMSampleBufferAttachmentKey_DrainAfterDecoding")]
+		DrainAfterDecoding,
+		[Field ("kCMSampleBufferAttachmentKey_PostNotificationWhenConsumed")]
+		PostNotificationWhenConsumed,
+		[Field ("kCMSampleBufferAttachmentKey_ResumeOutput")]
+		ResumeOutput,
+		[Field ("kCMSampleBufferAttachmentKey_TransitionID")]
+		TransitionId,
+		[Field ("kCMSampleBufferAttachmentKey_TrimDurationAtStart")]
+		TrimDurationAtStart,
+		[Field ("kCMSampleBufferAttachmentKey_TrimDurationAtEnd")]
+		TrimDurationAtEnd,
+		[Field ("kCMSampleBufferAttachmentKey_SpeedMultiplier")]
+		SpeedMultiplier,
+		[Field ("kCMSampleBufferAttachmentKey_Reverse")]
+		Reverse,
+		[Field ("kCMSampleBufferAttachmentKey_FillDiscontinuitiesWithSilence")]
+		FillDiscontinuitiesWithSilence,
+		[Field ("kCMSampleBufferAttachmentKey_EmptyMedia")]
+		EmptyMedia,
+		[Field ("kCMSampleBufferAttachmentKey_PermanentEmptyMedia")]
+		PermanentEmptyMedia,
+		[Field ("kCMSampleBufferAttachmentKey_DisplayEmptyMediaImmediately")]
+		DisplayEmptyMediaImmediately,
+		[Field ("kCMSampleBufferAttachmentKey_EndsPreviousSampleDuration")]
+		EndsPreviousSampleDuration,
+		[Field ("kCMSampleBufferAttachmentKey_SampleReferenceURL")]
+		SampleReferenceUrl,
+		[Field ("kCMSampleBufferAttachmentKey_SampleReferenceByteOffset")]
+		SampleReferenceByteOffset, 
+		[Field ("kCMSampleBufferAttachmentKey_GradualDecoderRefresh")]
+		GradualDecoderRefresh,
+		[iOS (6,0)][Mac (10,14, onlyOn64: true)]
+		[Field ("kCMSampleBufferAttachmentKey_DroppedFrameReason")]
+		DroppedFrameReason,
+		[iOS (9,0)][Mac (10,14, onlyOn64: true)]
+		[Field ("kCMSampleBufferAttachmentKey_StillImageLensStabilizationInfo")]
+		StillImageLensStabilizationInfo,
+		[iOS (11,0), Mac (10,13), TV (11,0)]
+		[Field ("kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix")]
+		CameraIntrinsicMatrix,
+		[Mac (10,14, onlyOn64: true)]
+		[Field ("kCMSampleBufferAttachmentKey_DroppedFrameReasonInfo")]
+		DroppedFrameReasonInfo,
+		[Field ("kCMSampleBufferAttachmentKey_ForceKeyFrame")]
+		ForceKeyFrame,
+	}
+}
