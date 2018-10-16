b2a596d680415bf3bbcff7b10a9624f1045ad841
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2018-10-15 20:00:03 -0400

    Apply XM audit fixes from `xcode10` (#4986)
    Backport of
    https://github.com/xamarin/xamarin-macios/commit/b40230c09d557991b776de918b047442cd41533f

--- src/foundation.cs
+++ src/foundation.cs
@@ -12369,7 +12369,11 @@ namespace Foundation
 	partial interface NSFilePresenter {
 		[Abstract]
 		[Export ("presentedItemURL", ArgumentSemantic.Retain)]
+#if XAMCORE_4_0
+		NSUrl PresentedItemUrl { get; }
+#else
 		NSUrl PresentedItemURL { get; }
+#endif
 
 #if XAMCORE_2_0
 		[Abstract]
