2ff3f2c5f02a7472a37dda6aeefbc2adc628ea8f
Vincent Dondain <vidondai@microsoft.com>
2018-10-16 10:41:28 -0400

    [coretelephony] Update for Xcode 10.1 beta 2
    Filed radar https://trello.com/c/WKIRTcLk for probably incorrect macOS API.

--- src/coretelephony.cs
+++ src/coretelephony.cs
@@ -147,11 +147,34 @@ namespace CoreTelephony {
 		string CarrierName { get; }
 	}
 
+	interface ICTSubscriberDelegate {}
+
+	[Protocol]
+	[iOS (12,1)]
+	interface CTSubscriberDelegate {
+		[Abstract]
+		[Export ("subscriberTokenRefreshed:")]
+		void SubscriberTokenRefreshed (CTSubscriber subscriber);
+	}
+
 	[BaseType (typeof (NSObject))]
 	[iOS (7,0)]
 	partial interface CTSubscriber {
 		[iOS (7,0), Export ("carrierToken")]
 		NSData CarrierToken { get; }
+
+		[iOS (12,1)]
+		[Export ("identifier")]
+		string Identifier { get; }
+
+		[iOS (12,1)]
+		[NullAllowed, Export ("delegate", ArgumentSemantic.Weak)]
+		NSObject WeakDelegate { get; set; }
+
+		[iOS (12,1)]
+		[Wrap ("WeakDelegate")]
+		[NullAllowed]
+		ICTSubscriberDelegate Delegate { get; set; }
 	}
 
 #if !XAMCORE_2_0
@@ -160,9 +183,15 @@ namespace CoreTelephony {
 
 	[iOS (6,0), BaseType (typeof (NSObject))]
 	partial interface CTSubscriberInfo {
+		[Deprecated (PlatformName.iOS, 12, 1, message : "Use 'Subscribers' instead.")]
 		[Static]
 		[Export ("subscriber")]
 		CTSubscriber Subscriber { get; }
+
+		[iOS (12,1)]
+		[Static]
+		[Export ("subscribers")]
+		CTSubscriber[] Subscribers { get; }
 	}
 
 	[iOS (12,0)]
