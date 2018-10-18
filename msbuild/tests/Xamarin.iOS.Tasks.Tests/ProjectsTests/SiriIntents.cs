8a3ab81276df12fbd57570e6a27756c262c609f2
Jeffrey Stedfast <jestedfa@microsoft.com>
2018-10-18 13:49:13 -0400

    [msbuild] Added SiriIntents build test

---
+++ msbuild/tests/Xamarin.iOS.Tasks.Tests/ProjectsTests/SiriIntents.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+using NUnit.Framework;
+
+namespace Xamarin.iOS.Tasks
+{
+	[TestFixture ("iPhone")]
+	[TestFixture ("iPhoneSimulator")]
+	public class SiriIntents : ProjectTest
+	{
+		public SiriIntents (string platform) : base (platform)
+		{
+		}
+
+		[Test]
+		public void BuildTest ()
+		{
+			BuildProject ("MyIntentsApp", Platform, "Debug");
+
+			var path = Path.Combine (AppBundlePath, "Intents.intentdefinition");
+
+			Assert.IsTrue (File.Exists (path), "`Intents.intentdefinition' does not exist.");
+		}
+	}
+}
