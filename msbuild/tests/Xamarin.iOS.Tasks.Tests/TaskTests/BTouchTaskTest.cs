d7b1c89ee40a902597328868bb88e573f943a5ec
Sebastien Pouliot <sebastien@xamarin.com>
2018-10-25 11:52:17 -0400

    [msbuild] Avoid an NRE in BtouchTask when an invalid extra argument is provided
    The task itself should not throw. An invalid argument is an error that
    should (and is) reported by `btouch` itself (and the task picks it up).
    This makes the error reporting much more useful and the way an exception
    is reported, from Windows, is also confusing
    ```
    MessagingRemoteException: An error occured on client Build4110732 while executing a reply for topic xvs/Build/4.11.0.732/execute-task/ClassLibrary1/6e85b94002fBTouch ArgumentNullException: Value cannot be null.
    ```
    
    Unit tests added.
    
    Fixes https://devdiv.visualstudio.com/DevDiv/_workitems/edit/656983

--- msbuild/tests/Xamarin.iOS.Tasks.Tests/TaskTests/BTouchTaskTest.cs
+++ msbuild/tests/Xamarin.iOS.Tasks.Tests/TaskTests/BTouchTaskTest.cs
@@ -22,29 +22,34 @@ namespace Xamarin.iOS.Tasks
 	[TestFixture]
 	public class BTouchTaskTests : TestBase
 	{
-		CustomBTouchTask Task {
-			get; set;
-		}
-
-		public override void Setup ()
-		{
-			base.Setup ();
-
-			Task = CreateTask<CustomBTouchTask> ();
-
-			Task.ApiDefinitions = new [] { new TaskItem ("apidefinition.cs") };
-			Task.References = new [] { new TaskItem ("a.dll"), new TaskItem ("b.dll"), new TaskItem ("c.dll") };
-		}
-
 		[Test]
 		public void StandardCommandline ()
 		{
-			var args = Task.GenerateCommandLineCommands ();
+			var task = CreateTask<CustomBTouchTask> ();
+
+			task.ApiDefinitions = new[] { new TaskItem ("apidefinition.cs") };
+			task.References = new[] { new TaskItem ("a.dll"), new TaskItem ("b.dll"), new TaskItem ("c.dll") };
+
+			var args = task.GenerateCommandLineCommands ();
 			Assert.IsTrue (args.Contains ("-r " + Path.Combine (Environment.CurrentDirectory, "a.dll")), "#1a");
 			Assert.IsTrue (args.Contains ("-r " + Path.Combine (Environment.CurrentDirectory, "b.dll")), "#1b");
 			Assert.IsTrue (args.Contains ("-r " + Path.Combine (Environment.CurrentDirectory, "c.dll")), "#1c");
 		}
 
+		[Test]
+		public void Bug656983 ()
+		{
+			var task = CreateTask<CustomBTouchTask> ();
+
+			task.ApiDefinitions = new[] { new TaskItem ("apidefinition.cs") };
+			task.References = new[] { new TaskItem ("a.dll"), new TaskItem ("b.dll"), new TaskItem ("c.dll") };
+			task.ProjectDir = "~/"; // not important, but required (so can't be null)
+
+			task.OutputAssembly = null; // default, but important for the bug iset n case that default changes)
+			task.ExtraArgs = "-invalid";
+			var args = task.GenerateCommandLineCommands ();
+			Assert.That (args.Contains (" -invalid"), "incorrect ExtraArg not causing an exception");
+		}
 	}
 }
 
