2f749de1ba3203c44d0cabbef9beff0674c72ff8
Sebastien Pouliot <sebastien@xamarin.com>
2018-10-25 17:28:40 -0400

    [tests] Fix comment (typo) and remove some compiler warnings

--- msbuild/tests/Xamarin.iOS.Tasks.Tests/TaskTests/BTouchTaskTest.cs
+++ msbuild/tests/Xamarin.iOS.Tasks.Tests/TaskTests/BTouchTaskTest.cs
@@ -9,11 +9,7 @@ namespace Xamarin.iOS.Tasks
 {
 	class CustomBTouchTask : BTouch
 	{
-		public CustomBTouchTask ()
-		{
-		}
-
-		public new string GenerateCommandLineCommands ()
+		public string GetCommandLineCommands ()
 		{
 			return base.GenerateCommandLineCommands ();
 		}
@@ -30,7 +26,7 @@ namespace Xamarin.iOS.Tasks
 			task.ApiDefinitions = new[] { new TaskItem ("apidefinition.cs") };
 			task.References = new[] { new TaskItem ("a.dll"), new TaskItem ("b.dll"), new TaskItem ("c.dll") };
 
-			var args = task.GenerateCommandLineCommands ();
+			var args = task.GetCommandLineCommands ();
 			Assert.IsTrue (args.Contains ("-r " + Path.Combine (Environment.CurrentDirectory, "a.dll")), "#1a");
 			Assert.IsTrue (args.Contains ("-r " + Path.Combine (Environment.CurrentDirectory, "b.dll")), "#1b");
 			Assert.IsTrue (args.Contains ("-r " + Path.Combine (Environment.CurrentDirectory, "c.dll")), "#1c");
@@ -45,9 +41,9 @@ namespace Xamarin.iOS.Tasks
 			task.References = new[] { new TaskItem ("a.dll"), new TaskItem ("b.dll"), new TaskItem ("c.dll") };
 			task.ProjectDir = "~/"; // not important, but required (so can't be null)
 
-			task.OutputAssembly = null; // default, but important for the bug iset n case that default changes)
+			task.OutputAssembly = null; // default, but important for the bug (in case that default changes)
 			task.ExtraArgs = "-invalid";
-			var args = task.GenerateCommandLineCommands ();
+			var args = task.GetCommandLineCommands ();
 			Assert.That (args.Contains (" -invalid"), "incorrect ExtraArg not causing an exception");
 		}
 	}
