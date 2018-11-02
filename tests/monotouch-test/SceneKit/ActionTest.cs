58608ecb40d6a59bd053ef1620c66d4ca34cf336
Sebastien Pouliot <sebastien.pouliot@microsoft.com>
2018-11-02 10:05:47 -0400

    Fix per review & adjust tests to match

--- tests/monotouch-test/SceneKit/ActionTest.cs
+++ tests/monotouch-test/SceneKit/ActionTest.cs
@@ -3,7 +3,6 @@
 //
 
 using System;
-using CoreAnimation;
 using Foundation;
 using SceneKit;
 using ObjCRuntime;
@@ -17,29 +16,41 @@ namespace MonoTouchFixtures.SceneKit {
 
 		float timeFunctionValue;
 
+#if !XAMCORE_4_0
 		[Test]
 		public void TimingFunction_5058 ()
 		{
 			var a = new SCNAction ();
 			Assert.Null (a.TimingFunction2, "TimingFunction2");
-#if !XAMCORE_4_0
 			Assert.Null (a.TimingFunction, "TimingFunction");
-#endif
 			a.TimingFunction2 = (float f) => {
 				timeFunctionValue = f;
 				return timeFunctionValue;
 			};
 			Assert.That (a.TimingFunction2 (Single.NaN), Is.NaN, "value returned");
-#if !XAMCORE_4_0
 			a.TimingFunction (Single.NaN);
 			Assert.That (timeFunctionValue, Is.NaN, "TimingFunction assigned from TimingFunction2");
+		}
 #endif
-			a.TimingFunction2 = null;
+
+		[Test]
+		public void TimingFunction_5072 ()
+		{
 			// https://github.com/xamarin/xamarin-macios/issues/5072
-			// Assert.Null (a.TimingFunction2, "TimingFunction2-end");
+			var a = new SCNAction ();
 #if !XAMCORE_4_0
-			// Assert.Null (a.TimingFunction, "TimingFunction-end");
+			a.TimingFunction2 = (float f) => {
+				timeFunctionValue = f;
+				return timeFunctionValue;
+			};
+			// Assert.Null (a.TimingFunction2, "TimingFunction2-end");
+#else
+			a.TimingFunction = (float f) => {
+				timeFunctionValue = f;
+				return timeFunctionValue;
+			};
 #endif
+			// Assert.Null (a.TimingFunction, "TimingFunction-end");
 		}
 	}
 }
