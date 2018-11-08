1c56ee6ff2d24a9c3d00114004fe6905beb9fd5c
Chris Hamons <chris.hamons@xamarin.com>
2018-11-08 15:26:20 -0600

    Fix nuget restore behavior

--- tests/msbuild-mac/src/RoslynSmokeTests.cs
+++ tests/msbuild-mac/src/RoslynSmokeTests.cs
@@ -8,15 +8,11 @@ namespace Xamarin.MMP.Tests
 	[TestFixture]
 	public partial class MMPTests
 	{
-		const string nugetPath = "/Library/Frameworks/Mono.framework/Versions/Current/Commands/nuget";
-
 		public string RoslynTestProjectRoot => Path.Combine (TI.FindSourceDirectory (), "TestProjects/RoslynTestApp/");
 
-		void RestoreRoslynNuget (string projectType)
+		void RestoreRoslynNuget (string projectPath)
 		{
-			StringBuilder nugetArgs = new StringBuilder ("restore " + Path.Combine (RoslynTestProjectRoot + projectType + "/") + "packages.config");
-
-			TI.RunAndAssert (nugetPath, nugetArgs, "Restore Nuget");
+			TI.RunAndAssert ("/Library/Frameworks/Mono.framework/Versions/Current/Commands/msbuild", $"/t:restore {projectPath}", "Restore Nuget");
 		}
 
 		[Test]
@@ -25,7 +21,7 @@ namespace Xamarin.MMP.Tests
 			string projectPath = Path.Combine (RoslynTestProjectRoot, "Modern/RoslynTestApp.sln");
 
 			TI.CleanUnifiedProject (projectPath);
-			RestoreRoslynNuget ("Modern");
+			RestoreRoslynNuget (projectPath);
 			TI.BuildProject (projectPath, true);
 			TI.RunAndAssert (Path.Combine (RoslynTestProjectRoot, "Modern/bin/Debug/RoslynTestApp.app/Contents/MacOS/RoslynTestApp"), new StringBuilder (), "Run");
 		}
@@ -36,7 +32,7 @@ namespace Xamarin.MMP.Tests
 			string projectPath = Path.Combine (RoslynTestProjectRoot, "Full/RoslynTestApp.sln");
 
 			TI.CleanUnifiedProject (projectPath);
-			RestoreRoslynNuget ("Full");
+			RestoreRoslynNuget (projectPath);
 			TI.BuildProject (projectPath, true);
 			TI.RunAndAssert (Path.Combine (RoslynTestProjectRoot, "Full/bin/Debug/RoslynTestApp.app/Contents/MacOS/RoslynTestApp"), new StringBuilder (), "Run");
 		}
