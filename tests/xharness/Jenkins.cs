802d517d7e7cc7171ccae923baf1fb705b6288b8
Ankit Jain <ankj@microsoft.com>
2018-11-15 05:46:31 -0500

    Integrate use of `xibuild` with the tests
    Update all uses of `msbuild` and invocations of tools like nunit that
    might depend on using the in-tree builds to use `xibuild`.

--- tests/xharness/Jenkins.cs
+++ tests/xharness/Jenkins.cs
@@ -2760,8 +2760,9 @@ function toggleAll (show)
 				await RestoreNugetsAsync (log, resource);
 
 				using (var xbuild = new Process ()) {
-					xbuild.StartInfo.FileName = UseMSBuild ? "msbuild" : "msbuild";
+					xbuild.StartInfo.FileName = "xibuild";
 					var args = new StringBuilder ();
+					args.Append ("-- ");
 					args.Append ("/verbosity:diagnostic ");
 					if (SpecifyPlatform)
 						args.Append ($"/p:Platform={ProjectPlatform} ");
@@ -2796,8 +2797,9 @@ function toggleAll (show)
 		{
 			// Don't require the desktop resource here, this shouldn't be that resource sensitive
 			using (var xbuild = new Process ()) {
-				xbuild.StartInfo.FileName = "msbuild";
+				xbuild.StartInfo.FileName = "xibuild";
 				var args = new StringBuilder ();
+				args.Append ("-- ");
 				args.Append ("/verbosity:diagnostic ");
 				if (project_platform != null)
 					args.Append ($"/p:Platform={project_platform} ");
@@ -2872,8 +2874,9 @@ function toggleAll (show)
 				using (var proc = new Process ()) {
 
 					proc.StartInfo.WorkingDirectory = WorkingDirectory;
-					proc.StartInfo.FileName = "/Library/Frameworks/Mono.framework/Commands/mono";
+					proc.StartInfo.FileName = "xibuild";
 					var args = new StringBuilder ();
+					args.Append ("-t -- ");
 					args.Append (StringUtils.Quote (Path.GetFullPath (TestExecutable))).Append (' ');
 					args.Append (StringUtils.Quote (Path.GetFullPath (TestLibrary))).Append (' ');
 					if (IsNUnit3) {
