7c8c31f38b042919a73e6669c87e863d5678dd38
Chris Hamons <chris.hamons@xamarin.com>
2018-11-21 11:45:27 -0600

    [msbuild] Add FindBindingResourcePackages task
    - This is the reflection of CreateBindingResourcePackage and invoked by applications before mmp\mtouch.
    - For each referenced assembly, search of a .resources package next to the resolved path
    - If found, then locate the manifest and sanity check the MVID
    - The XML representation is rehydrated and new NativeReference task items are created to be passed to mmp\mtouch
    
    As a reminder, we only need this for "out of solution" references, so we pass in the references we already resolved via
    recursive msbuild (GetBindingProjectResources) and skip those here.
    
    We also return the manifest as an item group, as the bundle most correctly depend upon itself and the native libraries
    for correct incremental build support

---
+++ msbuild/Xamarin.Mac.Tasks/Tasks/FindBindingResourcePackages.cs
@@ -0,0 +1,16 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.MacDev.Tasks;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Xamarin.Mac.Tasks
+{
+	public class FindBindingResourcePackages : FindBindingResourcePackagesBase
+	{
+	}
+}
