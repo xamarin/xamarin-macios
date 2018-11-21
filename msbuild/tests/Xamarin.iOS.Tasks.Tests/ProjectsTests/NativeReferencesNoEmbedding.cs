115772a1d514e6333955df393cac35c743ea8435
Chris Hamons <chris.hamons@xamarin.com>
2018-11-21 12:19:40 -0600

    [tests] Add tests for NoBindingEmbedding binding projects
    - XI is currently a subset of XM tests as it lacks flexibility in test project creation.
    - There are a few outstanding cases to be fixed:
    
    - When multiple native references are in a binding projet, you will harmlessly have
    multiple args to mmp\mtouch for the same assembly
    - We do not handle one case (App -> Library -> Binding Project) out of sln correctly, the test is commented out

---
+++ msbuild/tests/Xamarin.iOS.Tasks.Tests/ProjectsTests/NativeReferencesNoEmbedding.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.Tests;
+using Xamarin.Utils;
+
+namespace Xamarin.iOS.Tasks
+{
+	[TestFixture ("iPhone")]
+	[TestFixture ("iPhoneSimulator")]
+	public class NativeReferencesNoEmbedding : ProjectTest {
+
+		public NativeReferencesNoEmbedding (string platform) : base (platform)
+		{
+		}
+
+		void SetNoBindingEmbedding ()
+		{
+			// TODO - This is a hack. We should be setting it only on the binding project, but
+			// the iOS msbuild test system doesn't have an easy way to access since it's just a project reference
+			// and not loaded up here...
+			Engine.ProjectCollection.SetGlobalProperty ("NoBindingEmbedding", "true");
+		}
+
+		void BuildProjectNoEmbedding (ProjectPaths project, bool clean = true)
+		{
+			Engine.ProjectCollection.SetGlobalProperty ("Platform", Platform);
+			var proj = SetupProject (Engine, project.ProjectCSProjPath);
+
+			SetNoBindingEmbedding ();
+
+			if (clean)
+				RunTarget (proj, "Clean", 0);
+			RunTarget (proj, "Build", 0);
+		}
+
+		static void AssertMtouchArgs (string appBuildLog, int expectedNaitveRefLines, int expectedFrameworkLines)
+		{
+			string rspFileLine = appBuildLog.SplitLines ().First (x => x.Contains ("mtouch") && x.Contains ("response-file.rsp"));
+			string rpsFile = rspFileLine.Split (new char[] { ' ' })[1].Replace ("@", "");
+
+			List<string> responseLines = File.ReadAllLines (rpsFile).ToList ();
+
+			var nativeReferenceLines = responseLines.Where (x => x.Contains ("--native-reference")).ToList ();
+			Assert.AreEqual (expectedNaitveRefLines, nativeReferenceLines.Count, $"native-reference Count: {responseLines}");
+
+			var frameworkLines = responseLines.Where (x => x.Contains ("--framework")).ToList ();
+			Assert.AreEqual (expectedFrameworkLines, frameworkLines.Count, $"framework Count: {responseLines}");
+
+			var noEmbedLines = responseLines.Where (x => x.Contains ("binding-project-no-embedding")).ToList ();
+			// TODO - This should be 1 not 3 but we're harmlessly duplicating binding-project-no-embedding params
+			Assert.AreEqual (3, noEmbedLines.Count, $"binding-project-no-embedding Count: {responseLines}");
+		}
+
+		string GetMessages () => string.Join ("\n", Engine.Logger.MessageEvents.Select (x => x.Message));
+
+		void ClearMessages () => Engine.Logger.MessageEvents.Clear ();
+
+		// [TestCase (true)] MISSING_TEST - Tests are framework only
+		// [TestCase (false)] MISSING_TEST - Also, project reference only
+		public void LibrariesEmbeddedProperly (bool useProjectReference)
+		{
+			Assert.Fail ();
+		}
+
+		// [TestCase (false)] MISSING_TEST - Project reference only 
+		[TestCase (true)]
+		public void FrameworksEmbeddedProperly (bool useProjectReference)
+		{
+			// TODO - Checked in projects are project reference only...
+			Assert.True (useProjectReference);
+
+			var bindingApp = SetupProjectPaths ("MyiOSAppWithBinding", "../", true, Platform);
+
+			BuildProjectNoEmbedding (bindingApp);
+
+			AssertMtouchArgs (GetMessages (), 0, 3);
+
+			string libPath = Path.Combine (Directory.GetCurrentDirectory (), bindingApp.ProjectBinPath, "MyiOSBinding.dll");
+			Assert.True (File.Exists (libPath), $"Did not find expected library: {libPath}");
+
+			int returnValue = ExecutionHelper.Execute ("/Library/Frameworks/Mono.framework/Commands/monodis", "--presources " + libPath, out string monoDisResults);
+			Assert.AreEqual (0, returnValue);
+			Assert.IsFalse (monoDisResults.Contains ("XTest.framework"), $"Binding Library contianed embedded resource: {monoDisResults}");
+
+			string finalFrameworkPath = Path.Combine (bindingApp.AppBundlePath, "Frameworks/XTest.framework/XTest");
+			Assert.True (File.Exists (finalFrameworkPath), $"{finalFrameworkPath} file was not part of bundle?");
+		}
+
+		// [Test] MISSING_TEST - No LinkWith only projects
+		public void DoesNotSupportLinkWith ()
+		{
+			Assert.Fail ();
+		}
+
+		[TestCase (true)]
+		// [TestCase (false)] MISSING_TEST - Framework only tests
+		public void ShouldNotUnnecessarilyRebuildBindingProject (bool framework)
+		{
+			Assert.True (framework);
+
+			var bindingLib = SetupProjectPaths ("MyiOSFrameworkBinding", "../", true, "");
+
+			const string CreatePackageString = "Creating binding resource package";
+
+			// First build should create a package
+			BuildProjectNoEmbedding (bindingLib);
+			Assert.True (GetMessages ().Contains (CreatePackageString), "First build did not create package?");
+			ClearMessages ();
+
+			// No change build should not
+			BuildProjectNoEmbedding (bindingLib, clean : false);
+			Assert.False (GetMessages ().Contains (CreatePackageString), "Rebuild build did create package?");
+			ClearMessages ();
+
+			// Touching the binding project should
+			Touch (bindingLib.ProjectCSProjPath);
+			BuildProjectNoEmbedding (bindingLib, clean: false);
+			Assert.True (GetMessages ().Contains (CreatePackageString), "Bindng project build did not create package?");
+			ClearMessages ();
+
+			// Touching the binding file should
+			Touch (Path.Combine (Path.GetDirectoryName (bindingLib.ProjectCSProjPath), @"../../../tests/bindings-framework-test/ApiDefinition.cs"));
+			BuildProjectNoEmbedding (bindingLib, clean: false);
+			Assert.True (GetMessages ().Contains (CreatePackageString), "Bindng file build did not create package?");
+			ClearMessages ();
+
+			// No change build should not
+			BuildProjectNoEmbedding (bindingLib, clean: false);
+			Assert.False (GetMessages ().Contains (CreatePackageString), "Second rebuild build did create package?");
+			ClearMessages ();
+
+			// Touching native library should
+			Touch (Path.Combine (Path.GetDirectoryName (bindingLib.ProjectCSProjPath), @"../../../tests/test-libraries/.libs/ios/XTest.framework/XTest"));
+			BuildProjectNoEmbedding (bindingLib, clean: false);
+			Assert.True (GetMessages ().Contains (CreatePackageString), "Bindng build did not create package?");
+		}
+
+		[TestCase (true)]
+		// [TestCase (false)] MISSING_TEST - Project reference only 
+		public void ShouldNotUnnecessarilyRebuildFinalProject (bool useProjectReference)
+		{
+			Assert.True (useProjectReference);
+
+			var appProject = SetupProjectPaths ("MyiOSAppWithBinding", "../", true, "");
+
+			const string BuildString = "/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/bin/mtouch";
+
+			// First build should create run mtouch
+			BuildProjectNoEmbedding (appProject);
+			Assert.True (GetMessages ().Contains (BuildString), "First build did not run mtouch?");
+			ClearMessages ();
+
+			// But not a rebuild
+			BuildProjectNoEmbedding (appProject, clean: false);
+			Assert.False (GetMessages ().Contains (BuildString), "Rebuild build did run mtouch?");
+			ClearMessages ();
+
+			if (!useProjectReference) {
+				Assert.Fail (); // TODO - Checked in projects are project reference only...
+			} else {
+				Touch (Path.Combine (Path.GetDirectoryName (appProject.ProjectCSProjPath), @"../../../tests/test-libraries/.libs/ios/XTest.framework/XTest"));
+				BuildProjectNoEmbedding (appProject, clean: false);
+				Assert.True (GetMessages ().Contains (BuildString), "Binding binary build did not run mtouch?");
+			}
+		}
+
+		// [Test] MISSING_TEST - Project framework only, this test only makes sense with the non-project reference cases
+		public void MVIDMismatchDetected ()
+		{
+			Assert.Fail ();
+		}
+
+		// [TestCase (true)] - MISSING_TEST - Requires special "chain" project that is not checked in
+		// [TestCase (false)] - TODO App -> Lib -> Binding is a known failing case for now
+		public void MultipleDependencyChain (bool useProjectReference)
+		{
+			Assert.Fail ();
+		}
+	}
+}
---
+++ tests/mmptest/src/BindingProjectNoEmbeddingTests.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Xamarin.Utils;
+
+namespace Xamarin.MMP.Tests
+{
+	public class BindingProjectNoEmbeddingTests
+	{
+		static void Touch (string projectPath) => File.SetLastWriteTimeUtc (projectPath, DateTime.UtcNow);
+
+		static void AssertNoResourceWithName (string tmpDir, string projectName, string resourceName)
+		{
+			string bindingName = BindingProjectTests.RemoveCSProj (projectName);
+			string bindingLibraryPath = Path.Combine (tmpDir, $"bin/Debug/{bindingName}.dll");
+			string resourceOutput = TI.RunAndAssert ("/Library/Frameworks/Mono.framework/Commands/monodis", "--presources " + bindingLibraryPath, "monodis");
+			Assert.False (resourceOutput.Contains (resourceName), "Binding project output contained embedded library");
+		}
+
+		static void AssertSingleReferenceAndEmbedCommand (string appBuildLog)
+		{
+			var nativeReferenceLines = appBuildLog.SplitLines ().Where (x => x.Contains ("native-reference")).ToList ();
+			Assert.AreEqual (1, nativeReferenceLines.Count, $"Native References Count: {TI.PrintAndTrimIfLong (appBuildLog)}");
+
+			var noEmbedLines = appBuildLog.SplitLines ().Where (x => x.Contains ("binding-project-no-embedding")).ToList ();
+			Assert.AreEqual (1, noEmbedLines.Count, $"Embed Lines Count: {TI.PrintAndTrimIfLong (appBuildLog)}");
+		}
+
+		static void AssertFileInBundle (string tmpDir, BindingProjectType type, string path)
+		{
+			Assert.True (File.Exists (Path.Combine (tmpDir, $"bin/Debug/{(type == BindingProjectType.Modern ? "UnifiedExample" : "XM45Example")}.app/Contents/{path}")), $"{path} not in bundle as expected.");
+		}
+
+		[TestCase (BindingProjectType.Modern, false)]
+		[TestCase (BindingProjectType.Full, true)]
+		public void LibrariesEmbeddedProperly (BindingProjectType type, bool useProjectReference)
+		{
+			MMPTests.RunMMPTest (tmpDir => {
+				var projects = BindingProjectTests.GenerateTestProject (type, tmpDir);
+				BindingProjectTests.SetNoEmbedding (projects.Item1);
+
+				string appBuildLog = BindingProjectTests.SetupAndBuildLinkedTestProjects (projects.Item1, projects.Item2, tmpDir, useProjectReference, setupDefaultNativeReference: true).Item2;
+
+				AssertNoResourceWithName (tmpDir, projects.Item1.ProjectName, "SimpleClassDylib.dylib");
+
+				AssertSingleReferenceAndEmbedCommand (appBuildLog);
+
+				AssertFileInBundle (tmpDir, type, "MonoBundle/SimpleClassDylib.dylib");
+			});
+		}
+
+		[TestCase (BindingProjectType.Modern, true)]
+		[TestCase (BindingProjectType.Full, false)]
+		public void FrameworksEmbeddedProperly (BindingProjectType type, bool useProjectReference)
+		{
+			MMPTests.RunMMPTest (tmpDir => {
+				string frameworkPath = FrameworkBuilder.CreateThinFramework (tmpDir);
+
+				var projects = BindingProjectTests.GenerateTestProject (type, tmpDir);
+				BindingProjectTests.SetNoEmbedding (projects.Item1);
+				projects.Item1.ItemGroup = NativeReferenceTests.CreateSingleNativeRef (frameworkPath, "Framework");
+
+				string appBuildLog = BindingProjectTests.SetupAndBuildLinkedTestProjects (projects.Item1, projects.Item2, tmpDir, useProjectReference, false).Item2;
+			
+				AssertNoResourceWithName (tmpDir, projects.Item1.ProjectName, "Foo");
+
+				AssertSingleReferenceAndEmbedCommand (appBuildLog);
+
+				AssertFileInBundle (tmpDir, type, "Frameworks/Foo.framework/Foo");
+			});
+		}
+
+		[TestCase (BindingProjectType.Modern)]
+		public void DoesNotSupportLinkWith (BindingProjectType type)
+		{
+			MMPTests.RunMMPTest (tmpDir => {
+				var projects = BindingProjectTests.GenerateTestProject (type, tmpDir);
+				BindingProjectTests.SetNoEmbedding (projects.Item1);
+
+				projects.Item1.LinkWithName = "SimpleClassDylib.dylib";
+
+				string libBuildLog = BindingProjectTests.SetupAndBuildBindingProject (projects.Item1, false);
+				Assert.True (libBuildLog.Contains ("Unable to create a Binding Resource Package with no Native References"), $"Did not fail as expected: {TI.PrintAndTrimIfLong (libBuildLog)}");
+			});
+		}
+
+		[TestCase (true)]
+		[TestCase (false)]
+		public void ShouldNotUnnecessarilyRebuildBindingProject (bool framework)
+		{
+			MMPTests.RunMMPTest (tmpDir => {
+				var projects = BindingProjectTests.GenerateTestProject (BindingProjectType.Modern, tmpDir);
+				BindingProjectTests.SetNoEmbedding (projects.Item1);
+
+				string projectPath = Path.Combine (tmpDir, "MobileBinding.csproj");
+				string bindingFilePath = Path.Combine (tmpDir, "ApiDefinition.cs");
+				const string CreatePackageString = "Creating binding resource package";
+
+				// First build should create a package
+				string frameworkPath = null;
+
+				if (framework) {
+					frameworkPath = FrameworkBuilder.CreateThinFramework (tmpDir);
+					projects.Item1.ItemGroup += NativeReferenceTests.CreateSingleNativeRef (Path.GetFullPath (frameworkPath), "Framework");
+				}
+				string libBuildLog = BindingProjectTests.SetupAndBuildBindingProject (projects.Item1, setupDefaultNativeReference: !framework);
+
+				Assert.True (libBuildLog.Contains (CreatePackageString), $"First build did not create package? {TI.PrintAndTrimIfLong (libBuildLog)}");
+
+				// No change build should not
+				libBuildLog = TI.BuildProject (projectPath, true);
+				Assert.False (libBuildLog.Contains (CreatePackageString), $"Rebuild build did create package? {TI.PrintAndTrimIfLong (libBuildLog)}");
+
+				// Touching the binding project should
+				Touch (projectPath);
+				libBuildLog = TI.BuildProject (projectPath, true);
+				Assert.True (libBuildLog.Contains (CreatePackageString), $"Binding Project build did not create package? {TI.PrintAndTrimIfLong (libBuildLog)}");
+
+				// Touching the binding file should
+				Touch (bindingFilePath);
+				libBuildLog = TI.BuildProject (projectPath, true);
+				Assert.True (libBuildLog.Contains (CreatePackageString), $"Binding File build did not create package? {TI.PrintAndTrimIfLong (libBuildLog)}");
+
+				// No change build should not
+				libBuildLog = TI.BuildProject (projectPath, true);
+				Assert.False (libBuildLog.Contains (CreatePackageString), $"Second Rebuild build did create package? {TI.PrintAndTrimIfLong (libBuildLog)}");
+
+				// Touching native library should
+				Touch (framework ? frameworkPath + "/Foo" : NativeReferenceTests.SimpleDylibPath);
+				libBuildLog = TI.BuildProject (projectPath, true);
+				Assert.True (libBuildLog.Contains (CreatePackageString), $"Native Library build did not create package? {TI.PrintAndTrimIfLong (libBuildLog)}");
+			});
+		}
+
+		[TestCase (true, true)]
+		[TestCase (true, false)]
+		[TestCase (false, true)]
+		[TestCase (false, false)]
+		public void ShouldNotUnnecessarilyRebuildFinalProject (bool useProjectReference, bool framework)
+		{
+			MMPTests.RunMMPTest (tmpDir => {
+				var projects = BindingProjectTests.GenerateTestProject (BindingProjectType.Modern, tmpDir);
+				BindingProjectTests.SetNoEmbedding (projects.Item1);
+
+				const string BuildString = "xcrun -sdk macosx clang";
+
+				string frameworkPath = null;
+				if (framework) {
+					frameworkPath = FrameworkBuilder.CreateThinFramework (tmpDir);
+					projects.Item1.ItemGroup += NativeReferenceTests.CreateSingleNativeRef (Path.GetFullPath (frameworkPath), "Framework");
+				}
+				string appBuildLog = BindingProjectTests.SetupAndBuildLinkedTestProjects (projects.Item1, projects.Item2, tmpDir, useProjectReference, setupDefaultNativeReference: !framework).Item2;
+
+				Assert.True (appBuildLog.Contains (BuildString), $"First build did not run mmp? {TI.PrintAndTrimIfLong (appBuildLog)}");
+
+				string projectPath = Path.Combine (tmpDir, "UnifiedExample.csproj");
+				string mainPath = Path.Combine (tmpDir, "Main.cs");
+
+				// No change build should not
+				string buildLog = TI.BuildProject (projectPath, true);
+				Assert.False (buildLog.Contains (BuildString), $"Rebuild ran mmp again? {TI.PrintAndTrimIfLong (buildLog)}");
+
+				if (!useProjectReference) {
+					string bindingManifestPath = Path.Combine (tmpDir, "bin/Debug/MobileBinding.resources/manifest");
+					string bindingLibPath = Path.Combine (tmpDir, $"bin/Debug/MobileBinding.resources/{(framework ? "Foo.framework/Foo" : "SimpleClassDylib.dylib")}");
+
+					// Touching the binding manifest should
+					Touch (bindingManifestPath);
+					buildLog = TI.BuildProject (projectPath, true);
+					Assert.True (buildLog.Contains (BuildString), $"Binding Manifest build did not run mmp again? {TI.PrintAndTrimIfLong (buildLog)}");
+
+					// Touching the binding should
+					Touch (bindingLibPath);
+					buildLog = TI.BuildProject (projectPath, true);
+					Assert.True (buildLog.Contains (BuildString), $"Binding build did not run mmp again? {TI.PrintAndTrimIfLong (buildLog)}");
+				} else {
+					// Touching the starting binding file should
+					Touch (framework ? frameworkPath + "/Foo" : NativeReferenceTests.SimpleDylibPath);
+					buildLog = TI.BuildProject (projectPath, true);
+					Assert.True (buildLog.Contains (BuildString), $"Starting Binding build did not run mmp again? {TI.PrintAndTrimIfLong (buildLog)}");
+				}
+			});
+		}
+
+		[Test]
+		public void MVIDMismatchDetected ()
+		{
+			MMPTests.RunMMPTest (tmpDir => {
+				var projects = BindingProjectTests.GenerateTestProject (BindingProjectType.Modern, tmpDir);
+				BindingProjectTests.SetNoEmbedding (projects.Item1);
+
+				// This test is only valid in non-project reference cases
+				BindingProjectTests.SetupAndBuildLinkedTestProjects (projects.Item1, projects.Item2, tmpDir, false, setupDefaultNativeReference: true);
+
+				string bindingProject = Path.Combine (tmpDir, "MobileBinding.csproj");
+				string appProject = Path.Combine (tmpDir, "UnifiedExample.csproj");
+
+				// Save the first build and rebuild to create a second copy of the binding
+				string buildBindingPath = Path.Combine (tmpDir, "bin/Debug/MobileBinding.dll");
+				string savedCopyBindingPath = Path.Combine (tmpDir, "MobileBinding_First.dll");
+
+				File.Copy (buildBindingPath, savedCopyBindingPath);
+				Touch (bindingProject);
+				TI.BuildProject (bindingProject, true);
+
+				// Move the old file back on top to create MVID mismatch situtation
+				File.Delete (buildBindingPath);
+				File.Move (savedCopyBindingPath, buildBindingPath);
+
+				// Now rebuild and expect an error - MSBuild is not providing a non-zero return code even those we are logging an error
+				string appFailLog = TI.BuildProject (appProject, true);
+				Assert.True (appFailLog.Contains ("Assembly MVID mismatch in binding resource package"), $"Log did not contain expected error: {TI.PrintAndTrimIfLong (appFailLog)}");
+			});
+		}
+
+		[TestCase (true)]
+		[TestCase (false)]
+		public void MultipleNativeReferences (bool useProjectReference)
+		{
+			MMPTests.RunMMPTest (tmpDir => {
+				// This is a bit of a hack, you can't just rename dylibs like this
+				string secondNativeLibPath = Path.Combine (tmpDir, "SimpleClassDylib2.dylib");
+				File.Copy (NativeReferenceTests.SimpleDylibPath, secondNativeLibPath);
+
+				var projects = BindingProjectTests.GenerateTestProject (BindingProjectType.Modern, tmpDir);
+				BindingProjectTests.SetNoEmbedding (projects.Item1);
+				projects.Item1.ItemGroup += NativeReferenceTests.CreateSingleNativeRef (secondNativeLibPath, "Dynamic");
+
+				BindingProjectTests.SetupAndBuildLinkedTestProjects (projects.Item1, projects.Item2, tmpDir, useProjectReference, setupDefaultNativeReference: true);
+
+				// manifest and 2 dylibs
+				Assert.AreEqual (3, Directory.GetFiles (Path.Combine (tmpDir, "bin/Debug/MobileBinding.resources")).Length);
+
+				// 2 dylibs + libMonoPosixHelper.dylib
+				Assert.AreEqual (3, Directory.GetFiles (Path.Combine (tmpDir, "bin/Debug/UnifiedExample.app/Contents/MonoBundle")).Where (x => x.EndsWith (".dylib")).Count ());
+			});
+		}
+
+		[TestCase (true)]
+		// [TestCase (false)] - TODO App -> Lib -> Binding is a known failing case for now
+		public void MultipleDependencyChain (bool useProjectReference)
+		{
+			// App can depend on Lib that depends on binding lib with native reference and everything gets packaged in correctly
+			MMPTests.RunMMPTest (tmpDir => {
+				TI.UnifiedTestConfig binding = new TI.UnifiedTestConfig (tmpDir) { ProjectName = "MobileBinding.csproj" };
+				binding.ItemGroup += NativeReferenceTests.CreateSingleNativeRef (NativeReferenceTests.SimpleDylibPath, "Dynamic");
+				binding.APIDefinitionConfig += @"[BaseType (typeof (NSObject))]
+  interface SimpleClass {
+    [Export (""doIt"")]
+    int DoIt ();
+  }";
+				BindingProjectTests.SetNoEmbedding (binding);
+
+				TI.GenerateBindingLibraryProject (binding);
+				TI.BuildProject (Path.Combine (tmpDir, "MobileBinding.csproj"), true);
+
+
+				TI.UnifiedTestConfig library = new TI.UnifiedTestConfig (tmpDir) { ProjectName = "UnifiedLibrary" };
+				library.TestCode = "public class MyClass { public static void Go () { var c = new ExampleBinding.SimpleClass (); c.DoIt (); } }";
+
+				if (useProjectReference) 
+					library.References = $@"<ProjectReference Include=""MobileBinding.csproj"" />";
+				else 
+					library.References = $@"<Reference Include=""MobileBinding""><HintPath>{Path.Combine (tmpDir, "bin/Debug", "MobileBinding.dll")}</HintPath></Reference>";
+
+				TI.GenerateUnifiedLibraryProject (library);
+				TI.BuildProject (Path.Combine (tmpDir, "UnifiedLibrary.csproj"), true);
+
+				TI.UnifiedTestConfig project = new TI.UnifiedTestConfig (tmpDir) { ProjectName = "UnifiedExample.csproj" };
+				project.TestCode = "MyClass.Go ();";
+
+				if (useProjectReference) 
+					project.References = $@"<ProjectReference Include=""UnifiedLibrary.csproj"" />";
+				else 
+					project.References = $@"<Reference Include=""UnifiedLibrary""><HintPath>{Path.Combine (tmpDir, "bin/Debug", "UnifiedLibrary.dll")}</HintPath></Reference>";
+
+				TI.GenerateUnifiedExecutableProject (project);
+				TI.BuildProject (Path.Combine (tmpDir, "UnifiedExample.csproj"), true);
+			});
+		}
+
+		[Test]
+		public void CleanShouldRemoveBundle ()
+		{
+			MMPTests.RunMMPTest (tmpDir => {
+				var projects = BindingProjectTests.GenerateTestProject (BindingProjectType.Modern, tmpDir);
+				BindingProjectTests.SetNoEmbedding (projects.Item1);
+				
+				string libBuildLog = BindingProjectTests.SetupAndBuildBindingProject (projects.Item1, false);
+
+				TI.CleanUnifiedProject (Path.Combine (tmpDir, projects.Item1.ProjectName));
+				Assert.False (Directory.Exists (Path.Combine (tmpDir, "bin/Debug/MobileBinding.resources")), "Resource bundle was not cleaned up");
+			});
+		}
+	}
+}
