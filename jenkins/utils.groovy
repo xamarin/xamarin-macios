7360d4f7a780945be6d4722b3e6621493a21f637
Rolf Bjarne Kvinge <rolf@xamarin.com>
2018-12-13 16:38:08 +0100

    [jenkins] Specify repo manually for GitHub statuses to fix maccore#898. (#5269)
    Fixes https://github.com/xamarin/maccore/issues/898.

--- jenkins/utils.groovy
+++ jenkins/utils.groovy
@@ -1,6 +1,7 @@
 def reportGitHubStatus(commitHash, context, backref, statusResult, statusResultMessage) {
     step([
         $class: 'GitHubCommitStatusSetter',
+        reposSource: [$class: "ManuallyEnteredRepositorySource", url: "https://github.com/xamarin/xamarin-macios"],
         commitShaSource: [$class: "ManuallyEnteredShaSource", sha: commitHash],
         contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: context],
         statusBackrefSource: [$class: 'ManuallyEnteredBackrefSource', backref: backref],
