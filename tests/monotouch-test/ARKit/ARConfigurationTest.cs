6f6bece72bdf52b92ae485a7575d781e1a882469
Sebastien Pouliot <sebastien.pouliot@microsoft.com>
2019-01-04 13:54:43 -0500

    [arkit] Fix crash in ARConfiguration.SupportedVideoFormats. Fixes #5347
    Even if 'supportedVideoFormats' is static the type is abstract.
    
    > Important
    > ARConfiguration is an abstract base class, so its implementation of
    > this property always returns an empty array. Read this property from
    > the configuration subclass you plan to use for your AR session, such
    > as ARWorldTrackingConfiguration or ARFaceTrackingConfiguration.
    https://developer.apple.com/documentation/arkit/arconfiguration/2942261-supportedvideoformats?language=objc
    
    and this behave differently in Objective-C (than .net) as every (static)
    method will be different (and not a single implementation like C#).
    
    The existing implementation (as a property) calls `ARConfiguration`
    implementation which simply throws a (native) exception
    
    > NSInvalidArgumentException Reason: Supported video formats should be called on individual configuration class
    
    The solution is to obsolete the property (can't be subclassed in .net
    since it's static) and add, only on the subclasses, a method that
    call the 'supportedVideoFormats' selector on the current (not base)
    type.
    
    Added unit test to detect the addition of newer subclasses - since they
    will also need to expose this method.
    
    reference: https://github.com/xamarin/xamarin-macios/issues/5347

---
+++ tests/monotouch-test/ARKit/ARConfigurationTest.cs
@@ -0,0 +1,47 @@
+﻿#if XAMCORE_2_0 && __IOS__
+
+using System;
+using System.Reflection;
+using ARKit;
+using Foundation;
+using NUnit.Framework;
+
+namespace MonoTouchFixtures.ARKit {
+
+	[TestFixture]
+	[Preserve (AllMembers = true)]
+	public class ARCondigurationTest {
+
+		[Test]
+		public void SupportedVideoFormats ()
+		{
+			var svf = ARConfiguration.SupportedVideoFormats;
+			Assert.That (svf, Is.Empty, "empty");
+		}
+
+		[Test]
+		public void GetSupportedVideoFormats ()
+		{
+			Assert.NotNull (ARWorldTrackingConfiguration.GetSupportedVideoFormats (), "ARWorldTrackingConfiguration");
+			Assert.NotNull (AROrientationTrackingConfiguration.GetSupportedVideoFormats (), "AROrientationTrackingConfiguration");
+			Assert.NotNull (ARFaceTrackingConfiguration.GetSupportedVideoFormats (), "ARFaceTrackingConfiguration");
+			Assert.NotNull (ARImageTrackingConfiguration.GetSupportedVideoFormats (), "ARImageTrackingConfiguration");
+			Assert.NotNull (ARObjectScanningConfiguration.GetSupportedVideoFormats (), "ARObjectScanningConfiguration");
+		}
+
+		[Test]
+		public void Subclasses ()
+		{
+			// all subclasses of ARConfiguration must (re)export 'GetSupportedVideoFormats'
+			var c = typeof (ARConfiguration);
+			foreach (var sc in c.Assembly.GetTypes ()) {
+				if (!sc.IsSubclassOf (c))
+					continue;
+				var m = sc.GetMethod ("GetSupportedVideoFormats", BindingFlags.Static | BindingFlags.Public);
+				Assert.NotNull (m, sc.FullName);
+			}
+		}
+	}
+}
+
+#endif
