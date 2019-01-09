1c3ff0d5d387bb85d12b45203f419de0400065f1
Vincent Dondain <vidondai@microsoft.com>
2019-01-09 13:22:49 -0500

    [msbuild/tests] Ensure our FrameworkList files have correct assemblies
    One mono bump (https://github.com/xamarin/xamarin-macios/commit/6f2ebedb74fe8da250c8f53b61ffae2eb86fc711#diff-e801bb766cbaad95b50b1487b865f971) changed our `Xamarin.iOS-FrameworkList.xml.in` (and the tvOS and watchOS ones) and added 2 files
    
    ```
    <File AssemblyName="System.Buffers" Version="4.0.99.0" />
    <File AssemblyName="System.Memory" Version="4.0.99.0" />
    ```
    
    Around the same time this change was made in the IDE: https://github.com/mono/monodevelop/blob/e355f65870da4847d77ed1f3f2d07d1f78889cec/main/src/core/MonoDevelop.Core/MonoDevelop.Core.Assemblies/TargetFramework.cs#L260
    That means that VSMac, when creating a new cache file listing the assemblies, won’t scan the assemblies in those directories `/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS`.
    This resulted in an incomplete cache file missing most assemblies leading to the IDE showing red checkmarks for known and installed assemblies.
    
    Ideally the FrameworkList files we ship should have a list of all the assemblies with PublicKeyToken and friends (the framework shouldn't be lazy and expect the IDEs to generate that file for us).
    
    This commit ships a test using the same logic as the IDE to populate the FrameworkList file. The test will report any missing assembly and print the xml element that should be added to the file.
    E.g: `<File AssemblyName="FSharp.Core" Version="3.98.4.0" PublicKeyToken="b03f5f7f11d50a3a" ProcessorArchitecture="MSIL" />`
    Then it's only a matter of copy/pasting that (:
    
    Note: even though this is the msbuild tests for iOS land I'm also scanning the Xamarin.Mac files here for simplicity.

---
+++ msbuild/tests/Xamarin.iOS.Tasks.Tests/FrameworkListTest.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Xml;
+using NUnit.Framework;
+using Xamarin.Tests;
+using System.Linq;
+
+namespace Xamarin.iOS.Tasks
+{
+	[TestFixture]
+	public class FrameworkListTests
+	{
+		[TestCase ("Xamarin.iOS-FrameworkList.xml.in", false)]
+		[TestCase ("Xamarin.TVOS-FrameworkList.xml.in", false)]
+		[TestCase ("Xamarin.WatchOS-FrameworkList.xml.in", false)]
+		[TestCase ("FrameworkList.xml.in", true)] // Xamarin.Mac
+		public void CheckFrameworkListFile (string frameworkListFile, bool isMac)
+		{
+			var errorCount = 0;
+			var frameworkListAssemblies = ScanFrameworkListXml (frameworkListFile, isMac);
+			var frameworkName = isMac ? "Xamarin.Mac" : frameworkListFile.Split ('-')[0];
+			var installedAssemblies = ScanAssemblyDirectory (frameworkName, isMac);
+			using (var sw = new StringWriter ()) {
+				using (var writer = XmlWriter.Create (sw, new XmlWriterSettings { Encoding = Encoding.UTF8, ConformanceLevel = ConformanceLevel.Fragment, Indent = true })) {
+					foreach (var assembly in installedAssemblies) {
+						if (!frameworkListAssemblies.Any (a => a.Name == assembly.Name)) {
+							writer.WriteStartElement ("File");
+							writer.WriteAttributeString ("AssemblyName", assembly.Name);
+							WriteNonEmptyAttribute ("Version", assembly.Version);
+							WriteNonEmptyAttribute ("PublicKeyToken", assembly.PublicKeyToken);
+							WriteNonEmptyAttribute ("Culture", assembly.Culture);
+							if (assembly.ProcessorArchitecture != ProcessorArchitecture.None)
+								writer.WriteAttributeString ("ProcessorArchitecture", assembly.ProcessorArchitecture.ToString ());
+							if (assembly.InGac)
+								writer.WriteAttributeString ("InGac", "true");
+							writer.WriteEndElement ();
+							errorCount++;
+						}
+					}
+
+					void WriteNonEmptyAttribute (string name, string val)
+					{
+						if (!string.IsNullOrEmpty (val))
+							writer.WriteAttributeString (name, val);
+					}
+				}
+				Assert.AreEqual (0, errorCount, $"Missing assemblies in '{frameworkListFile}'\n{sw.ToString ()}");
+			}
+		}
+
+		List<AssemblyInfo> ScanFrameworkListXml (string frameworkListFile, bool isMac)
+		{
+			var assemblies = new List<AssemblyInfo> ();
+			var path = string.Empty;
+			if (isMac)
+				path = Path.GetFullPath (Path.Combine ("..", "..", "Xamarin.Mac.Tasks", frameworkListFile));
+			else
+				path = Path.GetFullPath (Path.Combine ("..", "..", "Xamarin.iOS.Tasks.Core", frameworkListFile));
+			using (var reader = XmlReader.Create (path)) {
+				while (reader.Read ()) {
+					if (reader.IsStartElement ()) {
+						switch (reader.LocalName) {
+						case "File":
+							assemblies.Add (ReadFileElement (reader));
+							break;
+						}
+					}
+				}
+			}
+			return assemblies;
+		}
+
+		List<AssemblyInfo> ScanAssemblyDirectory (string frameworkName, bool isMac)
+		{
+			var assemblies = new List<AssemblyInfo> ();
+			var path = string.Empty;
+			if (isMac)
+				path = Path.GetFullPath (Path.Combine (Configuration.SdkRootXM, "lib", "mono", frameworkName));
+			else
+				path = Path.GetFullPath (Path.Combine (Configuration.MonoTouchRootDirectory, "lib", "mono", frameworkName));
+			foreach (var f in Directory.EnumerateFiles (path, "*.dll")) {
+				try {
+					var an = AssemblyName.GetAssemblyName (f);
+					var ainfo = new AssemblyInfo ();
+					ainfo.Update (an);
+					assemblies.Add (ainfo);
+				} catch (Exception ex) {
+					Console.WriteLine ("Error reading assembly '{0}' in framework '{1}':{2}{3}", f, frameworkName, Environment.NewLine, ex);
+				}
+			}
+			return assemblies;
+		}
+
+		static AssemblyInfo ReadFileElement (XmlReader reader)
+		{
+			var ainfo = new AssemblyInfo ();
+			if (reader.MoveToAttribute ("AssemblyName") && reader.ReadAttributeValue ())
+				ainfo.Name = reader.ReadContentAsString ();
+			if (string.IsNullOrEmpty (ainfo.Name))
+				throw new Exception ("Missing AssemblyName attribute");
+			if (reader.MoveToAttribute ("Version") && reader.ReadAttributeValue ())
+				ainfo.Version = reader.ReadContentAsString ();
+			if (reader.MoveToAttribute ("PublicKeyToken") && reader.ReadAttributeValue ())
+				ainfo.PublicKeyToken = reader.ReadContentAsString ();
+			if (reader.MoveToAttribute ("Culture") && reader.ReadAttributeValue ())
+				ainfo.Culture = reader.ReadContentAsString ();
+			if (reader.MoveToAttribute ("ProcessorArchitecture") && reader.ReadAttributeValue ())
+			ainfo.ProcessorArchitecture = (ProcessorArchitecture)
+			Enum.Parse (typeof (ProcessorArchitecture), reader.ReadContentAsString (), true);
+			if (reader.MoveToAttribute ("InGac") && reader.ReadAttributeValue ())
+				ainfo.InGac = reader.ReadContentAsBoolean ();
+			return ainfo;
+		}
+	}
+
+	class AssemblyInfo
+	{
+		public string Name;
+
+		public string Version;
+
+		public string PublicKeyToken;
+
+		public string Culture;
+
+		public ProcessorArchitecture ProcessorArchitecture = ProcessorArchitecture.MSIL;
+
+		public bool InGac;
+
+		public void Update (AssemblyName aname)
+		{
+			Name = aname.Name;
+			Version = aname.Version.ToString ();
+			ProcessorArchitecture = aname.ProcessorArchitecture;
+			Culture = aname.CultureInfo.Name;
+			string fn = aname.ToString ();
+			string key = "publickeytoken=";
+			int i = fn.IndexOf (key, StringComparison.OrdinalIgnoreCase) + key.Length;
+			int j = fn.IndexOf (',', i);
+			if (j == -1) j = fn.Length;
+			PublicKeyToken = fn.Substring (i, j - i);
+		}
+	}
+}
