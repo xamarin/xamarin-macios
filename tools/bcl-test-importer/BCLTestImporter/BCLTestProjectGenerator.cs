879629bc9dbfdc7358af356b1209463dc43edea6
Manuel de la Pena <mandel@microsoft.com>
2019-01-17 22:58:31 +0100

    [XHarness] Reenable the System tests on iOS, WatchOS and TVOs. (#5425)
    All the failing tests have been ignored. There are a large number of
    tests ignored on the watchOS platform beacuase atm the iOS test dll is
    used.
    
    Fixes https://github.com/xamarin/maccore/issues/1135

--- tools/bcl-test-importer/BCLTestImporter/BCLTestProjectGenerator.cs
+++ tools/bcl-test-importer/BCLTestImporter/BCLTestProjectGenerator.cs
@@ -109,7 +109,6 @@ namespace BCLTestImporter {
 		};
 			
 		static readonly List <string> CommonIgnoredAssemblies = new List <string> {
-			"monotouch_System_test.dll", // issues https://github.com/xamarin/maccore/issues/1135
 			"monotouch_System.Security_test.dll", // issue https://github.com/xamarin/maccore/issues/1139
 			"monotouch_Commons.Xml.Relaxng_test.dll", // not supported by xamarin
 			"monotouch_Cscompmgd_test.dll", // not supported by xamarin
