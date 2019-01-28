7669cc1ac862f41bc2aee8e1144d379766d5e58e
Vincent Dondain <vidondai@microsoft.com>
2019-01-28 10:26:20 -0500

    [healthkit] Update for Xcode 10.2 beta 1

--- src/healthkit.cs
+++ src/healthkit.cs
@@ -687,6 +687,10 @@ namespace HealthKit {
 		[Watch (5, 0), iOS (12, 0)]
 		[Export ("CrossTrainerDistance")]
 		HKQuantity CrossTrainerDistance { get; set; }
+
+		[Watch (5, 2), iOS (12, 2)]
+		[Export ("HeartRateEventThreshold")]
+		HKQuantity HeartRateEventThreshold { get; set; }
 	}
 		
 	[Watch (2,0)]
@@ -837,6 +841,10 @@ namespace HealthKit {
 		[Watch (5, 0), iOS (12, 0)]
 		[Field ("HKMetadataKeyCrossTrainerDistance")]
 		NSString CrossTrainerDistance { get; }
+
+		[Watch (5, 2), iOS (12, 2)]
+		[Field ("HKMetadataKeyHeartRateEventThreshold")]
+		NSString HeartRateEventThreshold { get; }
 	}
 
 	[Watch (2,0)]
@@ -1740,6 +1748,18 @@ namespace HealthKit {
 		[iOS (10,0), Watch (3,0)]
 		[Field ("HKCategoryTypeIdentifierMindfulSession")]
 		MindfulSession,
+
+		[Watch (5,2), iOS (12,2)]
+		[Field ("HKCategoryTypeIdentifierHighHeartRateEvent")]
+		HighHeartRateEvent,
+
+		[Watch (5,2), iOS (12,2)]
+		[Field ("HKCategoryTypeIdentifierLowHeartRateEvent")]
+		LowHeartRateEvent,
+
+		[Watch (5,2), iOS (12,2)]
+		[Field ("HKCategoryTypeIdentifierIrregularHeartRhythmEvent")]
+		IrregularHeartRhythmEvent,
 	}
 
 	[Watch (2,0)]
