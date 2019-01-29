42705c6a00aef5cb7ca11ed95b77fa392df83b48
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-01-29 08:53:26 +0100

    [xharness] Flush the log after getting results from listing simulators/devices with mlaunch.
    Now constantly refreshing the device/simulator listing log will show the
    output as mlaunch writes it, which makes the impatient me happier.

--- tests/xharness/Simulators.cs
+++ tests/xharness/Simulators.cs
@@ -705,6 +705,7 @@ namespace xharness
 							throw new Exception ("Failed to list devices.");
 						log.WriteLine ("Result:");
 						log.WriteLine (File.ReadAllText (tmpfile));
+						log.Flush ();
 
 						var doc = new XmlDocument ();
 						doc.LoadWithoutNetworkAccess (tmpfile);
@@ -738,6 +739,7 @@ namespace xharness
 				} finally {
 					connected_devices.SetCompleted ();
 					File.Delete (tmpfile);
+					log.Flush ();
 				}
 			});
 		}
