281861ac8727a2f583561f86db387136ea5ba61e
Chris Hamons <chris.hamons@xamarin.com>
2019-02-01 14:46:35 -0600

    [xm] Support UseHardenendRuntime in code signing

--- msbuild/Xamarin.MacDev.Tasks.Core/Tasks/CodesignTaskBase.cs
+++ msbuild/Xamarin.MacDev.Tasks.Core/Tasks/CodesignTaskBase.cs
@@ -45,6 +45,8 @@ namespace Xamarin.MacDev.Tasks
 
 		public bool IsAppExtension { get; set; }
 
+		public bool UseHardenendRuntime { get; set; }
+
 		public string ToolExe {
 			get { return toolExe ?? ToolName; }
 			set { toolExe = value; }
@@ -92,6 +94,9 @@ namespace Xamarin.MacDev.Tasks
 
 			if (IsAppExtension)
 				args.Add ("--deep");
+
+			if (UseHardenendRuntime)
+				args.Add ("-o runtime");
 
 			args.Add ("--sign");
 			args.AddQuoted (SigningKey);
