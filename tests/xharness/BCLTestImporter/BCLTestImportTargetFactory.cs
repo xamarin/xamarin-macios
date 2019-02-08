e7f7b1fbe0312b08827743ec08bb8db4a15338d1
Manuel de la Pena <mandel@microsoft.com>
2019-02-08 14:45:20 +0100

    [XHarness] Ensure we do a nuget restore on bcl imported tests. Fixes #5383
    We need to ensure that a nuget restore is done, otherwise we might have
    build issues with missing libs.
    
    Fixes https://github.com/xamarin/xamarin-macios/issues/5383

--- tests/xharness/BCLTestImporter/BCLTestImportTargetFactory.cs
+++ tests/xharness/BCLTestImporter/BCLTestImportTargetFactory.cs
@@ -2,6 +2,10 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 using BCLTestImporter;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Text;
+using Xamarin.Utils;
 
 namespace xharness.BCLTestImporter {
 	// Class that is use as the connection between xharness and the BCLImporter
@@ -27,6 +31,29 @@ namespace xharness.BCLTestImporter {
 			};
 		}
 		
+		async Task<TestExecutingResult> RestoreNugetsAsync (string projectPath)
+		{
+
+			using (var nuget = new Process ()) {
+				nuget.StartInfo.FileName = "/Library/Frameworks/Mono.framework/Versions/Current/Commands/nuget";
+				var args = new StringBuilder ();
+				args.Append ("restore ");
+				args.Append (StringUtils.Quote (projectPath));
+				nuget.StartInfo.Arguments = args.ToString ();
+
+				var timeout = TimeSpan.FromMinutes (15);
+				var result = await nuget.RunAsync (Harness.HarnessLog, true, timeout);
+				if (result.TimedOut) {
+					Harness.HarnessLog.WriteLine ("Nuget restore timed out after {0} seconds.", timeout.TotalSeconds);
+					return TestExecutingResult.TimedOut;
+				} 
+				if (!result.Succeeded) {
+					return TestExecutingResult.Failed;;
+				}
+				return TestExecutingResult.Succeeded;
+			}
+		}
+		
 		// generate all the different test targets.
 		public List<iOSTestProject> GetiOSBclTargets ()
 		{
@@ -39,6 +66,11 @@ namespace xharness.BCLTestImporter {
 					SkiptvOSVariation = !platforms.Contains (Platform.TvOS),
 					SkipwatchOSVariation = !platforms.Contains (Platform.WatchOS),
 					FailureMessage = failure,
+					Dependency = async () => {
+						var nugetRestoreResult = await RestoreNugetsAsync (path);
+						if (nugetRestoreResult != TestExecutingResult.Succeeded)
+							throw new Exception ($"Nuget restore failed. {nugetRestoreResult}");
+					}
 				});
 			}
 			return result;
@@ -63,6 +95,10 @@ namespace xharness.BCLTestImporter {
 						var rv = await Harness.BuildBclTests ();
 						if (!rv.Succeeded)
 							throw new Exception ($"Failed to build BCL tests, exit code: {rv.ExitCode}. Check the harness log for more details.");
+
+						var nugetRestoreResult = await RestoreNugetsAsync (path);
+						if (nugetRestoreResult != TestExecutingResult.Succeeded)
+							throw new Exception ($"Nuget restore failed. {nugetRestoreResult}");
 					}
 				});
 			}
@@ -79,3 +115,4 @@ namespace xharness.BCLTestImporter {
 		}
 	}
 }
+
