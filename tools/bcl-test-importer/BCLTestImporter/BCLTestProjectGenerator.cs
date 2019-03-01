1b1792cb4354ee5c330e99b73b9917729bbd0efb
Manuel de la Pena <mandel@microsoft.com>
2019-03-01 17:02:42 +0100

    [XHarness] Reenable the System.IO.Compression and (#5694)
    System.IO.Compression.FileSystem tests on mac os x.
    
    Reenable the tests that can be ran without failures.
    
    Fixes https://github.com/xamarin/maccore/issues/1195
    Fixes https://github.com/xamarin/maccore/issues/1146

--- tools/bcl-test-importer/BCLTestImporter/BCLTestProjectGenerator.cs
+++ tools/bcl-test-importer/BCLTestImporter/BCLTestProjectGenerator.cs
@@ -196,8 +196,6 @@ namespace BCLTestImporter {
 			"xammac_net_4_5_System.Core_test.dll", // issue https://github.com/xamarin/maccore/issues/1191
 			"xammac_net_4_5_System.Data_test.dll", // issues https://github.com/xamarin/maccore/issues/1192 and https://github.com/xamarin/maccore/issues/1193
 			"xammac_net_4_5_System.IdentityModel_test.dll", // issues https://github.com/xamarin/maccore/issues/1194
-			"xammac_net_4_5_System.IO.Compression.FileSystem_test.dll", // issues https://github.com/xamarin/maccore/issues/1195
-			"xammac_net_4_5_System.IO.Compression_test.dll", // issue https://github.com/xamarin/maccore/issues/1146
 			"xammac_net_4_5_System.IdentityModel_test.dll", // issues https://github.com/xamarin/maccore/issues/1196
 			"xammac_net_4_5_System.Security_test.dll", // issue https://github.com/xamarin/maccore/issues/1197
 			"xammac_net_4_5_System.ServiceModel_test.dll", // issues https://github.com/xamarin/maccore/issues/1198
