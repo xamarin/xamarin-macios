4145023876501d4766a46ba34b086ead083240a9
Manuel de la Pena <mandel@microsoft.com>
2019-03-05 15:30:50 +0100

    Address the reviews.

--- tools/bcl-test-importer/BCLTestImporter/BCLTestProjectGenerator.cs
+++ tools/bcl-test-importer/BCLTestImporter/BCLTestProjectGenerator.cs
@@ -392,10 +392,10 @@ namespace BCLTestImporter {
 		internal static string GetProjectGuid (string projectName) 
 		{
 			//use MD5 hash to get a 16-byte hash of the string: 
-			using (var provider = new MD5CryptoServiceProvider ()) {
-				var inputBytes = Encoding.Default.GetBytes (projectName);
+			using (var provider = MD5.Create ()) {
+				var inputBytes = Encoding.UTF8.GetBytes (projectName);
 				var hashBytes = provider.ComputeHash (inputBytes);
-				return new Guid (hashBytes).ToString ().ToUpper ();
+				return new Guid (hashBytes).ToString ().ToUpperInvariant ();
 			}
 		} 
 
