4a173ec96fd742761408a91c369055971649c478
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-03-19 18:51:09 +0100

    [registrar] Fix multiple ref/out parameters for a single method.
    The previous code would break the loop looking for ref/out parameters after finding the first one.

--- runtime/trampolines-invoke.m
+++ runtime/trampolines-invoke.m
@@ -587,7 +587,6 @@ xamarin_invoke_trampoline (enum TrampolineType type, id self, SEL sel, iterator_
 						exception_gchandle = xamarin_get_exception_for_parameter (8030, 0, "Unable to marshal the out/ref parameter", sel, method, p, i, false);
 						goto exception_handling;
 					}
-					break;
 				}
 				ofs++;
 			}
