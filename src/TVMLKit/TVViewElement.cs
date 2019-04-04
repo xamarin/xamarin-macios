d95c065d694695844167b8daaa27e4ff455c5325
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-04-04 17:42:31 +0200

    [TVMLKit] Work around Apple breaking API for enum values in TVElementUpdateType.
    Reference: https://trello.com/c/Eyox7vlH/118-41123682-tvelementupdatetype-enum-values-changed
    Reference: rdar://41123682 (closed as by design)

--- src/TVMLKit/TVViewElement.cs
+++ src/TVMLKit/TVViewElement.cs
@@ -1,6 +1,7 @@
 using System;
 using Foundation;
 using ObjCRuntime;
+using UIKit;
 
 namespace TVMLKit {
 
@@ -16,6 +17,44 @@ namespace TVMLKit {
 		public bool IsDispatched { get; set; }
 
 		public bool IsCancelled { get; set; }
+#endif
+	}
+
+	public partial class TVViewElement {
+#if !COREBUILD
+#if !XAMCORE_4_0
+		[Export ("updateType")]
+		public virtual TVElementUpdateType UpdateType {
+			get {
+				var value = _UpdateType;
+				switch ((long) value) {
+				case 2:
+					if (UIDevice.CurrentDevice.CheckSystemVersion (12, 0)) {
+						return TVElementUpdateType.Styles;
+					} else {
+						return TVElementUpdateType.Children;
+					}
+					break;
+				case 3:
+					if (UIDevice.CurrentDevice.CheckSystemVersion (12, 0)) {
+						return TVElementUpdateType.Children;
+					} else {
+						return TVElementUpdateType.Self;
+					}
+					break;
+				case 4:
+					if (UIDevice.CurrentDevice.CheckSystemVersion (12, 0)) {
+						return TVElementUpdateType.Self;
+					} else {
+						return TVElementUpdateType.Styles;
+					}
+					break;
+				default:
+					return value;
+				}
+			}
+		}
+#endif // !XAMCORE_4_0
 #endif
 	}
 }
