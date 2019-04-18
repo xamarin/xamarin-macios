8e486b94b50cd51c14ce4aa59d70f4b385fdd23a
Sebastien Pouliot <sebastien@xamarin.com>
2019-04-18 17:24:55 -0400

    [metalperformanceshaders] Remove private API

---
+++ src/MetalPerformanceShaders/MPSCompat.cs
@@ -0,0 +1,23 @@
+#if !XAMCORE_4_0
+
+using System;
+
+namespace MetalPerformanceShaders {
+
+	public partial class MPSCnnConvolutionTransposeNode {
+
+		[Obsolete ("Always return null (not a public API).")]
+		static public MPSCnnConvolutionTransposeNode Create (MPSNNImageNode sourceNode,  MPSCnnConvolutionStateNode convolutionState, IMPSCnnConvolutionDataSource weights)
+		{
+			return null;
+		}
+
+		[Obsolete ("Always throw a 'NotSupportedExceptipn' (not a public API).")]
+		public MPSCnnConvolutionTransposeNode (MPSNNImageNode sourceNode, MPSCnnConvolutionStateNode convolutionState, IMPSCnnConvolutionDataSource weights) : base (IntPtr.Zero)
+		{
+			throw new NotSupportedException ();
+		}
+	}
+}
+
+#endif
