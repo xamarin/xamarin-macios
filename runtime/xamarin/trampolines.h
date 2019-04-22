7a9cb8372a046308002f276ab32e77e4c60392ec
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-04-22 17:50:38 +0200

    [registrar] Extract code to create a managed array from an NSArray to a separate method.

--- runtime/xamarin/trampolines.h
+++ runtime/xamarin/trampolines.h
@@ -181,6 +181,7 @@ id xamarin_nsdirectionaledgeinsets_to_nsvalue(MonoObject *value, void *context, 
 
 // These functions can be passed as xamarin_id_to_managed_func/xamarin_managed_to_id_func parameters
 id           xamarin_convert_string_to_nsstring (MonoObject *obj, void *context, guint32 *exception_gchandle);
+void *       xamarin_convert_nsstring_to_string (id value, void *ptr, MonoClass *managedType, void *context, guint32 *exception_gchandle);
 
 // These are simpler versions of the above string<->nsstring conversion functions.
 NSString *   xamarin_string_to_nsstring (MonoString *obj, bool retain);
@@ -189,10 +190,16 @@ MonoString * xamarin_nsstring_to_string (MonoDomain *domain, NSString *obj);
 
 // Either managed_type or managed_class has to be provided
 NSArray *   xamarin_managed_array_to_nsarray (MonoArray *array, MonoType *managed_type, MonoClass *managed_class, guint32 *exception_gchandle);
+// Either managed_type or managed_class has to be provided
+MonoArray * xamarin_nsarray_to_managed_array (NSArray *array, MonoType *managed_type, MonoClass *managed_class, guint32 *exception_gchandle);
 
 NSArray *   xamarin_managed_string_array_to_nsarray (MonoArray *array, guint32 *exception_gchandle);
 NSArray *   xamarin_managed_nsobject_array_to_nsarray (MonoArray *array, guint32 *exception_gchandle);
 NSArray *   xamarin_managed_inativeobject_array_to_nsarray (MonoArray *array, guint32 *exception_gchandle);
+
+MonoArray * xamarin_nsarray_to_managed_string_array (NSArray *array, guint32 *exception_gchandle);
+MonoArray * xamarin_nsarray_to_managed_nsobject_array (NSArray *array, MonoType *array_type, MonoClass *element_class, guint32 *exception_gchandle);
+MonoArray * xamarin_nsarray_to_managed_inativeobject_array (NSArray *array, MonoType *array_type, MonoClass *element_class, guint32 *exception_gchandle);
 
 /* Copied from SGen */
 
