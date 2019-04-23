84b99d85e5366efbb57a8b98d86f6c4b124ce7e8
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-04-23 07:59:32 +0200

    [registrar] Reuse some more array conversion code from the static registrar.

--- runtime/xamarin/trampolines.h
+++ runtime/xamarin/trampolines.h
@@ -201,6 +201,7 @@ NSArray *   xamarin_managed_inativeobject_array_to_nsarray (MonoArray *array, gu
 MonoArray * xamarin_nsarray_to_managed_string_array (NSArray *array, guint32 *exception_gchandle);
 MonoArray * xamarin_nsarray_to_managed_nsobject_array (NSArray *array, MonoType *array_type, MonoClass *element_class, guint32 *exception_gchandle);
 MonoArray * xamarin_nsarray_to_managed_inativeobject_array (NSArray *array, MonoType *array_type, MonoClass *element_class, guint32 *exception_gchandle);
+MonoArray * xamarin_nsarray_to_managed_inativeobject_array_static (NSArray *array, MonoType *array_type, MonoClass *element_class, uint32_t iface_token_ref, uint32_t implementation_token_ref, guint32 *exception_gchandle);
 
 /* Copied from SGen */
 
