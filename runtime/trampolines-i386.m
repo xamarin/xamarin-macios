37a280d974d5456a8ef97ee8efaf05c4e6fbad7a
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-05-06 19:29:28 +0200

    [runtime] Put LOGZ directive in a header to not duplicate it everywhere. (#6001)

--- runtime/trampolines-i386.m
+++ runtime/trampolines-i386.m
@@ -15,13 +15,6 @@
 #include "runtime-internal.h"
 #include "trampolines-i386.h"
 
-//#define TRACE
-#ifdef TRACE
-#define LOGZ(...) fprintf (stderr, __VA_ARGS__);
-#else
-#define LOGZ(...) ;
-#endif
-
 #ifdef TRACE
 static void
 dump_state (struct XamarinCallState *state)
