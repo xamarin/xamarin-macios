259d7ff9c4af31e64e4f52a049252284f24ca2b0
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2019-05-07 08:35:15 -0400

    [foundation] Use the direct value for `Tls13` so a mono update is not needed

--- src/Foundation/NSUrlSessionHandler.cs
+++ src/Foundation/NSUrlSessionHandler.cs
@@ -168,7 +168,7 @@ namespace Foundation {
 				configuration.TLSMinimumSupportedProtocol = SslProtocol.Tls_1_1;
 			else if ((sp & SecurityProtocolType.Tls12) != 0)
 				configuration.TLSMinimumSupportedProtocol = SslProtocol.Tls_1_2;
-			else if ((sp &  SecurityProtocolType.Tls13) != 0)
+			else if ((sp & (SecurityProtocolType.Tls13) 12288) != 0)
 				configuration.TLSMinimumSupportedProtocol = SslProtocol.Tls_1_3;
 				
 			session = NSUrlSession.FromConfiguration (configuration, (INSUrlSessionDelegate) new NSUrlSessionHandlerDelegate (this), null);
