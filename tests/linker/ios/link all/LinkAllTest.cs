71f9341c59b23e04ac38b7e2c8b7b13b9284e79a
Sebastien Pouliot <sebastien@xamarin.com>
2019-06-12 13:57:51 -0400

    [tests][link all] Workaround issue with UIPasteboard.General.Images
    ref: https://github.com/xamarin/xamarin-macios/issues/6254

--- tests/linker/ios/link all/LinkAllTest.cs
+++ tests/linker/ios/link all/LinkAllTest.cs
@@ -310,6 +310,13 @@ namespace LinkAll {
 				using (var cgimg = CGImage.FromPNG (dp, null, false, CGColorRenderingIntent.Default)) {
 					using (var img = new UIImage (cgimg)) {
 						UIPasteboard.General.Images = new UIImage[] { img };
+						if (TestRuntime.CheckXcodeVersion (8,0))
+							Assert.True (UIPasteboard.General.HasImages, "HasImages");
+
+						// https://github.com/xamarin/xamarin-macios/issues/6254
+						if (TestRuntime.CheckXcodeVersion (11, 0))
+							return;
+
 						Assert.AreEqual (1, UIPasteboard.General.Images.Length, "a - length");
 
 						UIPasteboard.General.Images = new UIImage[] { img, img };
