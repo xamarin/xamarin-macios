85ec17a5068de1077c95f7223f776f32e7379aa7
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-06-21 15:00:19 +0200

    [Network] Improve bindings for NWProtocolMetadata. (#6389)
    * [Network] Improve bindings for NWProtocolMetadata.
    
    It turns out the NWProtocolMetadata can contain metadata for different
    protocols (Ip/Tls/Tcp). This is important; if someone tries to get a value for
    one protocol and the metadata is for another protocol, then they invoke the
    wrath of superior beings who will smite that poor someone with uninitialized
    memory.
    
    At that point there's not much left but to pray.
    
    I don't like to depend on divine intervention, so I've modified the API here
    to check if the metadata's protocol is the required type for the native API
    we're calling, and if the check fails, we throw a nice and dependable managed
    exception.
    
    This is a functional breaking change; but if there are any lost souls who
    likes to pray, they can always re-implement the P/Invokes themselves and skip
    the sanity checks.
    
    In addition I've renamed a few properties whose name didn't clearly specify
    which protocol type they operate on.
    
    Ref: Apple feedback FB6155967.
    Ref: https://trello.com/c/1TW0BSKJ/145-fb6155967-nwipcreatemetadata-returns-uninitialized-metadata-in-ios-13
    
    * Fix casing in exception message.
    
    * [tests] Adjust the SecProtocolMetadataTest according to new knowledge about the Network API.
    
    * Fix alternative property name in obsolete attribute.

--- tests/monotouch-test/Security/SecProtocolMetadataTest.cs
+++ tests/monotouch-test/Security/SecProtocolMetadataTest.cs
@@ -1,6 +1,9 @@
 ﻿#if !__WATCHOS__
 using System;
 using System.Runtime.InteropServices;
+using System.Threading;
+
+using CoreFoundation;
 using Foundation;
 using Network;
 using ObjCRuntime;
@@ -21,44 +24,55 @@ namespace MonoTouchFixtures.Security {
 		}
 
 		[Test]
-		public void IPDefaults ()
+		public void TlsDefaults ()
 		{
-			if (TestRuntime.CheckXcodeVersion (11, 0))
-				Assert.Ignore ("NWProtocolMetadata.CreateIPMetadata () returns a metadata object with uninitialized metadata, which means the asserts here fail randomly.");
+			using (var ep = NWEndpoint.Create ("www.microsoft.com", "https"))
+			using (var parameters = NWParameters.CreateSecureTcp ())
+			using (var queue = new DispatchQueue (GetType ().FullName)) {
+				var connection = new NWConnection (ep, parameters);
 
-			using (var m = NWProtocolMetadata.CreateIPMetadata ()) {
-				var s = m.SecProtocolMetadata;
-				// This is mostly, but not always, returning false
-				// Assert.False (s.EarlyDataAccepted, "EarlyDataAccepted");
-				Assert.That (s.NegotiatedCipherSuite, Is.EqualTo (SslCipherSuite.SSL_NULL_WITH_NULL_NULL), "NegotiatedCipherSuite");
-				Assert.Null (s.NegotiatedProtocol, "NegotiatedProtocol");
-				Assert.That (s.NegotiatedProtocolVersion, Is.EqualTo (SslProtocol.Unknown), "NegotiatedProtocolVersion");
-				Assert.Null (s.PeerPublicKey, "PeerPublicKey");
-#if false
-				Assert.True (SecProtocolMetadata.ChallengeParametersAreEqual (s, s), "ChallengeParametersAreEqual");
-				Assert.True (SecProtocolMetadata.PeersAreEqual (s, s), "PeersAreEqual");
-#endif
-			}
-		}
+				var ready = new ManualResetEvent (false);
+				connection.SetStateChangeHandler ((state, error) => {
+					Console.WriteLine (state);
+					switch (state) {
+					case NWConnectionState.Cancelled:
+					case NWConnectionState.Failed:
+						// We can't dispose until the connection has been closed or it failed.
+						connection.Dispose ();
+						break;
+					case NWConnectionState.Invalid:
+					case NWConnectionState.Preparing:
+					case NWConnectionState.Waiting:
+						break;
+					case NWConnectionState.Ready:
+						ready.Set ();
+						break;
+					default:
+						break;
+					}
+				});
 
-#if false
-		[DllImport (Constants.CoreFoundationLibrary)]
-		extern static nint CFGetRetainCount (IntPtr handle);
+				connection.SetQueue (queue);
+				connection.Start ();
 
-		[Test]
-		public void CreateSecret ()
-		{
-			using (var npm = NWProtocolMetadata.CreateIPMetadata ()) {
-				// `npm` and `spm` have the same handle - same internal object satistfy both protocols
-				Console.WriteLine ($"{CFGetRetainCount (npm.Handle)}");
-				using (var spm = npm.SecProtocolMetadata) {
-					Console.WriteLine ($"{CFGetRetainCount (npm.Handle)}");
-					Console.WriteLine ($"{CFGetRetainCount (spm.Handle)}");
-					var secret = spm.CreateSecret ("test", 16); // crash
+				// Wait until the connection is ready.
+				Assert.True (ready.WaitOne (TimeSpan.FromSeconds (10)), "Connection is ready");
+
+				using (var m = connection.GetProtocolMetadata (NWProtocolDefinition.TlsDefinition)) {
+					var s = m.TlsSecProtocolMetadata;
+					Assert.False (s.EarlyDataAccepted, "EarlyDataAccepted");
+					Assert.That (s.NegotiatedCipherSuite, Is.Not.EqualTo (SslCipherSuite.SSL_NULL_WITH_NULL_NULL), "NegotiatedCipherSuite");
+					Assert.Null (s.NegotiatedProtocol, "NegotiatedProtocol");
+					Assert.That (s.NegotiatedProtocolVersion, Is.EqualTo (SslProtocol.Tls_1_2).Or.EqualTo (SslProtocol.Tls_1_3), "NegotiatedProtocolVersion");
+					Assert.NotNull (s.PeerPublicKey, "PeerPublicKey");
+
+					Assert.True (SecProtocolMetadata.ChallengeParametersAreEqual (s, s), "ChallengeParametersAreEqual");
+					Assert.True (SecProtocolMetadata.PeersAreEqual (s, s), "PeersAreEqual");
 				}
+
+				connection.Cancel ();
 			}
 		}
-#endif
 	}
 }
 #endif
