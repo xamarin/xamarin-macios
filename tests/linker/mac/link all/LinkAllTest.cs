891dfcb7d94a6019cbca4a06408223604428f5a2
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-07-11 01:24:21 -0700

    [mmptest] Port link-uithread-1/2 to an NUnit LinkAll test. Partial fix for #4975. (#6546)
    Fixes part of https://github.com/xamarin/xamarin-macios/issues/4975.

--- tests/linker/mac/link all/LinkAllTest.cs
+++ tests/linker/mac/link all/LinkAllTest.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+using AppKit;
 using Foundation;
 
 using NUnit.Framework;
@@ -24,6 +28,43 @@ namespace LinkAllTests
 			Check ("UmAlQuraCalendar", false);
 			Check ("HijriCalendar", false);
 			Check ("ThaiBuddhistCalendar", false);
+		}
+
+		[Test]
+		public void EnsureUIThreadException ()
+		{
+			// works on main/ui thread
+			NSApplication.EnsureUIThread ();
+
+			ThreadPool.QueueUserWorkItem ((v) => Tester.Test ());
+			Assert.IsTrue (Tester.mre.WaitOne (TimeSpan.FromSeconds (10)), "Successful wait");
+			// The UI thread check only happens for debug builds, on release build it's linked away.
+#if DEBUG
+			var expected_ex_thrown = true;
+#else
+			var expected_ex_thrown = false;
+#endif
+			Assert.AreEqual (expected_ex_thrown, Tester.exception_thrown, "Success");
+		}
+
+
+		class Tester : NSObject {
+			public static ManualResetEvent mre = new ManualResetEvent (false);
+			public static bool exception_thrown;
+
+			[CompilerGenerated]
+			[Export ("foo")]
+			public static void Test ()
+			{
+				try {
+					exception_thrown = false;
+					NSApplication.EnsureUIThread ();
+				} catch (AppKitThreadAccessException) {
+					exception_thrown = true;
+				} finally {
+					mre.Set ();
+				}
+			}
 		}
 	}
 }
