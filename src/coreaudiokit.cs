799cf06a9948ecc15aa8d93a7e2c135e44318b71
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2019-07-15 14:44:36 -0400

    [coreaudiokit] Update for Xcode 11 beta 1 (#6582)
    No change in beta 2 and beta 3

--- src/coreaudiokit.cs
+++ src/coreaudiokit.cs
@@ -174,6 +174,8 @@ namespace CoreAudioKit {
 	}
 
 	[iOS (8,0)]
+	[Deprecated (PlatformName.iOS, 13,0, message: "Use 'AudioUnit' instead.")]
+	[Unavailable (PlatformName.UIKitForMac)][Advice ("This API is not available when using UIKit on macOS.")]
 	[BaseType (typeof (UIView))]
 	interface CAInterAppAudioSwitcherView {
 		[Export ("initWithFrame:")]
@@ -190,6 +192,8 @@ namespace CoreAudioKit {
 	}
 
 	[iOS (8,0)]
+	[Deprecated (PlatformName.iOS, 13,0, message: "Use 'AudioUnit' instead.")]
+	[Unavailable (PlatformName.UIKitForMac)][Advice ("This API is not available when using UIKit on macOS.")]
 	[BaseType (typeof (UIView))]
 	interface CAInterAppAudioTransportView {
 		[Export ("initWithFrame:")]
