3ef8cc8ac0c81ae79a37f2f90584496ce70f1143
Whitney Schmidt <whschm@microsoft.com>
2019-07-16 11:07:56 -0400

    remove onlyOn64: true

--- src/quicklookUI.cs
+++ src/quicklookUI.cs
@@ -169,7 +169,7 @@ namespace QuickLookUI {
 		[Export ("preparePreviewOfSearchableItemWithIdentifier:queryString:completionHandler:")]
 		void PreparePreviewOfSearchableItem (string identifier, string queryString, Action<NSError> ItemLoadingHandler);
 
-		[Mac (10,15, onlyOn64: true)]
+		[Mac (10,15)]
 		[Export ("preparePreviewOfFileAtURL:completionHandler:")]
 		void PreparePreviewOfFile (NSUrl url, Action<NSError> completionHandler);
 	}
