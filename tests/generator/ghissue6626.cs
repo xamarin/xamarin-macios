a743830468b6cbc1a9c5e2103b636c0bea80b0e5
Manuel de la Pena <mandel@microsoft.com>
2019-07-22 11:18:50 +0200

    Add missing test.

---
+++ tests/generator/ghissue6626.cs
@@ -0,0 +1,17 @@
+using System;
+using Foundation;
+using AudioToolbox;
+using ObjCRuntime;
+
+namespace GHIssue6626 {
+
+	public delegate int AVAudioSourceNodeRenderBlock (bool isSilence, double timestamp, uint frameCount, ref AudioBuffers outputData);
+
+	[BaseType (typeof (NSObject))]
+	interface AVAudioSourceNode
+	{
+		[Export ("initWithRenderBlock:")]
+		[DesignatedInitializer]
+		IntPtr Constructor (AVAudioSourceNodeRenderBlock block);
+	}
+}
