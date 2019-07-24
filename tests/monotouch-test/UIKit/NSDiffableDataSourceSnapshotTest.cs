be48e89d10fea86bd448c066d8995772162f4a7f
Alex Soto <alex@alexsoto.me>
2019-07-24 13:59:40 -0400

    [registrar] Fix a generics type issue with dynamic registrar
    Fixes xamarin/xamarin-macios#6567
    
    This Fixes an issue in the registrar where the dynamic registrar
    misses a case to check for a NSObject constraint triggered by UIKit's
    `NSDiffableDataSourceSnapshot` object.
    
    This also enables the rest of missing bindings in UIKit for Xcode 11 B4
    which also works as a test case for the registrar fix. Without this fix
    introspection test would throw an `AggregateException` and also
    includes `NSDiffableDataSourceSnapshotTest` to check that the created
    objects are usable.

---
+++ tests/monotouch-test/UIKit/NSDiffableDataSourceSnapshotTest.cs
@@ -0,0 +1,46 @@
+﻿//
+// Unit tests for NSDiffableDataSourceSnapshot
+//
+// Authors:
+//	Alex Soto <alexsoto@microsoft.com>
+//
+//
+// Copyright 2019 Microsoft Corporation.
+//
+
+#if !__WATCHOS__
+using System;
+using NUnit.Framework;
+using Foundation;
+using UIKit;
+using ObjCRuntime;
+
+namespace MonoTouchFixtures.UIKit {
+	[TestFixture]
+	[Preserve (AllMembers = true)]
+	public class NSDiffableDataSourceSnapshotTest {
+
+		// https://github.com/xamarin/xamarin-macios/issues/6567
+		[Test]
+		public void GHIssue6567Test ()
+		{
+			var type = typeof (UICollectionViewDiffableDataSource<,>);
+			Assert.NotNull (type, $"{nameof (type)} was null;");
+
+			Class cls = null;
+			Assert.DoesNotThrow (() => cls = new Class (type), "Should not throw");
+			Assert.NotNull (cls, $"{nameof (cls)} was null");
+		}
+
+		[Test]
+		public void ObjectUsageTest ()
+		{
+			var diff = new NSDiffableDataSourceSnapshot<NSNumber, NSUuid> ();
+			diff.AppendSections (new NSNumber [] { NSNumber.FromNInt (1) });
+			diff.AppendItems (new NSUuid [] { new NSUuid () }, NSNumber.FromNInt (1));
+			Assert.That (diff.NumberOfSections, Is.GreaterThan (0), "Sections");
+			Assert.That (diff.GetNumberOfItems (NSNumber.FromNInt (1)), Is.GreaterThan (0), "Items");
+		}
+	}
+}
+#endif //!__WATCHOS__
