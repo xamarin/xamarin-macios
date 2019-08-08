1bb82d0b1eccf83a57fc0a3febade53599049636
Sebastien Pouliot <sebastien@xamarin.com>
2019-08-08 10:54:37 -0400

    Move code (to a better location) to avoid collection/exception changes. Also add an unit test

--- tests/linker/ios/link sdk/LinkSdkRegressionTest.cs
+++ tests/linker/ios/link sdk/LinkSdkRegressionTest.cs
@@ -1093,5 +1093,15 @@ namespace LinkSdk {
 		{
 			return Type.GetType (name, throwOnError);
 		}
+
+		[Test]
+		// https://github.com/xamarin/xamarin-macios/issues/6711
+		public void PreserveINativeObject ()
+		{
+			// linker will keep the MTAudioProcessingTap type
+			var mta = typeof (MediaToolbox.MTAudioProcessingTap);
+			// and we check that it still implement INativeObject
+			Assert.IsNotNull (mta.GetInterface ("ObjCRuntime.INativeObject"), "INativeObject");
+		}
 	}
 }
