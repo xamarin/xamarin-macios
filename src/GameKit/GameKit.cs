8d2e9d95c1b16ee9b751917af66c110d64933b57
Sebastien Pouliot <sebastien.pouliot@microsoft.com>
2019-08-14 14:31:24 -0400

    [gamekit] Update for Xcode 11 beta 1 to 5
    `gamePlayerID` and `teamPlayerID` are decorated as `[iOS (12,4)]...` since
    the headers mention so in both Xcode 11 betas and the recent 10.3 (stable)
    https://github.com/xamarin/xamarin-macios/wiki/GameKit-iOS-xcode103-final
    
    The `enum GKError` has been unified (at some point) so it was simplified.

--- src/GameKit/GameKit.cs
+++ src/GameKit/GameKit.cs
@@ -109,10 +109,8 @@ namespace GameKit {
 		InvalidPlayer,
 		ScoreNotSet,
 		ParentalControlsBlocked,
-#if !MONOMAC
 		PlayerStatusExceedsMaximumLength,
 		PlayerStatusInvalid,
-#endif
 		MatchRequestInvalid = 13,
 		Underage,
 		GameUnrecognized,
@@ -125,15 +123,17 @@ namespace GameKit {
 		TurnBasedInvalidParticipant,
 		TurnBasedInvalidTurn,
 		TurnBasedInvalidState,
-#if MONOMAC
+#if MONOMAC && !XAMCORE_4_0
+		[Obsolete ("This value was re-used on macOS only and removed later.")]
 		Offline = 25,
-#else
+#endif
 		InvitationsDisabled = 25, // iOS 7.0
 		PlayerPhotoFailure = 26,  // iOS 8.0
 		UbiquityContainerUnavailable = 27, // iOS 8.0
-#endif
 		MatchNotConnected = 28,
 		GameSessionRequestInvalid = 29,
+		RestrictedToAutomatch = 30,
+		ApiNotAvailable = 31,
 	}
 
 	[Native]
