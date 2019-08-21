55a95cff879db23b19a84b4bf94a52b8b1135659
Manuel de la Pena <mandel@microsoft.com>
2019-08-21 10:54:19 +0200

    [Tests] Allow to ignore tests that fail on the interpreter. (#6802)
    There are some failures that only happen in the interpreter. We add a
    TestRuntime method that allow to check if the interpreter is used.

--- tests/common/TestRuntime.cs
+++ tests/common/TestRuntime.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Reflection.Emit;
 
 #if XAMCORE_2_0
 using AVFoundation;
@@ -109,6 +110,20 @@ partial class TestRuntime
 		if (!in_ci)
 			return;
 		NUnit.Framework.Assert.Ignore (message);
+	}
+
+	public static bool CheckExecutingWithInterpreter ()
+	{
+		// until System.Runtime.CompilerServices.RuntimeFeature.IsSupported("IsDynamicCodeCompiled") returns a valid result, atm it
+		// always return true, try to build an object of a class that should fail without introspection, and catch the exception to do the
+		// right thing
+		try {
+			AssemblyBuilder ab = AppDomain.CurrentDomain.DefineDynamicAssembly (null, AssemblyBuilderAccess.RunAndSave);
+			return true;
+		} catch (PlatformNotSupportedException) {
+			// we do not have the interpreter, lets continue
+			return false;
+		}
 	}
 
 	public static void AssertXcodeVersion (int major, int minor, int build = 0)
