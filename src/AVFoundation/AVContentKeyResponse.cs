9be95cef55b9812f6f9a21de5717bebf60a438b0
Manuel de la Pena <mandel@microsoft.com>
2019-09-04 15:08:51 -0400

    Fix API changes and crashes on 32b.

--- src/AVFoundation/AVContentKeyResponse.cs
+++ src/AVFoundation/AVContentKeyResponse.cs
@@ -23,8 +23,9 @@ namespace AVFoundation {
 
 	public partial class AVContentKeyResponse {
 
-		public static AVContentKeyResponse Create (NSData data) => Create (data, AVContentKeyResponseDataType.FairPlayStreamingKeyResponseData);
+		public static AVContentKeyResponse Create (NSData fairPlayStreamingKeyResponseData) => Create (fairPlayStreamingKeyResponseData, AVContentKeyResponseDataType.FairPlayStreamingKeyResponseData);
 			
+		[TV (10,2), Mac (10,12,4), iOS (10,3), NoWatch]
 		public static AVContentKeyResponse Create (NSData data, AVContentKeyResponseDataType dataType = AVContentKeyResponseDataType.FairPlayStreamingKeyResponseData) {
 			switch (dataType) {
 			case AVContentKeyResponseDataType.AuthorizationTokenData:
