5c572fb8886a03631f773e3c4b0fee1a8a69af56
Waleed Chaudhry <wachaudh@microsoft.com>
2019-09-09 16:41:28 -0400

    Add PathIsAlwaysUpdatedWithNewHandlerTest and convert spaces to tabs

--- tests/monotouch-test/Network/NWPathMonitorTest.cs
+++ tests/monotouch-test/Network/NWPathMonitorTest.cs
@@ -1,5 +1,6 @@
 ﻿#if !__WATCHOS__
 using System;
+using System.Threading;
 using Network;
 using CoreFoundation;
 using Foundation;
@@ -7,56 +8,110 @@ using Foundation;
 using NUnit.Framework;
 
 
-namespace monotouchtest.Network {
-    [TestFixture]
-    [Preserve(AllMembers = true)]
-    public class NWPathMonitorTest {
+namespace monotouchtest.Network
+{
+	[TestFixture]
+	[Preserve(AllMembers = true)]
+	public class NWPathMonitorTest
+	{
 
-        NWPathMonitor monitor;
+		NWPathMonitor monitor;
 
-        [TestFixtureSetUp]
-        public void Init()
-        {
-            TestRuntime.AssertXcodeVersion (10, 0);
-            monitor = new NWPathMonitor ();
-        }
+		[TestFixtureSetUp]
+		public void Init()
+		{
+			TestRuntime.AssertXcodeVersion(10, 0);
+			monitor = new NWPathMonitor();
+		}
 
-        [Test]
-        public void StatusPropertyTest ()
-        {
-            Assert.That (monitor.CurrentPath, Is.Null, "'CurrentPath' property should be null");
+		[Test]
+		public void StatusPropertyTest()
+		{
+			Assert.That(monitor.CurrentPath, Is.Null, "'CurrentPath' property should be null");
 
-            NWPath finalPath = null;
-            bool isPathUpdated = false;
-            
-            TestRuntime.RunAsync (DateTime.Now.AddSeconds (30), async () =>
-            {
+			NWPath finalPath = null;
+			bool isPathUpdated = false;
 
-                monitor.SnapshotHandler =  ((path) =>
-                {
-                    if (path != null) {
-                        finalPath = monitor.CurrentPath;
-                        isPathUpdated = true;
-                    }
+			TestRuntime.RunAsync(DateTime.Now.AddSeconds(30), async () =>
+			{
 
-                });
+				monitor.SnapshotHandler = ((path) =>
+				{
+					if (path != null)
+					{
+						finalPath = monitor.CurrentPath;
+						isPathUpdated = true;
+					}
 
-                var q = new DispatchQueue (label: "monitor");
-                monitor.SetQueue (q);
-                monitor.Start ();
+				});
 
-            }, () => isPathUpdated);
+				var q = new DispatchQueue(label: "monitor");
+				monitor.SetQueue(q);
+				monitor.Start();
 
-            Assert.That (finalPath, Is.Not.Null, "'CurrentPath' property should not be null");
-        }
+			}, () => isPathUpdated);
+
+			Assert.That(finalPath, Is.Not.Null, "'CurrentPath' property should not be null");
+		}
+
+		[Test]
+		public void PathIsAlwaysUpdatedWithNewHandlerTest()
+		{
+			NWPath oldPath = monitor.CurrentPath;
+			NWPath newPath = monitor.CurrentPath;
+			bool isOldPathSet = false;
+			bool isNewPathSet = false;
+			var cbEvent = new AutoResetEvent(false);
+
+			TestRuntime.RunAsync(DateTime.Now.AddSeconds(30), async () =>
+			{
+
+				monitor.SnapshotHandler = ((path) =>
+				{
+					if (path != null)
+					{
+						oldPath = monitor.CurrentPath;
+						isOldPathSet = true;
+						Console.WriteLine("oldPath: " + oldPath);
+						cbEvent.Set();
+					}
+				});
+
+				var q = new DispatchQueue(label: "monitor");
+				monitor.SetQueue(q);
+				monitor.Start();
+
+			}, () => isOldPathSet);
 
 
-        [TearDown]
-        public void TearDown ()
-        {
-            monitor?.Dispose ();
-        }
+			TestRuntime.RunAsync(DateTime.Now.AddSeconds(30), async () =>
+			{
+				cbEvent.WaitOne();
+				monitor.SnapshotHandler = ((path) =>
+				{
+					if (path != null)
+					{
+						newPath = monitor.CurrentPath;
+						Console.WriteLine("newPath: " + newPath);
+						isNewPathSet = true;
+					}
 
-    }
+				});
+
+				var q = new DispatchQueue(label: "monitor");
+				monitor.SetQueue(q);
+				monitor.Start();
+			}, () => isNewPathSet);
+
+			Assert.AreNotEqual(oldPath, newPath, "'CurrentPath' wasn't updated when a new SnapshotHandler was assigned");
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			monitor?.Dispose();
+		}
+
+	}
 }
 #endif
