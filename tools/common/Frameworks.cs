b5a85a115b3dfa2d42b0e9e7d677f6e1723c4168
Manuel de la Pena <mandel@microsoft.com>
2019-09-10 13:03:26 -0400

    [CoreHaptics] Add support for the new framework up to beta 7. (#6943)

--- tools/common/Frameworks.cs
+++ tools/common/Frameworks.cs
@@ -188,6 +188,7 @@ public class Frameworks : Dictionary <string, Framework>
 					{ "PencilKit", "PencilKit", 10,15 },
 					{ "Speech", "Speech", 10,15 },
 					{ "LinkPresentation", "LinkPresentation", 10,15 },
+					{ "CoreHaptics", "CoreHaptics", 10,15 },
 				};
 			}
 			return mac_frameworks;
@@ -314,6 +315,7 @@ public class Frameworks : Dictionary <string, Framework>
 				{ "Network", "Network", 12, 0 },
 
 				{ "BackgroundTasks", "BackgroundTasks", 13, 0 },
+				{ "CoreHaptics", "CoreHaptics", 13, 0 },
 				{ "LinkPresentation", "LinkPresentation", 13, 0 },
 				{ "PencilKit", "PencilKit", 13, 0 },
 				{ "QuickLookThumbnailing", "QuickLookThumbnailing", 13,0 },
