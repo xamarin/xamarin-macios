3ee1329e0e3f501afd5aa59979f8d37f88c5d96e
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-09-11 05:12:43 -0700

    [generator] Handle inheriting the same member from multiple protocols. Fixes #6366. (#6961)
    * [generator] Handle inheriting the same member from multiple protocols. Fixes #6366.
    
    Fixes https://github.com/xamarin/xamarin-macios/issues/6366.
    
    * Update xtro.

---
+++ tests/generator/tests/diamond-protocol-errors.cs
@@ -0,0 +1,154 @@
+using System;
+using Foundation;
+
+namespace DiamondProtocol
+{
+	namespace A
+	{
+		[Protocol]
+		interface P1
+		{
+			[Abstract]
+			[Export ("p1")]
+			int P1 { get; }
+		}
+
+		[Protocol]
+		interface P2
+		{
+			[Abstract]
+			[Export ("p1")]
+			int P1 { set; }
+		}
+
+		[Protocol]
+		interface C : P1, P2
+		{
+		}
+	}
+
+	namespace B
+	{
+		[Protocol]
+		interface P1
+		{
+			[Abstract]
+			[Export ("p1")]
+			int P1 { get; }
+		}
+
+		[Protocol]
+		interface P2
+		{
+			[Abstract]
+			[Export ("p1")]
+			int P1 { get; set; }
+		}
+
+		[Protocol]
+		interface C : P1, P2
+		{
+		}
+	}
+
+	namespace C
+	{
+		[Protocol]
+		interface P1
+		{
+			[Abstract]
+			[Export ("p1")]
+			int P1 { get; set; }
+		}
+
+		[Protocol]
+		interface P2
+		{
+			[Abstract]
+			[Export ("p1")]
+			long P1 { get; set; }
+		}
+
+		[Protocol]
+		interface C : P1, P2
+		{
+		}
+	}
+
+	namespace D
+	{
+		[Protocol]
+		interface P1
+		{
+			[Abstract]
+			[Export ("pA")]
+			int P1 { get; set; }
+		}
+
+		[Protocol]
+		interface P2
+		{
+			[Abstract]
+			[Export ("pB")]
+			int P1 { get; set; }
+		}
+
+		[Protocol]
+		interface C : P1, P2
+		{
+		}
+	}
+	namespace Z
+	{
+
+		[Protocol]
+		interface P1
+		{
+			[Abstract]
+			[Export ("m1")]
+			void M1 ();
+		}
+
+		[Protocol]
+		interface P2
+		{
+			[Abstract]
+			[Export ("m1")]
+			int M1 ();
+		}
+
+
+		//[BaseType (typeof (NSObject))]
+		[Protocol]
+		interface C : P1, P2
+		{
+		}
+	}
+
+	namespace Y
+	{
+
+		[Protocol]
+		interface P1
+		{
+			[Abstract]
+			[Export ("m1:")]
+			void M1 (int why);
+		}
+
+		[Protocol]
+		interface P2
+		{
+			[Abstract]
+			[Export ("m1:")]
+			int M1 (bool because);
+		}
+
+
+		//[BaseType (typeof (NSObject))]
+		[Protocol]
+		interface C : P1, P2
+		{
+		}
+	}
+}
