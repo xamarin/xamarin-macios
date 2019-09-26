6d6b773c7e8eaa7cc519aec7f250ab458562514b
Sebastien Pouliot <sebastien.pouliot@microsoft.com>
2019-09-26 13:14:06 -0400

    Indent new file properly

--- src/ObjCRuntime/TrampolineBlockBase.cs
+++ src/ObjCRuntime/TrampolineBlockBase.cs
@@ -7,26 +7,26 @@ using System.Runtime.InteropServices;
 namespace ObjCRuntime {
 
 	public abstract class TrampolineBlockBase {
-			protected IntPtr blockPtr;
+		protected IntPtr blockPtr;
 
-			[DllImport ("/usr/lib/libobjc.dylib")]
-			static extern IntPtr _Block_copy (IntPtr ptr);
+		[DllImport ("/usr/lib/libobjc.dylib")]
+		static extern IntPtr _Block_copy (IntPtr ptr);
 
-			protected unsafe TrampolineBlockBase (BlockLiteral *block)
-			{
-				blockPtr = _Block_copy ((IntPtr) block);
-			}
-			
-			~TrampolineBlockBase ()
-			{
-				Runtime.ReleaseBlockOnMainThread (blockPtr);
-			}
+		protected unsafe TrampolineBlockBase (BlockLiteral *block)
+		{
+			blockPtr = _Block_copy ((IntPtr) block);
+		}
 
-			protected unsafe static object GetExistingManagedDelegate (IntPtr block)
-			{
-				if (!BlockLiteral.IsManagedBlock (block))
-					return null;
-				return ((BlockLiteral *) block)->Target;
-			}
+		~TrampolineBlockBase ()
+		{
+			Runtime.ReleaseBlockOnMainThread (blockPtr);
+		}
+
+		protected unsafe static object GetExistingManagedDelegate (IntPtr block)
+		{
+			if (!BlockLiteral.IsManagedBlock (block))
+				return null;
+			return ((BlockLiteral *) block)->Target;
+		}
 	}
 }
