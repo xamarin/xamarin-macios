914da4620ea2b2b3ef81d3a9f6dd6c3142f17d2e
William Kent <wjk011@gmail.com>
2019-10-03 10:43:20 -0400

    Remove more outdated copyright header text

--- src/Foundation/NSXpcInterface.cs
+++ src/Foundation/NSXpcInterface.cs
@@ -1,7 +1,4 @@
 //
-// Copyright 2009-2010, Novell, Inc.
-// Copyright 2011, 2012 Xamarin Inc
-//
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
 // "Software"), to deal in the Software without restriction, including
