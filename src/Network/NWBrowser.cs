4cff2bc4e340c54edc27a0d887fe74e904036e48
Manuel de la Pena <mandel@microsoft.com>
2019-10-15 14:31:31 -0400

    [Networking] Improve the NWBrowser API. (#7214)
    Kept the API as it was but added a new handler that will be called when
    all the changes from the browser have been done. The old Set* method
    could be deleted in a future release and just expose the property.

--- src/Network/NWBrowser.cs
+++ src/Network/NWBrowser.cs
@@ -7,6 +7,7 @@
 // Copyrigh 2019 Microsoft Inc
 //
 using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 using ObjCRuntime;
@@ -28,6 +29,10 @@ namespace Network {
 		Cancelled = 3,
 	}
 
+	public delegate void NWBrowserChangesDelegate (NWBrowseResult oldResult, NWBrowseResult newResult, bool completed);
+
+	public delegate void NWBrowserCompleteChangesDelegate (List<(NWBrowseResult result, NWBrowseResultChange change)> changes);
+
 	[TV (13,0), Mac (10,15), iOS (13,0), Watch (6,0)]
 	public class NWBrowser : NativeObject {
 
@@ -35,7 +40,10 @@ namespace Network {
 		bool queueSet = false;
 		object startLock = new Object ();
 
-		internal NWBrowser (IntPtr handle, bool owns) : base (handle, owns) {}
+		internal NWBrowser (IntPtr handle, bool owns) : base (handle, owns)
+		{
+			SetChangesHandler (InternalChangesHandler);
+		}
 
 		[DllImport (Constants.NetworkLibrary)]
 		static extern OS_nw_browser nw_browser_create (OS_nw_browse_descriptor descriptor, OS_nw_parameters parameters);
@@ -46,6 +54,7 @@ namespace Network {
 				throw new ArgumentNullException (nameof (descriptor));
 
 			InitializeHandle (nw_browser_create (descriptor.Handle, parameters.GetHandle ()));
+			SetChangesHandler (InternalChangesHandler);
 		}
 
 		public NWBrowser (NWBrowserDescriptor descriptor) : this (descriptor, null) {}
@@ -113,22 +122,66 @@ namespace Network {
 		[DllImport (Constants.NetworkLibrary)]
 		unsafe static extern void nw_browser_set_browse_results_changed_handler (OS_nw_browser browser, void *handler);
 
-		delegate void nw_browser_browse_results_changed_handler_t (IntPtr block, IntPtr oldResult, IntPtr newResult);
+		delegate void nw_browser_browse_results_changed_handler_t (IntPtr block, IntPtr oldResult, IntPtr newResult, bool completed);
 		static nw_browser_browse_results_changed_handler_t static_ChangesHandler = TrampolineChangesHandler;
 
 		[MonoPInvokeCallback (typeof (nw_browser_browse_results_changed_handler_t))]
-		static void TrampolineChangesHandler (IntPtr block, IntPtr oldResult, IntPtr newResult)
+		static void TrampolineChangesHandler (IntPtr block, IntPtr oldResult, IntPtr newResult, bool completed)
 		{
-			var del = BlockLiteral.GetTarget<Action<NWBrowseResult, NWBrowseResult>> (block);
+			var del = BlockLiteral.GetTarget<NWBrowserChangesDelegate> (block);
 			if (del != null) {
-				using (var nwOldResult = new NWBrowseResult (oldResult, owns: false))
-				using (var nwNewResult = new NWBrowseResult (newResult, owns: false))
-					del (nwOldResult, nwNewResult);
+				// we do the cleanup of the objs in the internal handlers
+				NWBrowseResult nwOldResult = (oldResult == IntPtr.Zero) ? null : new NWBrowseResult (oldResult, owns: false);
+				NWBrowseResult nwNewResult = (newResult == IntPtr.Zero) ? null : new NWBrowseResult (newResult, owns: false);
+				del (nwOldResult, nwNewResult, completed);
+			}
+		}
+
+		public Action<NWBrowseResult, NWBrowseResult> IndividualChangesDelegate { get; set; }
+
+		// syntactic sugar for the user, nicer to get all the changes at once
+		public NWBrowserCompleteChangesDelegate CompleteChangesDelegate { get; set; }
+		object changesLock = new object ();
+		List<(NWBrowseResult result, NWBrowseResultChange change)> changes = new List<(NWBrowseResult result, NWBrowseResultChange change)> ();
+
+		void InternalChangesHandler (NWBrowseResult oldResult, NWBrowseResult newResult, bool completed)
+		{
+			// we allow the user to listen to both, individual changes AND complete ones, individual is simple, just
+			// call the cb, completed, we need to get a collection and call the cb when completed
+			var individualCb = IndividualChangesDelegate;
+			individualCb?.Invoke (oldResult, newResult);
+			var completeCb = CompleteChangesDelegate;
+			if (completeCb == null) {
+				// we do not want to keep a list of the new results if the user does not care, dispose and move on
+				// results can be null, since we could have a not old one
+				oldResult?.Dispose ();
+				newResult?.Dispose ();
+				return; 
+			}
+			// get the change, add it to the list
+			var change = NWBrowseResult.GetChanges (oldResult, newResult);
+			var result = (result: newResult, change: change);
+			// at this point, we do not longer need the old result
+			// results can be null
+			oldResult?.Dispose ();
+			List<(NWBrowseResult result, NWBrowseResultChange change)> tmp_changes = null;
+			lock (changesLock) {
+				changes.Add (result);
+				// only call when we know we are done
+				if (completed)  {
+					tmp_changes = changes;
+					changes = new List<(NWBrowseResult result, NWBrowseResultChange change)> ();
+				}
+			}
+			if (completed) {
+				completeCb.Invoke (tmp_changes);
+				foreach (var c in tmp_changes)
+					c.result?.Dispose ();
 			}
 		}
 
 		[BindingImpl (BindingImplOptions.Optimizable)]
-		public void SetChangesHandler (Action<NWBrowseResult, NWBrowseResult> handler)
+		void SetChangesHandler (NWBrowserChangesDelegate handler)
 		{
 			unsafe {
 				if (handler == null) {
@@ -145,6 +198,12 @@ namespace Network {
 				}
 			}
 		}	
+
+		// let to not change the API, but would be nice to remove it in the following releases.
+#if !XAMCORE_4_0
+		[Obsolete ("Uset the 'IndividualChangesDelegate' instead.")]
+		public void SetChangesHandler (Action<NWBrowseResult, NWBrowseResult> handler) => IndividualChangesDelegate = handler;
+#endif
 
 		[DllImport (Constants.NetworkLibrary)]
 		unsafe static extern void nw_browser_set_state_changed_handler (OS_nw_browser browser, void *state_changed_handler);
--- tests/monotouch-test/Network/NWBrowserTest.cs
+++ tests/monotouch-test/Network/NWBrowserTest.cs
@@ -25,7 +25,8 @@ namespace MonoTouchFixtures.Network {
 
 		NWBrowserDescriptor descriptor;
 		NWBrowser browser;
-		string type = "_ssh._tcp.";
+
+		string type = "_tictactoe._tcp";
 		string domain = "local.";
 
 		[TestFixtureSetUp]
@@ -35,7 +36,8 @@ namespace MonoTouchFixtures.Network {
 		public void SetUp ()
 		{
 			descriptor = NWBrowserDescriptor.CreateBonjourService (type, domain);
-			browser = new NWBrowser (descriptor);
+			using (var parameters = new NWParameters { IncludePeerToPeer = true})
+				browser = new NWBrowser (descriptor);
 			browser.SetDispatchQueue (DispatchQueue.DefaultGlobalQueue);
 		}
 
@@ -78,14 +80,78 @@ namespace MonoTouchFixtures.Network {
 		[Test]
 		public void TestStateChangesHandler ()
 		{
-			var e = new AutoResetEvent (false);
-			browser.SetStateChangesHandler ((st, er) => {
-				Assert.IsNotNull (st, "State");
-				Assert.IsNull (er, "Error");
-				e.Set ();
-			});
-			browser.Start ();
-			e.WaitOne ();
+			// In the test we are doing the following:
+			//
+			// 1. Start a browser. At this point, we have no listeners (unless someone is exposing it in the lab)
+			// and therefore the browser cannot find any services/listeners.
+			// 2. Start a listener that is using the same type/domain pair that the browser expects.
+			// 3. Browser picks up the new listener, and sends an event (service found).
+			// 4. Listener stops, and the service disappears.
+			// 5. The browser is not yet canceled, so it picks up that the service/listener is not longer then and returns it.
+			// 
+			// The test will block until the different events are set by the callbacks that are executed in a diff thread.
+
+			bool firstRun = true;
+			bool eventsDone = false;
+			bool listeningDone = false;
+			var changesEvent = new AutoResetEvent (false);
+			var browserReady = new AutoResetEvent (false);
+			var finalEvent = new AutoResetEvent (false);
+			TestRuntime.RunAsync (DateTime.Now.AddSeconds (30), async () => {
+				// start the browser, before the listener
+				browser.SetStateChangesHandler ((st, er) => {
+					Assert.IsNotNull (st, "State");
+					Assert.IsNull (er, "Error");
+					if (st == NWBrowserState.Ready)
+						browserReady.Set ();
+				});
+				browser.SetChangesHandler ((oldResult, newResult) => {
+					// first time, listener appears, so we do not have an old result, second time
+					// listener goes, so we do not have a new result
+					if (firstRun) {
+						Assert.IsNull (oldResult, "oldResult first run.");
+						Assert.IsNotNull (newResult, "newResult first run");
+						firstRun = false;
+					} else {
+						Assert.IsNotNull (oldResult, "oldResult first run.");
+						Assert.IsNull (newResult, "newResult first run");
+					}
+					changesEvent.Set ();
+					eventsDone = true;
+
+				});
+				browser.Start ();
+				browserReady.WaitOne (30000);
+				using (var advertiser = NWAdvertiseDescriptor.CreateBonjourService ("MonoTouchFixtures.Network", type))
+				using (var tcpOptions = NWProtocolOptions.CreateTcp ())
+				using (var tlsOptions = NWProtocolOptions.CreateTls ())
+				using (var paramenters = NWParameters.CreateTcp ()) {
+					paramenters.ProtocolStack.PrependApplicationProtocol (tlsOptions);
+					paramenters.ProtocolStack.PrependApplicationProtocol (tcpOptions);
+					paramenters.IncludePeerToPeer = true;
+					using (var listener = NWListener.Create ("1234", paramenters)) {
+						listener.SetQueue (DispatchQueue.CurrentQueue);
+						listener.SetAdvertiseDescriptor (advertiser);
+						// we need the connection handler, else we will get an exception
+						listener.SetNewConnectionHandler ((c) => { });
+						listener.SetStateChangedHandler ((s, e) => {
+							if (e != null) {
+								Console.WriteLine ($"Got error {e.ErrorCode} {e.ErrorDomain} '{e.CFError.FailureReason}' {e.ToString ()}");
+							}
+						});
+						listener.Start ();
+						changesEvent.WaitOne (30000);
+						listener.Cancel ();
+						listeningDone = true;
+						finalEvent.Set ();
+					}
+				}
+
+			}, () => eventsDone);
+
+			finalEvent.WaitOne (30000);
+			Assert.IsTrue (eventsDone);
+			Assert.IsTrue (listeningDone);
 			browser.Cancel ();
 		}
 	}
