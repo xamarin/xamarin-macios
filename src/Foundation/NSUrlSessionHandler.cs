7fac28c567c5187540565eb259370a23efb456d3
Thays Grazia <thaystg@gmail.com>
2019-10-17 10:47:12 -0300

    Needs this ifdef because of this https://github.com/mono/mono/pull/17260.

--- src/Foundation/NSUrlSessionHandler.cs
+++ src/Foundation/NSUrlSessionHandler.cs
@@ -831,7 +831,11 @@ namespace Foundation {
 				base.Dispose (disposing);
 			}
 
+#if __WATCHOS__
+			protected override Task SerializeToStreamAsync (Stream stream, TransportContext context)
+#else			
 			protected internal override Task SerializeToStreamAsync (Stream stream, TransportContext context)
+#endif
 			{
 				if (contentCopied) {
 					if (!content.CanSeek) {
