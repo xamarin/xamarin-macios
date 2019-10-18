d400ec014c386613f1c02f988ac2589acac5392f
Pramit Mallick <prmallic@microsoft.com>
2019-10-18 15:44:14 -0400

    updated copyright info in Xkit/Types header

--- src/XKit/Types.cs
+++ src/XKit/Types.cs
@@ -1,5 +1,5 @@
 //
-// Copyright 2011, Xamarin, Inc.
+// Copyright 2019 Microsoft Inc
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
