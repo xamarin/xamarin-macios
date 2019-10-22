6561d9af791c7206aef51088b8d363d144385a66
Pramit Mallick <prmallic@microsoft.com>
2019-10-22 12:13:36 -0400

    throw argument exception for exhaustive case

--- src/AppKit/NSCollectionLayoutAnchor.cs
+++ src/AppKit/NSCollectionLayoutAnchor.cs
@@ -17,8 +17,9 @@ namespace AppKit {
 		public static NSCollectionLayoutAnchor Create (NSDirectionalRectEdge edges, NSCollectionLayoutAnchorOffsetType offsetType, CGPoint offset) =>
 		    offsetType switch
 		    {
-			    NSCollectionLayoutAnchorOffsetType.Absolute => _LayoutAnchorWithEdgesAbsoluteOffset (edges, offset),
+			    NSCollectionLayoutAnchorOffsetType.Absolute   => _LayoutAnchorWithEdgesAbsoluteOffset (edges, offset),
 			    NSCollectionLayoutAnchorOffsetType.Fractional => _LayoutAnchorWithEdgesFractionalOffset (edges, offset),
+			    _                                             => throw new ArgumentException(message: "Invalid enum value", paramName: nameof(offsetType)),
 		    };
 	}
 }
