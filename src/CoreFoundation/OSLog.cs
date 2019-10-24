9be58e4b9cc64802630972781914b56ae388d645
William Kent <wjk011@gmail.com>
2019-10-24 17:46:03 -0400

    os_log(3) support (#7141)
    * Add xamarin_os_log function
    
    See the comment in the function for an explanation
    of why this wrapper function is required.
    
    * Add Darwin/OSLog.cs
    
    * Add xamarin_os_log to header
    
    This ensures that the symbol will not be subject
    to C++ name mangling, therefore breaking mmp.
    With this change applied, OSLog works as expected.
    
    * Resolve stylistic PR feedback
    
    * Move OSLog into CoreFoundation namespace
    
    This is where the NativeObject class lives, and it
    also feels like a better fit for a low-level API
    that is available on non-Mac platforms than the
    macOS-only Darwin namespace.

---
+++ src/CoreFoundation/OSLog.cs
@@ -0,0 +1,98 @@
+//
+// OSLog.cs: Bindings to the os_log(3) API from Apple
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using ObjCRuntime;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+namespace CoreFoundation {
+	[Mac (10,12), iOS (10,0), Watch (3,0), TV (10,0)]
+	[Introduced (PlatformName.MacCatalyst, 13, 0)]
+	public sealed class OSLog : NativeObject {
+		public static OSLog Default { get; } = new OSLog (IntPtr.Zero, false);
+
+		protected override void Retain ()
+		{
+			if (Handle != IntPtr.Zero)
+				os_retain (Handle);
+		}
+
+		protected override void Release ()
+		{
+			if (Handle != IntPtr.Zero)
+				os_release (Handle);
+		}
+
+		[DllImport (Constants.libSystemLibrary)]
+		extern static IntPtr os_log_create (string subsystem, string category);
+
+		[DllImport (Constants.libSystemLibrary)]
+		extern static IntPtr os_retain (IntPtr handle);
+
+		[DllImport (Constants.libSystemLibrary)]
+		extern static void os_release (IntPtr handle);
+
+		[DllImport ("__Internal")]
+		extern static void xamarin_os_log (IntPtr logHandle, OSLogLevel level, string message);
+
+		OSLog (IntPtr handle, bool owns)
+			: base (handle, owns)
+		{
+		}
+
+		public OSLog (string subsystem, string category)
+		{
+			if (subsystem == null)
+				throw new ArgumentNullException (nameof (subsystem));
+			if (category == null)
+				throw new ArgumentNullException (nameof (category));
+
+			Handle = os_log_create (subsystem, category);
+		}
+
+		public void Log (string message)
+		{
+			Log (OSLogLevel.Default, message);
+		}
+
+		public void Log (OSLogLevel level, string message)
+		{
+			if (message == null)
+				throw new ArgumentNullException (nameof (message));
+
+			xamarin_os_log (Handle, level, message);
+		}
+	}
+
+	[Mac (10,12), iOS (10,0), Watch (3,0), TV (10,0)]
+	[Introduced (PlatformName.MacCatalyst, 13, 0)]
+	public enum OSLogLevel : byte
+	{
+		// These values must match the os_log_type_t enum in <os/log.h>.
+		Default = 0x00,
+		Info    = 0x01,
+		Debug   = 0x02,
+		Error   = 0x10,
+		Fault   = 0x11,
+	}
+}
