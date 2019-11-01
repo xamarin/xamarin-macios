2dd166f7c7cd954e5f9eadd874bb747aea278fe4
Martin Baulig <mabaul@microsoft.com>
2019-11-01 15:03:51 -0400

    Trying to get a mono from source build to work on Jenkins.

--- mk/mono.mk
+++ mk/mono.mk
@@ -1,6 +1,5 @@
-NEEDED_MONO_VERSION := 4847b8e7a721a488c228080a932124337966e5a8
-NEEDED_MONO_BRANCH := 2019-10
-
+NEEDED_MONO_VERSION := 9403a4dd791c4c8ae7e4d415d45a24e540e0c764
+NEEDED_MONO_BRANCH := work-empty-ten
 
 MONO_DIRECTORY := mono
-MONO_MODULE    := https://github.com/mono/mono
+MONO_MODULE    := https://github.com/baulig/mono
