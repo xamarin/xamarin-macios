0b3799c588eb41dda22fdce2d6c1cf0d7cdc601c
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-11-14 07:40:46 +0100

    Verbose provisioning.

--- jenkins/provision-deps.sh
+++ jenkins/provision-deps.sh
@@ -16,7 +16,7 @@ if test -n "$ghprbPullId" && ./jenkins/fetch-pr-labels.sh --check=skip-public-je
 	exit 0
 fi
 
-./system-dependencies.sh --provision-all
+./system-dependencies.sh --provision-all -v
 
 sudo rm -Rf /Developer/MonoTouch
 sudo rm -Rf /Library/Frameworks/Xamarin.iOS.framework
