7e810718258bcb41f4b2e07b1a233582fda17487
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-12-03 13:45:17 +0100

    Disable pushing of perf data for now.

--- tools/devops/publish-performance-data.sh
+++ tools/devops/publish-performance-data.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -ex
 
-# git clone https://github.com/xamarin/xamarin-macios-performance-data
-mkdir xamarin-macios-performance-data
-( cd xamarin-macios-performance-data && git init . )
+DATA_REPO_NAME=xamarin-macios-data
 
-DIR=xamarin-macios-performance-data/$BUILD_SOURCEBRANCHNAME/$BUILD_SOURCEVERSION/$SYSTEM_JOBID
+# git clone https://github.com/xamarin/$DATA_REPO_NAME
+mkdir $DATA_REPO_NAME
+( cd $DATA_REPO_NAME && git init . )
+
+DIR=$DATA_REPO_NAME/$BUILD_SOURCEBRANCHNAME/$BUILD_SOURCEVERSION/$SYSTEM_JOBID
 mkdir -p "$DIR"
 
 cp -c tests/sampletester/bin/Debug/tmp-test-dir/execution-logs/*.xml "$DIR/"
@@ -12,10 +14,10 @@ cd "$DIR"
 git add .
 git commit -m "Add performance data for $BUILD_SOURCEBRANCHNAME/$BUILD_SOURCEVERSION from the $SYSTEM_JOBNAME job."
 
-# Try to push 5 times, just in case someone else pushed first.
-COUNTER=5
-while [[ $COUNTER -gt 0 ]]; do
-	if git push; then break; fi
-	git pull
-	(( COUNTER-- ))
-done
+# # Try to push 5 times, just in case someone else pushed first.
+# COUNTER=5
+# while [[ $COUNTER -gt 0 ]]; do
+# 	if git push; then break; fi
+# 	git pull
+# 	(( COUNTER-- ))
+# done
