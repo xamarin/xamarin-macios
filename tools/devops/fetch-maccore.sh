260862f23ea4855ddd6aee98ea786acdf1b2ca05
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-12-05 00:31:51 +0100

    More debug spew.

--- tools/devops/fetch-maccore.sh
+++ tools/devops/fetch-maccore.sh
@@ -7,6 +7,11 @@
 AUTH=$(git config -l | grep AUTHORIZATION | sed 's/.*AUTHORIZATION: //')
 git config --global http.extraheader "AUTHORIZATION: $AUTH"
 
+# Debug spew, checking if the authorization token is correct
+git ls-remote https://github.com/xamarin/maccore || true
+git ls-remote https://github.com/xamarin/xamarin-macios-data || true
+
+
 # fetch maccore
 # the github auth we use only works with https, so change maccore's url to be https:// instead of git@
 make reset-maccore MACCORE_MODULE="$(grep ^MACCORE_MODULE mk/xamarin.mk | sed -e 's/.*:= //' -e 's_git@github.com:_https://github.com/_' -e 's/[.]git//')" V=1
