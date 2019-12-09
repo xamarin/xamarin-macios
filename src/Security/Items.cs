b872077701ab816b7855735e8d0109a72b558eb5
Marius Ungureanu <therzok@gmail.com>
2019-12-09 17:38:45 +0200

    Switch to using statement.

--- src/Security/Items.cs
+++ src/Security/Items.cs
@@ -147,12 +147,11 @@ namespace Security {
 					if (max == 1)
 						return new NSData [] { new NSData (ptr, false) };
 
-					using (var array = new NSArray (ptr)) {
-						var records = new NSData [array.Count];
-						for (uint i = 0; i < records.Length; i++)
-							records [i] = new NSData (array.ValueAt (i), false);
-						return records;
-					}
+					using var array = new NSArray (ptr);
+					var records = new NSData [array.Count];
+					for (uint i = 0; i < records.Length; i++)
+						records [i] = new NSData (array.ValueAt (i), false);
+					return records;
 				}
 				return null;
 			}
@@ -201,12 +200,11 @@ namespace Security {
 				result = SecItem.SecItemCopyMatching (copy.Handle, out ptr);
 				n = null;
 				if (result == SecStatusCode.Success){
-					using (var array = new NSArray (ptr)) {
-						var records = new SecRecord [array.Count];
-						for (uint i = 0; i < records.Length; i++)
-							records [i] = new SecRecord (new NSMutableDictionary (array.ValueAt (i), false));
-						return records;
-					}
+					using var array = new NSArray (ptr);
+					var records = new SecRecord [array.Count];
+					for (uint i = 0; i < records.Length; i++)
+						records [i] = new SecRecord (new NSMutableDictionary (array.ValueAt (i), false));
+					return records;
 				}
 				return null;
 			}
