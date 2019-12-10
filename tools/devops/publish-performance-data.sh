fa30094c4bb5c31bae7cfbd9a89f60bce1bf0dbc
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-12-10 15:58:09 +0100

    Try the multi repo implementation.

--- tools/devops/publish-performance-data.sh
+++ tools/devops/publish-performance-data.sh
@@ -3,27 +3,27 @@
 DATA_REPO_NAME=xamarin-macios-data
 
 # grab Azure Devop's authorization token from the current repo, and add it to the global git configuration
-AUTH=$(git config -l | grep AUTHORIZATION | head -1 | sed 's/.*AUTHORIZATION: //')
-AUTH_MD5=$(echo "$AUTH" | md5)
-git config --global http.extraheader "AUTHORIZATION: $AUTH"
-echo "AUTH_MD5=$AUTH_MD5"
+#AUTH=$(git config -l | grep AUTHORIZATION | head -1 | sed 's/.*AUTHORIZATION: //')
+#AUTH_MD5=$(echo "$AUTH" | md5)
+#git config --global http.extraheader "AUTHORIZATION: $AUTH"
+#echo "AUTH_MD5=$AUTH_MD5"
 
 # Debug spew, checking if the authorization token is correct
-git ls-remote https://github.com/xamarin/maccore || true
-git ls-remote https://github.com/xamarin/xamarin-macios-data || true
+#git ls-remote https://github.com/xamarin/maccore || true
+#git ls-remote https://github.com/xamarin/xamarin-macios-data || true
 
-pushd .
-cd ../maccore
-git fetch
-popd
+#pushd .
+#cd ../maccore
+#git fetch
+#popd
 
 
-git clone https://github.com/xamarin/$DATA_REPO_NAME
+#git clone https://github.com/xamarin/$DATA_REPO_NAME
 
 DIR=$DATA_REPO_NAME/perf-data/samples/$BUILD_SOURCEBRANCHNAME/$BUILD_SOURCEVERSION/$SYSTEM_JOBID
 mkdir -p "$DIR"
 
-cp -c tests/sampletester/bin/Debug/tmp-test-dir/execution-logs/*.xml "$DIR/"
+cp -c xamarin-macios/tests/sampletester/bin/Debug/tmp-test-dir/execution-logs/*.xml "$DIR/"
 cd "$DIR"
 git add .
 git commit -m "Add performance data for $BUILD_SOURCEBRANCHNAME/$BUILD_SOURCEVERSION from the $SYSTEM_JOBNAME job."
