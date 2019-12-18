5783e35c36bedd947fc11577ec72b39041ef9d75
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-12-18 12:33:38 +0100

    duh

--- tools/devops/fetch-maccore.sh
+++ tools/devops/fetch-maccore.sh
@@ -5,7 +5,7 @@
 # use https for the repository (instead of git@), since GitHub auth on Azure
 # Devops only works with https.
 
-cd "cd $(git rev-parse --show-toplevel)/../maccore"
+cd "$(git rev-parse --show-toplevel)/../maccore"
 git remote add -f xamarin https://github.com/xamarin/maccore
 cd ../xamarin-macios
 
