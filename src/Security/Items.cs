7b1049cf24f2bc39f5c50faa148225e4857941bb
Marius Ungureanu <maungu@microsoft.com>
2019-12-18 18:28:23 +0200

    Wrap some objects in using statements (#7560)
    * Wrap some objects in using statements
    
    * Switch to using statement.

--- src/Security/Items.cs
+++ src/Security/Items.cs
@@ -147,7 +147,7 @@ namespace Security {
 					if (max == 1)
 						return new NSData [] { new NSData (ptr, false) };
 
-					var array = new NSArray (ptr);
+					using var array = new NSArray (ptr);
 					var records = new NSData [array.Count];
 					for (uint i = 0; i < records.Length; i++)
 						records [i] = new NSData (array.ValueAt (i), false);
@@ -200,7 +200,7 @@ namespace Security {
 				result = SecItem.SecItemCopyMatching (copy.Handle, out ptr);
 				n = null;
 				if (result == SecStatusCode.Success){
-					var array = new NSArray (ptr);
+					using var array = new NSArray (ptr);
 					var records = new SecRecord [array.Count];
 					for (uint i = 0; i < records.Length; i++)
 						records [i] = new SecRecord (new NSMutableDictionary (array.ValueAt (i), false));
