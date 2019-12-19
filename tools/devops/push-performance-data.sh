1b847a12bb3bf3c005db18236dd423d461c20e55
Rolf Bjarne Kvinge <rolf@xamarin.com>
2019-12-19 08:13:15 +0100

    Merge xml files

--- tools/devops/push-performance-data.sh
+++ tools/devops/push-performance-data.sh
@@ -8,11 +8,22 @@ mv ./*/*.zip .
 for zip in ./*.zip; do
 	unzip "$zip"
 done
-rm -f ./*.zip
 
-git add .
+# Merge each individual xml file into one big xml file
+DIR=perf-data/samples/$BUILD_SOURCEBRANCHNAME/$BUILD_SOURCEVERSION
+cd "$DIR"
+{
+	echo '<?xml version="1.0" encoding="utf-8" standalone="yes"?>'
+	echo '<performance>'
+	find . -name '*perfdata*.xml' -print0 | xargs -0 -n 1 tail -n +2 | grep -F -v -e '<performance>' -e '</performance>'
+	echo '</performance>'
+} > data.xml
+
+# Add the big xml file to git
+git add data.xml
 git commit -m "Add performance data for $BUILD_SOURCEBRANCHNAME/$BUILD_SOURCEVERSION."
 
+# Push!
 # Try to push 5 times, just in case someone else pushed first.
 COUNTER=5
 while [[ $COUNTER -gt 0 ]]; do
