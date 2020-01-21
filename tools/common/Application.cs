9aa135f39190a685d863134ddac95a07950d0e90
Manuel de la Pena <mandel@microsoft.com>
2020-01-21 16:37:03 -0500

    Address reviews.
    * Undoo changes in the plist.
    * Add new variables to track the target version.
    * Add method to get the target version.
    * Modify mtouch to check agains the target framework rather than the
    SDK.
    
    This will allow to keep track of three independent things:
    
    1. The SDK max version.
    2. The Simulator max version.
    3. The target version of the device.
    
    This had to be added becuase 13.2 has targets to 13.2 but simulators for
    13.3

--- tools/common/Application.cs
+++ tools/common/Application.cs
@@ -330,7 +330,7 @@ namespace Xamarin.Bundler {
 			if (DeploymentTarget != null) {
 				if (DeploymentTarget < Xamarin.SdkVersions.GetMinVersion (Platform))
 					throw new PlatformException (73, true, "{4} {0} does not support a deployment target of {1} for {3} (the minimum is {2}). Please select a newer deployment target in your project's Info.plist.", Constants.Version, DeploymentTarget, Xamarin.SdkVersions.GetMinVersion (Platform), PlatformName, ProductName);
-				if (DeploymentTarget > Xamarin.SdkVersions.GetVersion (Platform))
+				if (DeploymentTarget > Xamarin.SdkVersions.GetTargetVersion (Platform))
 					throw new PlatformException (74, true, "{4} {0} does not support a deployment target of {1} for {3} (the maximum is {2}). Please select an older deployment target in your project's Info.plist or upgrade to a newer version of {4}.", Constants.Version, DeploymentTarget, Xamarin.SdkVersions.GetVersion (Platform), PlatformName, ProductName);
 			}
 
