197557078fcfece834e80e5072a986dc90a8eba6
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-01-24 08:53:13 +0100

    [msbuild] Don't manually quote, instead rely on CommandLineArgumentBuilder's logic. (#7749)
    This fixes an issue related to single quotes and that the fact that mono's behavior
    regarding quoting them has changed (by not using single quotes).
    
    This becomes an issue when running with dotnet.

--- msbuild/Xamarin.iOS.Tasks.Core/Tasks/CodesignVerifyTaskBase.cs
+++ msbuild/Xamarin.iOS.Tasks.Core/Tasks/CodesignVerifyTaskBase.cs
@@ -10,7 +10,7 @@ namespace Xamarin.iOS.Tasks
 
 			args.Add ("--verify");
 			args.Add ("-vvvv");
-			args.Add ("-R='anchor apple generic and certificate 1[field.1.2.840.113635.100.6.2.1] exists and (certificate leaf[field.1.2.840.113635.100.6.1.2] exists or certificate leaf[field.1.2.840.113635.100.6.1.4] exists)'");
+			args.AddQuoted ("-R=anchor apple generic and certificate 1[field.1.2.840.113635.100.6.2.1] exists and (certificate leaf[field.1.2.840.113635.100.6.1.2] exists or certificate leaf[field.1.2.840.113635.100.6.1.4] exists)");
 
 			args.AddQuoted (Resource);
 
