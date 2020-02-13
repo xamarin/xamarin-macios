dc63adf39fa042da5500430594a6c95c1f2fff6f
Manuel de la Pena <mandel@microsoft.com>
2020-02-13 13:02:44 -0500

    [VSTS] Remove the some hardcoded vars. (#7883)
    Move to use a template for the DDFun pipeline. Copy the current pipeline
    to a template, set some parameters and recreate the pipeline importing
    the template.

--- tools/devops/run-tests.sh
+++ tools/devops/run-tests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -ex
 
-export TESTS_EXTRA_ARGUMENTS="--label=run-ios-64-tests"
-
 make -C xamarin-macios/builds download -j
 make -C xamarin-macios/builds .stamp-mono-ios-sdk-destdir -j
 make -C xamarin-macios/tests vsts-device-tests
