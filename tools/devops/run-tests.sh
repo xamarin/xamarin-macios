101a980641c1efd9d11b96d924263adaa999f62f
Manuel de la Pena <mandel@microsoft.com>
2020-02-18 09:15:18 -0500

    [VSTS] Correct order of params for make. (#7927)

--- tools/devops/run-tests.sh
+++ tools/devops/run-tests.sh
@@ -6,6 +6,6 @@ if [[ "$SYSTEM_DEBUG" == "true" ]]; then
 else
   DEBUG=""
 fi
-make -C $DEBUG xamarin-macios/builds download -j
-make -C $DEBUG xamarin-macios/builds .stamp-mono-ios-sdk-destdir -j
-make -C $DEBUG xamarin-macios/tests vsts-device-tests
+make $DEBUG -C xamarin-macios/builds download -j
+make $DEBUG -C xamarin-macios/builds .stamp-mono-ios-sdk-destdir -j
+make $DEBUG -C xamarin-macios/tests vsts-device-tests
