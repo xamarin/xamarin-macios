1e193656afe5e7eee8d4bcfd6b1d5ad0a51795e9
Whitney Schmidt <whschm@microsoft.com>
2020-02-25 18:28:54 -0500

    try out messy pwsh

---
+++ tools/devops/templates/publish_results.ps1
@@ -0,0 +1,62 @@
+#!/bin/bash -eux
+
+## don't need context here b/c we are combining all device tests into one post?
+#$json_payload = @"{"token": $TOKEN, "hash":$BUILD_REVISION "state": $GH_STATE, "target-url": $TARGET_URL, "description": $DESCRIPTION, "context": "VSTS: device tests $DEVICE_TYPE"}"
+
+# add real device type here
+# add description back in
+$json_payload = @"
+{
+    "hash" : $BUILD_REVISION,
+    "state" : $GH_STATE,
+    "target-url" : $TARGET_URL,
+    "description" : "description placeholder", 
+    "context" : "VSTS: device tests"
+}
+"@
+
+
+$url = "https://api.github.com/repos/xamarin/xamarin-macios/statuses/$BUILD_REVISION"
+
+$params = @{
+    Uri = $url
+    Headers = @{"Authorization: token $TOKEN"}
+    Method = 'POST'
+    Body = $json_payload
+    ContentType = 'application/json'
+}
+
+$response = Invoke-RestMethod @params
+
+# -Uri $url
+# -Method Post
+# -Body $json_payload
+#$response = Invoke-RestMethod -Uri $url -ContentType application/json  -Method Post -Body @json_payload
+<# 
+(
+	printf '{\n'
+	printf "\t\"state\": \"%s\",\n" "$STATE"
+	printf "\t\"target_url\": \"%s\",\n" "$TARGET_URL"
+	printf "\t\"description\": %s,\n" "$(echo -n "$DESCRIPTION" | python -c 'import json,sys; print(json.dumps(sys.stdin.read()))')"
+	printf "\t\"context\": \"%s\"\n" "$CONTEXT"
+	printf "}\n"
+) > "$JSONFILE"
+
+if test -n "$VERBOSE"; then
+	echo "JSON file:"
+	sed 's/^/    /' "$JSONFILE";
+fi
+
+if ! curl -f -v -H "Authorization: token $TOKEN" -H "User-Agent: command line tool" -d "@$JSONFILE" "https://api.github.com/repos/xamarin/xamarin-macios/statuses/$HASH" > "$LOGFILE" 2>&1; then
+	echo "Failed to add status."
+	echo "curl output:"
+	sed 's/^/    /' "$LOGFILE"
+	echo "Json body:"
+	sed 's/^/    /' "$JSONFILE"
+	exit 1 #>
+
+# 	printf "\t\"state\": \"%s\",\n" "$STATE"
+#	printf "\t\"target_url\": \"%s\",\n" "$TARGET_URL"
+#	printf "\t\"description\": %s,\n" "$(echo -n "$DESCRIPTION" | python -c 'import json,sys; print(json.dumps(sys.stdin.read()))')"
+#	printf "\t\"context\": \"%s\"\n" "$CONTEXT"
+# ./jenkins/add-commit-status.sh --token="$TOKEN" --hash="$BUILD_REVISION" --state="$GH_STATE" --target-url="$VSTS_BUILD_URL" --description="$DESCRIPTION" --context="VSTS: device tests ($DEVICE_TYPE)"
