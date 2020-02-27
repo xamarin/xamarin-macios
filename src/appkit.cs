53f9734bbd84959cb1a4279ae02091026d97f038
Chris Hamons <chris.hamons@xamarin.com>
2020-02-27 15:49:27 -0600

    [AppKit] Allow NSStringDrawing on non-UI threads
    - https://github.com/xamarin/xamarin-macios/issues/7986

--- src/appkit.cs
+++ src/appkit.cs
@@ -9812,6 +9812,7 @@ namespace AppKit {
 
 	}
 
+	[ThreadSafe]
 	[Mac (10,11)]
 	[BaseType (typeof(NSObject))]
 	interface NSStringDrawingContext
@@ -9848,6 +9849,7 @@ namespace AppKit {
 		void DrawInRect (CGRect rect, AppKit.NSStringAttributes attributes);
 	}
 
+	[ThreadSafe]
 	[Category, BaseType (typeof (NSAttributedString))]
 	interface NSStringDrawing_NSAttributedString {
 		[Export ("size")]
@@ -9860,6 +9862,7 @@ namespace AppKit {
 		void DrawInRect (CGRect rect);
 	}
 		
+	[ThreadSafe]
 	[Category, BaseType (typeof (NSString))]
 	interface NSExtendedStringDrawing {
 		[Mac (10,11)]
@@ -9880,6 +9883,7 @@ namespace AppKit {
 	}
 
 	// @interface NSExtendedStringDrawing (NSAttributedString)
+	[ThreadSafe]
 	[Category]
 	[BaseType (typeof(NSAttributedString))]
 	interface NSAttributedString_NSExtendedStringDrawing
