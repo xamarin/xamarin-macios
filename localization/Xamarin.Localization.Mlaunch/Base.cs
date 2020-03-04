f35b8b71d4aaf99e57aa3b04dc974f9ff694f37b
Waleed Chaudhry <wachaudh@microsoft.com>
2020-03-04 12:10:10 -0500

    Rework mlaunch localization

---
+++ localization/Xamarin.Localization.Mlaunch/Base.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Xamarin.Hosting")]
+[assembly: InternalsVisibleTo("mlaunch")]
+namespace Xamarin.Localization.Mlaunch
+{
+	public class Base
+	{
+		public Base()
+		{
+		}
+	}
+}
