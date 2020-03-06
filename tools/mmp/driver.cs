a8f38e3696cd79ef4918c62e41908ace30451dd3
Alex Soto <alex@alexsoto.me>
2020-03-05 19:48:28 -0500

    [AutomaticAssessmentConfiguration] Update bindings to Xcode 11.4 Beta 3 (#8049)
    * [AutomaticAssessmentConfiguration] Update bindings to Xcode 11.4 Beta 3

--- tools/mmp/driver.cs
+++ tools/mmp/driver.cs
@@ -587,6 +587,8 @@ namespace Xamarin.Bundler {
 		static Version MutateSDKVersionToPointRelease (Version rv)
 		{
 			if (rv.Major == 10 && (rv.Revision == 0 || rv.Revision == -1)) {
+				if (rv.Minor == 15 && XcodeVersion >= new Version (11, 4))
+					return new Version (rv.Major, rv.Minor, 4);
 				if (rv.Minor == 13 && XcodeVersion >= new Version (9, 3))
 					return new Version (rv.Major, rv.Minor, 4);
 				if (rv.Minor == 13 && XcodeVersion >= new Version (9, 2))
