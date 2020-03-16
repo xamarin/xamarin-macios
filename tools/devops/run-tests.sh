fb5aca88e9a3078cf669bc9bf39e3cdf3405f5a2
Whitney Schmidt <whschm@microsoft.com>
2020-03-16 16:05:36 -0400

    try out fix for run-tests.sh

--- tools/devops/run-tests.sh
+++ tools/devops/run-tests.sh
@@ -2,13 +2,13 @@
 
 ls -l
 pwd
-cd xamarin-macios
+ls -l xamarin-macios/
 ls -l
 if [[ "$SYSTEM_DEBUG" == "true" ]]; then
   DEBUG="-d"
 else
   DEBUG=""
 fi
-make -C $DEBUG xamarin-macios/builds download -j
-make -C $DEBUG xamarin-macios/builds .stamp-mono-ios-sdk-destdir -j
-make -C $DEBUG xamarin-macios/tests vsts-device-tests
+make $DEBUG -C xamarin-macios/builds download -j
+make $DEBUG -C xamarin-macios/builds .stamp-mono-ios-sdk-destdir -j
+make $DEBUG -C xamarin-macios/tests vsts-device-tests
