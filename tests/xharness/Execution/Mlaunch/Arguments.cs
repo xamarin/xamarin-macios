c407f393bca2e1263f0d19f1c220e0a94e48cfa8
Premek Vysoky <prvysoky@microsoft.com>
2020-03-17 14:44:19 +0100

    Add MlaunchArguments docs

--- tests/xharness/Execution/Mlaunch/Arguments.cs
+++ tests/xharness/Execution/Mlaunch/Arguments.cs
@@ -1,47 +1,90 @@
 ﻿namespace Xharness.Execution.Mlaunch {
 
+	/// <summary>
+	/// Specify the location of Apple SDKs, default to 'xcode-select' value.
+	/// </summary>
 	public sealed class SdkRootArgument : SingleValueArgument {
 		public SdkRootArgument (string sdkPath) : base ("sdkroot", sdkPath)
 		{
 		}
 	}
 
+	/// <summary>
+	/// List the currently connected devices and their UDIDs.
+	/// </summary>
 	public sealed class ListDevicesArgument : SingleValueArgument {
 		public ListDevicesArgument (string outputFile) : base ("listdev", outputFile)
 		{
 		}
 	}
 
+	/// <summary>
+	/// List the available simulators. The output is xml, and written to the specified file.
+	/// </summary>
 	public sealed class ListSimulatorsArgument : SingleValueArgument {
 		public ListSimulatorsArgument (string outputFile) : base ("listsim", outputFile)
 		{
 		}
 	}
 
+	/// <summary>
+	/// Lists crash reports on the specified device
+	/// </summary>
 	public sealed class ListCrashReportsArgument : SingleValueArgument {
 		public ListCrashReportsArgument (string outputFile) : base ("list-crash-reports", outputFile)
 		{
 		}
 	}
 
+	/// <summary>
+	/// Specify which device (when many are present) the [install|lauch|kill|log]dev command applies
+	/// </summary>
 	public sealed class DeviceNameArgument : SingleValueArgument {
 		public DeviceNameArgument (string deviceName) : base ("devname", deviceName)
 		{
 		}
 	}
-
+	
+	/// <summary>
+	/// Specify the output format for some commands as Default.
+	/// </summary>
 	public sealed class DefaultOutputFormatArgument : SingleValueArgument {
 		public DefaultOutputFormatArgument () : base ("output-format", "Default")
 		{
 		}
 	}
 
+	/// <summary>
+	/// Specify the output format for some commands as XML.
+	/// </summary>
 	public sealed class XmlOutputFormatArgument : SingleValueArgument {
 		public XmlOutputFormatArgument () : base ("output-format", "XML")
 		{
 		}
 	}
 
+	/// <summary>
+	/// Download a crash report from the specified device.
+	/// </summary>
+	public sealed class DownloadCrashReportArgument : SingleValueArgument {
+		public DownloadCrashReportArgument (string deviceName) : base ("download-crash-report", deviceName)
+		{
+		}
+	}
+
+	/// <summary>
+	/// Specifies the file to save the downloaded crash report.
+	/// </summary>
+	public sealed class DownloadCrashReportToArgument : SingleValueArgument {
+		public DownloadCrashReportToArgument (string outputFile) : base ("download-crash-report-to", outputFile)
+		{
+		}
+	}
+
+	/// <summary>
+	/// Include additional data (which can take some time to fetch) when listing the connected devices.
+	/// Only applicable when output format is xml.
+	/// </summary>
 	public sealed class ListExtraDataArgument : OptionArgument {
 		public ListExtraDataArgument () : base ("list-extra-data")
 		{
