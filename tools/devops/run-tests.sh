1a0cde12685cf474fd16f17e4f4c12c3d996cf81
Whitney Schmidt <whschm@microsoft.com>
2020-03-17 17:38:17 -0400

    add GH status for failures, set tests to always fail:

--- tools/devops/run-tests.sh
+++ tools/devops/run-tests.sh
@@ -12,15 +12,6 @@ fi
 #make $DEBUG -C xamarin-macios/builds download -j
 #make $DEBUG -C xamarin-macios/builds .stamp-mono-ios-sdk-destdir -j
 #make $DEBUG -C xamarin-macios/tests vsts-device-tests
-echo "exit 0" > bar.sh
+echo "exit 1" > bar.sh
 chmod a+x bar.sh
 ./bar.sh
-# if [ $? -eq 0 ]; then
-
-# publish to GH that there's a failure
-#   echo "##vso[task.setvariable variable=TestState]success"
-#   echo "hello"
-# else
-#   echo "##vso[task.setvariable variable=TestState]failure"
-# fi
-# echo "$(TestState)"
