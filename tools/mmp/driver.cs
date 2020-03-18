xamarin/xamarin-macios

commit 13a56ffb956f21f3284c6fa5728d23e5949b96cd

[mtouch/mmp] Rework how we find developer tools. Partial fix for #4634 and fixes #8005. (#8121)
Partial fix for https://github.com/xamarin/xamarin-macios/issues/4634.
Fixes https://github.com/xamarin/xamarin-macios/issues/8005.

tools/mmp/driver.cs
@@ -758,17 +758,9 @@ namespace Xamarin.Bundler {
 			Watch ("Copy Dependencies", 1);
 
 			// MDK check
-			var ret = Compile ();
+			Compile ();
 			Watch ("Compile", 1);
-			if (ret != 0) {
-				if (ret == 1)
-					throw new MonoMacException (5109, true, Errors.MM5109);
-				if (ret == 69)
-					throw new MonoMacException (5308, true, Errors.MM5308);
-				// if not then the compilation really failed
-				throw new MonoMacException (5103, true, Errors.MM5103, ret);
-			}
-
+			
 			if (generate_plist)
 				GeneratePList ();
 
@@ -1032,10 +1024,8 @@ namespace Xamarin.Bundler {
 				throw ErrorHelper.CreateError (1, Errors.MM0001, MonoVersions.MinimumMonoVersion, mono_version);
 		}
 
-		static int Compile ()
+		static void Compile ()
 		{
-			int ret = 1;
-
 			string [] cflags = Array.Empty<string> ();
 
 			string mainSource = GenerateMain ();
@@ -1270,13 +1260,10 @@ namespace Xamarin.Bundler {
 				sourceFiles.Add (main);
 				args.AddRange (sourceFiles);
 
-
-				ret = XcodeRun ("clang", args, null);
+				RunClang (args);
 			} catch (Win32Exception e) {
 				throw new MonoMacException (5103, true, e, Errors.MM5103, "driver");
 			}
-			
-			return ret;
 		}
 
 		static string RunPkgConfig (string option, bool force_system_mono = false)
@@ -1425,9 +1412,7 @@ namespace Xamarin.Bundler {
 					string libName = Path.GetFileName (linkWith);
 					string finalLibPath = Path.Combine (mmp_dir, libName);
 					Application.UpdateFile (linkWith, finalLibPath);
-					int ret = XcodeRun ("install_name_tool", new [] { "-id", "@executable_path/../" + BundleName + "/" + libName, finalLibPath });
-					if (ret != 0)
-						throw new MonoMacException (5310, true, Errors.MM5310, ret);
+					RunInstallNameTool (new [] { "-id", "@executable_path/../" + BundleName + "/" + libName, finalLibPath });
 					native_libraries_copied_in.Add (libName);
 				}
 			}
@@ -1455,10 +1440,7 @@ namespace Xamarin.Bundler {
 				}
 				// if required update the paths inside the .dylib that was copied
 				if (sb.Count > 0) {
-					sb.Add (library);
-					int ret = XcodeRun ("install_name_tool", sb);
-					if (ret != 0)
-						throw new MonoMacException (5310, true, Errors.MM5310, ret);
+					RunInstallNameTool (sb);
 					sb.Clear ();
 				}
 			}
@@ -1595,11 +1577,8 @@ namespace Xamarin.Bundler {
 				LipoLibrary (name, dest);
 
 			if (native_references.Contains (real_src)) {
-				if (!isStaticLib) {
-					int ret = XcodeRun ("install_name_tool", new [] { "-id", "@executable_path/../" + BundleName + "/" + name, dest });
-					if (ret != 0)
-						throw new MonoMacException (5310, true, Errors.MM5310, ret);
-				}
+				if (!isStaticLib)
+					RunInstallNameTool (new [] { "-id", "@executable_path/../" + BundleName + "/" + name, dest });
 				native_libraries_copied_in.Add (name);
 			}
 
@@ -1631,9 +1610,7 @@ namespace Xamarin.Bundler {
 			if (existingArchs.Count () < 2)
 				return;
 
-			int ret = XcodeRun ("lipo", new [] { dest, "-thin", arch, "-output", dest });
-			if (ret != 0)
-				throw new MonoMacException (5311, true, Errors.MM5311, ret);
+			RunLipo (new [] { dest, "-thin", arch, "-output", dest });
 			if (name != "MonoPosixHelper" && name != "libmono-native-unified" && name != "libmono-native-compat")
 				ErrorHelper.Warning (2108, Errors.MM2108, name, arch);
 		}

tests/mmptest/src/MMPTest.cs
@@ -26,7 +26,7 @@ namespace Xamarin.MMP.Tests
 			TI.UnifiedTestConfig test = new TI.UnifiedTestConfig (tmpDir) { CSProjConfig = projectConfig };
 			string buildOutput = TI.TestUnifiedExecutable (test).BuildOutput;
 			string [] splitBuildOutput = TI.TestUnifiedExecutable (test).BuildOutput.Split (new string[] { Environment.NewLine }, StringSplitOptions.None);
-			string clangInvocation = splitBuildOutput.Single (x => x.Contains ("clang"));
+			string clangInvocation = splitBuildOutput.Single (x => x.Contains ("usr/bin/clang"));
 			return clangInvocation.Split (new string[] { " " }, StringSplitOptions.None);
 		}
 

tests/mmptest/src/NativeReferencesTests.cs
@@ -112,7 +112,7 @@ namespace Xamarin.MMP.Tests
 			MMPTests.RunMMPTest (tmpDir => {
 				TI.UnifiedTestConfig test = new TI.UnifiedTestConfig (tmpDir) { ItemGroup = CreateSingleNativeRef (SimpleStaticPath, "Static") };
 				NativeReferenceTestCore (tmpDir, test, "Unified_WithNativeReferences_InMainProjectWorks - Static", null, true, false, s => {
-					string clangLine = s.Split ('\n').First (x => x.Contains ("xcrun -sdk macosx clang"));
+					string clangLine = s.Split ('\n').First (x => x.Contains ("usr/bin/clang"));
 					return clangLine.Contains ("SimpleClassStatic.a");
 				});
 			});

tools/common/Driver.cs
@@ -754,28 +754,6 @@ namespace Xamarin.Bundler {
 			Driver.Log (1, "Using Xcode {0} ({2}) found in {1}", XcodeVersion, sdk_root, XcodeProductVersion);
 		}
 
-		public static int XcodeRun (string command, params string [] arguments)
-		{
-			return XcodeRun (command, (IList<string>) arguments, null);
-		}
-
-		public static int XcodeRun (string command, IList<string> arguments, StringBuilder output = null)
-		{
-			string [] env = DeveloperDirectory != String.Empty ? new string [] { "DEVELOPER_DIR", DeveloperDirectory } : null;
-			var args = new List<string> ();
-			args.Add ("-sdk");
-			args.Add ("macosx");
-			args.Add (command);
-			args.AddRange (arguments);
-			int ret = RunCommand ("xcrun", args, env, output);
-			if (ret != 0 && Verbosity > 1) {
-				StringBuilder debug = new StringBuilder ();
-				RunCommand ("xcrun", new [] { "--find", command }, env, debug);
-				Console.WriteLine ("failed using `{0}` from: {1}", command, debug);
-			}
-			return ret;
-		}
-
 		internal static bool TryParseBool (string value, out bool result)
 		{
 			if (string.IsNullOrEmpty (value)) {
@@ -809,5 +787,162 @@ namespace Xamarin.Bundler {
 			return result;
 		}
 
+		static readonly Dictionary<string, string> tools = new Dictionary<string, string> ();
+		static string FindTool (string tool)
+		{
+			string path;
+
+			lock (tools) {
+				if (tools.TryGetValue (tool, out path))
+					return path;
+			}
+
+			path = LocateTool (tool);
+			static string LocateTool (string tool)
+			{
+				if (XcrunFind (tool, out var path))
+					return path;
+
+				// either /Developer (Xcode 4.2 and earlier), /Applications/Xcode.app/Contents/Developer (Xcode 4.3) or user override
+				path = Path.Combine (DeveloperDirectory, "usr", "bin", tool);
+				if (File.Exists (path))
+					return path;
+
+				// Xcode 4.3 (without command-line tools) also has a copy of 'strip'
+				path = Path.Combine (DeveloperDirectory, "Toolchains", "XcodeDefault.xctoolchain", "usr", "bin", tool);
+				if (File.Exists (path))
+					return path;
+
+				// Xcode "Command-Line Tools" install a copy in /usr/bin (and it can be there afterward)
+				path = Path.Combine ("/usr", "bin", tool);
+				if (File.Exists (path))
+					return path;
+
+				return null;
+			}
+
+			// We can end up finding the same tool multiple times.
+			// That's not a problem.
+			lock (tools)
+				tools [tool] = path;
+
+			if (path == null)
+				throw ErrorHelper.CreateError (5307, Errors.MX5307 /* Missing '{0}' tool. Please install Xcode 'Command-Line Tools' component */, tool);
+
+			return path;
+		}
+
+		static bool XcrunFind (string tool, out string path)
+		{
+			return XcrunFind (ApplePlatform.None, false, tool, out path);
+		}
+
+		static bool XcrunFind (ApplePlatform platform, bool is_simulator, string tool, out string path)
+		{
+			var env = new List<string> ();
+			// Unset XCODE_DEVELOPER_DIR_PATH. See https://github.com/xamarin/xamarin-macios/issues/3931.
+			env.Add ("XCODE_DEVELOPER_DIR_PATH");
+			env.Add (null);
+			// Set DEVELOPER_DIR if we have it
+			if (!string.IsNullOrEmpty (DeveloperDirectory)) {
+				env.Add ("DEVELOPER_DIR");
+				env.Add (DeveloperDirectory);
+			}
+
+			path = null;
+
+			var args = new List<string> ();
+			if (platform != ApplePlatform.None) {
+				args.Add ("-sdk");
+				switch (platform) {
+				case ApplePlatform.iOS:
+					args.Add (is_simulator ? "iphonesimulator" : "iphoneos");
+					break;
+				case ApplePlatform.MacOSX:
+					args.Add ("macosx");
+					break;
+				case ApplePlatform.TVOS:
+					args.Add (is_simulator ? "appletvsimulator" : "appletvos");
+					break;
+				case ApplePlatform.WatchOS:
+					args.Add (is_simulator ? "watchsimulator" : "watchos");
+					break;
+				default:
+					throw ErrorHelper.CreateError (71, Errors.MX0071 /* Unknown platform: {0}. This usually indicates a bug in {1}; please file a bug report at https://github.com/xamarin/xamarin-macios/issues/new with a test case. */, platform.ToString (), PRODUCT);
+				}
+			}
+			args.Add ("-f");
+			args.Add (tool);
+
+			var output = new StringBuilder ();
+			int ret = RunCommand ("xcrun", args, env.ToArray (), output);
+
+			if (ret == 0) {
+				path = output.ToString ().Trim ();
+			} else {
+				Log (1, "Failed to locate the developer tool '{0}', 'xcrun {1}' returned with the exit code {2}:\n{3}", tool, string.Join (" ", args), ret, output.ToString ());
+			}
+
+			return ret == 0;
+		}
+
+		public static void RunXcodeTool (string tool, params string[] arguments)
+		{
+			RunXcodeTool (tool, (IList<string>) arguments);
+		}
+
+		public static void RunXcodeTool (string tool, IList<string> arguments)
+		{
+			var executable = FindTool (tool);
+			var rv = RunCommand (executable, arguments);
+			if (rv != 0)
+				throw ErrorHelper.CreateError (5309, Errors.MX5309 /* Failed to execute the tool '{0}', it failed with an error code '{1}'. Please check the build log for details. */, tool, rv);
+		}
+
+		public static void RunClang (IList<string> arguments)
+		{
+			RunXcodeTool ("clang", arguments);
+		}
+
+		public static void RunInstallNameTool (IList<string> arguments)
+		{
+			RunXcodeTool ("install_name_tool", arguments);
+		}
+
+		public static void RunBitcodeStrip (IList<string> arguments)
+		{
+			RunXcodeTool ("bitcode_strip", arguments);
+		}
+
+		public static void RunLipo (string output, IEnumerable<string> inputs)
+		{
+			var sb = new List<string> ();
+			sb.AddRange (inputs);
+			sb.Add ("-create");
+			sb.Add ("-output");
+			sb.Add (output);
+			RunLipo (sb);
+		}
+
+		public static void RunLipo (IList<string> options)
+		{
+			RunXcodeTool ("lipo", options);
+		}
+
+		public static void CreateDsym (string output_dir, string appname, string dsym_dir)
+		{
+			RunDsymUtil (Path.Combine (output_dir, appname), "-num-threads", "4", "-z", "-o", dsym_dir);
+			RunCommand ("/usr/bin/mdimport", dsym_dir);
+		}
+
+		public static void RunDsymUtil (params string [] options)
+		{
+			RunXcodeTool ("dsymutil", options);
+		}
+
+		public static void RunStrip (IList<string> options)
+		{
+			RunXcodeTool ("strip", options);
+		}
 	}
 }

tools/mmp/Application.cs
@@ -9,6 +9,7 @@ namespace Xamarin.Bundler {
 		public bool Is32Build => false;
 		public bool Is64Build => true;
 		public bool IsDualBuild => false;
+		public bool IsSimulatorBuild => false;
 
 		bool RequiresXcodeHeaders => Driver.Registrar == RegistrarMode.Static && LinkMode == LinkMode.None;
 

tools/mmp/mmp.csproj
@@ -9,6 +9,7 @@
     <AssemblyName>mmp</AssemblyName>
     <RootNamespace>mmp</RootNamespace>
     <TargetFrameworkVersion>v4.6</TargetFrameworkVersion>
+    <LangVersion>8.0</LangVersion>
   </PropertyGroup>
   <PropertyGroup Condition=" '$(Configuration)|$(Platform)' == 'Debug|AnyCPU' ">
     <DebugSymbols>True</DebugSymbols>

tools/mtouch/Application.cs
@@ -1811,7 +1811,7 @@ namespace Xamarin.Bundler {
 							Driver.RunLipo (targetPath, files);
 						}
 						if (LibMonoLinkMode == AssemblyBuildTarget.Framework)
-							Driver.XcodeRun ("install_name_tool", "-change", "@rpath/libmonosgen-2.0.dylib", "@rpath/Mono.framework/Mono", targetPath);
+							Driver.RunInstallNameTool (new [] { "-change", "@rpath/libmonosgen-2.0.dylib", "@rpath/Mono.framework/Mono", targetPath });
 
 						// Remove architectures we don't care about.
 						if (IsDeviceBuild)
@@ -1863,7 +1863,7 @@ namespace Xamarin.Bundler {
 			}
 			sb.Add ("-o");
 			sb.Add (macho_file);
-			Driver.XcodeRun ("bitcode_strip", sb);
+			Driver.RunBitcodeStrip (sb);
 		}
 
 		// Returns true if is up-to-date

tools/mtouch/Target.cs
@@ -1716,7 +1716,7 @@ namespace Xamarin.Bundler
 			}
 			if (sb.Count > 0) {
 				sb.Add (output);
-				Driver.XcodeRun ("install_name_tool", sb);
+				Driver.RunInstallNameTool (sb);
 				sb.Clear ();
 			}
 		}

commit 79fb2485176ec52be197e1315102a5f4efef3fd9

[mmp] Fix install_name_tool invocation.
This regressed in 13a56ffb956f21f3284c6fa5728d23e5949b96cd.

tools/mmp/driver.cs
@@ -1440,6 +1440,7 @@ namespace Xamarin.Bundler {
 				}
 				// if required update the paths inside the .dylib that was copied
 				if (sb.Count > 0) {
+					sb.Add (library);
 					RunInstallNameTool (sb);
 					sb.Clear ();
 				}