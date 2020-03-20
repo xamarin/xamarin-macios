a6cabe20f3e92d5fc6d8e16a51710cabf32fb60a
Whitney Schmidt <whschm@microsoft.com>
2020-03-20 11:58:29 -0400

    fix error in publish_failure

--- tools/devops/templates/publish_failure.ps1
+++ tools/devops/templates/publish_failure.ps1
@@ -1,3 +1,4 @@
+# When this runs, a test failure has occurred
 
 Set-Location Env:
 Get-ChildItem
@@ -52,3 +53,91 @@ $response | ConvertTo-Json | Write-Host
 ) > "$JSONFILE"
 
 if test -n "$VERBOSE"; then
+
+
+
+# Add a GitHub commit comment
+
+# Query job status (always failure) and set result emoji
+GH_STATE=failure
+DESCRIPTION="Running device tests"
+RESULT_EMOJII=
+if test -n "$START"; then
+	GH_STATE=pending
+	DESCRIPTION="Running device tests on $DEVICE_TYPE"
+else
+	case "$(echo "$AGENT_JOBSTATUS" | tr '[:upper:]' '[:lower:]')" in
+		succeeded)
+			GH_STATE=success
+			DESCRIPTION="Device tests passed on $DEVICE_TYPE"
+			RESULT_EMOJII="✅ "
+			;;
+		failed | canceled | succeededwithissues | *)
+			GH_STATE=error
+			DESCRIPTION="Device tests completed ($AGENT_JOBSTATUS) on $DEVICE_TYPE"
+			RESULT_EMOJII="🔥 "
+			;;
+	esac
+fi
+
+
+
+# create a temp file to construct commit message
+# add cleanup code
+# add message header: 	if [[ $DEVICE_TYPE == *"DDFun"* ]]; then printf "### :construction: Experimental DDFun pipeline\\n" > "$MESSAGE_FILE"
+# add the following to the file: $DEVICE_TYPE + emoji + $DESCRIPTION + $VSTS_BUILD_URL + $HTMLREPORT + RESULT_EMOJI
+# look for testsummary.md
+    # if not found
+        # add "🔥 Tests failed catastrophically on $DEVICE_TYPE  (no summary found)\\n"
+    # if found
+        # cat testsummary.md > messagefile
+if test -z "$START"; then
+	# When we're done, add a GitHub comment to the commit we're testing
+	MESSAGE_FILE=commit-message.txt
+	cleanup ()
+	{
+		rm -f "$MESSAGE_FILE"
+	}
+	trap cleanup ERR
+	trap cleanup EXIT
+
+	HTML_REPORT=""
+	if [[ $DEVICE_TYPE == *"DDFun"* ]]; then
+		printf "### :construction: Experimental DDFun pipeline\\n" > "$MESSAGE_FILE"
+	else
+		P=$(cat tmp.p)
+		HTML_REPORT=": [Html Report](http://xamarin-storage/${P}/jenkins-results/tests/index.html)"
+	fi
+
+	printf "%s%s on [Azure DevOps](%s)($DEVICE_TYPE)%s %s\\n\\n" "$RESULT_EMOJII" "$DESCRIPTION" "$VSTS_BUILD_URL" "$HTML_REPORT" "$RESULT_EMOJII" >> "$MESSAGE_FILE"
+
+	FILE=$PWD/TestSummary.md
+	if ! test -f "$FILE"; then
+		printf "🔥 Tests failed catastrophically on $DEVICE_TYPE  (no summary found)\\n" >> "$MESSAGE_FILE"
+	else
+		cat "$FILE" >> "$MESSAGE_FILE"
+	fi
+
+	./jenkins/add-commit-comment.sh --token="$TOKEN" --file="$MESSAGE_FILE" "--hash=$BUILD_REVISION"
+
+# construct a json payload and publish it to GH
+# url is probably something like:
+$comment_url = "https://api.github.com/repos/xamarin/xamarin-macios/commits/$Env:BUILD_REVISION/comments"
+
+# need to print out JSON payload from jenkins to do comparison
+
+
+    if ! curl -f -v -H "Authorization: token $TOKEN" -H "User-Agent: command line tool" -d "@$JSONFILE" "https://api.github.com/repos/xamarin/xamarin-macios/commits/$HASH/comments" > "$LOGFILE" 2>&1; then
+	echo "Failed to add commit message."
+	echo "curl output:"
+	sed 's/^/    /' "$LOGFILE"
+	echo "Json body:"
+	sed 's/^/    /' "$JSONFILE"
+	exit 1
+else
+	if test -n "$VERBOSE"; then sed 's/^/    /' "$LOGFILE"; fi
+	echo "Successfully added commit message to https://github.com/xamarin/xamarin-macios/commit/$HASH"
+fi
+
+fi
+#>
