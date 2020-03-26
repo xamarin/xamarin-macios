4a0fc9876dafa05f02c86ae92130d183291a0c06
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-03-26 12:05:59 +0100

    BuildFixes

--- msbuild/dotnet/legacy.mk
+++ msbuild/dotnet/legacy.mk
@@ -123,15 +123,14 @@ $(MAC_DESTDIR)$(MAC_FRAMEWORK_CURRENT_DIR)/lib/dotnet/Xamarin.macOS.Legacy.Sdk/t
 targets/Xamarin.Shared.Legacy.Sdk.Versions.props: targets/Xamarin.Shared.Sdk.Versions.props
 	$(Q) $(CP) $< $@
 
-legacy-pack: legacy-prepare
-	$(Q) $(MAKE) $(foreach var,iOS tvOS watchOS macOS,legacy-pack-$(var))
+legacy-pack: $(foreach var,iOS tvOS watchOS macOS,legacy-pack-$(var))
 
 # just a temporary target while debugging for faster turnaround
 legacy-pack-simple: legacy-prepare
 	$(Q) mkdir -p nupkgs
 	$(Q) $(MAKE) legacy-pack-iOS
 
-legacy-pack-%: $(TEMPLATED_FILES)
+legacy-pack-%: legacy-prepare $(TEMPLATED_FILES)
 	$(Q) rm -Rf $(TEST_FEED_PATH)/xamarin.$(shell echo $* | tr '[:upper:]' '[:lower:]').legacy.sdk
 	$(Q) rm -f nupkgs/Xamarin.$*.Legacy.Sdk.*.nupkg
 	$(if $(V),,@echo "PACK     nupkgs/Xamarin.$*.Legacy.Sdk.nupkg";) $(DOTNET) pack package/$*/Xamarin.$*.Legacy.csproj --output nupkgs $(DOTNET_PACK_VERBOSITY)
