8182bd1a9a971eb0078100fdb1c574030ec10b94
Přemek Vysoký <premek.vysoky@microsoft.com>
2020-03-27 11:34:20 +0100

    Update tests/xharness/Xharness.Tests/Hardware/Tests/DevicesTest.cs
    Co-Authored-By: Rolf Bjarne Kvinge <rolf@xamarin.com>

--- tests/xharness/Xharness.Tests/Hardware/Tests/DevicesTest.cs
+++ tests/xharness/Xharness.Tests/Hardware/Tests/DevicesTest.cs
@@ -99,7 +99,7 @@ namespace Xharness.Tests.Hardware.Tests {
 					
 					// we get the temp file that was passed as the args, and write our sample xml, which will be parsed to get the devices :)
 					var tempPath = args.Where (a => a is ListDevicesArgument).First ().AsCommandLineArgument ();
-					tempPath = tempPath.Substring(tempPath.IndexOf('=') + 1).Replace("\"", string.Empty);
+					tempPath = tempPath.Substring (tempPath.IndexOf('=') + 1).Replace ("\"", string.Empty);
 
 					var name = GetType ().Assembly.GetManifestResourceNames ().Where (a => a.EndsWith ("devices.xml", StringComparison.Ordinal)).FirstOrDefault ();
 					using (var outputStream = new StreamWriter (tempPath))
