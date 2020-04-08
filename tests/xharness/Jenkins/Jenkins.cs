efa34d8dc9a4e94e6b7900774f5d76d62d274bb5
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-04-08 16:19:58 +0200

    [xharness] Don't overwrite variations. (#8312)
    This means different variations of tests won't show up as identical in the
    html report.

--- tests/xharness/Jenkins/Jenkins.cs
+++ tests/xharness/Jenkins/Jenkins.cs
@@ -1085,7 +1085,7 @@ namespace Xharness.Jenkins {
 					}
 
 					foreach (var e in execs)
-						e.Variation = config;
+						e.Variation = string.IsNullOrEmpty (e.Variation) ? config : e.Variation;
 
 					Tasks.AddRange (execs);
 				}
