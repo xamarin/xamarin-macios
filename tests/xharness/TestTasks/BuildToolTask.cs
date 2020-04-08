5b998593218fca7adaddce0ec130e793e85f861b
Manuel de la Pena <mandel@microsoft.com>
2020-04-08 15:03:33 -0400

    [Harness] Move BuildToolTask to a more general namespace. (#8287)
    Move the task and use composition so that we can reuse the code. This
    will later allow other projects to use the class without the need of
    Jenkins or Harness and just implement the base class.
    
    
    
    Co-authored-by: Rolf Bjarne Kvinge <rolf@xamarin.com>
    Co-authored-by: Přemek Vysoký <premek.vysoky@microsoft.com>

---
+++ tests/xharness/TestTasks/BuildToolTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.DotNet.XHarness.iOS.Shared.Execution;
+
+namespace Xharness.TestTasks {
+
+	public class BuildToolTask
+	{
+		public IProcessManager ProcessManager { get; }
+		public TestPlatform Platform { get; set; }
+
+		public bool SpecifyPlatform { get; set; } = true;
+		public bool SpecifyConfiguration { get; set; } = true;
+
+		public BuildToolTask (IProcessManager processManager)
+		{ 
+			ProcessManager = processManager ?? throw new ArgumentNullException (nameof (processManager));
+		}
+
+		public BuildToolTask (IProcessManager processManager, TestPlatform platform) : this (processManager)
+		{
+			Platform = platform;
+		}
+
+		public virtual string Mode {
+			get { return Platform.ToString (); }
+			set { throw new NotSupportedException (); }
+		}
+
+		public virtual Task CleanAsync ()
+		{
+			Console.WriteLine ("Clean is not implemented for {0}", GetType ().Name);
+			return Task.CompletedTask;
+		}
+	}
+}
