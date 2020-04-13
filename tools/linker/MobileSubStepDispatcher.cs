37b71366e017a8de9e48f68ca565aa1a807443b8
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-04-13 13:17:54 +0200

    MobileSubStepDispatcher.

--- tools/linker/MobileSubStepDispatcher.cs
+++ tools/linker/MobileSubStepDispatcher.cs
@@ -1,17 +1,26 @@
 using System.Collections.Generic;
 
+#if NET
+using Mono.Linker.Steps;
+#else
 using Mono.Tuner;
+#endif
 
 namespace Xamarin.Linker {
 
 
 	public class MobileSubStepDispatcher : SubStepsDispatcher {
 		public MobileSubStepDispatcher ()
+#if NET
+			: base ()
+#else
 			: base (MonoTouch.Tuner.Linker.GetSubSteps ())
+#endif
 		{
 		}
 	}
 
+#if !NET
 	public class SubStepsDispatcher : SubStepDispatcher {
 		public SubStepsDispatcher (IEnumerable<ISubStep> subSteps)
 		{
@@ -19,4 +28,5 @@ namespace Xamarin.Linker {
 				Add (step);
 		}
 	}
+#endif
 }
