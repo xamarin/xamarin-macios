3133bf908513d27334eb84bb117b53a84e839c2a
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-04-13 09:33:29 +0200

    Custom Extensions.

---
+++ tools/dotnet-linker/Extensions.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+using Mono.Cecil;
+
+using Mono.Linker;
+
+namespace Mono.Tuner {
+
+	public static partial class Extensions {
+
+		public static bool TryGetLinkedAssembly (this LinkContext context, string name, out AssemblyDefinition assembly)
+		{
+			assembly = GetAssembly (context, name);
+			if (assembly == null)
+				return false;
+
+			return context.Annotations.GetAction (assembly) == AssemblyAction.Link;
+		}
+
+		public static AssemblyDefinition GetAssembly (this LinkContext context, string assembly_name)
+		{
+			return context.GetLoadedAssembly (assembly_name);
+		}
+
+		// note: direct check, no inheritance
+		public static bool Is (this TypeReference type, string @namespace, string name)
+		{
+			return ((type != null) && (type.Name == name) && (type.Namespace == @namespace));
+		}
+	}
+}
