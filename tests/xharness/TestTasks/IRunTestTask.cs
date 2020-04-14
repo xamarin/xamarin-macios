d77e84db4bcaef4c78e4f1d9946c9937b4157c0d
Manuel de la Pena <mandel@microsoft.com>
2020-04-14 10:49:01 -0400

    [Harness] Generalize the RunTestTask to allow it to be used in the CLI.
    Move all the logis outside and use it as a Composition pattern, later
    this class can be used in the CLI so that we share the logic of building
    + tested.

---
+++ tests/xharness/TestTasks/IRunTestTask.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+using Microsoft.DotNet.XHarness.iOS.Shared.Logging;
+
+namespace Xharness.TestTasks {
+	public interface IRunTestTask : ITestTask {
+		Task RunTestAsync ();
+		Task VerifyBuildAsync ();
+	}
+}
