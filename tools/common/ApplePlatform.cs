56228e803382a872985a2e7e609065e4f36910ff
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-04-16 13:41:41 +0200

    Extract the ApplePlatform enum into its own file.

---
+++ tools/common/ApplePlatform.cs
@@ -0,0 +1,15 @@
+//
+// ApplePlatform.cs
+//
+// Copyright 2020 Microsoft Corp. All Rights Reserved.
+
+namespace Xamarin.Utils
+{
+	public enum ApplePlatform {
+		None,
+		MacOSX,
+		iOS,
+		WatchOS,
+		TVOS,
+	}
+}
