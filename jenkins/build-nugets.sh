a2e7260ddefd8f6196ad1157da109589fb3d963d
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-04-17 14:22:37 +0200

    Remove the legacy nugets

--- jenkins/build-nugets.sh
+++ jenkins/build-nugets.sh
@@ -4,8 +4,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 make -C msbuild/dotnet
 make -C msbuild/dotnet pack -j
-# disable legacy packing for now, it takes a while to execute, produces big packages, and rarely change.
-#make -C msbuild/dotnet legacy-pack -j
 
 mkdir -p ../package/
 rm -f ../package/*.nupkg
