4e19e05b86f0aefa71c854938478a39d80a9a367
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-04-21 09:59:27 +0200

    [mtouch] Move unresolved symbols into a C# class.
    The symbols are parsed using grep/sed from the Makefile, and included directly
    as source code into the MSBuild tasks.

---
+++ tools/mtouch/UnresolvedSymbols.cs
@@ -0,0 +1,41 @@
+namespace Xamarin.Bundler {
+	public partial class Driver {
+		public static string [] UnresolvedSymbols = new []
+		{
+			// This file is processed in this directory's Makefile as well
+			"_xamarin_dyn_objc_msgSend",
+			"_xamarin_dyn_objc_msgSendSuper",
+			"_xamarin_dyn_objc_msgSend_stret",
+			"_xamarin_dyn_objc_msgSendSuper_stret",
+			"_xamarin_init_nsthread",
+			"_xamarin_get_block_descriptor",
+			"_xamarin_get_locale_country_code",
+			"_xamarin_log",
+			"_xamarin_localized_string_format",
+			"_xamarin_localized_string_format_1",
+			"_xamarin_localized_string_format_2",
+			"_xamarin_localized_string_format_3",
+			"_xamarin_localized_string_format_4",
+			"_xamarin_localized_string_format_5",
+			"_xamarin_localized_string_format_6",
+			"_xamarin_localized_string_format_7",
+			"_xamarin_localized_string_format_8",
+			"_xamarin_localized_string_format_9",
+			"_xamarin_start_wwan",
+			"_xamarin_timezone_get_data",
+			"_xamarin_timezone_get_names",
+			"_xamarin_timezone_get_local_name",
+			"_xamarin_IntPtr_objc_msgSend_IntPtr",
+			"_xamarin_IntPtr_objc_msgSendSuper_IntPtr",
+			"_xamarin_release_managed_ref",
+			"_xamarin_create_managed_ref",
+			"_xamarin_CGPoint__VNNormalizedFaceBoundingBoxPointForLandmarkPoint_Vector2_CGRect_nuint_nuint_string",
+			"_xamarin_CGPoint__VNImagePointForFaceLandmarkPoint_Vector2_CGRect_nuint_nuint_string",
+			"_CloseZStream",
+			"_CreateZStream",
+			"_Flush",
+			"_ReadZStream",
+			"_WriteZStream",
+		};
+	}
+}
