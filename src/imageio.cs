d19f66050b1b60dcf8ae89f70434f345e1ba862a
Whitney Schmidt <whschm@microsoft.com>
2020-04-27 11:54:29 -0400

    NSNumber -> nuint for NSUInteger

--- src/imageio.cs
+++ src/imageio.cs
@@ -2388,8 +2388,8 @@ namespace ImageIO {
 	interface CGImageAnimationOptions {
 		double DelayTime { get; set; }
 
-		NSNumber LoopCount { get; set; }
+		nuint LoopCount { get; set; }
 
-		NSNumber StartIndex { get; set; }
+		nuint StartIndex { get; set; }
 	}
 }
