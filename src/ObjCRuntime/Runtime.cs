e2ee5a27103e72d14ba2e47c7264602a42635e77
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-05-04 11:08:34 +0200

    Convert Runtime.GetSelector to use GCHandle.

--- src/ObjCRuntime/Runtime.cs
+++ src/ObjCRuntime/Runtime.cs
@@ -633,7 +633,7 @@ namespace ObjCRuntime {
 
 		static IntPtr GetSelector (IntPtr sel)
 		{
-			return ObjectWrapper.Convert (new Selector (sel));
+			return AllocGCHandle (new Selector (sel));
 		}
 
 		static void GetMethodForSelector (IntPtr cls, IntPtr sel, bool is_static, IntPtr desc)
