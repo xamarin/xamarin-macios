ebde63f5ec08b181f4a6e949e4bf90f436382e11
Whitney Schmidt <whschm@microsoft.com>
2020-05-08 20:02:34 -0400

    try fixing per-device pwsh env var for summaries

--- tools/devops/templates/publish_failure.ps1
+++ tools/devops/templates/publish_failure.ps1
@@ -79,7 +79,8 @@ $DESCRIPTION="Device test $Env:SYSTEM_JOBNAME $Env:AGENT_JOBSTATUS on $Env:CONTE
 # BUILD_DEFINITIONNAME: Pipeline name, e.g. "iOS Device Tests [DDFun]"
 $json_text = "$RESULT_EMOJI $DESCRIPTION on [Azure DevOps]($target_url) ($Env:BUILD_DEFINITIONNAME) $RESULT_EMOJI"
 
-$file = $Env:PIPELINE_WORKSPACE
+$file = $Env:SYSTEM_DEFAULTWORKINGDIRECTORY
+#$Env:PIPELINE_WORKSPACE
 Get-ChildItem $file | Write-Host
 $file = "$file/Summaries/TestSummary-$Env:CONTEXT"
 Get-ChildItem $file | Write-Host
