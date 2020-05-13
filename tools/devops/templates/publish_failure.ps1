7ec48a8ab5f57cf24042681e9b26c4e8870ad913
Whitney Schmidt <whschm@microsoft.com>
2020-05-13 17:33:25 -0400

    clean up publish_failure + add try/catch

--- tools/devops/templates/publish_failure.ps1
+++ tools/devops/templates/publish_failure.ps1
@@ -36,35 +36,25 @@ $params = @{
 
 Write-Host $params
 
-$response = Invoke-RestMethod @params
-
-$response | ConvertTo-Json | Write-Host
-
+try {
+    $response = Invoke-RestMethod @params
+    $response | ConvertTo-Json | Write-Host
+} catch {
+    Write-Host "Error posting GH status: $_"
+}
 
 ###
 ### Construct commit message with test summary
 ###
 
-$RESULT_EMOJI
-If ($Env:AGENT_JOBSTATUS -eq 'Failed')
-{
-	$RESULT_EMOJI = ":fire: "
-} Else {
-	$RESULT_EMOJI = ":white_check_mark: " # maybe remove this since we only add commit messages on failure?
-}
-
+$RESULT_EMOJI = ":fire: " # we only run when there's a failure
 
 If ($Env:BUILD_DEFINITIONNAME -like '*DDFun*')
 {
     ## do stuff
 }
-#Else{
-#HTML Report jenkins stuff
-#}
-
 
 $HEADER = "### :bangbang: :construction: TESTING Experimental DDFun pipeline"
-
 
 # SYSTEM_JOBNAME: Xamarin | Monotouch | xUnitBCL | NUnitBCL | mscorlib
 # CONTEXT: tvOS | iOS | iOS32
@@ -73,10 +63,9 @@ $DESCRIPTION="Device test **$Env:SYSTEM_JOBNAME** $Env:AGENT_JOBSTATUS on **$Env
 # BUILD_DEFINITIONNAME: Pipeline name, e.g. "iOS Device Tests [DDFun]"
 $json_text = "$RESULT_EMOJI $DESCRIPTION on [Azure DevOps]($target_url) ($Env:BUILD_DEFINITIONNAME) $RESULT_EMOJI"
 
-$file = $Env:SYSTEM_DEFAULTWORKINGDIRECTORY
-Get-ChildItem $file | Write-Host
-
-$file = "$file/xamarin-macios/tests"
+# Grab test results from default working directory
+# Cannot access the published pipeline artifact because it is not always available when this script runs :(
+$file = "$Env:SYSTEM_DEFAULTWORKINGDIRECTORY/xamarin-macios/tests"
 
 Write-Host "xamarin-macios tests children"
 Get-ChildItem $file | Write-Host
@@ -85,8 +74,6 @@ $file = "$file/TestSummary.md"
 
 Write-Host $file
 Get-Content $file | Write-Host
-
-
 
 # stringbuilder for extra flavor
 $msg = [System.Text.StringBuilder]::new()
@@ -103,8 +90,6 @@ foreach ($line in Get-Content -Path $file)
 
 $message_url = "https://api.github.com/repos/xamarin/xamarin-macios/commits/$Env:BUILD_REVISION/comments"
 
-
-# create pwsh object to store payload
 $payload = @{
 	body = $msg.ToString()
 }
@@ -124,6 +109,9 @@ $params = @{
 
 Write-Host $params
 
-$response = Invoke-RestMethod @params
-
-$response | ConvertTo-Json | Write-Host
+try {
+    $response = Invoke-RestMethod @params
+    $response | ConvertTo-Json | Write-Host
+} catch {
+    Write-Host "Error posting GH commit message: $_"
+}
