83b3aca8730133b4c1da3585589ae2fd1d7a3240
Manuel de la Pena <mandel@microsoft.com>
2020-05-19 13:57:09 -0400

    [Harness] Remove methods that are already present in the KnowledgeBase class. (#8619)

--- tests/xharness/Jenkins/Jenkins.cs
+++ tests/xharness/Jenkins/Jenkins.cs
@@ -1271,60 +1271,6 @@ namespace Xharness.Jenkins {
 			}
 		}
 
-		bool IsHE0038Error (ILog log) {
-			if (log == null)
-				return false;
-			if (File.Exists (log.FullPath) && new FileInfo (log.FullPath).Length > 0) {
-				using (var reader = log.GetReader ()) {
-					while (!reader.EndOfStream) {
-						string line = reader.ReadLine ();
-						if (line == null)
-							continue;
-						if (line.Contains ("error HE0038: Failed to launch the app"))
-							return true;
-					}
-				}
-			}
-			return false;
-		}
-
-		bool IsMonoMulti3Issue (ILog log) {
-			if (log == null)
-				return false;
-			if (File.Exists (log.FullPath) && new FileInfo (log.FullPath).Length > 0) {
-				using var reader = log.GetReader ();
-				while (!reader.EndOfStream) {
-					string line = reader.ReadLine ();
-					if (line == null)
-						continue;
-					if (line.Contains ("error MT5210: Native linking failed, undefined symbol: ___multi3"))
-						return true;
-				}
-			}
-			return false;
-		}
-
-
-		public bool IsKnownBuildIssue (ILog buildLog, out string knownFailureMessage)
-		{
-			knownFailureMessage = null;
-			if (IsMonoMulti3Issue (buildLog)) {
-				knownFailureMessage = $"<a href='https://github.com/mono/mono/issues/18560'>Undefined symbol ___multi3 on Release Mode</a>";
-				return true;
-			}
-			return false;
-		}
-
-		public bool IsKnownTestIssue (ILog runLog, out string knownFailureMessage)
-		{
-			knownFailureMessage = null;
-			if (IsHE0038Error (runLog)) {
-				knownFailureMessage = $"<a href='https://github.com/xamarin/maccore/issues/581'>HE0038</a>";
-				return true;
-			}
-			return false;
-		}
-		
 		string previous_test_runs;
 		void GenerateReportImpl (Stream stream, StreamWriter markdown_summary = null)
 		{
