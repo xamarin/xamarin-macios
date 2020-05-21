bfeb25d590adf419074ccfd1f7b0e63d46a01e25
Manuel de la Pena <mandel@microsoft.com>
2020-05-20 23:08:28 -0400

    [Harness] Move the devition of the tasks outside.
    Create some enumerables that will state the tests to run, that way, if
    new tests need to be added, there will not be a need to edit the
    jenkins class.
    
    Add tests to ensure that all the tests are added. That away we will make
    sure that tests are not removed by accident.

---
+++ tests/xharness/MacFlavorsExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.DotNet.XHarness.iOS.Shared;
+
+namespace Xharness {
+	public static class MacFlavorsExtensions {
+
+		public static TestPlatform ToTestPlatform (this MacFlavors flavor)
+		{
+			return flavor switch
+			{
+				MacFlavors.Console => TestPlatform.Mac,
+				MacFlavors.Full => TestPlatform.Mac_Full,
+				MacFlavors.Modern => TestPlatform.Mac_Modern,
+				MacFlavors.System => TestPlatform.Mac_System,
+				_ => throw new NotImplementedException (flavor.ToString ()),
+			};
+		}
+	}
+}
