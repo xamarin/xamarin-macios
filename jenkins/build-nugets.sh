732847bd85ed5b86f785184f3a0a2120648c5df8
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-05-27 15:10:29 +0200

    [jenkins] Use the existing DOTNET_NUPKG_DIR variable when looking for built nugets. (#8691)

--- jenkins/build-nugets.sh
+++ jenkins/build-nugets.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -ex
 
+DOTNET_NUPKG_DIR=$(make print-abspath-variable VARIABLE=DOTNET_NUPKG_DIR)
+
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 mkdir -p ../package/
 rm -f ../package/*.nupkg
-cp -c dotnet/nupkgs/*.nupkg ../package/
+cp -c "$DOTNET_NUPKG_DIR"/*.nupkg ../package/
