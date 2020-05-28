d5e0100263b75008dcbd6c4027dc9611b37007d0
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-05-28 12:55:53 +0200

    [msbuild] Share the _DetectSigningIdentity target.
    Also rework the class hierarchy a little bit, so that Xamarin.iOS and
    Xamarin.Mac are identical:
    
    	DetectSigningIdentityTaskBase
    	└─── iOS/DetectSigningIdentityTaskCore
    	│    └─── iOS/DetectSigningIdentity
    	└─── Mac/DetectSigningIdentityTaskCore
    	│    └─── Mac/DetectSigningIdentity

--- msbuild/Xamarin.Mac.Tasks/Tasks/DetectSigningIdentity.cs
+++ msbuild/Xamarin.Mac.Tasks/Tasks/DetectSigningIdentity.cs
@@ -6,21 +6,9 @@
 //
 // Copyright 2014 Xamarin Inc.
 
-using Xamarin.MacDev.Tasks;
-using Xamarin.Utils;
-
 namespace Xamarin.Mac.Tasks
 {
-	public class DetectSigningIdentity : DetectSigningIdentityTaskBase
+	public class DetectSigningIdentity : DetectSigningIdentityTaskCore
 	{
-		static readonly string[] appStoreDistributionPrefixes = { "3rd Party Mac Developer Application", "Apple Distribution" };
-		static readonly string[] directDistributionPrefixes = { "Developer ID Application" };
-		static readonly string[] developmentPrefixes = { "Mac Developer", "Apple Development" };
-
-		protected override string[] DevelopmentPrefixes { get { return developmentPrefixes; } }
-		protected override string[] DirectDistributionPrefixes { get { return directDistributionPrefixes; } }
-		protected override string[] AppStoreDistributionPrefixes { get { return appStoreDistributionPrefixes; } }
-		protected override string DeveloperRoot { get { return MacOSXSdks.Native.DeveloperRoot; } }
-		protected override string ApplicationIdentifierKey { get { return "com.apple.application-identifier"; } }
 	}
 }
