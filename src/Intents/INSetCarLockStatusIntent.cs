aa0d9e18b34f335ba9e3859d579d2687f8d516c6
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-06-01 15:25:32 +0200

    Intents

--- src/Intents/INSetCarLockStatusIntent.cs
+++ src/Intents/INSetCarLockStatusIntent.cs
@@ -7,7 +7,7 @@
 // Copyright 2017 Xamarin Inc. All rights reserved.
 //
 
-#if XAMCORE_2_0 && (IOS || TVOS)
+#if IOS || TVOS
 
 using Foundation;
 using Intents;
--- src/Intents/INSpeakableString.cs
+++ src/Intents/INSpeakableString.cs
@@ -7,7 +7,6 @@
 // Copyright 2017 Xamarin Inc. All rights reserved.
 //
 
-#if XAMCORE_2_0
 using System;
 using Foundation;
 using ObjCRuntime;
@@ -30,4 +29,3 @@ namespace Intents {
 		}
 	}
 }
-#endif
