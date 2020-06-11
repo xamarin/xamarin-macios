2a81b709d6ea4f2b76cd1260d4d93dc51e199598
Alex Soto <alex@alexsoto.me>
2020-06-11 13:07:27 -0400

    [xcode11.6] Initial bump to support Xcode 11.6 Beta 1

--- tests/monotouch-test/HealthKit/CategoryTypeIdentifierTest.cs
+++ tests/monotouch-test/HealthKit/CategoryTypeIdentifierTest.cs
@@ -47,6 +47,44 @@ namespace MonoTouchFixtures.HealthKit {
 					if (!TestRuntime.CheckXcodeVersion (11, 0))
 						continue;
 					break;
+#if !__WATCHOS__
+				case HKCategoryTypeIdentifier.AbdominalCramps:
+				case HKCategoryTypeIdentifier.Acne:
+				case HKCategoryTypeIdentifier.AppetiteChanges:
+				case HKCategoryTypeIdentifier.GeneralizedBodyAche:
+				case HKCategoryTypeIdentifier.Bloating:
+				case HKCategoryTypeIdentifier.BreastPain:
+				case HKCategoryTypeIdentifier.ChestTightnessOrPain:
+				case HKCategoryTypeIdentifier.Chills:
+				case HKCategoryTypeIdentifier.Constipation:
+				case HKCategoryTypeIdentifier.Coughing:
+				case HKCategoryTypeIdentifier.Diarrhea:
+				case HKCategoryTypeIdentifier.Dizziness:
+				case HKCategoryTypeIdentifier.Fainting:
+				case HKCategoryTypeIdentifier.Fatigue:
+				case HKCategoryTypeIdentifier.Fever:
+				case HKCategoryTypeIdentifier.Headache:
+				case HKCategoryTypeIdentifier.Heartburn:
+				case HKCategoryTypeIdentifier.HotFlashes:
+				case HKCategoryTypeIdentifier.LowerBackPain:
+				case HKCategoryTypeIdentifier.LossOfSmell:
+				case HKCategoryTypeIdentifier.LossOfTaste:
+				case HKCategoryTypeIdentifier.MoodChanges:
+				case HKCategoryTypeIdentifier.Nausea:
+				case HKCategoryTypeIdentifier.PelvicPain:
+				case HKCategoryTypeIdentifier.RapidPoundingOrFlutteringHeartbeat:
+				case HKCategoryTypeIdentifier.RunnyNose:
+				case HKCategoryTypeIdentifier.ShortnessOfBreath:
+				case HKCategoryTypeIdentifier.SinusCongestion:
+				case HKCategoryTypeIdentifier.SkippedHeartbeat:
+				case HKCategoryTypeIdentifier.SleepChanges:
+				case HKCategoryTypeIdentifier.SoreThroat:
+				case HKCategoryTypeIdentifier.Vomiting:
+				case HKCategoryTypeIdentifier.Wheezing:
+					if (!TestRuntime.CheckXcodeVersion (11, 6))
+						continue;
+					break;
+#endif
 				default:
 					if (!TestRuntime.CheckXcodeVersion (7, 0))
 						continue;
