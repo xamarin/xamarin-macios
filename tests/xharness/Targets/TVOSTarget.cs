9ddb35720b5455611efa86daa2e56b5a070a353c
Manuel de la Pena <mandel@microsoft.com>
2020-06-16 16:04:38 -0400

    [Harness] Add a more general IHarness interface. (#8868)
    In order to be able to test the TestSelectro in PR
    https://github.com/xamarin/xamarin-macios/pull/8768 we need a more
    general interface to mock and ensure that the correct properties are set
    by the TestSelector.

--- tests/xharness/Targets/TVOSTarget.cs
+++ tests/xharness/Targets/TVOSTarget.cs
@@ -112,7 +112,7 @@ namespace Xharness.Targets {
 				MonoNativeHelper.RemoveSymlinkMode (inputProject);
 			}
 
-			var srcDirectory = Path.Combine (Harness.RootDirectory, "..", "src");
+			var srcDirectory = Path.Combine (HarnessConfiguration.RootDirectory, "..", "src");
 
 			string project_guid;
 			var mt_nunitlite_project_path = Path.GetFullPath (Path.Combine (srcDirectory, "MonoTouch.NUnitLite.tvos.csproj"));
