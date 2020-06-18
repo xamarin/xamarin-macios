ac7056ccb95e799d3546694b0f4d58c21e6dc48d
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-06-18 12:05:59 +0200

    Bump maccore. (#8866)
    * Bump maccore.
    
    New commits in xamarin/maccore:
    
    * xamarin/maccore@f1526568f4 [tests] Run the correct sample tests when launched from CI (#2234)
    
    Diff: https://github.com/xamarin/maccore/compare/ed6d1468221f2220cd3f39294427160f0d3fbced..f1526568f4797283512c67c3f2058152556d9104
    
    * Bump maccore to get additional fix.
    
    New commits in xamarin/maccore:
    
    * xamarin/maccore@8b437d6ae3 [tests] Fix parameters to launch sample tests in CI. (#2238)
    
    Diff: https://github.com/xamarin/maccore/compare/f1526568f4797283512c67c3f2058152556d9104..8b437d6ae31554a8bc5e8d0beaa42ffc757d0077

--- mk/xamarin.mk
+++ mk/xamarin.mk
@@ -7,7 +7,7 @@ MONO_BRANCH    := $(shell cd $(MONO_PATH) 2> /dev/null && git symbolic-ref --sho
 endif
 
 ifdef ENABLE_XAMARIN
-NEEDED_MACCORE_VERSION := ed6d1468221f2220cd3f39294427160f0d3fbced
+NEEDED_MACCORE_VERSION := 8b437d6ae31554a8bc5e8d0beaa42ffc757d0077
 NEEDED_MACCORE_BRANCH := main
 
 MACCORE_DIRECTORY := maccore
