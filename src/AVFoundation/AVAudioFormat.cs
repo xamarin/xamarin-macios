073528b1912671ac34d941386115f23f98a7d9dc
Chris Hamons <chris.hamons@xamarin.com>
2020-06-19 11:03:13 -0500

    Remove just the Equals/GetHashCode instead

---
+++ src/AVFoundation/AVAudioFormat.cs
@@ -0,0 +1,25 @@
+//
+// Copyright 2014 Xamarin Inc
+//
+// Authors:
+//   Miguel de Icaza (miguel@xamarin.com)
+//
+using Foundation;
+using ObjCRuntime;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AVFoundation {
+	public partial class AVAudioFormat {
+		public static bool operator == (AVAudioFormat a, AVAudioFormat b)
+		{
+			return a != null && b != null && a.Equals (b);
+		}
+		
+		public static bool operator != (AVAudioFormat a, AVAudioFormat b)
+		{
+			return a != null && b != null && !a.Equals (b);
+		}
+	}
+}
