4f39bef6e21f7dbc2a965b0bc7e049108ff753cd
Sebastien Pouliot <sebastien@xamarin.com>
2020-06-26 16:27:19 -0400

    [xcode12][security] Update for beta 1

--- src/Security/Enums.cs
+++ src/Security/Enums.cs
@@ -69,6 +69,7 @@ namespace Security {
 		Pkcs12VerifyFailure 				= -25264,
 		NotSigner 							= -26267,
 		MissingEntitlement				= -34018,
+		RestrictedApi                       = -34020,
 		ServiceNotAvailable 				= -67585,
 		InsufficientClientID 				= -67586,
 		DeviceReset 						= -67587,
