3506f4563f57f0b27decc409c1ce2fbc16e426e5
Sebastien Pouliot <sebastien.pouliot@microsoft.com>
2020-07-07 10:19:26 -0400

    [tests] Add UIKit tests for manual bindings

--- tests/monotouch-test/UIKit/CellAccessoryTest.cs
+++ tests/monotouch-test/UIKit/CellAccessoryTest.cs
@@ -1,8 +1,39 @@
 ﻿using System;
-namespace monotouchtest.UIKit {
+using Foundation;
+using UIKit;
+using ObjCRuntime;
+using NUnit.Framework;
+
+namespace MonoTouchFixtures.UIKit {
+
+	[TestFixture]
+	[Preserve (AllMembers = true)]
 	public class CellAccessoryTest {
-		public CellAccessoryTest ()
+
+		[Test]
+		public void GetPositionBeforeAccessory ()
 		{
+			TestRuntime.AssertXcodeVersion (12, 0);
+			var cap = UICellAccessory.GetPositionBeforeAccessory (new Class ("UIButton"));
+			Assert.NotNull (cap, "Class/cap");
+			Assert.That (cap (new UICellAccessory [0]), Is.EqualTo (0), "Class/Invoke");
+
+			cap = UICellAccessory.GetPositionBeforeAccessory (new Class (typeof (UICellAccessory)));
+			Assert.NotNull (cap, "Type/cap");
+			Assert.That (cap (new UICellAccessory [0]), Is.EqualTo (0), "Type/Invoke");
+		}
+
+		[Test]
+		public void GetPositionAfterAccessory ()
+		{
+			TestRuntime.AssertXcodeVersion (12, 0);
+			var cap = UICellAccessory.GetPositionAfterAccessory (new Class ("UIButton"));
+			Assert.NotNull (cap, "Class/cap");
+			Assert.That (cap (new UICellAccessory [0]), Is.EqualTo (0), "Class/Invoke");
+
+			cap = UICellAccessory.GetPositionAfterAccessory (typeof (UICellAccessory));
+			Assert.NotNull (cap, "Type/cap");
+			Assert.That (cap (new UICellAccessory [0]), Is.EqualTo (0), "Type/Invoke");
 		}
 	}
 }
