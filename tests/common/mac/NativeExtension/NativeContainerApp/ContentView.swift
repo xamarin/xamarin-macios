82e1be9b265a7cef768b14b75c19d442ee6cc8a0
Chris Hamons <chris.hamons@xamarin.com>
2020-07-09 11:15:14 -0500

    [test] Native macOS extension project
    - Not much to see here either, just lining up the bundle ID

---
+++ tests/common/mac/NativeExtension/NativeContainerApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  NativeContainerApp
+//
+//  Created by Chris Hamons on 7/9/20.
+//  Copyright © 2020 Chris Hamons. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tests/common/mac/NativeExtension/NativeTodayExtension/TodayViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TodayViewController.swift
+//  NativeTodayExtension
+//
+//  Created by Chris Hamons on 7/9/20.
+//  Copyright © 2020 Chris Hamons. All rights reserved.
+//
+
+import Cocoa
+import NotificationCenter
+
+class TodayViewController: NSViewController, NCWidgetProviding {
+
+    override var nibName: NSNib.Name? {
+        return NSNib.Name("TodayViewController")
+    }
+
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Update your data and prepare for a snapshot. Call completion handler when you are done
+        // with NoData if nothing has changed or NewData if there is new data since the last
+        // time we called you
+        completionHandler(.noData)
+    }
+
+}
