bb980d7308862ebb6fcbca5ca87669bd09c97334
Chris Hamons <chris.hamons@xamarin.com>
2020-07-09 11:00:10 -0500

    [test] Interesting parts of XI/XM extension tests

---
+++ msbuild/tests/Xamarin.iOS.Tasks.Tests/ProjectsTests/EmbeddedExtension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+using Xamarin.Tests;
+
+namespace Xamarin.iOS.Tasks {
+	[TestFixture ("iPhone")]
+	[TestFixture ("iPhoneSimulator")]
+	public class EmbeddedExtension : ProjectTest {
+		
+		public EmbeddedExtension (string platform) : base (platform)      
+		{
+		}
+
+		[Test]
+		public void BasicTest ()
+		{
+			var mtouchPaths = SetupProjectPaths ("ManagedContainer", "../NativeExtensionEmbedding/managed", true, Platform);
+
+			Engine.ProjectCollection.SetGlobalProperty ("Platform", Platform);
+
+			var xcodeProjectFolder = Path.Combine (mtouchPaths.ProjectPath , "..", "..", "native");
+			string [] xcodeBuildArgs;
+			if (this.Platform  == "iPhone")
+				xcodeBuildArgs = new [] { "-configuration", "Debug", "-target", "NativeTodayExtension", "-arch", "arm64", "-sdk", "iphoneos13.5" };
+			else
+				xcodeBuildArgs = new [] { "-configuration", "Debug", "-target", "NativeTodayExtension", "-arch", "x86_64", "-sdk", "iphonesimulator13.5" };
+			Assert.AreEqual (0, ExecutionHelper.Execute ("/usr/bin/xcodebuild", xcodeBuildArgs.Concat (new [] { "clean" }).ToArray (), out System.Text.StringBuilder cleanOutput, xcodeProjectFolder));
+			Assert.AreEqual (0, ExecutionHelper.Execute ("/usr/bin/xcodebuild", xcodeBuildArgs.Concat (new [] { "build" }).ToArray (), out System.Text.StringBuilder _, xcodeProjectFolder));
+
+			AppBundlePath = mtouchPaths.AppBundlePath;
+
+			var proj = SetupProject (Engine, mtouchPaths.ProjectCSProjPath);
+
+			RunTarget (proj, "Clean", 0);
+			RunTarget (proj, "Build", 0);
+
+			Assert.That (File.Exists (Path.Combine (AppBundlePath, "PlugIns", "NativeTodayExtension.appex", "NativeTodayExtension")), "NativeTodayExtension");
+		}
+	}
+}
+
---
+++ tests/mmptest/src/NativeExtensionEmbedding.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.Tests;
+
+namespace Xamarin.MMP.Tests
+{
+	[TestFixture]
+	public class NativeExtensionEmbeddingTests
+	{
+		[TestCase (true)]
+		[TestCase (false)]
+		public void NativeExtensionEmbedding (bool XM45)
+		{
+			MMPTests.RunMMPTest (tmpDir => {
+				TI.CopyDirectory (Path.Combine (TI.FindSourceDirectory (), @"NativeExtension"), tmpDir);
+
+				var xcodeProjectFolder = Path.Combine (tmpDir, "NativeExtension");
+				string [] xcodeBuildArgs = new [] { "-configuration", "Debug", "-target", "NativeTodayExtension", "-arch", "x86_64", "-sdk", "macosx10.15" };
+				Assert.AreEqual (0, ExecutionHelper.Execute ("/usr/bin/xcodebuild", xcodeBuildArgs.Concat (new [] { "clean" }).ToArray (), out System.Text.StringBuilder cleanOutput, xcodeProjectFolder));
+				Assert.AreEqual (0, ExecutionHelper.Execute ("/usr/bin/xcodebuild", xcodeBuildArgs.Concat (new [] { "build" }).ToArray (), out System.Text.StringBuilder _, xcodeProjectFolder));
+
+				var items = @"
+<ItemGroup>
+	<AdditionalAppExtensions Include=""$(MSBuildProjectDirectory)/NativeExtension"">
+		<Name>NativeTodayExtension</Name>
+		<BuildOutput>build/Debug</BuildOutput>
+	</AdditionalAppExtensions>
+</ItemGroup>";
+
+				var config = new TI.UnifiedTestConfig (tmpDir) { XM45 = XM45, ItemGroup = items};
+				string csprojTarget = TI.GenerateUnifiedExecutableProject (config);
+				TI.BuildProject (csprojTarget);
+
+				Assert.That (File.Exists (Path.Combine (tmpDir, "bin", "Debug", (XM45 ? "XM45Example.app" : "UnifiedExample.app"), "Contents", "PlugIns", "NativeTodayExtension.appex", "Contents", "MacOS", "NativeTodayExtension")), "NativeTodayExtension");
+			});
+		}
+	}
+}
