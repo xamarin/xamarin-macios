4587b6543309ad17d3ffbab46f2291a083c31673
Sebastien Pouliot <sebastien@xamarin.com>
2020-07-08 22:51:25 -0400

    [tests][monotouch-test] Re-enable some tests with beta 2
    Some issues we detected in beta 1 are not present in beta 2
    https://github.com/xamarin/xamarin-macios/issues/8943

--- tests/monotouch-test/MapKit/AnnotationViewTest.cs
+++ tests/monotouch-test/MapKit/AnnotationViewTest.cs
@@ -62,9 +62,7 @@ namespace MonoTouchFixtures.MapKit {
 		{
 			var frame = new CGRect (10, 10, 100, 100);
 			using (MKAnnotationView av = new MKAnnotationView (frame)) {
-				// broke in xcode 12 beta 1
-				if (!TestRuntime.CheckXcodeVersion (12,0))
-					Assert.That (av.Frame, Is.EqualTo (frame), "Frame");
+				Assert.That (av.Frame, Is.EqualTo (frame), "Frame");
 				Assert.Null (av.Annotation, "Annotation");
 			}
 		}
