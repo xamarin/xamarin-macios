38438399897b8daaf3a87923cec6e3623f7583de
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-07-17 18:20:28 +0200

    [xharness] Generate projects into a subdirectory for each project.
    Because it's cumbersome to keep the build working when the same directory has
    multiple project files.

--- tests/xharness/Targets/iOSTarget.cs
+++ tests/xharness/Targets/iOSTarget.cs
@@ -27,7 +27,7 @@ namespace Xharness.Targets
 		{
 			XmlDocument info_plist = new XmlDocument ();
 			var target_info_plist = Path.Combine (TargetDirectory, "Info" + Suffix + ".plist");
-			info_plist.LoadWithoutNetworkAccess (Path.Combine (TargetDirectory, "Info.plist"));
+			info_plist.LoadWithoutNetworkAccess (Path.Combine (TargetDirectory, OriginalInfoPListInclude));
 			BundleIdentifier = info_plist.GetCFBundleIdentifier ();
 			info_plist.SetMinimumOSVersion (GetMinimumOSVersion (info_plist.GetMinimumOSVersion ()));
 			info_plist.SetUIDeviceFamily (UIDeviceFamily);
