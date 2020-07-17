fe213fff89c5057bd1eee79131257564ba070a35
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-07-17 16:38:40 +0200

    [dotnet-linker] Add a Constants class to the build.
    The bundler (mtouch/mmp) code needs it to compile.

---
+++ tools/dotnet-linker/Constants.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Xamarin.Bundler {
+	public static partial class Constants {
+		public static string Version {
+			get { throw new NotImplementedException (); }
+		}
+		internal static string Revision {
+			get { throw new NotImplementedException (); }
+		}
+		public static string SdkVersion {
+			get { throw new NotImplementedException (); }
+		}
+	}
+}
