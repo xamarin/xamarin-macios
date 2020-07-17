d410221f9a8f042e90f8e88c5da539991ebae7e3
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-07-17 16:38:40 +0200

    [monotouch-test] Adjust tests based on Environment.OSVersion
    With Mono, Environment.OSVersion returns the macOS version when running in the
    simulator. On Catalina, Mono returns 19.* as the Environment.OSVersion, which
    means that the check for major version = 15 in the simulator turns out to be a
    constant value now (since we don't support running in the simulator on a
    4-year-old macOS version).
    
    With .NET, Environment.OSVersion returns the iOS version instead, which means
    that the check would have to be reworked.
    
    However, since these version checks have effectively been constant for a few
    years now, we can just remove the checks and the resulting unreachable code.

--- tests/monotouch-test/MetalPerformanceShaders/MPSImageHistogramEqualizationTest.cs
+++ tests/monotouch-test/MetalPerformanceShaders/MPSImageHistogramEqualizationTest.cs
@@ -24,8 +24,8 @@ namespace MonoTouchFixtures.MetalPerformanceShaders
 #if !MONOMAC
 			TestRuntime.AssertXcodeVersion (7, 0);
 
-			if (Runtime.Arch == Arch.SIMULATOR && Environment.OSVersion.Version.Major >= 15)
-				Assert.Inconclusive ("Metal is not supported in the simulator on macOS 10.15");
+			if (Runtime.Arch == Arch.SIMULATOR)
+				Assert.Inconclusive ("Metal Performance Shaders is not supported in the simulator");
 #else
 			TestRuntime.AssertXcodeVersion (9, 0);
 #endif
