c394c12c543a276160320facf58bfe5d1ae7e8b1
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-07-17 11:09:09 +0200

    [xharness] De-asyncify template expansion. (#9104)
    This avoids:
    
    * Getting the resource stream for every template expansion
    * Reading the resource stream
    * A lot of async code.
    
    Instead just read the resource stream once, into a string, and return that.
    
    This speeds up the startup by ~10% (from ~0.96s to ~0.83s on my machine).

--- tests/xharness/Microsoft.DotNet.XHarness.iOS.Shared/TestImporter/Templates/Managed/InfoPlistGenerator.cs
+++ tests/xharness/Microsoft.DotNet.XHarness.iOS.Shared/TestImporter/Templates/Managed/InfoPlistGenerator.cs
@@ -11,23 +11,18 @@ namespace Microsoft.DotNet.XHarness.iOS.Shared.TestImporter.Templates.Managed {
 		public static readonly string IndentifierReplacement = "%BUNDLE INDENTIFIER%";
 		public static readonly string WatchAppIndentifierReplacement = "%WATCHAPP INDENTIFIER%";
 
-		public static async Task<string> GenerateCodeAsync (Stream template, string projectName)
+		public static string GenerateCode (string template, string projectName)
 		{
 			if (template == null)
 				throw new ArgumentNullException (nameof (template));
 			if (projectName == null)
 				throw new ArgumentNullException (nameof (projectName));
 			// got the lines we want to add, read the template and substitute
-			using (var reader = new StreamReader (template)) {
-				var result = await reader.ReadToEndAsync ();
-				result = result.Replace (ApplicationNameReplacement, projectName);
-				result = result.Replace (IndentifierReplacement, $"com.xamarin.bcltests.{projectName}");
-				result = result.Replace (WatchAppIndentifierReplacement, $"com.xamarin.bcltests.{projectName}.container");
-				return result;
-			}
+			var result = template;
+			result = result.Replace (ApplicationNameReplacement, projectName);
+			result = result.Replace (IndentifierReplacement, $"com.xamarin.bcltests.{projectName}");
+			result = result.Replace (WatchAppIndentifierReplacement, $"com.xamarin.bcltests.{projectName}.container");
+			return result;
 		}
-
-		// Generates the code for the type registration using the give path to the template to use
-		public static string GenerateCode (Stream template, string projectName) => GenerateCodeAsync (template, projectName).Result;
 	}
 }
