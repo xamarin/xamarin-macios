6b066e0e85512df808181243e440757b7777031d
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-07-23 12:58:46 +0200

    [dotnet] Only pass a single custom step to the linker.
    The linker will load the assemblies with the custom steps once per custom step
    argument, which means that each step is effectively in a different assembly,
    making it impossible to share state between steps.
    
    This behavior is filed as a linker bug: https://github.com/mono/linker/issues/1314
    
    Until this is fixed, we can just have a single step that injects all the other
    steps programmatically.

--- tools/dotnet-linker/SetupStep.cs
+++ tools/dotnet-linker/SetupStep.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 
+using Mono.Linker;
 using Mono.Linker.Steps;
 
 using Xamarin.Bundler;
@@ -10,11 +13,34 @@ namespace Xamarin {
 
 	public class SetupStep : ConfigurationAwareStep {
 
+		List<IStep> _steps;
+		public List<IStep> Steps {
+			get {
+				if (_steps == null) {
+					var pipeline = typeof (LinkContext).GetProperty ("Pipeline").GetGetMethod ().Invoke (Context, null);
+					_steps = (List<IStep>) pipeline.GetType ().GetField ("_steps", BindingFlags.Instance | BindingFlags.NonPublic).GetValue (pipeline);
+				}
+				return _steps;
+			}
+		}
+
 		protected override void Process ()
 		{
-			// This will be replaced with something more useful later.
-			Console.WriteLine ("Hello SetupStep");
+			// Don't use --custom-step to load each step, because this assembly
+			// is loaded into the current process once per --custom-step,
+			// which makes it very difficult to share state between steps.
+			Steps.Add (new GenerateMainStep ());
+			Steps.Add (new GatherFrameworksStep ());
+
 			Configuration.Write ();
+
+			if (Configuration.Verbosity > 0) {
+				Console.WriteLine ();
+				Console.WriteLine ("Pipeline Steps:");
+				foreach (var step in Steps) {
+					Console.WriteLine ($"    {step}");
+				}
+			}
 
 			ErrorHelper.Platform = Configuration.Platform;
 			Directory.CreateDirectory (Configuration.ItemsDirectory);
