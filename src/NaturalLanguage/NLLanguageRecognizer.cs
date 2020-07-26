cb132d684bb99d21b2cd8dc4105353f7f12fe8a3
Marius Ungureanu <therzok@gmail.com>
2020-07-26 16:24:28 +0300

    Remove completely unused System.Linq references
    Contributes to fixing #9019

--- src/NaturalLanguage/NLLanguageRecognizer.cs
+++ src/NaturalLanguage/NLLanguageRecognizer.cs
@@ -20,7 +20,6 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 using System;
-using System.Linq;
 using System.Collections.Generic;
 
 using Foundation;
--- src/Security/Trust.cs
+++ src/Security/Trust.cs
@@ -28,7 +28,6 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 using System;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security;
 using System.Security.Cryptography.X509Certificates;
