2af386408312f729b06b790eb9d73faa04b8cd28
Matthew Leibowitz <mattleibow@live.com>
2020-07-27 08:57:09 +0200

    Make sure to Uri escape paths
    If the path to the project or compiled app has spaces, then the URI
    that is generated for the on-demand resources in the
    AssetPackManifestTemplate.plist file is invalid
    
    Fixes https://github.com/xamarin/xamarin-macios/issues/8452

--- msbuild/Xamarin.MacDev.Tasks.Core/Tasks/CreateAssetPackManifestTaskBase.cs
+++ msbuild/Xamarin.MacDev.Tasks.Core/Tasks/CreateAssetPackManifestTaskBase.cs
@@ -134,7 +134,7 @@ namespace Xamarin.MacDev.Tasks
 				}
 
 				// update AssetPackManifestTemplate.plist
-				resource.Add ("URL", new PString ("http://127.0.0.1" + Path.GetFullPath (dir)));
+				resource.Add ("URL", new PString ("http://127.0.0.1" + Uri.EscapeUriString (Path.GetFullPath (dir))));
 				resource.Add ("bundleKey", new PString (bundleIdentifier));
 
 				if (!double.IsNaN (priority))
