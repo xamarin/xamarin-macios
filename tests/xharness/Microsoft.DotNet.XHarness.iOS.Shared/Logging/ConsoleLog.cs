740a12e26c5c042b4a3c0c7811fde5225e462497
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-08-03 18:51:01 +0200

    [xharness] Implement Log.GetReader for all loggers. (#9257)
    This makes it easier to write code that parses logs, since that code won't
    have to worry about if a particular log supports GetReader or not.

--- tests/xharness/Microsoft.DotNet.XHarness.iOS.Shared/Logging/ConsoleLog.cs
+++ tests/xharness/Microsoft.DotNet.XHarness.iOS.Shared/Logging/ConsoleLog.cs
@@ -1,34 +1,11 @@
 ﻿using System;
-using System.IO;
-using System.Text;
 
 namespace Microsoft.DotNet.XHarness.iOS.Shared.Logging {
 	// A log that writes to standard output
-	public class ConsoleLog : Log {
-		readonly StringBuilder captured = new StringBuilder ();
+	public class ConsoleLog : CallbackLog {
 
-		protected override void WriteImpl (string value)
-		{
-			lock (captured)
-				captured.Append (value);
-			Console.Write (value);
-		}
-
-		public override string FullPath => throw new NotSupportedException ();
-
-		public override StreamReader GetReader ()
-		{
-			lock (captured) {
-				var str = new MemoryStream (Encoding.GetBytes (captured.ToString ()));
-				return new StreamReader (str, Encoding, false);
-			}
-		}
-
-		public override void Flush ()
-		{
-		}
-
-		public override void Dispose ()
+		public ConsoleLog ()
+			: base (Console.Write, "Console log")
 		{
 		}
 	}
