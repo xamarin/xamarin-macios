d96827cd893a983eb6a925ed1b0e042a7fd5da04
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-08-10 13:55:54 +0200

    [msbuild] Share the _CompileEntitlements target. (#9323)
    Use two separate output variables (EntitlementsInExecutable/EntitlementsInSignature)
    instead of using the same output variable for two different purposes. This makes
    the code more self-explanatory.
    
    Also move the simulator check to the C# code, that way it's easier to re-use elsewhere.
    
    * [msbuild] Share the _CompileEntitlements task.
    
    * [msbuild] Create the same class hierarchy for Xamarin.Mac and Xamarin.iOS for the CompileEntitlements task.
    
        CompileEntitlementsTaskBase
        └─── iOS/CompileEntitlementsTaskCore
        │    └─── iOS/CompileEntitlements
        └─── Mac/CompileEntitlementsTaskCore
             └─── Mac/CompileEntitlements
    
    This also means we can remove a known failure in the list of MSBuild tasks that don't
    conform to our 'no code in final task implementation' requirements.

---
+++ msbuild/Xamarin.Mac.Tasks.Core/Tasks/CompileEntitlementsTaskCore.cs
@@ -0,0 +1,48 @@
+using System.IO;
+using System.Collections.Generic;
+
+using Xamarin.MacDev;
+using Xamarin.MacDev.Tasks;
+
+namespace Xamarin.Mac.Tasks {
+	public abstract class CompileEntitlementsCore : CompileEntitlementsTaskBase {
+		static readonly HashSet<string> allowedProvisioningKeys = new HashSet<string> {
+			"com.apple.application-identifier",
+			"com.apple.developer.aps-environment",
+			"com.apple.developer.default-data-protection",
+			//"com.apple.developer.icloud-container-development-container-identifiers",
+			//"com.apple.developer.icloud-container-identifiers",
+			//"com.apple.developer.icloud-container-environment",
+			//"com.apple.developer.icloud-services",
+			"com.apple.developer.pass-type-identifiers",
+			"com.apple.developer.team-identifier",
+			//"com.apple.developer.ubiquity-container-identifiers",
+			"get-task-allow",
+		};
+
+		protected override string ApplicationIdentifierKey {
+			get { return "com.apple.application-identifier"; }
+		}
+
+		protected override HashSet<string> AllowedProvisioningKeys {
+			get { return allowedProvisioningKeys; }
+		}
+
+		protected override string DefaultEntitlementsPath {
+			get { return Path.Combine (SdkDevPath, "Platforms/MacOSX.platform/Entitlements.plist"); }
+		}
+
+		protected override string EntitlementBundlePath { get { return Path.Combine (AppBundleDir, "Contents", "Resources"); } }
+
+		protected override PDictionary GetCompiledEntitlements (MobileProvision profile, PDictionary template)
+		{
+			var entitlements = base.GetCompiledEntitlements (profile, template);
+			PBoolean sandbox;
+
+			if (Debug && entitlements.TryGetValue ("com.apple.security.app-sandbox", out sandbox) && sandbox.Value)
+				entitlements ["com.apple.security.network.client"] = new PBoolean (true);
+
+			return entitlements;
+		}
+	}
+}
