f99ef7b0128ece966f67d253643c4390b7d3dd8f
Whitney Schmidt <whschm@microsoft.com>
2020-08-13 13:13:29 -0400

    partial fix for https://github.com/xamarin/xamarin-macios/issues/9267

--- src/AVFoundation/AVAudioFormat.cs
+++ src/AVFoundation/AVAudioFormat.cs
@@ -21,11 +21,19 @@ namespace AVFoundation {
 	public partial class AVAudioFormat {
 		public static bool operator == (AVAudioFormat a, AVAudioFormat b)
 		{
+			if ((object) a == (object) b)
+				return true;
+			if ((object) a == null ^ (object) b == null)
+				return false;
 			return a.Equals (b);
 		}
 		
 		public static bool operator != (AVAudioFormat a, AVAudioFormat b)
 		{
+			if ((object) a != (object) b)
+				return true;
+			if ((object) a == null ^ (object) b == null)
+				return true;
 			return !a.Equals (b);
 		}
 	}
