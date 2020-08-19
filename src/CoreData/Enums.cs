89eae78a72c72b4980403126e32d7246935b407a
Manuel de la Pena <mandel@microsoft.com>
2020-08-19 14:40:08 -0400

    [CoreData] Update framework for Xcode 12 beta 4.

--- src/CoreData/Enums.cs
+++ src/CoreData/Enums.cs
@@ -226,4 +226,21 @@ namespace CoreData {
 		DryRun = 1 << 1,
 		PrintSchema = 1 << 2,
 	}
+
+	[Watch (7,0), TV (14,0), Mac (11,0), iOS (14,0)]
+	[Native]
+	public enum NSPersistentCloudKitContainerEventResultType : long {
+		Events = 0,
+		CountEvents,
+	}
+
+	[Watch (7,0), TV (14,0), Mac (11,0), iOS (14,0)]
+	[Native]
+	public enum NSPersistentCloudKitContainerEventType : long {
+		Setup,
+		Import,
+		Export, 
+	}
+
+
 }
