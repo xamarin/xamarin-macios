ae0c125b4df7088ba943f9cb537eadeebfba1976
Sebastien Pouliot <sebastien.pouliot@microsoft.com>
2020-08-21 16:00:54 -0400

    [tests][monotouch-test] Re-enable some ARKit tests
    Things are a bit better with beta 5
    part of #8943

--- tests/monotouch-test/ARKit/ARAnchorTest.cs
+++ tests/monotouch-test/ARKit/ARAnchorTest.cs
@@ -28,8 +28,6 @@ namespace MonoTouchFixtures.ARKit {
 		{
 			TestRuntime.AssertXcodeVersion (10, 0);
 			var faceAnchor = new ARAnchor ("My Anchor", MatrixFloat4x4.Identity);
-			if ((Runtime.Arch == Arch.SIMULATOR) && TestRuntime.CheckXcodeVersion (12, 0))
-				Assert.Ignore ("broken with beta 1");
 			Assert.AreEqual (MatrixFloat4x4.Identity, faceAnchor.Transform, "Transform");
 		}
 	}
