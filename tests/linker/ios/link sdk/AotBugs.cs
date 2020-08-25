a44fdae53d18096e928b6fc9bad10b143914c520
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-08-25 17:15:04 +0200

    [tests] Ignore a link sdk test that calls MulticastDelegate.BeginInvoke.
    MulticastDelegate.BeginInvoke isn't supported in .NET.
    
    Ref: https://github.com/dotnet/runtime/issues/16312

--- tests/linker/ios/link sdk/AotBugs.cs
+++ tests/linker/ios/link sdk/AotBugs.cs
@@ -469,6 +469,9 @@ namespace LinkSdk.Aot {
 		}
 
 		[Test]
+#if NET
+		[Ignore ("MulticastDelegate.BeginInvoke isn't supported in .NET (https://github.com/dotnet/runtime/issues/16312)")]
+#endif
 		public void Bug5354 ()
 		{
 			Action<string> testAction = (string s) => { s.ToString (); };
