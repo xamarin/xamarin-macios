561214eb071cac95b127d522344fba27eea7c63f
Alex Soto <alex@alexsoto.me>
2020-08-26 00:46:10 -0400

    [xcode12] Bump PR for Xcode 12 Beta 6 (#9507)

--- tools/common/Frameworks.cs
+++ tools/common/Frameworks.cs
@@ -547,7 +547,7 @@ public class Frameworks : Dictionary <string, Framework>
 	// This checks if a framework is unavailable due to bugs in Xcode (such as Apple forgetting to ship a library or headers for a framework, which seems to happen at least once a year).
 	public static bool IsFrameworkBroken (Application app, string framework)
 	{
-		if (app.IsSimulatorBuild && Driver.XcodeProductVersion == "12A8189h" /* Xcode 12 beta 5 */) {
+		if (app.IsSimulatorBuild && Driver.XcodeProductVersion == "12A8189n" /* Xcode 12 beta 6 */) {
 			switch (framework) {
 			// Apple seems to have forgotten to ship the several libraries for the simulator in Xcode 12 betas (it's still available for device builds).
 			// https://github.com/xamarin/maccore/issues/2266
