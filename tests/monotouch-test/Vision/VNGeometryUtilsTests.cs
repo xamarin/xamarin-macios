c5ea0a57dba4c2eca2f34fd85dc3fcccfe701e4e
Alex Soto <alex@alexsoto.me>
2020-08-27 22:43:11 -0400

    fix assert messages

--- tests/monotouch-test/Vision/VNGeometryUtilsTests.cs
+++ tests/monotouch-test/Vision/VNGeometryUtilsTests.cs
@@ -40,7 +40,7 @@ namespace MonoTouchFixtures.Vision {
 
 			var ncircle = VNGeometryUtils.CreateBoundingCircle (nvectors, out var nerror);
 			Assert.Null (nerror, "nerror was not null");
-			Assert.NotNull (ncircle, "ncircle not null");
+			Assert.NotNull (ncircle, "ncircle was null");
 
 			var vectors = new [] {
 				new Vector2 (1,1),
@@ -51,7 +51,7 @@ namespace MonoTouchFixtures.Vision {
 
 			var circle = VNGeometryUtils.CreateBoundingCircle (vectors, out var error);
 			Assert.Null (error, "Error was not null");
-			Assert.NotNull (circle, "circle not null");
+			Assert.NotNull (circle, "circle was null");
 
 			Assert.AreEqual (ncircle.Diameter, circle.Diameter, "Diameter");
 			Assert.AreEqual (ncircle.Radius, circle.Radius, "Radius");
