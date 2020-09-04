97b842f3da1ddc2da72ff95115a66b7441d68228
TJ Lambert <tjlambert@microsoft.com>
2020-09-04 18:05:29 -0500

    removed switch, added dictionary, and fixed small bug

--- msbuild/tests/Xamarin.iOS.Tasks.Tests/TaskTests/LocalizationStringTest.cs
+++ msbuild/tests/Xamarin.iOS.Tasks.Tests/TaskTests/LocalizationStringTest.cs
@@ -64,8 +64,8 @@ namespace Xamarin.iOS.Tasks {
 
 				Assert.IsFalse (task.Execute (), "Execute failure");
 				Assert.AreEqual (count+1, Engine.Logger.ErrorEvents.Count, "ErrorCount");
-				count++; 
 				bool isTranslated = TranslationAvailable (culture, Engine.Logger.ErrorEvents[count].Message);
+				count++;
 				if (!isTranslated)
 					reportString += culture + ": is not supported correctly. ";
 			}
@@ -73,52 +73,27 @@ namespace Xamarin.iOS.Tasks {
 		}
 
 		public bool TranslationAvailable (string locale, string respectiveErrorMessage) {
-			bool isTranslated = false;
-			switch (locale) {
-			case "cs-CZ": 
-				isTranslated = respectiveErrorMessage.Contains ("došlo k chybě: neznámý formát image");
-				break;
-			case "de-DE": 
-				isTranslated = respectiveErrorMessage.Contains ("Unbekanntes Imageformat.");
-				break;
-			case "en-US": 
-				isTranslated = respectiveErrorMessage.Contains ("Unknown image format.");
-				break;
-			case "es-ES": 
-				isTranslated = respectiveErrorMessage.Contains ("formato de imagen desconocido.");
-				break;
-			case "fr-FR": 
-				isTranslated = respectiveErrorMessage.Contains ("format d'image inconnu.");
-				break;
-			case "it-IT": 
-				isTranslated = respectiveErrorMessage.Contains ("Formato immagine sconosciuto.");
-				break;
-			case "ja-JP": 
-				isTranslated = respectiveErrorMessage.Contains ("の読み込みでエラーが発生しました: 画像の形式が不明です。");
-				break;
-			case "ko-KR": 
-				isTranslated = respectiveErrorMessage.Contains ("을(를) 로드하는 동안 오류 발생: 알 수 없는 이미지 형식입니다.");
-				break;
-			case "pl-PL": 
-				isTranslated = respectiveErrorMessage.Contains ("nieznany format obrazu.");
-				break;
-			case "pt-BR": 
-				isTranslated = respectiveErrorMessage.Contains ("formato de imagem desconhecido.");
-				break;
-			case "ru-RU": 
-				isTranslated = respectiveErrorMessage.Contains ("неизвестный формат изображения.");
-				break;
-			case "tr-TR": 
-				isTranslated = respectiveErrorMessage.Contains ("yüklenirken hata oluştu: Görüntü biçimi bilinmiyor.");
-				break;
-			case "zh-CN": 
-				isTranslated = respectiveErrorMessage.Contains ("时出错: 未知图像格式");
-				break;
-			case "zh-TW": 
-				isTranslated = respectiveErrorMessage.Contains ("時發生錯誤: 未知的映像格式。");
-				break;
-			}
-			return isTranslated;
+			var translatedMessages = new Dictionary<string, string> (){
+				["cs-CZ"] = "došlo k chybě: neznámý formát image",
+				["de-DE"] =  "Unbekanntes Imageformat.",
+				["en-US"] =  "Unknown image format.",
+				["es-ES"] =  "formato de imagen desconocido.",
+				["fr-FR"] =  "format d'image inconnu.",
+				["it-IT"] =  "Formato immagine sconosciuto.",
+				["ja-JP"] =  "の読み込みでエラーが発生しました: 画像の形式が不明です。",
+				["ko-KR"] =  "을(를) 로드하는 동안 오류 발생: 알 수 없는 이미지 형식입니다.",
+				["pl-PL"] =  "nieznany format obrazu.",
+				["pt-BR"] =  "formato de imagem desconhecido.",
+				["ru-RU"] =  "неизвестный формат изображения.",
+				["tr-TR"] =  "yüklenirken hata oluştu: Görüntü biçimi bilinmiyor.",
+				["zh-CN"] =  "时出错: 未知图像格式",
+				["zh-TW"] =  "時發生錯誤: 未知的映像格式。",
+			};
+
+			if (translatedMessages.ContainsKey (locale) && respectiveErrorMessage.Contains (translatedMessages[locale]))
+				return true;
+			else
+				return false;
 		}
 	}
 }
