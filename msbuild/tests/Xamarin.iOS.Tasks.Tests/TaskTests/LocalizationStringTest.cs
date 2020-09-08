725e902e6b39a49e99ae6dc0e542acea5198c47e
TJ Lambert <tjlambert@microsoft.com>
2020-09-08 10:50:26 -0500

    removing first test and helper function

--- msbuild/tests/Xamarin.iOS.Tasks.Tests/TaskTests/LocalizationStringTest.cs
+++ msbuild/tests/Xamarin.iOS.Tasks.Tests/TaskTests/LocalizationStringTest.cs
@@ -9,37 +9,21 @@ using System.Collections.Generic;
 namespace Xamarin.iOS.Tasks {
 	[TestFixture]
 	public class LocalizationStringTest : TestBase {
-
-		[Test]
-		public void OperatingSystemTranslations ()
-		{
-			CultureInfo currentCulture = Thread.CurrentThread.CurrentUICulture;
-			string locale = currentCulture.Name;
-
-			var task = CreateTask<CollectITunesArtwork> ();
-			task.ITunesArtwork = new TaskItem [] { new TaskItem (Assembly.GetExecutingAssembly ().Location) };
-
-			Assert.IsFalse (task.Execute (), "Execute failure");
-			Assert.AreEqual (1, Engine.Logger.ErrorEvents.Count, "ErrorCount");
-			bool isTranslated = TranslationAvailable (locale, Engine.Logger.ErrorEvents [0].Message);
-			Assert.IsTrue (isTranslated, "Your current locale is not supported: " + locale + ".");
-		}
-
-		[TestCase ("cs-CZ")]
-		[TestCase ("de-DE")]
-		[TestCase ("en-US")]
-		[TestCase ("es-ES")]
-		[TestCase ("fr-FR")]
-		[TestCase ("it-IT")]
-		[TestCase ("ja-JP")]
-		[TestCase ("ko-KR")]
-		[TestCase ("pl-PL")]
-		[TestCase ("pt-BR")]
-		[TestCase ("ru-RU")]
-		[TestCase ("tr-TR")]
-		[TestCase ("zh-CN")]
-		[TestCase ("zh-TW")]
-		public void AllSupportedTranslations (string culture)
+		[TestCase ("cs-CZ", "došlo k chybě: neznámý formát image")]
+		[TestCase ("de-DE", "Unbekanntes Imageformat.")]
+		[TestCase ("en-US", "Unknown image format.")]
+		[TestCase ("es-ES", "formato de imagen desconocido.")]
+		[TestCase ("fr-FR", "format d'image inconnu.")]
+		[TestCase ("it-IT", "Formato immagine sconosciuto.")]
+		[TestCase ("ja-JP", "の読み込みでエラーが発生しました: 画像の形式が不明です。")]
+		[TestCase ("ko-KR", "을(를) 로드하는 동안 오류 발생: 알 수 없는 이미지 형식입니다.")]
+		[TestCase ("pl-PL", "nieznany format obrazu.")]
+		[TestCase ("pt-BR", "formato de imagem desconhecido.")]
+		[TestCase ("ru-RU", "неизвестный формат изображения.")]
+		[TestCase ("tr-TR", "yüklenirken hata oluştu: Görüntü biçimi bilinmiyor.")]
+		[TestCase ("zh-CN", "时出错: 未知图像格式")]
+		[TestCase ("zh-TW", "時發生錯誤: 未知的映像格式。")]
+		public void AllSupportedTranslations (string culture, string errorMessage)
 		{
 			CultureInfo originalCulture = Thread.CurrentThread.CurrentUICulture;
 			CultureInfo newCulture;
@@ -56,34 +40,10 @@ namespace Xamarin.iOS.Tasks {
 
 			Assert.IsFalse (task.Execute (), "Execute failure");
 			Assert.AreEqual (1, Engine.Logger.ErrorEvents.Count, "ErrorCount");
-			bool isTranslated = TranslationAvailable (culture, Engine.Logger.ErrorEvents[0].Message);
+			bool isTranslated = Engine.Logger.ErrorEvents[0].Message.Contains (errorMessage);
 			Assert.IsTrue (isTranslated, culture + ": is not supported correctly. ");
 
 			Thread.CurrentThread.CurrentUICulture = originalCulture;
-		}
-
-		public bool TranslationAvailable (string locale, string respectiveErrorMessage) {
-			var translatedMessages = new Dictionary<string, string> (){
-				["cs-CZ"] = "došlo k chybě: neznámý formát image",
-				["de-DE"] =  "Unbekanntes Imageformat.",
-				["en-US"] =  "Unknown image format.",
-				["es-ES"] =  "formato de imagen desconocido.",
-				["fr-FR"] =  "format d'image inconnu.",
-				["it-IT"] =  "Formato immagine sconosciuto.",
-				["ja-JP"] =  "の読み込みでエラーが発生しました: 画像の形式が不明です。",
-				["ko-KR"] =  "을(를) 로드하는 동안 오류 발생: 알 수 없는 이미지 형식입니다.",
-				["pl-PL"] =  "nieznany format obrazu.",
-				["pt-BR"] =  "formato de imagem desconhecido.",
-				["ru-RU"] =  "неизвестный формат изображения.",
-				["tr-TR"] =  "yüklenirken hata oluştu: Görüntü biçimi bilinmiyor.",
-				["zh-CN"] =  "时出错: 未知图像格式",
-				["zh-TW"] =  "時發生錯誤: 未知的映像格式。",
-			};
-
-			if (translatedMessages.ContainsKey (locale) && respectiveErrorMessage.Contains (translatedMessages[locale]))
-				return true;
-			else
-				return false;
 		}
 	}
 }
