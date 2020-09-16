57e860f1da3c4b3e8f8534c3a8b85562a93afab6
Whitney Schmidt <whschm@microsoft.com>
2020-09-16 18:57:39 -0400

    fix up unit tests

--- tests/apitest/src/ScriptingBridge/SBApplicationTest.cs
+++ tests/apitest/src/ScriptingBridge/SBApplicationTest.cs
@@ -8,105 +8,87 @@ using ScriptingBridge;
 
 namespace Xamarin.Mac.Tests {
 
-	public class MySbApp : SBApplication {
-		public MySbApp () : base (NSObjectFlag.Empty) { }
+	public class MySBApp : SBApplication {
+		public MySBApp () : base (NSObjectFlag.Empty) { }
 
-		public MySbApp (NSCoder coder) : base (coder) { }
+		public MySBApp (NSCoder coder) : base (coder) { }
 
-		public MySbApp (NSUrl url) : base (url) { }
+		public MySBApp (NSUrl url) : base (url) { }
 
-		public MySbApp (int pid) : base (pid) { }
+		public MySBApp (int pid) : base (pid) { }
 
-		public MySbApp (string ident) : base (ident) { }
+		public MySBApp (string ident) : base (ident) { }
 
-		protected MySbApp (NSObjectFlag t) : base (t) { }
+		protected MySBApp (NSObjectFlag t) : base (t) { }
 
-		protected internal MySbApp (IntPtr handle) : base (handle) { }
+		protected internal MySBApp (IntPtr handle) : base (handle) { }
 	}
 
 	[TestFixture]
 	public class SBApplicationTest {
 
-		[TestFixture]
-		public class GetSBApplicationTest {
-
-			[Test]
-			public void TestGetApplicationFromBundleIdentifier ()
-			{
-				const string knownBundle = "com.apple.finder";
-				const string unknownBundle = "com.unknown.bundle";
+		[Test]
+		public void TestGetApplicationFromBundleIdentifier ()
+		{
+			const string knownBundle = "com.apple.finder";
+			const string unknownBundle = "com.unknown.bundle";
 #if !XAMCORE_4_0
-				var app2 = SBApplication.FromBundleIdentifier<MySbApp> (knownBundle);
-				Assert.IsNotNull (app2);
-				var app1 = SBApplication.FromBundleIdentifier (unknownBundle);
-				Assert.IsNotNull (app1);
-				var app4 = SBApplication.FromBundleIdentifier<MySbApp> (unknownBundle);
-
-				var app3 = SBApplication.FromBundleIdentifier (unknownBundle);
-				Assert.IsNull (app3);
-				Assert.IsNull (app4);
+			using (var app1 = SBApplication.FromBundleIdentifier (knownBundle))
+			using (var app2 = SBApplication.FromBundleIdentifier<MySBApp> (knownBundle))
+			using (var app4 = SBApplication.FromBundleIdentifier<MySBApp> (unknownBundle))
+			using (var app3 = SBApplication.FromBundleIdentifier (unknownBundle))
 #else
-				var app1 = SBApplication.GetApplicationFromBundleIdentifier (knownBundle);
-				Assert.IsNotNull (app1);
-				var app2 = SBApplication.GetApplicationFromBundleIdentifier<MySbApp> (knownBundle);
-				Assert.IsNotNull (app2);
-				var app3 = SBApplication.GetApplicationFromBundleIdentifier (unknownBundle);
-				var app4 = SBApplication.GetApplicationFromBundleIdentifier<MySbApp> (unknownBundle);
-				Assert.IsNull (app3);
-				Assert.IsNull (app4);
+			var (app1 = SBApplication.GetApplicationFromBundleIdentifier (knownBundle))
+			var (app2 = SBApplication.GetApplicationFromBundleIdentifier<MySbApp> (knownBundle))
+			var (app3 = SBApplication.GetApplicationFromBundleIdentifier (unknownBundle))
+			var (app4 = SBApplication.GetApplicationFromBundleIdentifier<MySbApp> (unknownBundle))
 #endif
+			{
+				Assert.IsNotNull (app1, "SBApplication from known bundle is null");
+				Assert.IsNotNull (app2, "MySBApp from known bundle is null");
+				Assert.IsNull (app3, "SBApplication from unknown bundle is non-null");
+				Assert.IsNull (app4, "MySBApp from unknown bundle is non-null");
 			}
+		}
 
-			[Test]
-			public void TestGetApplicationFromUrl ()
-			{
-				NSUrl knownUrl = new NSUrl("http://www.xamarin.com");
-				NSUrl unknownUrl = new NSUrl ("thisisnotaurl");
+		[Test]
+		public void TestGetApplicationFromUrl ()
+		{
+			using (NSUrl knownUrl = new NSUrl ("http://www.xamarin.com"))
 #if !XAMCORE_4_0
-				var app1 = SBApplication.FromURL (knownUrl);
-				Assert.IsNotNull (app1);
-				var app2 = SBApplication.FromURL<MySbApp> (knownUrl);
-				Assert.IsNotNull (app2);
-				var app3 = SBApplication.FromURL (unknownUrl);
-				var app4 = SBApplication.FromURL<MySbApp> (unknownUrl);
-				Assert.IsNull (app3);
-				Assert.IsNull (app4);
+			using (var app1 = SBApplication.FromURL (knownUrl))
+			using (var app2 = SBApplication.FromURL<MySBApp> (knownUrl))
 #else
-				var app1 = SBApplication.GetApplicationFromUrl (knownUrl);
-				Assert.IsNotNull (app1);
-				var app2 = SBApplication.GetApplicationFromUrl<MySbApp> (knownUrl);
-				Assert.IsNotNull (app2);
-				var app3 = SBApplication.GetApplicationFromUrl (unknownUrl);
-				var app4 = SBApplication.GetApplicationFromUrl<MySbApp> (unknownUrl);
-				Assert.IsNull (app3);
-				Assert.IsNull (app4);
+			using (var app1 = SBApplication.GetApplicationFromUrl (knownUrl))
+			using (var app2 = SBApplication.GetApplicationFromUrl<MySbApp> (knownUrl))
 #endif
+			{
+				Assert.IsNotNull (app1, "SBApplication from known URL is null");
+				Assert.IsNotNull (app2, "MySBApp from known URL is null");
 			}
+		}
 
-			[Test]
-			public void TestGetApplicationFromPid ()
-			{
-				int knownPid = 1;
-				int unknownPid = -1; // valid pid is > 0?
+		[Test]
+		public void TestGetApplicationFromPid ()
+		{
+			int knownPid = System.Diagnostics.Process.GetCurrentProcess ().Id;
+			int unknownPid = -1; // valid pid is > 0
 #if !XAMCORE_4_0
-				var app1 = SBApplication.FromProcessIdentifier (knownPid);
-				Assert.IsNotNull (app1);
-				var app2 = SBApplication.FromProcessIdentifier<MySbApp> (knownPid);
-				Assert.IsNotNull (app2);
-				var app3 = SBApplication.FromProcessIdentifier (unknownPid);
-				var app4 = SBApplication.FromProcessIdentifier<MySbApp> (unknownPid);
-				Assert.IsNull (app3);
-				Assert.IsNull (app4);
+			using (var app1 = SBApplication.FromProcessIdentifier (knownPid))
+			using (var app2 = SBApplication.FromProcessIdentifier<MySBApp> (knownPid))
+			using (var app3 = SBApplication.FromProcessIdentifier (unknownPid))
+			using (var app4 = SBApplication.FromProcessIdentifier<MySBApp> (unknownPid))
 #else
-				var app1 = SBApplication.GetApplicationFromProcessIdentifier (knownPid);
-				Assert.IsNotNull (app1);
-				var app2 = SBApplication.GetApplicationFromProcessIdentifier<MySbApp> (knownPid);
-				Assert.IsNotNull (app2);
-				var app3 = SBApplication.GetApplicationFromProcessIdentifier (unknownPid);
-				var app4 = SBApplication.GetApplicationFromProcessIdentifier<MySbApp> (unknownPid);
-				Assert.IsNull (app3);
-				Assert.IsNull (app4);
+			using (var app1 = SBApplication.GetApplicationFromProcessIdentifier (knownPid))
+			using (var app2 = SBApplication.GetApplicationFromProcessIdentifier<MySbApp> (knownPid))
+			using (var app3 = SBApplication.GetApplicationFromProcessIdentifier (unknownPid))
+			using (var app4 = SBApplication.GetApplicationFromProcessIdentifier<MySbApp> (unknownPid)
 #endif
+			{
+				Assert.IsNotNull (app1, "SBApplication from known pid is null");
+				Assert.IsNotNull (app2, "MySBApp from known pid is null");
+				Assert.IsNotNull (app3, "SBApplication from unknown pid is null");
+				Assert.IsNotNull (app4, "MySBApp from unknown pid is null");
 			}
 		}
 	}
