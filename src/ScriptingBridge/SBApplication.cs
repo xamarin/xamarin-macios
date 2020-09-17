da5054885cc0d1de67df7e599b7ebde464ada327
Whitney Schmidt <whschm@microsoft.com>
2020-09-17 12:25:51 -0400

    tidy up

--- src/ScriptingBridge/SBApplication.cs
+++ src/ScriptingBridge/SBApplication.cs
@@ -8,17 +8,17 @@ namespace ScriptingBridge {
 	public partial class SBApplication
 	{
 #if XAMCORE_4_0
-		public static SBApplication GetApplicationFromBundleIdentifier (string ident)  => Runtime.GetNSObject<SBApplication> (_FromBundleIdentifier (ident));
+		public static SBApplication GetApplication (string ident)  => Runtime.GetNSObject<SBApplication> (_FromBundleIdentifier (ident));
 
-		public static T GetApplicationFromBundleIdentifier<T> (string ident) where T : SBApplication => Runtime.GetINativeObject<T> (_FromBundleIdentifier (ident), forced_type: true, owns: false);
+		public static T GetApplication<T> (string ident) where T : SBApplication => Runtime.GetINativeObject<T> (_FromBundleIdentifier (ident), forced_type: true, owns: false);
 
-		public static SBApplication GetApplicationFromUrl (NSUrl url)  => Runtime.GetNSObject<SBApplication> (_FromURL (url) ;
+		public static SBApplication GetApplication (NSUrl url)  => Runtime.GetNSObject<SBApplication> (_FromURL (url) ;
 
-		public static T GetApplicationFromUrl<T> (NSUrl url) where T : SBApplication => Runtime.GetINativeObject<T> (_FromURL (url), forced_type: true, owns: false);
+		public static T GetApplication<T> (NSUrl url) where T : SBApplication => Runtime.GetINativeObject<T> (_FromURL (url), forced_type: true, owns: false);
 
-		public static SBApplication GetApplicationFromProcessIdentifier (int pid)  => Runtime.GetNSObject<SBApplication> (_FromProcessIdentifier (pid));
+		public static SBApplication GetApplication (int pid)  => Runtime.GetNSObject<SBApplication> (_FromProcessIdentifier (pid));
 
-		public static T GetApplicationFromProcessIdentifier<T> (int pid) where T : SBApplication => Runtime.GetINativeObject<T> (_FromProcessIdentifier (pid), forced_type: true, owns: false);
+		public static T GetApplication<T> (int pid) where T : SBApplication => Runtime.GetINativeObject<T> (_FromProcessIdentifier (pid), forced_type: true, owns: false);
 #else
 		public static SBApplication FromBundleIdentifier (string ident)  => Runtime.GetNSObject<SBApplication> (_FromBundleIdentifier (ident));
 
