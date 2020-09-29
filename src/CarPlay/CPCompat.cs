c7010e1b6591787dbca593b59b5a4e433e431ad2
Alex Soto <alex@alexsoto.me>
2020-09-29 11:29:37 -0400

    Apply feedback

--- src/CarPlay/CPCompat.cs
+++ src/CarPlay/CPCompat.cs
@@ -17,19 +17,21 @@ namespace CarPlay {
 	[Register (SkipRegistration = true)]
 	[Introduced (PlatformName.iOS, 14, 0)]
 	[Unavailable (PlatformName.iOS, PlatformArchitecture.All)]
-	[Obsolete ("'CPEntity' has been removed from iOS.")]
+	[Obsolete ("This API has been removed from the native SDK.")]
 	[EditorBrowsable (EditorBrowsableState.Never)]
 	public class CPEntity : NSObject, INSSecureCoding {
 
-		public CPEntity () => throw new NotImplementedException ();
+		public CPEntity () => throw new NotSupportedException ();
 
-		[EditorBrowsable (EditorBrowsableState.Advanced)]
-		protected CPEntity (NSObjectFlag t) => throw new NotImplementedException ();
+		public CPEntity (NSCoder coder) => throw new NotSupportedException ();
 
-		[EditorBrowsable (EditorBrowsableState.Advanced)]
-		protected internal CPEntity (IntPtr handle) => throw new NotImplementedException ();
+		protected CPEntity (NSObjectFlag t) => throw new NotSupportedException ();
 
-		public virtual void EncodeTo (NSCoder coder) => throw new NotImplementedException ();
+		protected internal CPEntity (IntPtr handle) => throw new NotSupportedException ();
+
+		public virtual void EncodeTo (NSCoder encoder) => throw new NotSupportedException ();
+
+		public override IntPtr ClassHandle => throw new NotSupportedException ();
 	}
 }
 #endif
