5deee9ea952b82897142d7c4a85ff6c8d48c4830
Alex Soto <alex@alexsoto.me>
2020-10-02 18:33:38 -0400

    [xcode12.2] Initial bump to Xcode 12.2 Beta 2 (#9730)
    * [xcode12.2] Initial bump to Xcode 12.2 Beta 2
    
    * Add tvOS specific checks for exact Xcode check

--- tests/monotouch-test/MediaAccessibility/ImageCaptioningTest.cs
+++ tests/monotouch-test/MediaAccessibility/ImageCaptioningTest.cs
@@ -67,6 +67,14 @@ namespace MonoTouchFixtures.MediaAccessibility {
 		[Test]
 		public void SetCaption ()
 		{
+#if !__MACOS__
+			// looks like Apple broke something inside Xcode 12.2 Beta 2 in both sim and device
+			// NSInvalidArgumentException Reason: -[__NSCFType _getValue:forType:]: unrecognized selector sent to instance
+			// when calling MAImageCaptioning.SetCaption
+			if (TestRuntime.CheckExactXcodeVersion (12, 2, 2))
+				Assert.Ignore ("Broken in Xcode 12.2 Beta 2");
+#endif
+
 			TestRuntime.AssertXcodeVersion (11, 0);
 			Assert.Throws<ArgumentNullException> (() => MAImageCaptioning.SetCaption (null, "xamarin", out _));
 			// note: calling on a remote URL crash the process - not that it should work but...
