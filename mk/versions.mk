d638eb9ff6f29a4eb2ee8c24a2720a3fb4abeed0
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-10-05 10:32:33 +0200

    Add ikvm-fork as a submodule instead of using the sources embedded in the mono archive.
    There are two reasons for this:
    
    * It grants us independence from the mono archive for .NET 6.
    * We need a bugfix in ikvm, but we can't necessarily bump mono.

--- mk/versions.mk
+++ mk/versions.mk
@@ -68,6 +68,7 @@ $(eval $(call CheckSubmoduleTemplate,Touch.Unit,TOUCH_UNIT))
 $(eval $(call CheckSubmoduleTemplate,opentk,OPENTK))
 $(eval $(call CheckSubmoduleTemplate,Xamarin.MacDev,XAMARIN_MACDEV))
 $(eval $(call CheckSubmoduleTemplate,macios-binaries,MACIOS_BINARIES))
+$(eval $(call CheckSubmoduleTemplate,ikvm-fork,IKVM))
 
 include $(TOP)/mk/xamarin.mk
 
