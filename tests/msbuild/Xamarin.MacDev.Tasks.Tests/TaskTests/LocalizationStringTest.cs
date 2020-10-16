bab42d648778a489e9ea4d6760da52d24948c01f
TJ Lambert <tjlambert@microsoft.com>
2020-10-16 15:09:47 -0500

    Revert "re-adding the original test"
    This reverts commit 4ba2521770a2850bc129e22b041f87d0f759e206.

--- tests/msbuild/Xamarin.MacDev.Tasks.Tests/TaskTests/LocalizationStringTest.cs
+++ tests/msbuild/Xamarin.MacDev.Tasks.Tests/TaskTests/LocalizationStringTest.cs
@@ -11,39 +11,6 @@ using Xamarin.Localization.MSBuild;
 namespace Xamarin.iOS.Tasks {
 	[TestFixture]
 	public class LocalizationStringTest : TestBase {
-		[TestCase ("cs-CZ", "došlo k chybě: neznámý formát image")]
-		[TestCase ("de-DE", "Unbekanntes Imageformat.")]
-		[TestCase ("en-US", "Unknown image format.")]
-		[TestCase ("es-ES", "formato de imagen desconocido.")]
-		[TestCase ("fr-FR", "format d'image inconnu.")]
-		[TestCase ("it-IT", "Formato immagine sconosciuto.")]
-		[TestCase ("ja-JP", "の読み込みでエラーが発生しました: 画像の形式が不明です。")]
-		[TestCase ("ko-KR", "을(를) 로드하는 동안 오류 발생: 알 수 없는 이미지 형식입니다.")]
-		[TestCase ("pl-PL", "nieznany format obrazu.")]
-		[TestCase ("pt-BR", "formato de imagem desconhecido.")]
-		[TestCase ("ru-RU", "неизвестный формат изображения.")]
-		[TestCase ("tr-TR", "yüklenirken hata oluştu: Görüntü biçimi bilinmiyor.")]
-		[TestCase ("zh-CN", "时出错: 未知图像格式")]
-		[TestCase ("zh-TW", "時發生錯誤: 未知的映像格式。")]
-		public void AllSupportedTranslations (string culture, string errorMessage)
-		{
-			CultureInfo originalCulture = Thread.CurrentThread.CurrentUICulture;
-			CultureInfo newCulture;
-			try {
-				newCulture = new CultureInfo (culture);
-				Thread.CurrentThread.CurrentUICulture = newCulture;
-				var task = CreateTask<CollectITunesArtwork> ();
-				task.ITunesArtwork = new TaskItem [] { new TaskItem (Assembly.GetExecutingAssembly ().Location) };
-
-				Assert.IsFalse (task.Execute (), "Execute failure");
-				Assert.AreEqual (1, Engine.Logger.ErrorEvents.Count, "ErrorCount");
-				bool isTranslated = Engine.Logger.ErrorEvents[0].Message.Contains (errorMessage);
-				Assert.IsTrue (isTranslated, culture + ": is not supported correctly. ");
-			} finally {
-				Thread.CurrentThread.CurrentUICulture = originalCulture;
-			}
-		}
-
 		[TestCase ("cs-CZ")]
 		[TestCase ("de-DE")]
 		[TestCase ("es-ES")]
