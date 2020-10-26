ab5ba2f62ca00542a9c29f89980916f21e85d672
TJ Lambert <tjlambert@microsoft.com>
2020-10-26 16:32:41 -0500

    Initial adding of test to go over all translation strings

--- tests/msbuild/Xamarin.MacDev.Tasks.Tests/TaskTests/LocalizationStringTest.cs
+++ tests/msbuild/Xamarin.MacDev.Tasks.Tests/TaskTests/LocalizationStringTest.cs
@@ -7,6 +7,7 @@ using System.Globalization;
 using System.Threading;
 using System.Collections.Generic;
 using Xamarin.Localization.MSBuild;
+using System.ComponentModel;
 
 namespace Xamarin.iOS.Tasks {
 	[TestFixture]
@@ -82,6 +83,87 @@ namespace Xamarin.iOS.Tasks {
 			Thread.CurrentThread.CurrentUICulture = cultureInfo;
 			PropertyInfo propertyInfo = typeof (MSBStrings).GetProperty (errorCode);
 			return (string) propertyInfo.GetValue (null, null);
+		}
+
+		[TestCase ("cs-CZ")]
+		[TestCase ("de-DE")]
+		[TestCase ("es-ES")]
+		[TestCase ("fr-FR")]
+		[TestCase ("it-IT")]
+		[TestCase ("ja-JP")]
+		[TestCase ("ko-KR")]
+		[TestCase ("pl-PL")]
+		[TestCase ("pt-BR")]
+		[TestCase ("ru-RU")]
+		[TestCase ("tr-TR")]
+		[TestCase ("zh-CN")]
+		[TestCase ("zh-TW")]
+		public void AllErrorTranslation (string culture)
+		{
+			string errorCode = string.Empty;
+			string errorList = string.Empty;
+			CultureInfo originalCulture = Thread.CurrentThread.CurrentUICulture;
+			string path = $"{Directory.GetCurrentDirectory ()}/TaskTests/LocalizationIgnore";
+
+			List<string> commonIgnoreList = ReadFile ($"{path}/common-Translations.ignore");
+			List<string> cultureIgnoreList = ReadFile ($"{path}/{culture}-Translations.ignore");
+
+			foreach (var errorCodeInfo in typeof (MSBStrings).GetProperties ()) {
+				try {
+					errorCode = errorCodeInfo.Name.ToString ();
+					if (errorCode == "ResourceManager" || errorCode == "Culture")
+						continue;
+
+					Assert.IsFalse (string.IsNullOrEmpty (errorCode), "Error code is null or empty");
+					string englishError = TranslateError ("en-US", errorCode);
+					string newCultureError = TranslateError (culture, errorCode);
+
+					if (commonIgnoreList.Contains (errorCode)){
+						if (englishError != newCultureError){
+							Assert.IsFalse (true, $"{errorCode} is translated. Remove {errorCode} from ~/LocalizationIgnore/common-Translations.ignore");
+						}
+					}
+					else if (cultureIgnoreList.Contains (errorCode)){
+						if (englishError != newCultureError){
+							Assert.IsFalse (true, $"{errorCode} is translated. Remove {errorCode} from ~/LocalizationIgnore/{culture}-Translations.ignore");
+						}
+					}
+					else if (englishError == newCultureError){
+						errorList += $"{errorCode} ";
+					}
+				} finally {
+					Thread.CurrentThread.CurrentUICulture = originalCulture;
+				}
+			}
+
+			Assert.AreEqual (string.Empty, errorList, $"The following errors were not translated. Add them to ~/LocalizationIgnore/common-Translations.ignore or ~/LocaliztionIgnore/{culture}-Translations.ignore");
+		}
+
+		private List<string> ReadFile (string path){
+			List<string> ignoreList = new List<string> ();
+			string temp;
+			try
+			{
+				using (var sr = new StreamReader (path))
+				{
+					while (!sr.EndOfStream){
+						if (sr.Peek () == -1)
+							break;
+						temp = sr.ReadLine ();
+						if (string.IsNullOrEmpty (temp) || temp.Trim ()[0] == '#'){
+							continue;
+						}
+						else {
+							ignoreList.Add (temp.Trim ());
+						}
+					}
+				}
+			}
+			catch (IOException e)
+			{
+				Assert.IsTrue (false, $"Could not read file at: {path}");
+			}
+			return ignoreList;
 		}
 	}
 }
