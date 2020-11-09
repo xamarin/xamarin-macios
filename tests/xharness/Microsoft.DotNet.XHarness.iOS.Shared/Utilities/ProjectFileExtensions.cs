cb332981826a381ecd66ebc77269ac86b2f5fece
Sebastien Pouliot <sebastien@xamarin.com>
2020-11-09 14:58:48 -0500

    [tests] Update framework tests not to embed/zip the native binaries
    Manually added `NoBindingEmbedding` to bindings .csproj and removed
    `[LinkWith]` attributes.
    
    Also added support for NativeReference to harness and updated the
    workaround for watchOS (since it can't link against ModelIO)

--- tests/xharness/Microsoft.DotNet.XHarness.iOS.Shared/Utilities/ProjectFileExtensions.cs
+++ tests/xharness/Microsoft.DotNet.XHarness.iOS.Shared/Utilities/ProjectFileExtensions.cs
@@ -479,7 +479,7 @@ namespace Microsoft.DotNet.XHarness.iOS.Shared.Utilities {
 
 		public static void FixTestLibrariesReferences (this XmlDocument csproj, string platform)
 		{
-			var nodes = csproj.SelectNodes ("//*[local-name() = 'ObjcBindingNativeLibrary' or local-name() = 'ObjcBindingNativeFramework']");
+			var nodes = csproj.SelectNodes ("//*[local-name() = 'ObjcBindingNativeLibrary' or local-name() = 'ObjcBindingNativeFramework' or local-name() = 'NativeReference']");
 			var test_libraries = new string [] {
 				"libtest.a",
 				"libtest2.a",
@@ -963,6 +963,7 @@ namespace Microsoft.DotNet.XHarness.iOS.Shared.Utilities {
 				new string [] { "FilesToCopyChannels", "Include" },
 				new string [] { "CustomMetalSmeltingInput", "Include" },
 				new string [] { "Metal", "Include" },
+				new string [] { "NativeReference", "Include" },
 			};
 			var nodes_with_variables = new string []
 			{
