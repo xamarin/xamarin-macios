94e44ed4441d7939cd28bf8860d09e924daec21c
Manuel de la Pena <mandel@microsoft.com>
2020-11-16 19:37:43 -0500

    [Maccore] Bump maccore to fix governance issues.
    Commits:
    
    * Insert keychain into search list without overwriting other keychains https://github.com/xamarin/maccore/commit/243810037e19a81e39c8414d46d0567f73b2104a
    * Fix working in comment https://github.com/xamarin/maccore/commit/243810037e19a81e39c8414d46d0567f73b2104a
    * Insert login keychain without overwriting other keychains in the search https://github.com/xamarin/maccore/commit/7e6beed1d0d3120797e914ac8f49c3b986d176b2
    * Merge pull request #2332 from xamarin/yaml-pipeline-device-tests https://github.com/xamarin/maccore/commit/7e6beed1d0d3120797e914ac8f49c3b986d176b2
    * Avoid adding duplicate keychains to keychain list (#2333) https://github.com/xamarin/maccore/commit/7e6beed1d0d3120797e914ac8f49c3b986d176b2
    * [Governance] Fix some small polycheck issues. https://github.com/xamarin/maccore/commit/51bea51d0669179da599c3d3139c568b48668219
    * [Governance] Fix some small polycheck issues. https://github.com/xamarin/maccore/commit/f490737c8e3a7ecedb6005bfc8cc9792fe046931
    
    Full diff: https://github.com/xamarin/maccore/compare/6a86f16e0f8bf5cad707eb9bb145a40596a7f438..f490737c8e3a7ecedb6005bfc8cc9792fe046931

--- mk/xamarin.mk
+++ mk/xamarin.mk
@@ -7,7 +7,7 @@ MONO_BRANCH    := $(shell cd $(MONO_PATH) 2> /dev/null && git symbolic-ref --sho
 endif
 
 ifdef ENABLE_XAMARIN
-NEEDED_MACCORE_VERSION := 6a86f16e0f8bf5cad707eb9bb145a40596a7f438
+NEEDED_MACCORE_VERSION := f490737c8e3a7ecedb6005bfc8cc9792fe046931
 NEEDED_MACCORE_BRANCH := main
 
 MACCORE_DIRECTORY := maccore
