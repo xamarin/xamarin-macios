25ed862e341f03d392efe336723c4537e01c9025
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-11-17 09:30:08 +0100

    [msbuild] Catalyst uses the macOS certificates and profiles for codesigning.

--- msbuild/Xamarin.MacDev.Tasks.Core/Tasks/CompileEntitlementsTaskBase.cs
+++ msbuild/Xamarin.MacDev.Tasks.Core/Tasks/CompileEntitlementsTaskBase.cs
@@ -326,6 +326,9 @@ namespace Xamarin.MacDev.Tasks
 			case "MacOSX":
 				platform = MobileProvisionPlatform.MacOS;
 				break;
+			case "MacCatalyst":
+				platform = MobileProvisionPlatform.MacOS;
+				break;
 			default:
 				Log.LogError (MSBStrings.E0048, SdkPlatform);
 				return false;
