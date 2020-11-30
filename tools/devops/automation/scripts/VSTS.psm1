9b71b9f87cfdc0481bfbea7479ad10358d7f779a
Manuel de la Pena <mandel@microsoft.com>
2020-11-30 18:45:48 -0500

    [VSTS] Use the rest API to workaround the fact that we build from forks. (#10143)
    Due to some security concerns, PRs cannot add tags to builds, but we can access a secret. With that secret we can access the VSTS REST API and do all we want.
    
    “If you want to keep a secret, you must also hide it from yourself.”

--- tools/devops/automation/scripts/VSTS.psm1
+++ tools/devops/automation/scripts/VSTS.psm1
@@ -9,6 +9,36 @@ function Get-BuildUrl {
 
 <#
     .SYNOPSIS
+        Returns the uri to be used for the VSTS rest API for tags.
+#>
+function Get-TagsRestAPIUrl {
+    param
+    (
+        [Parameter(Mandatory)]
+        [String]
+        $Tag
+    )
+
+    $targetUrl = $Env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI + "$Env:SYSTEM_TEAMPROJECT/_apis/build/builds/" + $Env:BUILD_BUILDID + "/tags/" + $Tag + "?api-version=6.0"
+    return $targetUrl
+}
+
+<#
+    .SYNOPSIS
+        Returns the auth heater to use with the REST API of VSTS.
+#>
+function Get-AuthHeader([string] $AccessToken)
+{
+    # User name can be anything. It is the personal access token (PAT) token that matters.
+    $user = "AnyUser"
+    $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user, $AccessToken)))
+    $headers = @{Authorization = "Basic {0}" -f $base64AuthInfo}
+
+    return $headers
+}
+
+<#
+    .SYNOPSIS
         Cancels the pipeline and no other steps of job will be executed.
 
     .EXAMPLE
@@ -103,9 +133,7 @@ function Set-PipelineResult {
 
     $url = Get-BuildUrl
 
-    $headers = @{
-        Authorization = ("Bearer {0}" -f $Env:SYSTEM_ACCESSTOKEN)
-    }
+    $headers = Get-AuthHeader -AccessToken  $Env:SYSTEM_ACCESSTOKEN
 
     $payload = @{
         result = $Status
@@ -114,6 +142,45 @@ function Set-PipelineResult {
     return Invoke-RestMethod -Uri $url -Headers $headers -Method "PATCH" -Body ($payload | ConvertTo-json) -ContentType 'application/json'
 }
 
+function Set-BuildTags {
+    param
+    (
+        [String[]]
+        $Tags
+    )
+
+    $envVars = @{
+        "SYSTEM_TEAMFOUNDATIONCOLLECTIONURI" = $Env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI;
+        "SYSTEM_TEAMPROJECT" = $Env:SYSTEM_TEAMPROJECT;
+        "BUILD_BUILDID" = $Env:BUILD_BUILDID;
+        "SYSTEM_ACCESSTOKEN" = $Env:SYSTEM_ACCESSTOKEN
+    }
+
+    foreach ($key in $envVars.Keys) {
+        if (-not($envVars[$key])) {
+            Write-Debug "Environment variable missing: $key"
+            throw [System.InvalidOperationException]::new("Environment variable missing: $key")
+        }
+    }
+
+    # there is an api to just do one request, but it is not clear what should the body be, and we are trying and failing, ergo, use
+    # the API that sets one tag at at time. 
+    # This is why people should write documentation, now I'm being  annoying with the tags
+
+    $headers = Get-AuthHeader -AccessToken  $Env:SYSTEM_ACCESSTOKEN
+
+    foreach ($t in $Tags) {
+        $url = Get-TagsRestAPIUrl -Tag $t
+        Write-Host "Uri is $url"
+
+        Invoke-RestMethod -Uri $url -Headers $headers -Method "PUT"  -ContentType 'application/json'
+    }
+
+
+}
+
+
 # export public functions, other functions are private and should not be used ouside the module.
 Export-ModuleMember -Function Stop-Pipeline
 Export-ModuleMember -Function Set-PipelineResult 
+Export-ModuleMember -Function Set-BuildTags
