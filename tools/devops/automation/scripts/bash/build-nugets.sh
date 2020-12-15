bd5eb198db527049eb77949393f9665c12b3a32d
Manuel de la Pena <mandel@microsoft.com>
2020-12-15 14:31:44 -0500

    [VSTS] Reuse the script used in jenkins to build the nugets.  (#10293)

---
+++ tools/devops/automation/scripts/bash/build-nugets.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -ex
+
+# env var shuold have been defined by the CI
+cd $XAM_TOP
+
+DOTNET_NUPKG_DIR=$(make -C tools/devops print-abspath-variable VARIABLE=DOTNET_NUPKG_DIR | grep "^DOTNET_NUPKG_DIR=" | sed -e 's/^DOTNET_NUPKG_DIR=//')
+
+mkdir -p ../package/
+rm -f ../package/*.nupkg
+cp -c "$DOTNET_NUPKG_DIR"/*.nupkg ../package/
+
+DOTNET_PKG_DIR=$(make -C tools/devops print-abspath-variable VARIABLE=DOTNET_PKG_DIR | grep "^DOTNET_PKG_DIR=" | sed -e 's/^DOTNET_PKG_DIR=//')
+make -C dotnet package -j
+cp -c "$DOTNET_PKG_DIR"/*.pkg ../package/
+cp -c "$DOTNET_PKG_DIR"/*.msi ../package/
