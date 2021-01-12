734b8a7f2a94319adf928932495faa2b34eec608
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2021-01-12 16:02:01 -0500

    [msbuild] Simplify resolving xcframeworks (#10376)
    TD&LR: This PR simplifies how we refer to user frameworks and fixes both
    warnings and non-optimal (app) output.
    
    Much longer story:
    
    Additional testing on macOS showed some build-time warnings and an
    [extra (dupe) file](https://github.com/spouliot/xcframework/commit/a20f8aba419e32e2338cb7c5e4b45d0f38862893#diff-54fd7d9cd5deae57f30195be0a43133eace03c1132401741a317e0ae8d5e13fdR34).
    
    Logs shows that we referred to the xcframework several times, where once
    should have been enough.
    
    ```
    /native-reference:/Users/poupou/git/spouliot/xcframework/Universal.xcframework
    /native-reference:/Users/poupou/git/spouliot/xcframework/Universal.xcframework/macos-arm64_x86_64/Universal.framework
    /native-reference:/Users/poupou/git/spouliot/xcframework/Universal.xcframework/macos-arm64_x86_64/Universal.framework/Universal
    ```
    
    The first `/native-reference` line produced a warning like:
    
    ```
    MMP warning MM2006: Native library 'Universal.xcframework' was referenced but could not be found.
    ```
    
    which makes sense as the tools (both `mmp` and `mtouch`) are not, by
    design, aware of (unresolved) xcframeworks.
    
    Removing `{NativeReference}` from `Xamarin.Mac.Common.targets` (and
    `Xamarin.iOS.Common.targets`) as it has already been processed by
    `_ExpandNativeReferences` solves this.
    
    The other part of the issue (next two lines) is because `msbuild` does
    not track changes to directories like it does for files - and the
    workaround (in `_ExpandNativeReferences`) had to be copied in other
    places (both XI and XM `_CompileToNative`) and that was not enough (and
    would eventually need to be duplicated again and again).
    
    This could lead to duplicate entries (i msbuild logs) like
    
    ```
    NativeReferences
    ../../Universal.xcframework/macos-arm64_x86_64/Universal.framework
    ../../Universal.xcframework/macos-arm64_x86_64/Universal.framework/Univeral
    ```
    which maps to our extra entries.
    
    In order to simplify things we make the `_ExpandNativeReferences` resolve
    the full path to the library name (not the `.framework` directory) which
    simplifies both `_CompileToNative` and ensure a single way (at least for
    `msbuild`) to provide this data to the tools (`mmp` and `mtouch`).
    
    Using a file, instead of a directory, is also more consistent for the
    existing `-framework` option, e.g. we provide the names like:
    
    ```
    --framework=CoreLocation
    --framework=ModelIO
    ```
    
    So adding a full path that include the name is more appropriate, e.g.
    
    ``` --framework=/Users/poupou/git/master/xamarin-macios/tests/xharness/tmp-test-dir/xcframework-test760/bin/AnyCPU/Debug/bindings-xcframework-test.resources/XTest.xcframework/ios-i386_x86_64-simulator/XTest.framework/XTest
    ```
    
    Finally for macOS applications it turns out we were embedding yet another
    copy of the framework's library inside the `MonoBundle`, which is clearly
    wrong, because of the last entry.
    
    ```
    $ l bin/Release/xcf-mac.app/Contents/MonoBundle/Universal
    -rwxr-xr-x  1 poupou  staff  167152  2 Dec 16:16 bin/Release/xcf-mac.app/Contents/MonoBundle/Universal
    ```
    
    The tool now checks if a provided library is inside a framework (or not)
    which is a good validation to have anyway when it gets called directly,
    i.e. not thru `msbuild`.

--- tests/msbuild/Xamarin.MacDev.Tasks.Tests/TaskTests/ResolveNativeReferencesTaskTest.cs
+++ tests/msbuild/Xamarin.MacDev.Tasks.Tests/TaskTests/ResolveNativeReferencesTaskTest.cs
@@ -9,16 +9,16 @@ namespace Xamarin.MacDev.Tasks.Tests {
 	public class ResolveNativeReferencesTaskTest {
 
 		// single arch request (subset are fine)
-		[TestCase ("iOS", null, "arm64", "ios-arm64/Universal.framework")]
-		[TestCase ("iOS", "simulator", "x86_64", "ios-arm64_x86_64-simulator/Universal.framework")] // subset
-		[TestCase ("iOS", "maccatalyst", "x86_64", "ios-arm64_x86_64-maccatalyst/Universal.framework")] // subset
-		[TestCase ("tvOS", null, "arm64", "tvos-arm64/Universal.framework")]
-		[TestCase ("tvOS", "simulator", "x86_64", "tvos-arm64_x86_64-simulator/Universal.framework")] // subset
-		[TestCase ("watchOS", null, "arm64_32", "watchos-arm64_32_armv7k/Universal.framework")]
-		[TestCase ("watchOS", "simulator", "x86_64", "watchos-arm64_x86_64-simulator/Universal.framework")] // subset
-		[TestCase ("macOS", null, "x86_64", "macos-arm64_x86_64/Universal.framework")] // subset
+		[TestCase ("iOS", null, "arm64", "ios-arm64/Universal.framework/Universal")]
+		[TestCase ("iOS", "simulator", "x86_64", "ios-arm64_x86_64-simulator/Universal.framework/Universal")] // subset
+		[TestCase ("iOS", "maccatalyst", "x86_64", "ios-arm64_x86_64-maccatalyst/Universal.framework/Universal")] // subset
+		[TestCase ("tvOS", null, "arm64", "tvos-arm64/Universal.framework/Universal")]
+		[TestCase ("tvOS", "simulator", "x86_64", "tvos-arm64_x86_64-simulator/Universal.framework/Universal")] // subset
+		[TestCase ("watchOS", null, "arm64_32", "watchos-arm64_32_armv7k/Universal.framework/Universal")]
+		[TestCase ("watchOS", "simulator", "x86_64", "watchos-arm64_x86_64-simulator/Universal.framework/Universal")] // subset
+		[TestCase ("macOS", null, "x86_64", "macos-arm64_x86_64/Universal.framework/Universal")] // subset
 		// multiple arch request (all must be present)
-		[TestCase ("macOS", null, "x86_64, arm64", "macos-arm64_x86_64/Universal.framework")]
+		[TestCase ("macOS", null, "x86_64, arm64", "macos-arm64_x86_64/Universal.framework/Universal")]
 		// failure to resolve requested architecture
 		[TestCase ("iOS", "simulator", "i386, x86_64", "")] // i386 not available
 		// failure to resolve mismatched variant
@@ -34,10 +34,10 @@ namespace Xamarin.MacDev.Tasks.Tests {
 			Assert.That (result, Is.EqualTo (expected), expected);
 		}
 
-		[TestCase ("iOS", null, "ARMv7", "ios-arm64_armv7_armv7s/XTest.framework")]
+		[TestCase ("iOS", null, "ARMv7", "ios-arm64_armv7_armv7s/XTest.framework/XTest")]
 		// there was no 64bits simulator for watchOS but a i386 one was available
 		[TestCase ("watchOS", "simulator", "x86_64", "")]
-		[TestCase ("watchOS", "simulator", "i386", "watchos-i386-simulator/XTest.framework")]
+		[TestCase ("watchOS", "simulator", "i386", "watchos-i386-simulator/XTest.framework/XTest")]
 		public void PreXcode12 (string platform, string variant, string architecture, string expected)
 		{
 			var path = Path.Combine (Path.GetDirectoryName (GetType ().Assembly.Location), "Resources", "xcf-prexcode12.plist");
