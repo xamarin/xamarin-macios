b715cb31e4f1a2f25b1a78c9a5775b7775b4c578
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-02-03 15:01:06 +0100

    [runtime] Create VM-specific code and header files and include them in the build

---
+++ runtime/coreclr-bridge.m
@@ -0,0 +1,14 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+*  Authors: Rolf Bjarne Kvinge
+*
+*  Copyright (C) 2021 Microsoft Corp.
+*
+*/
+
+#if defined (CORECLR_RUNTIME)
+
+#include "xamarin/xamarin.h"
+#include "xamarin/coreclr-bridge.h"
+
+#endif // CORECLR_RUNTIME
---
+++ runtime/monovm-bridge.m
@@ -0,0 +1,17 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+*  Authors: Rolf Bjarne Kvinge
+*
+*  Copyright (C) 2021 Microsoft Corp.
+*
+*/
+
+/* Support code for using MonoVM */
+
+#if !defined (CORECLR_RUNTIME)
+
+#include "xamarin/xamarin.h"
+#include "xamarin/monovm-bridge.h"
+
+
+#endif // !CORECLR_RUNTIME
