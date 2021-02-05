b29cb8596d858be4778322dd56936ee06a70d986
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2021-02-04 20:39:41 -0500

    [pdfkit] Add nullability to (generated and manual) bindings (#10577)
    See [HOWTO](https://github.com/xamarin/xamarin-macios/wiki/Nullability)
    
    Also small change to generator so strong dictionaries can accept `null`
    `NSDictionary`, which was already accepted by the class itself.
    
    and expose two properties that were not usable before

--- src/ObjCRuntime/ObsoleteConstants.cs
+++ src/ObjCRuntime/ObsoleteConstants.cs
@@ -11,6 +11,8 @@ namespace ObjCRuntime {
 
 		internal const string WatchKitRemoved = "The WatchKit framework has been removed from iOS.";
 
+		internal const string UnavailableOniOS = "This type is not available on iOS.";
+
 		internal const string UnavailableOnWatchOS = "This type is not available on watchOS.";
 
 		internal const string MacOS32bitsUnavailable = "This framework is not available on 64bits macOS versions.";
