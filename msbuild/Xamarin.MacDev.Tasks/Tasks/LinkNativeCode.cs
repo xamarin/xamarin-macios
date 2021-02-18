2f31c7f3955a00ed488f1ef52bf09a33d194e74d
emaf <ema@xamarin.com>
2021-02-18 10:50:26 -0300

    [dotnet] Fixes builds for iOS physical devices from Windows
    - We need to create output files for the AOTCompile task on Windows and copy the AOT Compiler to the Mac since it is part of a NuGet package that will not exist on the Mac
    - The EmbedProvisionProfile task needs to be run remotely so the embedded.mobileprovision file is included in the app bundle
    - When copying input files to the Mac for LinkNativeCode we should skip files that already exist on the Mac because those on Windows are empty. If we copy those we are essentially replacing the real files by the empty ones from Windows.

--- msbuild/Xamarin.MacDev.Tasks/Tasks/LinkNativeCode.cs
+++ msbuild/Xamarin.MacDev.Tasks/Tasks/LinkNativeCode.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Microsoft.Build.Framework;
 using Xamarin.Messaging.Build.Client;
@@ -7,15 +8,22 @@ namespace Xamarin.MacDev.Tasks
 {
 	public class LinkNativeCode : LinkNativeCodeTaskBase, ITaskCallback
 	{
+		string outputPath;
+
 		public override bool Execute ()
 		{
-			if (!string.IsNullOrEmpty (SessionId))
+			if (!string.IsNullOrEmpty (SessionId)) {
+				outputPath = Path.GetDirectoryName (OutputFile);
+
 				return new TaskRunner (SessionId, BuildEngine4).RunAsync (this).Result;
+			}
 
 			return base.Execute ();
 		}
 
-		public bool ShouldCopyToBuildServer (ITaskItem item) => false;
+		// We should avoid copying files from the output path because those already exist on the Mac
+		// and the ones on Windows are empty, so we will break the build
+		public bool ShouldCopyToBuildServer (ITaskItem item) => Path.GetDirectoryName(item.ItemSpec) != outputPath;
 
 		public bool ShouldCreateOutputFile (ITaskItem item) => false;
 
