2a61de0d3c2796407e582cd780107c0d7e2d2992
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-02-22 12:29:32 +0100

    [dotnet-tools] We need to set Application.NativeSdkVersion to the initial SdkVersion.

--- tools/dotnet-linker/LinkerConfiguration.cs
+++ tools/dotnet-linker/LinkerConfiguration.cs
@@ -234,6 +234,7 @@ namespace Xamarin.Linker {
 			Application.Cache.Location = CacheDirectory;
 			Application.DeploymentTarget = DeploymentTarget;
 			Application.SdkVersion = SdkVersion;
+			Application.NativeSdkVersion = SdkVersion;
 
 			DerivedLinkContext.Target = Target;
 			Target.Abis = Abis;
