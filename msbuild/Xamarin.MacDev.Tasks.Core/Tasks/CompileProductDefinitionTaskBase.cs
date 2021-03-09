d2103aeeb753950c916619d9a67dc11782e02c3f
Mathieu Bourgeois <mathieu.bourgeois@gameloft.com>
2021-03-09 12:06:14 -0500

    Add compile product definition task
    Xamarin.Mac can be provided with a ProductDefinition file for the generated pkg. Normally, providing a product definition was optional. However, with Apple Silicon, we have an extra issue : `productbuild` needs to know what architectures your package target. If not provided with them, it will guess to the best of its abilities. However, on Catalina and lower, the guess is x86_64, even if you have an arm64 slice. To fix this, we add a new task to compile the product definition and use this file to create the pkg. If you provide your own Product Definition, we can check and warn if the architectures don't match what we expect. If the file doesn't exist or there is no architecture, we set it ourselves based on our target architectures.

---
+++ msbuild/Xamarin.MacDev.Tasks.Core/Tasks/CompileProductDefinitionTaskBase.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+using Xamarin.Localization.MSBuild;
+using Xamarin.Utils;
+
+namespace Xamarin.MacDev.Tasks {
+	public abstract class CompileProductDefinitionTaskBase : XamarinTask {
+		#region Inputs
+
+		public string ProductDefinition { get; set; }
+
+		[Required]
+		public string OutputDirectory { get; set; }
+
+		[Required]
+		public string TargetArchitectures { get; set; }
+		#endregion
+
+		#region Outputs
+
+		[Output]
+		public ITaskItem CompiledProductDefinition { get; set; }
+
+		#endregion
+
+		protected TargetArchitecture architectures;
+
+		public override bool Execute ()
+		{
+			PDictionary plist;
+
+			if (File.Exists (ProductDefinition)) {
+				try {
+					plist = PDictionary.FromFile (ProductDefinition);
+				} catch (Exception ex) {
+					LogProductDefinitionError (MSBStrings.E0010, ProductDefinition, ex.Message);
+					return false;
+				}
+			} else {
+				plist = new PDictionary ();
+			}
+
+			if (!string.IsNullOrEmpty (TargetArchitectures) && !Enum.TryParse (TargetArchitectures, out architectures)) {
+				LogProductDefinitionError (MSBStrings.E0012, TargetArchitectures);
+				return false;
+			}
+
+			// productbuild can do a guess of the targeted architectures if not provided, but the guess
+			// is very simple : on Catalina and lower, it will suppose it's x86_64 (even with an arm64 slice).
+			HashSet <string> archStrings = new HashSet <string> (architectures.ToArray ().Select (a => a.ToNativeArchitecture ()));
+			if (plist.TryGetValue (ProductDefinitionKeys.Architectures, out PArray archArray)) {
+				var existingArchs = archArray.ToStringArray ();
+				if (!archStrings.SetEquals (existingArchs)) {
+					LogProductDefinitionWarning (MSBStrings.E7072, string.Join (", ", existingArchs), string.Join (", ", archStrings));
+				}
+			}
+
+			if (archArray == null) {
+				archArray = new PArray ();
+				foreach (var arch in archStrings) {
+					archArray.Add (new PString (arch));
+				}
+			}
+			plist [ProductDefinitionKeys.Architectures] = archArray;
+
+			CompiledProductDefinition = new TaskItem (Path.Combine (OutputDirectory, "Product.plist"));
+			plist.Save (CompiledProductDefinition.ItemSpec, true, false);
+
+			return !Log.HasLoggedErrors;
+		}
+
+		protected void LogProductDefinitionError (string format, params object [] args)
+		{
+			// Log an error linking to the Product.plist file
+			Log.LogError (null, null, null, ProductDefinition, 0, 0, 0, 0, format, args);
+		}
+
+		protected void LogProductDefinitionWarning (string format, params object [] args)
+		{
+			// Log a warning linking to the Product.plist file
+			Log.LogWarning (null, null, null, ProductDefinition, 0, 0, 0, 0, format, args);
+		}
+	}
+
+	public static class ProductDefinitionKeys {
+		public const string Architectures = "arch";
+	}
+}
