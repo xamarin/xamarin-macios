5448fbe4c3f64b83bb0ef4451392cab55044cd1c
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-03-18 07:58:15 +0100

    [tests] Add a performance test using BenchmarkDotNet.

--- tests/test-libraries/libtest.h
+++ tests/test-libraries/libtest.h
@@ -75,9 +75,12 @@ typedef unsigned int (^RegistrarTestBlock) (unsigned int magic);
 	@property char Pc4;
 	@property char Pc5;
 
+	@property (nonatomic, retain) NSObject* someObject;
+	@property (nonatomic, retain) NSArray* someArray;
 #include "libtest.properties.h"
 
 	-(void) V;
+	+(void) staticV;
 
 	-(float) F;
 	-(double) D;
