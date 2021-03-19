38e82078db09ce7d9c7c50a178ec62ca1d07a84f
Sebastien Pouliot <sebastien.pouliot@microsoft.com>
2021-03-19 09:42:11 -0400

    Fix monotouch-test build for MacCatalyst (removed members)

--- tests/monotouch-test/GameKit/LeaderboardViewControllerTest.cs
+++ tests/monotouch-test/GameKit/LeaderboardViewControllerTest.cs
@@ -7,7 +7,7 @@
 // Copyright 2013 Xamarin Inc. All rights reserved.
 //
 
-#if !__TVOS__ && !__WATCHOS__
+#if !__TVOS__ && !__WATCHOS__ && !__MACCATALYST__
 
 using System;
 using System.IO;
@@ -48,4 +48,4 @@ namespace MonoTouchFixtures.GameKit {
 	}
 }
 
-#endif // !__TVOS__ && !__WATCHOS__
+#endif // !__TVOS__ && !__WATCHOS__ && !__MACCATALYST__
