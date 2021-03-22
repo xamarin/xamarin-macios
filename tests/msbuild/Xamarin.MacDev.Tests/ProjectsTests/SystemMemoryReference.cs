76389bcec39cb8622f71fa6fec1058ad5d86eaf9
Filip Navara <navara@emclient.com>
2021-03-22 17:33:42 +0100

    Add test case

---
+++ tests/msbuild/Xamarin.MacDev.Tests/ProjectsTests/SystemMemoryReference.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using NUnit.Framework;
+
+using Xamarin.Tests;
+
+namespace Xamarin.iOS.Tasks {
+	[TestFixture ("iPhone")]
+	[TestFixture ("iPhoneSimulator")]
+	public class SystemMemoryReferenceTests : ProjectTest {
+		
+		public SystemMemoryReferenceTests (string platform) : base (platform)      
+		{
+		}
+
+		[Test]
+		public void BasicTest ()
+		{
+			this.BuildProject ("SystemMemoryReference", clean: false);
+
+			Assert.IsFalse (File.Exists (Path.Combine (AppBundlePath, "System.Memory.dll")), "System.Memory.dll was incorrectly copied from NuGet");
+		}
+	}
+}
+
