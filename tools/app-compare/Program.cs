4e5f5fe03020e474b2af57ef58dc866b8896de2b
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2021-04-07 16:10:40 -0400

    [tests[ Add app bundle size comparison between legacy and dotnet (#11149)
    Based on Rolf's earlier work [1] and a copy of my `app-compare` code [2]
    
    [1] https://github.com/rolfbjarne/xamarin-macios/tree/dotnet-size-comparison-helloworld
    [2] https://github.com/spouliot/dotnet-tools/tree/master/app-compare

---
+++ tools/app-compare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Tools;
+
+namespace AppCompare {
+	class MainClass {
+		public static void Main (string[] args)
+		{
+			var dc = new AppComparer (args [0], args [1]);
+			dc.Compare ();
+		}
+	}
+}
