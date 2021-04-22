5f68e12172ac82349692f02ccae67d3ae3e2fa10
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-04-22 09:13:04 +0200

    [runtime] There's no need to attach/detach the current thread in CoreCLR.

--- runtime/xamarin/runtime.h
+++ runtime/xamarin/runtime.h
@@ -391,10 +391,14 @@ public:
 
 #endif /* !TARGET_OS_WATCH */
 
+#if defined(CORECLR_RUNTIME)
+// this is not needed for CoreCLR
+#define MONO_THREAD_ATTACH
+#define MONO_THREAD_DETACH
 // Once we have one mono clone again the TARGET_OS_WATCH
 // condition should be removed (DYNAMIC_MONO_RUNTIME should still
 // be here though).
-#if TARGET_OS_WATCH && !defined (DYNAMIC_MONO_RUNTIME)
+#elif TARGET_OS_WATCH && !defined (DYNAMIC_MONO_RUNTIME)
 #define MONO_THREAD_ATTACH \
 	do { \
 		gpointer __thread_dummy; \
