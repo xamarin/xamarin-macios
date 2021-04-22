a44c0337f54c97eca6906aba6e945223309aa579
Manuel de la Pena <mandel@microsoft.com>
2021-04-21 21:28:00 -0400

    Stupid mistakes.

--- tools/devops/automation/scripts/bash/vsts-compare.sh
+++ tools/devops/automation/scripts/bash/vsts-compare.sh
@@ -1,4 +1,8 @@
+#!/bin/bash -e
 # set the env vars that are required for the compare to work. XAM_TOP is not set because
 # the yaml takes care of that.
 export WORKSPACE="$BUILD_ARTIFACTSTAGINGDIRECTORY"
 echo "GH PR: $ghprbPullId"
+cd $XAM_TOP
+
+./tools/devops/automation/scripts/bash/compare.sh
