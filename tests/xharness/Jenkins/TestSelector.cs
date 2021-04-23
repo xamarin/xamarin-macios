4272c83cbebce9657a14b80dddd7b5de584060d4
Manuel de la Pena <mandel@microsoft.com>
2021-04-23 14:08:35 -0400

    [Xharness] Fix the test selector.
    Fix the tests selector by setting the PR number in an env variable that
    is not longer linked with Jenkins.
    
    Fixes #11299

--- tests/xharness/Jenkins/TestSelector.cs
+++ tests/xharness/Jenkins/TestSelector.cs
@@ -292,8 +292,8 @@ namespace Xharness.Jenkins {
 		
 		public void SelectTests ()
 		{
-			if (!int.TryParse (Environment.GetEnvironmentVariable ("ghprbPullId"), out int pullRequest))
-				MainLog.WriteLine ("The environment variable 'ghprbPullId' was not found, so no pull requests will be checked for test selection.");
+			if (!int.TryParse (Environment.GetEnvironmentVariable ("PrID"), out int pullRequest))
+				MainLog.WriteLine ("The environment variable 'PrID' was not found, so no pull requests will be checked for test selection.");
 
 			// First check if can auto-select any tests based on which files were modified.
 			// This will only enable additional tests, never disable tests.
