451b52ee0ad3c8238ff5563227cf9e4b3713923c
Manuel de la Pena <mandel@microsoft.com>
2021-04-27 16:29:17 -0400

    Check if empty.

--- tools/devops/automation/scripts/bash/vsts-compare.sh
+++ tools/devops/automation/scripts/bash/vsts-compare.sh
@@ -5,7 +5,9 @@ export WORKSPACE="$BUILD_ARTIFACTSTAGINGDIRECTORY"
 echo "GH PR: $PR_ID"
 cd $XAM_TOP
 
-echo "Fetch all pull requests."
-git fetch --no-tags --progress https://github.com/xamarin/xamarin-macios +refs/pull/$PR_ID/*:refs/remotes/origin/pr/$PR_ID/*
+if test -z "$PR_ID"; then
+    git fetch --no-tags --progress https://github.com/xamarin/xamarin-macios +refs/pull/$PR_ID/*:refs/remotes/origin/pr/$PR_ID/*
+fi
+
 
 ./tools/devops/automation/scripts/bash/compare.sh
