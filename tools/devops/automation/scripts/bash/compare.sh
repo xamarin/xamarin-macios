c4c1c98b4cea64ac61d4c7152afab467519835a6
Manuel de la Pena <mandel@microsoft.com>
2021-04-28 19:46:38 -0400

    Fix generator.

--- tools/devops/automation/scripts/bash/compare.sh
+++ tools/devops/automation/scripts/bash/compare.sh
@@ -82,9 +82,9 @@ else
 fi
 printf "\\n" >> "$WORKSPACE/api-diff-comments.md"
 
-if ! test -s jenkins-results/generator-diff/generator.diff; then
+if ! test -s $API_COMPARISON/generator-diff/generator.diff; then
 	printf ":white_check_mark: [Generator Diff](%s) (no change)" "$GENERATOR_URL" >> "$WORKSPACE/api-diff-comments.md"
-elif grep "^[+-][^+-]" jenkins-results/generator-diff/generator.diff | grep -v "^.[[]assembly: AssemblyInformationalVersion" | grep -v "^[+-][[:space:]]*internal const string Revision =" >/dev/null 2>&1; then
+elif grep "^[+-][^+-]" $API_COMPARISON/generator-diff/generator.diff | grep -v "^.[[]assembly: AssemblyInformationalVersion" | grep -v "^[+-][[:space:]]*internal const string Revision =" >/dev/null 2>&1; then
 	printf ":information_source: [Generator Diff](%s) (please review changes)" "$GENERATOR_URL" >> "$WORKSPACE/api-diff-comments.md"
 else
 	printf ":white_check_mark: [Generator Diff](%s) (only version changes)" "$GENERATOR_URL" >> "$WORKSPACE/api-diff-comments.md"
