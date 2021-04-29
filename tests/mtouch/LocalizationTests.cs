74e71f5746ee7ee3eb873dba5f7791d72cd5f6fa
tj_devel709 <antlambe@microsoft.com>
2021-04-29 16:33:27 -0500

    This one is working

--- tests/mtouch/LocalizationTests.cs
+++ tests/mtouch/LocalizationTests.cs
@@ -8,6 +8,7 @@ using System.Globalization;
 using System.Threading;
 using System.Reflection;
 using Xamarin.Bundler;
+using System.Resources;
 
 namespace Xamarin.Tests
 {
@@ -38,6 +39,8 @@ namespace Xamarin.Tests
 			try {
 				string englishError = TranslateError ("en-US", errorCode);
 				string newCultureError = TranslateError (culture, errorCode);
+				
+				
 
 				Assert.AreNotEqual (englishError, newCultureError, $"\"{errorCode}\" is not translated in {culture}.");
 			} catch (NullReferenceException){
@@ -53,15 +56,38 @@ namespace Xamarin.Tests
 		{
 			CultureInfo cultureInfo = new CultureInfo (culture);
 			Thread.CurrentThread.CurrentUICulture = cultureInfo;
+			Thread.CurrentThread.CurrentCulture = cultureInfo;
 
-			var type = typeof (MachO).Assembly.GetType ("Xamarin.Bundler.Errors");
-			PropertyInfo propertyInfo = type.GetProperty (errorCode, BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
-			var value = (string) propertyInfo.GetValue (null, null);
-			var value2 = (string) propertyInfo.GetValue (null, BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static, null, null, null);
+			string [] resNames = typeof (MachO).Assembly.GetManifestResourceNames ();
 
-			return value;
-			//PropertyInfo propertyInfo = typeof (Errors).GetProperty (errorCode);
-			//return (string) propertyInfo.GetValue (null, null);
+			//var type = typeof (MachO).Assembly.GetType ("Xamarin.Bundler.Errors");
+			//PropertyInfo propertyInfo = type.GetProperty (errorCode, BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+			//var value = (string) propertyInfo.GetValue (null, null);
+			//var value2 = (string) propertyInfo.GetValue (null, BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static, null, null, null);
+
+			//var rm = new System.Resources.ResourceManager ("Errors.mtouch", typeof (MachO).Assembly);
+			//var resourceSet = rm.GetResourceSet (cultureInfo, false, true);
+			//var tryThis = rm.GetString ("MT0015", cultureInfo);
+
+			var rm2 = new System.Resources.ResourceManager ("mtouch.Errors", typeof (MachO).Assembly);
+			var rm2ResouceSet = rm2.GetResourceSet (cultureInfo, false, true);
+			var tryThis4 = rm2.GetString ("MT0015", cultureInfo);
+
+			// Try to get the resources from CreateFileBasedResourceManager
+			ResourceSet resourceSet2;
+			string tryThis2;
+			string tryThis3;
+			var curDir = Directory.GetCurrentDirectory ();
+			var manuallyGrabbedResourceMan = System.Resources.ResourceManager.CreateFileBasedResourceManager ("mtouch.Errors", "../../../../tools/mtouch/obj/Debug/", null);
+			if (manuallyGrabbedResourceMan != null) {
+				resourceSet2 = manuallyGrabbedResourceMan.GetResourceSet (cultureInfo, false, true);
+				tryThis2 = manuallyGrabbedResourceMan.GetString ("MT0015", cultureInfo);
+				tryThis3 = resourceSet2.GetString ("MT0015");
+			}
+			
+
+
+			return tryThis4;
 		}
 	}
 }
