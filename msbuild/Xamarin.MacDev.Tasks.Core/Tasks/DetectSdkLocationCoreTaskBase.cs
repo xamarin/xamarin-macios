70f71367d63328db54c0193b9ad696fe2a1220d3
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-05-04 20:19:03 +0200

    [msbuild/dotnet] Fix build logic when using .NET to not try to use nor require any version of installed Xamarin.iOS/Xamarin.Mac. Fixes #10827.
    We do this by setting the _XamarinSdkRoot variable in our .NET logic, which
    our existing shared build logic reads, passes to the DetectSdkLocations task,
    and then sets our override environment variable
    (MD_MTOUCH_SDK_ROOT/XAMMAC_FRAMEWORK_PATH) to the install location, so that
    existing code (which honors the override variable) continues to work as-is.
    
    Fixes https://github.com/xamarin/xamarin-macios/issues/10827.

--- msbuild/Xamarin.MacDev.Tasks.Core/Tasks/DetectSdkLocationCoreTaskBase.cs
+++ msbuild/Xamarin.MacDev.Tasks.Core/Tasks/DetectSdkLocationCoreTaskBase.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using Microsoft.Build.Framework;
 
 using Xamarin.Localization.MSBuild;
+using Xamarin.Utils;
 
 namespace Xamarin.MacDev.Tasks {
 	public abstract class DetectSdkLocationsCoreTaskBase : XamarinTask {
@@ -76,6 +77,21 @@ namespace Xamarin.MacDev.Tasks {
 		protected abstract string GetDefaultXamarinSdkRoot ();
 		protected abstract IAppleSdkVersion GetDefaultSdkVersion ();
 
+		protected string GetEnvironmentVariableOverride ()
+		{
+			switch (Platform) {
+			case ApplePlatform.iOS:
+			case ApplePlatform.TVOS:
+			case ApplePlatform.WatchOS:
+			case ApplePlatform.MacCatalyst:
+				return "MD_MTOUCH_SDK_ROOT";
+			case ApplePlatform.MacOSX:
+				return "XAMMAC_FRAMEWORK_PATH";
+			default:
+				throw new InvalidOperationException (string.Format (MSBStrings.InvalidPlatform, Platform));
+			}
+		}
+
 		protected void EnsureSdkPath ()
 		{
 			SdkPlatform = GetSdkPlatform (SdkIsSimulator);
@@ -132,6 +148,11 @@ namespace Xamarin.MacDev.Tasks {
 
 		public override bool Execute ()
 		{
+			// If XamarinSdkRoot is set, then make that override any other value, and in order to do so,
+			// set the corresponding environment variable accordingly.
+			if (!string.IsNullOrEmpty (XamarinSdkRoot))
+				Environment.SetEnvironmentVariable (GetEnvironmentVariableOverride (), XamarinSdkRoot);
+
 			if (EnsureAppleSdkRoot ())
 				EnsureSdkPath ();
 			EnsureXamarinSdkRoot ();
