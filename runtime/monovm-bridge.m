95b032057bb72e8250e7ed538d79cb502d602cc0
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-05-05 16:30:00 +0200

    [runtime] We always need a native xamarin_mono_object_retain function.

--- runtime/monovm-bridge.m
+++ runtime/monovm-bridge.m
@@ -298,6 +298,24 @@ xamarin_bridge_vm_initialize (int propertyCount, const char **propertyKeys, cons
 	return rv == 0;
 }
 
+// We have a P/Invoke to xamarin_mono_object_retain in managed code, but the
+// corresponding native method only really exists when using CoreCLR. However,
+// the P/Invoke might not always be linked away (if the linker isn't enabled
+// for instance), in which case we must still have a native function. So
+// provide an empty implementation of xamarin_mono_object_retain (since it
+// doesn't have to do anything when using MonoVM). We still keep the #define
+// that does nothing, so that all the native code that calls
+// xamarin_mono_object_retain, will completely disappear when using MonoVM.
+#undef xamarin_mono_object_retain
+extern "C" {
+	void xamarin_mono_object_retain (MonoObject *mobj);
+}
+void
+xamarin_mono_object_retain (MonoObject *mobj)
+{
+	// Nothing to do here
+}
+
 #endif // DOTNET
 
 #endif // !CORECLR_RUNTIME
