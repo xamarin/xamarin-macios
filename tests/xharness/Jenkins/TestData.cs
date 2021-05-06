4354b96889c4fc755822880412589f690f7e2998
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-05-06 15:11:42 +0200

    [xharness] Add an ARM64 variation of the xammac tests.

--- tests/xharness/Jenkins/TestData.cs
+++ tests/xharness/Jenkins/TestData.cs
@@ -21,5 +21,6 @@ namespace Xharness.Jenkins {
 		public bool? UseMonoRuntime;
 		public MonoNativeLinkMode MonoNativeLinkMode;
 		public IEnumerable<IDevice> Candidates;
+		public string XamMacArch;
 	}
 }
