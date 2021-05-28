3e723fc34289822b97b7ce86fb9c8a56aba44e1b
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-05-28 08:32:55 +0200

    [introspection] Improve error message to include method and type data.

--- tests/introspection/ApiSelectorTest.cs
+++ tests/introspection/ApiSelectorTest.cs
@@ -946,7 +946,7 @@ namespace Introspection {
 				bool result = bool_objc_msgSend_IntPtr (class_ptr, responds_handle, Selector.GetHandle (name));
 				bool response = CheckResponse (result, t, m, ref name);
 				if (!response)
-					ReportError ("Selector not found for {0}", name);
+					ReportError ("Selector not found for {0} in {1} on {2}", name, m, t.FullName);
 				n++;
 			}
 		}
