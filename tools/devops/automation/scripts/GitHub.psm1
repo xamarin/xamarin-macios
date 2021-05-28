0ac1de56f15d888e21041af09d46cf0c73630546
Manuel de la Pena <mandel@microsoft.com>
2021-05-28 12:32:55 -0400

    Add missing build id.

--- tools/devops/automation/scripts/GitHub.psm1
+++ tools/devops/automation/scripts/GitHub.psm1
@@ -562,7 +562,7 @@ function New-GitHubSummaryComment {
 
     $githubPagePrefix = "https://xamarin.github.io/macios.ci"
     if (-not [string]::IsNullOrEmpty($Env:PR_ID)) {
-        $githubPagePrefix = "$githubPagePrefix/pr/PR$Env:PR_ID"
+        $githubPagePrefix = "$githubPagePrefix/pr/PR$Env:PR_ID/$Env:BUILD_BUILDID"
         $sb.AppendLine("# GitHub pages")
         $sb.AppendLine()
         $sb.AppendLine("Results can be found in the following github pages (it might take some time to publish):")
