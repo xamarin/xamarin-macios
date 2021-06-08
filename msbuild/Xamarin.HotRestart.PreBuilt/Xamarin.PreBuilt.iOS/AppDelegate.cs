fa1762b55fe85061f309e57be4b1e03e29cb8d38
emaf <ema@xamarin.com>
2021-06-08 14:56:56 -0300

    [net6] Adds initial Hot Restart app
    Includes:
    - Source code for the Hot Restart app
    - PreBuilt app
    - Fixes targets

---
+++ msbuild/Xamarin.HotRestart.PreBuilt/Xamarin.PreBuilt.iOS/AppDelegate.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+using Foundation;
+using UIKit;
+using System.Reflection;
+
+namespace Xamarin.PreBuilt.iOS
+{
+ 
+}
