e416449bedfec082357e5e9efcfe6211a89aad7f
Manuel de la Pena <mandel@microsoft.com>
2021-06-28 18:26:37 -0400

    Update src/chip.cs
    Co-authored-by: Alex Soto <alex@alexsoto.me>

--- src/chip.cs
+++ src/chip.cs
@@ -1649,7 +1649,7 @@ namespace Chip {
 		bool Startup ([NullAllowed] IChipPersistentStorageDelegate storageDelegate);
 
 		[Export ("shutdown")]
-		bool Shutdown();
+		bool Shutdown ();
 	}
 
 	[Mac (12,0), Watch (8,0), TV (15,0), iOS (15,0), MacCatalyst (15,0)]
