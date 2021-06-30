f0e8ddb2a759968301dc7c2bac050c40966a1925
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-06-30 15:55:34 +0200

    [AppKit] Add availability attributes to enums that exist in Mac Catalyst

--- src/AppKit/Enums.cs
+++ src/AppKit/Enums.cs
@@ -173,6 +173,7 @@ namespace AppKit {
 		
 	}
 
+	[MacCatalyst (13, 0)]
 	[Native]
 	public enum NSRectEdge : ulong {
 		MinXEdge, MinYEdge, MaxXEdge, MaxYEdge
@@ -1580,12 +1581,13 @@ namespace AppKit {
 		WritingPromised = 1 << 9
 	}
 
-
+	[MacCatalyst (13, 0)]
 	[Native]
 	public enum NSToolbarDisplayMode : ulong {
 		Default, IconAndLabel, Icon, Label
 	}
 
+	[MacCatalyst (13, 0)]
 	[Native]
 	public enum NSToolbarSizeMode : ulong {
 		Default, Regular, Small
