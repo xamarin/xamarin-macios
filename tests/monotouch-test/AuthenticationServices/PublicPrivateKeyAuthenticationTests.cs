f3a69a40dd203b2805a51889ae031029ff7d9ecb
tj_devel709 <antlambe@microsoft.com>
2021-07-07 17:03:28 -0500

    adding test

---
+++ tests/monotouch-test/AuthenticationServices/PublicPrivateKeyAuthenticationTests.cs
@@ -0,0 +1,41 @@
+//
+// Copyright 2021 Microsoft Corp
+//
+// Authors:
+//	TJ Lambert (antlambe@microsoft.com)
+//
+
+#if !__TVOS__ && !__WATCHOS__
+
+using System;
+using Foundation;
+using AuthenticationServices;
+using NUnit.Framework;
+
+namespace MonoTouchFixtures.AuthenticationServices {
+
+	[TestFixture]
+	[Preserve (AllMembers = true)]
+	public class PublicPrivateKeyAuthenticationTests {
+
+		[SetUp]
+		public void Setup ()
+		{
+			TestRuntime.AssertXcodeVersion (13, 0);
+		}
+
+		ASAuthorizationSecurityKeyPublicKeyCredentialDescriptorTransport [] transports = {
+			ASAuthorizationSecurityKeyPublicKeyCredentialDescriptorTransport.Usb,
+			ASAuthorizationSecurityKeyPublicKeyCredentialDescriptorTransport.Nfc,
+			ASAuthorizationSecurityKeyPublicKeyCredentialDescriptorTransport.Bluetooth,
+		};
+
+		[Test]
+		public void GetAllSupportedPublicKeyCredentialDescriptorTransports ()
+		{
+			var value = PublicPrivateKeyAuthentication.GetAllSupportedPublicKeyCredentialDescriptorTransports ();
+			Assert.Contains (PublicPrivateKeyAuthentication.GetAllSupportedPublicKeyCredentialDescriptorTransports (), transports, "The three transports are not supported as expected");
+		}
+	}
+}
+#endif
