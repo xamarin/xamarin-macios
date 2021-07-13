b7396d0195d6e1c16f2e6fa97fe007907096a4c5
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-07-13 08:42:10 +0200

    [dotnet/templates] Use file-scoped namespaces. Fixes #12085.
    Fixes https://github.com/xamarin/xamarin-macios/issues/12085.

--- dotnet/Templates/Microsoft.macOS.Templates/macos/AppDelegate.cs
+++ dotnet/Templates/Microsoft.macOS.Templates/macos/AppDelegate.cs
@@ -1,15 +1,14 @@
+namespace macOSApp1;
 
-namespace macOSApp1 {
-	[Register ("AppDelegate")]
-	public class AppDelegate : NSApplicationDelegate {
-		public override void DidFinishLaunching (NSNotification notification)
-		{
-			// Insert code here to initialize your application
-		}
+[Register ("AppDelegate")]
+public class AppDelegate : NSApplicationDelegate {
+	public override void DidFinishLaunching (NSNotification notification)
+	{
+		// Insert code here to initialize your application
+	}
 
-		public override void WillTerminate (NSNotification notification)
-		{
-			// Insert code here to tear down your application
-		}
+	public override void WillTerminate (NSNotification notification)
+	{
+		// Insert code here to tear down your application
 	}
 }
