495ab78a9a434fdf03f66c186fb24df8764e40ee
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-07-21 09:29:20 +0200

    [dont link] Ignore the Calendar tests on Mac Catalyst.

--- tests/linker/ios/dont link/CalendarTest.cs
+++ tests/linker/ios/dont link/CalendarTest.cs
@@ -10,6 +10,9 @@ namespace DontLink.Calendars {
 	[TestFixture]
 	// we want the tests to be available because we use the linker
 	[Preserve (AllMembers = true)]
+#if NET && __MACCATALYST__
+	[Ignore ("No globalization data yet - https://github.com/xamarin/xamarin-macios/issues/8906")]
+#endif
 	public class CalendarTest {
 
 		// application must *NOT* be build with I18N.MidEast and I18N.Other (Thai)
@@ -18,14 +21,22 @@ namespace DontLink.Calendars {
 		public void UmAlQura ()
 		{
 			var ci = CultureInfo.GetCultureInfo ("ar");
+#if NET // https://github.com/dotnet/runtime/issues/50859
+			Assert.That (ci.Calendar.ToString (), Is.EqualTo ("System.Globalization.GregorianCalendar"), "Calendar");
+#else
 			Assert.That (ci.Calendar.ToString (), Is.EqualTo ("System.Globalization.UmAlQuraCalendar"), "Calendar");
+#endif
 		}
 
 		[Test]
 		public void Hijri ()
 		{
 			var ci = CultureInfo.GetCultureInfo ("ps");
+#if NET // https://github.com/dotnet/runtime/issues/50859
+			Assert.That (ci.Calendar.ToString (), Is.EqualTo ("System.Globalization.PersianCalendar"), "Calendar");
+#else
 			Assert.That (ci.Calendar.ToString (), Is.EqualTo ("System.Globalization.HijriCalendar"), "Calendar");
+#endif
 		}
 
 		[Test]
