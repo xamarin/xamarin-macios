802fad188dd9b65be19f83753da9aeb28efb2c7a
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-08-02 13:03:29 +0200

    [xharness] Automatically inline Imports to a "shared.csproj". Fixes #12316 and #12277.
    This way we process the shared.csproj that we use in our .NET test projects. The
    main effect is that we'll clone any project references included in the shared.csproj,
    which on one hand will fix #12316, and on another it means that we'll add/remove
    defines in referenced projects, which fixes #12277.
    
    Fixes https://github.com/xamarin/xamarin-macios/issues/12316.
    Fixes https://github.com/xamarin/xamarin-macios/issues/12277.

--- tests/xharness/ProjectFileExtensions.cs
+++ tests/xharness/ProjectFileExtensions.cs
@@ -14,8 +14,68 @@ using Microsoft.DotNet.XHarness.iOS.Shared.Utilities;
 using Xamarin.Utils;
 
 #nullable enable
+
 namespace Xharness {
 	public static class EvolvedProjectFileExtensions {
+		// Evaluates a text and replaces '$(Variable)' with the property value specified in the 'properties' dictionary.
+		// Contrary to what MSBuild does, if the variable can't be found in the dictionary, it's not replaced with
+		// an empty string, instead the variable reference stays as-is.
+		public static string EvaluateAsMSBuildText (this string text, Dictionary<string, string> properties)
+		{
+			if (text.Length < 4)
+				return text;
+
+			if (text.IndexOf ('$') == -1)
+				return text;
+
+			var sb = new StringBuilder ();
+
+			for (var i = 0; i < text.Length; i++) {
+				if (text [i] != '$' || i + 2 >= text.Length) {
+					sb.Append (text [i]);
+					continue;
+				}
+
+				if (text [i + 1] != '(') {
+					sb.Append ('$');
+					sb.Append (text [i + 1]);
+					continue;
+				}
+
+				var endParenthesis = text.IndexOf (')', i + 2);
+				if (endParenthesis == -1)
+					continue;
+
+				var variable = text.Substring (i + 2, endParenthesis - i - 2);
+				if (properties.TryGetValue (variable, out var value)) {
+					sb.Append (value);
+				} else {
+					// Put back the variable reference
+					sb.Append ('$');
+					sb.Append ('(');
+					sb.Append (variable);
+					sb.Append (')');
+				}
+
+				i = endParenthesis;
+			}
+
+			return sb.ToString ();
+		}
+
+		// Collect all top-level properties in a csproj. They're evaluated as they're found, so that multiple properties concatenating
+		// strings work correctly. The 'forcedProperties' dictionary contains properties that are hardcoded and should be returned as
+		// the dictionary says.
+		public static Dictionary<string, string> CollectAndEvaluateTopLevelProperties (this XmlDocument doc, Dictionary<string, string> forcedProperties)
+		{
+			var collectedProperties = new Dictionary<string, string> (forcedProperties);
+			var properties = doc.SelectNodes ("/Project/*[local-name() = 'PropertyGroup' and not(@Condition)]//*");
+			foreach (XmlNode prop in properties) {
+				if (!forcedProperties.ContainsKey (prop.Name))
+					collectedProperties [prop.Name] = prop.InnerText.EvaluateAsMSBuildText (collectedProperties);
+			}
+			return collectedProperties;
+		}
 
 		static IEnumerable<XmlNode> SelectElementNodes (this XmlNode node, string name)
 		{
@@ -35,7 +95,7 @@ namespace Xharness {
 		}
 
 		// This is an evolved version of https://github.com/dotnet/xharness/blob/b2297d610df1ae15fc7ba8bd8c9bc0a7192aaefa/src/Microsoft.DotNet.XHarness.iOS.Shared/Utilities/ProjectFileExtensions.cs#L1168
-		public static void ResolveAllPaths (this XmlDocument csproj, string project_path, string? rootDirectory = null)
+		public static void ResolveAllPaths (this XmlDocument csproj, string project_path, Dictionary<string, string>? variableSubstitution = null)
 		{
 			var dir = Path.GetDirectoryName (project_path)!;
 			var nodes_with_paths = new string []
@@ -65,7 +125,7 @@ namespace Xharness {
 				new string [] { "ObjcBindingCoreSource", "Include" },
 				new string [] { "ObjcBindingNativeLibrary", "Include" },
 				new string [] { "ObjcBindingNativeFramework", "Include" },
-				new string [] { "Import", "Project", "CustomBuildActions.targets", "..\\shared.targets" },
+				new string [] { "Import", "Project" },
 				new string [] { "FilesToCopy", "Include" },
 				new string [] { "FilesToCopyFoo", "Include" },
 				new string [] { "FilesToCopyFooBar", "Include" },
@@ -92,22 +152,16 @@ namespace Xharness {
 					return string.Join (";", split);
 				}
 
-				if (input [0] == '/') {
-					return input; // This is already a full path.
+				if (variableSubstitution != null)
+					input = input.EvaluateAsMSBuildText (variableSubstitution);
+
+				var makeFullPath = input [0] != '$';
+				if (makeFullPath) {
+					input = input.Replace ('\\', '/'); // make unix-style
+					input = Path.GetFullPath (Path.Combine (dir, input));
+					input = input.Replace ('/', '\\'); // make windows-style again
 				}
 
-				input = input.Replace ('\\', '/'); // make unix-style
-
-				if (rootDirectory != null) {
-					input = input.Replace ("$(RootTestsDirectory)", rootDirectory);
-				}
-
-				// Don't process anything that starts with a variable, it's either a full path already, or the variable will be updated according to the new location
-				if (input.StartsWith ("$(", StringComparison.Ordinal))
-					return input;
-
-				input = Path.GetFullPath (Path.Combine (dir, input));
-				input = input.Replace ('/', '\\'); // make windows-style again
 				return input;
 			};
 
@@ -134,12 +188,7 @@ namespace Xharness {
 
 					// entries after index 2 is a list of values to filter the attribute value against.
 					var found = kvp.Length == 2;
-					var skipLogicalName = kvp.Length > 2;
-					for (var i = 2; i < kvp.Length; i++)
-						found |= a.Value == kvp [i];
-
-					if (!found)
-						continue;
+					var skipLogicalName = element == "Import" || element == "ProjectReference";
 
 					// Fix any default LogicalName values (but don't change existing ones).
 					var ln = node.SelectElementNodes ("LogicalName")?.SingleOrDefault ();
