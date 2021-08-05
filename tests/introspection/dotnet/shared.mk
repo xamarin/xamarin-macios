0d39801450696ff9310d34d701c6e6b82f72840b
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-08-05 11:25:30 +0200

    [tests] Share project logic in the .NET version of introspection.
    Also add a macOS variant of introspection.

--- tests/introspection/dotnet/shared.mk
+++ tests/introspection/dotnet/shared.mk
@@ -1,12 +1,29 @@
 TOP=../../../..
-
 include $(TOP)/Make.config
+include $(TOP)/mk/colors.mk
 
 prepare:
-	$(MAKE) -C $(TOP)/tests/dotnet copy-dotnet-config
+	$(Q) $(MAKE) -C $(TOP)/tests/dotnet copy-dotnet-config
+
+reload:
+	$(Q) rm -Rf $(TOP)/tests/dotnet/packages
+	$(Q) $(MAKE) -C $(TOP) -j8 all
+	$(Q) $(MAKE) -C $(TOP) -j8 install
+	$(Q) git clean -xfdq
+
+reload-and-build:
+	$(Q) $(MAKE) reload
+	$(Q) $(MAKE) build
+
+reload-and-run:
+	$(Q) $(MAKE) reload
+	$(Q) $(MAKE) run
 
 build: prepare
-	$(DOTNET6) build /bl *.csproj $(MSBUILD_VERBOSITY)
+	$(Q) $(DOTNET6) build /bl *.csproj $(MSBUILD_VERBOSITY) $(BUILD_ARGUMENTS)
 
 run: prepare
-	$(DOTNET6) build /bl *.csproj $(MSBUILD_VERBOSITY) /t:Run
+	$(Q) $(DOTNET6) build /bl *.csproj $(MSBUILD_VERBOSITY) $(BUILD_ARGUMENTS) -t:Run
+
+diag: prepare
+	$(Q) $(DOTNET6) build /v:diag msbuild.binlog
