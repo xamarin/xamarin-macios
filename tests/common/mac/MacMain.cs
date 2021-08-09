bdee7ee6ab4731d46655fbeb9fc3e8fb19dc0033
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-08-09 16:18:12 +0200

    [tests] Make interdependent-binding-projects work on macOS.

--- tests/common/mac/MacMain.cs
+++ tests/common/mac/MacMain.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,7 +14,11 @@ namespace Xamarin.Mac.Tests {
 			var arguments = new List<string> (args);
 			arguments.RemoveAll ((arg) => arg.StartsWith ("-psn_", StringComparison.Ordinal));
 
-			var exit_code = await MonoTouch.NUnit.UI.MacRunner.MainAsync (arguments, true, _exit, typeof (MainClass).Assembly);
+			var assemblies = new List<Assembly> ();
+			assemblies.Add (typeof (MainClass).Assembly);
+			TestLoader.AddTestAssemblies (assemblies);
+
+			var exit_code = await MonoTouch.NUnit.UI.MacRunner.MainAsync (arguments, true, _exit, assemblies.ToArray ());
 
 #if NET
 			var exit_monitor = new Thread (() =>
@@ -36,5 +42,15 @@ namespace Xamarin.Mac.Tests {
 
 		[DllImport ("/usr/lib/libSystem.dylib")]
 		static extern void _exit (int exit_code);
+	}
+
+	public static partial class TestLoader
+	{
+		static partial void AddTestAssembliesImpl (List<Assembly> assemblies);
+
+		public static void AddTestAssemblies (List<Assembly> assemblies)
+		{
+			AddTestAssembliesImpl (assemblies);
+		}
 	}
 }
--- tests/interdependent-binding-projects/Main.cs
+++ tests/interdependent-binding-projects/Main.cs
@@ -1,13 +1,27 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 using Foundation;
+#if !__MACOS__
 using UIKit;
+#endif
 
 using MonoTouch.NUnit.UI;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 
-#if !__WATCHOS__
+#if __MACOS__
+namespace Xamarin.Mac.Tests {
+	public static partial class TestLoader {
+		static partial void AddTestAssembliesImpl (List<Assembly> assemblies)
+		{
+			assemblies.Add (typeof (Xamarin.BindingTests2.BindingTest).Assembly);
+			assemblies.Add (typeof (Xamarin.BindingTests.ProtocolTest).Assembly);
+		}
+	}
+}
+#elif !__WATCHOS__
 [Register ("AppDelegate")]
 public partial class AppDelegate : UIApplicationDelegate
 {
@@ -48,3 +62,13 @@ public static partial class TestLoader {
 }
 
 #endif // !__WATCHOS__
+
+// In some cases NUnit fails if asked to run tests from an assembly that doesn't have any tests. So add a dummy test here to not fail in that scenario.
+[TestFixture]
+public class DummyTest
+{
+	public void TestMe ()
+	{
+		Assert.True (true, "YAY!");
+	}
+}
