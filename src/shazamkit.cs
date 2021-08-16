a8c1bc273ac633e9002141cfd5683207be1e19a8
tj_devel709 <antlambe@microsoft.com>
2021-08-16 11:28:50 -0500

    removing unnecessary attributes

--- src/shazamkit.cs
+++ src/shazamkit.cs
@@ -81,7 +81,6 @@ namespace ShazamKit {
 		bool Write (NSUrl url, [NullAllowed] out NSError error);
 
 		[Static]
-		[Watch (8,0), TV (15,0), Mac (12,0), iOS (15,0)]
 		[Export ("new")]
 		[return: Release]
 		SHCustomCatalog Create ();
