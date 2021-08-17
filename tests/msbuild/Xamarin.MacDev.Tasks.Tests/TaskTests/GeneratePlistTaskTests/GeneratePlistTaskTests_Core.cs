716a5a7bd19a15c58f582b6fbde65877ecb44088
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-08-17 15:29:42 +0200

    [msbuild] Add a property that specifies the final path to the Info.plist in the app bundle (#12433)
    * Add an '_AppBundleManifest' property that specifies the final path to the
      Info.plist in the app bundle.
    * Remove the '_AppBundleManifestPath' property, it's not used anywhere.
    * Adjust the CompileAppManifest task to take the final path to the Info.plist,
      instead of computing it and returning it. This way the CompileAppManifest
      task does not output anything back into MSBuild (which is important, because
      the CompileAppManifest task won't execute if the output file is up-to-date,
      and if it's not executed, any output properties won't be set either).

--- tests/msbuild/Xamarin.MacDev.Tasks.Tests/TaskTests/GeneratePlistTaskTests/GeneratePlistTaskTests_Core.cs
+++ tests/msbuild/Xamarin.MacDev.Tasks.Tests/TaskTests/GeneratePlistTaskTests/GeneratePlistTaskTests_Core.cs
@@ -2,6 +2,8 @@ using System.IO;
 
 using NUnit.Framework;
 
+using Microsoft.Build.Utilities;
+
 using Xamarin.MacDev;
 using System.Linq;
 
@@ -33,7 +35,7 @@ namespace Xamarin.iOS.Tasks
 			Task = CreateTask<CompileAppManifest> ();
 
 			Task.AppBundleName = appBundleName;
-			Task.AppManifestBundleDirectory = Path.Combine (Cache.CreateTemporaryDirectory (), "AppBundlePath");
+			Task.CompiledAppManifest = new TaskItem (Path.Combine (Cache.CreateTemporaryDirectory (), "AppBundlePath", "Info.plist"));
 			Task.AssemblyName = assemblyName;
 			Task.AppManifest = CreateTempFile ("foo.plist");
 			Task.BundleIdentifier = bundleIdentifier;
@@ -68,7 +70,7 @@ namespace Xamarin.iOS.Tasks
 		public void NormalPlist ()
 		{
 			Assert.IsTrue (Task.Execute (), "#1");
-			Assert.IsNotNull (Task.CompiledAppManifest, "#2");
+			Assert.IsNotNull (Task.CompiledAppManifest?.ItemSpec, "#2");
 			Assert.IsTrue (File.Exists (Task.CompiledAppManifest.ItemSpec), "#3");
 		}
 
