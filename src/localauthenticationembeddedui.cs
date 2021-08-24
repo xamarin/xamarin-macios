254d37275ef46086a5823ed457cc33d0ba4b4cb7
Rachel Kang <rachelkang@microsoft.com>
2021-08-24 18:05:47 -0400

    [LocalAuthenticationEmbeddedUI] Update bindings for Xcode 13.0 beta 2 and 3

---
+++ src/localauthenticationembeddedui.cs
@@ -0,0 +1,38 @@
+//
+// LocalAuthenticationEmbeddedUI C# bindings
+//
+// Authors:
+//	Rachel Kang  <rachelkang@microsoft.com>
+//
+// Copyright 2021 Microsoft Corporation All rights reserved.
+//
+
+using System;
+using ObjCRuntime;
+using Foundation;
+using AppKit;
+using CoreGraphics;
+using LocalAuthentication;
+
+namespace LocalAuthenticationEmbeddedUI {
+    
+    [NoWatch, NoTV, NoiOS, NoMacCatalyst, Mac (12,0)]
+    [BaseType (typeof(NSView))]
+    interface LAAuthenticationView
+    {
+        [Export ("initWithFrame:")]
+        IntPtr Constructor (CGRect frameRect);
+
+        [Export ("initWithContext:")]
+        IntPtr Constructor (LAContext context);
+
+        [Export ("initWithContext:controlSize:")]
+        IntPtr Constructor (LAContext context, NSControlSize controlSize);
+
+        [Export ("context")]
+        LAContext Context { get; }
+
+        [Export ("controlSize")]
+        NSControlSize ControlSize { get; }
+    }
+}
