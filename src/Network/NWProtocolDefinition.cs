678e9a19cf3e8504212e9f689146f2f19ca8072c
TJ Lambert <tjlambert@microsoft.com>
2021-08-30 17:22:49 -0500

    undo noisey ordering

--- src/Network/NWProtocolDefinition.cs
+++ src/Network/NWProtocolDefinition.cs
@@ -96,8 +96,8 @@ namespace Network {
 
 #if !XAMCORE_4_0
 #if !NET
-		[TV (13,0), Mac (10,15), iOS (13,0)]
 		[Obsolete ("Use 'CreateWebSocketDefinition' method instead.")]
+		[TV (13,0), Mac (10,15), iOS (13,0)]
 #else
 		[SupportedOSPlatform ("ios13.0")]
 		[SupportedOSPlatform ("tvos13.0")]
