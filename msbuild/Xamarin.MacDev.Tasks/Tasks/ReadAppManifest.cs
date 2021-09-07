4203f2c4cf4d75b29e6917cfe0cd6c04148fe1a1
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-09-07 16:38:25 +0200

    [msbuild] Implement ITaskCallback in ReadAppManifest to tell the remote task execution that any files on Windows shouldn't be copied back to the mac.
    Fixes https://dev.azure.com/devdiv/DevDiv/_workitems/edit/1394206.

--- msbuild/Xamarin.MacDev.Tasks/Tasks/ReadAppManifest.cs
+++ msbuild/Xamarin.MacDev.Tasks/Tasks/ReadAppManifest.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Build.Framework;
 using Xamarin.Messaging.Build.Client;
 
 namespace Xamarin.MacDev.Tasks
 {
-	public class ReadAppManifest : ReadAppManifestTaskBase
+	public class ReadAppManifest : ReadAppManifestTaskBase, ITaskCallback
 	{
 		public override bool Execute ()
 		{
@@ -11,5 +14,12 @@ namespace Xamarin.MacDev.Tasks
 
 			return base.Execute ();
 		}
+
+		public bool ShouldCopyToBuildServer (ITaskItem item) => false;
+
+		public bool ShouldCreateOutputFile (ITaskItem item) => false;
+
+		public IEnumerable<ITaskItem> GetAdditionalItemsToBeCopied () => Enumerable.Empty<ITaskItem> ();
 	}
 }
+
