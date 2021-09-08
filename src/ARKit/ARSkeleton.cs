2f7d122d95f4fa5a4d364811921432f3b014d174
tj_devel709 <antlambe@microsoft.com>
2021-09-07 22:55:17 -0500

    initial changes

--- src/ARKit/ARSkeleton.cs
+++ src/ARKit/ARSkeleton.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 
 using Foundation;
 using ObjCRuntime;
@@ -10,11 +11,19 @@ using ObjCRuntime;
 namespace ARKit {
 	public partial class ARSkeleton {
 
+#if !NET
 		[iOS (14,0)]
+#else
+		[SupportedOSPlatform ("ios14.0")]
+#endif
 		[DllImport (Constants.ARKitLibrary)]
 		static extern IntPtr /* NSString */ ARSkeletonJointNameForRecognizedPointKey (/* NSString */ IntPtr recognizedPointKey);
 		
+#if !NET
 		[iOS (14,0)]
+#else
+		[SupportedOSPlatform ("ios14.0")]
+#endif
 		public static NSString CreateJointName (NSString recognizedPointKey)
 		{
 			if (recognizedPointKey == null)
