a0bf71f82c24d461c8d13898cf0ad81e92601f0c
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-09-21 15:13:23 +0200

    [tests] Share makefile logic for .NET files.
    Also unset the MSBUILD_EXE_PATH variable.

---
+++ tests/common/shared-dotnet.mk
@@ -0,0 +1,46 @@
+
+include $(TOP)/Make.config
+include $(TOP)/mk/colors.mk
+
+unexport MSBUILD_EXE_PATH
+
+BINLOG_TIMESTAMP:=$(shell date +%Y-%m-%d--%H:%M:%S)
+
+TESTNAME:=$(notdir $(shell dirname "$(shell dirname "$(CURDIR)")"))
+
+prepare:
+	$(Q) $(MAKE) -C $(TOP)/tests/dotnet copy-dotnet-config
+
+reload:
+	$(Q) $(MAKE) -C $(TOP) -j8 all
+	$(Q) $(MAKE) -C $(TOP) -j8 install
+	$(Q) git clean -xfdq
+
+reload-and-build:
+	$(Q) $(MAKE) reload
+	$(Q) $(MAKE) build
+
+reload-and-run:
+	$(Q) $(MAKE) reload
+	$(Q) $(MAKE) run
+
+build: prepare
+	$(Q) $(DOTNET6) build "/bl:$@-$(BINLOG_TIMESTAMP).binlog" *.csproj $(MSBUILD_VERBOSITY) $(BUILD_ARGUMENTS)
+
+run: prepare
+	$(Q) $(DOTNET6) build "/bl:$@-$(BINLOG_TIMESTAMP).binlog" *.csproj $(MSBUILD_VERBOSITY) $(BUILD_ARGUMENTS) -t:Run
+
+run-bare:
+	$(Q) ./bin/Debug/net6.0-*/*/"$(TESTNAME)".app/Contents/MacOS/"$(TESTNAME)"
+
+BINLOGS:=$(wildcard *.binlog)
+diag: prepare
+	$(Q) if [[ "$(words $(BINLOGS))" == "1" ]]; then \
+		$(DOTNET6) build /v:diag $(BINLOGS); \
+	else \
+		echo "Choose your binlog to print:"; \
+		select binlog in $(BINLOGS); do $(DOTNET6) build /v:diag $$binlog; break; done \
+	fi
+
+clean:
+	rm -Rf bin obj *.binlog
