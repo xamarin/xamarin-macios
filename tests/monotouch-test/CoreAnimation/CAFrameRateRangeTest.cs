330668b4500ba0057b758e5a416af556836f3592
Manuel de la Pena <mandel@microsoft.com>
2021-09-22 16:47:29 -0400

    Fix tests.

--- tests/monotouch-test/CoreAnimation/CAFrameRateRangeTest.cs
+++ tests/monotouch-test/CoreAnimation/CAFrameRateRangeTest.cs
@@ -1,3 +1,4 @@
+#if !__WATCHOS__
 using Foundation;
 using CoreAnimation;
 using NUnit.Framework;
@@ -16,17 +17,18 @@ namespace MonoTouchFixtures.CoreAnimation {
 		public void IsEqualToTest ()
 		{
 			var first = new CAFrameRateRange (1.0f, 1.0f, 1.0f);
-			var second = new CAFrameRateRange (1.0f, 1.0f, 1.0f);
+			var second = new CAFrameRateRange (1.5f, 1.0f, 1.0f);
 			
 			Assert.IsFalse (first.IsEqualTo (second), "Not equal");
-			Assert.IsTrue (first.IsEqualTo (first));
+			Assert.IsTrue (first.IsEqualTo (first), "Equal");
 		}
 
 		[Test]
 		public void DefaultTest ()
 		{
 			var defaultValue = CAFrameRateRange.Default;
-			Assert.IsTrue (defaultValue.IsEqualTo (defaultValue), "Equal");
+			Assert.IsTrue (defaultValue.IsEqualTo (defaultValue), "Default");
 		}
 	}
 }
+#endif
