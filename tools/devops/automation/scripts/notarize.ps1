ef44c44ab96b758b42238c4fe93294a49e8719cd
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-09-22 14:01:55 +0200

    [devops] Show a better error in the GitHub comment if ESRP signing failed.

---
+++ tools/devops/automation/scripts/notarize.ps1
@@ -0,0 +1,15 @@
+$summaryPath = "$Env:SYSTEM_DEFAULTWORKINGDIRECTORY/xamarin-macios/tests/TestSummary.md"
+try {
+    # Notarize
+    & "$Env:BUILD_SOURCESDIRECTORY/release-scripts/notarize-make-this-fail.ps1" -FolderForApps $Env:BUILD_SOURCESDIRECTORY/package/notarized
+
+    # Verify that the notarized output is valid
+    $notarizedRoot = Join-Path $Env:BUILD_SOURCESDIRECTORY package notarized
+    Get-ChildItem -Path $notarizedRoot -Filter *.pkg -Recurse -File | ForEach-Object {
+        Write-Host "pkgutil --check-signature $($_.FullName)"
+        pkgutil --check-signature "$($_.FullName)"
+    }
+} catch {
+    Add-Content -Path $summaryPath -Value "# :x: Notarization (ESRP) failed :x:`n`n```````n$PSItem`n```````n"
+    throw
+}
