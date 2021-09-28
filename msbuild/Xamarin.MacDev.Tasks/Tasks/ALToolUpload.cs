09905e80211dec4250a8e9a3701808b9aa457f73
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-09-28 17:38:51 +0200

    [msbuild] Share the ALToolUpload and ALToolValidate task implementations between iOS and macOS. (#12852)

--- msbuild/Xamarin.MacDev.Tasks/Tasks/ALToolUpload.cs
+++ msbuild/Xamarin.MacDev.Tasks/Tasks/ALToolUpload.cs
@@ -1,12 +1,15 @@
+using Microsoft.Build.Framework;
 using Xamarin.Messaging.Build.Client;
 
-namespace Xamarin.iOS.Tasks
+namespace Xamarin.MacDev.Tasks
 {
-	public class ALToolValidate : ALToolValidateTaskBase
+	public class ALToolUpload : ALToolTaskBase, ICancelableTask
 	{
+		protected override string ALToolAction => "--upload-app";
+
 		public override bool Execute ()
 		{
-			if (ShouldExecuteRemotely ())
+			if (ShouldExecuteRemotely())
 				return new TaskRunner (SessionId, BuildEngine4).RunAsync (this).Result;
 
 			return base.Execute ();
