30e468a6b13539874b2ba95bd963a9365230c326
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-09-30 08:18:15 +0200

    [tests] Add more special cases for random network errors on CI.
    Ref: https://github.com/xamarin/maccore/issues/2304

--- tests/monotouch-test/System.Net.Http/MessageHandlers.cs
+++ tests/monotouch-test/System.Net.Http/MessageHandlers.cs
@@ -566,7 +566,7 @@ namespace MonoTests.System.Net.Http
 			if (!done) { // timeouts happen in the bots due to dns issues, connection issues etc.. we do not want to fail
 				Assert.Inconclusive ("Request timedout.");
 			} else {
-				if (httpStatus == HttpStatusCode.BadGateway)
+				if (httpStatus == HttpStatusCode.BadGateway || httpStatus == HttpStatusCode.GatewayTimeout)
 					TestRuntime.IgnoreInCI ("Transient network failure - ignore in CI");
 				Assert.IsNull (ex, "Exception not null");
 				Assert.AreEqual (expectedStatus, httpStatus, "Status not ok");
@@ -606,7 +606,7 @@ namespace MonoTests.System.Net.Http
 			if (!done) { // timeouts happen in the bots due to dns issues, connection issues etc.. we do not want to fail
 				Assert.Inconclusive ("First request timedout.");
 			} else {
-				if (httpStatus == HttpStatusCode.BadGateway)
+				if (httpStatus == HttpStatusCode.BadGateway || httpStatus == HttpStatusCode.GatewayTimeout)
 					TestRuntime.IgnoreInCI ("Transient network failure - ignore in CI");
 				Assert.IsNull (ex, "First request exception not null");
 				Assert.AreEqual (HttpStatusCode.OK, httpStatus, "First status not ok");
@@ -637,7 +637,7 @@ namespace MonoTests.System.Net.Http
 			if (!done) { // timeouts happen in the bots due to dns issues, connection issues etc.. we do not want to fail
 				Assert.Inconclusive ("Second request timedout.");
 			} else {
-				if (httpStatus == HttpStatusCode.BadGateway)
+				if (httpStatus == HttpStatusCode.BadGateway || httpStatus == HttpStatusCode.GatewayTimeout)
 					TestRuntime.IgnoreInCI ("Transient network failure - ignore in CI");
 				Assert.IsNull (ex, "Second request exception not null");
 				Assert.AreEqual (HttpStatusCode.Unauthorized, httpStatus, "Second status not ok");
