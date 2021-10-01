26771dfefa0096bb2fa020ca7348d694d0d1c81d
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-10-01 08:33:18 +0200

    [xharness] Remove iOS 32-bit simulator support, current Xcodes don't support it. (#12880)
    Our existing tests that claimed to run iOS 32-bit tests were really executing
    in 64-bit mode.

--- tests/xharness/TestTargetExtensions.cs
+++ tests/xharness/TestTargetExtensions.cs
@@ -15,9 +15,9 @@ namespace Xharness {
 			case TestPlatform.watchOS_64_32:
 				return new TestTarget [] { TestTarget.Simulator_watchOS };
 			case TestPlatform.iOS_Unified:
-				return new TestTarget [] { TestTarget.Simulator_iOS32, TestTarget.Simulator_iOS64 };
+				return new TestTarget [] { TestTarget.Simulator_iOS64 };
 			case TestPlatform.iOS_Unified32:
-				return new TestTarget [] { TestTarget.Simulator_iOS32 };
+				throw new NotSupportedException ($"32-bit simulators aren't supported anymore.");
 			case TestPlatform.iOS_Unified64:
 			case TestPlatform.iOS_TodayExtension64:
 				return new TestTarget [] { TestTarget.Simulator_iOS64 };
