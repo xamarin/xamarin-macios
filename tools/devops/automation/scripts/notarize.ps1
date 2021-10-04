44c4194916a8e716011d70635c76f6555ddd412b
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-10-04 08:37:43 +0200

    [devops] Show a better message when supposedly signed packages fail signature validation.

--- tools/devops/automation/scripts/notarize.ps1
+++ tools/devops/automation/scripts/notarize.ps1
@@ -8,6 +8,12 @@ try {
     Get-ChildItem -Path $notarizedRoot -Filter *.pkg -Recurse -File | ForEach-Object {
         Write-Host "pkgutil --check-signature $($_.FullName)"
         pkgutil --check-signature "$($_.FullName)"
+        if ($LastExitCode -eq 0) {
+            Write-Host "Signature check of $($_.FullName) succeeded."
+        } else {
+            Write-Host "Signature check of $($_.FullName) failed (exit code: $LastExitCode)."
+            throw "Signature check of $($_.FullName) failed (exit code: $LastExitCode)."
+        }
     }
 } catch {
     Add-Content -Path $summaryPath -Value "# :x: Notarization (ESRP) failed :x:`n`n```````n$PSItem`n```````n"
