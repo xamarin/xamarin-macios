e67e92458352ed1a032b35413a660b77b7d8683d
Chris Hamons <chris.hamons@xamarin.com>
2021-10-26 16:19:55 -0500

    Rework test to use XM which is much faster

--- tests/mtouch/MTouch.cs
+++ tests/mtouch/MTouch.cs
@@ -4251,10 +4251,14 @@ class C {
 		[Test]
 		public void BindingLibraryDSymCreated ()
 		{
-			var testDir = Path.Combine (Configuration.SourceRoot, "tests", "framework-test", "iOS");
-			var csproj = Path.Combine (testDir, "framework-test-ios.csproj");
-			XBuild.BuildXI (csproj, "Release", "iPhone", timeout: TimeSpan.FromMinutes (15));
-			DirectoryAssert.Exists(Path.Combine (Configuration.SourceRoot, "tests", "framework-test", "iOS", "bin", "iPhone", "Release-unified", "XTest.framework.dSYM"));
+			// framework-test for macOS has binding library that should have dSYMs
+			var testDir = Path.Combine (Configuration.SourceRoot, "tests", "framework-test", "macOS");
+			var csproj = Path.Combine (testDir, "framework-test-mac.csproj");
+			var arguments = new string [] {
+				"/p:ArchiveOnBuild=true",
+			};
+			XBuild.BuildXM (csproj, "Release", "x86", arguments: arguments, timeout: TimeSpan.FromMinutes (15));
+			DirectoryAssert.Exists(Path.Combine (Configuration.SourceRoot, "tests", "framework-test", "macOS", "bin", "x86", "Release", "XTest.framework.dSYM"));
 		}
 
 		public void XamarinSdkAdjustLibs ()
