c42504fcbd59fc37b968ef0095545d0ee39f6c95
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-10-27 10:56:40 +0200

    [tests] Adjust the BGen.ResponseFile test for .NET.

--- tests/generator/GeneratorTests.cs
+++ tests/generator/GeneratorTests.cs
@@ -5,6 +5,7 @@ using NUnit.Framework;
 
 using Xamarin;
 using Xamarin.Tests;
+using Xamarin.Utils;
 
 namespace GeneratorTests
 {
@@ -18,7 +19,11 @@ namespace GeneratorTests
 			var bgen = new BGenTool ();
 			bgen.CreateTemporaryBinding ("");
 			bgen.ResponseFile = Path.Combine (Cache.CreateTemporaryDirectory (), "rspfile");
+#if NET
+			File.WriteAllLines (bgen.ResponseFile, new string [] { $"--target-framework:{TargetFramework.DotNet_6_0_iOS_String}" });
+#else
 			File.WriteAllLines (bgen.ResponseFile, new string [] { "--target-framework:Xamarin.iOS,v1.0" });
+#endif
 			bgen.AssertExecute ("response file");
 			bgen.AssertNoWarnings ();
 		}
