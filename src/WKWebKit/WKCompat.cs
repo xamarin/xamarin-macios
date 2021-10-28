e15551f7a72403547a811612fc0af3fafded0c91
VS MobileTools Engineering Service 2 <vsmobiletoolsengsvc2@microsoft.com>
2021-10-27 23:00:32 -0700

    [WebKit] Add methods removed by Apple. (#13139)
    Apple removed methods without deprecating and did not add them back
    after a rdar was reported. We can implement those methofs using the new
    ones.
    
    Co-authored-by: Manuel de la Pena <mandel@microsoft.com>

--- src/WKWebKit/WKCompat.cs
+++ src/WKWebKit/WKCompat.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Threading.Tasks;
+using System.Runtime.Versioning;
 
 using Foundation;
+using ObjCRuntime;
 
 #nullable enable
 
@@ -13,6 +16,131 @@ namespace WebKit {
 		public WKWebsiteDataStore ()
 		{
 		}
+	}
+
+	public partial class WKWebView {
+
+#if !NET
+		[Mac (11,3), iOS (14,5), MacCatalyst (14,5)]
+#else
+		[SupportedOSPlatform ("ios14.5"), SupportedOSPlatform ("macos11.3"), SupportedOSPlatform ("maccatalyst14.5")]
+#endif
+		[Obsolete ("Use 'CloseAllMediaPresentations (Action completionHandler)' instead.")]
+		public virtual void CloseAllMediaPresentations () {
+#if IOS || __MACCATALYST__
+ 				if (UIKit.UIDevice.CurrentDevice.CheckSystemVersion (15, 0))
+#elif MONOMAC
+ 				if (PlatformHelper.CheckSystemVersion (12, 0))
+#endif
+					CloseAllMediaPresentationsAsync ().Wait();
+				else
+					_OldCloseAllMediaPresentations ();
+		}
+
+#if !NET
+		[Mac (11,3), iOS (14,5), MacCatalyst (14,5)]
+#else
+		[SupportedOSPlatform ("ios14.5"), SupportedOSPlatform ("macos11.3"), SupportedOSPlatform ("maccatalyst14.5")]
+#endif
+ 		public virtual void PauseAllMediaPlayback (Action? completionHandler)
+		{
+#if IOS || __MACCATALYST__
+ 				if (UIKit.UIDevice.CurrentDevice.CheckSystemVersion (15, 0))
+#elif MONOMAC
+ 				if (PlatformHelper.CheckSystemVersion (12, 0))
+#endif
+					_NewPauseAllMediaPlayback (completionHandler);
+				else
+					_OldPauseAllMediaPlayback (completionHandler);
+		}
+
+#if !NET
+		[Mac (11,3), iOS (14,5), MacCatalyst (14,5)]
+#else
+		[SupportedOSPlatform ("ios14.5"), SupportedOSPlatform ("macos11.3"), SupportedOSPlatform ("maccatalyst14.5")]
+#endif
+ 		public virtual Task PauseAllMediaPlaybackAsync ()
+		{
+#if IOS || __MACCATALYST__
+ 				if (UIKit.UIDevice.CurrentDevice.CheckSystemVersion (15, 0))
+#elif MONOMAC
+ 				if (PlatformHelper.CheckSystemVersion (12, 0))
+#endif
+					return _NewPauseAllMediaPlaybackAsync ();
+				else
+					return _OldPauseAllMediaPlaybackAsync ();
+		}
+
+#if !NET
+		[Mac (11,3), iOS (14,5), MacCatalyst (14,5)]
+#else
+		[SupportedOSPlatform ("ios14.5"), SupportedOSPlatform ("macos11.3"), SupportedOSPlatform ("maccatalyst14.5")]
+#endif
+		[Obsolete ("Use 'SetAllMediaPlaybackSuspended' instead.")]
+ 		public virtual void SuspendAllMediaPlayback (Action? completionHandler)
+		{
+#if IOS || __MACCATALYST__
+ 				if (UIKit.UIDevice.CurrentDevice.CheckSystemVersion (15, 0))
+#elif MONOMAC
+ 				if (PlatformHelper.CheckSystemVersion (12, 0))
+#endif
+					SetAllMediaPlaybackSuspended (true, completionHandler);
+				else
+					_OldSuspendAllMediaPlayback (completionHandler);
+		}
+
+#if !NET
+		[Mac (11,3), iOS (14,5), MacCatalyst (14,5)]
+#else
+		[SupportedOSPlatform ("ios14.5"), SupportedOSPlatform ("macos11.3"), SupportedOSPlatform ("maccatalyst14.5")]
+#endif
+ 		public virtual Task SuspendAllMediaPlaybackAsync ()
+		{
+#if IOS || __MACCATALYST__
+ 				if (UIKit.UIDevice.CurrentDevice.CheckSystemVersion (15, 0))
+#elif MONOMAC
+ 				if (PlatformHelper.CheckSystemVersion (12, 0))
+#endif
+					return SetAllMediaPlaybackSuspendedAsync (true);
+				else
+					return _OldSuspendAllMediaPlaybackAsync ();
+		}
+
+#if !NET
+		[Mac (11,3), iOS (14,5), MacCatalyst (14,5)]
+#else
+		[SupportedOSPlatform ("ios14.5"), SupportedOSPlatform ("macos11.3"), SupportedOSPlatform ("maccatalyst14.5")]
+#endif
+		[Obsolete ("Use 'SetAllMediaPlaybackSuspended' instead.")]
+ 		public virtual void ResumeAllMediaPlayback (Action? completionHandler)
+		{
+#if IOS || __MACCATALYST__
+ 				if (UIKit.UIDevice.CurrentDevice.CheckSystemVersion (15, 0))
+#elif MONOMAC
+ 				if (PlatformHelper.CheckSystemVersion (12, 0))
+#endif
+					SetAllMediaPlaybackSuspended (false, completionHandler);
+				else
+					_OldResumeAllMediaPlayback (completionHandler);
+		}
+
+#if !NET
+		[Mac (11,3), iOS (14,5), MacCatalyst (14,5)]
+#else
+		[SupportedOSPlatform ("ios14.5"), SupportedOSPlatform ("macos11.3"), SupportedOSPlatform ("maccatalyst14.5")]
+#endif
+ 		public virtual Task ResumeAllMediaPlaybackAsync ()
+		{
+#if IOS || __MACCATALYST__
+ 				if (UIKit.UIDevice.CurrentDevice.CheckSystemVersion (15, 0))
+#elif MONOMAC
+ 				if (PlatformHelper.CheckSystemVersion (12, 0))
+#endif
+					return SetAllMediaPlaybackSuspendedAsync (false);
+				else
+					return _OldResumeAllMediaPlaybackAsync ();
+		}
+
 	}
 #endif
 }
