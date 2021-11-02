7736ea5b2d8a6dd8cc6de18db58a25347b246066
Manuel de la Pena <mandel@microsoft.com>
2021-11-02 17:23:35 -0400

    [CoreAnimation] Add support for Xcode 13.1 rc.

---
+++ tests/monotouch-test/CoreAnimation/CAFrameRateRangeTest.cs
@@ -0,0 +1,26 @@
+using System;
+using NUnit.Framework;
+
+using Foundation;
+using CoreAnimation;
+namespace MonoTouchFixtures.CoreAnimation {
+
+	[TestFixture]
+	[Preserve (AllMembers = true)]
+	public class CAFrameRateRangeTest {
+
+		[SetUp]
+		public void SetUp ()
+		{
+			TestRuntime.AssertXcodeVersion (13,0);
+		}
+
+		[Test]
+		public void IsEqualToTest ()
+			=> Assert.True (CAFrameRateRange.Default.IsEqualTo (CAFrameRateRange.Default));
+
+		[Test]
+		public void DefaultTest ()
+			=> Assert.IsNotNull (CAFrameRateRange.Default, "Default");
+	}
+}
