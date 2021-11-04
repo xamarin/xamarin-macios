e95dc63c2290367fd5f891443e7a38c24b3e49d3
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-11-04 12:02:37 +0100

    [Class] Enable nullability + fix a few code updates.
    * Enable nullability and fix code accordingly.
    * Use 'is' and 'is not' instead of '==' and '!=' for object identity.
    * Use 'nameof (parameter)' instead of string constants.
    * Make the 'handle' field private.
    * Remove the (IntPtr) constructor for .NET
    * Make the (IntPtr, bool) constructor internal for .NET
    * Remove unused 'Construct' method.

--- src/uikit.cs
+++ src/uikit.cs
@@ -20085,7 +20085,7 @@ namespace UIKit {
 		Class SceneClass { get; set; }
 
 		Type SceneType {
-			[Wrap ("Class.Lookup (SceneClass)")] get;
+			[Wrap ("Class.Lookup (SceneClass)!")] get;
 			[Wrap ("SceneClass = value is null ? null : new Class (value)")] set;
 		}
 
@@ -20094,7 +20094,7 @@ namespace UIKit {
 		Class DelegateClass { get; set; }
 
 		Type DelegateType {
-			[Wrap ("Class.Lookup (DelegateClass)")] get;
+			[Wrap ("Class.Lookup (DelegateClass)!")] get;
 			[Wrap ("DelegateClass = value is null ? null : new Class (value)")] set;
 		}
 
