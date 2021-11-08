bbb7690e1a968579dd7a59527c1a434b153820ef
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-11-08 22:09:47 +0100

    [Security] Remove the SslCipherSuite enum from .NET. Fixes #11498. (#13307)
    * Remove the SslCipherSuite enum from .NET, it's complicated to implement
      correctly on macOS for both x64 and arm64, and it's also obsolete, so just
      remove it.
    * Change the type for NSUrlSessionTaskTransactionMetrics.NegotiatedTlsCipherSuite
      to be TlsCipherSuite instead of SslCipherSuite for .NET (this is in fact the
      correct value according to the headers).
    
    Fixes https://github.com/xamarin/xamarin-macios/issues/11498.

--- src/Security/SslContext.cs
+++ src/Security/SslContext.cs
@@ -286,6 +286,7 @@ namespace Security {
 		}
 
 
+#if !NET
 		[DllImport (Constants.SecurityLibrary)]
 		extern unsafe static /* OSStatus */ SslStatus SSLGetNumberSupportedCiphers (/* SSLContextRef */ IntPtr context, /* size_t* */ out nint numCiphers);
 
@@ -354,6 +355,7 @@ namespace Security {
 				return value;
 			}
 		}
+#endif
 
 		[DllImport (Constants.SecurityLibrary)]
 		extern unsafe static /* OSStatus */ SslStatus SSLGetDatagramWriteSize (/* SSLContextRef */ IntPtr context, /* size_t* */ out nint bufSize);
