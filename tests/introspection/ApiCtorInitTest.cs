9899e90dd81c78c443b26cee6ad8aba9a84695f3
Rolf Bjarne Kvinge <rolf@xamarin.com>
2021-11-12 07:57:19 +0100

    Fix SKView skipping logic.

--- tests/introspection/ApiCtorInitTest.cs
+++ tests/introspection/ApiCtorInitTest.cs
@@ -128,7 +128,7 @@ namespace Introspection {
 #else
 				return TestRuntime.CheckExactXcodeVersion (12, 2, beta: 3);
 #endif
-			case "SpriteKit.SKView":
+			case "SKView":
 				// Causes a crash later. Filed as radar://18440271.
 				// Apple said they won't fix this ('init' isn't a designated initializer)
 				return true;
