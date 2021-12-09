d866ece9db99f74eaa4188a9c478e2005263c525
Manuel de la Pena <mandel@microsoft.com>
2021-12-09 12:05:07 -0500

    [Tests] Fix the retrieval of variables in .Net after quotes were added. Fixes #13516

--- tests/common/Configuration.cs
+++ tests/common/Configuration.cs
@@ -704,7 +704,9 @@ namespace Xamarin.Tests
 
 		public static IList<string> GetRuntimeIdentifiers (ApplePlatform platform)
 		{
-			return GetVariable ($"DOTNET_{platform.AsString ().ToUpper ()}_RUNTIME_IDENTIFIERS", string.Empty).Split (new char [] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+			// varables with more than one value are wrapped in ', get the var remove the '' and split
+			var variable = GetVariable ($"DOTNET_{platform.AsString ().ToUpper ()}_RUNTIME_IDENTIFIERS", string.Empty).Trim ('\'');
+			return variable.Split (new char [] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 		}
 
 		public static IEnumerable<string> GetBaseLibraryImplementations (ApplePlatform platform)
