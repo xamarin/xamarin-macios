8758da5f65883c904e6fa8f1a8b0fb1c7d39152f
Chris Hamons <chris.hamons@xamarin.com>
2022-01-07 14:20:07 -0600

    Add NET6 attributes

--- src/ARKit/ARSkeleton.cs
+++ src/ARKit/ARSkeleton.cs
@@ -11,11 +11,19 @@ using ObjCRuntime;
 namespace ARKit {
 	public partial class ARSkeleton {
 
+#if NET
+		[SupportedOSPlatform ("ios14.0")]
+#else
 		[iOS (14,0)]
+#endif
 		[DllImport (Constants.ARKitLibrary)]
 		static extern IntPtr /* NSString */ ARSkeletonJointNameForRecognizedPointKey (/* NSString */ IntPtr recognizedPointKey);
 		
+#if NET
+		[SupportedOSPlatform ("ios14.0")]
+#else
 		[iOS (14,0)]
+#endif
 		public static NSString? CreateJointName (NSString recognizedPointKey)
 		{
 			if (recognizedPointKey is null)
