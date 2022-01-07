0cd30b5cc4a50d6826c4e6a8136fcb60f71916f4
Chris Hamons <chris.hamons@xamarin.com>
2022-01-07 14:19:39 -0600

    Remove existing NET6 attributes

--- src/ARKit/ARSkeleton.cs
+++ src/ARKit/ARSkeleton.cs
@@ -11,19 +11,11 @@ using ObjCRuntime;
 namespace ARKit {
 	public partial class ARSkeleton {
 
-#if !NET
 		[iOS (14,0)]
-#else
-		[SupportedOSPlatform ("ios14.0")]
-#endif
 		[DllImport (Constants.ARKitLibrary)]
 		static extern IntPtr /* NSString */ ARSkeletonJointNameForRecognizedPointKey (/* NSString */ IntPtr recognizedPointKey);
 		
-#if !NET
 		[iOS (14,0)]
-#else
-		[SupportedOSPlatform ("ios14.0")]
-#endif
 		public static NSString? CreateJointName (NSString recognizedPointKey)
 		{
 			if (recognizedPointKey is null)
