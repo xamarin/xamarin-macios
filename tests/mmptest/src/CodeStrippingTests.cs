c80a8797e4fca19540d60ebfb33f9524cdbce5e5
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-01-17 17:11:59 +0100

    [tests] Simplify mmptest a bit by using existing mechanisms to create frameworks.

--- tests/mmptest/src/CodeStrippingTests.cs
+++ tests/mmptest/src/CodeStrippingTests.cs
@@ -90,7 +90,8 @@ namespace Xamarin.MMP.Tests
 				var frameworkPath = FrameworkBuilder.CreateFatFramework (tmpDir);
 				TI.UnifiedTestConfig test = CreateStripTestConfig (strip, tmpDir, $"--native-reference={frameworkPath}");
 
-				StripTestCore (test, debugStrips, releaseStrips, "Contents/Frameworks/Foo.framework/Foo", shouldWarn: true);
+				var frameworkName = Path.GetFileNameWithoutExtension (frameworkPath);
+				StripTestCore (test, debugStrips, releaseStrips, $"Contents/Frameworks/{frameworkName}.framework/{frameworkName}", shouldWarn: true);
 			});
 		}
 
