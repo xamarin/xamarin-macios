f116c062d065f9f5ad7a6c90a74b16552b19372f
Chris Hamons <chris.hamons@xamarin.com>
2022-01-19 16:42:21 -0600

    Add required usings

--- src/Compression/Compression.cs
+++ src/Compression/Compression.cs
@@ -11,6 +11,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Runtime.Versioning;
 
 using ObjCRuntime;
 
--- src/Compression/CompressionStreamStruct.cs
+++ src/Compression/CompressionStreamStruct.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 
 using ObjCRuntime;
 
