c49750ffb6a6fe753b5dd613d6fded4c34ea8cd1
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-01-20 00:53:00 +0100

    [msbuild] Move the GetFiles task to shared msbuild code (Xamarin.MacDev.Tasks).

--- msbuild/Xamarin.MacDev.Tasks/Tasks/GetFiles.cs
+++ msbuild/Xamarin.MacDev.Tasks/Tasks/GetFiles.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using Microsoft.Build.Framework;
 using Xamarin.Messaging.Build.Client;
 
-namespace Xamarin.iOS.Tasks
+namespace Xamarin.MacDev.Tasks
 {
 	public class GetFiles : GetFilesTaskBase, ITaskCallback, ICancelableTask
 	{
