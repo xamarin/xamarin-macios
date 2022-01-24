0daa1354b80ee82e16d365be2dcefa6e8d8b5c08
Chris Hamons <chris.hamons@xamarin.com>
2022-01-24 11:12:51 -0600

    Revert CBPeer.cs as not needed

--- src/CoreBluetooth/CBPeer.cs
+++ src/CoreBluetooth/CBPeer.cs
@@ -10,20 +10,8 @@ using System.Runtime.Versioning;
 namespace CoreBluetooth {
 	public partial class CBPeer  {
 #if !NET
-#if NET
-		[SupportedOSPlatform ("ios8.0")]
-		[SupportedOSPlatform ("macos10.13")]
-		[UnsupportedOSPlatform ("ios7.0")]
-#if IOS
-		[Obsolete ("Starting with ios7.0.", DiagnosticId = "BI1234", UrlFormat = "https://github.com/xamarin/xamarin-macios/wiki/Obsolete")]
-#endif
-#if IOS
-		[Obsolete ("Starting with ios9.0.", DiagnosticId = "BI1234", UrlFormat = "https://github.com/xamarin/xamarin-macios/wiki/Obsolete")]
-#endif
-#else
 		[Deprecated (PlatformName.iOS, 7, 0)]
 		[Obsoleted (PlatformName.iOS, 9, 0)]
-#endif
 		public virtual CBUUID UUID { 
 			get {
 				return CBUUID.FromCFUUID (_UUID);	
