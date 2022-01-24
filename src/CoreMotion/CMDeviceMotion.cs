22df8ce080fb74fd51eae1340fc9c67ad88afb1f
Chris Hamons <chris.hamons@xamarin.com>
2022-01-24 13:22:19 -0600

    Remove existing NET6 attributes

--- src/CoreMotion/CMDeviceMotion.cs
+++ src/CoreMotion/CMDeviceMotion.cs
@@ -10,11 +10,7 @@ using System.Runtime.Versioning;
 namespace CoreMotion {
 
 	// CMDeviceMotion.h
-#if !NET
 	[Mac (10,15)]
-#else
-	[SupportedOSPlatform ("macos10.15")]
-#endif
 	[StructLayout (LayoutKind.Sequential)]
 	public struct CMCalibratedMagneticField {
 		public CMMagneticField Field;
