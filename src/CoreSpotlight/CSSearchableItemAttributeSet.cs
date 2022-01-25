3825c348746a25a0d568a949e1210a5f46f2e8e0
Chris Hamons <chris.hamons@xamarin.com>
2022-01-25 13:36:28 -0600

    Remove existing attributes

--- src/CoreSpotlight/CSSearchableItemAttributeSet.cs
+++ src/CoreSpotlight/CSSearchableItemAttributeSet.cs
@@ -21,11 +21,7 @@ namespace CoreSpotlight {
 		}
 
 		// Manually deal with these properties until we get BindAs working
-#if !NET
 		[iOS (11,0)]
-#else
-		[SupportedOSPlatform ("ios11.0")]
-#endif
 		public bool? IsUserCreated { 
 			get {
 				return _IsUserCreated?.BoolValue;
@@ -34,11 +30,7 @@ namespace CoreSpotlight {
 			}
 		}
 
-#if !NET
 		[iOS (11,0)]
-#else
-		[SupportedOSPlatform ("ios11.0")]
-#endif
 		public bool? IsUserOwned {
 			get {
 				return _IsUserOwned?.BoolValue;
@@ -47,11 +39,7 @@ namespace CoreSpotlight {
 			}
 		}
 
-#if !NET
 		[iOS (11,0)]
-#else
-		[SupportedOSPlatform ("ios11.0")]
-#endif
 		public bool? IsUserCurated { 
 			get {
 				return _IsUserCurated?.BoolValue;
