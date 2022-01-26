a4b23fa40997de0587155d72cf1f02271c3f241a
Chris Hamons <chris.hamons@xamarin.com>
2022-01-26 14:21:27 -0600

    Add required usings

--- src/HomeKit/HMCharacteristicMetadata.cs
+++ src/HomeKit/HMCharacteristicMetadata.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Runtime.Versioning;
+
 using ObjCRuntime;
 using Foundation;
 
--- src/HomeKit/HMService.cs
+++ src/HomeKit/HMService.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Threading.Tasks;
+using System.Runtime.Versioning;
+
 using ObjCRuntime;
 using Foundation;
 
