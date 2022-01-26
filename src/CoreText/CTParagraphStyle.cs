9d588187371daf7810911cb30e96d668818c5e63
Chris Hamons <chris.hamons@xamarin.com>
2022-01-26 10:11:40 -0600

    Remove unnecessary whitespace diff

--- src/CoreText/CTParagraphStyle.cs
+++ src/CoreText/CTParagraphStyle.cs
@@ -122,14 +122,10 @@ namespace CoreText {
 
 	[StructLayout (LayoutKind.Explicit)]
 	internal struct CTParagraphStyleSettingValue {
-		[FieldOffset (0)]
-		public byte int8;
-		[FieldOffset (0)]
-		public nfloat single;
-		[FieldOffset (0)]
-		public nuint  native_uint;
-		[FieldOffset (0)]
-		public IntPtr pointer;
+		[FieldOffset (0)] public byte int8;
+		[FieldOffset (0)] public nfloat single;
+		[FieldOffset (0)] public nuint  native_uint;
+		[FieldOffset (0)] public IntPtr pointer;
 	}
 
 	internal abstract class CTParagraphStyleSpecifierValue {
