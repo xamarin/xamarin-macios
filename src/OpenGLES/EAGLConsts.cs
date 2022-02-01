432b16f07a055400fc45ef08dfdebc13c0ba1db9
tj_devel709 <antlambe@microsoft.com>
2022-02-01 10:53:00 -0600

    Attribute Conversion

--- src/OpenGLES/EAGLConsts.cs
+++ src/OpenGLES/EAGLConsts.cs
@@ -13,8 +13,18 @@ using ObjCRuntime;
 
 namespace OpenGLES {
 
+#if NET
+	[UnsupportedOSPlatform ("tvos12.0")]
+	[UnsupportedOSPlatform ("ios12.0")]
+#if TVOS
+	[Obsolete ("Starting with tvos12.0 use 'Metal' instead.", DiagnosticId = "BI1234", UrlFormat = "https://github.com/xamarin/xamarin-macios/wiki/Obsolete")]
+#elif IOS
+	[Obsolete ("Starting with ios12.0 use 'Metal' instead.", DiagnosticId = "BI1234", UrlFormat = "https://github.com/xamarin/xamarin-macios/wiki/Obsolete")]
+#endif
+#else
 	[Deprecated (PlatformName.iOS, 12,0, message: "Use 'Metal' instead.")]
 	[Deprecated (PlatformName.TvOS, 12,0, message: "Use 'Metal' instead.")]
+#endif
 	public static class EAGLDrawableProperty {
 		public static readonly NSString ColorFormat;
 		public static readonly NSString RetainedBacking;
@@ -29,8 +39,18 @@ namespace OpenGLES {
 		}
 	}
 
+#if NET
+	[UnsupportedOSPlatform ("tvos12.0")]
+	[UnsupportedOSPlatform ("ios12.0")]
+#if TVOS
+	[Obsolete ("Starting with tvos12.0 use 'Metal' instead.", DiagnosticId = "BI1234", UrlFormat = "https://github.com/xamarin/xamarin-macios/wiki/Obsolete")]
+#elif IOS
+	[Obsolete ("Starting with ios12.0 use 'Metal' instead.", DiagnosticId = "BI1234", UrlFormat = "https://github.com/xamarin/xamarin-macios/wiki/Obsolete")]
+#endif
+#else
 	[Deprecated (PlatformName.iOS, 12,0, message: "Use 'Metal' instead.")]
 	[Deprecated (PlatformName.TvOS, 12,0, message: "Use 'Metal' instead.")]
+#endif
 	public static class EAGLColorFormat {
 		public static readonly NSString RGB565;
 		public static readonly NSString RGBA8;
--- src/OpenGLES/EAGLContext.cs
+++ src/OpenGLES/EAGLContext.cs
@@ -22,15 +22,39 @@ namespace OpenGLES
 		}
 #endif
 
+#if NET
+		[SupportedOSPlatform ("ios10.0")]
+		[SupportedOSPlatform ("tvos10.0")]
+		[UnsupportedOSPlatform ("tvos12.0")]
+		[UnsupportedOSPlatform ("ios12.0")]
+#if TVOS
+		[Obsolete ("Starting with tvos12.0 use 'Metal' instead.", DiagnosticId = "BI1234", UrlFormat = "https://github.com/xamarin/xamarin-macios/wiki/Obsolete")]
+#elif IOS
+		[Obsolete ("Starting with ios12.0 use 'Metal' instead.", DiagnosticId = "BI1234", UrlFormat = "https://github.com/xamarin/xamarin-macios/wiki/Obsolete")]
+#endif
+#else
 		[iOS (10,0)]
 		[TV (10,0)]
+#endif
 		public virtual bool PresentRenderBuffer (nuint target, double presentationTime)
 		{
 			return _PresentRenderbufferAtTime (target, presentationTime);
 		}
 
+#if NET
+		[SupportedOSPlatform ("ios10.3")]
+		[SupportedOSPlatform ("tvos10.2")]
+		[UnsupportedOSPlatform ("tvos12.0")]
+		[UnsupportedOSPlatform ("ios12.0")]
+#if TVOS
+		[Obsolete ("Starting with tvos12.0 use 'Metal' instead.", DiagnosticId = "BI1234", UrlFormat = "https://github.com/xamarin/xamarin-macios/wiki/Obsolete")]
+#elif IOS
+		[Obsolete ("Starting with ios12.0 use 'Metal' instead.", DiagnosticId = "BI1234", UrlFormat = "https://github.com/xamarin/xamarin-macios/wiki/Obsolete")]
+#endif
+#else
 		[iOS (10,3)]
 		[TV (10,2)]
+#endif
 		public virtual bool PresentRenderBuffer (nuint target, double presentationTime, PresentationMode mode)
 		{
 			switch (mode) {
