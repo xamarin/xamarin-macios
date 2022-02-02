1f93aa53e4836892efb96cc12373286388e62d8c
TJ Lambert <tjlambert@microsoft.com>
2022-02-02 08:11:35 -0600

    Reverting change to src/PassKit/PKEnums.cs

--- src/PassKit/PKEnums.cs
+++ src/PassKit/PKEnums.cs
@@ -8,15 +8,8 @@ namespace PassKit {
 
 	// untyped enum -> PKError.h
 	// This never seemed to be deprecatd, yet in iOS8 it's obsoleted
-#if NET
-	[UnsupportedOSPlatform ("macos")]
-#if IOS
-	[Obsolete ("Starting with ios8.0.", DiagnosticId = "BI1234", UrlFormat = "https://github.com/xamarin/xamarin-macios/wiki/Obsolete")]
-#endif
-#else
 	[Obsoleted (PlatformName.iOS, 8, 0)]
 	[NoMac]
-#endif
 	public enum PKErrorCode {
 		None = 0,
 		Unknown = 1,
@@ -25,11 +18,7 @@ namespace PassKit {
 	}
 
 	// NSInteger -> PKPass.h
-#if NET
-	[SupportedOSPlatform ("macos11.0")]
-#else
 	[Mac (11,0)]
-#endif
 	[ErrorDomain ("PKPassKitErrorDomain")]
 	[Native]
 	public enum PKPassKitErrorCode : long {
@@ -42,13 +31,8 @@ namespace PassKit {
 	}
 
 	// NSInteger -> PKPassLibrary.h
-#if NET
-	[SupportedOSPlatform ("ios7.0")]
-	[SupportedOSPlatform ("macos11.0")]
-#else
 	[iOS (7,0)]
 	[Mac (11,0)]
-#endif
 	[Native]
 	public enum PKPassLibraryAddPassesStatus : long {
 		DidAddPasses,
@@ -56,127 +40,58 @@ namespace PassKit {
 		DidCancelAddPasses
 	}
 
-#if NET
-	[SupportedOSPlatform ("macos11.0")]
-#else
 	[Mac (11,0)]
-#endif
 	[Native]
 	public enum PKPassType : ulong {
 		Barcode,
 		SecureElement,
-#if NET
-		[UnsupportedOSPlatform ("ios13.4")]
-#if IOS
-		[Obsolete ("Starting with ios13.4 use 'SecureElement' instead.", DiagnosticId = "BI1234", UrlFormat = "https://github.com/xamarin/xamarin-macios/wiki/Obsolete")]
-#endif
-		[UnsupportedOSPlatform ("macos")]
-#else
 		[NoMac]
 		[Deprecated (PlatformName.iOS, 13, 4, message: "Use 'SecureElement' instead.")]
 		[Deprecated (PlatformName.WatchOS, 6, 2, message: "Use 'SecureElement' instead.")]
-#endif
 		Payment = SecureElement,
 		Any = ulong.MaxValue,
 	}
 
-#if NET
-	[SupportedOSPlatform ("macos11.0")]
-#else
 	[Mac (11,0)]
 	[Watch (3,0)]
-#endif
 	[Native]
 	public enum PKPaymentAuthorizationStatus : long {
 		Success,
 		Failure,
 
-#if NET
-		[UnsupportedOSPlatform ("ios11.0")]
-#if IOS
-		[Obsolete ("Starting with ios11.0 use 'Failure' and 'PKPaymentRequest.CreatePaymentBillingAddressInvalidError'.", DiagnosticId = "BI1234", UrlFormat = "https://github.com/xamarin/xamarin-macios/wiki/Obsolete")]
-#endif
-		[UnsupportedOSPlatform ("macos")]
-#else
 		[NoMac]
 		[Deprecated (PlatformName.WatchOS, 4,0, message: "Use 'Failure' and 'PKPaymentRequest.CreatePaymentBillingAddressInvalidError'.")]
 		[Deprecated (PlatformName.iOS, 11,0, message: "Use 'Failure' and 'PKPaymentRequest.CreatePaymentBillingAddressInvalidError'.")]
-#endif
 		InvalidBillingPostalAddress,
 
-#if NET
-		[UnsupportedOSPlatform ("ios11.0")]
-#if IOS
-		[Obsolete ("Starting with ios11.0 use 'Failure' and 'PKPaymentRequest.CreatePaymentShippingAddressInvalidError'.", DiagnosticId = "BI1234", UrlFormat = "https://github.com/xamarin/xamarin-macios/wiki/Obsolete")]
-#endif
-		[UnsupportedOSPlatform ("macos")]
-#else
 		[NoMac]
 		[Deprecated (PlatformName.WatchOS, 4,0, message: "Use 'Failure' and 'PKPaymentRequest.CreatePaymentShippingAddressInvalidError'.")]
 		[Deprecated (PlatformName.iOS, 11,0, message: "Use 'Failure' and 'PKPaymentRequest.CreatePaymentShippingAddressInvalidError'.")]
-#endif
 		InvalidShippingPostalAddress,
 
-#if NET
-		[UnsupportedOSPlatform ("ios11.0")]
-#if IOS
-		[Obsolete ("Starting with ios11.0 use 'Failure' and 'PKPaymentRequest.CreatePaymentContactInvalidError'.", DiagnosticId = "BI1234", UrlFormat = "https://github.com/xamarin/xamarin-macios/wiki/Obsolete")]
-#endif
-		[UnsupportedOSPlatform ("macos")]
-#else
 		[NoMac]
 		[Deprecated (PlatformName.WatchOS, 4,0, message: "Use 'Failure' and 'PKPaymentRequest.CreatePaymentContactInvalidError'.")]
 		[Deprecated (PlatformName.iOS, 11,0, message: "Use 'Failure' and 'PKPaymentRequest.CreatePaymentContactInvalidError'.")]
-#endif
 		InvalidShippingContact,
 
-#if NET
-		[SupportedOSPlatform ("ios9.2")]
-		[SupportedOSPlatform ("macos11.0")]
-#else
 		[iOS (9,2)]
-#endif
 		PinRequired,
-#if NET
-		[SupportedOSPlatform ("ios9.2")]
-		[SupportedOSPlatform ("macos11.0")]
-#else
 		[iOS (9,2)]
-#endif
 		PinIncorrect,
-#if NET
-		[SupportedOSPlatform ("ios9.2")]
-		[SupportedOSPlatform ("macos11.0")]
-#else
 		[iOS (9,2)]
-#endif
 		PinLockout
 	}
 
-#if NET
-	[UnsupportedOSPlatform ("ios13.4")]
-#if IOS
-	[Obsolete ("Starting with ios13.4 use 'PKSecureElementPassActivationState' instead.", DiagnosticId = "BI1234", UrlFormat = "https://github.com/xamarin/xamarin-macios/wiki/Obsolete")]
-#endif
-	[UnsupportedOSPlatform ("macos")]
-#else
 	[NoMac]
 	[Deprecated (PlatformName.iOS, 13, 4, message: "Use 'PKSecureElementPassActivationState' instead.")]
 	[Deprecated (PlatformName.WatchOS, 6, 2, message: "Use 'PKSecureElementPassActivationState' instead.")]
-#endif
 	[Native]
 	public enum PKPaymentPassActivationState : ulong {
 		Activated, RequiresActivation, Activating, Suspended, Deactivated
 	}
 
-#if NET
-	[SupportedOSPlatform ("macos11.0")]
-	[SupportedOSPlatform ("ios13.4")]
-#else
 	[Mac (11,0)]
-	[Watch (6,2)]
-	[iOS (13,4)]
-#endif
+	[Watch (6,2), iOS (13,4)]
 	[Native]
 	public enum PKSecureElementPassActivationState : long {
 		Activated,
@@ -186,12 +101,8 @@ namespace PassKit {
 		Deactivated,
 	}
 
-#if NET
-	[SupportedOSPlatform ("macos11.0")]
-#else
 	[Mac (11,0)]
 	[Watch (3,0)]
-#endif
 	[Native]
 	public enum PKMerchantCapability : ulong {
 		ThreeDS = 1 << 0,
@@ -200,18 +111,10 @@ namespace PassKit {
 		Debit = 1 << 3
 	}
 
-#if NET
-	[UnsupportedOSPlatform ("ios11.0")]
-#if IOS
-	[Obsolete ("Starting with ios11.0 use 'PKContactField' instead.", DiagnosticId = "BI1234", UrlFormat = "https://github.com/xamarin/xamarin-macios/wiki/Obsolete")]
-#endif
-	[UnsupportedOSPlatform ("macos")]
-#else
 	[NoMac]
 	[Watch (3,0)]
 	[Deprecated (PlatformName.iOS, 11,0, message: "Use 'PKContactField' instead.")]
 	[Deprecated (PlatformName.WatchOS, 4,0, message: "Use 'PKContactField' instead.")]
-#endif
 	[Native]
 	[Flags]
 	public enum PKAddressField : ulong {
@@ -219,171 +122,65 @@ namespace PassKit {
 		PostalAddress = 1 << 0,
 		Phone = 1 << 1,
 		Email = 1 << 2,
-#if NET
-		[SupportedOSPlatform ("ios8.3")]
-		[UnsupportedOSPlatform ("ios11.0")]
-#if IOS
-		[Obsolete ("Starting with ios11.0 use 'PKContactField' instead.", DiagnosticId = "BI1234", UrlFormat = "https://github.com/xamarin/xamarin-macios/wiki/Obsolete")]
-#endif
-		[UnsupportedOSPlatform ("macos")]
-#else
 		[iOS (8,3)]
-#endif
 		Name = 1 << 3,
 		All = PostalAddress|Phone|Email|Name
 	}
 
-#if NET
-	[SupportedOSPlatform ("macos11.0")]
-	[SupportedOSPlatform ("ios8.3")]
-#else
 	[Mac (11,0)]
 	[NoWatch]
 	[iOS (8,3)]
-#endif
 	[Native]
 	public enum PKPaymentButtonStyle : long {
 		White,
 		WhiteOutline,
 		Black,
-#if NET
-		[SupportedOSPlatform ("ios14.0")]
-		[SupportedOSPlatform ("macos11.0")]
-#else
 		[iOS (14,0)]
-#endif
 		Automatic = 3,
 	}
 
-#if NET
-	[SupportedOSPlatform ("macos11.0")]
-	[SupportedOSPlatform ("ios8.3")]
-#else
 	[Mac (11,0)]
 	[NoWatch]
 	[iOS (8,3)]
-#endif
 	[Native]
 	public enum PKPaymentButtonType : long {
 		Plain,
 		Buy,
-#if NET
-		[SupportedOSPlatform ("ios9.0")]
-		[SupportedOSPlatform ("macos11.0")]
-#else
 		[iOS (9,0)]
-#endif
 		SetUp,
-#if NET
-		[SupportedOSPlatform ("ios10.0")]
-		[SupportedOSPlatform ("macos11.0")]
-#else
 		[iOS (10,0)]
-#endif
 		InStore,
-#if NET
-		[SupportedOSPlatform ("ios10.2")]
-		[SupportedOSPlatform ("macos11.0")]
-#else
 		[iOS (10,2)]
-#endif
 		Donate,
-#if NET
-		[SupportedOSPlatform ("ios12.0")]
-		[SupportedOSPlatform ("macos11.0")]
-#else
 		[iOS (12,0)]
-#endif
 		Book,
-#if NET
-		[SupportedOSPlatform ("ios12.0")]
-		[SupportedOSPlatform ("macos11.0")]
-#else
 		[iOS (12,0)]
-#endif
 		Checkout,
-#if NET
-		[SupportedOSPlatform ("ios12.0")]
-		[SupportedOSPlatform ("macos11.0")]
-#else
 		[iOS (12,0)]
-#endif
 		Subscribe,
-#if NET
-		[SupportedOSPlatform ("ios14.0")]
-		[SupportedOSPlatform ("macos11.0")]
-#else
 		[iOS (14,0)]
-#endif
 		Reload = 8,
-#if NET
-		[SupportedOSPlatform ("ios14.0")]
-		[SupportedOSPlatform ("macos11.0")]
-#else
 		[iOS (14,0)]
-#endif
 		AddMoney = 9,
-#if NET
-		[SupportedOSPlatform ("ios14.0")]
-		[SupportedOSPlatform ("macos11.0")]
-#else
 		[iOS (14,0)]
-#endif
 		TopUp = 10,
-#if NET
-		[SupportedOSPlatform ("ios14.0")]
-		[SupportedOSPlatform ("macos11.0")]
-#else
 		[iOS (14,0)]
-#endif
 		Order = 11,
-#if NET
-		[SupportedOSPlatform ("ios14.0")]
-		[SupportedOSPlatform ("macos11.0")]
-#else
 		[iOS (14,0)]
-#endif
 		Rent = 12,
-#if NET
-		[SupportedOSPlatform ("ios14.0")]
-		[SupportedOSPlatform ("macos11.0")]
-#else
 		[iOS (14,0)]
-#endif
 		Support = 13,
-#if NET
-		[SupportedOSPlatform ("ios14.0")]
-		[SupportedOSPlatform ("macos11.0")]
-#else
 		[iOS (14,0)]
-#endif
 		Contribute = 14,
-#if NET
-		[SupportedOSPlatform ("ios14.0")]
-		[SupportedOSPlatform ("macos11.0")]
-#else
 		[iOS (14,0)]
-#endif
 		Tip = 15,
-#if NET
-		[SupportedOSPlatform ("macos12.0")]
-		[SupportedOSPlatform ("ios15.0")]
-#else
-		[Mac (12,0)]
-		[iOS (15,0)]
-		[Watch (8,0)]
-#endif
+		[Mac (12,0), iOS (15,0), Watch (8,0)]
 		Continue = 16,
 	}
 
-#if NET
-	[SupportedOSPlatform ("macos11.0")]
-	[SupportedOSPlatform ("ios8.3")]
-#else
 	[Mac (11,0)]
 	[Watch (3,0)]
 	[iOS (8,3)]
-#endif
 	[Native]
 	public enum PKShippingType : ulong {
 		Shipping,
@@ -392,14 +189,9 @@ namespace PassKit {
 		ServicePickup,
 	}
 
-#if NET
-	[SupportedOSPlatform ("ios9.0")]
-	[SupportedOSPlatform ("macos11.0")]
-#else
 	[Watch (6,0)]
 	[iOS (9,0)]
 	[Mac (11,0)]
-#endif
 	[Native]
 	public enum PKAddPaymentPassError : long
 	{
@@ -408,14 +200,9 @@ namespace PassKit {
 		SystemCancelled
 	}
 
-#if NET
-	[SupportedOSPlatform ("macos11.0")]
-	[SupportedOSPlatform ("ios9.0")]
-#else
 	[Mac (11,0)]
 	[NoWatch]
 	[iOS (9,0)]
-#endif
 	[Native]
 	public enum PKAutomaticPassPresentationSuppressionResult : ulong
 	{
@@ -426,14 +213,9 @@ namespace PassKit {
 		Success
 	}
 
-#if NET
-	[SupportedOSPlatform ("macos11.0")]
-	[SupportedOSPlatform ("ios9.0")]
-#else
 	[Mac (11,0)]
 	[Watch (3,0)]
 	[iOS (9,0)]
-#endif
 	[Native]
 	public enum PKPaymentMethodType : ulong
 	{
@@ -445,14 +227,9 @@ namespace PassKit {
 		EMoney,
 	}
 
-#if NET
-	[SupportedOSPlatform ("macos11.0")]
-	[SupportedOSPlatform ("ios9.0")]
-#else
 	[Mac (11,0)]
 	[Watch (3,0)]
 	[iOS (9,0)]
-#endif
 	[Native]
 	public enum PKPaymentSummaryItemType : ulong
 	{
@@ -460,28 +237,17 @@ namespace PassKit {
 		Pending
 	}
 
-#if NET
-	[SupportedOSPlatform ("ios9.0")]
-	[UnsupportedOSPlatform ("macos")]
-#else
 	[NoWatch]
 	[NoMac] // under `#if TARGET_OS_IOS`
 	[iOS (9,0)]
-#endif
 	[Native]
 	public enum PKAddPassButtonStyle : long {
 		Black = 0,
 		Outline
 	}
 
-#if NET
-	[SupportedOSPlatform ("macos11.0")]
-	[SupportedOSPlatform ("ios11.0")]
-#else
 	[Mac (11,0)]
-	[Watch (4,0)]
-	[iOS (11,0)]
-#endif
+	[Watch (4,0)][iOS (11,0)]
 	[ErrorDomain ("PKPaymentErrorDomain")]
 	[Native]
 	public enum PKPaymentErrorCode : long {
@@ -493,28 +259,17 @@ namespace PassKit {
 		CouponCodeExpired,
 	}
 
-#if NET
-	[SupportedOSPlatform ("ios12.0")]
-	[SupportedOSPlatform ("macos11.0")]
-#else
 	[iOS (12,0)]
 	[Mac (11,0)]
 	[NoWatch] // https://feedbackassistant.apple.com/feedback/6301809 https://github.com/xamarin/maccore/issues/1819
-#endif
 	[Native]
 	public enum PKAddPaymentPassStyle : ulong {
 		Payment,
 		Access,
 	}
 
-#if NET
-	[SupportedOSPlatform ("ios13.4")]
-	[SupportedOSPlatform ("macos11.0")]
-#else
-	[Watch (6,2)]
-	[iOS (13,4)]
+	[Watch (6,2), iOS (13,4)]
 	[Mac (11,0)]
-#endif
 	[ErrorDomain ("PKAddSecureElementPassErrorDomain")]
 	[Native]
 	public enum PKAddSecureElementPassErrorCode : long {
