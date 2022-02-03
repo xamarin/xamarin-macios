56313f896d0abbc22eae0c3eb35b88b103df6bcd
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-02-03 17:56:20 +0100

    [AppKit] Use the correct interface for inlining protocols in our api definitions in .NET. (#14041)
    Also update/fix code to show the corresponding warning in the generator, and
    make a note of why MKUserLocation/MKAnnotation is special.

--- src/appkit.cs
+++ src/appkit.cs
@@ -27184,7 +27184,11 @@ namespace AppKit {
 	[NoMacCatalyst]
 	[BaseType (typeof(NSObject))]
 	[DisableDefaultCtor]
+#if NET
+	interface NSFontAssetRequest : NSProgressReporting
+#else
 	interface NSFontAssetRequest : INSProgressReporting
+#endif
 	{
 		[Export ("initWithFontDescriptors:options:")]
 		[DesignatedInitializer]
