2f71b9aef3b401e36accb5b3f9edb97c41586293
tj_devel709 <antlambe@microsoft.com>
2022-02-03 07:07:47 -0600

    Attribute Conversion

--- src/VideoSubscriberAccount/VSAccountMetadataRequest.cs
+++ src/VideoSubscriberAccount/VSAccountMetadataRequest.cs
@@ -8,7 +8,15 @@ namespace VideoSubscriberAccount {
 
 	public partial class VSAccountMetadataRequest {
 
-		[TV (10,1)][iOS (10,2)]
+#if NET
+		[SupportedOSPlatform ("tvos10.1")]
+		[SupportedOSPlatform ("ios10.2")]
+		[SupportedOSPlatform ("macos10.14")]
+		[UnsupportedOSPlatform ("maccatalyst")]
+#else
+		[TV (10,1)]
+		[iOS (10,2)]
+#endif
 		public VSAccountProviderAuthenticationScheme[] SupportedAuthenticationSchemes {
 			get {
 				return VSAccountProviderAuthenticationSchemeExtensions.GetValues (SupportedAuthenticationSchemesString);
