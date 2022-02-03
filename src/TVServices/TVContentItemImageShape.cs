a1f815f8e095dd096e81fb7d83ba3a7d35c8b139
tj_devel709 <antlambe@microsoft.com>
2022-02-03 06:44:37 -0600

    Attribute Conversion

--- src/TVServices/TVContentItemImageShape.cs
+++ src/TVServices/TVContentItemImageShape.cs
@@ -13,12 +13,26 @@ namespace TVServices {
 
 	static public class TVContentItemImageShapeExtensions {
 
+#if NET
+		[UnsupportedOSPlatform ("tvos13.0")]
+#if TVOS
+		[Obsolete ("Starting with tvos13.0.", DiagnosticId = "BI1234", UrlFormat = "https://github.com/xamarin/xamarin-macios/wiki/Obsolete")]
+#endif
+#else
 		[Deprecated (PlatformName.TvOS, 13,0)]
+#endif
 		[DllImport (Constants.TVServicesLibrary)]
 		static extern CGSize TVTopShelfImageSizeForShape (/* TVContentItemImageShape */ nint shape,
 			/* TVTopShelfContentStyle */ nint style);
 
+#if NET
+		[UnsupportedOSPlatform ("tvos13.0")]
+#if TVOS
+		[Obsolete ("Starting with tvos13.0 use 'TVTopShelfSectionedContent.GetImageSize' or 'TVTopShelfInsetContent.ImageSize' instead.", DiagnosticId = "BI1234", UrlFormat = "https://github.com/xamarin/xamarin-macios/wiki/Obsolete")]
+#endif
+#else
 		[Deprecated (PlatformName.TvOS, 13,0, message: "Use 'TVTopShelfSectionedContent.GetImageSize' or 'TVTopShelfInsetContent.ImageSize' instead.")]
+#endif
 		static public CGSize GetSize (this TVContentItemImageShape self, TVTopShelfContentStyle style)
 		{
 			return TVTopShelfImageSizeForShape ((nint) (int) self, (nint) (int) style);
--- src/TVServices/TVEnums.cs
+++ src/TVServices/TVEnums.cs
@@ -12,7 +12,11 @@ using ObjCRuntime;
 
 namespace TVServices {
 
+#if NET
+	[SupportedOSPlatform ("tvos9.0")]
+#else
 	[TV (9,0)]
+#endif
 	[Native]
 	public enum TVContentItemImageShape : long {
 		None = 0,
@@ -24,14 +28,22 @@ namespace TVServices {
 		ExtraWide
 	}
 
+#if NET
+	[SupportedOSPlatform ("tvos9.0")]
+#else
 	[TV (9,0)]
+#endif
 	[Native]
 	public enum TVTopShelfContentStyle : long {
 		Inset = 1,
 		Sectioned = 2
 	}
 
+#if NET
+	[SupportedOSPlatform ("tvos11.0")]
+#else
 	[TV (11,0)]
+#endif
 	[Native]
 	[Flags]
 	public enum TVContentItemImageTrait : ulong {
