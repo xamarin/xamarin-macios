3b33deb2171f3f2a274b1107f1a01612a7f04fcd
tj_devel709 <antlambe@microsoft.com>
2022-02-03 07:07:30 -0600

    Remove existing attributes

--- src/VideoSubscriberAccount/VSAccountMetadataRequest.cs
+++ src/VideoSubscriberAccount/VSAccountMetadataRequest.cs
@@ -8,12 +8,7 @@ namespace VideoSubscriberAccount {
 
 	public partial class VSAccountMetadataRequest {
 
-#if NET
-		[SupportedOSPlatform ("ios10.2")]
-		[SupportedOSPlatform ("tvos10.1")]
-#else
 		[TV (10,1)][iOS (10,2)]
-#endif
 		public VSAccountProviderAuthenticationScheme[] SupportedAuthenticationSchemes {
 			get {
 				return VSAccountProviderAuthenticationSchemeExtensions.GetValues (SupportedAuthenticationSchemesString);
