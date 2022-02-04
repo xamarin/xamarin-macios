2cd02092b995d49bc8368cfca2329d81799e3fb0
Stephen Hawley <sthawl@microsoft.com>
2022-02-04 10:00:42 -0500

    Remove existing attributes

--- src/XKit/Types.cs
+++ src/XKit/Types.cs
@@ -34,12 +34,7 @@ namespace AppKit {
 namespace UIKit {
 #endif
 
-#if !NET
 	[Watch (4,0), TV (11,0), iOS (11,0)]
-#else
-	[SupportedOSPlatform ("ios11.0")]
-	[SupportedOSPlatform ("tvos11.0")]
-#endif
 	[StructLayout (LayoutKind.Sequential)]
 	public struct NSDirectionalEdgeInsets {
 
