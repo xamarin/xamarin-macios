c4620d8b84dbce7fb4897b372975ce1072069206
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-02-14 11:11:59 +0100

    [ObjCRuntime] Keep accepting IntPtr constructors in .NET as an alternative to NativeHandle constructors. Fixes #14046.
    As a part of the breaking changes in .NET, we introduced a new type,
    `ObjCRuntime.NativeHandle`, to represent native handles.
    
    This meant that constructors taking taking `IntPtr handle`:
    
    ```cs
    public class MyUIViewController : UIViewController {
        protected MyUIViewController (IntPtr handle)
            : base (handle)
        {
        }
    }
    ```
    
    would have to be ported to take `NativeHandle handle`:
    
    ```cs
    public class MyUIViewController : UIViewController {
        protected MyUIViewController (NativeHandle handle)
            : base (handle)
        {
        }
    }
    ```
    
    The unfortunate part is that there will be no compiler warnings or errors
    flagging this, so users won't know to do it unless they either read the
    documentation (🤣) or run into the problem, googles for a while, runs into
    someone else who had the same problem, and applies their (probably broken)
    fix.
    
    So we change our logic to:
    
    1. Look for and use an `(IntPtr)` (or `(IntPtr, bool)`) constructor in .NET if
       the `NativeHandle` version isn't found.
    2. Show a warning.
    3. Some time in the future maybe remove this hack/workaround.
    
    Fixes https://github.com/xamarin/xamarin-macios/issues/14046.

--- src/ObjCRuntime/Runtime.cs
+++ src/ObjCRuntime/Runtime.cs
@@ -1206,7 +1206,11 @@ namespace ObjCRuntime {
 
 			var ctorArguments = new object [1];
 #if NET
-			ctorArguments [0] = new NativeHandle (ptr);
+			if (ctor.GetParameters () [0].ParameterType == typeof (IntPtr)) {
+				ctorArguments [0] = ptr;
+			} else {
+				ctorArguments [0] = new NativeHandle (ptr);
+			}
 #else
 			ctorArguments [0] = ptr;
 #endif
@@ -1232,7 +1236,11 @@ namespace ObjCRuntime {
 
 			var ctorArguments = new object [2];
 #if NET
-			ctorArguments [0] = new NativeHandle (ptr);
+			if (ctor.GetParameters () [0].ParameterType == typeof (IntPtr)) {
+				ctorArguments [0] = ptr;
+			} else {
+				ctorArguments [0] = new NativeHandle (ptr);
+			}
 #else
 			ctorArguments [0] = ptr;
 #endif
@@ -1253,11 +1261,17 @@ namespace ObjCRuntime {
 					return rv;
 			}
 			var ctors = type.GetConstructors (BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic);
+			ConstructorInfo? backupConstructor = null;
 			for (int i = 0; i < ctors.Length; ++i) {
 				var param = ctors[i].GetParameters ();
 				if (param.Length != 1)
 					continue;
 #if NET
+				if (param [0].ParameterType == typeof (IntPtr)) {
+					backupConstructor = ctors [i];
+					continue;
+				}
+
 				if (param [0].ParameterType != typeof (NativeHandle))
 #else
 				if (param [0].ParameterType != typeof (IntPtr))
@@ -1268,6 +1282,16 @@ namespace ObjCRuntime {
 					intptr_ctor_cache [type] = ctors [i];
 				return ctors [i];
 			}
+
+#if NET
+			if (backupConstructor is not null) {
+				Console.WriteLine ("The type {0} does not have a constructor that takes an ObjCRuntime.NativeHandle parameter, but a constructor that takes an System.IntPtr parameter was found instead. It's highly recommended to change the signature of the System.IntPtr constructor to be ObjCRuntime.NativeHandle instead.", type.FullName);
+				lock (intptr_ctor_cache)
+					intptr_ctor_cache [type] = backupConstructor;
+				return backupConstructor;
+			}
+#endif
+
 			return null;
 		}
 
@@ -1278,23 +1302,40 @@ namespace ObjCRuntime {
 					return rv;
 			}
 			var ctors = type.GetConstructors (BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic);
+			ConstructorInfo? backupConstructor = null;
 			for (int i = 0; i < ctors.Length; ++i) {
 				var param = ctors[i].GetParameters ();
 				if (param.Length != 2)
 					continue;
+
+				if (param [1].ParameterType != typeof (bool))
+					continue;
 #if NET
+				if (param [0].ParameterType == typeof (IntPtr)) {
+					backupConstructor = ctors [i];
+					continue;
+				}
+
 				if (param [0].ParameterType != typeof (NativeHandle))
 #else
 				if (param [0].ParameterType != typeof (IntPtr))
 #endif
-					continue;
-				if (param [1].ParameterType != typeof (bool))
 					continue;
 
 				lock (intptr_bool_ctor_cache)
 					intptr_bool_ctor_cache [type] = ctors [i];
 				return ctors [i];
 			}
+
+#if NET
+			if (backupConstructor is not null) {
+				Console.WriteLine ("The type {0} does not have a constructor that takes two (ObjCRuntime.NativeHandle, bool) arguments. However, a constructor that takes two (System.IntPtr, bool) parameters was found (and will be used instead). It's highly recommended to change the signature of the (System.IntPtr, bool) constructor to be (ObjCRuntime.NativeHandle, bool).", type.FullName);
+				lock (intptr_bool_ctor_cache)
+					intptr_bool_ctor_cache [type] = backupConstructor;
+				return backupConstructor;
+			}
+#endif
+
 			return null;
 		}
 
