e09f330cb9b82a9a574daf153712f2171e123a73
TJ Lambert <50846373+tj-devel709@users.noreply.github.com>
2022-02-28 10:41:40 -0600

    [addressbook] Add nullability to (generated and manual) bindings (#14146)
    * using Is Null and better error throwing
    
    * missing nullable enable
    
    * using nullable string properties instead
    
    Co-authored-by: TJ Lambert <tjlambert@microsoft.com>

--- src/AddressBook/ABRecord.cs
+++ src/AddressBook/ABRecord.cs
@@ -80,7 +80,7 @@ namespace AddressBook {
 		internal static ABRecord FromHandle (IntPtr handle, ABAddressBook? addressbook, bool owns = true)
 		{
 			if (handle == IntPtr.Zero)
-				throw new ArgumentNullException (nameof (handle));
+				ObjCRuntime.ThrowHelper.ThrowArgumentNullException (nameof (handle));
 			// TODO: does ABGroupCopyArrayOfAllMembers() have Create or Get
 			// semantics for the array elements?
 			var type = ABRecordGetRecordType (handle);
