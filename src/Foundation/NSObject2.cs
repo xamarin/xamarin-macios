4b75c756e133d7e2c23a44fc125f1b1173308f40
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2022-02-28 09:57:59 -0500

    Reduce string duplication in the platform assemblies (part 2) (#14223)
    The size of unicode strings for the minimal MySingleView.app (.net
    version) goes from 5171 to 4553 characters (each being 2 bytes).
    
    This was 6315 before [part 1](https://github.com/xamarin/xamarin-macios/pull/14191).
    
    [Size report](https://gist.github.com/9f24ce012556c6f4e1a7d09de8f83efc)
    
    Part of the work to fix https://github.com/xamarin/xamarin-macios/issues/14188

--- src/Foundation/NSObject2.cs
+++ src/Foundation/NSObject2.cs
@@ -331,8 +331,8 @@ namespace Foundation {
 		private void InitializeObject (bool alloced) {
 			if (alloced && handle == NativeHandle.Zero && Class.ThrowOnInitFailure) {
 				if (ClassHandle == NativeHandle.Zero)
-					throw new Exception ($"Could not create an native instance of the type '{GetType ().FullName}': the native class hasn't been loaded.\n{Constants.SetThrowOnInitFailureToFalse}");
-				throw new Exception ($"Could not create an native instance of the type '{new Class (ClassHandle).Name}'.\n{Constants.SetThrowOnInitFailureToFalse}");
+					throw new Exception ($"Could not create an native instance of the type '{GetType ().FullName}': the native class hasn't been loaded.\n{Constants.SetThrowOnInitFailureToFalse}.");
+				throw new Exception ($"Could not create an native instance of the type '{new Class (ClassHandle).Name}'.\n{Constants.SetThrowOnInitFailureToFalse}.");
 			}
 
 			// The authorative value for the IsDirectBinding value is the register attribute:
@@ -596,13 +596,13 @@ namespace Foundation {
 		{
 			if (this.handle == NativeHandle.Zero && Class.ThrowOnInitFailure) {
 				if (ClassHandle == NativeHandle.Zero)
-					throw new Exception ($"Could not create an native instance of the type '{GetType ().FullName}': the native class hasn't been loaded.\n{Constants.SetThrowOnInitFailureToFalse}");
-				throw new Exception ($"Could not create an native instance of the type '{new Class (ClassHandle).Name}'.\n{Constants.SetThrowOnInitFailureToFalse}");
+					throw new Exception ($"Could not create an native instance of the type '{GetType ().FullName}': the native class hasn't been loaded.\n{Constants.SetThrowOnInitFailureToFalse}.");
+				throw new Exception ($"Could not create an native instance of the type '{new Class (ClassHandle).Name}'.\n{Constants.SetThrowOnInitFailureToFalse}.");
 			}
 
 			if (handle == NativeHandle.Zero && Class.ThrowOnInitFailure) {
 				Handle = NativeHandle.Zero; // We'll crash if we don't do this.
-				throw new Exception ($"Could not initialize an instance of the type '{GetType ().FullName}': the native '{initSelector}' method returned nil.\n{Constants.SetThrowOnInitFailureToFalse}");
+				throw new Exception ($"Could not initialize an instance of the type '{GetType ().FullName}': the native '{initSelector}' method returned nil.\n{Constants.SetThrowOnInitFailureToFalse}.");
 			}
 
 			this.Handle = handle;
