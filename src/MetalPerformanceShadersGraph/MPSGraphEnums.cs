f266883f8272ad0974e1c90989351339a261d3f4
Frank A. Krueger <fak@praeclarum.org>
2022-03-01 19:50:17 -0800

    Add graph options and placeholder tensors

--- src/MetalPerformanceShadersGraph/MPSGraphEnums.cs
+++ src/MetalPerformanceShadersGraph/MPSGraphEnums.cs
@@ -6,4 +6,12 @@ using ObjCRuntime;
 using Metal;
 
 namespace MetalPerformanceShadersGraph {
+
+	[iOS (14,0), TV (14,0), Mac (11,0), MacCatalyst (14,0)]
+	public enum MPSGraphOptions : ulong {
+		None = 0,
+		SynchronizeResults = 1,
+		Verbose = 2,
+		Default = SynchronizeResults
+	}
 }
--- src/metalperformanceshadersgraph.cs
+++ src/metalperformanceshadersgraph.cs
@@ -8,12 +8,49 @@ using ObjCRuntime;
 using NativeHandle = System.IntPtr;
 #endif
 
-namespace MetalPerformanceShadersGraph {
+using MPSGraphTensorDataDictionary = Foundation.NSDictionary<MetalPerformanceShadersGraph.MPSGraphTensor, MetalPerformanceShadersGraph.MPSGraphTensorData>;
+using MPSGraphTensorShapedTypeDictionary = Foundation.NSDictionary<MetalPerformanceShadersGraph.MPSGraphTensor, MetalPerformanceShadersGraph.MPSGraphShapedType>;
 
+namespace MetalPerformanceShadersGraph
+{
 	// MPSGraph.h
 
-	[TV (14,0), Mac (11,0), iOS (14,0)]
-	[BaseType (typeof(NSObject))]
+	[iOS (14,0), TV (14,0), Mac (11,0), MacCatalyst (14,0)]
+	[BaseType (typeof (NSObject))]
 	interface MPSGraph {
+		[iOS (14,0), TV (14,0), Mac (11,0), MacCatalyst (14,0)]
+		[Static]
+		[Export ("new")]
+		MPSGraph Create ();
+
+		[iOS (14,0), TV (14,0), Mac (11,0), MacCatalyst (14,0)]
+		[Export ("options")]
+		MPSGraphOptions Options { get; set; }
+
+		[iOS (14,0), TV (14,0), Mac (11,0), MacCatalyst (14,0)]
+		[Export ("placeholderTensors")]
+		NSArray<MPSGraphTensor> PlaceholderTensors { get; }
+	}
+
+	// MPSGraphCore.h
+
+	[iOS (14,0), TV (14,0), Mac (11,0), MacCatalyst (14,0)]
+	[BaseType (typeof (NSObject))]
+	[DisableDefaultCtor]
+	interface MPSGraphShapedType : NSCopying {
+	}
+
+	// MPSGraphTensor.h
+
+	[iOS (14,0), TV (14,0), Mac (11,0), MacCatalyst (14,0)]
+	[BaseType (typeof (NSObject))]
+	[DisableDefaultCtor]
+	interface MPSGraphTensor : NSCopying {
+	}
+
+	[iOS (14,0), TV (14,0), Mac (11,0), MacCatalyst (14,0)]
+	[BaseType (typeof (NSObject))]
+	[DisableDefaultCtor]
+	interface MPSGraphTensorData {
 	}
 }
