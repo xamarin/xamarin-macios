10ff4abcf724b875f465c2e2e052b6b33d005f57
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-03-10 21:04:18 +0100

    [msbuild] Make the SymbolStrip task have the old behavior of not copying input files.

--- msbuild/Xamarin.MacDev.Tasks/Tasks/SymbolStrip.cs
+++ msbuild/Xamarin.MacDev.Tasks/Tasks/SymbolStrip.cs
@@ -1,10 +1,15 @@
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.Build.Framework;
+
 using Xamarin.Messaging.Build.Client;
 
 #nullable enable
 
 namespace Xamarin.MacDev.Tasks
 {
-	public class SymbolStrip : SymbolStripTaskBase
+	public class SymbolStrip : SymbolStripTaskBase, ITaskCallback
 	{
 		public override bool Execute ()
 		{
@@ -13,5 +18,11 @@ namespace Xamarin.MacDev.Tasks
 
 			return base.Execute ();
 		}
+
+		public bool ShouldCopyToBuildServer (ITaskItem item) => false;
+
+		public bool ShouldCreateOutputFile (ITaskItem item) => false;
+
+		public IEnumerable<ITaskItem> GetAdditionalItemsToBeCopied () => Enumerable.Empty<ITaskItem> ();
 	}
 }
