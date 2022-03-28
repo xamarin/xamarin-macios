6487ec485e416e803a2e5bad48f9e11c6a6a0415
Steve Hawley <sthawl@microsoft.com>
2022-03-28 16:06:54 -0400

    Lets do warnings (#14510)
    * Remove existing attributes
    
    * Attribute Conversion
    
    * Added some events
    
    * init only properties and uint
    
    * propagation error

---
+++ tools/nnyeah/nnyeah/WarningEventArgs.cs
@@ -0,0 +1,20 @@
+using System;
+
+#nullable enable
+
+namespace nnyeah {
+	public class WarningEventArgs : BaseTransformEventArgs {
+		public WarningEventArgs (string containerName, string methodName, string targetOperand, string message)
+			: base (containerName, methodName, targetOperand)
+		{
+			Message = message;
+		}
+
+		public string Message { get; init; }
+
+		public override string HelpfulMessage ()
+		{
+			return $"In {ContainerName}.{MethodName}, found reference to {TargetOperand} - this is not transformable and will likely not work if invoked.";
+		}
+	}
+}
