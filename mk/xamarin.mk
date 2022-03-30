3d27ed4f73065feb66766aaa48523b1fb4373f04
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-03-30 20:55:47 +0200

    Bump maccore again.
    New commits in xamarin/maccore:
    
    * xamarin/maccore@cf9f7409e9 [mlaunch] Fix booting simulator devices on older Xcode versions.
    
    Diff: https://github.com/xamarin/maccore/compare/1c5dfc89f0a104d5ef4fcf839ca2d02acf547415..cf9f7409e9a79d0d0835863cc9f5afe77ba5e74a

--- mk/xamarin.mk
+++ mk/xamarin.mk
@@ -7,7 +7,7 @@ MONO_BRANCH    := $(shell cd $(MONO_PATH) 2> /dev/null && git symbolic-ref --sho
 endif
 
 ifdef ENABLE_XAMARIN
-NEEDED_MACCORE_VERSION := 1c5dfc89f0a104d5ef4fcf839ca2d02acf547415
+NEEDED_MACCORE_VERSION := cf9f7409e9a79d0d0835863cc9f5afe77ba5e74a
 NEEDED_MACCORE_BRANCH := main
 
 MACCORE_DIRECTORY := maccore
