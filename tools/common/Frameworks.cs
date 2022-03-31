77bac878d63a765ae4d1da7d087a98d6e6846b0a
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-03-31 14:20:08 +0200

    [IdentityLookupUI] Add this framework to Mac Catalyst. (#14385)
    Co-authored-by: Manuel de la Pena <mandel@microsoft.com>

--- tools/common/Frameworks.cs
+++ tools/common/Frameworks.cs
@@ -661,7 +661,6 @@ public class Frameworks : Dictionary <string, Framework>
 				case "iAd":
 				case "CHIP":
 #endif
-				case "IdentityLookupUI":
 				case "WatchConnectivity":
 					f.Unavailable = true;
 					break;
