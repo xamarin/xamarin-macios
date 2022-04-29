a07223783ae59cb442cb599292641fb5a86afb18
Manuel de la Pena <mandel@microsoft.com>
2022-04-29 13:08:28 -0400

    [Nnyeah] Set nullability enabled by default.

--- tools/nnyeah/nnyeah/Transformation.cs
+++ tools/nnyeah/nnyeah/Transformation.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Mono.Cecil.Cil;
 
-#nullable enable
-
 namespace Microsoft.MaciOS.Nnyeah {
 	public class Transformation {
 		static List<Instruction> emptyInstructions = new List<Instruction> ();
