75909452e83f53c913c5678dc7e65d72dde53091
TJ Lambert <tjlambert@microsoft.com>
2022-04-29 11:07:26 -0500

    add nullable enable

--- src/PrintCore/Defs.cs
+++ src/PrintCore/Defs.cs
@@ -1,4 +1,4 @@
-//
+﻿//
 // Defs.cs: PrintCore definitions
 //
 // Authors:
@@ -6,6 +6,9 @@
 //
 // Copyright 2016 Microsoft Inc
 //
+
+#nullable enable
+
 using System;
 using System.Runtime.InteropServices;
 using System.Threading;
