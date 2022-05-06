27694be29db516cb908db2a9b38589636d726151
Chris Hamons <chris.hamons@xamarin.com>
2022-05-06 13:32:35 -0500

    Fix whitespace

--- tools/nnyeah/tests/utils/Compiler.cs
+++ tools/nnyeah/tests/utils/Compiler.cs
@@ -16,6 +16,7 @@ namespace Microsoft.MaciOS.Nnyeah.Tests {
 		watchOS,
 		tvOS,
 	}
+
 	public class Compiler {
 		const string MonoCompiler = "/Library/Frameworks/Mono.framework/Versions/Current/Commands/csc";
 
