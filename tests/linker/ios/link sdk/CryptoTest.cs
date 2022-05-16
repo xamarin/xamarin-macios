0b169195b2db1bfe38a4c6c84983c071e8ad359d
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-05-16 11:41:13 +0200

    [tests] Adjust a few linker tests to cope with unreliable networks. (#15018)
    * Handle 502 and 503 errors in the TrustUsingOldPolicy and TrustUsingNewCallback tests. Fixes:
    
        [FAIL] TrustUsingOldPolicy : System.Net.WebException : The remote server returned an error: (503) Service Unavailable.
        [FAIL] TrustUsingNewCallback : System.Net.WebException : The remote server returned an error: (503) Service Unavailable.
    
    * Add more http and https urls to try (and don't use microsoft.com). Hopefully fixes:
    
        [FAIL] WebClient_SSL_Leak : At least one url should work
    
    This is a follow-up to #14943.

--- tests/linker/ios/link sdk/CryptoTest.cs
+++ tests/linker/ios/link sdk/CryptoTest.cs
@@ -68,6 +68,12 @@ namespace LinkSdk {
 				// caching means it will be called at least for the first run, but it might not
 				// be called again in subsequent requests (unless it expires)
 				Assert.That (trust_validation_callback, Is.GreaterThan (0), "validation done");
+			} catch (WebException we) {
+				// The remote server returned an error: (502) Bad Gateway.
+				// The remote server returned an error: (503) Service Unavailable.
+				if (we.Message.Contains ("(502)") || we.Message.Contains ("(503)"))
+					Assert.Inconclusive (we.Message);
+				throw;
 			}
 			finally {
 				ServicePointManager.ServerCertificateValidationCallback = null;
