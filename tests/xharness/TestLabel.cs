7819bb1b9ea45dd1aee617d5ebd86705261f3507
Manuel de la Pena <mandel@microsoft.com>
2022-06-01 16:18:32 -0400

    Remove not needed label.

--- tests/xharness/TestLabel.cs
+++ tests/xharness/TestLabel.cs
@@ -30,52 +30,50 @@ namespace Xharness {
 		BindingFramework = 1 << 4,
 		[Label("bindings-xcframework")]
 		BindingsXcframework = 1 << 5,
-		[Label ("btouch")]
-		Btouch = 1 << 6,
 		[Label ("cecil")]
-		Cecil = 1 << 7,
+		Cecil = 1 << 6,
 		[Label ("docs")]
-		Docs = 1 << 8,
+		Docs = 1 << 7,
 		[Label ("dotnettests")]
-		DotnetTest = 1 << 9,
+		DotnetTest = 1 << 8,
 		[Label ("fsharp")]
-		Fsharp = 1 << 10,
+		Fsharp = 1 << 9,
 		[Label("framework")]
-		Framework = 1 << 11,
+		Framework = 1 << 10,
 		[Label ("generator")]
-		Generator = 1 << 12,
+		Generator = 1 << 11,
 		[Label ("interdependent-binding-projects")]
-		InterdependentBindingProjects = 1 << 13,
+		InterdependentBindingProjects = 1 << 12,
 		[Label("install-source")]
-		InstallSource = 1 << 14,
+		InstallSource = 1 << 13,
 		[Label ("introspection")]
-		Introspection = 1 << 15,
+		Introspection = 1 << 14,
 		[Label ("linker")]
-		Linker = 1 << 16,
+		Linker = 1 << 15,
 		[Label ("library-projects")]
-		LibraryProjects = 1 << 17,
+		LibraryProjects = 1 << 16,
 		[Label ("mac-binding-project")]
-		MacBindingProject = 1 << 18,
+		MacBindingProject = 1 << 17,
 		[Label ("mmp")]
-		Mmp = 1 << 19,
+		Mmp = 1 << 18,
 		[Label ("mononative")]
-		Mononative = 1 << 20,
+		Mononative = 1 << 19,
 		[Label ("monotouch")]
-		Monotouch = 1 << 21,
+		Monotouch = 1 << 20,
 		[Label ("msbuild")]
-		Msbuild = 1 << 22,
+		Msbuild = 1 << 21,
 		[Label ("mtouch")]
-		Mtouch = 1 << 23,
+		Mtouch = 1 << 22,
 		[Label ("sampletester")]
-		SampleTester = 1 << 24,
+		SampleTester = 1 << 23,
 		[Label ("system-permission")]
-		SystemPermission = 1 << 25,
+		SystemPermission = 1 << 24,
 		[Label ("xammac")]
-		Xammac = 1 << 26,
+		Xammac = 1 << 25,
 		[Label ("xcframework")]
-		Xcframework = 1 << 27,
+		Xcframework = 1 << 26,
 		[Label ("xtro")]
-		Xtro = 1 << 28,
+		Xtro = 1 << 27,
 		[Label ("all")]
 		All =  Int64.MaxValue, 
 	}
