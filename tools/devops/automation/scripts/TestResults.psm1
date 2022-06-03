9e2e2cb91ef9580baab49a26cef4aa8e3d539a32
Manuel de la Pena <mandel@microsoft.com>
2022-06-03 08:48:04 -0400

    Start building the comment.

--- tools/devops/automation/scripts/TestResults.psm1
+++ tools/devops/automation/scripts/TestResults.psm1
@@ -3,15 +3,18 @@ using module ".\\GitHub.psm1"
 class TestResults {
     [string] $ResultsPath # path to the file with the results
     [string] $TestsJobStatus # the value of the env var that lets us know if the tests passed or not can be null or empty
+    [string] $Label
     [string] $Context
 
     TestResults (
         [string] $path,
         [string] $status,
+        [string] $label,
         [string] $context
     ) {
         $this.ResultsPath = $path
         $this.TestsJobStatus = $status
+        $this.Label = $label
         $this.Context = $context
     }
 
@@ -20,7 +23,7 @@ class TestResults {
     }
 
     [void] WriteComment($stringBuilder) {
-        $stringBuilder.AppendLine("## Test results")
+        $stringBuilder.AppendLine("## Test results - $(this.Context)")
         $stringBuilder.AppendLine("")
         if (-not (Test-Path $this.ResultsPath -PathType Leaf)) {
              $stringBuilder.AppendLine(":fire: Tests failed catastrophically on $($this.Context) (no summary found).") 
@@ -42,7 +45,7 @@ class TestResults {
                     $stringBuilder.AppendLine($line)
                 }
             }
-        } 
+        }
 
     }
 
@@ -63,6 +66,59 @@ class TestResults {
     }
 }
 
+class ParallelTestsResults {
+    [string] $Context
+    [TestResults[]] $Results
+
+    ParallelTestsResults (
+        [string] $context,
+        [TestResults[]] $results
+    ) {
+        $this.Content = $context
+        $this.Results = $results
+    }
+
+    [object] GetFailingTests() {
+        $failingTests = [System.Collections.ArrayList]@()
+        foreach ($result in $this.Results) {
+            if (-not $result.IsSuccess()) {
+                $failingTests.Add($result)
+            }
+        }
+        return $failingTests
+    }
+
+    [string] GetTestCount() {
+        return ""
+    }
+
+    [void] WriteComment($stringBuilder) {
+        $stringBuilder.AppendLine("# Test results")
+        # We need to add a small summary at the top. We check if it was a success, if that is 
+        # the case, we just need to state it and 
+        $failingTests = $this.GetFailingTests()
+
+        if ($failingTests.Count -eq 0) {
+            $stringBuilder.AppendLine(":white_check_mark: All tests passed on $($this.Context).")
+            $stringBuilder.AppendLine("")
+            $stringBuilder.AppendLine($this.GetTestCount())
+        } else {
+            $stringBuilder.AppendLine(":x: Tests failed on $($this.Context)") 
+            $stringBuilder.AppendLine("")
+            $stringBuilder.AppendLine($this.GetTestCount())
+            $stringBuilder.AppendLine("")
+            $stringBuilder.AppendLine("## Failures")
+            # loop over all results and add the content
+            foreach ($result in $failingTests)
+            {
+                # create a detail per test result with the name of the test and will contain the exact summary
+                $stringBuilder.AppendLine("<details><summary>Tests for $($result.Label)</summary>")
+                $result.WriteComment($stringBuilder)
+                $stringBuilder.AppendLine("</details>")
+            }
+        }
+    }
+}
 
 <# 
     .SYNOPSIS
@@ -74,6 +130,8 @@ function New-TestResults {
         $Path,
         [string]
         $Status,
+        [string]
+        $Label,
         [string]
         $Context
     )
