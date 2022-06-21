6eff1160997586dcf0b999087664a6190d37172e
Manuel de la Pena <mandel@microsoft.com>
2022-06-21 10:40:03 -0400

    [CI] Enable Write-Debug when the pipeline was executed in debug mode.

--- tools/devops/automation/scripts/GitHub.psm1
+++ tools/devops/automation/scripts/GitHub.psm1
@@ -135,7 +135,7 @@ class GitHubStatuses {
         foreach ($s in $presentStatuses) {
             # we found a status from a previous build that was a success, we do not want to step on it
             if (($s.context -eq $context) -and ($s.state -eq "success")) {
-                Write-Host "WARNING: Found status for $Context because it was already set as a success, overriding result."
+                Write-Debug "WARNING: Found status for $Context because it was already set as a success, overriding result."
             }
         }
 
@@ -693,7 +693,7 @@ function New-GitHubComment {
     }
 
     $request = Invoke-Request -Request { Invoke-RestMethod -Uri $url -Headers $headers -Method "POST" -Body ($payload | ConvertTo-Json) -ContentType 'application/json' }
-    Write-Host $request
+    Write-Debug $request
     return $request
 }
 
@@ -870,7 +870,7 @@ function New-GitHubSummaryComment {
     }
 
     if (-not (Test-Path $TestSummaryPath -PathType Leaf)) {
-        Write-Host "No test summary found"
+        Write-Debug "No test summary found"
         $request = New-GitHubComment -Header "Tests failed catastrophically on $Context (no summary found)." -Emoji ":fire:" -Description "Result file $TestSummaryPath not found. $headerLinks"
     } else {
         if ($Env:TESTS_JOBSTATUS -eq "") {
@@ -916,7 +916,7 @@ function Get-GitHubPRInfo {
     }
 
     $request = Invoke-Request -Request { Invoke-RestMethod -Uri $url -Method "GET" -ContentType 'application/json' -Headers $headers }
-    Write-Host $request
+    Write-Debug $request
     return $request
 }
 
@@ -1018,8 +1018,8 @@ function New-GistWithContent {
 
     $url = "https://api.github.com/gists"
     $payloadJson = $payload | ConvertTo-Json
-    Write-Host "Url is $url"
-    Write-Host "Payload is $payloadJson"
+    Write-Debug "Url is $url"
+    Write-Debug "Payload is $payloadJson"
 
     $headers = @{
         Accept = "application/vnd.github.v3+json";
@@ -1027,7 +1027,7 @@ function New-GistWithContent {
     } 
 
     $request = Invoke-RestMethod -Uri $url -Headers $headers -Method "POST" -Body $payloadJson -ContentType 'application/json'
-    Write-Host $request
+    Write-Debug $request
     return $request.html_url
 }
 
@@ -1104,8 +1104,8 @@ function New-GistWithFiles {
 
     $url = "https://api.github.com/gists"
     $payloadJson = $payload | ConvertTo-Json
-    Write-Host "Url is $url"
-    Write-Host "Payload is $payloadJson"
+    Write-Debug "Url is $url"
+    Write-Debug "Payload is $payloadJson"
 
     $headers = @{
         Accept = "application/vnd.github.v3+json";
@@ -1113,7 +1113,7 @@ function New-GistWithFiles {
     } 
 
     $request = Invoke-Request -Request { Invoke-RestMethod -Uri $url -Headers $headers -Method "POST" -Body $payloadJson -ContentType 'application/json' }
-    Write-Host $request
+    Write-Debug $request
     return $request.html_url
 }
 
@@ -1152,7 +1152,7 @@ function Push-RepositoryDispatch {
     }
 
     $url = "https://api.github.com/repos/$Org/$Repository/dispatches"
-    Write-Host $url
+    Write-Debug $url
     $payloadJson = $payload | ConvertTo-Json
 
     $headers = @{
@@ -1161,8 +1161,8 @@ function Push-RepositoryDispatch {
     } 
 
     $request = Invoke-Request -Request { Invoke-RestMethod -Uri $url -Headers $headers -Method "POST" -Body $payloadJson -ContentType 'application/json' }
-    Write-Host $request
-    Write-Host $request.Content
+    Write-Debug $request
+    Write-Debug $request.Content
 }
 
 # This function processes markdown, and replaces:
@@ -1202,7 +1202,7 @@ function Convert-Markdown {
                         $gistUrl = New-GistWithFiles $fileToGist $filesToGist
                         $gistText = "[gist](" + $gistUrl + ")"
                     } catch {
-                        Write-Host "Unable to create gist: $_"
+                        Write-Debug "Unable to create gist: $_"
                         $gistText = "Unable to create gist: $($_.Exception.Message)"
                     }
                 }
