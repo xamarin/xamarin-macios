9b22ee8bb73936236213b969f731b7c3e0eca802
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-06-30 17:57:04 +0200

    Revert "Add a fix for macOS."
    This reverts commit 2505981ffe5e2c331fdc3ac97798788cf82f70a2.

--- src/ObjCRuntime/Constants.cs
+++ src/ObjCRuntime/Constants.cs
@@ -6,13 +6,7 @@ namespace ObjCRuntime {
 	public static partial class Constants {
 		public const string libSystemLibrary = "/usr/lib/libSystem.B.dylib";
 		public const string libcLibrary = libSystemLibrary;
-#if __MACOS__ && NET
-		// For macOS we have a redirect for the messaging functions that depends on the library being "/usr/lib/libobjc.dylib":
-		// https://github.com/dotnet/runtime/blob/cca6bb6be279ee92325148b8525ece2ebdcb2070/src/coreclr/vm/interoplibinterface_objc.cpp#L120
-		public const string ObjectiveCLibrary = "/usr/lib/libobjc.dylib";
-#else
 		public const string ObjectiveCLibrary = "/usr/lib/libobjc.A.dylib";
-#endif
 		public const string SystemLibrary = libSystemLibrary;
 		public const string libdispatchLibrary = "/usr/lib/system/libdispatch.dylib";
 #if !NET
