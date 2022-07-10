35654a1905985ffdcdfc44df828915b3753209c9
Marius Ungureanu <maungu@microsoft.com>
2022-07-10 23:18:33 +0300

    Replace loop with Array.ConvertAll

--- src/Foundation/NSUrlSessionHandler.cs
+++ src/Foundation/NSUrlSessionHandler.cs
@@ -715,9 +715,7 @@ namespace Foundation {
 			{
 				if (sessionHandler.cookieContainer is not null && cookies.Length > 0)
 					lock (sessionHandler.inflightRequestsLock) { // ensure we lock when writing to the collection
-						var cookiesContents = new string [cookies.Length];
-						for (var index = 0; index < cookies.Length; index++)
-							cookiesContents [index] = cookies [index].GetHeaderValue ();
+						var cookiesContents = Array.ConvertAll(cookies, static cookie => cookie.GetHeaderValue());
 						sessionHandler.cookieContainer.SetCookies (uri, string.Join (',', cookiesContents)); //  as per docs: The contents of an HTTP set-cookie header as returned by a HTTP server, with Cookie instances delimited by commas.
 					}
 			}
