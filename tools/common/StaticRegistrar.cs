cb84ba9ad9be32d0babbda33d294caeb0fb68225
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-07-13 11:45:10 +0200

    [runtime] Use a custom native -> managed trampoline for calling NSObject.InvokeConformsToProtocol from the generated static registrar code.
    This avoids one case where we we embed metadata tokens to a different assembly
    in the generated static registrar code.
    
    This is required for supporting per-assembly static registration
    (https://github.com/xamarin/xamarin-macios/issues/12067).

--- tools/common/StaticRegistrar.cs
+++ tools/common/StaticRegistrar.cs
@@ -3340,6 +3340,22 @@ namespace Registrar {
 				return;
 			}
 
+			var customConformsToProtocol = method.Selector == "conformsToProtocol:" && method.Method.DeclaringType.Is ("Foundation", "NSObject") && method.Method.Name == "InvokeConformsToProtocol" && method.Parameters.Length == 1;
+			if (customConformsToProtocol) {
+				if (Driver.IsDotNet) {
+					customConformsToProtocol &= method.Parameters [0].Is ("ObjCRuntime", "NativeHandle");
+				} else {
+					customConformsToProtocol &= method.Parameters [0].Is ("System", "IntPtr");
+				}
+				if (customConformsToProtocol) {
+					sb.AppendLine ("-(BOOL) conformsToProtocol: (void *) protocol");
+					sb.AppendLine ("{");
+					sb.AppendLine ("return xamarin_invoke_conforms_to_protocol (self, (Protocol *) protocol);");
+					sb.AppendLine ("}");
+					return;
+				}
+			}
+
 			var rettype = string.Empty;
 			var returntype = method.ReturnType;
 			var isStatic = method.IsStatic;
