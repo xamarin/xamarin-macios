a6a5214b9d6a34b4e4614198c0aa170f4d2b9301
Manuel de la Pena <mandel@microsoft.com>
2022-08-04 16:55:05 -0400

    [SafaryServices] Add support for Xcode14 upt to beta 4.

--- src/safariservices.cs
+++ src/safariservices.cs
@@ -531,4 +531,19 @@ namespace SafariServices {
 		[Export ("invalidate")]
 		void Invalidate ();
 	}
+
+	[iOS (16,0)]
+	[BaseType (typeof(NSObject))]
+	[DisableDefaultCtor]
+	interface SFSafariViewControllerDataStore
+	{
+		[Static]
+		[Export ("defaultDataStore", ArgumentSemantic.Strong)]
+		SFSafariViewControllerDataStore DefaultDataStore { get; }
+
+		[Async]
+		[Export ("clearWebsiteDataWithCompletionHandler:")]
+		void ClearWebsiteData ([NullAllowed] Action completion);
+	}
+
 }
