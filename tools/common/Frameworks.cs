75e799559a49d8d9b4df7bbbbcb35fafd2a975dc
Manuel de la Pena <mandel@microsoft.com>
2022-08-05 17:17:49 -0400

    [BackgroundAssets] Add new framework from Xcode1 4 beta 4.

--- tools/common/Frameworks.cs
+++ tools/common/Frameworks.cs
@@ -275,6 +275,8 @@ public class Frameworks : Dictionary <string, Framework>
 					{ "ShazamKit", "ShazamKit", 12,0 },
 
 					{ "ScreenCaptureKit", "ScreenCaptureKit", 12,3 },
+
+					{ "BackgroundAssets", "BackgroundAssets", 13,0},
 				};
 			}
 			return mac_frameworks;
@@ -444,6 +446,8 @@ public class Frameworks : Dictionary <string, Framework>
 				{ "OSLog", "OSLog", 15,0 },
 				{ "ShazamKit", "ShazamKit", new Version (15,0), NotAvailableInSimulator},
 				{ "ThreadNetwork", "ThreadNetwork", new Version (15,0), NotAvailableInSimulator},
+
+				{ "BackgroundAssets", "BackgroundAssets", 16,0},
 
 				// the above MUST be kept in sync with simlauncher
 				// see tools/mtouch/Makefile
