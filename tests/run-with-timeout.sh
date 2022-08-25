5ab9af842a966706bf0d5f3ebdedb0b58c5ceba1
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-08-25 17:54:00 +0200

    Improve script.

--- tests/run-with-timeout.sh
+++ tests/run-with-timeout.sh
@@ -26,12 +26,36 @@ PID=$$
 	done
 
 	# ⏰ - prepare to die
+
+	# try to capture the screen
+	mkdir -p screenshots-$PID
+	screencapture -x screenshots-$PID/capture-$PID.png
+	echo "##vso[artifact.upload containerfolder=screenshots;artifactname=screenshots-$PID/capture-$PID.png]"
+
 	# but first send a QUIT to get a managed stack trace dump
+	echo "The command '$*' timed out after $TIMEOUT second(s). Sending SIGQUIT..."
 	kill -s SIGQUIT $PID
-	sleep 10
+	sleep 10 # printing the stack trace dump might take a few seconds
+
 	# and then abort since that creates a crash report
+	echo "The command '$*' timed out after $TIMEOUT second(s). Sending SIGABRT..."
 	kill -s SIGABRT $PID
-	echo "The command '$*' timed out after $TIMEOUT second(s)."
+
+	# wait 10 seconds to see if the process really exited. Sometimes Mono will hang while processing SIGABRT
+	echo "The command '$*' timed out after $TIMEOUT second(s). Waiting for process to exit...."
+	C=1
+	while kill -0 $PID; do
+		sleep 0.1
+		((C++))
+		if [[ $C -ge 100 ]]; then
+			echo "The command '$*' timed out after $TIMEOUT second(s). Sending SIGKILL..."
+			kill -s SIGKILL $PID
+			break
+		fi
+	done
+
+	echo "The command '$*' timed out after $TIMEOUT second(s). The process has been killed."
+
 ) 2> /dev/null &
 
 exec "$@"
