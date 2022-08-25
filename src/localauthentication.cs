4132c6ef170069d7bbee42650ea06b977351836e
Israel Soto <issoto@microsoft.com>
2022-08-25 11:35:21 -0500

    [LocalAuthentication] Add support for Xcode 14 b1-3

--- src/localauthentication.cs
+++ src/localauthentication.cs
@@ -3,9 +3,13 @@ using ObjCRuntime;
 using Foundation;
 using Security;
 
+#if !NET
+using NativeHandle = System.IntPtr;
+#endif
+
 namespace LocalAuthentication {
 
-	[Mac (10,13,2)][iOS (11,0)]
+	[Mac (10,13,2)][iOS (11,0)][NoWatch][NoTV]
 	[Native]
 	public enum LABiometryType : long {
 		None,
@@ -22,7 +26,7 @@ namespace LocalAuthentication {
 	[iOS (8,0), Mac (10,10)]
 	delegate void LAContextReplyHandler (bool success, NSError error);
 
-	[iOS (8,0), Mac (10,10)] // ".objc_class_name_LAContext", referenced from: '' not found
+	[iOS (8,0), Mac (10,10), NoTV] // ".objc_class_name_LAContext", referenced from: '' not found
 	[BaseType (typeof (NSObject))]
 	interface LAContext {
 		[NullAllowed] // by default this property is null
@@ -30,7 +34,7 @@ namespace LocalAuthentication {
 		string LocalizedFallbackTitle { get; set; }
 
 #if !NET
-		[iOS (8,3)]
+		[iOS (8,3), NoTV]
 		[Field ("LAErrorDomain")]
 		NSString ErrorDomain { get; }
 #endif
@@ -92,8 +96,188 @@ namespace LocalAuthentication {
 		[Export ("interactionNotAllowed")]
 		bool InteractionNotAllowed { get; set; }
 
-		[Mac (10,13,2)][iOS (11,0)]
+		[Mac (10,13,2)][iOS (11,0)][NoWatch]
 		[Export ("biometryType")]
 		LABiometryType BiometryType { get; }
+	}
+
+	[Mac (13,0), iOS (16,0), MacCatalyst (16,0), NoWatch, NoTV]
+	[BaseType (typeof (LARight))]
+	[DisableDefaultCtor]
+	interface LAPersistedRight
+	{
+		[Export ("key")]
+		LAPrivateKey Key { get; }
+
+		[Export ("secret")]
+		LASecret Secret { get; }
+	}
+
+	delegate void LAPrivateKeyCompletionHandler ([NullAllowed] NSData data, [NullAllowed] NSError error);
+
+	[Mac (13,0), iOS (16,0), MacCatalyst (16,0), NoWatch, NoTV]
+	[BaseType (typeof (NSObject))]
+	[DisableDefaultCtor]
+	interface LAPrivateKey
+	{
+		[Export ("publicKey")]
+		LAPublicKey PublicKey { get; }
+
+		[Async]
+		[Export ("signData:secKeyAlgorithm:completion:")]
+		void SignData (NSData data, SecKeyAlgorithm algorithm, LAPrivateKeyCompletionHandler handler);
+
+		[Export ("canSignUsingSecKeyAlgorithm:")]
+		bool CanSign (SecKeyAlgorithm algorithm);
+
+		[Async]
+		[Export ("decryptData:secKeyAlgorithm:completion:")]
+		void DecryptData (NSData data, SecKeyAlgorithm algorithm, LAPrivateKeyCompletionHandler handler);
+
+		[Export ("canDecryptUsingSecKeyAlgorithm:")]
+		bool CanDecrypt (SecKeyAlgorithm algorithm);
+
+		[Async]
+		[Export ("exchangeKeysWithPublicKey:secKeyAlgorithm:secKeyParameters:completion:")]
+		void ExchangeKeysWithPublicKey (NSData publicKey, SecKeyAlgorithm algorithm, NSDictionary parameters, LAPrivateKeyCompletionHandler handler);
+
+		[Export ("canExchangeKeysUsingSecKeyAlgorithm:")]
+		bool CanExchangeKeys (SecKeyAlgorithm algorithm);
+	}
+
+	delegate void LAPublicKeyCompletionHandler ([NullAllowed] NSData data, [NullAllowed] NSError error);
+	delegate void LAPublicKeyVerifyDataCompletionHandler ([NullAllowed] NSError error);
+
+	[Mac (13,0), iOS (16,0), MacCatalyst (16,0), NoWatch, NoTV]
+	[BaseType (typeof (NSObject))]
+	[DisableDefaultCtor]
+	interface LAPublicKey
+	{
+		[Async]
+		[Export ("exportBytesWithCompletion:")]
+		void ExportBytes (LAPublicKeyCompletionHandler handler);
+
+		[Export ("encryptData:secKeyAlgorithm:completion:")]
+		void EncryptData (NSData data, SecKeyAlgorithm algorithm, LAPublicKeyCompletionHandler handler);
+
+		[Export ("canEncryptUsingSecKeyAlgorithm:")]
+		bool CanEncrypt (SecKeyAlgorithm algorithm);
+
+		[Export ("verifyData:signature:secKeyAlgorithm:completion:")]
+		void VerifyData (NSData signedData, NSData signature, SecKeyAlgorithm algorithm, LAPublicKeyVerifyDataCompletionHandler handler);
+
+		[Export ("canVerifyUsingSecKeyAlgorithm:")]
+		bool CanVerify (SecKeyAlgorithm algorithm);
+	}
+
+	[Mac (13,0), iOS (16,0), MacCatalyst (16,0), NoWatch, NoTV]
+	[BaseType (typeof (NSObject))]
+	interface LAAuthenticationRequirement
+	{
+		[Static]
+		[Export ("defaultRequirement")]
+		LAAuthenticationRequirement DefaultRequirement { get; }
+
+		[Static]
+		[Export ("biometryRequirement")]
+		LAAuthenticationRequirement BiometryRequirement { get; }
+
+		[Static]
+		[Export ("biometryCurrentSetRequirement")]
+		LAAuthenticationRequirement BiometryCurrentSetRequirement { get; }
+
+		[Static]
+		[Export ("biometryRequirementWithFallback:")]
+		LAAuthenticationRequirement GetBiometryRequirement (LABiometryFallbackRequirement fallback);
+	}
+
+	[Mac (13,0), iOS (16,0), MacCatalyst (16,0), NoWatch, NoTV]
+	[BaseType (typeof (NSObject))]
+	interface LABiometryFallbackRequirement
+	{
+		[Static]
+		[Export ("defaultRequirement")]
+		LABiometryFallbackRequirement DefaultRequirement { get; }
+
+		[Static]
+		[Export ("devicePasscodeRequirement")]
+		LABiometryFallbackRequirement DevicePasscodeRequirement { get; }
+	}
+
+	delegate void LARightAuthorizeCompletionHandler ([NullAllowed] NSError error);
+
+	[Mac (13,0), iOS (16,0), MacCatalyst (16,0), NoWatch, NoTV]
+	[BaseType (typeof (NSObject))]
+	interface LARight
+	{
+		[Export ("state")]
+		LARightState State { get; }
+
+		[Export ("tag")]
+		nint Tag { get; set; }
+
+		[Export ("initWithRequirement:")]
+		NativeHandle Constructor (LAAuthenticationRequirement requirement);
+
+		[Async]
+		[Export ("authorizeWithLocalizedReason:completion:")]
+		void Authorize (string localizedReason, LARightAuthorizeCompletionHandler handler);
+
+		[Async]
+		[Export ("checkCanAuthorizeWithCompletion:")]
+		void CheckCanAuthorize (LARightAuthorizeCompletionHandler handler);
+
+		[Async]
+		[Export ("deauthorizeWithCompletion:")]
+		void Deauthorize (Action handler);
+	}
+
+	delegate void LARightStoreCompletionHandler ([NullAllowed] LAPersistedRight right, [NullAllowed] NSError error);
+	delegate void LARightStoreRemoveRightCompletionHandler ([NullAllowed] NSError error);
+
+	[Mac (13,0), iOS (16,0), MacCatalyst (16,0), NoWatch, NoTV]
+	[BaseType (typeof(NSObject))]
+	[DisableDefaultCtor]
+	interface LARightStore
+	{
+		[Static]
+		[Export ("sharedStore")]
+		LARightStore SharedStore { get; }
+
+		[Async]
+		[Export ("rightForIdentifier:completion:")]
+		void GetRight (string identifier, LARightStoreCompletionHandler handler);
+
+		[Async]
+		[Export ("saveRight:identifier:completion:")]
+		void SaveRight (LARight right, string identifier, LARightStoreCompletionHandler handler);
+
+		[Async]
+		[Export ("saveRight:identifier:secret:completion:")]
+		void SaveRight (LARight right, string identifier, NSData secret, LARightStoreCompletionHandler handler);
+
+		[Async]
+		[Export ("removeRight:completion:")]
+		void RemoveRight (LAPersistedRight right, LARightStoreRemoveRightCompletionHandler handler);
+
+		[Async]
+		[Export ("removeRightForIdentifier:completion:")]
+		void RemoveRight (string identifier, LARightStoreRemoveRightCompletionHandler handler);
+
+		[Async]
+		[Export ("removeAllRightsWithCompletion:")]
+		void RemoveAllRights (LARightStoreRemoveRightCompletionHandler handler);
+	}
+
+	delegate void LASecretCompletionHandler ([NullAllowed] NSData data, [NullAllowed] NSError error);
+
+	[Mac (13,0), iOS (16,0), MacCatalyst (16,0), NoWatch, NoTV]
+	[BaseType (typeof (NSObject))]
+	[DisableDefaultCtor]
+	interface LASecret
+	{
+		[Async]
+		[Export ("loadDataWithCompletion:")]
+		void LoadData (LASecretCompletionHandler handler);
 	}
 }
