4f155ea598408a7352df36ff5aa2812c1adbeafe
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-08-29 17:05:53 +0200

    [tests] Add .NET test projects for additional app extensions.

---
+++ tests/dotnet/AdditionalAppExtensionConsumer/nativeextension/macOS/NativeContainer/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  NativeContainer
+//
+//  Created by Rolf Bjarne Kvinge on 30/8/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
