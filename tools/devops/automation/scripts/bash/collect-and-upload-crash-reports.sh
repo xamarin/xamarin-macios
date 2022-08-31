7ac3a466d5bfa0243a7d7fd2789dc728511887ad
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-08-31 12:29:56 +0200

    [devops] Fix echo command that prints a comment.
    Fix echo command so that the text to be printed isn't interpreted as a
    comment.
    
    Really everything should be quoted in bash :/

--- tools/devops/automation/scripts/bash/collect-and-upload-crash-reports.sh
+++ tools/devops/automation/scripts/bash/collect-and-upload-crash-reports.sh
@@ -6,7 +6,7 @@ if test -d "$HOME/Library/Logs/DiagnosticReports"; then
   zip -9r "$SYSTEM_DEFAULTWORKINGDIRECTORY/crash-reports.zip" "$HOME/Library/Logs/DiagnosticReports"
   if test -f "$SYSTEM_DEFAULTWORKINGDIRECTORY/crash-reports.zip"; then
     set +x
-    echo ##vso[artifact.upload containerfolder=CrashReports;artifactname=crash-reports-$SYSTEM_STAGEDISPLAYNAME-$BUILD_BUILDID-$SYSTEM_JOBATTEMPT)]$SYSTEM_DEFAULTWORKINGDIRECTORY/crash-reports.zip
+    echo "##vso[artifact.upload containerfolder=CrashReports;artifactname=crash-reports-$SYSTEM_STAGEDISPLAYNAME-$BUILD_BUILDID-$SYSTEM_JOBATTEMPT)]$SYSTEM_DEFAULTWORKINGDIRECTORY/crash-reports.zip"
     set -x
   fi
 fi
