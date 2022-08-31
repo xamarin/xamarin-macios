15671952e4fed09cbcbf72f0ede0edc4f5dea90f
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-08-31 15:41:20 +0200

    A few more tweaks.

--- tools/devops/automation/scripts/bash/collect-and-upload-crash-reports.sh
+++ tools/devops/automation/scripts/bash/collect-and-upload-crash-reports.sh
@@ -3,10 +3,10 @@
 env | sort
 
 if test -d "$HOME/Library/Logs/DiagnosticReports"; then
-  zip -9r "$SYSTEM_DEFAULTWORKINGDIRECTORY/crash-reports.zip" "$HOME/Library/Logs/DiagnosticReports"
+  zip -9rj "$SYSTEM_DEFAULTWORKINGDIRECTORY/crash-reports.zip" "$HOME/Library/Logs/DiagnosticReports"
   if test -f "$SYSTEM_DEFAULTWORKINGDIRECTORY/crash-reports.zip"; then
     set +x
-    echo "##vso[artifact.upload containerfolder=CrashReports;artifactname=crash-reports-$SYSTEM_STAGEDISPLAYNAME-$BUILD_BUILDID-$SYSTEM_JOBATTEMPT)]$SYSTEM_DEFAULTWORKINGDIRECTORY/crash-reports.zip"
+    echo "##vso[artifact.upload containerfolder=CrashReports;artifactname=crash-reports-$SYSTEM_STAGEDISPLAYNAME-$BUILD_BUILDID-$SYSTEM_JOBATTEMPT]$SYSTEM_DEFAULTWORKINGDIRECTORY/crash-reports.zip"
     set -x
   fi
 fi
