e70d077ae16dfdcdb0ec5326cb61f0f539520b3b
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-09-09 18:58:17 +0200

    [devops] Only add platforms we're building for into WorkloadRollback.json.
    Also move bash code from yml to a .sh file for easier reading, writing and
    debugging.

---
+++ tools/devops/automation/scripts/bash/generate-workload-rollback.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -eux
+
+WORKLOAD_DST="$BUILD_SOURCESDIRECTORY/WorkloadRollback.json"
+
+var=$(make -C "$BUILD_SOURCESDIRECTORY/xamarin-macios/tools/devops" print-variable VARIABLE=DOTNET_PLATFORMS)
+DOTNET_PLATFORMS=${var#*=}
+echo "Dotnet platforms are '$DOTNET_PLATFORMS'"
+
+echo "{" > "$WORKLOAD_DST"
+for platform in $DOTNET_PLATFORMS; do
+    CURRENT_UPPER=$(echo "$platform" | tr "[:lower:]" "[:upper:]")
+    CURRENT_LOWER=$(echo "$platform" | tr "[:upper:]" "[:lower:]")
+
+    var=$(make -C "$BUILD_SOURCESDIRECTORY"/xamarin-macios/tools/devops print-variable "VARIABLE=${CURRENT_UPPER}_NUGET_VERSION_FULL")
+    NUGET_VERSION_FULL=${var#*=}
+    NUGET_VERSION_FULL=$(echo "$NUGET_VERSION_FULL" | cut -d "+" -f1)
+
+    echo "\"microsoft.net.sdk.$CURRENT_LOWER\": \"$NUGET_VERSION_FULL\"," >> "$WORKLOAD_DST"
+done
+echo "}" >>  "$WORKLOAD_DST"
+
+echo "Rollback file contents:"
+cat "$WORKLOAD_DST"
