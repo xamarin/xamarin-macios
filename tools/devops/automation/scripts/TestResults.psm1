5902c865b7ea5c33aa70d354a42bd01069878814
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-09-22 07:34:24 +0200

    [devops] Add the job attempt to the html report as well. (#15908)
    This makes sure subsequent attempts also uploads the html report instead of
    failing because there's already an html report with the same name.

--- tools/devops/automation/scripts/TestResults.psm1
+++ tools/devops/automation/scripts/TestResults.psm1
@@ -252,8 +252,8 @@ class ParallelTestsResults {
     }
 
     [string] GetDownloadLinks($testResult) {
-        $dropsIndex = "$($this.VSDropsIndex)/$($this.TestPrefix)$($testResult.Label)/;/tests/vsdrops_index.html"
-        $artifactUrl = "$Env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI$Env:SYSTEM_TEAMPROJECT/_apis/build/builds/$Env:BUILD_BUILDID/artifacts?artifactName=HtmlReport-$($this.TestPrefix)$($testResult.Label)&api-version=6.0&`$format=zip"
+        $dropsIndex = "$($this.VSDropsIndex)/$($this.TestPrefix)$($testResult.Label)-$($testResult.Attempt)/;/tests/vsdrops_index.html"
+        $artifactUrl = "$Env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI$Env:SYSTEM_TEAMPROJECT/_apis/build/builds/$Env:BUILD_BUILDID/artifacts?artifactName=HtmlReport-$($this.TestPrefix)$($testResult.Label)-$($testResult.Attempt)&api-version=6.0&`$format=zip"
         $downloadInfo = "[Html Report (VSDrops)]($dropsIndex) [Download]($artifactUrl)"
         return $downloadInfo
     }
