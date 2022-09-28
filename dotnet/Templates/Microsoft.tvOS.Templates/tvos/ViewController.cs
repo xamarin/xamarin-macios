547df5117293d1100307d4b28bea185e9aa005a2
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-09-28 15:48:28 +0200

    [dotnet] Add numerous item templates. Fixes #15836.
    Add the following item templates for all platforms:
    
    * Controller (View Controller with UI written in code)
    * View
    * View Controller (View Controller with UI written in XIB)
    * Storyboard
    
    Item templates won't show up in VSMac until this is released: https://github.com/xamarin/vsmac/pull/9133.
    
    Fixes https://github.com/xamarin/xamarin-macios/issues/15836.

--- dotnet/Templates/Microsoft.tvOS.Templates/tvos/ViewController.cs
+++ dotnet/Templates/Microsoft.tvOS.Templates/tvos/ViewController.cs
@@ -5,5 +5,7 @@ namespace tvOSApp1;
 public partial class ViewController : UIViewController {
 	protected ViewController (NativeHandle handle) : base (handle)
 	{
+		// This constructor is required if the view controller is loaded from a xib or a storyboard.
+		// Do not put any initialization here, use ViewDidLoad instead.
 	}
 }
