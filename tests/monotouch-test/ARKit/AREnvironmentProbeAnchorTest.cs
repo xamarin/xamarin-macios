35f2188ab27684e19ea860434c8562047b44858b
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-10-06 13:10:46 +0200

    [tests] Re-enable tests that were disabled in Xcode 12 beta 1. Fixes #9531.
    Looks like these tests are working in the simulator now.
    
    Fixes https://github.com/xamarin/xamarin-macios/issues/9531.

--- tests/monotouch-test/ARKit/AREnvironmentProbeAnchorTest.cs
+++ tests/monotouch-test/ARKit/AREnvironmentProbeAnchorTest.cs
@@ -43,9 +43,7 @@ namespace MonoTouchFixtures.ARKit {
 		{
 			var probeAnchor = new AREnvironmentProbeAnchor (MatrixFloat4x4.Identity, new VectorFloat3 (1, 1, 1));
 			Assert.AreEqual (MatrixFloat4x4.Identity, probeAnchor.Transform, "Transform");
-			// broken since xcode 12 beta 1 on simulator (only)
-			if ((Runtime.Arch == Arch.DEVICE) || !TestRuntime.CheckXcodeVersion (12, 0))
-				Assert.AreEqual (new VectorFloat3 (1, 1, 1), probeAnchor.Extent, "Extent");
+			Assert.AreEqual (new VectorFloat3 (1, 1, 1), probeAnchor.Extent, "Extent");
 		}
 
 		[Test]
@@ -53,9 +51,7 @@ namespace MonoTouchFixtures.ARKit {
 		{
 			var probeAnchorWithName = new AREnvironmentProbeAnchor ("My Anchor", MatrixFloat4x4.Identity, new VectorFloat3 (1, 1, 1));
 			Assert.AreEqual (MatrixFloat4x4.Identity, probeAnchorWithName.Transform, "Transform");
-			// broken since xcode 12 beta 1 on simulator (only)
-			if ((Runtime.Arch == Arch.DEVICE) || !TestRuntime.CheckXcodeVersion (12, 0))
-				Assert.AreEqual (new VectorFloat3 (1, 1, 1), probeAnchorWithName.Extent, "Extent");
+			Assert.AreEqual (new VectorFloat3 (1, 1, 1), probeAnchorWithName.Extent, "Extent");
 		}
 	}
 }
