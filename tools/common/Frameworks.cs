11b1d1e1c79eed6e6443a6245f36c1dad0a48b2f
Israel Soto <issoto@microsoft.com>
2022-10-10 09:51:50 -0500

    [ThreadNetwork] Added support for Xcode 14.1 b3 (#16196)
    This PR adds support for Mac and MacCatalyst. Apple says that these
    platforms are now supported; framework's headers does not specify them
    but do not deny them either, so let's see what intro says about this.
    
    I'm just creating this PR to test it against intro once we can build the
    branch.
    
    Co-authored-by: Manuel de la Pena <mandel@microsoft.com>

--- tools/common/Frameworks.cs
+++ tools/common/Frameworks.cs
@@ -279,6 +279,7 @@ public class Frameworks : Dictionary<string, Framework> {
 					{ "HealthKit", "HealthKit", 13,0 },
 					{ "SharedWithYouCore", "SharedWithYouCore", 13, 0 },
 					{ "ExtensionKit", "ExtensionKit", 13,0 },
+					{ "ThreadNetwork", "ThreadNetwork", 13,0 },
 				};
 			}
 			return mac_frameworks;
@@ -645,6 +646,7 @@ public class Frameworks : Dictionary<string, Framework> {
 			var min = new Version (13, 0);
 			var v14_0 = new Version (14, 0);
 			var v14_2 = new Version (14, 2);
+			var v16_1 = new Version (16, 1);
 			foreach (var f in catalyst_frameworks.Values) {
 				switch (f.Name) {
 				// These frameworks were added to Catalyst after they were added to iOS, so we have to adjust the Versions fields
@@ -659,6 +661,9 @@ public class Frameworks : Dictionary<string, Framework> {
 				case "UserNotificationsUI":
 					f.Version = v14_2;
 					f.VersionAvailableInSimulator = v14_2;
+					break;
+				case "ThreadNetwork":
+					f.Version = v16_1;
 					break;
 				// These frameworks are not available on Mac Catalyst
 				case "OpenGLES":
