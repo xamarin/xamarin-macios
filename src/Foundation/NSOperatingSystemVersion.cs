44c6b2324b5c3a274ae81c01fa06a696c35709d6
Haritha Mohan <harithamohan@microsoft.com>
2022-10-14 09:53:15 -0700

    Changes spaces to tabs for indentation

--- src/Foundation/NSOperatingSystemVersion.cs
+++ src/Foundation/NSOperatingSystemVersion.cs
@@ -28,66 +28,66 @@ using System.Runtime.Versioning;
 using ObjCRuntime;
 #nullable enable
 
-namespace Foundation  {
+namespace Foundation {
 
 
 #if NET
-	[SupportedOSPlatform ("ios")]
-	[SupportedOSPlatform ("maccatalyst")]
-	[SupportedOSPlatform ("macos")]
-	[SupportedOSPlatform ("tvos")]
+[SupportedOSPlatform ("ios")]
+[SupportedOSPlatform ("maccatalyst")]
+[SupportedOSPlatform ("macos")]
+[SupportedOSPlatform ("tvos")]
 #endif
 
-	public struct NSOperatingSystemVersion : IEquatable<NSOperatingSystemVersion>, IComparable 
-    {
-		public nint Major, Minor, PatchVersion;
+public struct NSOperatingSystemVersion : IEquatable<NSOperatingSystemVersion>, IComparable 
+{
+	public nint Major, Minor, PatchVersion;
+	
+	public NSOperatingSystemVersion (nint major, nint minor, nint patchVersion)
+	{
+		Major = major;
+		Minor = minor;
+		PatchVersion = patchVersion;
+		}
 		
-        public NSOperatingSystemVersion (nint major, nint minor, nint patchVersion)
+		public NSOperatingSystemVersion (nint major, nint minor) : this (major, minor, 0) {}
+		
+		public NSOperatingSystemVersion (nint major) : this (major, 0, 0) {}
+		
+		public int CompareTo (NSOperatingSystemVersion otherVersion) 
 		{
-			Major = major;
-			Minor = minor;
-			PatchVersion = patchVersion;
+			var majorValue = Major.CompareTo (otherVersion.Major);
+			if (majorValue == 0) 
+			{
+				var minorValue = Minor.CompareTo (otherVersion.Minor);
+				return (minorValue == 0)? PatchVersion.CompareTo (otherVersion.PatchVersion) : minorValue;
+			}
+			return majorValue;
 		}
 
-        public NSOperatingSystemVersion (nint major, nint minor) : this (major, minor, 0) {}
-        
-        public NSOperatingSystemVersion (nint major) : this (major, 0, 0) {}
-
-        public int CompareTo (NSOperatingSystemVersion otherVersion) 
-        {
-            var majorValue = Major.CompareTo (otherVersion.Major);
-            if (majorValue == 0) 
-            {
-                var minorValue = Minor.CompareTo (otherVersion.Minor);
-                return (minorValue == 0)? PatchVersion.CompareTo (otherVersion.PatchVersion) : minorValue;
-            }
-            return majorValue;
-        }
-
-        public int CompareTo (Object? obj) 
-            => (obj is NSOperatingSystemVersion other) ? CompareTo (other) : 1;
+		public int CompareTo (Object? obj) 
+			=> (obj is NSOperatingSystemVersion other) ? CompareTo (other) : 1;
 
 		public override string ToString ()
 			=> $"{Major}.{Minor}.{PatchVersion}";
 
 		public bool Equals (NSOperatingSystemVersion other) 
-            => Major == other.Major && Minor == other.Minor && PatchVersion == other.PatchVersion;
+			=> Major == other.Major && Minor == other.Minor && PatchVersion == other.PatchVersion;
 
 		public override bool Equals (Object? obj)
-        {
-            if (obj is NSOperatingSystemVersion versionObj)
-                return Equals(versionObj);   
-            return false; 
-        }
+		{
+			if (obj is NSOperatingSystemVersion versionObj)
+				return Equals(versionObj);   
+			return false; 
+		}
 
-        public override int GetHashCode()
-            => Tuple.Create(Major, Minor, PatchVersion).GetHashCode();
+		public override int GetHashCode()
+			=> Tuple.Create(Major, Minor, PatchVersion).GetHashCode();
 
-        public static bool operator == (NSOperatingSystemVersion os1, NSOperatingSystemVersion os2)
-            => os1.Equals(os2);
-    
-        public static bool operator != (NSOperatingSystemVersion os1, NSOperatingSystemVersion os2)
-            => !(os1 == os2);
+		public static bool operator == (NSOperatingSystemVersion os1, NSOperatingSystemVersion os2)
+			=> os1.Equals(os2);
+
+		public static bool operator != (NSOperatingSystemVersion os1, NSOperatingSystemVersion os2)
+			=> !(os1 == os2);
 
 	}
 }
