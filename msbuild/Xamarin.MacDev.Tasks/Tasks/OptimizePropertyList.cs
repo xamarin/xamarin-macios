dcdaf2497d263541969b550665c66bb2ea7cbdb6
Mauro Agnoletti <mauro.agnoletti@gmail.com>
2022-10-24 15:58:54 -0400

    Adapted Tasks to Xamarin.Messaging API changes

--- msbuild/Xamarin.MacDev.Tasks/Tasks/OptimizePropertyList.cs
+++ msbuild/Xamarin.MacDev.Tasks/Tasks/OptimizePropertyList.cs
@@ -24,7 +24,7 @@ namespace Xamarin.MacDev.Tasks {
 			base.Cancel ();
 
 			if (!string.IsNullOrEmpty (SessionId))
-				BuildConnection.CancelAsync (SessionId, BuildEngine4).Wait ();
+				BuildConnection.CancelAsync (BuildEngine4).Wait ();
 		}
 	}
 }
