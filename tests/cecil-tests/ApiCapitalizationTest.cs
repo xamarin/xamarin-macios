a9c9f6a3562c0b8255812d94bed11eadaae0871a
Haritha Mohan <harithamohan@microsoft.com>
2022-10-26 09:37:10 -0700

    update tests to reflect exceptions

--- tests/cecil-tests/ApiCapitalizationTest.cs
+++ tests/cecil-tests/ApiCapitalizationTest.cs
@@ -21,8 +21,41 @@ namespace Cecil.Tests {
 		{
 			if (method == null)
 				return false;
+			
+			//method.Name == "get_path_radio_type" || method.Name== "dlclose"
+			//if (method.Name == "eUbiquitousContainerIdentifierKey") {
+			//	var r = from f in method.CustomAttributes
+			//			select f.AttributeType.Name;
+			//	Console.WriteLine ("attrs: ");
+			//	foreach(var i in r) {
+			//		Console.WriteLine (i);
+			//	}
+			//}
 
 			if (method.HasCustomAttributes && method.CustomAttributes.Where ((m) => m.AttributeType.Name == "ObsoleteAttribute").Any ()) {
+				return true;
+			}
+
+
+			return false;
+		}
+
+		bool IsTypeObsolete (TypeDefinition type)
+		{
+			if (type == null)
+				return false;
+
+			//method.Name == "get_path_radio_type" || method.Name== "dlclose"
+			if (type.Name == "UTType") {
+				var r = from f in type.CustomAttributes
+						select f.AttributeType.Name;
+				Console.WriteLine ("attrs: ");
+				foreach(var i in r) {
+					Console.WriteLine (i);
+				}
+			}
+
+			if (type.HasCustomAttributes && type.CustomAttributes.Where ((m) => m.AttributeType.Name == "ObsoleteAttribute" || m.AttributeType.Name == "DepracatedAttribute").Any ()) {
 				return true;
 			}
 
@@ -35,6 +68,15 @@ namespace Cecil.Tests {
 			if (property == null)
 				return false;
 
+			//if (property.Name == "eUbiquitousContainerIdentifierKey" || property.Name == "k3dObject") {
+			//	var r = from f in property.CustomAttributes
+			//			select f.AttributeType.Name;
+			//	Console.WriteLine ("attrs: ");
+			//	foreach (var i in r) {
+			//		Console.WriteLine (i);
+			//	}
+			//}
+
 			if (property.HasCustomAttributes && property.CustomAttributes.Where ((p) => p.AttributeType.Name == "ObsoleteAttribute").Any ()) {
 				return true;
 			}
@@ -44,11 +86,13 @@ namespace Cecil.Tests {
 
 
 		bool IsException (MethodDefinition m)
-		{
+		{ 
+
 			if (m == null)
 				return false;
 
-			if (m.IsRemoveOn || m.IsAddOn || m.IsConstructor || m.IsSpecialName || IsMethodObsolete (m))
+
+			if (m.IsRemoveOn || m.IsAddOn || m.IsConstructor || m.IsSpecialName || IsMethodObsolete (m) || m.IsFamilyOrAssembly || m.IsPInvokeImpl)
 				return true;
 
 			return false;
@@ -58,6 +102,7 @@ namespace Cecil.Tests {
 		Dictionary<string, HashSet<string>> allowedProperties = new Dictionary<string, HashSet<string>> () {
 			["MPMusicPlayerController"] = new HashSet<string> () { "iPodMusicPlayer" },
 			["ABPersonPhoneLabel"] = new HashSet<string> () { "iPhone" },
+			["CNLabelPhoneNumberKey"] = new HashSet<string> () { "iPhone"},
 			["AVMetadata"] = new HashSet<string> () { "iTunesMetadataKeyAccountKind",
 			"iTunesMetadataKeyAcknowledgement",
 			"iTunesMetadataKeyAlbum",
@@ -109,48 +154,10 @@ namespace Cecil.Tests {
 		};
 
 		Dictionary<string, HashSet<string>> allowedFields = new Dictionary<string, HashSet<string>> () {
-			["MPMusicPlayerController"] = new HashSet<string> () { "iPodMusicPlayer" },
-			["ABPersonPhoneLabel"] = new HashSet<string> () { "iPhone" },
-			["CMRotationMatrix"] = new HashSet<string> () { "m11", "m12", "m13", "m21", "m22", "m23", "m31", "m32", "m33" },
-			["CTRunDelegateCallbacks"] = new HashSet<string> () { "version", "dealloc", "getAscent", "getDescent", "getWidth" },
-			["CTParagraphStyleSettingValue"] = new HashSet<string> () { "int8", "single", "native_uint", "pointer" },
-			["CTParagraphStyleSetting"] = new HashSet<string> () { "spec", "valueSize", "value" },
-			["CMQuaternion"] = new HashSet<string> () { "x", "y", "z", "w" },
-			["CMRotationRate"] = new HashSet<string> () { "x", "y", "z" },
-			["CIFormat"] = new HashSet<string> () { "kRGBAf", "kBGRA8", "kRGBA8" },
-			["MusicSequenceFileTypeID"] = new HashSet<string> () { "iMelody" },
-			["UIImageStatusDispatcher"] = new HashSet<string> () { "callbackSelector" },
-			["UIVideoStatusDispatcher"] = new HashSet<string> () { "callbackSelector" },
-			["vImageAffineTransformFloat"] = new HashSet<string> () { "a", "b", "c", "d", "tx", "ty" },
-			["vImageAffineTransformDouble"] = new HashSet<string> () { "a", "b", "c", "d", "tx", "ty" },
-			["vImageGamma"] = new HashSet<string> () { "kUseGammaValue", "kUseGammaValueHalfPrecision",
-				"k5over9_", "k9over5_", "k5over11_", "k11over5_",
-				"ksRGB_", "ksRGB_", "k11over9_", "k9over11_",
-				"kBT709_", "kBT709_" },
-			["vImageMDTableUsageHint"] = new HashSet<string> () { "k16Q12", "kFloat" },
-			["BlockDescriptor"] = new HashSet<string> () { "reserved", "size", "copy_helper", "dispose", "signature" },
-			["XamarinBlockDescriptor"] = new HashSet<string> () { "descriptor", "ref_count" },
-			["Constants"] = new HashSet<string> () { "libSystemLibrary", "libcLibrary", "libdispatchLibrary", "libcompressionLibrary" },
-			["LinkTarget"] = new HashSet<string> () { "i386", "x86_64" },
-			["MTLFeatureSet"] = new HashSet<string> () { "iOS_", "macOS_", "tvOS_" },
-			["MTLGpuFamily"] = new HashSet<string> () { "iOSMac1", "iOSMac2" },
-			["MTLLanguageVersion"] = new HashSet<string> () { "v1_0", "v1_1", "v1_2", "v2_0", "v2_1", "v2_2", "v2_3", "v2_4" },
-			["MTLTextureType"] = new HashSet<string> () { "k1D", "k1DArray", "k2D", "k2DArray", "k2DMultisample", "kCube",
-				"kCubeArray", "k3D", "k2DMultisampleArray", "kTextureBuffer" },
-			["INCarAudioSource"] = new HashSet<string> () { "iPod" },
-			["INPersonHandleLabel"] = new HashSet<string> () { "iPhone" },
-			["NSDecimal"] = new HashSet<string> () { "fields", "m1", "m2", "m3", "m4", "m5", "m6", "m7", "m8" },
-			["CGVector"] = new HashSet<string> () { "dx", "dy" },
-			["AudioFormatType"] = new HashSet<string> () { "iLBC" },
-			["NSTextScalingType"] = new HashSet<string> () { "iOS" },
-			["ITLibDistinguishedPlaylistKind"] = new HashSet<string> () { "iTunesU" },
-			["ITLibMediaItemMediaKind"] = new HashSet<string> () { "iOSApplication", "iTunesU" },
-			["PHAssetSourceType"] = new HashSet<string> () { "iTunesSynced" }
 		};
 
 
 		Dictionary<string, HashSet<string>> allowedMethods = new Dictionary<string, HashSet<string>> () {
-
 		};
 
 		public bool Skip (string type, string m, Dictionary<string, HashSet<string>> allowed)
@@ -173,7 +180,7 @@ namespace Cecil.Tests {
 				var typeName = type.Name;
 				var c = type.Properties
 						.Where (p => p.GetMethod?.IsPublic == true || p.SetMethod?.IsPublic == true)
-						.Where (p => !Skip (type.Name, p.Name, allowedProperties) && !IsPropertyObsolete (p))
+						.Where (p => !Skip (type.Name, p.Name, allowedProperties) && !IsPropertyObsolete (p) && !p.IsSpecialName)
 						.Select (p => p.Name);
 				return c;
 			};
@@ -188,7 +195,7 @@ namespace Cecil.Tests {
 			Func<TypeDefinition, IEnumerable<string>> selectLambda = (type) => {
 				var typeName = type.Name;
 				var c = from m in type.Methods
-						where !IsException (m) && m.IsPublic && !Skip (type.Name, m.Name, allowedMethods)
+						where m.IsPublic && !IsException (m) && !Skip (type.Name, m.Name, allowedMethods)
 						select m.Name;
 				return c;
 			};
@@ -220,7 +227,7 @@ namespace Cecil.Tests {
 				var typeName = type.Name;
 				var c = from f in type.Fields
 						where f.IsPublic && !Skip (type.Name, f.Name, allowedFields)
-						where f.Name != "value__"
+						where f.IsFamilyOrAssembly
 						select f.Name;
 				return c;
 			};
@@ -237,7 +244,7 @@ namespace Cecil.Tests {
 
 			var typeDict = new Dictionary<string, string> ();
 
-			var publicTypes = assembly.MainModule.Types.Where ((t) => t.IsPublic);
+			var publicTypes = assembly.MainModule.Types.Where ((t) => t.IsPublic && !IsTypeObsolete(t));
 
 			foreach (var type in publicTypes) {
 				var err = selectLambda (type);
