6b7c08d21f8e2ff82dec32cd74c8d00c9223417e
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-11-03 22:24:50 +0100

    [devops] Move logic to store and load the build configuration to disk into shareable PowerShell functions. (#16552)
    * This makes it much easier to maintain the code, since it's possible to run it locally.
    * Add tests!
    * Add a DOTNET_PLATFORMS variable to the build configuration to list all the
      enabled .NET platforms.
    * Add INCLUDE_DOTNET_<platform> variables to the build configuration for each
      enabled .NET platform. These new variables will be used in a future pull request.

--- tools/devops/automation/scripts/VSTS.Tests.ps1
+++ tools/devops/automation/scripts/VSTS.Tests.ps1
@@ -179,3 +179,123 @@ Describe 'Set-PipelineResult' {
         }
     }
 }
+
+Describe 'New-BuildConfiguration' {
+    Context 'with all the env vars present' {
+        BeforeAll {
+            $Script:envVariables = @{
+                "ACCESSTOKEN" = "ACCESSTOKEN"
+                "BUILD_BUILDID" = "BUILD_BUILDID";
+                "BUILD_REASON" = "BUILD_REASON"
+                "BUILD_SOURCEBRANCH" = "BUILD_SOURCEBRANCH"
+                "BUILD_SOURCEBRANCHNAME" = "BUILD_SOURCEBRANCHNAME"
+                "BUILD_SOURCEVERSION" = "BUILD_SOURCEVERSION"
+                "CONFIGURE_PLATFORMS_DOTNET_PLATFORMS" = "iOS tvOS"
+                "CONFIGURE_PLATFORMS_INCLUDE_DOTNET_TVOS" = "true"
+                "SYSTEM_TEAMFOUNDATIONCOLLECTIONURI" = "SYSTEM_TEAMFOUNDATIONCOLLECTIONURI";
+                "SYSTEM_TEAMPROJECT" = "SYSTEM_TEAMPROJECT";
+            }
+
+            $envVariables.GetEnumerator() | ForEach-Object {
+                $key = $_.Key
+                Set-Item -Path "Env:$key" -Value $_.Value
+            }
+        }
+
+        It 'gets the build build configuration' {
+            $buildConfiguration = New-BuildConfiguration -AddTags $false
+
+            $buildConfiguration | ConvertTo-Json | Should -Be "{
+  ""BuildReason"": ""BUILD_REASON"",
+  ""BuildSourceBranchName"": ""BUILD_SOURCEBRANCHNAME"",
+  ""BuildSourceBranch"": ""BUILD_SOURCEBRANCH"",
+  ""BuildId"": ""BUILD_BUILDID"",
+  ""DOTNET_PLATFORMS"": ""iOS tvOS"",
+  ""INCLUDE_DOTNET_IOS"": null,
+  ""INCLUDE_DOTNET_TVOS"": ""true"",
+  ""Commit"": ""BUILD_SOURCEVERSION"",
+  ""Tags"": [
+    ""ciBuild"",
+    ""BUILD_SOURCEBRANCHNAME""
+  ]
+}"
+        }
+
+        It 'writes the file' {
+            $testDirectory = Join-Path "." "subdir"
+            $configFile = Join-Path $testDirectory "buildconfiguration.json"
+            New-Item -Path "$testDirectory" -ItemType "directory" -Force
+            New-BuildConfiguration -AddTags $false -ConfigFile $configFile
+
+            $buildConfiguration = Get-Content -Path $configFile -Raw
+            Remove-Item -Path $testDirectory -Recurse
+
+            # Write-Host $buildConfiguration
+            $buildConfiguration | Should -Be "{
+  ""BuildReason"": ""BUILD_REASON"",
+  ""BuildSourceBranchName"": ""BUILD_SOURCEBRANCHNAME"",
+  ""BuildSourceBranch"": ""BUILD_SOURCEBRANCH"",
+  ""BuildId"": ""BUILD_BUILDID"",
+  ""DOTNET_PLATFORMS"": ""iOS tvOS"",
+  ""INCLUDE_DOTNET_IOS"": null,
+  ""INCLUDE_DOTNET_TVOS"": ""true"",
+  ""Commit"": ""BUILD_SOURCEVERSION"",
+  ""Tags"": [
+    ""ciBuild"",
+    ""BUILD_SOURCEBRANCHNAME""
+  ]
+}
+"
+        }
+    }
+}
+
+Describe 'Import-BuildConfiguration' {
+    Context 'import' {
+        It 'gets the right values' {
+            $config = "{
+  ""BuildReason"": ""BUILD_REASON"",
+  ""BuildSourceBranchName"": ""BUILD_SOURCEBRANCHNAME"",
+  ""BuildSourceBranch"": ""BUILD_SOURCEBRANCH"",
+  ""BuildId"": ""BUILD_BUILDID"",
+  ""DOTNET_PLATFORMS"": ""iOS tvOS"",
+  ""INCLUDE_DOTNET_IOS"": null,
+  ""INCLUDE_DOTNET_TVOS"": ""true"",
+  ""Commit"": ""BUILD_SOURCEVERSION"",
+  ""Tags"": [
+    ""ciBuild"",
+    ""BUILD_SOURCEBRANCHNAME""
+  ]
+}"
+
+            $testDirectory = Join-Path "." "subdir"
+            $configFile = Join-Path $testDirectory "buildconfiguration.json"
+            New-Item -Path "$testDirectory" -ItemType "directory" -Force
+            Set-Content -Path $configFile -Value $config
+
+            $buildConfiguration = Import-BuildConfiguration -ConfigFile $configFile
+
+            Remove-Item -Path $testDirectory -Recurse
+        }
+
+        It 'null' {
+            $config = "null"
+
+            $testDirectory = Join-Path "." "subdir"
+            $configFile = Join-Path $testDirectory "buildconfiguration.json"
+            New-Item -Path "$testDirectory" -ItemType "directory" -Force
+            Set-Content -Path $configFile -Value $config
+
+            { Import-BuildConfiguration -ConfigFile $configFile } | Should -Throw
+
+            Remove-Item -Path $testDirectory -Recurse
+        }
+
+        It 'inexistent path' {
+            $testDirectory = Join-Path "." "subdir"
+            $configFile = Join-Path $testDirectory "buildconfiguration.json"
+
+            { Import-BuildConfiguration -ConfigFile $configFile } | Should -Throw
+        }
+    }
+}
