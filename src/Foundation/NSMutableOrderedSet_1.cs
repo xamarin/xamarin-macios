c4c0d35a7f22d51b8bebca9a2c3b877be80da171
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-11-11 08:21:20 +0100

    [autoformat] Add the CoreWlan, EventKit, EventKitUI, ExternalAccessory, FileProvider, FinderSync and Foundation frameworks. (#16714)
    Co-authored-by: Manuel de la Pena <mandel@microsoft.com>

--- src/Foundation/NSMutableOrderedSet_1.cs
+++ src/Foundation/NSMutableOrderedSet_1.cs
@@ -68,7 +68,7 @@ namespace Foundation {
 		public new TKey this [nint idx] {
 			get {
 				var ret = _GetObject (idx);
-				return Runtime.GetINativeObject <TKey> (ret, false);
+				return Runtime.GetINativeObject<TKey> (ret, false);
 			}
 
 			set {
@@ -82,7 +82,7 @@ namespace Foundation {
 		public NSSet<TKey> AsSet ()
 		{
 			var ret = _AsSet ();
-			return Runtime.GetINativeObject <NSSet<TKey>> (ret, false);
+			return Runtime.GetINativeObject<NSSet<TKey>> (ret, false);
 		}
 
 		public void Insert (TKey obj, nint atIndex)
@@ -145,7 +145,7 @@ namespace Foundation {
 			_RemoveObject (obj.Handle);
 		}
 
-		public void RemoveObjects (params TKey[] objects)
+		public void RemoveObjects (params TKey [] objects)
 		{
 			if (objects == null)
 				throw new ArgumentNullException (nameof (objects));
