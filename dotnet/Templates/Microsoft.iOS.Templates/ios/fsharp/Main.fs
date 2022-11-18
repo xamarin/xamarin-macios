fd3b7a741bf76ac2a9e7d8dd132b6c5b78b6ac40
Timothé LARIVIERE <timothe.lariviere@outlook.com>
2022-11-18 10:44:21 +0100

    Add an additional F# template for Microsoft.iOS.iOSApp

---
+++ dotnet/Templates/Microsoft.iOS.Templates/ios/fsharp/Main.fs
@@ -0,0 +1,12 @@
+﻿namespace iOSApp1
+
+open UIKit
+
+module Main =
+    // This is the main entry point of the application.
+    // If you want to use a different Application Delegate class from "AppDelegate"
+    // you can specify it here.
+    [<EntryPoint>]
+    let main args = 
+        UIApplication.Main(args, null, typeof<AppDelegate>)
+        0
