24e285ae2ed540cfec7f4aed7cbea0919fa01648
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-11-21 20:55:14 +0100

    [autoformat] Add the rest of the subdirectories in src/.

--- src/System.Drawing/PointSizeRectangleTypeForwarders.cs
+++ src/System.Drawing/PointSizeRectangleTypeForwarders.cs
@@ -1,8 +1,8 @@
 #if !NET
-[assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Drawing.Point))]
-[assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Drawing.PointF))]
-[assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Drawing.Rectangle))]
-[assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Drawing.RectangleF))]
-[assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Drawing.Size))]
-[assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Drawing.SizeF))]
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo (typeof (System.Drawing.Point))]
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo (typeof (System.Drawing.PointF))]
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo (typeof (System.Drawing.Rectangle))]
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo (typeof (System.Drawing.RectangleF))]
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo (typeof (System.Drawing.Size))]
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo (typeof (System.Drawing.SizeF))]
 #endif
