ae3e305a0bef0b70c6e1700258b96441115e1fd4
Stephen Hawley <sthawl@microsoft.com>
2022-12-13 15:13:53 -0500

    make it nullable, add some doc'n

--- src/ObjCRuntime/NativeString.cs
+++ src/ObjCRuntime/NativeString.cs
@@ -1,11 +1,21 @@
 using System;
 using System.Runtime.InteropServices;
 
+#nullable enable
+
 namespace ObjCRuntime {
+	// a short-lived holder for a C-like string for native interop
+	// typical usage:
+	// using var cstring = new NativeString (str);
+	// SomePInvoke (cstring);
+	//
+	// If SomePInvoke doesn't make a copy, this is not the right tool
+	// for you.
 	internal struct NativeString : IDisposable {
 		IntPtr ptr;
-		public NativeString (string str)
+		public NativeString (string? str)
 		{
+			// the docs say when str is null the IntPtr will be 0
 			ptr = Marshal.StringToHGlobalAuto (str);
 		}
 
