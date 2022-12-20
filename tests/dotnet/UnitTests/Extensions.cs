96835917ea74fdba71072c4bc3618027b11763d0
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-12-20 18:40:55 +0100

    [dotnet/msbuild] Only compile entitlements once for universal builds. Fixes #15632.
    Fixes https://github.com/xamarin/xamarin-macios/issues/15632.

---
+++ tests/dotnet/UnitTests/Extensions.cs
@@ -0,0 +1,17 @@
+using System;
+
+#nullable enable
+
+namespace Xamarin.Tests {
+	public static class Extensions
+	{
+		public static void AssertNoWarnings (this ExecutionResult result)
+		{
+			var warnings = BinLog.GetBuildLogWarnings (result.BinLogPath).ToArray ();
+			if (!warnings.Any ())
+				return;
+
+			Assert.Fail ($"No warnings expected, but got:\n\t{string.Join ("\n\t", warnings.Select (v => v.ToString ()))}");
+		}
+	}
+}
