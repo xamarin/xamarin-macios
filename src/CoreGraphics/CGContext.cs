1162fd0d93fbed541454cec211e7adaab366053f
Stephen Hawley <sthawl@microsoft.com>
2023-01-03 15:07:39 -0500

    CoreGraphics convert strings in pinvokes

--- src/CoreGraphics/CGContext.cs
+++ src/CoreGraphics/CGContext.cs
@@ -860,7 +860,7 @@ namespace CoreGraphics {
 #endif
 		[DllImport (Constants.CoreGraphicsLibrary)]
 		extern static void CGContextSelectFont (/* CGContextRef */ IntPtr c,
-			/* const char* __nullable */ string? name, /* CGFloat */ nfloat size, CGTextEncoding textEncoding);
+			/* const char* __nullable */ IntPtr name, /* CGFloat */ nfloat size, CGTextEncoding textEncoding);
 
 #if NET
 		[SupportedOSPlatform ("ios")]
@@ -875,7 +875,8 @@ namespace CoreGraphics {
 #endif
 		public void SelectFont (string? name, nfloat size, CGTextEncoding textEncoding)
 		{
-			CGContextSelectFont (Handle, name, size, textEncoding);
+			using var namePtr = new TransientString (name);
+			CGContextSelectFont (Handle, namePtr, size, textEncoding);
 		}
 
 		[DllImport (Constants.CoreGraphicsLibrary)]
@@ -904,7 +905,7 @@ namespace CoreGraphics {
 		[Deprecated (PlatformName.MacOSX, 10, 9)]
 #endif
 		[DllImport (Constants.CoreGraphicsLibrary)]
-		extern static void CGContextShowText (/* CGContextRef */ IntPtr c, /* const char* __nullable */ string? s, /* size_t */ nint length);
+		extern static void CGContextShowText (/* CGContextRef */ IntPtr c, /* const char* __nullable */ IntPtr s, /* size_t */ nint length);
 
 #if NET
 		[SupportedOSPlatform ("ios")]
@@ -923,7 +924,8 @@ namespace CoreGraphics {
 				count = 0;
 			else if (count > str.Length)
 				throw new ArgumentException (nameof (count));
-			CGContextShowText (Handle, str, count);
+			using var strPtr = new TransientString (str);
+			CGContextShowText (Handle, strPtr, count);
 		}
 
 #if NET
@@ -939,7 +941,8 @@ namespace CoreGraphics {
 #endif
 		public void ShowText (string? str)
 		{
-			CGContextShowText (Handle, str, str is null ? 0 : str.Length);
+			using var strPtr = new TransientString (str);
+			CGContextShowText (Handle, strPtr, str is null ? 0 : str.Length);
 		}
 
 #if NET
@@ -1005,7 +1008,7 @@ namespace CoreGraphics {
 #endif
 		[DllImport (Constants.CoreGraphicsLibrary)]
 		extern static void CGContextShowTextAtPoint (/* CGContextRef __nullable */ IntPtr c, /* CGFloat */ nfloat x,
-			/* CGFloat */ nfloat y, /* const char* __nullable */ string? str, /* size_t */ nint length);
+			/* CGFloat */ nfloat y, /* const char* __nullable */ IntPtr str, /* size_t */ nint length);
 
 #if NET
 		[SupportedOSPlatform ("ios")]
@@ -1020,7 +1023,8 @@ namespace CoreGraphics {
 #endif
 		public void ShowTextAtPoint (nfloat x, nfloat y, string? str, int length)
 		{
-			CGContextShowTextAtPoint (Handle, x, y, str, length);
+			using var strPtr = new TransientString (str);
+			CGContextShowTextAtPoint (Handle, x, y, strPtr, length);
 		}
 
 #if NET
@@ -1036,7 +1040,8 @@ namespace CoreGraphics {
 #endif
 		public void ShowTextAtPoint (nfloat x, nfloat y, string? str)
 		{
-			CGContextShowTextAtPoint (Handle, x, y, str, str is null ? 0 : str.Length);
+			using var strPtr = new TransientString (str);
+			CGContextShowTextAtPoint (Handle, x, y, strPtr, str is null ? 0 : str.Length);
 		}
 
 #if NET
