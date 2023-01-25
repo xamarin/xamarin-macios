bf639a99d76af3a98512e127b67b89b3a496f7ad
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-01-25 16:23:12 +0100

    [dotnet-linker] Add the scaffolding for a ManagedRegistrarStep and a ManagedRegistrarLookupTablesStep.

---
+++ tools/dotnet-linker/Steps/ManagedRegistrarStep.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+using Xamarin.Bundler;
+using Xamarin.Utils;
+
+#nullable enable
+
+namespace Xamarin.Linker {
+	public class ManagedRegistrarStep : ConfigurationAwareStep {
+		protected override string Name { get; } = "ManagedRegistrar";
+		protected override int ErrorCode { get; } = 2430;
+
+		List<Exception> exceptions = new List<Exception> ();
+
+		void AddException (Exception exception)
+		{
+			if (exceptions is null)
+				exceptions = new List<Exception> ();
+			exceptions.Add (exception);
+		}
+
+		protected override void TryEndProcess (out List<Exception>? exceptions)
+		{
+			base.TryEndProcess ();
+
+			// Report back any exceptions that occurred during the processing.
+			exceptions = this.exceptions;
+		}
+
+		protected override void TryProcessAssembly (AssemblyDefinition assembly)
+		{
+			base.TryProcessAssembly (assembly);
+		}
+	}
+}
