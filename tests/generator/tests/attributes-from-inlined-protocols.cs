672dfd00d8c286e6e7b2ddcb470586d746e7ae09
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-01-26 07:59:35 +0100

    [tests] Adjust a few generator tests now that we're no longer implying Mac Catalyst attributes.

--- tests/generator/tests/attributes-from-inlined-protocols.cs
+++ tests/generator/tests/attributes-from-inlined-protocols.cs
@@ -10,15 +10,18 @@ namespace NS {
 	}
 
 	[Introduced (PlatformName.iOS, 11, 0)]
+	[Introduced (PlatformName.MacCatalyst, 13, 1)]
 	[Introduced (PlatformName.TvOS, 11, 0)]
 	[Protocol]
 	interface ProtocolA {
 		[Introduced (PlatformName.iOS, 12, 0)]
+		[Introduced (PlatformName.MacCatalyst, 13, 1)]
 		[Introduced (PlatformName.TvOS, 12, 0)]
 		[Export ("someMethod1:")]
 		void SomeMethod1 (nint row);
 
 		[Introduced (PlatformName.iOS, 12, 0)]
+		[Introduced (PlatformName.MacCatalyst, 13, 1)]
 		[Export ("someMethod2:")]
 		void SomeMethod2 (nint row);
 
