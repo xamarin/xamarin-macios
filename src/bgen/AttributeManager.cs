17e8200b30ca93287732b9c38892660f20d2b0e9
Manuel de la Pena <mandel@microsoft.com>
2023-02-14 10:18:48 -0500

    [Generator] Move CustomAttributeDataExtensions to its own file and enable nullability. (#17526)
    Co-authored-by: GitHub Actions Autoformatter <github-actions-autoformatter@xamarin.com>

--- src/bgen/AttributeManager.cs
+++ src/bgen/AttributeManager.cs
@@ -857,26 +857,3 @@ public static class EnumerableExtensions {
 		yield return item;
 	}
 }
-
-public static class CustomAttributeDataExtensions {
-#if !NET
-	static Type roCustomAttributeDataType;
-	static PropertyInfo attributeTypeProperty;
-
-	static CustomAttributeDataExtensions ()
-	{
-		roCustomAttributeDataType = typeof (MetadataLoadContext).Assembly.GetType ("System.Reflection.TypeLoading.RoCustomAttributeData");
-		attributeTypeProperty = roCustomAttributeDataType.GetProperty ("AttributeType");
-	}
-
-	public static Type GetAttributeType (this CustomAttributeData data)
-	{
-		// Workaround for CustomAttributeData.AttributeType not being declared as virtual in Mono
-		if (data.GetType ().IsSubclassOf (roCustomAttributeDataType))
-			return (Type) attributeTypeProperty.GetValue (data);
-		return data.AttributeType;
-	}
-#else
-	public static Type GetAttributeType (this CustomAttributeData data) => data.AttributeType;
-#endif
-}
