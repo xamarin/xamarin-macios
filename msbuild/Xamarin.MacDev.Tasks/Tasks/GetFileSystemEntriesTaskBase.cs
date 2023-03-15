62e9afdb59aa517c58090cbe9c636851cdad5369
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-03-15 17:44:56 +0100

    [msbuild] Enable nullability and some additional improvements to the GetFileSystemEntries task.
    * Add support for multiple input paths.
    * Copy any metadata from input paths to the output paths.
    * Set the 'RecursiveDir' metadata on output paths, to match how globbing works in MSBuild.
    * Also set the 'OriginalItemSpec' metadata so that it's easier to compute derived paths afterwards.
    * Enable nullability.

--- msbuild/Xamarin.MacDev.Tasks/Tasks/GetFileSystemEntriesTaskBase.cs
+++ msbuild/Xamarin.MacDev.Tasks/Tasks/GetFileSystemEntriesTaskBase.cs
@@ -6,15 +6,17 @@ using System.Linq;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
+#nullable enable
+
 namespace Xamarin.MacDev.Tasks {
 	public abstract class GetFileSystemEntriesTaskBase : XamarinTask {
 		#region Inputs
 
 		[Required]
-		public string DirectoryPath { get; set; }
+		public ITaskItem[] DirectoryPath { get; set; } = Array.Empty<ITaskItem> ();
 
 		[Required]
-		public string Pattern { get; set; }
+		public string Pattern { get; set; } = string.Empty;
 
 		[Required]
 		public bool Recursive { get; set; }
@@ -26,18 +28,31 @@ namespace Xamarin.MacDev.Tasks {
 		#region Outputs
 
 		[Output]
-		public ITaskItem [] Entries { get; set; }
+		public ITaskItem [] Entries { get; set; } = Array.Empty<ITaskItem> ();
 
 		#endregion
 
 		public override bool Execute ()
 		{
 			var searchOption = Recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
-			var entriesFullPath = IncludeDirectories ?
-				Directory.GetFileSystemEntries (DirectoryPath, Pattern, searchOption) :
-				Directory.GetFiles (DirectoryPath, Pattern, searchOption);
+			var entries = new List<ITaskItem> ();
+			foreach (var item in DirectoryPath) {
+				var path = item.ItemSpec.TrimEnd ('\\', '/');
+				var entriesFullPath = IncludeDirectories ?
+					Directory.GetFileSystemEntries (path, Pattern, searchOption) :
+					Directory.GetFiles (path, Pattern, searchOption);
 
-			Entries = entriesFullPath.Select (v => new TaskItem (v)).ToArray ();
+				foreach (var entry in entriesFullPath) {
+					var recursiveDir = entry.Substring (path.Length + 1);
+					var newItem = new TaskItem (entry);
+					item.CopyMetadataTo (newItem);
+					newItem.SetMetadata ("RecursiveDir", recursiveDir);
+					newItem.SetMetadata ("OriginalItemSpec", item.ItemSpec);
+					entries.Add (newItem);
+				}
+			}
+
+			Entries = entries.ToArray ();
 
 			return !Log.HasLoggedErrors;
 		}
