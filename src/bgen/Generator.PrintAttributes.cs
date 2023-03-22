37e283bdf77a2df62c66c30937896fdf6460d805
Manuel de la Pena <mandel@microsoft.com>
2023-03-22 13:23:24 -0400

    Unify forgotten file.

--- src/bgen/Generator.PrintAttributes.cs
+++ src/bgen/Generator.PrintAttributes.cs
@@ -84,4 +84,18 @@ public partial class Generator {
 		return memberAvailability.ToArray ();
 	}
 }
+
+#else
+
+using System.Linq;
+using System.Reflection;
+using ObjCRuntime;
+
+#nullable enable
+
+public partial class Generator {
+
+	AvailabilityBaseAttribute [] GetPlatformAttributesToPrint (MemberInfo mi, MemberInfo? context, MemberInfo? inlinedType)
+		=> AttributeManager.GetCustomAttributes<AvailabilityBaseAttribute> (mi).ToArray ();
+}
 #endif
