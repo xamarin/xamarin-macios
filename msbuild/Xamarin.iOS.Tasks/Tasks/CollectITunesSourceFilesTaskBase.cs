8c9b04625566e71d1648a948a8ef431a7e57743f
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-03-23 08:34:40 +0100

    [msbuild] Merge the CollectITunesSourceFiles and CollectITunesSourceFilesTaskBase classes.
    We no longer need two have overridable logic for remote builds, so the
    non-abstract task class and the abstract base class can be merged.
    
    Also enable nullability and fix any issues.

--- msbuild/Xamarin.iOS.Tasks/Tasks/CollectITunesSourceFilesTaskBase.cs
+++ msbuild/Xamarin.iOS.Tasks/Tasks/CollectITunesSourceFilesTaskBase.cs
@@ -6,27 +6,33 @@ using Microsoft.Build.Utilities;
 using Microsoft.Build.Framework;
 
 using Xamarin.MacDev.Tasks;
+using Xamarin.Messaging.Build.Client;
+
+#nullable enable
 
 namespace Xamarin.iOS.Tasks {
-	public abstract class CollectITunesSourceFilesTaskBase : XamarinTask {
+	public class CollectITunesSourceFiles : XamarinTask, ICancelableTask {
 		static readonly string [] iTunesFileNames = { "iTunesMetadata.plist", "iTunesArtwork@2x", "iTunesArtwork" };
 
 		#region Inputs
 
 		[Required]
-		public string OutputPath { get; set; }
+		public string OutputPath { get; set; } = string.Empty;
 
 		#endregion
 
 		#region Outputs
 
 		[Output]
-		public ITaskItem [] ITunesSourceFiles { get; set; }
+		public ITaskItem [] ITunesSourceFiles { get; set; } = Array.Empty<ITaskItem> ();
 
 		#endregion
 
 		public override bool Execute ()
 		{
+			if (ShouldExecuteRemotely ())
+				return new TaskRunner (SessionId, BuildEngine4).RunAsync (this).Result;
+
 			var files = new List<ITaskItem> ();
 
 			foreach (var fileName in iTunesFileNames) {
@@ -41,6 +47,12 @@ namespace Xamarin.iOS.Tasks {
 			Log.LogTaskProperty ("ITunesSourceFiles", ITunesSourceFiles);
 
 			return !Log.HasLoggedErrors;
+		}
+
+		public void Cancel ()
+		{
+			if (ShouldExecuteRemotely ())
+				BuildConnection.CancelAsync (BuildEngine4).Wait ();
 		}
 	}
 }
