ec441bf0cdc1568d8658ffd2c2d3181ff014fa75
Connor Adsit <cadsit@microsoft.com>
2023-03-24 18:12:44 -0400

    Use https instead of git+ssh

--- mk/xamarin.mk
+++ mk/xamarin.mk
@@ -13,7 +13,7 @@ NEEDED_MACCORE_BRANCH := main
 MACCORE_DIRECTORY := maccore
 # Use https to clone maccore during pipeline builds, avoiding broken SSH connections when GH changes RSA key
 ifdef SYSTEM_ACCESSTOKEN
-MACCORE_MODULE    := git@github.com:xamarin/maccore.git
+MACCORE_MODULE    := https://github.com/xamarin/maccore.git
 else
 MACCORE_MODULE    := git@github.com:xamarin/maccore.git
 endif
