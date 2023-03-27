542f8cd07e8a7d91f6b88ea0be4a0f04b8664f02
Alex Soto <alex@alexsoto.me>
2023-03-27 17:17:52 -0400

    [HealthKit] Update bindings to Xcode 14.3 Beta 2

--- src/HealthKit/Enums.cs
+++ src/HealthKit/Enums.cs
@@ -587,6 +587,12 @@ namespace HealthKit {
 		[MacCatalyst (14, 0)]
 		[Field ("HKFHIRResourceTypeCoverage")]
 		Coverage,
+		[iOS (14, 6), MacCatalyst (14, 6), Mac (13, 3)]
+		[Field ("HKFHIRResourceTypeDiagnosticReport")]
+		DiagnosticReport,
+		[iOS (14, 6), MacCatalyst (14, 6), Mac (13, 3)]
+		[Field ("HKFHIRResourceTypeDocumentReference")]
+		DocumentReference,
 	}
 
 	[Watch (5, 0), iOS (12, 0), Mac (13, 0)]
@@ -611,6 +617,10 @@ namespace HealthKit {
 		[MacCatalyst (14, 0)]
 		[Field ("HKClinicalTypeIdentifierCoverageRecord")]
 		CoverageRecord,
+		[Watch (9, 4), iOS (16, 4), Mac (13, 3)]
+		[MacCatalyst (16, 4)]
+		[Field ("HKClinicalTypeIdentifierClinicalNoteRecord")]
+		ClinicalNoteRecord,
 	}
 
 	[Watch (5, 0), iOS (12, 0), Mac (13, 0)]
