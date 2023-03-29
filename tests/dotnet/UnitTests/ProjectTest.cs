8deec35a207fdd1918de1fdf1085a83aba50530c
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-03-29 10:25:25 +0200

    [net8.0] Remove support for .NET 6. (#17901)
    Context: https://dotnet.microsoft.com/platform/support/policy/maui
    
    > A major version of .NET MAUI receives support for a minimum of 6
    > months after a successor (the next major release) ships. For
    > example, .NET MAUI 6.0 will be supported for 6 months after .NET
    > MAUI 7.0 ships. Similarly, .NET MAUI 7.0 will receive support for 6
    > months after .NET MAUI 8.0 ships.
    
    By the time .NET 8 GA ships, .NET 6 MAUI projects will not be supported.
    
    Remove .NET 6 support from our .NET 8 workloads.

--- tests/dotnet/UnitTests/ProjectTest.cs
+++ tests/dotnet/UnitTests/ProjectTest.cs
@@ -1032,18 +1032,10 @@ namespace Xamarin.Tests {
 			Clean (project_path);
 			var properties = GetDefaultProperties (runtimeIdentifiers);
 
-			var result = DotNet.AssertBuild (project_path, properties);
-			AssertThatLinkerExecuted (result);
-			var infoPlistPath = GetInfoPListPath (platform, appPath);
-			Assert.That (infoPlistPath, Does.Exist, "Info.plist");
-			var infoPlist = PDictionary.FromFile (infoPlistPath)!;
-			Assert.AreEqual ("com.xamarin.mysimpleapp", infoPlist.GetString ("CFBundleIdentifier").Value, "CFBundleIdentifier");
-			Assert.AreEqual ("MySimpleApp", infoPlist.GetString ("CFBundleDisplayName").Value, "CFBundleDisplayName");
-			Assert.AreEqual ("6.0", infoPlist.GetString ("CFBundleVersion").Value, "CFBundleVersion");
-			Assert.AreEqual ("6.0", infoPlist.GetString ("CFBundleShortVersionString").Value, "CFBundleShortVersionString");
-
-			var appExecutable = GetNativeExecutable (platform, appPath);
-			ExecuteWithMagicWordAndAssert (platform, runtimeIdentifiers, appExecutable);
+			var result = DotNet.AssertBuildFailure (project_path, properties);
+			var errors = BinLog.GetBuildLogErrors (result.BinLogPath).ToList ();
+			Assert.AreEqual (1, errors.Count, "Error Count");
+			Assert.That (errors [0].Message, Does.Contain ("To build this project, the following workloads must be installed: "), "Error message");
 		}
 
 		[Test]
