7217a354abc24573d2a4bd6fb12f95bef9680c86
tj-devel709 <tjlambert@microsoft.com>
2023-04-19 16:03:43 -0500

    use new custom powershell script

--- tools/devops/automation/scripts/localizationBomRemoval.ps1
+++ tools/devops/automation/scripts/localizationBomRemoval.ps1
@@ -1,4 +1,3 @@
-# This function is used to remove the BOM from the checked-in Localization files
 function RemoveUtf8Bom
 {
     param(
@@ -8,4 +7,9 @@ function RemoveUtf8Bom
     $encoding = New-Object -TypeName System.Text.UTF8Encoding
     $content = Get-Content -Path $FilePath -Raw
     [System.IO.File]::WriteAllLines($FilePath, $content, $encoding)
+}
+
+foreach ($locFile in (Get-Content -Path $env:XLocFileList))
+{
+    RemoveUtf8Bom -FilePath $locFile
 }
