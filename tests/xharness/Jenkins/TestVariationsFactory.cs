066ab68bc6d256a4ece8e85c9b3590ce2d437229
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-05-04 17:09:50 +0200

    [xharness] Append to MtouchExtraArgs/MonoBundlingExtraArgs instead of just setting the value. (#18200)
    When generating/cloning test projects and modifying
    MtouchExtraArgs/MonoBundlingExtraArgs, we always want to add to any existing
    properties instead of overwriting them, so do exactly that.
    
    With this change we now find the latest top-level PropertyGroup in the project
    file with no Condition, and add a MtouchExtraArgs/MonoBundlingArgs that adds
    to any existing property.

--- tests/xharness/Jenkins/TestVariationsFactory.cs
+++ tests/xharness/Jenkins/TestVariationsFactory.cs
@@ -225,11 +225,13 @@ namespace Xharness.Jenkins {
 						var canSymlink = task.Platform.CanSymlink ();
 
 						if (!string.IsNullOrEmpty (mtouch_extra_args))
-							clone.Xml.AddExtraMtouchArgs (mtouch_extra_args, task.ProjectPlatform, configuration);
+							clone.Xml.AppendExtraMtouchArgs (mtouch_extra_args);
 						if (!string.IsNullOrEmpty (bundling_extra_args))
-							clone.Xml.AddMonoBundlingExtraArgs (bundling_extra_args, task.ProjectPlatform, configuration);
-						if (!string.IsNullOrEmpty (link_mode))
-							clone.Xml.SetNode (isMac ? "LinkMode" : "MtouchLink", link_mode, task.ProjectPlatform, configuration);
+							clone.Xml.AppendMonoBundlingExtraArgs (bundling_extra_args);
+						if (!string.IsNullOrEmpty (link_mode)) {
+							clone.Xml.SetProperty ("LinkMode", link_mode);
+							clone.Xml.SetProperty ("MtouchLink", link_mode);
+						}
 						if (!string.IsNullOrEmpty (defines)) {
 							clone.Xml.AddAdditionalDefines (defines, task.ProjectPlatform, configuration);
 							if (clone.ProjectReferences != null) {
