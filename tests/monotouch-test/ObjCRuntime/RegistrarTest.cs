185a9a8c9a6b393fc091ee335215d1ef9b596b9a
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-05-05 11:24:26 +0200

    [xharness] Add a release + all optimization test variation of monotouch-test for desktop. (#18140)
    Add a 'release + all optimization' test variation of monotouch-test for
    macOS and Mac Catalyst.

--- tests/monotouch-test/ObjCRuntime/RegistrarTest.cs
+++ tests/monotouch-test/ObjCRuntime/RegistrarTest.cs
@@ -1450,10 +1450,8 @@ namespace MonoTouchFixtures.ObjCRuntime {
 		{
 			var cl = new Class (typeof (TestTypeEncodingsClass));
 			var sig = Runtime.GetNSObject<NSMethodSignature> (Messaging.IntPtr_objc_msgSend_IntPtr (cl.Handle, Selector.GetHandle ("methodSignatureForSelector:"), Selector.GetHandle ("foo::::::::::::::::")));
-#if MONOMAC
+#if MONOMAC || __MACCATALYST__
 			var boolEncoding = TrampolineTest.IsArm64CallingConvention ? "B" : "c";
-#elif __MACCATALYST__
-			var boolEncoding = "B";
 #else
 			var boolEncoding = (IntPtr.Size == 8 || TrampolineTest.IsArmv7k || TrampolineTest.IsArm64CallingConvention) ? "B" : "c";
 
