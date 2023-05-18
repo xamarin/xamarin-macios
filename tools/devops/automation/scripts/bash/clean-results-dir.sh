b878d0b9090ea340e2e375ac5b55818ede1cd273
Manuel de la Pena <mandel@microsoft.com>
2023-05-18 16:35:23 -0400

    Fix clean dirs bash.

--- tools/devops/automation/scripts/bash/clean-results-dir.sh
+++ tools/devops/automation/scripts/bash/clean-results-dir.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 
-rm -Rvf $(Build.SourcesDirectory)/package
-time make -C $(Build.SourcesDirectory)/xamarin-macios/ git-clean-all
+rm -Rvf package
+time make -C xamarin-macios/ git-clean-all
