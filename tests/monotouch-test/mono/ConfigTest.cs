37d5278ea2a991eb571966c429156b7085a4be8c
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-05-31 09:29:51 +0200

    Things are a bit more complicated...

--- tests/monotouch-test/mono/ConfigTest.cs
+++ tests/monotouch-test/mono/ConfigTest.cs
@@ -13,8 +13,18 @@ namespace MonoTouchFixtures {
 		[Test]
 		public void Existence ()
 		{
-			var config_file = Path.Combine (AppContext.BaseDirectory, Assembly.GetExecutingAssembly ().GetName ().Name + ".dll.config");
-			Assert.That (config_file, Does.Exist, "existence");
+			var base_path = Path.Combine (AppContext.BaseDirectory, Assembly.GetExecutingAssembly ().GetName ().Name);
+			var dll_config = base_path + ".dll.config";
+			var exe_config = base_path + ".exe.config";
+			string config_file;
+			if (File.Exists (dll_config)) {
+				config_file = dll_config;
+			} else if (File.Exists (exe_config)) {
+				config_file = exe_config;
+			} else {
+				Assert.Fail ("No config file found");
+				return;
+			}
 			Assert.That (File.ReadAllText (config_file), Contains.Substring ("<secretMessage>Xamarin rocks</secretMessage>"), "content");
 		}
 	}
