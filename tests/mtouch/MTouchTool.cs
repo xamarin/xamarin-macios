0ae13bb967b751ead20a4406252dbd670b6b76bb
Manuel de la Pena <mandel@microsoft.com>
2023-07-17 23:12:00 -0400

    [WatchOS] Fix tests and code to adapt to the fact that armv7k is not longer supported.

--- tests/mtouch/MTouchTool.cs
+++ tests/mtouch/MTouchTool.cs
@@ -245,7 +245,7 @@ namespace Xamarin {
 			case Profile.tvOS:
 				return isDevice ? "arm64" : "x86_64";
 			case Profile.watchOS:
-				return isDevice ? "armv7k" : "i386";
+				return isDevice ? "arm64_32" : "i386";
 			default:
 				throw new NotImplementedException ();
 			}
