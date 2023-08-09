11941064a277e67babc63bbb0d6f745a0372cc80
Alex Soto <alex@soto.dev>
2023-08-08 21:09:18 -0400

    Ensure that Selected Xcode has an accepted license

--- tools/devops/automation/scripts/bash/ensure-xcode-select.sh
+++ tools/devops/automation/scripts/bash/ensure-xcode-select.sh
@@ -3,7 +3,9 @@
 XCODE_SELECT=$(xcode-select -p)
 if [[ -d $XCODE_SELECT ]]; then
   echo "Using Xcode in path $XCODE_SELECT"
+  sudo xcrun xcodebuild -license accept
 else
   echo "Setting Xcode to point to the default location."
   xcode-select -s "/Applications/Xcode.app/Contents/Developer"
+  sudo xcrun xcodebuild -license accept
 fi
