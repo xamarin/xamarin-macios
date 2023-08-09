25eab7446dac3aa965e14b114551afecf6cd747a
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-08-09 10:49:34 +0200

    Fix boolean logic

--- tests/common/BinLog.cs
+++ tests/common/BinLog.cs
@@ -179,7 +179,7 @@ namespace Xamarin.Tests {
 				// Filter to warnings
 				.Where (v => v.Type == BuildLogEventType.Warning)
 				// We're often referencing earlier .NET projects (Touch.Unit/MonoTouch.Dialog), so ignore any out-of-support warnings.
-				.Where (v => v.Message?.Contains ("is out of support and will not receive security updates in the future") != false)
+				.Where (v => v.Message?.Contains ("is out of support and will not receive security updates in the future") != true)
 				;
 		}
 
