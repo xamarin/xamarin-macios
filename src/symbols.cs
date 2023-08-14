6b8153937945672a94e4e6574709f01458ce921f
Manuel de la Pena <mandel@microsoft.com>
2023-08-14 16:08:54 -0400

    Update src/symbols.cs
    Co-authored-by: Alex Soto <alex@alexsoto.me>

--- src/symbols.cs
+++ src/symbols.cs
@@ -7,6 +7,7 @@ using System;
 namespace Symbols {
 	[Watch (10, 0), TV (17, 0), Mac (14, 0), iOS (17, 0), MacCatalyst (17, 0)]
 	[BaseType (typeof (NSObject))]
+	[DisableDefaultCtor]
 	interface NSSymbolEffect : NSCopying, NSSecureCoding { }
 
 	[Watch (10, 0), TV (17, 0), Mac (14, 0), iOS (17, 0), MacCatalyst (17, 0)]
