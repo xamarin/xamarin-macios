b267425a3f6ade92bbfc1edeb39eca9b1f73348c
Haritha Mohan <harithamohan@microsoft.com>
2023-08-23 11:48:02 -0400

    [mlaunch] Bump mlaunch to get dotnet support
    Ref: https://github.com/xamarin/maccore/pull/2695
    Will update version without the PR id once build is successful.

--- mk/xamarin.mk
+++ mk/xamarin.mk
@@ -18,7 +18,7 @@ endif
 
 # Available versions can be seen here:
 # https://dev.azure.com/dnceng/public/_artifacts/feed/dotnet-eng/NuGet/Microsoft.Tools.Mlaunch/versions
-MLAUNCH_NUGET_VERSION=1.0.58
+MLAUNCH_NUGET_VERSION=1.0.85-ci.pr.gh2695.85
 
 define CheckVersionTemplate
 check-$(1)::
