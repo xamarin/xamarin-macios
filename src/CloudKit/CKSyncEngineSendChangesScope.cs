9313264765514ff26060a780aef7265b360cc7d8
Manuel de la Pena <mandel@microsoft.com>
2023-08-25 13:05:47 -0400

    Fix intro and make more robust code for the construtor.

--- src/CloudKit/CKSyncEngineSendChangesScope.cs
+++ src/CloudKit/CKSyncEngineSendChangesScope.cs
@@ -13,8 +13,11 @@ namespace CloudKit {
 		public CKSyncEngineSendChangesScope (NSSet<CKRecordZoneID>? zoneIds, bool excluded)
 		{
 			if (excluded) {
+				// needs to be converted to an empty set
+				zoneIds ??= new NSSet<CKRecordZoneID> ();
 				InitializeHandle (_InitWithExcludedZoneIds (zoneIds!), "initWithZoneIDs:");
 			} else {
+				// supports a null parameter
 				InitializeHandle (_InitWithZoneIds (zoneIds!), "initWithExcludedZoneIDs:");
 			}
 		}
