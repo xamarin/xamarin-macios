aee7cac3daccb84f191e68264e41191f491f9a62
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-08-28 14:48:28 +0200

    Fix another build error.

--- tests/dotnet/UnitTests/PostBuildTest.cs
+++ tests/dotnet/UnitTests/PostBuildTest.cs
@@ -32,7 +32,7 @@ namespace Xamarin.Tests {
 			var findString = "Output Property: ArchiveDir";
 			var archiveDirRecord = records.Where (v => v?.Args?.Message?.Contains (findString) == true).ToList ();
 			Assert.That (archiveDirRecord.Count, Is.GreaterThan (0), "ArchiveDir");
-			var archiveDir = archiveDirRecord [0].Args.Message.Substring (findString.Length + 1).Trim ();
+			var archiveDir = archiveDirRecord [0].Args.Message?.Substring (findString.Length + 1)?.Trim ();
 			Assert.That (archiveDir, Does.Exist, "Archive directory existence");
 			AssertDSymDirectory (appPath);
 		}
