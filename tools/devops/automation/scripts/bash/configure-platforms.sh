c7095a9b0ad394064f4b29b2eb0375c393f5d03a
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-08-29 10:06:46 +0200

    [release/7.0.3xx] [dotnet] Parameterize the pack names.
    We're going to change the pack names to support multi-targeting, so ahead
    of the pack name change I'm changing the existing logic to use a variable
    for the pack name in most places (this will make the rename much easier and
    simpler).
    
    These changes should have no effect by themselves.
    
    Backport of #18732.

--- tools/devops/automation/scripts/bash/configure-platforms.sh
+++ tools/devops/automation/scripts/bash/configure-platforms.sh
@@ -42,6 +42,34 @@ for platform in $DOTNET_PLATFORMS; do
 	PLATFORM_UPPER=$(echo "$platform" | tr '[:lower:]' '[:upper:]')
 	echo "##vso[task.setvariable variable=INCLUDE_DOTNET_$PLATFORM_UPPER;isOutput=true]1"
 	DISABLED_DOTNET_PLATFORMS=${DISABLED_DOTNET_PLATFORMS/ $platform / }
+
+	VARIABLE="${PLATFORM_UPPER}_NUGET_VERSION_NO_METADATA"
+	make -C "$BUILD_SOURCESDIRECTORY/xamarin-macios/tools/devops" print-variable-value-to-file FILE="$FILE" VARIABLE="$VARIABLE"
+	VALUE=$(cat "$FILE")
+	echo "##vso[task.setvariable variable=$VARIABLE;isOutput=true]$VALUE"
+
+	VARIABLE="${PLATFORM_UPPER}_NUGET_SDK_NAME"
+	make -C "$BUILD_SOURCESDIRECTORY/xamarin-macios/tools/devops" print-variable-value-to-file FILE="$FILE" VARIABLE="$VARIABLE"
+	VALUE=$(cat "$FILE")
+	echo "##vso[task.setvariable variable=$VARIABLE;isOutput=true]$VALUE"
+
+	VARIABLE="${PLATFORM_UPPER}_NUGET_REF_NAME"
+	make -C "$BUILD_SOURCESDIRECTORY/xamarin-macios/tools/devops" print-variable-value-to-file FILE="$FILE" VARIABLE="$VARIABLE"
+	VALUE=$(cat "$FILE")
+	echo "##vso[task.setvariable variable=$VARIABLE;isOutput=true]$VALUE"
+
+	VARIABLE="DOTNET_${PLATFORM_UPPER}_RUNTIME_IDENTIFIERS"
+	make -C "$BUILD_SOURCESDIRECTORY/xamarin-macios/tools/devops" print-variable-value-to-file FILE="$FILE" VARIABLE="$VARIABLE"
+	VALUE=$(cat "$FILE")
+	echo "##vso[task.setvariable variable=$VARIABLE;isOutput=true]$VALUE"
+
+	RIDS=$VALUE
+	for rid in $RIDS; do
+		VARIABLE="${rid}_NUGET_RUNTIME_NAME"
+		make -C "$BUILD_SOURCESDIRECTORY/xamarin-macios/tools/devops" print-variable-value-to-file FILE="$FILE" VARIABLE="$VARIABLE"
+		VALUE=$(cat "$FILE")
+		echo "##vso[task.setvariable variable=$VARIABLE;isOutput=true]$VALUE"
+	done
 done
 for platform in $DISABLED_DOTNET_PLATFORMS; do
 	PLATFORM_UPPER=$(echo "$platform" | tr '[:lower:]' '[:upper:]')
