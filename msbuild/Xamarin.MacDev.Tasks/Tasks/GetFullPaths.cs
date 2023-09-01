ab6e20afa9a4625a5084fc11ca99239fc3358db5
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-09-01 15:22:58 +0200

    [dotnet] Point app extensions to any frameworks in the root app bundle. Fixes #17876.
    Put any frameworks in app extensions in the Frameworks directory in the
    containing app bundle. This saves a lot of space if the same framework is used
    in both an app extension and the containing project (or multiple app
    extensions).
    
    Fixes https://github.com/xamarin/xamarin-macios/issues/17876.
    Fixes https://github.com/xamarin/xamarin-macios/issues/17679.

---
+++ msbuild/Xamarin.MacDev.Tasks/Tasks/GetFullPaths.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+using Xamarin.MacDev.Tasks;
+using Xamarin.Messaging.Build.Client;
+
+#nullable enable
+
+namespace Xamarin.MacDev.Tasks {
+	public class GetFullPaths : XamarinTask, ICancelableTask {
+		[Required]
+		public ITaskItem[] Items { get; set; } = Array.Empty<ITaskItem> ();
+
+		public string[] Metadata { get; set; } = Array.Empty<string> ();
+
+		[Output]
+		public ITaskItem[] Output { get; set; } = Array.Empty<ITaskItem> ();
+
+		public override bool Execute ()
+		{
+			if (ShouldExecuteRemotely ())
+				return new TaskRunner (SessionId, BuildEngine4).RunAsync (this).Result;
+
+			return ExecuteLocally ();
+		}
+
+		public void Cancel ()
+		{
+			if (ShouldExecuteRemotely ())
+				BuildConnection.CancelAsync (BuildEngine4).Wait ();
+		}
+
+		bool ExecuteLocally ()
+		{
+			var rv = new List<ITaskItem> ();
+
+			foreach (var item in Items) {
+				var identity = item.ItemSpec;
+				if (Metadata.Length == 0 || Array.IndexOf (Metadata, "Identity") >= 0)
+					identity = Path.GetFullPath (identity);
+				var newItem = new TaskItem (identity);
+				item.CopyMetadataTo (newItem);
+				foreach (var md in Metadata) {
+					if (string.IsNullOrEmpty (md))
+						continue;
+					if (md == "Identity")
+						continue;
+					newItem.SetMetadata (md, Path.GetFullPath (newItem.GetMetadata (md)));
+				}
+				rv.Add (newItem);
+			}
+
+			Output = rv.ToArray ();
+
+			return !Log.HasLoggedErrors;
+		}
+	}
+}
