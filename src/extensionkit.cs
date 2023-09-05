ccd1b6c20666be070b0140accdaaa8bdddce3f67
Manuel de la Pena <mandel@microsoft.com>
2023-09-05 16:13:29 -0400

    [ExtensionKit] Add support for xcode15 beta7.

--- src/extensionkit.cs
+++ src/extensionkit.cs
@@ -44,8 +44,11 @@ namespace ExtensionKit {
 		[Export ("hostViewControllerWillDeactivate:error:")]
 		void WillDeactivate (EXHostViewController viewController, [NullAllowed] NSError error);
 
+#if !XAMCORE_5_0
+		[Obsoleted (PlatformName.MacOSX, 14, 0, message: "Not longer required as of iOS 14.0")]
 		[Export ("shouldAcceptXPCConnection:")]
 		bool ShouldAcceptXpcConnection (NSXpcConnection connection);
+#endif
 	}
 
 	// @interface EXHostViewController : NSViewController
