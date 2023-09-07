6e651e64b0c30c9559dbe77ae8d3116b257e958e
Manuel de la Pena Saenz <mandel@microsoft.com>
2023-09-07 10:10:58 -0400

    Address reviews.

--- src/corespotlight.cs
+++ src/corespotlight.cs
@@ -74,9 +74,14 @@ namespace CoreSpotlight {
 		[Export ("initWithName:protectionClass:")]
 		NativeHandle Constructor (string name, [NullAllowed] NSString protectionClass);
 
+		[EditorBrowsable (EditorBrowsableState.Advanced)]
 		[iOS (9, 0), Mac (10, 11), MacCatalyst (13, 1), NoTV, NoWatch]
 		[Export ("initWithName:protectionClass:bundleIdentifier:options:")]
 		NativeHandle Constructor (string name, [NullAllowed] string protectionClass, string bundleIdentifier, nint options);
+
+		[Wrap ("this (name, protectionClass.GetConstant (), bundleIdentifier, options)")]
+		[iOS (9, 0), Mac (10, 11), MacCatalyst (13, 1), NoTV, NoWatch]
+		NativeHandle Constructor (string name, NSFileProtectionType protectionClass, string bundleIdentifier, nint options);
 
 		[Export ("indexSearchableItems:completionHandler:")]
 		[Async]
