9dca5fd701363f2ef12fcbba9504504dc7769a91
VS MobileTools Engineering Service 2 <vsmobiletoolsengsvc2@microsoft.com>
2023-09-13 10:10:10 -0700

    [release/7.0.3xx] [msbuild] Re-aot referencing assemblies. Fixes #17708. (#18654)
    If an assembly changes, then we must AOT compile that assembly again (which we already
    did), in addition to any assembly that references the modified assembly (which we
    didn't do).
    
    So rework the AOTCompile target: remove the Inputs and Outputs (because the dependency
    tracking is too complicated for MSBuild to resolve), and instead move the logic to
    detect if an assembly must be AOT-compiled again into the AOTCompile task.
    
    Fixes https://github.com/xamarin/xamarin-macios/issues/17708.
    
    Backport of #18509
    
    ---------
    
    Co-authored-by: Rolf Bjarne Kvinge <rolf@xamarin.com>

--- msbuild/Xamarin.MacDev.Tasks/Tasks/AOTCompileTaskBase.cs
+++ msbuild/Xamarin.MacDev.Tasks/Tasks/AOTCompileTaskBase.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
+using Mono.Cecil;
+
 using Xamarin.Localization.MSBuild;
 using Xamarin.Utils;
 
@@ -42,11 +44,111 @@ namespace Xamarin.MacDev.Tasks {
 		public ITaskItem []? FileWrites { get; set; }
 		#endregion
 
+		class AssemblyInfo {
+			public ITaskItem TaskItem;
+			public bool? IsUpToDate;
+
+			public AssemblyInfo (ITaskItem item)
+			{
+				TaskItem = item;
+			}
+		}
+
+		Dictionary<string, AssemblyInfo> assemblyInfos = new Dictionary<string, AssemblyInfo> (StringComparer.OrdinalIgnoreCase);
+
+		string GetAssemblyName (ITaskItem item)
+		{
+			return Path.GetFileNameWithoutExtension (item.ItemSpec);
+		}
+
+		bool IsUpToDate (ITaskItem assembly)
+		{
+			var assemblyPath = assembly.ItemSpec;
+			var key = GetAssemblyName (assembly);
+			if (assemblyInfos.TryGetValue (key, out var info)) {
+				if (!info.IsUpToDate.HasValue) {
+					Log.LogError (MSBStrings.E7119 /* Encountered an assembly reference cycle related to the assembly {0}. */, assemblyPath);
+					info.IsUpToDate = false;
+					return false;
+				}
+				return info.IsUpToDate.Value;
+			}
+
+			info = new AssemblyInfo (assembly);
+			assemblyInfos [key] = info;
+
+			var finfo = new FileInfo (assemblyPath);
+			if (!finfo.Exists) {
+				Log.LogError (MSBStrings.E0158 /* The file {0} does not exist. */, assemblyPath);
+				info.IsUpToDate = false;
+				return false;
+			}
+
+			// ObjectFile is required
+			var objectFile = assembly.GetMetadata ("ObjectFile");
+			if (string.IsNullOrEmpty (objectFile)) {
+				Log.LogError (MSBStrings.E7116 /* The assembly {0} does not provide an 'ObjectFile' metadata. */, assembly.ItemSpec);
+				info.IsUpToDate = false;
+				return false;
+			}
+			var objectFileInfo = new FileInfo (objectFile);
+			if (!IsUpToDate (finfo, objectFileInfo)) {
+				Log.LogMessage (MessageImportance.Low, "The assembly {0} is not up-to-date with regards to the object file {1}", assemblyPath, objectFile);
+				info.IsUpToDate = false;
+				return false;
+			}
+
+			// LLVMFile is optional
+			var llvmFile = assembly.GetMetadata ("LLVMFile");
+			if (!string.IsNullOrEmpty (llvmFile)) {
+				var llvmFileInfo = new FileInfo (llvmFile);
+				if (!IsUpToDate (finfo, llvmFileInfo)) {
+					Log.LogMessage (MessageImportance.Low, "The assembly {0} is not up-to-date with regards to the llvm file {1}", assemblyPath, llvmFile);
+					info.IsUpToDate = false;
+					return false;
+				}
+			}
+
+			// We know now the assembly itself is up-to-date, but what about every referenced assembly?
+			// This assembly must be AOT-compiled again if any referenced assembly has changed as well.
+			using var ad = AssemblyDefinition.ReadAssembly (assembly.ItemSpec, new ReaderParameters { ReadingMode = ReadingMode.Deferred });
+			foreach (var ar in ad.MainModule.AssemblyReferences) {
+				var referencedItems = Assemblies.Where (v => string.Equals (GetAssemblyName (v), ar.Name, StringComparison.OrdinalIgnoreCase)).ToArray ();
+				if (referencedItems.Length == 0) {
+					Log.LogMessage (MessageImportance.Low, $"Ignoring unresolved assembly {ar.Name} (referenced from {assemblyPath}).");
+					continue;
+				} else if (referencedItems.Length > 1) {
+					Log.LogError (MSBStrings.E7117 /* The assembly {0} was passed multiple times as an input assembly (referenced from {1}). */, ar.Name, assemblyPath);
+					info.IsUpToDate = false;
+					return false;
+				}
+				var referencedItem = referencedItems [0];
+				if (!IsUpToDate (referencedItem)) {
+					info.IsUpToDate = false;
+					Log.LogMessage (MessageImportance.Low, "The assembly {0} is not up-to-date with regards to the reference {1}", assemblyPath, ar.Name);
+					return false;
+				}
+			}
+
+			Log.LogMessage (MessageImportance.Low, $"The AOT-compiled code for {assemblyPath} is up-to-date.");
+			info.IsUpToDate = true;
+			return true;
+		}
+
+		bool IsUpToDate (FileInfo input, FileInfo output)
+		{
+			if (!output.Exists)
+				return false;
+			return input.LastWriteTimeUtc <= output.LastWriteTimeUtc;
+		}
+
 		public override bool Execute ()
 		{
 			var inputs = new List<string> (Assemblies.Length);
 			for (var i = 0; i < Assemblies.Length; i++) {
-				inputs.Add (Path.GetFullPath (Assemblies [i].ItemSpec));
+				var input = Path.GetFullPath (Assemblies [i].ItemSpec);
+				inputs.Add (input);
+				Assemblies [i].SetMetadata ("Input", input);
 			}
 
 			// All the assemblies to AOT must be in the same directory
@@ -64,6 +166,13 @@ namespace Xamarin.MacDev.Tasks {
 				InputDirectory = assemblyDirectories [0];
 			}
 
+			// Figure out which assemblies need to be aot'ed, and which are up-to-date.
+			var assembliesToAOT = Assemblies.Where (asm => !IsUpToDate (asm)).ToArray ();
+			if (assembliesToAOT.Length == 0) {
+				Log.LogMessage (MessageImportance.Low, $"All the AOT-compiled code is up-to-date.");
+				return !Log.HasLoggedErrors;
+			}
+
 			Directory.CreateDirectory (OutputDirectory);
 
 			var aotAssemblyFiles = new List<ITaskItem> ();
@@ -74,14 +183,14 @@ namespace Xamarin.MacDev.Tasks {
 
 			var globalAotArguments = AotArguments?.Select (v => v.ItemSpec).ToList ();
 			var listOfArguments = new List<(IList<string> Arguments, string Input)> ();
-			for (var i = 0; i < Assemblies.Length; i++) {
-				var asm = Assemblies [i];
-				var input = inputs [i];
-				var arch = Assemblies [i].GetMetadata ("Arch");
-				var aotArguments = Assemblies [i].GetMetadata ("Arguments");
-				var processArguments = Assemblies [i].GetMetadata ("ProcessArguments");
-				var aotData = Assemblies [i].GetMetadata ("AOTData");
-				var aotAssembly = Assemblies [i].GetMetadata ("AOTAssembly");
+			for (var i = 0; i < assembliesToAOT.Length; i++) {
+				var asm = assembliesToAOT [i];
+				var input = asm.GetMetadata ("Input");
+				var arch = asm.GetMetadata ("Arch");
+				var aotArguments = asm.GetMetadata ("Arguments");
+				var processArguments = asm.GetMetadata ("ProcessArguments");
+				var aotData = asm.GetMetadata ("AOTData");
+				var aotAssembly = asm.GetMetadata ("AOTAssembly");
 
 				var aotAssemblyItem = new TaskItem (aotAssembly);
 				aotAssemblyItem.SetMetadata ("Arguments", "-Xlinker -rpath -Xlinker @executable_path/ -Qunused-arguments -x assembler -D DEBUG");
@@ -110,7 +219,7 @@ namespace Xamarin.MacDev.Tasks {
 				ExecuteAsync (AOTCompilerPath, arg.Arguments, environment: environment, sdkDevPath: SdkDevPath, showErrorIfFailure: false /* we show our own error below */)
 					.ContinueWith ((v) => {
 						if (v.Result.ExitCode != 0)
-							Log.LogError ("Failed to AOT compile {0}, the AOT compiler exited with code {1}", Path.GetFileName (arg.Input), v.Result.ExitCode);
+							Log.LogError (MSBStrings.E7118 /* Failed to AOT compile {0}, the AOT compiler exited with code {1} */, Path.GetFileName (arg.Input), v.Result.ExitCode);
 
 						return System.Threading.Tasks.Task.FromResult<Execution> (v.Result);
 					})
@@ -122,8 +231,8 @@ namespace Xamarin.MacDev.Tasks {
 
 			// For Windows support it's necessary to have the files we're going to create as an Output parameter, so that the files are
 			// created on the windows side too, which makes the Inputs/Outputs logic work properly when working from Windows.
-			var objectFiles = Assemblies.Select (v => v.GetMetadata ("ObjectFile")).Where (v => !string.IsNullOrEmpty (v));
-			var llvmFiles = Assemblies.Select (v => v.GetMetadata ("LLVMFile")).Where (v => !string.IsNullOrEmpty (v));
+			var objectFiles = assembliesToAOT.Select (v => v.GetMetadata ("ObjectFile")).Where (v => !string.IsNullOrEmpty (v));
+			var llvmFiles = assembliesToAOT.Select (v => v.GetMetadata ("LLVMFile")).Where (v => !string.IsNullOrEmpty (v));
 			FileWrites = objectFiles.Union (llvmFiles).Select (v => new TaskItem (v)).ToArray ();
 
 			return !Log.HasLoggedErrors;
