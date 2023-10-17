cdb4150a3d5d3c87ad9c7f48819a14c87db39dde
tj_devel709 <antlambe@microsoft.com>
2023-10-17 13:49:39 -0500

    make static, add intro ignore

--- tests/monotouch-test/PassKit/PKPayLaterViewTest.cs
+++ tests/monotouch-test/PassKit/PKPayLaterViewTest.cs
@@ -17,14 +17,11 @@ namespace MonoTouchFixtures.PassKit {
 		{
 			TestRuntime.AssertXcodeVersion (15, 0);
 
-			var payLaterView = new PKPayLaterView (new NSDecimalNumber (100), "USD");
-
 			// From testing with Xcode 15.0.0, all values give the value of false to the completion handler.
 			// Perhaps this will change in the future.
-			// Also appears to be a static method in Xcode 15.0.0 but not in the headers or docs.
 			// Apple feedback was submitted here: https://feedbackassistant.apple.com/feedback/13268898
 			for (int i = 0; i < 1000; i++){
-				payLaterView.ValidateAmount (new NSDecimalNumber (i), "USD", (eligible) => {
+				PKPayLaterView.ValidateAmount (new NSDecimalNumber (i), "USD", (eligible) => {
 					Assert.False (eligible);
 				});
 			}
