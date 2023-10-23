3dc1ebbd5ee67ac9bf4cb8740439faf897858150
VS MobileTools Engineering Service 2 <vsmobiletoolsengsvc2@microsoft.com>
2023-10-23 06:06:17 -0700

    [release/8.0.1xx-rc2.1] Bump mlaunch to get resource fix. (#19293)
    New commits in xamarin/maccore:
    
    * xamarin/maccore@699ed5eaf2 [mlaunch] Fix the resource name for the
    error/warning strings.
    
    Diff: https://github.com/xamarin/maccore/compare/04b71d98c8..699ed5eaf2
    
    
    Backport of #19291
    
    Co-authored-by: Rolf Bjarne Kvinge <rolf@xamarin.com>

--- mk/xamarin.mk
+++ mk/xamarin.mk
@@ -18,7 +18,7 @@ endif
 
 # Available versions can be seen here:
 # https://dev.azure.com/dnceng/public/_artifacts/feed/dotnet-eng/NuGet/Microsoft.Tools.Mlaunch/versions
-MLAUNCH_NUGET_VERSION=1.0.82
+MLAUNCH_NUGET_VERSION=1.0.83
 
 define CheckVersionTemplate
 check-$(1)::
