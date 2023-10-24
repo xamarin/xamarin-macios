b08153313fb638b482bdb75ad5fa20e7d9f69d89
Manuel de la Pena <mandel@microsoft.com>
2023-10-24 17:45:04 -0400

    Fix a number of selector on catalyst.

--- src/xkit.cs
+++ src/xkit.cs
@@ -2788,7 +2788,7 @@ namespace UIKit {
 		[Export ("dimension")]
 		nfloat Dimension { get; }
 
-		[Watch (10, 0), TV (17, 0), iOS (17, 0), NoMac]
+		[Watch (10, 0), TV (17, 0), iOS (17, 0), NoMac, MacCatalyst (17, 0)]
 		[Static]
 		[Export ("uniformAcrossSiblingsWithEstimate:")]
 		NSCollectionLayoutDimension CreateUniformAcrossSiblings (nfloat estimatedDimension);
