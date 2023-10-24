b3e2859a51fa9c6c15919e6163b4f70f13d92050
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-10-24 08:04:05 +0200

    [devops] Upload crash reports into a different location for each test.

--- tools/devops/automation/scripts/bash/collect-and-upload-crash-reports.sh
+++ tools/devops/automation/scripts/bash/collect-and-upload-crash-reports.sh
@@ -9,10 +9,11 @@ if ! test -d "$HOME/Library/Logs/DiagnosticReports"; then
 elif [[ "$(find "$HOME/Library/Logs/DiagnosticReports" -type f | wc -l)" -eq 0 ]]; then
   echo "No crash reports found"  # nothing to do
 else
-  zip -9rj "$SYSTEM_DEFAULTWORKINGDIRECTORY/crash-reports.zip" "$HOME/Library/Logs/DiagnosticReports"
-  if test -f "$SYSTEM_DEFAULTWORKINGDIRECTORY/crash-reports.zip"; then
+  FILENAME=${MACIOS_UPLOAD_PREFIX}crash-reports-$SYSTEM_STAGEDISPLAYNAME-$BUILD_BUILDID-$SYSTEM_JOBATTEMPT
+  zip -9rj "$SYSTEM_DEFAULTWORKINGDIRECTORY/$FILENAME.zip" "$HOME/Library/Logs/DiagnosticReports"
+  if test -f "$SYSTEM_DEFAULTWORKINGDIRECTORY/$FILENAME.zip"; then
     set +x
-    echo "##vso[artifact.upload containerfolder=CrashReports;artifactname=${MACIOS_UPLOAD_PREFIX}crash-reports-$SYSTEM_STAGEDISPLAYNAME-$BUILD_BUILDID-$SYSTEM_JOBATTEMPT]$SYSTEM_DEFAULTWORKINGDIRECTORY/crash-reports.zip"
+    echo "##vso[artifact.upload containerfolder=CrashReports;artifactname=${FILENAME}]$SYSTEM_DEFAULTWORKINGDIRECTORY/$FILENAME.zip"
     set -x
   fi
 fi
