92587a8b563e2f834aab6cc1e2b563a466c4fe06
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-10-25 08:59:12 +0200

    Try something else.

--- tools/devops/automation/scripts/bash/collect-and-upload-crash-reports.sh
+++ tools/devops/automation/scripts/bash/collect-and-upload-crash-reports.sh
@@ -9,11 +9,16 @@ if ! test -d "$HOME/Library/Logs/DiagnosticReports"; then
 elif [[ "$(find "$HOME/Library/Logs/DiagnosticReports" -type f | wc -l)" -eq 0 ]]; then
   echo "No crash reports found"  # nothing to do
 else
-  FILENAME=${MACIOS_UPLOAD_PREFIX}crash-reports-$SYSTEM_STAGEDISPLAYNAME-$BUILD_BUILDID-$SYSTEM_JOBATTEMPT
+  if test -n "$MACIOS_UPLOAD_PREFIX"; then
+    FILENAME="${MACIOS_UPLOAD_PREFIX}-"
+  if test -n "$MACIOS_TEST_PREFIX"; then
+    FILENAME="$FILENAME${MACIOS_TEST_PREFIX}-"
+  fi
+  FILENAME="${FILENAME}crash-reports-$SYSTEM_STAGEDISPLAYNAME-$SYSTEM_JOBATTEMPT"
   zip -9rj "$SYSTEM_DEFAULTWORKINGDIRECTORY/$FILENAME.zip" "$HOME/Library/Logs/DiagnosticReports"
   if test -f "$SYSTEM_DEFAULTWORKINGDIRECTORY/$FILENAME.zip"; then
     set +x
-    echo "##vso[artifact.upload containerfolder=CrashReports;artifactname=${FILENAME}]$SYSTEM_DEFAULTWORKINGDIRECTORY/$FILENAME.zip"
+    echo "##vso[artifact.upload containerfolder=CrashReports;artifactname=CrashReports]$SYSTEM_DEFAULTWORKINGDIRECTORY/$FILENAME.zip"
     set -x
   fi
 fi
