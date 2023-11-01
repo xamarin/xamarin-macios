35f3644b71276cf1374386f1b0a1ed6c6885ec03
Dustin Wojciechowski <dustin.wojciechowski@microsoft.com>
2023-10-31 23:58:03 -0700

    Changed AppendFormat calls to use composite format strings instead of interpolated strings

--- src/bgen/Generator.cs
+++ src/bgen/Generator.cs
@@ -3451,14 +3451,14 @@ public partial class Generator : IMemberGatherer {
 					var nullable = TypeManager.GetUnderlyingNullableType (et);
 					if (nullable is not null) {
 						var nt = FormatType (mi.DeclaringType, nullable);
-						convs.AppendFormat ($"{nt}* converted_{safe_name} = null;\n");
-						convs.AppendFormat ($"{nt} converted_{safe_name}_v = default ({nt});\n");
-						convs.AppendFormat ($"if ({safe_name}.HasValue) {{\n");
-						convs.AppendFormat ($"\tconverted_{safe_name}_v = {safe_name}.Value;\n");
-						convs.AppendFormat ($"\tconverted_{safe_name} = &converted_{safe_name}_v;\n");
-						convs.AppendFormat ("}\n");
-						by_ref_processing.AppendFormat ($"if ({safe_name}.HasValue)").AppendLine ();
-						by_ref_processing.AppendFormat ($"\t{safe_name} = *converted_{safe_name};").AppendLine ();
+						convs.AppendFormat ("{0}* converted_{1} = null;\n", nt, safe_name);
+						convs.AppendFormat ("{0} converted_{1}_v = default ({0});\n", nt, safe_name);
+						convs.AppendFormat ("if ({0}.HasValue) {1}\n", safe_name, "{");
+						convs.AppendFormat ("\tconverted_{0}_v = {0}.Value;\n", safe_name);
+						convs.AppendFormat ("\tconverted_{0} = &converted_{0}_v;\n", safe_name);
+						convs.AppendFormat ("{0}\n", "}");
+						by_ref_processing.AppendFormat ("if ({0}.HasValue)", safe_name).AppendLine ();
+						by_ref_processing.AppendFormat ("\t{0} = *converted_{0};", safe_name).AppendLine ();
 					}
 				}
 			}
