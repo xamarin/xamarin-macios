8682a7ad64d567add4317817678577f1ff85daac
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-11-16 16:42:56 +0100

    [ModelIO] Include MDLMaterial.cs in the build.
    Looks like we forgot to add this file to the build when we bound the
    corresponding APIs.

--- src/ModelIO/MDLMaterial.cs
+++ src/ModelIO/MDLMaterial.cs
@@ -1,14 +1,14 @@
 #nullable enable
 
 namespace ModelIO {
-	public partial class MDLAsset {
-		public MDLMaterialProperty this [nuint index] {
+	public partial class MDLMaterial {
+		public MDLMaterialProperty? this [nuint index] {
 			get {
 				return ObjectAtIndexedSubscript (index);
 			}
 		}
 
-		public MDLMaterialProperty this [string name] {
+		public MDLMaterialProperty? this [string name] {
 			get {
 				return ObjectForKeyedSubscript (name);
 			}
