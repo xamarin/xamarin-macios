2e3d1003dcafc0ad505c08374096acaddeb3ef1b
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-12-13 13:40:47 +0100

    Refactor according to review.

--- src/bgen/BindingTouch.cs
+++ src/bgen/BindingTouch.cs
@@ -65,7 +65,6 @@ public class BindingTouch : IDisposable {
 	public MetadataLoadContext? universe;
 	public Frameworks? Frameworks;
 
-	Assembly? apiAssembly;
 	AttributeManager? attributeManager;
 	public AttributeManager AttributeManager => attributeManager!;
 
@@ -269,7 +268,7 @@ public class BindingTouch : IDisposable {
 				"mscorlib"
 			);
 
-			if (!TryLoadApi (tmpass, out apiAssembly) || !TryLoadApi (LibraryInfo.BaseLibDll, out Assembly? baselib))
+			if (!TryLoadApi (tmpass, out Assembly? apiAssembly) || !TryLoadApi (LibraryInfo.BaseLibDll, out Assembly? baselib))
 				return 1;
 
 			Frameworks = new Frameworks (CurrentPlatform);
@@ -371,11 +370,6 @@ public class BindingTouch : IDisposable {
 				Directory.Delete (config.TemporaryFileDirectory, true);
 		}
 		return 0;
-	}
-
-	public bool IsApiAssembly (Assembly assembly)
-	{
-		return assembly == apiAssembly;
 	}
 
 	// If anything is modified in this function, check if the _CompileApiDefinitions MSBuild target needs to be updated as well.
