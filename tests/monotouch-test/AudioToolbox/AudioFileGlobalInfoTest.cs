7bc4efca2fcb71ce8feda0db7e2ab0be58dca33f
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-12-14 18:59:50 +0100

    [AudioToolbox] Make P/Invokes in AudioFileGlobalInfo.cs have blittable signatures.
    Contributes towards #15684.

--- tests/monotouch-test/AudioToolbox/AudioFileGlobalInfoTest.cs
+++ tests/monotouch-test/AudioToolbox/AudioFileGlobalInfoTest.cs
@@ -7,7 +7,11 @@
 // Copyright 2012 Xamarin Inc. All rights reserved.
 //
 
+#nullable enable
+
 #if !__WATCHOS__
+using System;
+using System.Linq;
 
 using Foundation;
 using AudioToolbox;
@@ -22,14 +26,40 @@ namespace MonoTouchFixtures.AudioToolbox {
 		[Test]
 		public void Properties ()
 		{
-			var a1 = AudioFileGlobalInfo.ReadableTypes;
-			var a2 = AudioFileGlobalInfo.GetFileTypeName (AudioFileType.MP2);
-			var a3 = AudioFileGlobalInfo.GetAvailableFormats (AudioFileType.MPEG4);
-			var a4 = AudioFileGlobalInfo.GetAvailableStreamDescriptions (AudioFileType.MPEG4, AudioFormatType.MPEG4AAC);
-			var a5 = AudioFileGlobalInfo.AllExtensions;
-			var a6 = AudioFileGlobalInfo.GetExtensions (AudioFileType.MPEG4);
-			var a7 = AudioFileGlobalInfo.GetMIMETypes (AudioFileType.MPEG4);
-			var a8 = AudioFileGlobalInfo.GetUTIs (AudioFileType.MPEG4);
+			Assert.NotNull (AudioFileGlobalInfo.ReadableTypes, "ReadableTypes");
+			Assert.That (AudioFileGlobalInfo.ReadableTypes?.Length, Is.GreaterThan (0), "ReadableTypes #");
+
+			Assert.NotNull (AudioFileGlobalInfo.WritableTypes, "WritableTypes");
+			Assert.That (AudioFileGlobalInfo.WritableTypes?.Length, Is.GreaterThan (0), "WritableTypes #");
+
+			var validFileTypeAndAudioFormatTypeCombinations = 0;
+			foreach (var fileType in Enum.GetValues<AudioFileType> ()) {
+				Assert.NotNull (AudioFileGlobalInfo.GetFileTypeName (fileType), $"GetFileTypeName: {fileType}");
+
+				Assert.NotNull (AudioFileGlobalInfo.GetAvailableFormats (fileType), $"GetAvailableFormats: {fileType}");
+				Assert.That (AudioFileGlobalInfo.GetAvailableFormats (fileType)?.Length, Is.GreaterThan (0), $"GetAvailableFormats #: {fileType}");
+
+				Assert.NotNull (AudioFileGlobalInfo.GetExtensions (fileType), $"GetExtensions: {fileType}");
+				Assert.That (AudioFileGlobalInfo.GetExtensions (fileType)?.Length, Is.GreaterThan (0), $"GetExtensions #: {fileType}");
+
+				Assert.NotNull (AudioFileGlobalInfo.GetMIMETypes (fileType), $"GetMIMETypes: {fileType}");
+				Assert.That (AudioFileGlobalInfo.GetMIMETypes (fileType)?.Length, Is.GreaterThan (0), $"GetMIMETypes #: {fileType}");
+
+				Assert.NotNull (AudioFileGlobalInfo.GetUTIs (fileType), $"GetUTIs: {fileType}");
+				Assert.That (AudioFileGlobalInfo.GetUTIs (fileType)?.Length, Is.GreaterThan (0), $"GetUTIs #: {fileType}");
+
+				foreach (var audioFormatType in Enum.GetValues<AudioFormatType> ()) {
+					var descs = AudioFileGlobalInfo.GetAvailableStreamDescriptions (fileType, audioFormatType);
+					if (descs is not null) {
+						validFileTypeAndAudioFormatTypeCombinations++;
+						Assert.That (descs?.Length ?? -1, Is.GreaterThan (0), $"GetAvailableStreamDescriptions ({fileType}, {audioFormatType}) #");
+					}
+				}
+			}
+			Assert.That (validFileTypeAndAudioFormatTypeCombinations, Is.GreaterThan (50), "Valid FileType And AudioFormatType Combinations");
+
+			Assert.NotNull (AudioFileGlobalInfo.AllExtensions, "AllExtensions");
+			Assert.That (AudioFileGlobalInfo.AllExtensions.Length, Is.GreaterThan (0), $"AllExtensions #");
 		}
 	}
 }
