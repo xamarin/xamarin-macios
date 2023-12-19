d7b8b4d076ed654a54c6d75df7c2a77294201db5
Haritha Mohan <harithamohan@microsoft.com>
2023-12-19 13:03:23 -0500

    bump

--- mk/xamarin.mk
+++ mk/xamarin.mk
@@ -7,7 +7,7 @@ MONO_BRANCH    := $(shell cd $(MONO_PATH) 2> /dev/null && git symbolic-ref --sho
 endif
 
 ifdef ENABLE_XAMARIN
-NEEDED_MACCORE_VERSION := 738cb7da21ae7b141f92ec6b48bf2565c5086a86
+NEEDED_MACCORE_VERSION := 8cab9d8af6b3045c1486fea7ca34e3f7b22748a7
 NEEDED_MACCORE_BRANCH := debug-adr
 
 MACCORE_DIRECTORY := maccore
