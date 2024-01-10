e493ea1e7682a4080b53419348c371921a74dc4a
Michael Cummings <mcumming@microsoft.com>
2024-01-10 14:40:16 -0500

    Sort test configurations by Title

--- tools/devops/automation/scripts/TestResults.psm1
+++ tools/devops/automation/scripts/TestResults.psm1
@@ -524,10 +524,10 @@ function New-ParallelTestsResults {
     }
 
     $testResults = [System.Collections.ArrayList]@()
-    foreach ($suite in $suites.Values) {
+    foreach ($suite in $suites) {
         $label = $suite.Label
         Write-Host "Processing results for $label with $($suite.TestConfigurations.Length) configurations"
-        foreach ($testConfig in $suite.TestConfigurations) {
+        foreach ($testConfig in $suite.TestConfigurations | Sort-Object -Property Title) {
             $title = $testConfig.Title
             $testResult = $null
             Write-Host "`tProcessing config $title"
