4b8443da6820d3f9f6b95cf445f148f879668d52
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-01-25 09:13:13 +0100

    Fix legacy build.

--- tests/monotouch-test/AudioToolbox/AudioFileGlobalInfoTest.cs
+++ tests/monotouch-test/AudioToolbox/AudioFileGlobalInfoTest.cs
@@ -26,40 +26,53 @@ namespace MonoTouchFixtures.AudioToolbox {
 		[Test]
 		public void Properties ()
 		{
-			Assert.NotNull (AudioFileGlobalInfo.ReadableTypes, "ReadableTypes");
-			Assert.That (AudioFileGlobalInfo.ReadableTypes?.Length, Is.GreaterThan (0), "ReadableTypes #");
+			Assert.Multiple (() => {
+				Assert.NotNull (AudioFileGlobalInfo.ReadableTypes, "ReadableTypes");
+				Assert.That (AudioFileGlobalInfo.ReadableTypes?.Length, Is.GreaterThan (0), "ReadableTypes #");
 
-			Assert.NotNull (AudioFileGlobalInfo.WritableTypes, "WritableTypes");
-			Assert.That (AudioFileGlobalInfo.WritableTypes?.Length, Is.GreaterThan (0), "WritableTypes #");
+				Assert.NotNull (AudioFileGlobalInfo.WritableTypes, "WritableTypes");
+				Assert.That (AudioFileGlobalInfo.WritableTypes?.Length, Is.GreaterThan (0), "WritableTypes #");
 
-			var validFileTypeAndAudioFormatTypeCombinations = 0;
-			foreach (var fileType in Enum.GetValues<AudioFileType> ()) {
-				Assert.NotNull (AudioFileGlobalInfo.GetFileTypeName (fileType), $"GetFileTypeName: {fileType}");
+				var validFileTypeAndAudioFormatTypeCombinations = 0;
+#if NET
+				var validAudioFileTypes = Enum.GetValues<AudioFileType> ().ToList ();
+#else
+				var validAudioFileTypes = Enum.GetValues (typeof (AudioFileType)).Cast<AudioFileType> ().ToList ();
+#endif
+				validAudioFileTypes.Remove (AudioFileType.SoundDesigner2); // returns null in most APIs below
 
-				Assert.NotNull (AudioFileGlobalInfo.GetAvailableFormats (fileType), $"GetAvailableFormats: {fileType}");
-				Assert.That (AudioFileGlobalInfo.GetAvailableFormats (fileType)?.Length, Is.GreaterThan (0), $"GetAvailableFormats #: {fileType}");
+				foreach (var fileType in validAudioFileTypes) {
+					Assert.NotNull (AudioFileGlobalInfo.GetFileTypeName (fileType), $"GetFileTypeName: {fileType}");
 
-				Assert.NotNull (AudioFileGlobalInfo.GetExtensions (fileType), $"GetExtensions: {fileType}");
-				Assert.That (AudioFileGlobalInfo.GetExtensions (fileType)?.Length, Is.GreaterThan (0), $"GetExtensions #: {fileType}");
+					Assert.NotNull (AudioFileGlobalInfo.GetAvailableFormats (fileType), $"GetAvailableFormats: {fileType}");
+					Assert.That (AudioFileGlobalInfo.GetAvailableFormats (fileType)?.Length ?? -1, Is.GreaterThan (0), $"GetAvailableFormats #: {fileType}");
 
-				Assert.NotNull (AudioFileGlobalInfo.GetMIMETypes (fileType), $"GetMIMETypes: {fileType}");
-				Assert.That (AudioFileGlobalInfo.GetMIMETypes (fileType)?.Length, Is.GreaterThan (0), $"GetMIMETypes #: {fileType}");
+					Assert.NotNull (AudioFileGlobalInfo.GetExtensions (fileType), $"GetExtensions: {fileType}");
+					Assert.That (AudioFileGlobalInfo.GetExtensions (fileType)?.Length ?? -1, Is.GreaterThan (0), $"GetExtensions #: {fileType}");
 
-				Assert.NotNull (AudioFileGlobalInfo.GetUTIs (fileType), $"GetUTIs: {fileType}");
-				Assert.That (AudioFileGlobalInfo.GetUTIs (fileType)?.Length, Is.GreaterThan (0), $"GetUTIs #: {fileType}");
+					Assert.NotNull (AudioFileGlobalInfo.GetMIMETypes (fileType), $"GetMIMETypes: {fileType}");
+					Assert.That (AudioFileGlobalInfo.GetMIMETypes (fileType)?.Length ?? -1, Is.GreaterThan (0), $"GetMIMETypes #: {fileType}");
 
-				foreach (var audioFormatType in Enum.GetValues<AudioFormatType> ()) {
-					var descs = AudioFileGlobalInfo.GetAvailableStreamDescriptions (fileType, audioFormatType);
-					if (descs is not null) {
-						validFileTypeAndAudioFormatTypeCombinations++;
-						Assert.That (descs?.Length ?? -1, Is.GreaterThan (0), $"GetAvailableStreamDescriptions ({fileType}, {audioFormatType}) #");
+					Assert.NotNull (AudioFileGlobalInfo.GetUTIs (fileType), $"GetUTIs: {fileType}");
+					Assert.That (AudioFileGlobalInfo.GetUTIs (fileType)?.Length ?? -1, Is.GreaterThan (0), $"GetUTIs #: {fileType}");
+
+#if NET
+					foreach (var audioFormatType in Enum.GetValues<AudioFormatType> ()) {
+#else
+					foreach (AudioFormatType audioFormatType in Enum.GetValues (typeof (AudioFormatType))) {
+#endif
+						var descs = AudioFileGlobalInfo.GetAvailableStreamDescriptions (fileType, audioFormatType);
+						if (descs is not null) {
+							validFileTypeAndAudioFormatTypeCombinations++;
+							Assert.That (descs?.Length ?? -1, Is.GreaterThan (0), $"GetAvailableStreamDescriptions ({fileType}, {audioFormatType}) #");
+						}
 					}
 				}
-			}
-			Assert.That (validFileTypeAndAudioFormatTypeCombinations, Is.GreaterThan (50), "Valid FileType And AudioFormatType Combinations");
+				Assert.That (validFileTypeAndAudioFormatTypeCombinations, Is.GreaterThan (50), "Valid FileType And AudioFormatType Combinations");
 
-			Assert.NotNull (AudioFileGlobalInfo.AllExtensions, "AllExtensions");
-			Assert.That (AudioFileGlobalInfo.AllExtensions.Length, Is.GreaterThan (0), $"AllExtensions #");
+				Assert.NotNull (AudioFileGlobalInfo.AllExtensions, "AllExtensions");
+				Assert.That (AudioFileGlobalInfo.AllExtensions.Length, Is.GreaterThan (0), $"AllExtensions #");
+			});
 		}
 	}
 }
