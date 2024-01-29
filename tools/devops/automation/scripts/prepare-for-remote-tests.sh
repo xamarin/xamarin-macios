ff516d1732bca5018fe47c1aefb597b4f65bec8c
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-01-29 09:21:41 +0100

    [tests] Add infrastructure to run tests on Windows that need a remote Mac. Fixes #13924. (#19927)
    Also add a single test that builds a simple project remotely.
    
    This PR might be best reviewed commit-by-commit.
    
    Fixes https://github.com/xamarin/xamarin-macios/issues/13924.

---
+++ tools/devops/automation/scripts/prepare-for-remote-tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eux
+
+# I've seen machines with more than 1gb of Xamarin.Messaging logs, so clean that up.
+if du -hs ~/Library/Logs/Xamarin.Messaging*; then
+	rm -rf ~/Library/Logs/Xamarin.Messaging*
+fi
+
+# Make sure we don't have any old stuff installed
+if du -hs ~/Library/Caches/Xamarin; then
+	rm -rf ~/Library/Caches/Xamarin
+fi
+
+# Install the local .NET we're using into XMA's directory
+# (we can't point XMA to our local directory)
+mkdir -p ~/Library/Caches/Xamarin/XMA/SDKs
+cp -cRH "$BUILD_SOURCESDIRECTORY"/xamarin-macios/builds/downloads/dotnet ~/Library/Caches/Xamarin/XMA/SDKs
