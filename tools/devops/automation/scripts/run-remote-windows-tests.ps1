97241e54bae929a66989defdf17cb64f56cf5c93
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-01-29 20:17:08 +0100

    [tests] Disable verbose logging for 'dotnet test' in a few more places.

--- tools/devops/automation/scripts/run-remote-windows-tests.ps1
+++ tools/devops/automation/scripts/run-remote-windows-tests.ps1
@@ -4,6 +4,8 @@ $Env:ServerUser = $Env:MAC_AGENT_USER
 $Env:ServerPassword = $Env:XMA_PASSWORD
 $Env:_DotNetRootRemoteDirectory = "/Users/$Env:MAC_AGENT_USER/Library/Caches/Xamarin/XMA/SDKs/dotnet/"
 
+# Showing verbose logging in the terminal may cause spurious test failures: https://github.com/microsoft/vstest/issues/4852
+# # "--logger:console;verbosity=detailed"
 & $Env:DOTNET `
     test `
     "$Env:BUILD_SOURCESDIRECTORY/xamarin-macios/tests/dotnet/UnitTests/DotNetUnitTests.csproj" `
@@ -11,7 +13,6 @@ $Env:_DotNetRootRemoteDirectory = "/Users/$Env:MAC_AGENT_USER/Library/Caches/Xam
     --verbosity quiet `
     --settings $Env:BUILD_SOURCESDIRECTORY/xamarin-macios/tests/dotnet/Windows/config.runsettings `
     "--results-directory:$Env:BUILD_SOURCESDIRECTORY/xamarin-macios/jenkins-results/windows-remote-tests/" `
-    "--logger:console;verbosity=detailed" `
     "--logger:trx;LogFileName=$Env:BUILD_SOURCESDIRECTORY/xamarin-macios/jenkins-results/windows-remote-dotnet-tests.trx" `
     "--logger:html;LogFileName=$Env:BUILD_SOURCESDIRECTORY/xamarin-macios/jenkins-results/windows-remote-dotnet-tests.html" `
     "-bl:$Env:BUILD_SOURCESDIRECTORY/xamarin-macios/tests/dotnet/Windows/windows-remote-dotnet-tests.binlog"
