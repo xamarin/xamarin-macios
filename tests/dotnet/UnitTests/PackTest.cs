0e1497f804890a5b870058b9a1edff8574738087
Michael Cummings <mcumming@microsoft.com>
2024-01-31 15:06:53 -0500

    Add one more test to Multiplatform category

--- tests/dotnet/UnitTests/PackTest.cs
+++ tests/dotnet/UnitTests/PackTest.cs
@@ -90,6 +90,7 @@ namespace Xamarin.Tests {
 		}
 
 		[Test]
+		[Category ("Multiplatform")]
 		[TestCase (ApplePlatform.iOS, true)]
 		[TestCase (ApplePlatform.iOS, false)]
 		[TestCase (ApplePlatform.MacCatalyst, true)]
