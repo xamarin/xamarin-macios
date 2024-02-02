44e531e7165ceed87bb4c114db1aee9044d53aaf
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-02-02 06:26:05 +0100

    [MediaPlayer] Make P/Invokes have blittable signatures.
    Also fix the availability attributes for MPVolumeSettingsAlertIsVisible.

--- tests/cecil-tests/BlittablePInvokes.KnownFailures.cs
+++ tests/cecil-tests/BlittablePInvokes.KnownFailures.cs
@@ -375,7 +375,6 @@ namespace Cecil.Tests {
 			"System.Boolean MapKit.MKMapRect::MKMapRectSpans180thMeridian(MapKit.MKMapRect)",
 			"System.Boolean MediaAccessibility.MACaptionAppearance::MACaptionAppearanceAddSelectedLanguage(System.IntPtr,System.IntPtr)",
 			"System.Boolean MediaAccessibility.MAImageCaptioning::MAImageCaptioningSetCaption(System.IntPtr,System.IntPtr,System.IntPtr&)",
-			"System.Boolean MediaPlayer.MPVolumeSettings::AlertIsVisible()",
 			"System.Boolean MetalPerformanceShaders.MPSKernel::MPSSupportsMTLDevice(System.IntPtr)",
 			"System.Boolean MobileCoreServices.UTType::UTTypeConformsTo(System.IntPtr,System.IntPtr)",
 			"System.Boolean MobileCoreServices.UTType::UTTypeEqual(System.IntPtr,System.IntPtr)",
