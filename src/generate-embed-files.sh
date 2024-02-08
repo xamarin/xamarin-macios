a1485898383c8b42650e2a63452f37b8851a3bae
Haritha Mohan <harithamohan@microsoft.com>
2024-02-08 10:41:37 -0800

    change script to be more maintainable

--- src/generate-embed-files.sh
+++ src/generate-embed-files.sh
@@ -1,28 +1,15 @@
 #!/bin/bash -e
 
-find_files() {
-    find "$(realpath "$1")" -type f -name "$2" -exec printf "%s," {} +
-}
-
-build_dir="$1"
-platform="$2"
-
-if [ "$platform" = "macos" ]; then
-    modified_platform="mac"
-else
-    modified_platform="$platform"
-fi
-
-root="$(realpath "$build_dir/../../../..")"
-
-# find generated/untracked files
-# impt to include, otherwise sourcelink test will fail (don't have a valid source file to link to)
-files=$(find_files "$build_dir" "*.g.cs")
-files+=$(find_files "$root/src/build/common" "*.cs")
-files+=$(find_files "$root/src/build/$modified_platform" "AssemblyInfo.cs")
-files+=$(find_files "$root/src/build/$modified_platform" "Constants.cs")
-files+=$(find_files "$root/src/build/dotnet" "Constants.$platform.generated.cs")
-files+=$(find_files "$root/runtime" "*generated.cs") 
-files+=$(find_files "$root/src" "MinimumVersions.cs")
-files="-embed:${files%,}"
-echo $files
+gen_sources="$1"
+dotnet_sources="$2"
+IFS=$'\n' 
+arr=($(<$gen_sources))
+IFS=' '
+read -ra sources_array <<< "$dotnet_sources"
+arr+=("${sources_array[@]}")
+git_files=($(git ls-files))
+IFS=$'\n'
+filtered_files=($(printf "%s\n" "${arr[@]}" | grep -vF "${git_files[*]}"))
+IFS=','
+echo "-embed:${filtered_files[*]}"
+unset IFS
