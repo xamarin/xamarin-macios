648a1c9fdea9ae75b5b9636fa30905f9a28b2f5c
Manuel de la Pena <mandel@microsoft.com>
2024-02-14 12:37:03 -0500

    [CI] Provide the real platform version to APIScan.

--- tools/devops/automation/scripts/Governance.psm1
+++ tools/devops/automation/scripts/Governance.psm1
@@ -11,12 +11,17 @@ class APIScanConfiguration {
         Write-Host "enabledPlatforms: $($this.enabledPlatforms)"
 
         foreach ($platform in $this.enabledPlatforms) {
+            # each platform has its version in an environment variable, we need to get it, this
+            # could have been inlined when assigning the dictionary but we want to be cleaner.
+            $platfformVersionEnvVar = "$($platform.toUpper())_NUGET_OS_VERSION"
+            $platformVersion = $val = (Get-Item -Path env:\$platfformVersionEnvVar).Value
             # dictionary with the secrets needed by each matrix
             $platformVars = [ordered]@{
                 CLIENT_ID = $Env:API_SCAN_CLIENT_ID;
                 TENANT = $Env:API_SCAN_TENANT;
                 SECRET = "`$(API_SCAN_SECRET_$($platform.ToUpper()))";
                 PLATFORM = $platform.ToUpper();
+                VERSION = $platformVersion;
             }
             $vars[$platform] = $platformVars
         }
