da2525b7fb9e852717574c7a88a137c723c4b592
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-02-16 16:53:12 +0100

    [tests] Add test to keep track of trimmer warnings. (#20125)
    Add test to keep track of trimmer warnings and to make sure we don't accidentally
    introduce new ones.
    
    Ref: https://github.com/xamarin/xamarin-macios/issues/10405

--- tests/dotnet/UnitTests/Extensions.cs
+++ tests/dotnet/UnitTests/Extensions.cs
@@ -14,5 +14,72 @@ namespace Xamarin.Tests {
 
 			Assert.Fail ($"No warnings expected, but got:\n\t{string.Join ("\n\t", warnings.Select (v => v.ToString ()))}");
 		}
+
+		public static void AssertWarnings (this IEnumerable<BuildLogEvent> actualWarnings, IEnumerable<ExpectedBuildMessage> expectedWarnings)
+		{
+			// Source paths may be full (and local) paths. So make full paths relative to the root of the repository.
+			actualWarnings = actualWarnings.Select (w => {
+				if (w.File?.StartsWith (Configuration.SourceRoot) == true) {
+					var rv = w.Clone ();
+					rv.File = w.File [(Configuration.SourceRoot.Length + 1)..];
+					return rv;
+				}
+				return w;
+			});
+
+			var newWarnings = actualWarnings.Where (v => !expectedWarnings.Any (x => x.IsMatch (v))).ToArray ();
+			var missingWarnings = expectedWarnings.Where (v => !actualWarnings.Any (x => v.IsMatch (x))).ToArray ();
+
+			if (newWarnings.Length == 0 && missingWarnings.Length == 0)
+				return;
+
+			var sb = new StringBuilder ();
+			sb.AppendLine ($"\t\t\t\texpectedWarnings = new ExpectedBuildMessage [] {{");
+			foreach (var w in actualWarnings.OrderBy (v => v.File).ThenBy (v => v.Message)) {
+				sb.AppendLine ($"\t\t\t\t\tnew ExpectedBuildMessage (\"{w.File}\" /* line {w.LineNumber} */, \"{w.Message}\"),");
+			}
+			sb.AppendLine ($"\t\t\t\t}};");
+			if (newWarnings.Length > 0) {
+				Console.WriteLine ($"Got {newWarnings.Length} new warnings:");
+				Console.WriteLine ();
+				foreach (var evt in newWarnings)
+					Console.WriteLine ($"    {evt.File}:{evt.LineNumber} {evt.Message}");
+			}
+			if (missingWarnings.Length > 0) {
+				Console.WriteLine ($"Did not get {missingWarnings.Length} missing warnings:");
+				Console.WriteLine ();
+				foreach (var evt in missingWarnings)
+					Console.WriteLine ($"    {evt.File}: {evt.Message}");
+			}
+			Console.WriteLine ($"If this is expected, here's the updated list of expected warnings:");
+			Console.WriteLine (sb);
+			Assert.That (newWarnings, Is.Empty, "New warnings");
+			Assert.That (missingWarnings, Is.Empty, "Empty warnings");
+		}
+	}
+
+	public class ExpectedBuildMessage {
+		public string File;
+		public string Message;
+
+		public ExpectedBuildMessage (string file, string message)
+		{
+			File = file;
+			Message = message;
+		}
+
+		public bool IsMatch (BuildLogEvent evt)
+		{
+			if (evt.Message != Message)
+				return false;
+			if (evt.File != File)
+				return false;
+			return true;
+		}
+
+		public override string? ToString ()
+		{
+			return $"{File}: {Message}";
+		}
 	}
 }
