db9d5825cc058f374c33fa0473056630ef1cee53
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-03-06 11:49:45 +0100

    Add documentation.

--- src/DeviceDiscoveryUI/DDDevicePickerViewController.cs
+++ src/DeviceDiscoveryUI/DDDevicePickerViewController.cs
@@ -20,18 +20,32 @@ using OS_nw_error = System.IntPtr;
 namespace DeviceDiscoveryUI {
 
 	public partial class DDDevicePickerViewController {
+		/// <summary>
+		/// The type of the delegate that will be called when the user selects a device in the list of devices.
+		/// </summary>
 		public delegate void DevicePickerCompletionHandler (NWEndpoint? endpoint, NWError? error);
 
+		/// <summary>
+		/// Checks if device discovery is supported for the current device
+		/// </summary>
 		public static bool IsSupported (NWBrowserDescriptor browseDescriptor, NWParameters? parameters) =>
 			_IsSupported (browseDescriptor.GetNonNullHandle (nameof (browseDescriptor)), parameters.GetHandle ());
 
-		[Advice ("Verify if this controller is supported by calling DDDevicePickerViewController.IsSupported before creating an instance, as this will crash if not supported.")]
+		/// <summary>
+		/// A view controller that lists other devices on your network.
+		/// </summary>
+		/// <remarks>
+		/// Verify if this controller is supported by calling DDDevicePickerViewController.IsSupported before creating an instance, as this will crash if not supported.
+		/// </remarks>
 		[DesignatedInitializer]
 		public DDDevicePickerViewController (NWBrowserDescriptor browseDescriptor, NWParameters? parameters) : base (NSObjectFlag.Empty) =>
 			InitializeHandle (
 				_InitWithBrowseDescriptorAndParameters (browseDescriptor.GetNonNullHandle (nameof (browseDescriptor)), parameters.GetHandle ()),
 				"initWithBrowseDescriptor:parameters:");
 
+		/// <summary>
+		/// Set the delegate that will be called when the user selects a device in list of devices.
+		/// </summary>
 		public void SetDevicePicker (DevicePickerCompletionHandler devicePickerCompletionHandler)
 		{
 			_SetDevicePicker (InternalDevicePickerCompletionHandler);
