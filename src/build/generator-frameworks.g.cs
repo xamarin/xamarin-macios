b7af2fd9c956501492c2dc14370d3bc9da6f6ca0
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-03-06 17:12:38 +0100

    Update generated files.

--- src/build/generator-frameworks.g.cs
+++ src/build/generator-frameworks.g.cs
@@ -54,6 +54,7 @@ partial class Frameworks {
 		"CoreText",
 		"CoreVideo",
 		"DeviceCheck",
+		"DeviceDiscoveryExtension",
 		"EventKit",
 		"EventKitUI",
 		"ExternalAccessory",
@@ -603,6 +604,7 @@ partial class Frameworks {
 	bool? _CoreVideo;
 	bool? _CoreWlan;
 	bool? _DeviceCheck;
+	bool? _DeviceDiscoveryExtension;
 	bool? _EventKit;
 	bool? _EventKitUI;
 	bool? _ExecutionPolicy;
@@ -768,6 +770,7 @@ partial class Frameworks {
 	public bool HaveCoreVideo { get { if (!_CoreVideo.HasValue) _CoreVideo = GetValue ("CoreVideo"); return _CoreVideo.Value; } }
 	public bool HaveCoreWlan { get { if (!_CoreWlan.HasValue) _CoreWlan = GetValue ("CoreWlan"); return _CoreWlan.Value; } }
 	public bool HaveDeviceCheck { get { if (!_DeviceCheck.HasValue) _DeviceCheck = GetValue ("DeviceCheck"); return _DeviceCheck.Value; } }
+	public bool HaveDeviceDiscoveryExtension { get { if (!_DeviceDiscoveryExtension.HasValue) _DeviceDiscoveryExtension = GetValue ("DeviceDiscoveryExtension"); return _DeviceDiscoveryExtension.Value; } }
 	public bool HaveEventKit { get { if (!_EventKit.HasValue) _EventKit = GetValue ("EventKit"); return _EventKit.Value; } }
 	public bool HaveEventKitUI { get { if (!_EventKitUI.HasValue) _EventKitUI = GetValue ("EventKitUI"); return _EventKitUI.Value; } }
 	public bool HaveExecutionPolicy { get { if (!_ExecutionPolicy.HasValue) _ExecutionPolicy = GetValue ("ExecutionPolicy"); return _ExecutionPolicy.Value; } }
