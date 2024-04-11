215b925e80acb5ddbdee2165ecafe09c8af98751
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-04-11 11:22:53 +0200

    [src] Stop generating CGPoint, CGRect and CGSize from a t4 file.
    * Adding xml documentation to the t4 file will make it quite complicated and
      ugly.
    * The code for these types change very rarely, so at this point there's not
      much advantage to generating it.

--- src/CoreGraphics/CGPoint.cs
+++ src/CoreGraphics/CGPoint.cs
@@ -1,10 +1,17 @@
 #nullable enable
 
 using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 
+#if !NO_SYSTEM_DRAWING
+using System.Drawing;
+#endif
+
 using CoreFoundation;
+using Foundation;
 using ObjCRuntime;
 
 namespace CoreGraphics {
@@ -16,8 +23,161 @@ namespace CoreGraphics {
 	[SupportedOSPlatform ("macos")]
 	[SupportedOSPlatform ("tvos")]
 #endif
-	// the remaining of the struct is defined inside src/NativeTypes/Drawing.tt
-	public partial struct CGPoint {
+	[Serializable]
+	public struct CGPoint : IEquatable<CGPoint> {
+		nfloat x;
+		nfloat y;
+
+		public static readonly CGPoint Empty;
+
+#if !COREBUILD
+		public static bool operator == (CGPoint l, CGPoint r)
+		{
+			// the following version of Equals cannot be removed by the linker, while == can be
+			return l.Equals (r);
+		}
+
+		public static bool operator != (CGPoint l, CGPoint r)
+		{
+			return l.x != r.x || l.y != r.y;
+		}
+
+		public static CGPoint operator + (CGPoint l, CGSize r)
+		{
+			return new CGPoint (l.x + r.Width, l.y + r.Height);
+		}
+
+		public static CGPoint operator - (CGPoint l, CGSize r)
+		{
+			return new CGPoint (l.x - r.Width, l.y - r.Height);
+		}
+
+#if !NO_SYSTEM_DRAWING
+		public static implicit operator CGPoint (PointF point)
+		{
+			return new CGPoint (point.X, point.Y);
+		}
+
+		public static implicit operator CGPoint (Point point)
+		{
+			return new CGPoint (point.X, point.Y);
+		}
+
+		public static explicit operator PointF (CGPoint point)
+		{
+			return new PointF ((float) point.X, (float) point.Y);
+		}
+
+		public static explicit operator Point (CGPoint point)
+		{
+			return new Point ((int) point.X, (int) point.Y);
+		}
+#endif
+
+		public static CGPoint Add (CGPoint point, CGSize size)
+		{
+			return point + size;
+		}
+
+		public static CGPoint Subtract (CGPoint point, CGSize size)
+		{
+			return point - size;
+		}
+
+		public nfloat X {
+			get { return x; }
+			set { x = value; }
+		}
+
+		public nfloat Y {
+			get { return y; }
+			set { y = value; }
+		}
+
+		public bool IsEmpty {
+			get { return x == 0.0 && y == 0.0; }
+		}
+#endif // !COREBUILD
+
+		public CGPoint (nfloat x, nfloat y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+#if !COREBUILD
+		public CGPoint (double x, double y)
+		{
+			this.x = (nfloat) x;
+			this.y = (nfloat) y;
+		}
+
+		public CGPoint (float x, float y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+		public CGPoint (CGPoint point)
+		{
+			this.x = point.x;
+			this.y = point.y;
+		}
+
+		public static bool TryParse (NSDictionary dictionaryRepresentation, out CGPoint point)
+		{
+			if (dictionaryRepresentation is null) {
+				point = Empty;
+				return false;
+			}
+			unsafe {
+				point = default;
+				return NativeDrawingMethods.CGPointMakeWithDictionaryRepresentation (dictionaryRepresentation.Handle, (CGPoint*) Unsafe.AsPointer<CGPoint> (ref point)) != 0;
+			}
+		}
+
+		public NSDictionary ToDictionary ()
+		{
+			return new NSDictionary (NativeDrawingMethods.CGPointCreateDictionaryRepresentation (this));
+		}
+#endif // !COREBUILD
+
+		public override bool Equals (object obj)
+		{
+			return (obj is CGPoint t) && Equals (t);
+		}
+
+		public bool Equals (CGPoint point)
+		{
+			return point.x == x && point.y == y;
+		}
+
+#if !NET
+		public override int GetHashCode ()
+		{
+			var hash = 23;
+			hash = hash * 31 + x.GetHashCode ();
+			hash = hash * 31 + y.GetHashCode ();
+			return hash;
+		}
+#endif
+
+#if !COREBUILD
+		public void Deconstruct (out nfloat x, out nfloat y)
+		{
+			x = X;
+			y = Y;
+		}
+
+#if !NET
+		public override string ToString ()
+		{
+			return String.Format ("{{X={0}, Y={1}}}",
+				x.ToString (CultureInfo.CurrentCulture),
+				y.ToString (CultureInfo.CurrentCulture)
+			);
+		}
+#endif
 
 #if NET
 		public override int GetHashCode ()
@@ -35,13 +195,12 @@ namespace CoreGraphics {
 		extern static /* NSString* */ IntPtr NSStringFromCGPoint (CGPoint point);
 #endif // MONOMAC
 
-#if !COREBUILD
 		public override string? ToString ()
 		{
 			return CFString.FromHandle (NSStringFromCGPoint (this));
 		}
-#endif
 
 #endif // !NET
+#endif // !COREBUILD
 	}
 }
