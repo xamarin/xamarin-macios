48373839b1f1018f163d8bc0ec5367bbb974fea4
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-04-22 08:38:01 +0200

    [devops] Fix API scan when there are no enabled .NET platforms.

--- tools/devops/automation/scripts/Governance.psm1
+++ tools/devops/automation/scripts/Governance.psm1
@@ -10,6 +10,9 @@ class APIScanConfiguration {
         $vars = [ordered]@{}
         Write-Host "enabledPlatforms: $($this.enabledPlatforms)"
 
+        if ($vars.count -eq 0)
+            return "{}"
+
         foreach ($platform in $this.enabledPlatforms) {
             # each platform has its version in an environment variable, we need to get it, this
             # could have been inlined when assigning the dictionary but we want to be cleaner.
