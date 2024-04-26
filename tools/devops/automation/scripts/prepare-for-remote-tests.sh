7cd62942a61cc5328d329c1e20a6c725321b1324
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-04-26 15:19:43 +0200

    [devops] Add more diagnostics.

--- tools/devops/automation/scripts/prepare-for-remote-tests.sh
+++ tools/devops/automation/scripts/prepare-for-remote-tests.sh
@@ -6,6 +6,9 @@ mkdir -p ~/Library/Caches/Xamarin/XMA/SDKs
 cp -cRH "$BUILD_SOURCESDIRECTORY"/xamarin-macios/builds/downloads/dotnet ~/Library/Caches/Xamarin/XMA/SDKs
 sed '/local-tests-feed/d' "$BUILD_SOURCESDIRECTORY"/xamarin-macios/NuGet.config > ~/Library/Caches/Xamarin/XMA/SDKs/dotnet/NuGet.config
 
+mkdir -p ~/Library/Caches/Xamarin/XMA/SDKs/.home/.nuget/NuGet/
+cp ~/Library/Caches/Xamarin/XMA/SDKs/dotnet/NuGet.config ~/Library/Caches/Xamarin/XMA/SDKs/.home/.nuget/NuGet/NuGet.Config
+
 # some diagnostics
 cat ~/Library/Caches/Xamarin/XMA/SDKs/dotnet/NuGet.config
 cd ~/Library/Caches/Xamarin/XMA/SDKs/dotnet/
