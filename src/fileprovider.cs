1ce124b2dfb048a1da4c7589271cafddaec06633
Alex Soto <alex@soto.dev>
2024-05-21 22:02:17 -0400

    [FileProvider] Update bindings to Xcode 15.3

--- src/fileprovider.cs
+++ src/fileprovider.cs
@@ -181,6 +181,9 @@ namespace FileProvider {
 		VersionNoLongerAvailable = -2009,
 		ExcludedFromSync = -2010,
 		DomainDisabled = -2011,
+		ProviderDomainTemporarilyUnavailable = -2012,
+		ProviderDomainNotFound = -2013,
+		ApplicationExtensionNotFound = -2014,
 	}
 
 	[iOS (16, 0), Mac (12, 0), NoMacCatalyst]
