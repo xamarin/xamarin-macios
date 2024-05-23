59ad500cfde482ec1853be3b5ab93e5711a74400
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-05-23 13:24:45 +0200

    [devops] Unlock the default keychain when removing UI prompts.

--- tools/devops/automation/scripts/bash/remove-ui-prompt.sh
+++ tools/devops/automation/scripts/bash/remove-ui-prompt.sh
@@ -7,3 +7,10 @@ if [ $RC -eq 0 ]; then
 else
   echo "Security UI-prompt could NOT be removed."
 fi
+
+security unlock-keychain -p "$OSX_KEYCHAIN_PASS" || RC=$?
+if [ $RC -eq 0 ]; then
+  echo "Login keychain unlocked."
+else
+  echo "Login keychain could NOT be unlocked ($RC)"
+fi
