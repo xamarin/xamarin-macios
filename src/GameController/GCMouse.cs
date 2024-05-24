382ece87e3f975a5939c674af3c68fbdcacf519b
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-05-24 15:12:25 +0200

    Add the new file to the build.

--- src/GameController/GCMouse.cs
+++ src/GameController/GCMouse.cs
@@ -1,6 +1,8 @@
 #nullable enable
 
 using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 
 using ObjCRuntime;
 using Foundation;
@@ -22,20 +24,30 @@ namespace GameController {
 		}
 
 		[Export ("encodeWithCoder:")]
+#if NET
 		[SupportedOSPlatform ("ios14.0")]
 		[SupportedOSPlatform ("macos")]
 		[SupportedOSPlatform ("tvos14.0")]
 		[SupportedOSPlatform ("maccatalyst")]
+#endif
 		[BindingImpl (BindingImplOptions.Optimizable)]
 		[EditorBrowsable (EditorBrowsableState.Never)]
 		public virtual void EncodeTo (NSCoder encoder)
 		{
 			var encoder__handle__ = encoder!.GetNonNullHandle (nameof (encoder));
+#if NET
 			if (IsDirectBinding) {
 				global::ObjCRuntime.Messaging.void_objc_msgSend_NativeHandle (this.Handle, Selector.GetHandle ("encodeWithCoder:"), encoder__handle__);
 			} else {
 				global::ObjCRuntime.Messaging.void_objc_msgSendSuper_NativeHandle (this.SuperHandle, Selector.GetHandle ("encodeWithCoder:"), encoder__handle__);
 			}
+#else
+			if (IsDirectBinding) {
+				global::ObjCRuntime.Messaging.void_objc_msgSend_IntPtr (this.Handle, Selector.GetHandle ("encodeWithCoder:"), encoder__handle__);
+			} else {
+				global::ObjCRuntime.Messaging.void_objc_msgSendSuper_IntPtr (this.SuperHandle, Selector.GetHandle ("encodeWithCoder:"), encoder__handle__);
+			}
+#endif
 		}
 	}
 #endif // !XAMCORE_5_0
