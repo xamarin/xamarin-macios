2ac2aea1eadf38f31fe5efc430e0d6570755cc8a
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-06-18 12:59:25 +0200

    Fix typo.

--- tools/devops/automation/scripts/csharp/test.sh
+++ tools/devops/automation/scripts/csharp/test.sh
@@ -1,4 +1,4 @@
-#/bin/bash -ex
+#!/bin/bash -ex
 
 export BUILD_SOURCESDIRECTORY=/Users/rolf/work/maccore/windows/xamarin-macios
 donut run --project $BUILD_SOURCESDIRECTORY/tools/devops/automation/scripts/csharp/create-html-report/create-html-report.csproj
