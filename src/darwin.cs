9a674240fc8e1142d16b5d42f0c925829a24d92d
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-07-08 18:54:59 +0200

    [Darwin] Treat this code as a framework, which simplifies things a little bit.
    Also move the TimeSpec struct to its own file, this will be necessary for Xcode 16.

---
+++ src/darwin.cs
@@ -0,0 +1,2 @@
+// Darwin isn't really a framework, but to simplify our build logic we treat it as such.
+// This also means this file is needed, even though it's empty, because our build logic will automatically include it in the build.
