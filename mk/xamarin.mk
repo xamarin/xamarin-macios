8df3693af6b4d0fa4e0546ccbe8d9b873fe9ac78
Michael Cummings <mcumming@microsoft.com>
2024-08-01 17:23:45 -0400

    Bump xcsync to 1.0.78

--- mk/xamarin.mk
+++ mk/xamarin.mk
@@ -20,7 +20,7 @@ endif
 # https://dev.azure.com/dnceng/public/_artifacts/feed/dotnet-eng/NuGet/Microsoft.Tools.Mlaunch/versions
 # https://dev.azure.com/dnceng/public/_artifacts/feed/dotnet-eng/NuGet/Microsoft.Tools.xcsync/versions
 MLAUNCH_NUGET_VERSION=1.0.142
-XCSYNC_NUGET_VERSION=1.0.52
+XCSYNC_NUGET_VERSION=1.0.78
 
 define CheckVersionTemplate
 check-$(1)::
