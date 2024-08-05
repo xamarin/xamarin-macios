6aca25ad5d44bc0a27a79d0a0bff4802f6ee597a
Michael Cummings (MSFT) <mcumming@microsoft.com>
2024-08-05 12:40:36 -0400

    Add Xcsync tool to workload package (#20931)
    Co-authored-by: Haritha Mohan <harithamohan@microsoft.com>
    Co-authored-by: Alex Soto <alex@soto.dev>
    Co-authored-by: Rolf Bjarne Kvinge <rolf@xamarin.com>

--- mk/xamarin.mk
+++ mk/xamarin.mk
@@ -18,7 +18,9 @@ endif
 
 # Available versions can be seen here:
 # https://dev.azure.com/dnceng/public/_artifacts/feed/dotnet-eng/NuGet/Microsoft.Tools.Mlaunch/versions
+# https://dev.azure.com/dnceng/public/_artifacts/feed/dotnet-eng/NuGet/Microsoft.Tools.xcsync/versions
 MLAUNCH_NUGET_VERSION=1.0.142
+XCSYNC_NUGET_VERSION=1.0.86
 
 define CheckVersionTemplate
 check-$(1)::
