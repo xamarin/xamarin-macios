7df6d7282eca6ed0216c0eafcc8c0b998c8e8325
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-08-21 17:45:21 +0200

    [bgen] Fix generating delegates with nullable return types. #17130.
    Fixes https://github.com/xamarin/xamarin-macios/issues/17130.

---
+++ tests/generator/tests/delegate-nullable-return.cs
@@ -0,0 +1,17 @@
+using System;
+
+using Foundation;
+using ObjCRuntime;
+
+namespace NS {
+    [return: NullAllowed]
+    delegate NSObject MyCallback ([NullAllowed] NSObject obj);
+
+    [BaseType (typeof (NSObject))]
+    interface Widget
+    {
+        [Export ("foo")]
+        [NullAllowed]
+        MyCallback Foo { get; set; }
+    }
+}
