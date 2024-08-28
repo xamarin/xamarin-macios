41241b1e0abb934f3eb2a796cd7c2d37b9b2dd81
Michael Cummings (MSFT) <mcumming@microsoft.com>
2024-08-28 07:53:25 -0400

    [release/9.0.1xx-rc1] Remove xcsync from SDK and install as .NET Global Tool (#21127)
    This change removes the xcsync tool as an installed component of the SDK
    and uses it as a .NET Global Tool. The existing targets will continue to
    work, they will check if the tool is already installed, and if not,
    install it.
    
    Backport from: https://github.com/xamarin/xamarin-macios/pull/21126

--- mk/xamarin.mk
+++ mk/xamarin.mk
@@ -18,9 +18,7 @@ endif
 
 # Available versions can be seen here:
 # https://dev.azure.com/dnceng/public/_artifacts/feed/dotnet-eng/NuGet/Microsoft.Tools.Mlaunch/versions
-# https://dev.azure.com/dnceng/public/_artifacts/feed/dotnet-eng/NuGet/Microsoft.Tools.xcsync/versions
 MLAUNCH_NUGET_VERSION=1.0.142
-XCSYNC_NUGET_VERSION=1.0.90
 
 define CheckVersionTemplate
 check-$(1)::
