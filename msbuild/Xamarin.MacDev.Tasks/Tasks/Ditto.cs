9ef843e372152aba4bfca43e932cfb3de2e56209
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-09-20 19:04:38 +0200

    [msbuild] Convert Ditto to a XamarinTask.

--- msbuild/Xamarin.MacDev.Tasks/Tasks/Ditto.cs
+++ msbuild/Xamarin.MacDev.Tasks/Tasks/Ditto.cs
@@ -4,14 +4,16 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
 using Xamarin.Messaging.Build.Client;
+using Xamarin.Utils;
 
 namespace Xamarin.MacDev.Tasks {
-	public class Ditto : XamarinToolTask, ITaskCallback {
+	public class Ditto : XamarinTask, ICancelableTask {
 		#region Inputs
 
 		public string? AdditionalArguments { get; set; }
@@ -40,31 +42,7 @@ namespace Xamarin.MacDev.Tasks {
 
 		#endregion
 
-		protected override string ToolName {
-			get { return "ditto"; }
-		}
-
-		protected override string GenerateFullPathToTool ()
-		{
-			if (!string.IsNullOrEmpty (ToolPath))
-				return Path.Combine (ToolPath, ToolExe);
-
-			var path = Path.Combine ("/usr/bin", ToolExe);
-
-			return File.Exists (path) ? path : ToolExe;
-		}
-
-		protected override string GenerateCommandLineCommands ()
-		{
-			var args = new CommandLineArgumentBuilder ();
-
-			args.AddQuoted (Path.GetFullPath (Source!.ItemSpec));
-			args.AddQuoted (Path.GetFullPath (Destination!.ItemSpec));
-			if (!string.IsNullOrEmpty (AdditionalArguments))
-				args.Add (AdditionalArguments);
-
-			return args.ToString ();
-		}
+		CancellationTokenSource? cancellationTokenSource;
 
 		public override bool Execute ()
 		{
@@ -76,8 +54,25 @@ namespace Xamarin.MacDev.Tasks {
 				return taskRunner.RunAsync (this).Result;
 			}
 
-			if (!base.Execute ())
-				return false;
+			var args = new List<string> ();
+			args.Add (Path.GetFullPath (Source!.ItemSpec));
+			args.Add (Path.GetFullPath (Destination!.ItemSpec));
+#if NET
+			if (!string.IsNullOrEmpty (AdditionalArguments)) {
+#else
+			if (AdditionalArguments is not null && !string.IsNullOrEmpty (AdditionalArguments)) {
+#endif
+				if (StringUtils.TryParseArguments (AdditionalArguments, out var additionalArgs, out var ex)) {
+					args.AddRange (additionalArgs);
+				} else {
+					Log.LogError ("Unable to parse the AdditionalArguments: {0}", AdditionalArguments);
+					return false;
+				}
+			}
+
+			cancellationTokenSource = new CancellationTokenSource ();
+			cancellationTokenSource.CancelAfter (TimeSpan.FromMinutes (2)); // FIXME: remove this
+			ExecuteAsync (Log, "/usr/bin/ditto", args, cancellationToken: cancellationTokenSource.Token).Wait ();
 
 			// Create a list of all the files we've copied
 			var copiedFiles = new List<ITaskItem> ();
@@ -96,18 +91,13 @@ namespace Xamarin.MacDev.Tasks {
 			return !Log.HasLoggedErrors;
 		}
 
-		protected override void LogEventsFromTextOutput (string singleLine, MessageImportance messageImportance)
+		public void Cancel ()
 		{
-			// TODO: do proper parsing of error messages and such
-			Log.LogMessage (messageImportance, "{0}", singleLine);
-		}
-
-		public override void Cancel ()
-		{
-			base.Cancel ();
-
-			if (ShouldExecuteRemotely ())
+			if (ShouldExecuteRemotely ()) {
 				BuildConnection.CancelAsync (BuildEngine4).Wait ();
+			} else {
+				cancellationTokenSource?.Cancel ();
+			}
 		}
 
 		public IEnumerable<ITaskItem> GetAdditionalItemsToBeCopied ()
