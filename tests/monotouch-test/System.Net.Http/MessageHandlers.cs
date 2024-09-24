76336c5e402efac939e5f3ddb22697eb87f55ed4
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-09-24 16:27:29 +0200

    Fix Xcode version check.

--- tests/monotouch-test/System.Net.Http/MessageHandlers.cs
+++ tests/monotouch-test/System.Net.Http/MessageHandlers.cs
@@ -675,7 +675,7 @@ namespace MonoTests.System.Net.Http {
 			// The test requires access to the default system keychain on macOS 14 or earlier, which is really
 			// annoying on bots (a password dialog will pop up, blocking the tests). So just not run this test
 			// on anything earlier than macOS 15.0,
-			TestRuntime.CheckXcodeVersion (16, 0);
+			TestRuntime.AssertXcodeVersion (16, 0);
 			var storageFlags = X509KeyStorageFlags.Exportable;
 #else
 			var storageFlags = X509KeyStorageFlags.DefaultKeySet;
