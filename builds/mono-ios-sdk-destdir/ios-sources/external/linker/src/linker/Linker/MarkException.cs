9daafdae64d372f150769a57d4e8ac3853e99292
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-10-10 14:21:42 +0200

    [builds] Remove all legacy logic. (#21307)
    We still need some of the source files from the legacy Mono library, but it's
    only a few files, so just add them to the git repo.
    
    Also implement caching of the .NET download, which speeds up clean rebuilds
    significantly.
    
    Note: all the files in the `builds/mono-ios-sdk-destdir` are a straight import
    from the Mono archive, so those shouldn't need much reviewing (nor will I
    change them unless absolutely necessary).

---
+++ builds/mono-ios-sdk-destdir/ios-sources/external/linker/src/linker/Linker/MarkException.cs
@@ -0,0 +1,25 @@
+using System;
+
+using Mono.Cecil;
+
+namespace Mono.Linker {
+	public class MarkException : Exception {
+		public MethodDefinition Method { get; private set; }
+
+		public MarkException (string message)
+			: base (message)
+		{
+		}
+
+		public MarkException (string message, Exception innerException)
+			: base (message, innerException)
+		{
+		}
+
+		public MarkException (string message, Exception innerException, MethodDefinition method)
+			: base (message, innerException)
+		{
+			Method = method;
+		}
+	}
+}
