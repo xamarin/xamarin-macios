b546c9efe86398ee9e19b95386da0f2af85a5282
Manuel de la Pena <mandel@microsoft.com>
2024-10-22 12:02:43 -0400

    [Rgen] Set the projects to fail on warnings. Fix warnings.

--- src/rgen/Microsoft.Macios.Generator/Emitters/EnumEmitter.cs
+++ src/rgen/Microsoft.Macios.Generator/Emitters/EnumEmitter.cs
@@ -6,7 +6,9 @@ using Microsoft.Macios.Generator.Context;
 
 namespace Microsoft.Macios.Generator.Emitters;
 
+#pragma warning disable CS9113 // Parameter is unread. This class is work in progress
 class EnumEmitter (ISymbolBindingContext<EnumDeclarationSyntax> context, TabbedStringBuilder builder)
+#pragma warning restore CS9113 // Parameter is unread.
 	: ICodeEmitter {
 
 	public string SymbolName => $"{context.SymbolName}Extensions";
--- src/rgen/Microsoft.Macios.Generator/Emitters/InterfaceEmitter.cs
+++ src/rgen/Microsoft.Macios.Generator/Emitters/InterfaceEmitter.cs
@@ -6,7 +6,9 @@ using Microsoft.Macios.Generator.Context;
 
 namespace Microsoft.Macios.Generator.Emitters;
 
+#pragma warning disable CS9113 // Parameter is unread, this class is work in progress
 class InterfaceEmitter (ISymbolBindingContext<InterfaceDeclarationSyntax> context, TabbedStringBuilder builder) : ICodeEmitter {
+#pragma warning restore CS9113 // Parameter is unread.
 	public string SymbolName { get; } = string.Empty;
 	public bool TryEmit ([NotNullWhen (false)] out ImmutableArray<Diagnostic>? diagnostics)
 	{
