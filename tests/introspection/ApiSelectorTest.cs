883ea42e3fbf73f6d70eb691c48444a857997780
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-11-01 10:43:14 +0100

    Fix introspection.

--- tests/introspection/ApiSelectorTest.cs
+++ tests/introspection/ApiSelectorTest.cs
@@ -1063,6 +1063,13 @@ namespace Introspection {
 				}
 				break;
 #endif // __MACCATALYST__
+			case "UIBarButtonItem":
+				switch (selectorName) {
+				case "xamarinInvokeCallback:":
+					// This is an selector we use for our own purposes
+					return true;
+				}
+				break;
 			}
 
 			// old binding mistake
