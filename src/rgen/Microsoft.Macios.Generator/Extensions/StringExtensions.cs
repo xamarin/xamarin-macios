db526040e28dac8933b7e6f3e282cc4aaa526aae
Manuel de la Pena <mandel@microsoft.com>
2024-11-02 13:30:41 -0400

    Improve is valid identifier method.

--- src/rgen/Microsoft.Macios.Generator/Extensions/StringExtensions.cs
+++ src/rgen/Microsoft.Macios.Generator/Extensions/StringExtensions.cs
@@ -6,5 +6,10 @@ namespace Microsoft.Macios.Generator.Extensions;
 static class StringExtensions {
 
 	public static bool IsValidIdentifier ([NotNullWhen (true)] this string? self)
-		=> self is not null && SyntaxFacts.IsValidIdentifier (self);
+	{
+		if (self is null)
+			return false;
+		var kind = SyntaxFacts.GetKeywordKind (self) ;
+		return !SyntaxFacts.IsKeywordKind (kind) && SyntaxFacts.IsValidIdentifier (self);
+	}
 }
