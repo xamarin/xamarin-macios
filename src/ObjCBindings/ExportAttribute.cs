e77e54008a1b142b352803c4ad2f2612942618be
Manuel de la Pena <mandel@microsoft.com>
2024-11-25 12:46:45 -0500

    Fix cecil tests.

--- src/ObjCBindings/ExportAttribute.cs
+++ src/ObjCBindings/ExportAttribute.cs
@@ -12,19 +12,37 @@ namespace ObjCBindings {
 	[AttributeUsage (AttributeTargets.Method | AttributeTargets.Constructor | AttributeTargets.Property)]
 	public class ExportAttribute<T> : Attribute where T : Enum {
 
+		/// <summary>
+		/// Get/Set the selector that is expoed by the decorated method.
+		/// </summary >
 		public string? Selector { get; set; } = null;
 
+		/// <summary>
+		/// Get/Set the export configuration flags.
+		/// </summary >
 		public T? Flags { get; set; } = default (T);
 
+		/// <summary>
+		/// Get/Set the argment sematics to be used with the native method.
+		///  </summary >
 		public ArgumentSemantic ArgumentSemantic { get; set; } = ArgumentSemantic.None;
 
 		protected ExportAttribute () { }
 
+		/// <summary>
+		/// Mark a managed method as a exported selector.
+		/// </summary >
+		/// <param name="selector">The native selector to be exported.</param>
 		public ExportAttribute (string? selector)
 		{
 			Selector = selector;
 		}
 
+		/// <summary>
+		/// Mark a managed method as a exported selector.
+		/// </summary >
+		/// <param name="selector">The native selector to be exported.</param>
+		/// <param name="semantic">The argument semantics to use when callinb the native selector.</param>
 		public ExportAttribute (string? selector, ArgumentSemantic semantic)
 		{
 			Selector = selector;
@@ -32,6 +50,11 @@ namespace ObjCBindings {
 			Flags = default (T);
 		}
 
+		/// <summary>
+		/// Mark a managed method as a exported selector.
+		/// </summary >
+		/// <param name="selector">The native selector to be exported.</param>
+		/// <param name="flags">The configuration flags for the managed mathod."</param>
 		public ExportAttribute (string? selector, T? flags)
 		{
 			Selector = selector;
@@ -39,6 +62,12 @@ namespace ObjCBindings {
 			Flags = flags;
 		}
 
+		/// <summary>
+		/// Mark a managed method as a exported selector.
+		/// </summary >
+		/// <param name="selector">The native selector to be exported.</param>
+		/// <param name="semantic">The argument semantics to use when callinb the native selector.</param>
+		/// <param name="flags">The configuration flags for the managed mathod."</param>
 		public ExportAttribute (string? selector, ArgumentSemantic semantic, T? flags)
 		{
 			Selector = selector;
