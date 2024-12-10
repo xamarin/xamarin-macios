baa95feaf642a428c48d89f9bc8eaec75b159fcc
Michael Cummings <mcumming@microsoft.com>
2024-12-10 10:47:19 -0500

    Build ADR from macios-adr

--- mk/xamarin.mk
+++ mk/xamarin.mk
@@ -7,13 +7,22 @@ MONO_BRANCH    := $(shell cd $(MONO_PATH) 2> /dev/null && git symbolic-ref --sho
 endif
 
 ifdef ENABLE_XAMARIN
-NEEDED_MACCORE_VERSION := 4ebacfa6fcb3737c9fcdd516501fc95582928fbe
+NEEDED_MACCORE_VERSION := 5805eb9adacd8f2aeb2852ffbc78b24fb4a43866
 NEEDED_MACCORE_BRANCH := main
 
 MACCORE_DIRECTORY := maccore
 MACCORE_MODULE    := git@github.com:xamarin/maccore.git
 MACCORE_VERSION   := $(shell cd $(MACCORE_PATH) 2> /dev/null && git rev-parse HEAD 2> /dev/null)
 MACCORE_BRANCH    := $(shell cd $(MACCORE_PATH) 2> /dev/null && git symbolic-ref --short HEAD 2> /dev/null)
+
+
+NEEDED_ADR_VERSION := 9421b2bd2397e692b44464e9fb91307b072c0b7b
+NEEDED_ADR_BRANCH := main
+
+ADR_DIRECTORY := macios-adr
+ADR_MODULE    := https://devdiv@dev.azure.com/devdiv/DevDiv/_git/macios-adr
+ADR_VERSION   := $(shell cd $(ADR_PATH) 2> /dev/null && git rev-parse HEAD 2> /dev/null)
+ADR_BRANCH    := $(shell cd $(ADR_PATH) 2> /dev/null && git symbolic-ref --short HEAD 2> /dev/null)
 endif
 
 # Available versions can be seen here:
@@ -103,5 +112,10 @@ $(MACCORE_PATH):
 	$(Q) git clone --recursive $(MACCORE_MODULE) $(MACCORE_PATH)
 	$(Q) $(MAKE) reset-maccore
 
+$(ADR_PATH):
+	$(Q) git clone --recursive $(ADR_MODULE) $(ADR_PATH)
+	$(Q) $(MAKE) reset-adr
+
 $(eval $(call CheckVersionTemplate,maccore,MACCORE))
+$(eval $(call CheckVersionTemplate,macios-adr,ADR))
 endif
