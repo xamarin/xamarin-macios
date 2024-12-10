276611f3819544f5ce4b9c9da7c36c11948b44b5
Haritha Mohan <harithamohan@microsoft.com>
2024-12-09 16:05:38 -0800

    initial tool framework

---
+++ src/rgen/Microsoft.Macios.Transformer/TransformCommand.cs
@@ -0,0 +1,36 @@
+using System.CommandLine;
+
+namespace Microsoft.Macios.Transformer;
+
+public class TransformCommand : Command {
+
+	public TransformCommand () : base ("rgen-transform", "command to convert outdated bindings to be rgen compatible") {
+		var input = new Option<string> (["--input", "-i"], "input directory to search for bgen bindings") {
+			//single file support?
+			IsRequired = true
+		};
+		AddOption (input);
+
+		var output = new Option<string> (["--output", "-o"], "output directory to write rgen bindings") {
+			IsRequired = true
+		};
+		AddOption (output);
+
+		AddValidator (result => {
+			if (!Directory.Exists (result.GetValueForOption (input))) {
+				result.ErrorMessage = "Input directory does not exist";
+			}
+			if (!Directory.Exists (result.GetValueForOption (output))) {
+				Directory.CreateDirectory (result.GetValueForOption (output)!);
+			}
+		});
+		// this.AddOption (new Option<bool> (new string [] { "--verbose", "-v" }, "verbose output"));
+		
+		this.SetHandler (Execute);
+	}
+
+    public async Task Execute () {
+        // placeholder for loading/parsing/transforming/writing components
+        await Task.CompletedTask;
+    }
+}
