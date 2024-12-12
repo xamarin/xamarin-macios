1d21f578569d4d87ee5a7b7cb9391b0bb608dfa4
Manuel de la Pena <mandel@microsoft.com>
2024-12-12 18:36:15 -0500

    Set http.extraheader for all since we do not care.

--- tools/devops/automation/scripts/set_vsts_extra_header.ps1
+++ tools/devops/automation/scripts/set_vsts_extra_header.ps1
@@ -7,9 +7,6 @@
   git operation in any of our scripts. The way vsts uses git is to update the configuration of the 
   git operations to be performed without extra steps because we have added the token as part of the git configuration.
 
-  .PARAMETER Repository
-  Name of the VSTS repo we are interested in.
-
   .PARAMETER AccessToken
   The vsts token to be used to perform the git operations
 
@@ -19,10 +16,6 @@
 #>
 param
 (
-    [Parameter(Mandatory)]
-    [String]
-    $Repository,
-
     [Parameter(Mandatory)]
     [String]
     $AccessToken,
@@ -35,6 +28,6 @@ param
 # update the config of the repository url to use the correct authentication headers, for more information read: 
 # https://learn.microsoft.com/en-us/azure/devops/organizations/accounts/use-personal-access-tokens-to-authenticate?view=azure-devops&tabs=Windows#use-a-pat
 $headers = "Authorization: Basic " + [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes(":" + $AccessToken))
-git -C "$RepositoryPath" config "http.https://devdiv@dev.azure.com/devdiv/DevDiv/_git/$Repository.extraheader=$headers"
+git -C "$RepositoryPath" config http.extraheader="Authorization: bearer $AccessToken"
 git -C "$RepositoryPath" config --get-all http.extraheader
 
