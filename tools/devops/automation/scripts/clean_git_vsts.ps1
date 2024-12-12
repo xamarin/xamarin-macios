3276298c4e5ebbeff0929bdd4ba5968763f24f46
Manuel de la Pena <mandel@microsoft.com>
2024-12-12 17:51:26 -0500

    [CI] Do not double checkout macios-adr to keep the crendetials.
    Doing a second checkout is error prone. One of the reason is that there
    is no guarantee that any of our other scripts is not going to remove a
    random temp directory (actually, the API diff make does remove it).
    
    If the checkout directory is removed, the post checkout action from VSTS
    will fail with something of the kind:
    
    ```
    [error]An error occurred trying to start process '/usr/local/bin/git'
    with working directory
    '/Users/builder/azdo/_work/22/a/change-detection/tmp/src/macios-adr'. No
    such file or directory
    ```
    
    The correct way to workaround the issue is to 'mimic' the way VSTS uses
    git via https.extraheaders (note that this might not work with old
    versions of git lfs, but should work with versions newer than 2.7.1).
    
    We add a script that will update the configuration of a vsts git repo to
    use an extra base auth header with the system.token of the pipeline,
    which should have access to the repo.
    
    I have added some extra docs in the older script for github so that
    other developers understand the usage and will not make the same mistake
    with API diff again.

--- tools/devops/automation/scripts/clean_git_vsts.ps1
+++ tools/devops/automation/scripts/clean_git_vsts.ps1
@@ -1,3 +1,21 @@
+<#
+  .SYNOPSIS
+  Updates the maccore origin url to contain the given token, allowing this way the CI to checkout the repo.
+
+  .DESCRIPTION
+  Devops does not use ssh to check out the repositories, it uses https. This is problematic when we are trying to do a
+  git operation in any of our scripts. To fix that we can prepend the github token to the checkout url, this allows all
+  git operations to be performed without extra steps because the PAT is part of the url.
+
+  The url pattern for this to wori is "https://$GithubToken@github.com/xamarin/maccore.git"
+
+  .PARAMETER GithubToken
+  The token to be used in the origin url domain.
+
+  .PARAMETER MaccorePath
+  Maccore checkout location.
+
+#>
 param
 (
     [Parameter(Mandatory)]
---
+++ tools/devops/automation/scripts/set_vsts_extra_header.ps1
@@ -0,0 +1,41 @@
+<#
+  .SYNOPSIS
+  Adds extra headers to a vsts repository so that we can perform git operaitons without the need of auth.
+
+  .DESCRIPTION
+  Devops does not use ssh to check out the repositories, it uses https. This is problematic when we are trying to do a
+  git operation in any of our scripts. The way vsts uses git is to update the configuration of the 
+  git operations to be performed without extra steps because we have added the token as part of the git configuration.
+
+  .PARAMETER Repository
+  Name of the VSTS repo we are interested in.
+
+  .PARAMETER AccessToken
+  The vsts token to be used to perform the git operations
+
+  .PARAMETER RepositoryPath
+  The reopsitory checkout location.
+
+#>
+param
+(
+    [Parameter(Mandatory)]
+    [String]
+    $Repository,
+
+    [Parameter(Mandatory)]
+    [String]
+    $AccessToken,
+
+    [Parameter(Mandatory)]
+    [String]
+    $RepositoryPath
+)
+
+# update the config of the repository url to use the correct authentication headers, for more information read: 
+# https://learn.microsoft.com/en-us/azure/devops/organizations/accounts/use-personal-access-tokens-to-authenticate?view=azure-devops&tabs=Windows#use-a-pat
+$user = "AnyUser"
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user, $AccessToken)))
+$headers = @{Authorization = "Basic {0}" -f $base64AuthInfo}
+git -C "$RepositoryPath" config "http.https://devdiv@dev.azure.com/devdiv/DevDiv/_git/$Repository.extraheader" "$headers"
+
