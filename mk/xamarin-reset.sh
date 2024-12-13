ed62e79c760cf2130c229f3cdc5b35b03b6cdb9b
Manuel de la Pena <mandel@microsoft.com>
2024-12-13 06:51:55 -0500

    Try with concatenation.

--- mk/xamarin-reset.sh
+++ mk/xamarin-reset.sh
@@ -16,7 +16,10 @@ elif [[ $DEPENDENCY_MODULE =~ https://github.com ]]; then
 elif [[ $DEPENDENCY_MODULE =~ devdiv@dev.azure.com ]]; then
 	DEPENDENCY_REMOTE=${DEPENDENCY_MODULE/https:\/\/devdiv@dev.azure.com\/devdiv\/}
 	DEPENDENCY_REMOTE=${DEPENDENCY_REMOTE%%/_git*}
-	AUTH_HEADER='http.extraheader="AUTHORIZATION: bearer '"$SYSTEM_ACCESSTOKEN"'"'
+	AUTH_HEADER='http.extraheader="AUTHORIZATION: bearer '
+	AUTH_HEADER+="$SYSTEM_ACCESSTOKEN"
+	AUTH_HEADER+='"'
+	echo "Auth header is $AUTH_HEADER"
 	DEPENDENCY_AUTH=(-c "$AUTH_HEADER")
 fi
 
