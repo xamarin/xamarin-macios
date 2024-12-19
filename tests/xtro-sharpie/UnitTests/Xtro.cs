18376f4ca10752f4434d1281703b930ac446ea5d
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-12-19 16:48:14 +0100

    Fix xtro unit test.

--- tests/xtro-sharpie/UnitTests/Xtro.cs
+++ tests/xtro-sharpie/UnitTests/Xtro.cs
@@ -15,15 +15,21 @@ namespace Xamarin.Tests {
 				"report.zip",
 				"-j", "8",
 			};
-			ExecutionHelper.Execute ("make", args);
+			var rv = ExecutionHelper.Execute ("make", args);
 
 			var report = Path.Combine (dir, "report-dotnet", "index.html");
-			Console.WriteLine ($"Added {report} as attachment.");
-			TestContext.AddTestAttachment (report, "HTML report");
+			if (File.Exists (report)) {
+				Console.WriteLine ($"Added {report} as attachment.");
+				TestContext.AddTestAttachment (report, "HTML report");
+			}
 
 			var zippedReport = Path.Combine (dir, "report-dotnet", "report.zip");
-			Console.WriteLine ($"Added {zippedReport} as attachment.");
-			TestContext.AddTestAttachment (zippedReport, "HTML report (zipped)");
+			if (File.Exists (zippedReport)) {
+				Console.WriteLine ($"Added {zippedReport} as attachment.");
+				TestContext.AddTestAttachment (zippedReport, "HTML report (zipped)");
+			}
+
+			Assert.AreEqual (0, rv.ExitCode, "ExitCode");
 		}
 	}
 }
