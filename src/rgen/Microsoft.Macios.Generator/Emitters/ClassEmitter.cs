93647f1529d5441ee994121a812610815305da38
Manuel de la Pena <mandel@microsoft.com>
2024-12-27 17:54:29 -0500

    [RGen] Reduce the memory pressure of reference objects.
    The code changes structure should have all the needed data to perform
    the code generation. This allows to do the following:
    
    1. Remove the need of using a symbol context. The structure should be
       enough.
    2. With the removal of the context, we can move to use a readonly ref
       parameter to the emit method.
    
    This allows to reduce the memory footprint by:
    
    1. Using the stack more.
    2. Reduce the number of structurs that are copied. in parameters allow
       use to use the structs 'as' ref types.

--- src/rgen/Microsoft.Macios.Generator/Emitters/ClassEmitter.cs
+++ src/rgen/Microsoft.Macios.Generator/Emitters/ClassEmitter.cs
@@ -3,23 +3,25 @@ using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.CodeAnalysis;
 using Microsoft.Macios.Generator.Context;
+using Microsoft.Macios.Generator.DataModel;
 
 namespace Microsoft.Macios.Generator.Emitters;
 
-class ClassEmitter (SymbolBindingContext context, TabbedStringBuilder builder) : ICodeEmitter {
-	public string SymbolNamespace => context.Namespace;
-	public string SymbolName => context.SymbolName;
+#pragma warning disable CS9113 // Parameter is unread. This class is work in progress
+class ClassEmitter (RootBindingContext context, TabbedStringBuilder builder) : ICodeEmitter {
+#pragma warning restore CS9113 // Parameter is unread.
+	public string GetSymbolName (in CodeChanges codeChanges) => codeChanges.Name;
 
 	public IEnumerable<string> UsingStatements => [];
 
-	public bool TryEmit ([NotNullWhen (false)] out ImmutableArray<Diagnostic>? diagnostics)
+	public bool TryEmit (in CodeChanges codeChanges, [NotNullWhen (false)] out ImmutableArray<Diagnostic>? diagnostics)
 	{
 
 		builder.AppendLine ();
 		diagnostics = null;
 		// add the namespace and the class declaration
-		using (var namespaceBlock = builder.CreateBlock ($"namespace {context.Namespace}", true)) {
-			using (var classBlock = namespaceBlock.CreateBlock ($"public partial class {SymbolName}", true)) {
+		using (var namespaceBlock = builder.CreateBlock ($"namespace {codeChanges.Namespace[^1]}", true)) {
+			using (var classBlock = namespaceBlock.CreateBlock ($"public partial class {GetSymbolName (codeChanges)}", true)) {
 				classBlock.AppendLine ("// TODO: add binding code here");
 			}
 		}
