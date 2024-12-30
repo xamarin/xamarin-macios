c2cab4665ec0bbd59ce12777fc36eb0e3d610323
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-12-30 11:29:12 +0100

    [tools] Fix computing the product version for Mac Catalyst.
    This fixes a problem with the version number in error messages when only Mac Catalyst is enabled in the build:
    
    >  warning MT0079: The recommended Xcode version for Microsoft.MacCatalyst @IOS_NUGET_VERSION@ is Xcode 16.2 or later.

--- tools/common/ProductConstants.in.cs
+++ tools/common/ProductConstants.in.cs
@@ -24,10 +24,12 @@ namespace Xamarin {
 		public readonly static ProductConstants iOS = new ProductConstants ("@IOS_NUGET_VERSION@", "@IOS_NUGET_REVISION@");
 		public readonly static ProductConstants tvOS = new ProductConstants ("@TVOS_NUGET_VERSION@", "@TVOS_NUGET_REVISION@");
 		public readonly static ProductConstants macOS = new ProductConstants ("@MACOS_NUGET_VERSION@", "@MACOS_NUGET_REVISION@");
+		public readonly static ProductConstants MacCatalyst = new ProductConstants ("@MACCATALYST_NUGET_VERSION@", "@MACCATALYST_NUGET_VERSION@");
 #else
 		public readonly static ProductConstants iOS = new ProductConstants ("@IOS_VERSION@", "@IOS_REVISION@");
 		public readonly static ProductConstants tvOS = new ProductConstants ("@TVOS_VERSION@", "@TVOS_REVISION@");
 		public readonly static ProductConstants macOS = new ProductConstants ("@MACOS_VERSION@", "@MACOS_REVISION@");
+		public readonly static ProductConstants MacCatalyst = new ProductConstants ("@MACCATALYST_VERSION@", "@MACCATALYST_REVISION@");
 #endif
 	}
 }
