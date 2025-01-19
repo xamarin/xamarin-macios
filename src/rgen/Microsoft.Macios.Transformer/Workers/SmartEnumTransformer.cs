95aa337cd22659489acd931578687a86f0888f4c
Manuel de la Pena <mandel@microsoft.com>
2025-01-19 12:28:20 -0500

    [Rgen] Provide the intial implementation of the migration tool. (#22011)

---
+++ src/rgen/Microsoft.Macios.Transformer/Workers/SmartEnumTransformer.cs
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Marille;
+
+namespace Microsoft.Macios.Transformer.Workers;
+
+public class SmartEnumTransformer (string destinationDirectory) : IWorker<(string Path, string SymbolName)>, IErrorWorker<(string Path, string Example)> {
+
+	public Task ConsumeAsync ((string Path, string SymbolName) message, CancellationToken token = new ())
+	{
+		Console.WriteLine ($"SmartEnumTransformer: Transforming class {message.SymbolName} for path {message.Path} to {destinationDirectory}");
+		return Task.Delay (10);
+	}
+
+	public Task ConsumeAsync ((string Path, string Example) message, Exception exception,
+		CancellationToken token = new CancellationToken ())
+	{
+		return Task.CompletedTask;
+	}
+
+	public void Dispose () { }
+
+	public ValueTask DisposeAsync () => ValueTask.CompletedTask;
+
+	public bool UseBackgroundThread { get => true; }
+}
