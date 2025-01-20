05a3831d8aabf0691a319372590430c2852acc16
Manuel de la Pena <mandel@microsoft.com>
2025-01-20 10:56:34 -0500

    [Rgen] Share DataModel between the generator and the transformer. (#22015)
    In this change we are making the necesary changes to share the data
    model code between the generator and the transformer.
    
    When we talk about sharing code in C# the average dotnet developer would
    create a csproj with the shared code and would reference it. In normal
    circumtances this is not a bad option, but our case is different and
    here are some of the reasons we cannot take that path:
    
    1. Roslyn code generators with more than one dll are problematic to
    distribute. You can already see how bad it is by looking at the dll
    shared between the code generator and the analyzer. The shared library
    has to be added as an analyzer so that it loads with the generator. We
    want to keep the number of dlls to a minimun.
    2. While the data models are similar, they are not identical. The
    attributes used by the code generator and the old api defintions are
    different. We would around this by splitting the definitions in 2 files:
    a - A common file with all date that does not depend on the attributes.
    b - A application specific file that contains the properties needed by
    the common parts BUT that uses structures specific for the application.
    An example is the ExportData. The ExportData is a generic in the
    generator (which contains flags) while it is not in the transformer. We
    also split the TryCreate and Constructor methods since those need to
    init the application specific parths.
    
    In this commit we do not implement the transformer parts since that
    would make the diff hader. We are only moving code around and adding
    dummy implementations for the transformer project to compile.
    
    ---------
    
    Co-authored-by: GitHub Actions Autoformatter <github-actions-autoformatter@xamarin.com>

---
+++ src/rgen/Microsoft.Macios.Generator/DataModel/EnumMember.Generator.cs
@@ -0,0 +1,40 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections.Immutable;
+using Microsoft.Macios.Generator.Attributes;
+using Microsoft.Macios.Generator.Availability;
+using ObjCBindings;
+
+namespace Microsoft.Macios.Generator.DataModel;
+
+readonly partial struct EnumMember {
+
+	/// <summary>
+	/// The data of the field attribute used to mark the value as a binding.
+	/// </summary>
+	public FieldInfo<EnumValue>? FieldInfo { get; }
+
+	/// <summary>
+	/// Create a new change that happened on a member.
+	/// </summary>
+	/// <param name="name">The name of the changed member.</param>
+	/// <param name="libraryName">The library name of the smart enum.</param>
+	/// <param name="libraryPath">The library path to the library, null if it is a known frameworl.</param>
+	/// <param name="fieldData">The binding data attached to this enum value.</param>
+	/// <param name="symbolAvailability">The symbol availability of the member.</param>
+	/// <param name="attributes">The list of attribute changes in the member.</param>
+	public EnumMember (string name,
+		string libraryName,
+		string? libraryPath,
+		FieldData<EnumValue>? fieldData,
+		SymbolAvailability symbolAvailability,
+		ImmutableArray<AttributeCodeChange> attributes)
+	{
+		Name = name;
+		FieldInfo = fieldData is null ? null : new (fieldData.Value, libraryName, libraryPath);
+		SymbolAvailability = symbolAvailability;
+		Attributes = attributes;
+	}
+
+}
---
+++ src/rgen/Microsoft.Macios.Transformer/Attributes/ExportData.cs
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.Macios.Transformer.Attributes;
+
+public struct ExportData : IEquatable<ExportData> {
+
+	public bool Equals (ExportData other)
+	{
+		throw new NotImplementedException ();
+	}
+
+	/// <inheritdoc />
+	public override bool Equals (object? obj)
+	{
+		return obj is ExportData other && Equals (other);
+	}
+
+	/// <inheritdoc />
+	public override int GetHashCode ()
+	{
+		throw new NotImplementedException ();
+	}
+
+	public static bool operator == (ExportData x, ExportData y)
+	{
+		return x.Equals (y);
+	}
+
+	public static bool operator != (ExportData x, ExportData y)
+	{
+		return !(x == y);
+	}
+}
