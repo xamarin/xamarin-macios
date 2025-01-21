8705fb3fadd0c09f293810179add7774ef108ade
Manuel de la Pena Saenz <mandel@microsoft.com>
2025-01-21 18:43:09 -0500

    Try to fix tests in ci by building the needed apidefinitions.

--- tests/rgen/Microsoft.Macios.Transformer.Tests/BaseTransformerTestClass.cs
+++ tests/rgen/Microsoft.Macios.Transformer.Tests/BaseTransformerTestClass.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Collections.Immutable;
 using System.Runtime.CompilerServices;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
