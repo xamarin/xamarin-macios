ace6a4ad5c927cf015db5bc46fd378986f43513e
Manuel de la Pena Saenz <mandel@microsoft.com>
2025-01-21 17:13:28 -0500

    [Rgen] Allow disable porperties as zero copy.

--- src/ObjCBindings/ExportTag.cs
+++ src/ObjCBindings/ExportTag.cs
@@ -97,6 +97,21 @@ namespace ObjCBindings {
 		/// </summary>
 		CustomMarshalDirective = 1 << 5,
 
+		/// <summary>
+		/// Apply to strings parameters that are merely retained or assigned,
+		/// not copied this is an exception as it is advised in the coding
+		/// standard for Objective-C to avoid this, but a few properties do use
+		/// this.  Use this falg for properties flagged with `retain' or
+		/// `assign', which look like this:
+		///
+		/// @property (retain) NSString foo;
+		/// @property (assign) NSString assigned;
+		///
+		/// This forced the generator to create an NSString before calling the
+		/// API instead of using the fast string marshalling code.
+		/// </summary>
+		DisableZeroCopy = 1 << 6,
+
 	}
 }
 
