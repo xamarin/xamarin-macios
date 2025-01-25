8d24a435953a946ecd0d6155f38144adbd09ec84
Manuel de la Pena <mandel@microsoft.com>
2025-01-25 08:07:06 -0500

    [Rgen] Allow to mark bindings as thread safe. (#22045)
    This way we can later decide if we perform the UI thread check.
    
    ---------
    
    Co-authored-by: GitHub Actions Autoformatter <github-actions-autoformatter@xamarin.com>

--- tests/rgen/Microsoft.Macios.Generator.Tests/DataModel/MethodTests.cs
+++ tests/rgen/Microsoft.Macios.Generator.Tests/DataModel/MethodTests.cs
@@ -1,5 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
+
+#pragma warning disable APL0003
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,7 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Macios.Generator.Attributes;
 using Microsoft.Macios.Generator.Availability;
 using Microsoft.Macios.Generator.DataModel;
+using ObjCRuntime;
 using Xamarin.Tests;
 using Xamarin.Utils;
 using Xunit;
@@ -16,6 +19,38 @@ using static Microsoft.Macios.Generator.Tests.TestDataFactory;
 namespace Microsoft.Macios.Generator.Tests.DataModel;
 
 public class MethodTests : BaseGeneratorTestClass {
+
+	[Fact]
+	public void IsThreadSafe ()
+	{
+		var method = new Method (
+			type: "NS.MyClass",
+			name: "MyMethod",
+			returnType: ReturnTypeForVoid (),
+			symbolAvailability: new (),
+			exportMethodData: new ("selector", ArgumentSemantic.None, ObjCBindings.Method.IsThreadSafe),
+			attributes: [],
+			modifiers: [
+				SyntaxFactory.Token (SyntaxKind.PublicKeyword),
+			],
+			parameters: []
+		);
+		Assert.True (method.IsThreadSafe);
+
+		method = new Method (
+			type: "NS.MyClass",
+			name: "MyMethod",
+			returnType: ReturnTypeForVoid (),
+			symbolAvailability: new (),
+			exportMethodData: new ("selector", ArgumentSemantic.None, ObjCBindings.Method.Default),
+			attributes: [],
+			modifiers: [
+				SyntaxFactory.Token (SyntaxKind.PublicKeyword),
+			],
+			parameters: []
+		);
+		Assert.False (method.IsThreadSafe);
+	}
 	class TestDataFromMethodDeclaration : IEnumerable<object []> {
 		public IEnumerator<object []> GetEnumerator ()
 		{
