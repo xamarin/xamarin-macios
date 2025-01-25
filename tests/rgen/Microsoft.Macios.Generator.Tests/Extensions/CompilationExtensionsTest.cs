2196269c391f3922ebbe8222ce174e48f1786a53
Manuel de la Pena <mandel@microsoft.com>
2025-01-25 07:57:01 -0500

    [Rgen] Provide a method to return the current UI namespaces.
    Use the current compilation preprocessor symbols to determine which of
    the UI namespaces are present in the compilation.

--- tests/rgen/Microsoft.Macios.Generator.Tests/Extensions/CompilationExtensionsTest.cs
+++ tests/rgen/Microsoft.Macios.Generator.Tests/Extensions/CompilationExtensionsTest.cs
@@ -1,5 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using Microsoft.Macios.Generator.Extensions;
 using Xamarin.Tests;
 using Xamarin.Utils;
@@ -20,5 +24,77 @@ public class CompilationExtensionsTest : BaseGeneratorTestClass {
 		// the compilation
 		var (compilation, _) = CreateCompilation (platform);
 		Assert.Equal (expectedPlatform, compilation.GetCurrentPlatform ());
+	}
+
+	class TestDataGetUINamespaces : IEnumerable<object []> {
+		public IEnumerator<object []> GetEnumerator ()
+		{
+			yield return [
+				ApplePlatform.iOS, 
+				new [] {
+					"UIKit",
+					"Twitter",
+					"NewsstandKit",
+					"QuickLook",
+					"EventKitUI",
+					"AddressBookUI",
+					"MessageUI",
+					"PhotosUI",
+					"HealthKitUI",
+					"GameKit",
+					"MapKit"
+				}];
+			
+			yield return [
+				ApplePlatform.TVOS, 
+				new [] {
+					"UIKit", 
+					"PhotosUI", 
+					"GameKit", 
+					"MapKit",
+				}];
+			
+			yield return [
+				ApplePlatform.MacOSX, 
+				new [] {
+					"AppKit", 
+					"QuickLook",
+					"PhotosUI",
+					"GameKit",
+					"MapKit",
+				}];
+			
+			yield return [
+				ApplePlatform.MacCatalyst, 
+				new [] {
+					"AppKit", 
+					"UIKit", 
+					"QuickLook", 
+					"EventKitUI", 
+					"MessageUI", 
+					"PhotosUI", 
+					"HealthKitUI", 
+					"GameKit", 
+					"MapKit",
+				}];
+		}
+
+		IEnumerator IEnumerable.GetEnumerator () => GetEnumerator ();
+	}
+	
+	[Theory]
+	[ClassData (typeof(TestDataGetUINamespaces))]
+	public void GetUINamespacesTests (ApplePlatform platform, string [] expectedNamespaces)
+	{
+		var collectionComparer = new CollectionComparer<string> (StringComparer.Ordinal);
+		// we need a source to trigger the parsing. Otherwise the compilation will have no syntax
+		// trees and therefore no preprocessor symbols
+		const string dummyClass = @"
+using System;
+
+public class DummyClass {}
+";
+		var (compilation, _) = CreateCompilation (platform, sources: dummyClass);
+		Assert.True (collectionComparer.Equals (expectedNamespaces, compilation.GetUINamespaces (force: true)));
 	}
 }
