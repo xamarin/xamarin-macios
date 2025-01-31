b623a90787be0a9b571cae9a478db776d188b88f
Manuel de la Pena Saenz <mandel@microsoft.com>
2025-01-31 16:08:23 -0500

    [Rgen] Start implementing the missing parts of the data model for the transformer.
    The transformer and the code generation share 90% of the data model, but
    we need a 10% that is used to fix the mismatch between two projects.
    
    In this change we finish the implementation fo the TypeInfo struct in
    the transformer by providing the TryCreate method that will use the
    attribute data to decide if a type was marked as nullable or not.
    
    Remember that in the old xamarin bindings we did not use the ?
    annotation but we use the NullAllowedAttribute.

--- src/rgen/Microsoft.Macios.Transformer/DataModel/Parameter.Transformer.cs
+++ src/rgen/Microsoft.Macios.Transformer/DataModel/Parameter.Transformer.cs
@@ -2,6 +2,10 @@
 // Licensed under the MIT License.
 
 using Microsoft.Macios.Transformer.Attributes;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Macios.Generator.Extensions;
 
 namespace Microsoft.Macios.Generator.DataModel;
 
@@ -11,4 +15,29 @@ readonly partial struct Parameter {
 	/// Returns the bind from data if present in the binding.
 	/// </summary>
 	public BindAsData? BindAs => BindAsAttribute;
+
+	public static bool TryCreate (IParameterSymbol symbol, ParameterSyntax declaration, SemanticModel semanticModel,
+		[NotNullWhen (true)] out Parameter? parameter)
+	{
+		DelegateInfo? delegateInfo = null;
+		if (symbol.Type is INamedTypeSymbol namedTypeSymbol
+			&& namedTypeSymbol.DelegateInvokeMethod is not null) {
+			DelegateInfo.TryCreate (namedTypeSymbol.DelegateInvokeMethod, out delegateInfo);
+		}
+
+		// retrieve the parameter attributes because those might affect the parameter type, for example, the 
+		// NullAllowed attribute can change the parameter type to be nullable.
+		var parameterAttrs = symbol.GetAttributeData ();
+		parameter = new (symbol.Ordinal, new (symbol.Type, parameterAttrs), symbol.Name) {
+			IsOptional = symbol.IsOptional,
+			IsParams = symbol.IsParams,
+			IsThis = symbol.IsThis,
+			DefaultValue = (symbol.HasExplicitDefaultValue) ? symbol.ExplicitDefaultValue?.ToString () : null,
+			ReferenceKind = symbol.RefKind.ToReferenceKind (),
+			Delegate = delegateInfo,
+			Attributes = declaration.GetAttributeCodeChanges (semanticModel),
+			AttributesDictionary = parameterAttrs,
+		};
+		return true;
+	}
 }
