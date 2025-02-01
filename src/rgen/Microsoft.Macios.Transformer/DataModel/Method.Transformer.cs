fec4ab532e1f21f554486b77abac24cf22a4efeb
Manuel de la Pena Saenz <mandel@microsoft.com>
2025-02-01 07:17:24 -0500

    [Rgen] Mark 'Constructor' named methods as constructors in the transformer.
    The old bindings use that name to denote constructors, these methods in
    the new API will be converted to actual constructors.

--- src/rgen/Microsoft.Macios.Transformer/DataModel/Method.Transformer.cs
+++ src/rgen/Microsoft.Macios.Transformer/DataModel/Method.Transformer.cs
@@ -36,6 +36,12 @@ readonly partial struct Method {
 	/// True if the method was exported with the MarshalNativeExceptions flag allowing it to support native exceptions.
 	/// </summary>
 	public bool MarshalNativeExceptions => HasMarshalNativeExceptionsFlag;
+	
+	const string constructorName = "Constructor";
+	/// <summary>
+	/// True if the method is considered to be a constructor in the old bindings.
+	/// </summary>
+	public bool IsConstructor => Name == constructorName;
 
 	public Method (string type,
 		string name,
