68cbacaa0218efbe3f2e9cdfb719b3651a7204f8
Michael Cummings <mcumming@microsoft.com>
2025-02-11 14:51:33 -0500

    Remove old commented code

--- create-make-config.sh
+++ create-make-config.sh
@@ -33,10 +33,4 @@ if which ccache > /dev/null 2>&1; then
 	echo "Found ccache on the system, enabling it"
 fi
 
-# Detect maccore / xamarin
-# if test -d ../maccore; then
-# 	printf "ENABLE_XAMARIN=1\n" >> "$OUTPUT_FILE"
-# 	echo "Detected the maccore repository, automatically enabled the Xamarin build"
-# fi
-
 mv "$OUTPUT_FILE" "$OUTPUT"
