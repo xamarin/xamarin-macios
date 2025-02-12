4687bcb5758e62e88e50307f8aa3f0a406107a73
Rolf Bjarne Kvinge <rolf@xamarin.com>
2025-02-12 09:36:58 +0100

    [Intents] Clean up code. (#22152)
    * Clean up code that checks OS versions when the version checked for is earlier
      than the earliest OS version we support - in which case the check is
      redundant, and we can remove a lot of dead code.
    * Remove !NET code.
    * Enable nullability everywhere.
    * Add missing availability attributes.
    * Fix a few misc issues found by our tests.
    * Use BindAs in a few places to avoid manual code.

--- src/Intents/INSetSeatSettingsInCarIntent.cs
+++ src/Intents/INSetSeatSettingsInCarIntent.cs
@@ -4,19 +4,17 @@ using Foundation;
 using Intents;
 using ObjCRuntime;
 
+#nullable enable
+
 namespace Intents {
 
 	public partial class INSetSeatSettingsInCarIntent {
-
-#if NET
 		[SupportedOSPlatform ("ios")]
 		[SupportedOSPlatform ("maccatalyst")]
 		[ObsoletedOSPlatform ("ios12.0", "Use the overload that takes 'INSpeakableString carName'.")]
+		[ObsoletedOSPlatform ("maccatalyst13.1", "Use the overload that takes 'INSpeakableString carName'.")]
 		[UnsupportedOSPlatform ("macos")]
 		[UnsupportedOSPlatform ("tvos")]
-#else
-		[Deprecated (PlatformName.iOS, 12, 0, message: "Use the overload that takes 'INSpeakableString carName'.")]
-#endif
 		public INSetSeatSettingsInCarIntent (bool? enableHeating, bool? enableCooling, bool? enableMassage, INCarSeat seat, NSNumber level, INRelativeSetting relativeLevelSetting) :
 			this (enableHeating.HasValue ? new NSNumber (enableHeating.Value) : null, enableCooling.HasValue ? new NSNumber (enableCooling.Value) : null, enableMassage.HasValue ? new NSNumber (enableMassage.Value) : null, seat, level, relativeLevelSetting)
 		{
